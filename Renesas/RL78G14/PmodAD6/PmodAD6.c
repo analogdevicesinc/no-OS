3e0b4710c47ffaf93fe816cd5834c0742201c8ef
Dragos Bogdan <Dragos.Bogdan@Analog.com>
2013-05-23 16:16:09 +0300

    Added the PmodAD6 demonstration software.

---
+++ Renesas/RL78G14/PmodAD6/PmodAD6.c
@@ -0,0 +1,131 @@
+/***************************************************************************//**
+ *   @file   Main.c
+ *   @brief  Implementation of the program's main function.
+ *   @author DNechita (Dan.Nechita@analog.com)
+********************************************************************************
+ * Copyright 2012(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+********************************************************************************
+ *   SVN Revision: $WCREV$
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Option Bytes ***********************************/
+/******************************************************************************/
+#pragma location = "OPTBYTE"
+__root const unsigned char opbyte0 = 0x6E; // Watchdog timer settings.
+#pragma location = "OPTBYTE"
+__root const unsigned char opbyte1 = 0xFF; // Low voltage detector settings.
+#pragma location = "OPTBYTE"
+__root const unsigned char opbyte2 = 0xE8; // Flash op. mode and osc. settings.
+#pragma location = "OPTBYTE"
+__root const unsigned char opbyte3 = 0x85; // On-chip Debug Option Byte.
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "RDKRL78G14.h"     // RDKRL78G14 definitions.
+#include "ST7579.h"         // ST7579 definitions.
+#include "TIME.h"           // TIME definitions.
+#include "AD7091R.h"        // AD7091R definitions.
+
+/******************************************************************************/
+/************************ Variables Definitions *******************************/
+/******************************************************************************/
+float voltage = 0;
+short sample  = 0;
+long  delay   = 0;  
+
+/***************************************************************************//**
+* @brief Draws the ADI logo and displays the name of ADI component.
+*
+* @param componentName - The name of the ADI component.
+*
+* @return None.
+*******************************************************************************/
+void ADI_Component(unsigned char* string)
+{
+    const unsigned char adiComponent[7] =
+    {
+        0xFE, 0xFE, 0x82, 0xC6, 0xEE, 0xFE, 0xFE 
+    };
+    ST7579_Image(0, 0, adiComponent, 7, 8);
+    ST7579_String(0, 9, string, 0);
+}
+
+/***************************************************************************//**
+ * @brief Main function.
+ *
+ * @return None.
+*******************************************************************************/
+void main(void)
+{        
+    /* Initialize RDKRL78G14. */
+    RDKRL78G14_Init();
+    
+    /* Enable interrupts. */
+    __enable_interrupt();
+        
+    /* Initialize timer. */
+    TIME_Init();
+    
+    /* Initialize the ST7579 Display. */
+    ST7579_Init();
+    
+    /* Initialize AD7091R device. */
+    if(AD7091R_Init() == 0)
+    {
+        ADI_Component("AD7091R OK");
+    }
+    else
+    {
+        ADI_Component("AD7091R Error");
+    }
+    while(1)
+    {
+        /* Power up the device. */
+        AD7091R_PowerUp();
+        /* A 50ms delay is required after power-up. */
+        TIME_DelayMs(55);
+               
+        sample = AD7091R_ReadSample();
+        /* Power-down the device to reduce power consumption. */
+        AD7091R_PowerDown();
+        /* Display data on LCD. */
+        voltage = AD7091R_ConvertToVolts(sample, 2.5);
+        ST7579_String(3, 0, "   Voltage[V]:", 0);
+        ST7579_String(5, 2, "     ", 1);
+        ST7579_FloatNumber(5, 2, voltage, 2, 1);
+        TIME_DelayMs(500);
+    }
+}
---
+++ drivers/AD7091R/AD7091R.c
@@ -0,0 +1,169 @@
+/***************************************************************************//**
+ *   @file   AD7091R.c
+ *   @brief  Implementation of AD7091R Driver.
+ *   @author DNechita (Dan.Nechita@analog.com)
+********************************************************************************
+ * Copyright 2012(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+********************************************************************************
+ *   SVN Revision: $WCREV$
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "AD7091R.h"	
+#include "Communication.h"
+
+/******************************************************************************/
+/************************ Functions Definitions *******************************/
+/******************************************************************************/
+
+/***************************************************************************//**
+ * @brief Initializes the SPI communication peripheral.
+ *
+ * @return Result of the initialization procedure.
+ *				Example: -1 - SPI peripheral was not initialized.
+ *					  0 - SPI peripheral is initialized.
+*******************************************************************************/
+char AD7091R_Init(void)
+{
+    unsigned char status = 0;
+    unsigned char tmpVal = 0xFF;
+    
+    status = SPI_Init(0, 8000000, 0, 1);
+    /* Ensures that last state of SDO is high. */
+    SPI_Write(0, &tmpVal, 1);
+    AD7091R_SoftwareReset();
+
+    return status;
+}
+
+/***************************************************************************//**
+ * @brief Initiate a software reset of the device. The AD7091R requires the user
+ *        to initiate a software reset when power is first applied.
+ *
+ * @return None.
+*******************************************************************************/
+void AD7091R_SoftwareReset(void)
+{
+    unsigned char writeByte   = 0xBF;
+
+    /* Initiate a conversion. */
+    SPI_Write(0, &writeByte, 1);
+    /* Short cycle the read operation. */
+    writeByte = 0xFF;
+    SPI_Read(AD7091R_SLAVE_ID, &writeByte, 1);
+    /* Perform another conversion in order to reset the device. */
+    writeByte = 0xBF;
+    SPI_Write(0, &writeByte, 1);
+}
+
+/***************************************************************************//**
+ * @brief Initiates one conversion and reads back the result. During this
+ *        process the device runs in normal mode and operates without the busy
+ *        indicator.
+ *
+ * @return conversionResult - 12bit conversion result.
+*******************************************************************************/
+unsigned short AD7091R_ReadSample(void)
+{
+    unsigned short conversionResult = 0;
+    unsigned char  writeByte        = 0xBF;
+    unsigned char  buffer[2]        = {0xFF, 0xFF};
+
+    /* Initiate a conversion. */
+    SPI_Write(0, &writeByte, 1);
+    /* Read conversion data. */
+    SPI_Read(AD7091R_SLAVE_ID, buffer, 2);
+    conversionResult = (buffer[0] << 8) + buffer[1];
+    conversionResult >>= 4;
+
+    return conversionResult;
+}
+
+/***************************************************************************//**
+ * @brief Puts the device in power-down mode.
+ *
+ * @return None.
+*******************************************************************************/
+void AD7091R_PowerDown(void)
+{
+    unsigned char buffer[2]   = {0, 0};
+    unsigned char writeValue  = 0x00;
+
+    /* Initiate a conversion. */
+    SPI_Write(0, &writeValue, 1);
+    /* Perform a dummy read. */
+    SPI_Read(AD7091R_SLAVE_ID, buffer, 2);
+}
+
+/***************************************************************************//**
+ * @brief Powers up the device by pulling CONVST high. After calling this
+ *        function, a time delay is required before initiating another
+ *        conversion:
+ *         - 50 ms delay, when internal reference is used.(with 2.2uF capacitor)
+ *         - 100 us delay, when external reference is used.
+ *
+ * @return  None.
+*******************************************************************************/
+void AD7091R_PowerUp(void)
+{
+    unsigned char writeValue = 0xFF;
+
+    /* Pull CONVST signal high. */
+    SPI_Write(0, &writeValue, 1);
+}
+
+
+/***************************************************************************//**
+ * @brief Converts a 12-bit raw sample to volts.
+ *
+ * @param rawSample - 12-bit data sample.
+ * @param vRef      - The value of the voltage reference used by the device.
+ *
+ * @return voltage  - The result of the conversion expressed as volts.
+*******************************************************************************/
+float AD7091R_ConvertToVolts(short rawSample, float vRef)
+{
+    float voltage = 0;
+    
+    if(vRef == 0)
+    {
+        vRef = 2.5;
+    }
+    voltage = vRef * (float)rawSample / (1 << 12);
+    
+    return voltage;
+}
