9aa1348e0557c9a379c222e09a9d4066d51fbb51
Dragos Bogdan <Dragos.Bogdan@Analog.com>
2013-05-27 17:10:49 +0300

    PMODS: Added the PmodAD4 demonstration software.

---
+++ Renesas/RL78G14/PmodAD4/PmodAD4.c
@@ -0,0 +1,146 @@
+/***************************************************************************//**
+ *   @file   Main.c
+ *   @brief  Implementation of the program's main function.
+ *   @author DNechita (Dan.Nechita@analog.com)
+********************************************************************************
+ * Copyright 2012(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+********************************************************************************
+ *   SVN Revision: $WCREV$
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Option Bytes ***********************************/
+/******************************************************************************/
+#pragma location = "OPTBYTE"
+__root const unsigned char opbyte0 = 0x6E; // Watchdog timer settings.
+#pragma location = "OPTBYTE"
+__root const unsigned char opbyte1 = 0xFF; // Low voltage detector settings.
+#pragma location = "OPTBYTE"
+__root const unsigned char opbyte2 = 0xE8; // Flash op. mode and osc. settings.
+#pragma location = "OPTBYTE"
+__root const unsigned char opbyte3 = 0x85; // On-chip Debug Option Byte.
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "RDKRL78G14.h"     // RDKRL78G14 definitions.
+#include "ST7579.h"         // ST7579 definitions.
+#include "TIME.h"           // TIME definitions.
+#include "AD7980.h"         // AD7980 definitions.
+
+/******************************************************************************/
+/************************ Variables Definitions *******************************/
+/******************************************************************************/
+unsigned short data  = 0;    // Captured data.
+float          volts = 0;    // Measured voltage.
+
+/***************************************************************************//**
+* @brief Draws the ADI logo and displays the name of ADI component.
+*
+* @param componentName - The name of the ADI component.
+*
+* @return None.
+*******************************************************************************/
+void ADI_Component(unsigned char* string)
+{
+    const unsigned char adiComponent[7] =
+    {
+        0xFE, 0xFE, 0x82, 0xC6, 0xEE, 0xFE, 0xFE 
+    };
+    ST7579_Image(0, 0, adiComponent, 7, 8);
+    ST7579_String(0, 9, string, 0);
+}
+
+/***************************************************************************//**
+ * @brief Main function.
+ *
+ * @return None.
+*******************************************************************************/
+void main(void)
+{
+     
+    /* Initialize RDKRL78G14. */
+    RDKRL78G14_Init();
+    
+    /* Enable interrupts. */
+    __enable_interrupt();
+        
+    /* Initialize timer. */
+    TIME_Init();
+    
+    /* Initialize the ST7579 Display. */
+    ST7579_Init();
+    
+    /* Initialize AD7980 device. */
+    if(AD7980_Init() == 0)
+    {
+        ADI_Component("AD7980 OK");
+    }
+    else
+    {
+        ADI_Component("AD7980 Error");
+    }
+    /* Display device information on LDC. */
+    
+    ST7579_String(2, 0, "Conversion", 0);
+    ST7579_String(3, 0, "Result:", 0);	
+    ST7579_String(5, 0, "HEX:", 0);
+    ST7579_String(6, 0, "DEC:", 0);
+    ST7579_String(7, 0, "VOLT:", 0);
+    
+    while(1)
+    {
+        /* Acquire one sample. */
+        data = AD7980_Conversion();
+	
+        /* Display the value in hexa and decimal. */
+        ST7579_String(5, 36, "      ", 0);
+        ST7579_HexNumber(5, 36, data, 0);
+        ST7579_String(6, 36, "      ", 0);
+        ST7579_Number(6, 36, data, 0);
+        
+        //ST7579_HexNumber(4, 0, data, 0);
+       
+        /* On PmodAD4, vRef = 2.5V by default. */
+        /* Convert the sample in voltage. */
+        volts = AD7980_ConvertToVolts(data, 2.5);
+        
+        /* Display the voltage. */
+        ST7579_String(7, 36, "      ", 0);
+        ST7579_FloatNumber(7, 36, volts, 3, 0);
+              
+        TIME_DelayMs(100);        
+    }
+}
---
+++ drivers/AD7980/AD7980.c
@@ -0,0 +1,107 @@
+/***************************************************************************//**
+ *   @file   AD7980.c
+ *   @brief  Implementation of 7980 Driver.
+ *   @author Bancisor Mihai(Bancisor.Mihai@analog.com)
+********************************************************************************
+ * Copyright 2012(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+********************************************************************************
+ *   SVN Revision: $WCREV$
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "AD7980.h"           // AD7980 definitions.
+#include "Communication.h"    // Communication definitions.
+
+/******************************************************************************/
+/************************ Functions Definitions *******************************/
+/******************************************************************************/
+
+/***************************************************************************//**
+ * @brief Initializes the communication peripheral.
+ *
+ * @return status - Initialization status.
+ *                  Example: -1 - Initialization failed;
+ *                            0 - Initialization succeeded.
+*******************************************************************************/
+unsigned char AD7980_Init(void)
+{
+    unsigned char status = 0;
+    
+    status = SPI_Init(0, 1000000, 1, 1);
+    
+    return status;
+}
+
+/***************************************************************************//**
+ * @brief Initiates conversion and reads data.
+ *
+ * @return receivedData - Data read from the ADC.
+*******************************************************************************/
+unsigned short AD7980_Conversion(void)
+{
+	unsigned short receivedData = 0;
+	unsigned char  txData[1] = {0};
+	unsigned char  rxData[2] = {0, 0};
+	
+    txData[0] = 0x7F;
+	SPI_Write(0, txData, 1);
+	AD7980_CS_LOW;
+    rxData[0] = 0xFF;
+    rxData[1] = 0xFF;
+	SPI_Read(0, rxData, 2);
+	AD7980_CS_HIGH;
+	receivedData = (rxData[0] << 8) + rxData[1];
+	
+	return(receivedData);	
+}
+
+/***************************************************************************//**
+ * @brief Converts a 16-bit raw sample to volts.
+ *
+ * @param rawSample - 16-bit data sample.
+ * @param vRef      - The value of the voltage reference used by the device.
+ *
+ * @return voltage  - The result of the conversion expressed as volts.
+*******************************************************************************/
+float AD7980_ConvertToVolts(unsigned short rawSample, float vRef)
+{
+    float voltage = 0;
+    
+    voltage = vRef * (float)rawSample / (1ul << 16);
+    
+    return voltage;
+}
