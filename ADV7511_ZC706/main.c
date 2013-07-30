c22f1f4b895d4c4b04c6dce9bc5dc9611ad3006e
Dragos Bogdan <Dragos.Bogdan@Analog.com>
2013-07-30 16:17:05 +0300

    ADV7511: PS7 IIC was replaced by AXI IIC.

--- ADV7511_ZC706/main.c
+++ ADV7511_ZC706/main.c
@@ -160,7 +160,7 @@ int main()
 	Xil_ICacheEnable();
 	Xil_DCacheEnable();
 
-	HAL_PlatformInit(XPS_I2C0_BASEADDR,			/* Perform any required platform init */
+	HAL_PlatformInit(XPAR_AXI_IIC_0_BASEADDR,	/* Perform any required platform init */
 					 XPAR_SCUTIMER_DEVICE_ID,	/* including hardware reset to HDMI devices */
 					 XPAR_SCUGIC_SINGLE_DEVICE_ID,
 					 XPAR_SCUTIMER_INTR);
