3286832b5552a317e80aca76cc15e1d96cff60d7
luciansin <lucian.sin@analog.com>
2013-09-30 12:33:58 +0300

    commands: AD5791 changed
    AD5791 commands work also for: AD5760, AD5780, AD5781, AD5790, AD5791

---
+++ device_commands/AD5791/Command.h
@@ -0,0 +1,112 @@
+/***************************************************************************//**
+ *   @file   Command.h
+ *   @brief  Header file of the commands driver.
+ *   @author Lucian Sin (Lucian.Sin@analog.com)
+********************************************************************************
+ * Copyright 2013(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+*******************************************************************************/
+#ifndef __COMMAND_H__
+#define __COMMAND_H__
+
+/******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+#define NULL        ((void *)0)
+#define SUCCESS      0
+#define ERROR       -1
+
+/******************************************************************************/
+/*************************** Types Declarations *******************************/
+/******************************************************************************/
+typedef void (*cmdFunction)(double* param, char paramNo);
+struct cmd_info {
+    char* name;
+    char* description;
+    char* acceptedValue;
+    char* example;
+};
+
+/******************************************************************************/
+/************************ Functions Declarations ******************************/
+/******************************************************************************/
+/* Internal function for displaying error messages. */
+void DisplayError(unsigned char funcNo);
+
+/* Internal function for displaying all the commands with their description. */
+void DisplayCmdList();
+
+/* Initializes the device. */
+void DoDeviceLock(void);
+
+/* Initializes the device. */
+char DoDeviceInit(void);
+
+/* Displays all available commands. */
+void GetHelp(double* param, char paramNo);
+
+/* Resets the AD5791 device. */
+void DoReset(double* param, char paramNo);
+
+/* Selects the coding style. */
+void SetCoding(double* param, char paramNo);
+
+/* Display the current coding style. */
+void GetCoding(double* param, char paramNo);
+
+/* Writes to the DAC register. */
+void SetRegister(double* param, char paramNo);
+
+/* Displays last written value to the DAC register. */
+void GetRegister(double* param, char paramNo);
+
+/* Sets the DAC output voltage. */
+void SetVoltage(double* param, char paramNo);
+
+/* Displays the output voltage. */
+void GetVoltage(double* param, char paramNo);
+
+/* Selects the DAC output state. */
+void SetOutput(double* param, char paramNo);
+
+/* Displays the DAC output state. */
+void GetOutput(double* param, char paramNo);
+
+/* Sets/resets the RBUF bit from control register. */
+void SetRbuf(double* param, char paramNo);
+
+/* Displays the value of RBUF bit from control register. */
+void GetRbuf(double* param, char paramNo);
+
+#endif  // __COMMAND_H__
