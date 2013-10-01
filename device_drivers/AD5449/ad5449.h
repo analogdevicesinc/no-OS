49e1308d6fd1e9698df22a335ebd6fb04e82ba0b
luciansin <lucian.sin@analog.com>
2013-10-01 17:34:45 +0300

    device_drivers: changed AD5449 to
    work also for ad5415, ad5443, ad5449

--- device_drivers/AD5449/ad5449.h
+++ device_drivers/AD5449/ad5449.h
@@ -1,7 +1,9 @@
 /***************************************************************************//**
 *   @file   AD5449.h
-*   @brief  Header file of AD5449 Driver.
-*   @author Lucian Sin (Lucian.Sin@analog.com)
+*   @brief  Header file of AD5449 Driver. This driver supporting the following
+*              devices: AD5415, AD5443, AD5432, AD5426, AD5429, AD5439, AD5449
+*
+*   @author Istvan Csomortani (istvan.csomortani@analog.com)
 ********************************************************************************
 * Copyright 2013(c) Analog Devices, Inc.
 *
@@ -45,75 +47,116 @@
 /******************************************************************************/
 #include "Communication.h"
 
-/* COMMAND Bytes */
-#define AD5449_CMD_NOOPBOTH         0
-#define AD5449_CMD_LOADUPDATEA      1
-#define AD5449_CMD_READBACKA        2
-#define AD5449_CMD_LOADA            3
-#define AD5449_CMD_LOADUPDATEB      4
-#define AD5449_CMD_READBACKB        5
-#define AD5449_CMD_LOADB            6
-#define AD5449_CMD_UPDATEBOTH       7
-#define AD5449_CMD_LOADBOTH         8
-#define AD5449_CMD_DISDCHAIN        9
-#define AD5449_CMD_CLEARZERO        11
-#define AD5449_CMD_CLEARMID         12
-#define AD5449_CMD_CTRL             13
+typedef enum
+{
+    false,
+    true
+} bool_t;
 
-#define AD5449_CMD(x)           ((x & 0x0F) << 12)
+/* Supported devices */
+typedef enum {
+    ID_AD5415,
+    ID_AD5426,
+    ID_AD5429,
+    ID_AD5432,
+    ID_AD5439,
+    ID_AD5443,
+    ID_AD5449,
+} AD5449_type_t;
+
+/* Control Bits */
+#define AD5449_CTRL_NOP             0
+#define AD5449_CTRL_LOADUPDATE(x)   (1 + 3 * (x))
+#define AD5449_CTRL_READBACK(x)     (2 + 3 * (x))
+#define AD5449_CTRL_LOAD(x)         (3 + 3 * (x))
+#define AD5449_CTRL_UPDATEALL       7
+#define AD5449_CTRL_LOADALL         8
+#define AD5449_CTRL_DAISY_CHAIN     9
+#define AD5449_CTRL_CLK_EDGE        10
+#define AD5449_CTRL_CLR_ZERO        11
+#define AD5449_CTRL_CLR_MID         12
+#define AD5449_CTRL_REG             13
 
 /* AD5449 channels */
-#define AD5449_CH_A             0
-#define AD5449_CH_B             1
+#define AD5449_CH_A                 0
+#define AD5449_CH_B                 1
 
 /* Clear target scales */
-#define ZERO_SCALE              0
-#define MIDSCALE                1
+#define AD5449_ZERO_SCALE            0
+#define AD5449_MID_SCALE             1
+
+/* Active clock edge */
+#define AD5449_CLOCK_NEGEDGE         0
+#define AD5449_CLOCK_POSEDGE         1
+
+/* Daisy-Chain Control */
+#define AD5449_DAISY_CHAIN_DIS       0
+#define AD5449_DAISY_CHAIN_EN        1
 
 /* AD5449_CTRL_REG definition */
-#define AD5449_CTRL_SDO1        (1 << 11)
-#define AD5449_CTRL_SDO2        (1 << 10)
-#define AD5449_CTRL_DSY         (1 << 9)
-#define AD5449_CTRL_HCLR        (1 << 8)
-#define AD5449_CTRL_SCLK        (1 << 7)
+#define AD5449_SDO_MASK          (3 << 10)
+#define AD5449_DSY_MASK          (1 << 9)
+#define AD5449_HCLR_MASK         (1 << 8)
+#define AD5449_SCLK_MASK         (1 << 7)
+#define AD5449_SDO_BIT           10
+#define AD5449_DSY_BIT           9
+#define AD5449_HCLR_BIT          8
+#define AD5449_SCLK_BIT          7
 
 /* AD5449 GPIO */
-#define AD5449_LDAC_OUT         GPIO1_PIN_OUT
-#define AD5449_LDAC_LOW         GPIO1_LOW
-#define AD5449_LDAC_HIGH        GPIO1_HIGH
+#define AD5449_LDAC_OUT             GPIO1_PIN_OUT
+#define AD5449_LDAC_LOW             GPIO1_LOW
+#define AD5449_LDAC_HIGH            GPIO1_HIGH
 
-#define AD5449_CLR_OUT          GPIO2_PIN_OUT
-#define AD5449_CLR_LOW          GPIO2_LOW
-#define AD5449_CLR_HIGH         GPIO2_HIGH
+#define AD5449_CLR_OUT              GPIO2_PIN_OUT
+#define AD5449_CLR_LOW              GPIO2_LOW
+#define AD5449_CLR_HIGH             GPIO2_HIGH
+
+/* SDO Control Bits */
+#define AD5449_SDO_FULL             0
+#define AD5449_SDO_WEAK             1
+#define AD5449_SDO_OPEN_DRAIN       2
+#define AD5449_SDO_DISABLE          3
 
 /* SPI slave device ID */
-#define AD5449_SLAVE_ID         1
-
-
+#if(defined(AD5443) || defined(AD5432) || defined(AD5426))
+#define AD5449_SLAVE_ID             3
+#else
+#define AD5449_SLAVE_ID             1
+#endif
 /******************************************************************************/
 /************************ Functions Declarations ******************************/
 /******************************************************************************/
 /* Initialize SPI and Initial Values for AD5449 Board. */
-char AD5449_Init(void);
+char AD5449_Init(AD5449_type_t device);
 
-/* Write to input register and read from output register via SPI. */
-unsigned short AD5449_SetInputShiftReg(unsigned short function,
+/* Write to shift register via SPI. */
+unsigned short AD5449_SetInputShiftReg(unsigned short command,
                                        unsigned short data);
 
 /* Load and updates the selected DAC with a given value. */
-void AD5449_LoadUpdate(unsigned char channel, unsigned short dacValue);
+void AD5449_LoadUpdateChannel(unsigned char channel, unsigned short dacValue);
 
 /* Load selected DAC input register with a given value. */
-void AD5449_Load(unsigned char channel, unsigned short dacValue);
+void AD5449_LoadChannel(unsigned char channel, unsigned short dacValue);
 
 /* Read from the selected DAC register. */
-unsigned short AD5449_Readback(unsigned char channel);
+unsigned short AD5449_ReadbackChannel(unsigned char channel);
 
-/* Update both DAC outputs. */
-void AD5449_UpdateBoth(void);
+/* Update the DAC outputs (all channels). */
+void AD5449_UpdateAll(void);
 
-/* Load both DAC input registers. */
-void AD5449_LoadBoth(short dacValue);
+/* Load the DAC input registers. */
+void AD5449_LoadAll(short dacValue);
 
-/* Clear both DAC outputs to zero scale or to to midscale. */
-void AD5449_Clear(char type);
+/* Set up the scale where to the output will be cleared on active CLR signal */
+void AD5449_ClearScaleSetup(char type);
+
+/* Enable/disable the Daisy-Chain mode */
+void AD5449_DaisyChainSetup(char value);
+
+/* Control the SDO output driver strength */
+void AD5449_SDOControl(char controlBits);
+
+/* Set up the active clock edge of the SPI interface */
+void AD5449_SCLKSetup(char value);
