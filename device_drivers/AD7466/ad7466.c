f9bc63c1225f6d1ae3e8dc91e699135a874f31e8
luciansin <lucian.sin@analog.com>
2013-11-15 14:34:53 +0200

    device_drivers: added ad7466 driver

---
+++ device_drivers/AD7466/ad7466.c
@@ -0,0 +1,89 @@
+/**************************************************************************//**
+*   @file   ad7466.c
+*   @brief  Implementation of ad7466 Driver for Microblaze processor.
+*   @author Lucian Sin (Lucian.Sin@analog.com)
+*
+*******************************************************************************
+* Copyright 2013(c) Analog Devices, Inc.
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without modification,
+* are permitted provided that the following conditions are met:
+*  - Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*  - Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+*  - Neither the name of Analog Devices, Inc. nor the names of its
+*    contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*  - The use of this software may or may not infringe the patent rights
+*    of one or more patent holders.  This license does not release you
+*    from the requirement that you obtain separate licenses from these
+*    patent holders to use this software.
+*  - Use of the software either in source or binary form, must be run
+*    on or directly connected to an Analog Devices Inc. component.
+*
+* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY
+* AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+* INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "ad7466.h"
+
+/******************************************************************************/
+/*************************** Functions Definitions ****************************/
+/******************************************************************************/
+
+/**************************************************************************//**
+ * @brief Initializes the communication with the device.
+ *
+ * @param None.
+ *
+ * @return status - Result of the initialization procedure.
+ *                  Example:  0 - if initialization was successful;
+ *                           -1 - if initialization was unsuccessful.
+******************************************************************************/
+char AD7466_Init(void)
+{
+    char status = -1;
+
+    /* Setup SPI Interface */
+    status = SPI_Init(0, 1000000, 1, 1);
+
+    return status;
+}
+
+/**************************************************************************//**
+ * @brief Receives 16 bits from SPI.
+ *
+ * @return rxData - Received data in case of success.
+******************************************************************************/
+unsigned short AD7466_Read16Bits(void)
+{
+    unsigned short rxData;
+    char           bytesRead = 0;
+    unsigned char  data[2]   = {0, 0};
+
+    bytesRead = SPI_Read(AD7466_SLAVE_ID, data, 2);
+    if (bytesRead != 2)
+    {
+        return -1;
+    }
+    rxData = ((unsigned short)data[0] << 8) + data[1];
+
+    return(rxData);
+}
