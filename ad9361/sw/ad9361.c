2e42ae78e9b9cad884f8b2148150b2abd9d96271
dbogdan <dragos.bogdan@analog.com>
2015-03-30 14:45:16 +0300

    ad9361: Fix ENERGY_DETECT_COUNT computation
    f_agc_state_wait_time_ns, ENERGY_DETECT_COUNT

--- ad9361/sw/ad9361.c
+++ ad9361/sw/ad9361.c
@@ -1595,7 +1595,7 @@ static int32_t ad9361_gc_update(struct ad9361_rf_phy *phy)
 	 */
 
 	reg = DIV_ROUND_CLOSEST(phy->pdata->gain_ctrl.f_agc_state_wait_time_ns *
-				1000, clkrf / 1000UL);
+				(clkrf / 1000UL), 1000000UL);
 	reg = clamp_t(uint32_t, reg, 0U, 31U);
 	ret |= ad9361_spi_writef(spi, REG_FAST_ENERGY_DETECT_COUNT,
 			  ENERGY_DETECT_COUNT(~0),  reg);
