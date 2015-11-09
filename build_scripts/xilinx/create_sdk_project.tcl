83ebbd206a014d19aeb80214cada0a7d52ae3404
AndreiGrozav <Andrei.Grozav@analog.com>
2015-11-09 16:10:06 +0200

    build_scripts/xilinx/create_sdk_project.tcl: Update. Choose the hdf as the first argument, project location as the second argument and allow sdk to use the same project when another build is initialize with the same parameters.

--- build_scripts/xilinx/create_sdk_project.tcl
+++ build_scripts/xilinx/create_sdk_project.tcl
@@ -10,11 +10,11 @@ set index 1
 	}
 
 # setting the parsed arguments
-if { [catch {set project_location_arg $parameter(1)} fid] } {
+if { [catch {set system_hdf_location_arg $parameter(1)} fid] } {
 	puts "The tcl script was called with less parameters than required"
 	exit 1
 }
-if { [catch {set system_hdf_location_arg $parameter(2)} fid] } {
+if { [catch {set project_location_arg $parameter(2)} fid] } {
 	puts "The tcl script was called with less parameters than required"
 	exit 1
 }
@@ -55,9 +55,26 @@ puts "FPGA CPU: $cpu_name"
 # create project
 cd $project_location
 sdk set_workspace $project_location/workspace
-sdk create_hw_project -name hw -hwspec $system_hdf_location
-sdk create_bsp_project -name bsp -hwproject hw -proc $cpu_name -os standalone 
-sdk create_app_project -name sw -hwproject hw -proc $cpu_name -bsp bsp -os standalone -lang C -app {Empty Application} 
+if {[file exists ./workspace/hw]} {
+	puts "hardware platform \"hw\" already exists"
+} else {
+	sdk create_hw_project -name hw -hwspec $system_hdf_location
+	sdk create_bsp_project -name bsp -hwproject hw -proc $cpu_name -os standalone
+}
+if {[file exists ./workspace/sw]} {
+	puts "The software platform already exists, rebuilding sorces"
+	source $source_directory/../build_scripts/xilinx/parse_readme_copy_sources.tcl;
+	file copy -force -- $project_location/workspace/sw/src/lscript.ld $project_destination
+	sdk import_sources -name sw -path $project_destination
+	sdk clean_project -type app -name sw
+
+	# delete the copy of source files
+	file delete -force -- $project_location/$input
+	puts "Done."
+	exit
+} else {
+sdk create_app_project -name sw -hwproject hw -proc $cpu_name -bsp bsp -os standalone -lang C -app {Empty Application}
+}
 
 # import sources
 source $source_directory/../build_scripts/xilinx/parse_readme_copy_sources.tcl; 
