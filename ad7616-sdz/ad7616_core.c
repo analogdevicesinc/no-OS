f3b23f5dc8d723a6ba2a650043dbde38817c0eff
Istvan Csomortani <istvan.csomortani@analog.com>
2016-07-01 14:58:36 +0300

    ad7616_sdz: Deassert chip select after every SPI transaction

--- ad7616-sdz/ad7616_core.c
+++ ad7616-sdz/ad7616_core.c
@@ -135,12 +135,13 @@ int32_t ad7616_capture_serial(adc_core core,
 	spi_engine_write(SPI_ENGINE_REG_OFFLOAD_CTRL(0), 0x0);
 	mdelay(10);
 	spi_engine_write(SPI_ENGINE_REG_OFFLOAD_RESET(0), 0x1);
-	spi_engine_write(SPI_ENGINE_REG_OFFLOAD_SDO_MEM(0), 0x44);
+	spi_engine_write(SPI_ENGINE_REG_OFFLOAD_SDO_MEM(0), 0x00);
 	spi_engine_write(SPI_ENGINE_REG_OFFLOAD_CMD_MEM(0), 0x2103);
 	spi_engine_write(SPI_ENGINE_REG_OFFLOAD_CMD_MEM(0), 0x2000);
 	spi_engine_write(SPI_ENGINE_REG_OFFLOAD_CMD_MEM(0), 0x11fe);
 	spi_engine_write(SPI_ENGINE_REG_OFFLOAD_CMD_MEM(0), 0x0201);
 	spi_engine_write(SPI_ENGINE_REG_OFFLOAD_CMD_MEM(0), 0x3000);
+	spi_engine_write(SPI_ENGINE_REG_OFFLOAD_CMD_MEM(0), 0x11ff);
 	spi_engine_write(SPI_ENGINE_REG_OFFLOAD_CTRL(0), 0x1);
 
 	ad7616_core_write(core, AD7616_REG_UP_CTRL,
