ba9c9dd63956ec05d451dba5097214b8780af8ee
Rejeesh Kutty <rejeesh.kutty@analog.com>
2017-01-10 12:41:05 -0500

    daq3- make copy

---
+++ ad-fmcdaq3-ebz/ad_fmcdaq3_ebz.mk
@@ -0,0 +1,26 @@
+## NOOS-DIR:
+##		is the no-OS root variable (do not override)
+##
+## M_INC_DIRS:
+##		list all your include folders (all *.h and *.c files from these folders are included
+##		in compile and passed as directives)
+##
+## M_HDR_FILES:
+##		if you want to hand pick files, use this variable to list header files.
+##
+## M_SRC_FILES:
+##		if you want to hand pick files, use this variable to list source files.
+
+M_INC_DIRS := $(NOOS-DIR)/common_drivers/altxil_base
+M_INC_DIRS += $(NOOS-DIR)/common_drivers/adc_core
+M_INC_DIRS += $(NOOS-DIR)/common_drivers/dac_core
+M_INC_DIRS += $(NOOS-DIR)/common_drivers/adxcvr_core
+M_INC_DIRS += $(NOOS-DIR)/drivers/ad9144
+M_INC_DIRS += $(NOOS-DIR)/drivers/ad9523
+M_INC_DIRS += $(NOOS-DIR)/drivers/ad9680
+
+M_HDR_FILES :=
+
+M_SRC_FILES := $(NOOS-DIR)/ad-fmcdaq2-ebz/ad_fmcdaq2_ebz.c
+
+
