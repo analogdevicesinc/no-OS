de4ef37edfdb51a8d9dd59920f3d7dfafb7131ea
Istvan Csomortani <istvan.csomortani@analog.com>
2017-01-16 17:21:01 +0200

    common_drivers/dac_core: Reindent and delete DMA related code

--- common_drivers/dac_core/dac_core.c
+++ common_drivers/dac_core/dac_core.c
@@ -1,94 +1,53 @@
 /***************************************************************************//**
-* @file dac_core.c
-* @brief Implementation of DAC Core Driver.
-* @author DBogdan (dragos.bogdan@analog.com)
-********************************************************************************
-* Copyright 2014-2016(c) Analog Devices, Inc.
-*
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-* - Redistributions of source code must retain the above copyright
-* notice, this list of conditions and the following disclaimer.
-* - Redistributions in binary form must reproduce the above copyright
-* notice, this list of conditions and the following disclaimer in
-* the documentation and/or other materials provided with the
-* distribution.
-* - Neither the name of Analog Devices, Inc. nor the names of its
-* contributors may be used to endorse or promote products derived
-* from this software without specific prior written permission.
-* - The use of this software may or may not infringe the patent rights
-* of one or more patent holders. This license does not release you
-* from the requirement that you obtain separate licenses from these
-* patent holders to use this software.
-* - Use of the software either in source or binary form, must be run
-* on or directly connected to an Analog Devices Inc. component.
-*
-* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-* IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-* LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*******************************************************************************/
+ * @file dac_core.c
+ * @brief Implementation of DAC Core Driver.
+ * @author DBogdan (dragos.bogdan@analog.com)
+ ********************************************************************************
+ * Copyright 2014-2016(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ * - Neither the name of Analog Devices, Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * - The use of this software may or may not infringe the patent rights
+ * of one or more patent holders. This license does not release you
+ * from the requirement that you obtain separate licenses from these
+ * patent holders to use this software.
+ * - Use of the software either in source or binary form, must be run
+ * on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
 
 /******************************************************************************/
 /***************************** Include Files **********************************/
 /******************************************************************************/
 #include "dac_core.h"
 
-/******************************************************************************/
-/********************** Macros and Constants Definitions **********************/
-/******************************************************************************/
-const uint16_t sine_lut_1[128] = {
-		0x0000, 0x0640, 0x0C80, 0x12C0, 0x18F0, 0x1F10, 0x2520, 0x2B10,
-		0x30F0, 0x36B0, 0x3C50, 0x41C0, 0x4710, 0x4C30, 0x5120, 0x55F0,
-		0x5A70, 0x5ED0, 0x62E0, 0x66C0, 0x6A60, 0x6DC0, 0x70D0, 0x73A0,
-		0x7630, 0x7870, 0x7A70, 0x7C20, 0x7D80, 0x7E90, 0x7F50, 0x7FD0,
-		0x7FF0, 0x7FD0, 0x7F50, 0x7E90, 0x7D80, 0x7C20, 0x7A70, 0x7870,
-		0x7630, 0x73A0, 0x70D0, 0x6DC0, 0x6A60, 0x66C0, 0x62E0, 0x5ED0,
-		0x5A70, 0x55F0, 0x5120, 0x4C30, 0x4710, 0x41C0, 0x3C50, 0x36B0,
-		0x30F0, 0x2B10, 0x2520, 0x1F10, 0x18F0, 0x12C0, 0x0C80, 0x0640,
-		0x0000, 0xF9B0, 0xF370, 0xED30, 0xE700, 0xE0E0, 0xDAD0, 0xD4E0,
-		0xCF00, 0xC940, 0xC3A0, 0xBE30, 0xB8E0, 0xB3C0, 0xAED0, 0xAA00,
-		0xA580, 0xA120, 0x9D10, 0x9930, 0x9590, 0x9230, 0x8F20, 0x8C50,
-		0x89C0, 0x8780, 0x8580, 0x83D0, 0x8270, 0x8160, 0x80A0, 0x8020,
-		0x8000, 0x8020, 0x80A0, 0x8160, 0x8270, 0x83D0, 0x8580, 0x8780,
-		0x89C0, 0x8C50, 0x8F20, 0x9230, 0x9590, 0x9930, 0x9D10, 0xA120,
-		0xA580, 0xAA00, 0xAED0, 0xB3C0, 0xB8E0, 0xBE30, 0xC3A0, 0xC940,
-		0xCF00, 0xD4E0, 0xDAD0, 0xE0E0, 0xE700, 0xED30, 0xF370, 0xF9B0,
-};
-
-const uint16_t sine_lut_2[128] = {
-		0x7FF0, 0x7FD0, 0x7F50, 0x7E90, 0x7D80, 0x7C20, 0x7A70, 0x7870,
-		0x7630, 0x73A0, 0x70D0, 0x6DC0, 0x6A60, 0x66C0, 0x62E0, 0x5ED0,
-		0x5A70, 0x55F0, 0x5120, 0x4C30, 0x4710, 0x41C0, 0x3C50, 0x36B0,
-		0x30F0, 0x2B10, 0x2520, 0x1F10, 0x18F0, 0x12C0, 0x0C80, 0x0640,
-		0x0000, 0xF9B0, 0xF370, 0xED30, 0xE700, 0xE0E0, 0xDAD0, 0xD4E0,
-		0xCF00, 0xC940, 0xC3A0, 0xBE30, 0xB8E0, 0xB3C0, 0xAED0, 0xAA00,
-		0xA580, 0xA120, 0x9D10, 0x9930, 0x9590, 0x9230, 0x8F20, 0x8C50,
-		0x89C0, 0x8780, 0x8580, 0x83D0, 0x8270, 0x8160, 0x80A0, 0x8020,
-		0x8000, 0x8020, 0x80A0, 0x8160, 0x8270, 0x83D0, 0x8580, 0x8780,
-		0x89C0, 0x8C50, 0x8F20, 0x9230, 0x9590, 0x9930, 0x9D10, 0xA120,
-		0xA580, 0xAA00, 0xAED0, 0xB3C0, 0xB8E0, 0xBE30, 0xC3A0, 0xC940,
-		0xCF00, 0xD4E0, 0xDAD0, 0xE0E0, 0xE700, 0xED30, 0xF370, 0xF9B0,
-		0x0000, 0x0640, 0x0C80, 0x12C0, 0x18F0, 0x1F10, 0x2520, 0x2B10,
-		0x30F0, 0x36B0, 0x3C50, 0x41C0, 0x4710, 0x4C30, 0x5120, 0x55F0,
-		0x5A70, 0x5ED0, 0x62E0, 0x66C0, 0x6A60, 0x6DC0, 0x70D0, 0x73A0,
-		0x7630, 0x7870, 0x7A70, 0x7C20, 0x7D80, 0x7E90, 0x7F50, 0x7FD0,
-};
-
 /***************************************************************************//**
-* @brief dac_read
-*******************************************************************************/
+ * @brief dac_read
+ *******************************************************************************/
 int32_t dac_read(dac_core core,
-				 uint32_t reg_addr,
-				 uint32_t *reg_data)
+		uint32_t reg_addr,
+		uint32_t *reg_data)
 {
 	*reg_data = ad_reg_read((core.dac_baseaddr + 0x4000 + reg_addr));
 
@@ -96,11 +55,11 @@ int32_t dac_read(dac_core core,
 }
 
 /***************************************************************************//**
-* @brief dac_write
-*******************************************************************************/
+ * @brief dac_write
+ *******************************************************************************/
 int32_t dac_write(dac_core core,
-				  uint32_t reg_addr,
-				  uint32_t reg_data)
+		uint32_t reg_addr,
+		uint32_t reg_data)
 {
 	ad_reg_write((core.dac_baseaddr + 0x4000 + reg_addr), reg_data);
 
@@ -108,35 +67,11 @@ int32_t dac_write(dac_core core,
 }
 
 /***************************************************************************//**
- * @brief dac_dmac_read
-*******************************************************************************/
-int32_t dac_dmac_read(dac_core core,
-					  uint32_t reg_addr,
-					  uint32_t *reg_data)
-{
-	*reg_data = ad_reg_read(core.dmac_baseaddr + reg_addr);
-
-	return 0;
-}
-
-/***************************************************************************//**
- * @brief dac_dmac_write
-*******************************************************************************/
-int32_t dac_dmac_write(dac_core core,
-					   uint32_t reg_addr,
-					   uint32_t reg_data)
-{
-	ad_reg_write(core.dmac_baseaddr + reg_addr, reg_data);
-
-	return 0;
-}
-
-/***************************************************************************//**
  * @brief dds_set_frequency
-*******************************************************************************/
+ *******************************************************************************/
 int32_t dds_set_frequency(dac_core core,
-						  uint32_t chan,
-						  uint32_t freq)
+		uint32_t chan,
+		uint32_t freq)
 {
 	uint32_t pcore_version;
 	uint32_t val;
@@ -171,10 +106,10 @@ int32_t dds_set_frequency(dac_core core,
 
 /***************************************************************************//**
  * @brief dds_set_phase
-*******************************************************************************/
+ *******************************************************************************/
 int32_t dds_set_phase(dac_core core,
-					  uint32_t chan,
-					  uint32_t phase)
+		uint32_t chan,
+		uint32_t phase)
 {
 	uint32_t pcore_version;
 	uint64_t val64;
@@ -202,10 +137,10 @@ int32_t dds_set_phase(dac_core core,
 
 /***************************************************************************//**
  * @brief dds_set_scale
-*******************************************************************************/
+ *******************************************************************************/
 int32_t dds_set_scale(dac_core core,
-					  uint32_t chan,
-					  int32_t scale_micro_units)
+		uint32_t chan,
+		int32_t scale_micro_units)
 {
 	uint32_t pcore_version;
 	uint32_t scale_reg;
@@ -237,7 +172,7 @@ int32_t dds_set_scale(dac_core core,
 			int_part = 0;
 		}
 		fract_part = (uint32_t)(((uint64_t)scale_micro_units * 0x4000) / 1000000);
-	set_scale_reg:
+set_scale_reg:
 		scale_reg = (sign_part << 15) | (int_part << 14) | fract_part;
 	} else {
 		if (scale_micro_units >= 1000000) {
@@ -265,11 +200,11 @@ int32_t dds_set_scale(dac_core core,
 }
 
 /***************************************************************************//**
-* @brief dac_data_src_sel
-*******************************************************************************/
+ * @brief dac_data_src_sel
+ *******************************************************************************/
 int32_t dac_data_src_sel(dac_core core,
-						 int32_t chan,
-						 dac_data_src src)
+		int32_t chan,
+		dac_data_src src)
 {
 	uint32_t pcore_version;
 	int32_t i;
@@ -286,88 +221,25 @@ int32_t dac_data_src_sel(dac_core core,
 		uint32_t reg;
 
 		switch(src) {
-		case DAC_SRC_DDS:
-		case DAC_SRC_SED:
-		case DAC_SRC_DMA:
-			dac_read(core, DAC_REG_CNTRL_2, &reg);
-			reg &= ~DAC_DATA_SEL(~0);
-			reg |= DAC_DATA_SEL(src);
-			dac_write(core, DAC_REG_CNTRL_2, reg);
-			break;
-		default:
-			return -1;
+			case DAC_SRC_DDS:
+			case DAC_SRC_SED:
+			case DAC_SRC_DMA:
+				dac_read(core, DAC_REG_CNTRL_2, &reg);
+				reg &= ~DAC_DATA_SEL(~0);
+				reg |= DAC_DATA_SEL(src);
+				dac_write(core, DAC_REG_CNTRL_2, reg);
+				break;
+			default:
+				return -1;
 		}
 	}
 
 	return 0;
 }
 
-void ad_reg_write_16(uint32_t addr, uint32_t data)
-{
-  uint32_t m_data;
-  m_data = ad_reg_read(addr & ~0x3);
-  if ((addr & 0x3) == 0)
-    m_data = (m_data & ~0xffff) | data;
-  else
-    m_data = (m_data & 0xffff) | (data<<16);
-  ad_reg_write((addr & ~0x3), m_data);
-}
-
 /***************************************************************************//**
-* @brief dac_dma_setup
-*******************************************************************************/
-int32_t dac_dma_setup(dac_core core, uint32_t start_address)
-{
-	uint32_t no_of_samples;
-	uint32_t index;
-	uint32_t index_mem = 0;
-
-	no_of_samples = sizeof(sine_lut_1) / sizeof(typeof(sine_lut_1[0]));
-
-	for (index = 0; index < no_of_samples; index ++) {
-		switch (core.no_of_channels) {
-		case 1:
-			ad_reg_write_16(start_address + index_mem * 2, sine_lut_1[index]);
-			index_mem += 1;
-			break;
-		case 2:
-			ad_reg_write_16(start_address + (index_mem + 0) * 2, sine_lut_1[index]);
-			ad_reg_write_16(start_address + (index_mem + 1) * 2, sine_lut_2[index]);
-			index_mem += 2;
-			break;
-		case 4:
-			ad_reg_write_16(start_address + (index_mem + 0) * 2, sine_lut_1[index]);
-			ad_reg_write_16(start_address + (index_mem + 1) * 2, sine_lut_2[index]);
-			ad_reg_write_16(start_address + (index_mem + 2) * 2, sine_lut_1[index]);
-			ad_reg_write_16(start_address + (index_mem + 3) * 2, sine_lut_2[index]);
-			index_mem += 4;
-			break;
-		default:
-			ad_printf("Unsupported mode.\n\r");
-			return -1;
-		}
-	}
-	ad_dcache_flush();
-
-	dac_dmac_write(core, DAC_DMAC_REG_CTRL, 0);
-	dac_dmac_write(core, DAC_DMAC_REG_CTRL, DAC_DMAC_ENABLE);
-	dac_dmac_write(core, DAC_DMAC_REG_SRC_ADDRESS, start_address);
-	dac_dmac_write(core, DAC_DMAC_REG_SRC_STRIDE, 0x0);
-	dac_dmac_write(core, DAC_DMAC_REG_X_LENGTH,
-			(core.no_of_channels * sizeof(sine_lut_1)) - 1);
-	dac_dmac_write(core, DAC_DMAC_REG_Y_LENGTH, 0x0);
-	dac_dmac_write(core, DAC_DMAC_REG_FLAGS,
-			core.fifo_present ? DAC_DMAC_FLAGS_TLAST : DAC_DMAC_FLAGS_CYCLIC);
-	dac_dmac_write(core, DAC_DMAC_REG_START_TRANSFER, DAC_DMAC_ENABLE);
-
-	dac_data_src_sel(core, -1, DAC_SRC_DMA);
-
-	return 0;
-}
-
-/***************************************************************************//**
-* @brief dac_setup
-*******************************************************************************/
+ * @brief dac_setup
+ *******************************************************************************/
 int32_t dac_setup(dac_core core)
 {
 	uint32_t status;
--- common_drivers/dac_core/dac_core.h
+++ common_drivers/dac_core/dac_core.h
@@ -1,147 +1,116 @@
 /***************************************************************************//**
-* @file dac_core.h
-* @brief Header file of DAC Core Driver.
-* @author DBogdan (dragos.bogdan@analog.com)
-********************************************************************************
-* Copyright 2014-2016(c) Analog Devices, Inc.
-*
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-* - Redistributions of source code must retain the above copyright
-* notice, this list of conditions and the following disclaimer.
-* - Redistributions in binary form must reproduce the above copyright
-* notice, this list of conditions and the following disclaimer in
-* the documentation and/or other materials provided with the
-* distribution.
-* - Neither the name of Analog Devices, Inc. nor the names of its
-* contributors may be used to endorse or promote products derived
-* from this software without specific prior written permission.
-* - The use of this software may or may not infringe the patent rights
-* of one or more patent holders. This license does not release you
-* from the requirement that you obtain separate licenses from these
-* patent holders to use this software.
-* - Use of the software either in source or binary form, must be run
-* on or directly connected to an Analog Devices Inc. component.
-*
-* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-* IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-* LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*******************************************************************************/
+ * @file dac_core.h
+ * @brief Header file of DAC Core Driver.
+ * @author DBogdan (dragos.bogdan@analog.com)
+ ********************************************************************************
+ * Copyright 2014-2016(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ * - Neither the name of Analog Devices, Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * - The use of this software may or may not infringe the patent rights
+ * of one or more patent holders. This license does not release you
+ * from the requirement that you obtain separate licenses from these
+ * patent holders to use this software.
+ * - Use of the software either in source or binary form, must be run
+ * on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
 #ifndef DAC_CORE_H_
 #define DAC_CORE_H_
 
 /******************************************************************************/
 /***************************** Include Files **********************************/
 /******************************************************************************/
-#include <altxil_base.h>
+#include "altxil_base.h"
 
 /******************************************************************************/
 /********************** Macros and Constants Definitions **********************/
 /******************************************************************************/
-#define DAC_REG_VERSION					0x0000
-#define DAC_VERSION(x)					(((x) & 0xffffffff) << 0)
+#define DAC_REG_VERSION				0x0000
+#define DAC_VERSION(x)				(((x) & 0xffffffff) << 0)
 #define DAC_VERSION_IS(x,y,z)			((x) << 16 | (y) << 8 | (z))
 #define DAC_PCORE_VERSION_MAJOR(x)		((x) >> 16)
 
-#define DAC_REG_ID						0x0004
-#define DAC_ID(x)						(((x) & 0xffffffff) << 0)
+#define DAC_REG_ID				0x0004
+#define DAC_ID(x)				(((x) & 0xffffffff) << 0)
 
-#define DAC_REG_SCRATCH					0x0008
-#define DAC_SCRATCH(x)					(((x) & 0xffffffff) << 0)
+#define DAC_REG_SCRATCH				0x0008
+#define DAC_SCRATCH(x)				(((x) & 0xffffffff) << 0)
 
-#define DAC_REG_RSTN					0x040
-#define DAC_MMCM_RSTN					(1 << 1)
-#define DAC_RSTN						(1 << 0)
+#define DAC_REG_RSTN				0x040
+#define DAC_MMCM_RSTN				(1 << 1)
+#define DAC_RSTN				(1 << 0)
 
-#define DAC_REG_CNTRL_1					0x0044
-#define DAC_ENABLE						(1 << 0) /* v7.0 */
-#define DAC_SYNC						(1 << 0) /* v8.0 */
+#define DAC_REG_CNTRL_1				0x0044
+#define DAC_ENABLE				(1 << 0) /* v7.0 */
+#define DAC_SYNC				(1 << 0) /* v8.0 */
 
-#define DAC_REG_CNTRL_2					0x0048
-#define DAC_PAR_TYPE					(1 << 7)
-#define DAC_PAR_ENB						(1 << 6)
-#define DAC_R1_MODE						(1 << 5)
-#define DAC_DATA_FORMAT					(1 << 4)
-#define DAC_DATA_SEL(x)					(((x) & 0xF) << 0) /* v7.0 */
-#define DAC_TO_DATA_SEL(x)				(((x) >> 0) & 0xF) /* v7.0 */
+#define DAC_REG_CNTRL_2				0x0048
+#define DAC_PAR_TYPE				(1 << 7)
+#define DAC_PAR_ENB				(1 << 6)
+#define DAC_R1_MODE				(1 << 5)
+#define DAC_DATA_FORMAT				(1 << 4)
+#define DAC_DATA_SEL(x)				(((x) & 0xF) << 0) /* v7.0 */
+#define DAC_TO_DATA_SEL(x)			(((x) >> 0) & 0xF) /* v7.0 */
 
-#define DAC_REG_RATECNTRL				0x004C
-#define DAC_RATE(x)						(((x) & 0xFF) << 0)
-#define DAC_TO_RATE(x)					(((x) >> 0) & 0xFF)
+#define DAC_REG_RATECNTRL			0x004C
+#define DAC_RATE(x)				(((x) & 0xFF) << 0)
+#define DAC_TO_RATE(x)				(((x) >> 0) & 0xFF)
 
-#define DAC_REG_CLK_FREQ				0x0054
-#define DAC_CLK_FREQ(x)					(((x) & 0xFFFFFFFF) << 0)
-#define DAC_TO_CLK_FREQ(x)				(((x) >> 0) & 0xFFFFFFFF)
+#define DAC_REG_CLK_FREQ			0x0054
+#define DAC_CLK_FREQ(x)				(((x) & 0xFFFFFFFF) << 0)
+#define DAC_TO_CLK_FREQ(x)			(((x) >> 0) & 0xFFFFFFFF)
 
-#define DAC_REG_CLK_RATIO				0x0058
-#define DAC_CLK_RATIO(x)				(((x) & 0xFFFFFFFF) << 0)
-#define DAC_TO_CLK_RATIO(x)				(((x) >> 0) & 0xFFFFFFFF)
+#define DAC_REG_CLK_RATIO			0x0058
+#define DAC_CLK_RATIO(x)			(((x) & 0xFFFFFFFF) << 0)
+#define DAC_TO_CLK_RATIO(x)			(((x) >> 0) & 0xFFFFFFFF)
 
-#define DAC_REG_STATUS					0x005C
-#define DAC_MUX_PN_ERR					(1 << 3)
-#define DAC_MUX_PN_OOS					(1 << 2)
-#define DAC_MUX_OVER_RANGE				(1 << 1)
-#define DAC_STATUS						(1 << 0)
+#define DAC_REG_STATUS				0x005C
+#define DAC_MUX_PN_ERR				(1 << 3)
+#define DAC_MUX_PN_OOS				(1 << 2)
+#define DAC_MUX_OVER_RANGE			(1 << 1)
+#define DAC_STATUS				(1 << 0)
 
-#define DAC_REG_CHAN_CNTRL_1_IIOCHAN(x)	(0x0400 + ((x) >> 1) * 0x40 + ((x) & 1) * 0x8)
-#define DAC_DDS_SCALE(x)				(((x) & 0xFFFF) << 0)
-#define DAC_TO_DDS_SCALE(x)				(((x) >> 0) & 0xFFFF)
+#define DAC_REG_CHAN_CNTRL_1_IIOCHAN(x)		(0x0400 + ((x) >> 1) * 0x40 + ((x) & 1) * 0x8)
+#define DAC_DDS_SCALE(x)			(((x) & 0xFFFF) << 0)
+#define DAC_TO_DDS_SCALE(x)			(((x) >> 0) & 0xFFFF)
 
-#define DAC_REG_CHAN_CNTRL_2_IIOCHAN(x)	(0x0404 + ((x) >> 1) * 0x40 + ((x) & 1) * 0x8)
-#define DAC_DDS_INIT(x)					(((x) & 0xFFFF) << 16)
-#define DAC_TO_DDS_INIT(x)				(((x) >> 16) & 0xFFFF)
-#define DAC_DDS_INCR(x)					(((x) & 0xFFFF) << 0)
-#define DAC_TO_DDS_INCR(x)				(((x) >> 0) & 0xFFFF)
+#define DAC_REG_CHAN_CNTRL_2_IIOCHAN(x)		(0x0404 + ((x) >> 1) * 0x40 + ((x) & 1) * 0x8)
+#define DAC_DDS_INIT(x)				(((x) & 0xFFFF) << 16)
+#define DAC_TO_DDS_INIT(x)			(((x) >> 16) & 0xFFFF)
+#define DAC_DDS_INCR(x)				(((x) & 0xFFFF) << 0)
+#define DAC_TO_DDS_INCR(x)			(((x) >> 0) & 0xFFFF)
 
 #define DAC_REG_CHAN_CNTRL_7(c)			(0x0418 + (c) * 0x40) /* v8.0 */
-#define DAC_DAC_DDS_SEL(x)				(((x) & 0xF) << 0)
+#define DAC_DAC_DDS_SEL(x)			(((x) & 0xF) << 0)
 #define DAC_TO_DAC_DDS_SEL(x)			(((x) >> 0) & 0xF)
-
-#define DAC_DMAC_REG_IRQ_MASK			0x80
-#define DAC_DMAC_REG_IRQ_PENDING		0x84
-#define DAC_DMAC_REG_IRQ_SOURCE			0x88
-#define DAC_DMAC_REG_CTRL				0x400
-#define DAC_DMAC_REG_TRANSFER_ID		0x404
-#define DAC_DMAC_REG_START_TRANSFER		0x408
-#define DAC_DMAC_REG_FLAGS				0x40c
-#define DAC_DMAC_REG_DEST_ADDRESS		0x410
-#define DAC_DMAC_REG_SRC_ADDRESS		0x414
-#define DAC_DMAC_REG_X_LENGTH			0x418
-#define DAC_DMAC_REG_Y_LENGTH			0x41c
-#define DAC_DMAC_REG_DEST_STRIDE		0x420
-#define DAC_DMAC_REG_SRC_STRIDE			0x424
-#define DAC_DMAC_REG_TRANSFER_DONE		0x428
-#define DAC_DMAC_REG_ACTIVE_TRANSFER_ID 0x42c
-#define DAC_DMAC_REG_STATUS				0x430
-#define DAC_DMAC_REG_CURRENT_DEST_ADDR	0x434
-#define DAC_DMAC_REG_CURRENT_SRC_ADDR	0x438
-#define DAC_DMAC_REG_DBG0				0x43c
-#define DAC_DMAC_REG_DBG1				0x440
-
-#define DAC_DMAC_ENABLE					(1 << 0)
-#define DAC_DMAC_PAUSE					(1 << 1)
-
-#define DAC_DMAC_IRQ_SOT				(1 << 0)
-#define DAC_DMAC_IRQ_EOT				(1 << 1)
-
-#define DAC_DMAC_FLAGS_CYCLIC			(1 << 0)
-#define DAC_DMAC_FLAGS_TLAST			(1 << 1)
 
 /******************************************************************************/
 /*************************** Types Declarations *******************************/
 /******************************************************************************/
 typedef struct {
 	uint32_t dac_baseaddr;
-	uint32_t dmac_baseaddr;
 	uint8_t	 no_of_channels;
 	uint8_t	 resolution;
 	uint8_t	 fifo_present;
@@ -156,7 +125,7 @@ typedef enum {
 	DAC_SRC_PN15,
 	DAC_SRC_PN23,
 	DAC_SRC_PN31,
-	DAC_SRC_LB,		// Loopback data (ADC)
+	DAC_SRC_LB,	// Loopback data (ADC)
 	DAC_SRC_PNXX,	// Device specific
 } dac_data_src;
 
@@ -164,30 +133,23 @@ typedef enum {
 /************************ Functions Declarations ******************************/
 /******************************************************************************/
 int32_t dac_read(dac_core core,
-				 uint32_t reg_addr,
-				 uint32_t *reg_data);
+		uint32_t reg_addr,
+		uint32_t *reg_data);
 int32_t dac_write(dac_core core,
-				  uint32_t reg_addr,
-				  uint32_t reg_data);
-int32_t dac_dmac_read(dac_core core,
-					  uint32_t reg_addr,
-					  uint32_t *reg_data);
-int32_t dac_dmac_write(dac_core core,
-					   uint32_t reg_addr,
-					   uint32_t reg_data);
+		uint32_t reg_addr,
+		uint32_t reg_data);
+
 int32_t dac_setup(dac_core core);
 int32_t dds_set_frequency(dac_core core,
-						  uint32_t chan,
-						  uint32_t freq);
+		uint32_t chan,
+		uint32_t freq);
 int32_t dds_set_phase(dac_core core,
-					  uint32_t chan,
-					  uint32_t phase);
+		uint32_t chan,
+		uint32_t phase);
 int32_t dds_set_scale(dac_core core,
-					  uint32_t chan,
-					  int32_t scale_micro_units);
+		uint32_t chan,
+		int32_t scale_micro_units);
 int32_t dac_data_src_sel(dac_core core,
-						 int32_t chan,
-						 dac_data_src src);
-int32_t dac_dma_setup(dac_core core,
-					  uint32_t start_address);
+		int32_t chan,
+		dac_data_src src);
 #endif
