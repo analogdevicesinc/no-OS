0360830da96d7e21b8a9854ea608b2fde477aa96
Rejeesh Kutty <rejeesh.kutty@analog.com>
2017-01-19 14:28:28 -0500

    daq3- running at 12.5Gbps

--- ad-fmcdaq3-ebz/ad_fmcdaq3_ebz.c
+++ ad-fmcdaq3-ebz/ad_fmcdaq3_ebz.c
@@ -54,22 +54,6 @@
 /********************** Macros and Constants Definitions **********************/
 /******************************************************************************/
 
-#ifdef XILINX
-#define AD9152_ADXCVR_BASEADDR  XPAR_AXI_AD9152_XCVR_BASEADDR
-#define AD9680_ADXCVR_BASEADDR  XPAR_AXI_AD9680_XCVR_BASEADDR
-#define AD9152_CORE_BASEADDR    XPAR_AXI_AD9152_CORE_BASEADDR
-#define AD9680_CORE_BASEADDR    XPAR_AXI_AD9680_CORE_BASEADDR
-#define AD9152_JESD_BASEADDR    XPAR_AXI_AD9152_JESD_BASEADDR
-#define AD9680_JESD_BASEADDR    XPAR_AXI_AD9680_JESD_BASEADDR
-#define AD9152_DMA_BASEADDR     XPAR_AXI_AD9152_DMA_BASEADDR
-#define AD9680_DMA_BASEADDR     XPAR_AXI_AD9680_DMA_BASEADDR
-#endif
-
-#ifdef ZYNQ_PS7
-#define ADC_DDR_BASEADDR        XPAR_DDR_MEM_BASEADDR + 0x0800000
-#define DAC_DDR_BASEADDR        XPAR_DDR_MEM_BASEADDR + 0xA000000
-#endif
-
 #define GPIO_CLKD_STATUS_0      32
 #define GPIO_CLKD_STATUS_1      33
 #define GPIO_DAC_IRQ            34
@@ -135,10 +119,10 @@ int main(void)
 
   // adc-fpga-clock (625M) & sysref (2.44M)
 
-  ad9528_channels[1].channel_num = 4;
-  ad9528_channels[1].channel_divider = 2;
-  ad9528_channels[3].channel_num = 6;
-  ad9528_channels[3].channel_divider = 512;
+  ad9528_channels[6].channel_num = 4;
+  ad9528_channels[6].channel_divider = 2;
+  ad9528_channels[7].channel_num = 6;
+  ad9528_channels[7].channel_divider = 512;
 
   // pllx settings
 
@@ -159,44 +143,49 @@ int main(void)
 
   ad9152_param.lane_rate_kbps = 10000000;
 
-  ad9152_xcvr.base_address = XPAR_AXI_AD9152_XCVR_BASEADDR;
   ad9152_xcvr.mmcm_present = 0;
   ad9152_xcvr.reconfig_bypass = 1;
   ad9152_xcvr.rx_tx_n = 0;
   ad9152_xcvr.no_of_lanes = 4;
   ad9152_xcvr.lane_rate_kbps = 10*1000*1000;
 
-  ad9152_jesd.base_address = AD9152_JESD_BASEADDR;
   ad9152_jesd.rx_tx_n = 0;
   ad9152_jesd.octets_per_frame = 1;
   ad9152_jesd.frames_per_multiframe = 32;
   ad9152_jesd.subclass_mode = 1;
 
+  ad9152_core.no_of_channels = 2;
+  ad9152_core.resolution = 16;
+  ad9152_core.fifo_present = 1;
+
   // adc settings
 
   ad9680_param.lane_rate_kbps = 10000000;
 
-  ad9680_xcvr.base_address = XPAR_AXI_AD9152_XCVR_BASEADDR;
   ad9680_xcvr.mmcm_present = 0;
   ad9680_xcvr.reconfig_bypass = 1;
-  ad9680_xcvr.rx_tx_n = 0;
+  ad9680_xcvr.rx_tx_n = 1;
   ad9680_xcvr.no_of_lanes = 4;
   ad9680_xcvr.lane_rate_kbps = 10*1000*1000;
 
-  ad9680_jesd.base_address = AD9680_JESD_BASEADDR;
   ad9680_jesd.rx_tx_n = 1;
   ad9680_jesd.octets_per_frame = 1;
   ad9680_jesd.frames_per_multiframe = 32;
   ad9680_jesd.subclass_mode = 1;
 
-  ad9152_core.dac_baseaddr = AD9152_CORE_BASEADDR;
-  ad9152_core.no_of_channels = 2;
-  ad9152_core.resolution = 16;
-  ad9152_core.fifo_present = 1;
-
-  ad9680_core.adc_baseaddr = AD9680_CORE_BASEADDR;
   ad9680_core.no_of_channels = 2;
   ad9680_core.resolution = 14;
+
+  // base addresses
+
+#ifdef XILINX
+  ad9152_xcvr.base_address = XPAR_AXI_AD9152_XCVR_BASEADDR;
+  ad9152_jesd.base_address = XPAR_AXI_AD9152_JESD_BASEADDR;
+  ad9152_core.base_address = XPAR_AXI_AD9152_CORE_BASEADDR;
+  ad9680_xcvr.base_address = XPAR_AXI_AD9680_XCVR_BASEADDR;
+  ad9680_jesd.base_address = XPAR_AXI_AD9680_JESD_BASEADDR;
+  ad9680_core.base_address = XPAR_AXI_AD9680_CORE_BASEADDR;
+#endif
 
   // functions (do not modify below)
 
@@ -231,27 +220,18 @@ int main(void)
   dds_set_phase(ad9152_core, 3, 90000);
   dds_set_scale(ad9152_core, 3, 500000);
 
+  jesd_status(ad9680_jesd);
+  ad9680_spi_write(&ad9680_spi_device, AD9680_REG_ADC_TEST_MODE, 0x06);
+  ad9680_spi_write(&ad9680_spi_device, AD9680_REG_OUTPUT_MODE, 0x0);
+  adc_pn_mon(ad9680_core, 0);
 
-  ad9680_spi_write(&ad9680_spi_device, AD9680_REG_DEVICE_INDEX, 0x3);
+
+  jesd_status(ad9680_jesd);
   ad9680_spi_write(&ad9680_spi_device, AD9680_REG_ADC_TEST_MODE, 0x05);
   ad9680_spi_write(&ad9680_spi_device, AD9680_REG_OUTPUT_MODE, 0x0);
-
   adc_pn_mon(ad9680_core, 1);
 
-  xil_printf("PRBS test done.\n");
-
-  ad9680_spi_write(&ad9680_spi_device, AD9680_REG_DEVICE_INDEX, 0x3);
-  ad9680_spi_write(&ad9680_spi_device, AD9680_REG_ADC_TEST_MODE, 0x0f);
-  ad9680_spi_write(&ad9680_spi_device, AD9680_REG_OUTPUT_MODE, 0x1);
-
-
-  xil_printf("Ramp capture done.\n");
-
-  ad9680_spi_write(&ad9680_spi_device, AD9680_REG_DEVICE_INDEX, 0x3);
-  ad9680_spi_write(&ad9680_spi_device, AD9680_REG_ADC_TEST_MODE, 0x00);
-  ad9680_spi_write(&ad9680_spi_device, AD9680_REG_OUTPUT_MODE, 0x1);
-
-  xil_printf("Test mode off capture done.\n");
+  ad_printf("daq3: done\n");
 
   return 0;
 }
