91aea2e313717188f73ae7260bc26171abd48cf1
Istvan Csomortani <istvan.csomortani@analog.com>
2017-02-13 11:57:25 +0200

    ad9625: Re-indent source, no functional changes

--- drivers/ad9625/ad9625.c
+++ drivers/ad9625/ad9625.c
@@ -1,41 +1,41 @@
 /***************************************************************************//**
-* @file ad9625.c
-* @brief Implementation of AD9625 Driver.
-* @author DBogdan (dragos.bogdan@analog.com)
-********************************************************************************
-* Copyright 2014(c) Analog Devices, Inc.
-*
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-* - Redistributions of source code must retain the above copyright
-* notice, this list of conditions and the following disclaimer.
-* - Redistributions in binary form must reproduce the above copyright
-* notice, this list of conditions and the following disclaimer in
-* the documentation and/or other materials provided with the
-* distribution.
-* - Neither the name of Analog Devices, Inc. nor the names of its
-* contributors may be used to endorse or promote products derived
-* from this software without specific prior written permission.
-* - The use of this software may or may not infringe the patent rights
-* of one or more patent holders. This license does not release you
-* from the requirement that you obtain separate licenses from these
-* patent holders to use this software.
-* - Use of the software either in source or binary form, must be run
-* on or directly connected to an Analog Devices Inc. component.
-*
-* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-* IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-* LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*******************************************************************************/
+ * @file ad9625.c
+ * @brief Implementation of AD9625 Driver.
+ * @author DBogdan (dragos.bogdan@analog.com)
+ ********************************************************************************
+ * Copyright 2014(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ * - Neither the name of Analog Devices, Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * - The use of this software may or may not infringe the patent rights
+ * of one or more patent holders. This license does not release you
+ * from the requirement that you obtain separate licenses from these
+ * patent holders to use this software.
+ * - Use of the software either in source or binary form, must be run
+ * on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
 
 /******************************************************************************/
 /***************************** Include Files **********************************/
@@ -52,11 +52,11 @@
 uint8_t spi_is_initialized = 0;
 
 /***************************************************************************//**
-* @brief ad9625_spi_read
-*******************************************************************************/
+ * @brief ad9625_spi_read
+ *******************************************************************************/
 int32_t ad9625_spi_read(ad9625_dev *dev,
-						uint16_t reg_addr,
-						uint8_t *reg_data)
+		uint16_t reg_addr,
+		uint8_t *reg_data)
 {
 	uint8_t buf[3];
 	int32_t ret;
@@ -72,11 +72,11 @@ int32_t ad9625_spi_read(ad9625_dev *dev,
 }
 
 /***************************************************************************//**
-* @brief ad9625_spi_write
-*******************************************************************************/
+ * @brief ad9625_spi_write
+ *******************************************************************************/
 int32_t ad9625_spi_write(ad9625_dev *dev,
-						 uint16_t reg_addr,
-						 uint8_t reg_data)
+		uint16_t reg_addr,
+		uint8_t reg_data)
 {
 	uint8_t buf[3];
 	int32_t ret;
@@ -91,10 +91,10 @@ int32_t ad9625_spi_write(ad9625_dev *dev,
 }
 
 /***************************************************************************//**
-* @brief ad9625_setup
-*******************************************************************************/
+ * @brief ad9625_setup
+ *******************************************************************************/
 int32_t ad9625_setup(ad9625_dev **device,
-					 ad9625_init_param init_param)
+		ad9625_init_param init_param)
 {
 	ad9625_dev *dev;
 	uint8_t chip_id;
--- drivers/ad9625/ad9625.h
+++ drivers/ad9625/ad9625.h
@@ -1,41 +1,41 @@
 /***************************************************************************//**
-* @file ad9625.h
-* @brief Header file of AD9625 Driver.
-* @author DBogdan (dragos.bogdan@analog.com)
-********************************************************************************
-* Copyright 2014(c) Analog Devices, Inc.
-*
-* All rights reserved.
-*
-* Redistribution and use in source and binary forms, with or without
-* modification, are permitted provided that the following conditions are met:
-* - Redistributions of source code must retain the above copyright
-* notice, this list of conditions and the following disclaimer.
-* - Redistributions in binary form must reproduce the above copyright
-* notice, this list of conditions and the following disclaimer in
-* the documentation and/or other materials provided with the
-* distribution.
-* - Neither the name of Analog Devices, Inc. nor the names of its
-* contributors may be used to endorse or promote products derived
-* from this software without specific prior written permission.
-* - The use of this software may or may not infringe the patent rights
-* of one or more patent holders. This license does not release you
-* from the requirement that you obtain separate licenses from these
-* patent holders to use this software.
-* - Use of the software either in source or binary form, must be run
-* on or directly connected to an Analog Devices Inc. component.
-*
-* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
-* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
-* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-* IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
-* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-* LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
-* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*******************************************************************************/
+ * @file ad9625.h
+ * @brief Header file of AD9625 Driver.
+ * @author DBogdan (dragos.bogdan@analog.com)
+ ********************************************************************************
+ * Copyright 2014(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * - Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * - Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in
+ * the documentation and/or other materials provided with the
+ * distribution.
+ * - Neither the name of Analog Devices, Inc. nor the names of its
+ * contributors may be used to endorse or promote products derived
+ * from this software without specific prior written permission.
+ * - The use of this software may or may not infringe the patent rights
+ * of one or more patent holders. This license does not release you
+ * from the requirement that you obtain separate licenses from these
+ * patent holders to use this software.
+ * - Use of the software either in source or binary form, must be run
+ * on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
 #ifndef AD9625_H_
 #define AD9625_H_
 
@@ -48,18 +48,18 @@
 /********************** Macros and Constants Definitions **********************/
 /******************************************************************************/
 #define AD9625_REG_CHIP_PORT_CONF				0x000
-#define AD9625_REG_CHIP_ID						0x001
+#define AD9625_REG_CHIP_ID					0x001
 #define AD9625_REG_POWER_MODE					0x008
 #define AD9625_REG_PLL_STATUS					0x00A
 #define AD9625_REG_TEST_CNTRL					0x00D
 #define AD9625_REG_OUTPUT_MODE					0x014
 #define AD9625_REG_OUTPUT_ADJUST				0x015
-#define AD9625_REG_JESD204B_LINK_CNTRL_1		0x05F
-#define AD9625_REG_JESD204B_LANE_POWER_MODE		0x080
-#define AD9625_REG_TRANSFER						0x0FF
+#define AD9625_REG_JESD204B_LINK_CNTRL_1			0x05F
+#define AD9625_REG_JESD204B_LANE_POWER_MODE			0x080
+#define AD9625_REG_TRANSFER					0x0FF
 #define AD9625_REG_DIVCLK_OUT_CNTRL				0x120
 
-#define AD9625_CHIP_ID							0x041
+#define AD9625_CHIP_ID						0x041
 
 /******************************************************************************/
 /*************************** Types Declarations *******************************/
@@ -79,12 +79,12 @@ typedef struct {
 /************************ Functions Declarations ******************************/
 /******************************************************************************/
 int32_t ad9625_spi_read(ad9625_dev *dev,
-						uint16_t reg_addr,
-						uint8_t *reg_data);
+		uint16_t reg_addr,
+		uint8_t *reg_data);
 int32_t ad9625_spi_write(ad9625_dev *dev,
-						 uint16_t reg_addr,
-						 uint8_t reg_data);
+		uint16_t reg_addr,
+		uint8_t reg_data);
 int32_t ad9625_setup(ad9625_dev **device,
-					 ad9625_init_param init_param);
+		ad9625_init_param init_param);
 
 #endif
