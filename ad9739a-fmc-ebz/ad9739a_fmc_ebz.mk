566df3f562a2c6c4e35e40fd4724414c6969231c
AndreiGrozav <andrei.grozav@analog.com>
2017-04-24 19:32:00 +0300

    ad9739a-fmc-ebz: Update to the new framework

---
+++ ad9739a-fmc-ebz/ad9739a_fmc_ebz.mk
@@ -0,0 +1,23 @@
+## NOOS-DIR:
+##		is the no-OS root variable (do not override)
+##
+## M_INC_DIRS:
+##		list all your include folders (all *.h and *.c files from these folders are included
+##		in compile and passed as directives)
+##
+## M_HDR_FILES:
+##		if you want to hand pick files, use this variable to list header files.
+##
+## M_SRC_FILES:
+##		if you want to hand pick files, use this variable to list source files.
+
+M_INC_DIRS := $(NOOS-DIR)/common_drivers/platform_drivers
+M_INC_DIRS += $(NOOS-DIR)/common_drivers/dac_core
+M_INC_DIRS += $(NOOS-DIR)/common_drivers/dmac_core
+M_INC_DIRS += $(NOOS-DIR)/drivers/ad9739a
+M_INC_DIRS += $(NOOS-DIR)/drivers/adf4350
+
+M_HDR_FILES :=
+
+M_SRC_FILES := $(NOOS-DIR)/ad9739a-fmc-ebz/ad9739a_fmc_ebz.c
+
