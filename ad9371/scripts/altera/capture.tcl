65411160cd75595f04698d75cf69b7c7be7eb41a
stefpopa <Stefan.Popa@analog.com>
2017-05-26 14:49:25 +0300

    ad9371:  Added capture scripts for Altera
    Altera scripts for retrieving tx data from memory

---
+++ ad9371/scripts/altera/capture.tcl
@@ -0,0 +1,33 @@
+puts "Acquiring data"
+set mm [ lindex [ get_service_paths master ] 0 ]
+close_service master $mm
+open_service master $mm
+
+set startAddress 0x800000
+
+set fp1 [ open iq_rx1.csv a ]
+set fp2 [ open iq_rx2.csv a ]
+set readData [ master_read_32 $mm $startAddress 32768]
+
+for {set index 1} {$index < 65536} {incr index 4} {
+	set data [lindex $readData $index]
+	
+	set sampleI1 [expr {$data & 0xFFFF}]
+	set sampleQ1 [expr {($data >> 16) & 0xFFFF}]
+	
+	set data [lindex $readData [expr {$index + 2}]]
+	
+	set sampleI2 [expr {$data & 0xFFFF}]
+	set sampleQ2 [expr {($data >> 16) & 0xFFFF}]
+	
+	puts $fp1 $sampleI1
+	puts $fp1 $sampleQ1
+	puts $fp2 $sampleI2
+	puts $fp2 $sampleQ2
+}
+
+close $fp1
+close $fp2
+
+puts "Iteration complete"
+close_service master $mm
