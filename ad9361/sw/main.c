d6637ee20cc142160e7a90d397e7b61070a84d2e
Dragos Bogdan <dragos.bogdan@analog.com>
2017-09-21 10:10:23 +0300

    ad9361: Increase lvds_bias for ARRADIO
    It prevents the issues that can appear when the part operates at the highest sampling rate.

--- ad9361/sw/main.c
+++ ad9361/sw/main.c
@@ -259,7 +259,11 @@ AD9361_InitParam default_init_param = {
 	4,		//rx_data_delay *** adi,rx-data-delay
 	7,		//tx_fb_clock_delay *** adi,tx-fb-clock-delay
 	0,		//tx_data_delay *** adi,tx-data-delay
+#ifdef ALTERA_PLATFORM
+	300,	//lvds_bias_mV *** adi,lvds-bias-mV
+#else
 	150,	//lvds_bias_mV *** adi,lvds-bias-mV
+#endif
 	1,		//lvds_rx_onchip_termination_enable *** adi,lvds-rx-onchip-termination-enable
 	0,		//rx1rx2_phase_inversion_en *** adi,rx1-rx2-phase-inversion-enable
 	0xFF,	//lvds_invert1_control *** adi,lvds-invert1-control
