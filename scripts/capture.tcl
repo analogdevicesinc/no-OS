ca61e46288582bf6190f2c70e3b6fedbc2b2d98b
Istvan Csomortani <istvan.csomortani@analog.com>
2017-10-25 09:22:47 +0100

    scripts: Fix the capture script
    The XSDB command mrd expect number of words as argument, because each
    word will containt 2 samples in the memory, the num_of_word should be
    num_of_sample divided by two.
    The mrd command will return a list which have the following structure:
    ADDR0 DATA0 ADDR1 DATA1 ... We want to save just the data words, so
    index should start from 1 and incremented by 2, and run until it reaches
    2*num_of_word.

--- scripts/capture.tcl
+++ scripts/capture.tcl
@@ -24,11 +24,11 @@ puts "Moving data into .csv files..."
 after 1000
 
 set start_addr [expr 0x$start_addr]
-set num_of_word [expr 2 * $num_of_sample]
+set num_of_word [expr $num_of_sample / 2]
 set readData [mrd -force $start_addr $num_of_word]
 
 set fp [ open capture.csv w ]
-for {set index 1} {$index < $num_of_word} {incr index 4} {
+for {set index 1} {$index < $num_of_word*2} {incr index 2} {
 
 	set data [lindex $readData $index]
 
