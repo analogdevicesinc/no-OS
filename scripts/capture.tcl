1eb88a698048e679d07233037870f0b094f9f56f
AndreiGrozav <andrei.grozav@analog.com>
2017-11-13 10:32:02 +0200

    capture.tcl: Update
    - Add generic channel capture flow (files will be automatically
    created for the number of channels specified)
    - All data is presumed to be stored after cpack_core considerations
    - I/Q data should be considered as separated channels
    - Add skeleton for data capture features information about the stored
    data is written by software at start_address - info_offset
    (number of channels, real/stored bits, etc.)

--- scripts/capture.tcl
+++ scripts/capture.tcl
@@ -1,9 +1,16 @@
+# Data acquisition trough xsdb
 
-# xsdb tcl file
+# This script has two modes of geting info of the data to read from memory and store
+# in local files:
+#      1. Receive al info from the command line
+#      2. Receive the start address for the captured data in memory, the rest
+#         of the info will be automatically read out of the memory (NOT YET IMPLEMENTED);
 
 set m_type [lindex $argv 0]
-set start_addr [lindex $argv 1]
-set num_of_sample [lindex $argv 2]
+set start_addr [lindex $argv 1]      ;# hex (0x...)
+set num_of_samples [lindex $argv 2]  ;# decinal
+set num_of_channels [lindex $argv 3] ;# for RF projects consider 2x nr of channels(I and Q)
+set storage_bits  [lindex $argv 4]   ;# for 8-16bit rezolution data is stored on 16bit (max 32)
 
 connect
 
@@ -16,26 +23,115 @@ if {$m_type == "ZYNQ_PS7"} {
 }
 
 if {$m_type == "MICROBLAZE"} {
-  targets -set -filter {name =~ "*MicroBlaze*"}
+  targets -set -filter {name =~ "*MicroBlaze #0*"}
 }
-
-puts "Moving data into .csv files..."
 
 after 1000
 
-set start_addr [expr 0x$start_addr]
-set num_of_word [expr 2 * $num_of_sample]
+if { $storage_bits  == "" } {
+	puts "Read parameters from memory"
+	set format_address          [expr $start_addr - 0x02 1]
+	set real_bits_address       [expr $start_addr - 0x04 1]
+	set storage_bits_address    [expr $start_addr - 0x06 1] ;# bytes_per_sample
+	set shift_address           [expr $start_addr - 0x08 1]
+	set endianness_address      [expr $start_addr - 0x10 1]
+	set num_of_samples_address  [expr $start_addr - 0x12 1]
+	set num_of_channels_address [expr $start_addr - 0x20 1]
+
+	# Used information
+	set storage_bits [mrd -force $storage_bits_address]
+	set num_of_samples [mrd -force $num_of_samples_address]
+	set num_of_channels [mrd -force $num_of_channels_address]
+}
+
+if { $storage_bits  == "" } {
+	puts "WRONG PARAMETER VALUE storage_bits  = $storage_bits "
+	exit
+}
+if { $num_of_samples == ""} {
+	puts "WRONG PARAMETER VALUE num_of_samples = $num_of_samples"
+	exit
+}
+if { $num_of_channels == ""} {
+	puts "WRONG PARAMETER VALUE num_of_channels = $num_of_channels"
+	exit
+}
+
+puts "Script parameters:"
+puts "Start address = $start_addr"
+puts "num of samples = $num_of_samples"
+puts "num of channels = $num_of_channels"
+puts "bytes per sample = $storage_bits\n"
+
+puts "Moving data into .csv files..."
+
+set start_addr [expr $start_addr]
+set num_of_word [expr 2 * $num_of_samples]
 set readData [mrd -force $start_addr $num_of_word]
 
-set fp [ open capture.csv w ]
-for {set index 1} {$index < $num_of_word} {incr index 4} {
+set f_name "capture_ch"
+set f_type ".csv"
 
-	set data [lindex $readData $index]
-
-	puts $fp $data
-
+for {set index 1} {$index <= $num_of_channels} {incr index 1} {
+	set file_name $f_name$index$f_type
+	file delete -force $file_name
+	set f($index) [ open $file_name a ]
 }
-close $fp
+
+# mrd returns:
+# index     x       x+1
+# info   address: data(32b)
+
+# Arrange channel data into individual files (considering hdl system has a cpack core)
+set ch_x_index 1
+for {set index 1} {$index < $num_of_samples} {incr index 2} {
+	set data [lindex $readData $index] ; # 32 bit, 16 bit/channal
+	set data [expr 0x$data]
+	if { $storage_bits <= 16 } {
+		set sample1 [expr {$data & 0xFFFF}]
+		set sample2 [expr {($data >> 16) & 0xFFFF}]
+		if { $num_of_channels == 1 } {
+			puts $f($ch_x_index) $sample1
+			puts $f($ch_x_index) $sample2
+		} else {
+			for {set n 1} {$n < $num_of_channels } {incr n 2} {
+				puts $f($ch_x_index) $sample1
+				if {$ch_x_index < $num_of_channels} {
+					set ch_x_index [expr $ch_x_index + 1]
+				} else {
+					set ch_x_index 1
+				}
+
+				puts $f($ch_x_index) $sample2
+				if {$ch_x_index < $num_of_channels} {
+					set ch_x_index [expr $ch_x_index + 1]
+				} else {
+					set ch_x_index 1
+				}
+			}
+		}
+	} elseif { $storage_bits <= 32 } {
+		set sample [expr {$data & 0xFFFFFF}]
+		if { $num_of_channels == 1 } {
+			puts $f($ch_x_index) $sample
+		} else
+			for {set n 1} {$n < $num_of_channels } {incr n 1} {
+				if {$ch_x_index < $num_of_channels} {
+					set ch_x_index [expr $ch_x_index + 1]
+				} else {
+					set ch_x_index 1
+				}
+				puts $f($ch_x_index) $sample
+			}
+	} else {
+		puts "ERROR: Unsupported number of bits/sample"
+		exit
+	}
+}
+
+for {set index 1} {$index <= $num_of_channels} {incr index 1} {
+	close $f($index)
+}
 
 puts "Done."
 
