c48f6023920aaa04fc7f05e242622340bc1aea75
AndreiGrozav <andrei.grozav@analog.com>
2017-11-22 14:31:07 +0200

    fmcdaq3: Updates for rev C - first step
    At this point ad9528 does not lock

--- fmcdaq3/fmcdaq3.c
+++ fmcdaq3/fmcdaq3.c
@@ -100,54 +100,62 @@ int main(void)
 	ad9528_param.channels = &ad9528_channels[0];
 	ad9528_init(&ad9528_param);
 
-	// dac-device-clock (1.25G) & sysref (2.44M)
-
+	// dac-device-clock (1.2G)
 	ad9528_channels[0].channel_num = 2;
 	ad9528_channels[0].channel_divider = 1;
+
+	//adc sysref
 	ad9528_channels[1].channel_num = 8;
-	ad9528_channels[1].channel_divider = 512;
+	ad9528_channels[1].channel_divider = 1;
 
-	// dac-fpga-clock (625M) & sysref (2.44M)
-
+	// adc-fpga-clock (600M)
 	ad9528_channels[2].channel_num = 9;
 	ad9528_channels[2].channel_divider = 2;
+
+	// adc sysref clkd
 	ad9528_channels[3].channel_num = 7;
-	ad9528_channels[3].channel_divider = 512;
+	ad9528_channels[3].channel_divider = 2;
 
-	// adc-device-clock (1.2G) & sysref (2.44M)
-
+	// adc-device-clock (1.2G)
 	ad9528_channels[4].channel_num = 13;
 	ad9528_channels[4].channel_divider = 1;
+
+	// dac sysref
 	ad9528_channels[5].channel_num = 5;
-	ad9528_channels[5].channel_divider = 512;
+	ad9528_channels[5].channel_divider = 1;
 
-	// adc-fpga-clock (625M) & sysref (2.44M)
-
+	// dac-fpga-fmc (600M)
 	ad9528_channels[6].channel_num = 4;
 	ad9528_channels[6].channel_divider = 2;
+
+	// dac sysref clkd
 	ad9528_channels[7].channel_num = 6;
-	ad9528_channels[7].channel_divider = 512;
+	ad9528_channels[7].channel_divider = 2;
 
 	// pllx settings
 
 	ad9528_param.spi3wire = 1;
-	ad9528_param.vcxo_freq = 125000000;
+	ad9528_param.vcxo_freq = 100000000;
 	ad9528_param.osc_in_diff_en = 1;
-	ad9528_param.pll2_charge_pump_current_nA = 805000;
+	ad9528_param.pll2_charge_pump_current_nA = 35000;
 	ad9528_param.pll2_vco_diff_m1 = 3;
 	ad9528_param.pll2_ndiv_a_cnt = 2;
 	ad9528_param.pll2_ndiv_b_cnt = 7;
-	ad9528_param.pll2_n2_div = 10;
-	ad9528_param.sysref_k_div = 512;
+	ad9528_param.pll2_n2_div = 12;
+	ad9528_param.sysref_k_div = 128;
 	ad9528_param.rpole2 = RPOLE2_900_OHM;
 	ad9528_param.rzero= RZERO_3250_OHM;
 	ad9528_param.cpole1 = CPOLE1_16_PF;
 
 	// dac settings
 
-	ad9152_xcvr.mmcm_present = 0;
-	ad9152_xcvr.reconfig_bypass = 1;
-	ad9152_xcvr.lane_rate_kbps = 12500000;
+	xcvr_getconfig(&ad9152_xcvr);
+	ad9152_xcvr.reconfig_bypass = 0;
+	ad9152_xcvr.ref_clock_khz = 600000;
+	ad9152_xcvr.lane_rate_kbps = 12000000;
+#ifdef XILINX
+	ad9152_xcvr.dev.qpll_enable = 1;
+#endif
 
 	ad9152_jesd.rx_tx_n = 0;
 	ad9152_jesd.scramble_enable = 1;
@@ -181,15 +189,19 @@ int main(void)
 	ad9152_param.stpl_samples[1][2] = (ad9152_channels[1].pat_data>> 0) & 0xffff;
 	ad9152_param.stpl_samples[1][3] = (ad9152_channels[1].pat_data>>16) & 0xffff;
 	ad9152_param.interpolation = 1;
-	ad9152_param.lane_rate_kbps = 12500000;
+	ad9152_param.lane_rate_kbps = 12000000;
 
 	// adc settings
 
-	ad9680_param.lane_rate_kbps = 12500000;
+	ad9680_param.lane_rate_kbps = 12000000;
 
-	ad9680_xcvr.mmcm_present = 0;
-	ad9680_xcvr.reconfig_bypass = 1;
-	ad9680_xcvr.lane_rate_kbps = 12500000;
+	// xcvr_getconfig(&ad9680_xcvr);
+	ad9680_xcvr.reconfig_bypass = 0;
+	ad9680_xcvr.ref_clock_khz = 600000;
+	ad9680_xcvr.lane_rate_kbps = 12000000;
+#ifdef XILINX
+	ad9680_xcvr.dev.qpll_enable = 1;
+#endif
 
 	ad9680_jesd.rx_tx_n = 1;
 	ad9680_jesd.scramble_enable = 1;
@@ -221,21 +233,30 @@ int main(void)
 	ad9152_core.base_address = XPAR_AXI_AD9152_CORE_BASEADDR;
 	ad9680_xcvr.base_address = XPAR_AXI_AD9680_XCVR_BASEADDR;
 	ad9680_core.base_address = XPAR_AXI_AD9680_CORE_BASEADDR;
-	ad9152_jesd.base_address = XPAR_AXI_AD9152_JESD_BASEADDR;
-	ad9680_jesd.base_address = XPAR_AXI_AD9680_JESD_BASEADDR;
+	ad9152_jesd.base_address = XPAR_AXI_AD9152_JESD_TX_AXI_BASEADDR;
+	ad9680_jesd.base_address = XPAR_AXI_AD9680_JESD_RX_AXI_BASEADDR;
 	ad9680_dma.base_address = XPAR_AXI_AD9680_DMA_BASEADDR;
 #endif
 #ifdef ALTERA
-	ad9152_xcvr.base_address = AXI_AD9152_XCVR_BASE;
+	ad9152_xcvr.base_address = AD9152_JESD204_LINK_MANAGEMENT_BASE;
+	ad9152_xcvr.dev.link_pll.base_address = AD9152_JESD204_LINK_PLL_RECONFIG_BASE;
+	ad9152_xcvr.dev.atx_pll.base_address = AD9152_JESD204_LANE_PLL_RECONFIG_BASE;
 	ad9152_core.base_address = AXI_AD9152_CORE_BASE;
-	ad9680_xcvr.base_address = AXI_AD9680_XCVR_BASE;
+	AD9680_XCVR.BASE_ADDRESS = AD9680_JESD204_LINK_MANAGEMENT_BASE;
+	ad9680_xcvr.dev.link_pll.base_address = AD9680_JESD204_LINK_PLL_RECONFIG_BASE;
 	ad9680_core.base_address = AXI_AD9680_CORE_BASE;
-	ad9152_jesd.base_address = AVL_AD9152_XCVR_IP_RECONFIG_BASE;
-	ad9680_jesd.base_address = AVL_AD9680_XCVR_IP_RECONFIG_BASE;
-	ad9152_xcvr.mmcm_lpll_base_address = AVL_AD9152_XCVR_CORE_PLL_RECONFIG_BASE;
-	ad9680_xcvr.mmcm_lpll_base_address = AVL_AD9680_XCVR_CORE_PLL_RECONFIG_BASE;
-	ad9152_xcvr.tx_lane_pll_base_address = AVL_AD9152_XCVR_LANE_PLL_RECONFIG_BASE;
+	ad9152_jesd.base_address = AD9152_JESD204_LINK_RECONFIG_BASE;
+	ad9680_jesd.base_address = AD9680_JESD204_LINK_RECONFIG_BASE;
+
+	ad9152_xcvr.dev.channel_pll[0].type = cmu_tx_type;
+	ad9680_xcvr.dev.channel_pll[0].type = cmu_cdr_type;
+	ad9152_xcvr.dev.channel_pll[0].base_address = AVL_ADXCFG_0_RCFG_S0_BASE;
+	ad9680_xcvr.dev.channel_pll[0].base_address = AVL_ADXCFG_0_RCFG_S1_BASE;
+
 	ad9680_dma.base_address = AXI_AD9680_DMA_BASE;
+	ad9152_dma.base_address = AXI_AD9152_DMA_BASE;
+	rx_xfer.start_address =  0x800000;
+	tx_xfer.start_address =  0x900000;;
 #endif
 
 	// functions (do not modify below)
@@ -248,8 +269,8 @@ int main(void)
 
 	ad9152_setup(&ad9152_spi_device, ad9152_param);
 	jesd_setup(ad9152_jesd);
-	xcvr_setup(ad9152_xcvr);
-	jesd_status(ad9152_jesd);
+	xcvr_setup(&ad9152_xcvr);
+	axi_jesd204_tx_status_read(ad9152_jesd);
 	dac_setup(&ad9152_core);
 	ad9152_status(&ad9152_spi_device);
 
@@ -276,8 +297,8 @@ int main(void)
 
 	ad9680_setup(&ad9680_spi_device, ad9680_param);
 	jesd_setup(ad9680_jesd);
-	xcvr_setup(ad9680_xcvr);
-	jesd_status(ad9680_jesd);
+	xcvr_setup(&ad9680_xcvr);
+	axi_jesd204_tx_status_read(ad9680_jesd);
 	adc_setup(ad9680_core);
 	ad9680_test(&ad9680_spi_device, AD9680_TEST_PN9);
 	if(adc_pn_mon(ad9680_core, ADC_PN9) == -1) {
