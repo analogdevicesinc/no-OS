819f08655385cb3568260fea4e250cd7a28c5479
Andrei Drimbarean <andrei.drimbarean@analog.com>
2017-11-22 10:26:47 +0200

    AD7156: Update to new platform driver
    Signed-off-by: Andrei Drimbarean <andrei.drimbarean@analog.com>

--- drivers/AD7156/AD7156.c
+++ drivers/AD7156/AD7156.c
@@ -41,8 +41,10 @@
 /******************************************************************************/
 /***************************** Include Files **********************************/
 /******************************************************************************/
+#include <stdint.h>
+#include <stdlib.h>
+#include "platform_drivers.h"
 #include "AD7156.h"
-#include "Communication.h"
 
 /******************************************************************************/
 /************************ Variables Declarations ******************************/
@@ -57,82 +59,123 @@ float ad7156Channel2Range = 2;
 /***************************************************************************//**
  * @brief Performs a burst read of a specified number of registers.
  *
+ * @param dev             - The device structure.
  * @param pReadData       - The read values are stored in this buffer.
  * @param registerAddress - The start address of the burst read.
  * @param bytesNumber     - Number of bytes to read.
  *
  * @return None.
 *******************************************************************************/
-void AD7156_GetRegisterValue(unsigned char* pReadData,
+void AD7156_GetRegisterValue(ad7156_dev *dev,
+			     unsigned char* pReadData,
                              unsigned char registerAddress,
                              unsigned char bytesNumber)
 {
-    I2C_Write(AD7156_ADDRESS, &registerAddress, 1, 0);
-    I2C_Read(AD7156_ADDRESS, pReadData, bytesNumber, 1);
+	i2c_write(dev->i2c_desc, &registerAddress, 1, 0);
+	i2c_read(dev->i2c_desc, pReadData, bytesNumber, 1);
 }
 
 /***************************************************************************//**
  * @brief Writes data into one or two registers.
  *
+ * @param dev             - The device structure.
  * @param registerValue   - Data value to write.
  * @param registerAddress - Address of the register.
  * @param bytesNumber     - Number of bytes. Accepted values: 0 - 1.
  *
  * @return None.
 *******************************************************************************/
-void AD7156_SetRegisterValue(unsigned short registerValue,
+void AD7156_SetRegisterValue(ad7156_dev *dev,
+			     unsigned short registerValue,
                              unsigned char  registerAddress,
                              unsigned char  bytesNumber)
 {
     unsigned char dataBuffer[3] = {0, 0, 0};
-    
+
     dataBuffer[0] = registerAddress;
     dataBuffer[1] = (registerValue >> 8);
     dataBuffer[bytesNumber] = (registerValue & 0x00FF);
-    I2C_Write(AD7156_ADDRESS, dataBuffer, bytesNumber + 1, 1);
+	i2c_write(dev->i2c_desc, dataBuffer, bytesNumber + 1, 1);
 }
 
 /***************************************************************************//**
- * @brief Initializes the communication peripheral and checks if the device is
- *        present.
+ * @brief Initializes the communication peripheral and the initial Values for
+ *        AD7156 Board.
  *
- * @return status - The result of the initialization procedure.
- *                  Example: -1 - I2C peripheral was not initialized or the
- *                                 device is not present.
- *                            0 - I2C peripheral was initialized and the
- *                                 device is present.
+ * @param device     - The device structure.
+ * @param init_param - The structure that contains the device initial
+ * 		       parameters.
+ *
+ * @return ret - The result of the initialization procedure.
+ *               Example: -1 - I2C peripheral was not initialized or the
+ *                             device is not present.
+ *                         0 - I2C peripheral was initialized and the
+ *                             device is present.
 *******************************************************************************/
-char AD7156_Init(void)
+char AD7156_Init(ad7156_dev **device,
+		 ad7156_init_param init_param)
 {
+	ad7156_dev* dev;
     char          status = -1;
     unsigned char test   = 0;
-    
-    status = I2C_Init(100000);
-    AD7156_GetRegisterValue(&test, AD7156_REG_CHIP_ID, 1);
+
+	dev = (ad7156_dev *)malloc(sizeof(*dev));
+	if (!dev)
+		return -1;
+
+    status = i2c_init(&dev->i2c_desc, init_param.i2c_init);
+    AD7156_GetRegisterValue(dev,
+			    &test,
+			    AD7156_REG_CHIP_ID,
+			    1);
     if(test != AD7156_DEFAULT_ID)
     {
         status = -1;
     }
+
+	*device = dev;
+
     return status;
+}
+
+/***************************************************************************//**
+ * @brief Free the resources allocated by AD7156_Init().
+ *
+ * @param dev - The device structure.
+ *
+ * @return ret - The result of the remove procedure.
+*******************************************************************************/
+int32_t AD7156_remove(ad7156_dev *dev)
+{
+	int32_t status;
+
+	status = i2c_remove(dev->i2c_desc);
+
+	free(dev);
+
+	return status;
 }
 
 /***************************************************************************//**
  * @brief Resets the device.
  *
+ * @param dev - The device structure.
+ *
  * @return None.
 *******************************************************************************/
-void AD7156_Reset(void)
+void AD7156_Reset(ad7156_dev *dev)
 {
     unsigned char addressPointer = 0;
 
     addressPointer = AD7156_RESET_CMD;
-    I2C_Write(AD7156_ADDRESS, &addressPointer, 1, 1);
+	i2c_write(dev->i2c_desc, &addressPointer, 1, 1);
 }
 
 /***************************************************************************//**
  * @brief Sets the converter mode of operation.
  *
- * @oaram pwrMode - Mode of operation option.
+ * @param dev     - The device structure.
+ * @param pwrMode - Mode of operation option.
  *		    Example: AD7156_CONV_MODE_IDLE - Idle
  *                           AD7156_CONV_MODE_CONT_CONV  - Continuous conversion
  *                           AD7156_CONV_MODE_SINGLE_CONV - Single conversion
@@ -140,20 +183,28 @@ void AD7156_Reset(void)
  *
  * @return None.
 *******************************************************************************/
-void AD7156_SetPowerMode(unsigned char pwrMode)
+void AD7156_SetPowerMode(ad7156_dev *dev,
+			 unsigned char pwrMode)
 {
     unsigned char oldConfigReg = 0;
     unsigned char newConfigReg = 0;
 
-    AD7156_GetRegisterValue(&oldConfigReg, AD7156_REG_CONFIG, 1);
+    AD7156_GetRegisterValue(dev,
+			    &oldConfigReg,
+			    AD7156_REG_CONFIG,
+			    1);
     oldConfigReg &= ~AD7156_CONFIG_MD(0x3);
     newConfigReg = oldConfigReg| AD7156_CONFIG_MD(pwrMode);
-    AD7156_SetRegisterValue(newConfigReg, AD7156_REG_CONFIG, 1);
+    AD7156_SetRegisterValue(dev,
+			    newConfigReg,
+			    AD7156_REG_CONFIG,
+			    1);
 }
 
 /***************************************************************************//**
  * @brief Enables or disables conversion on the selected channel.
  *
+ * @param dev        - The device structure.
  * @param channel    - Channel option.
  *                      Example: AD7156_CHANNEL1
  *                               AD7156_CHANNEL2
@@ -163,26 +214,35 @@ void AD7156_SetPowerMode(unsigned char pwrMode)
  *
  * @return None.
 *******************************************************************************/
-void AD7156_ChannelState(unsigned char channel, unsigned char enableConv)
+void AD7156_ChannelState(ad7156_dev *dev,
+			 unsigned char channel,
+			 unsigned char enableConv)
 {
     unsigned char oldConfigReg = 0;
     unsigned char newConfigReg = 0;
     unsigned char channelMask  = 0;
-    
+
     channelMask = (channel == 1) ? AD7156_CONFIG_EN_CH1 : AD7156_CONFIG_EN_CH2;
 
-    AD7156_GetRegisterValue(&oldConfigReg, AD7156_REG_CONFIG, 1);
+    AD7156_GetRegisterValue(dev,
+			    &oldConfigReg,
+			    AD7156_REG_CONFIG,
+			    1);
     oldConfigReg &= ~channelMask;
     newConfigReg = oldConfigReg | (channelMask * enableConv);
-    AD7156_SetRegisterValue(newConfigReg, AD7156_REG_CONFIG, 1);
+    AD7156_SetRegisterValue(dev,
+			    newConfigReg,
+			    AD7156_REG_CONFIG,
+			    1);
 }
 
 /***************************************************************************//**
  * @brief Sets the input range of the specified channel.
  *
+ * @param dev     - The device structure.
  * @param channel - Channel option.
  *                  Example: AD7156_CHANNEL1
- *                           AD7156_CHANNEL2 
+ *                           AD7156_CHANNEL2
  * @param range   - Input range option.
  *                  Example: AD7156_CDC_RANGE_2_PF   - 2pF input range.
  *                           AD7156_CDC_RANGE_0_5_PF - 0.5pF input range.
@@ -191,45 +251,60 @@ void AD7156_ChannelState(unsigned char channel, unsigned char enableConv)
  *
  * @return None.
 *******************************************************************************/
-void AD7156_SetRange(unsigned channel, unsigned char range)
+void AD7156_SetRange(ad7156_dev *dev,
+		     unsigned channel,
+		     unsigned char range)
 {
     unsigned char oldSetupReg = 0;
     unsigned char newSetupReg = 0;
     unsigned char regAddress  = 0;
 
     regAddress = (channel == 1) ? AD7156_REG_CH1_SETUP : AD7156_REG_CH2_SETUP;
-    AD7156_GetRegisterValue(&oldSetupReg, regAddress, 1);
+    AD7156_GetRegisterValue(dev,
+			    &oldSetupReg,
+			    regAddress,
+			    1);
     oldSetupReg &= ~AD7156_CH1_SETUP_RANGE(0x3);
     newSetupReg = oldSetupReg | AD7156_CH1_SETUP_RANGE(range);
-    AD7156_SetRegisterValue(newSetupReg, regAddress, 1);
+    AD7156_SetRegisterValue(dev,
+			    newSetupReg,
+			    regAddress,
+			    1);
     /* Update global variables that hold range information. */
     if(channel == 1)
     {
-        ad7156Channel1Range = AD7156_GetRange(channel);
+        ad7156Channel1Range = AD7156_GetRange(dev,
+					      channel);
     }
     else
     {
-        ad7156Channel2Range = AD7156_GetRange(channel);
+        ad7156Channel2Range = AD7156_GetRange(dev,
+					      channel);
     }
 }
 
 /***************************************************************************//**
  * @brief Reads the range bits from the device and returns the range in pF.
  *
+ * @param dev     - The device structure.
  * @param channel - Channel option.
  *                  Example: AD7156_CHANNEL1
- *                           AD7156_CHANNEL2 
+ *                           AD7156_CHANNEL2
  *
  * @return The capacitive input range(pF).
 *******************************************************************************/
-float AD7156_GetRange(unsigned channel)
+float AD7156_GetRange(ad7156_dev *dev,
+		      unsigned channel)
 {
     unsigned char setupReg    = 0;
     unsigned char regAddress  = 0;
     float range = 0;
-    
+
     regAddress = (channel == 1) ? AD7156_REG_CH1_SETUP : AD7156_REG_CH2_SETUP;
-    AD7156_GetRegisterValue(&setupReg, regAddress, 1);
+    AD7156_GetRegisterValue(dev,
+			    &setupReg,
+			    regAddress,
+			    1);
     setupReg = (setupReg & AD7156_CH1_SETUP_RANGE(0x3)) >> 6;
     switch(setupReg)
     {
@@ -262,6 +337,7 @@ float AD7156_GetRange(unsigned channel)
 /***************************************************************************//**
  * @brief Selects the threshold mode of operation.
  *
+ * @param dev      - The device structure.
  * @param thrMode  - Output comparator mode.
  *                   Example: AD7156_THR_MODE_NEGATIVE
  *                            AD7156_THR_MODE_POSITIVE
@@ -273,22 +349,31 @@ float AD7156_GetRange(unsigned channel)
  *
  * @return None.
 *******************************************************************************/
-void AD7156_SetThresholdMode(unsigned char thrMode, unsigned char thrFixed)
+void AD7156_SetThresholdMode(ad7156_dev *dev,
+			     unsigned char thrMode,
+			     unsigned char thrFixed)
 {
     unsigned char oldConfigReg = 0;
     unsigned char newConfigReg = 0;
 
-    AD7156_GetRegisterValue(&oldConfigReg, AD7156_REG_CONFIG, 1);
+    AD7156_GetRegisterValue(dev,
+			    &oldConfigReg,
+			    AD7156_REG_CONFIG,
+			    1);
     oldConfigReg &= ~(AD7156_CONFIG_THR_FIXED | AD7156_CONFIG_THR_MD(0x3));
     newConfigReg = oldConfigReg |
                    (AD7156_CONFIG_THR_FIXED * thrFixed) |
                    (AD7156_CONFIG_THR_MD(thrMode));
-    AD7156_SetRegisterValue(newConfigReg, AD7156_REG_CONFIG, 1);
+    AD7156_SetRegisterValue(dev,
+			    newConfigReg,
+			    AD7156_REG_CONFIG,
+			    1);
 }
 
 /***************************************************************************//**
  * @brief Writes to the threshold register when threshold fixed mode is enabled.
  *
+ * @param dev     - The device structure.
  * @param channel - Channel option.
  *                  Example: AD7156_CHANNEL1
  *                           AD7156_CHANNEL2
@@ -297,7 +382,9 @@ void AD7156_SetThresholdMode(unsigned char thrMode, unsigned char thrFixed)
  *
  * @return None.
 *******************************************************************************/
-void AD7156_SetThreshold(unsigned char channel, float pFthr)
+void AD7156_SetThreshold(ad7156_dev *dev,
+			 unsigned char channel,
+			 float pFthr)
 {
     unsigned char  thrRegAddress  = 0;
     unsigned short rawThr         = 0;
@@ -305,7 +392,8 @@ void AD7156_SetThreshold(unsigned char channel, float pFthr)
 
     thrRegAddress = (channel == 1) ? AD7156_REG_CH1_SENS_THRSH_H :
                                      AD7156_REG_CH2_SENS_THRSH_H;
-    range = AD7156_GetRange(channel);
+    range = AD7156_GetRange(dev,
+			    channel);
     rawThr = (unsigned short)((pFthr * 0xA000 / range) + 0x3000);
     if(rawThr > 0xD000)
     {
@@ -315,13 +403,17 @@ void AD7156_SetThreshold(unsigned char channel, float pFthr)
     {
         rawThr = 0x3000;
     }
-    AD7156_SetRegisterValue(rawThr, thrRegAddress, 2);
+    AD7156_SetRegisterValue(dev,
+			    rawThr,
+			    thrRegAddress,
+			    2);
 }
 
 /***************************************************************************//**
- * @brief Writes a value(pF) to the sensitivity register. This functions 
+ * @brief Writes a value(pF) to the sensitivity register. This functions
  * should be used when adaptive threshold mode is selected.
  *
+ * @param dev           - The device structure.
  * @param channel       - Channel option.
  *                        Example: AD7156_CHANNEL1
  *                                 AD7156_CHANNEL2
@@ -329,7 +421,9 @@ void AD7156_SetThreshold(unsigned char channel, float pFthr)
  *
  * @return None.
 *******************************************************************************/
-void AD7156_SetSensitivity(unsigned char channel, float pFsensitivity)
+void AD7156_SetSensitivity(ad7156_dev *dev,
+			   unsigned char channel,
+			   float pFsensitivity)
 {
     unsigned char  sensitivityRegAddr = 0;
     unsigned short rawSensitivity     = 0;
@@ -340,18 +434,23 @@ void AD7156_SetSensitivity(unsigned char channel, float pFsensitivity)
     range = (channel == 1) ? ad7156Channel1Range : ad7156Channel2Range;
     rawSensitivity = (unsigned short)(pFsensitivity * 0xA00 / range);
     rawSensitivity = (rawSensitivity << 4) & 0x0FF0;
-    AD7156_SetRegisterValue(rawSensitivity, sensitivityRegAddr, 2);
+    AD7156_SetRegisterValue(dev,
+			    rawSensitivity,
+			    sensitivityRegAddr,
+			    2);
 }
 
 /***************************************************************************//**
  * @brief Reads a 12-bit sample from the selected channel.
  *
+ * @param dev     - The device structure.
  * @param channel - Channel option.
  *                  Example: AD7156_CHANNEL1
  *                           AD7156_CHANNEL2
  * @return Conversion result form the selected channel.
 *******************************************************************************/
-unsigned short AD7156_ReadChannelData(unsigned char channel)
+unsigned short AD7156_ReadChannelData(ad7156_dev *dev,
+				      unsigned char channel)
 {
     unsigned short chResult   = 0;
     unsigned char  regData[2] = {0, 0};
@@ -365,7 +464,10 @@ unsigned short AD7156_ReadChannelData(unsigned char channel)
     {
         chAddress = AD7156_REG_CH2_DATA_H;
     }
-    AD7156_GetRegisterValue(regData, chAddress, 2);
+    AD7156_GetRegisterValue(dev,
+			    regData,
+			    chAddress,
+			    2);
     chResult = (regData[0] << 8) + regData[1];
 
     return chResult;
@@ -375,12 +477,14 @@ unsigned short AD7156_ReadChannelData(unsigned char channel)
  * @brief Waits for a finished CDC conversion and reads a 12-bit sample from
  *        the selected channel.
  *
+ * @param dev     - The device structure.
  * @param channel - Channel option.
  *                  Example: AD7156_CHANNEL1
  *                           AD7156_CHANNEL2
  * @return Conversion result form the selected channel.
 *******************************************************************************/
-unsigned short AD7156_WaitReadChannelData(unsigned char channel)
+unsigned short AD7156_WaitReadChannelData(ad7156_dev *dev,
+					  unsigned char channel)
 {
     unsigned short chResult   = 0;
     unsigned char  regData[2] = {0, 0};
@@ -400,9 +504,15 @@ unsigned short AD7156_WaitReadChannelData(unsigned char channel)
     }
     do
     {
-        AD7156_GetRegisterValue(&status, AD7156_REG_STATUS, 1);
+        AD7156_GetRegisterValue(dev,
+				&status,
+				AD7156_REG_STATUS,
+				1);
     }while((status & chRdyMask) != 0);
-    AD7156_GetRegisterValue(regData, chAddress, 2);
+    AD7156_GetRegisterValue(dev,
+			    regData,
+			    chAddress,
+			    2);
     chResult = (regData[0] << 8) + regData[1];
 
     return chResult;
@@ -412,19 +522,22 @@ unsigned short AD7156_WaitReadChannelData(unsigned char channel)
  * @brief Reads a sample the selected channel and converts the data to
  *        picofarads(pF).
  *
+ * @param dev     - The device structure.
  * @param channel - Channel option.
  *                  Example: AD7156_CHANNEL1
  *                           AD7156_CHANNEL2
  * @return Conversion result form the selected channel as picofarads(pF).
 *******************************************************************************/
-float AD7156_ReadChannelCapacitance(unsigned char channel)
+float AD7156_ReadChannelCapacitance(ad7156_dev *dev,
+				    unsigned char channel)
 {
     unsigned short rawCh = 0;
     float chRange = 0;
     float pFdata = 0;
 
     chRange = (channel == 1) ? ad7156Channel1Range : ad7156Channel2Range;
-    rawCh = AD7156_ReadChannelData(channel);
+    rawCh = AD7156_ReadChannelData(dev,
+				   channel);
     if(rawCh < 0x3000)
     {
         rawCh= 0x3000;
@@ -442,19 +555,22 @@ float AD7156_ReadChannelCapacitance(unsigned char channel)
  * @brief Waits for a finished CDC conversion the selected channel, reads a
  *        sample and converts the data to picofarads(pF).
  *
+ * @param dev     - The device structure.
  * @param channel - Channel option.
  *                  Example: AD7156_CHANNEL1
  *                           AD7156_CHANNEL2
  * @return Conversion result form the selected channel as picofarads(pF).
 *******************************************************************************/
-float AD7156_WaitReadChannelCapacitance(unsigned char channel)
+float AD7156_WaitReadChannelCapacitance(ad7156_dev *dev,
+					unsigned char channel)
 {
     unsigned short rawCh = 0;
     float chRange = 0;
     float pFdata = 0;
 
     chRange = (channel == 1) ? ad7156Channel1Range : ad7156Channel2Range;
-    rawCh = AD7156_WaitReadChannelData(channel);
+    rawCh = AD7156_WaitReadChannelData(dev,
+				       channel);
     if(rawCh < 0x3000)
     {
         rawCh= 0x3000;
