7728f86595a6625005f34733d164d27b1e3a23cb
Stefan Popa <stefan.popa@analog.com>
2018-01-04 17:50:07 +0200

    ad738x: No functional changes
    Ran astyle code beautifier
    
    Signed-off-by: Stefan Popa <stefan.popa@analog.com>

--- ad738x-fmcz/ad738x_fmc.c
+++ ad738x-fmcz/ad738x_fmc.c
@@ -59,21 +59,21 @@
 #define AD738X_SPI_CS                   0
 
 ad738x_init_param ad738x_default_init_param = {
-                /* SPI engine*/
-                {
-                        AD738X_SPI_ENGINE_BASEADDR, // adc_baseaddr
-                        AD738X_SPI_CS,              // chip_select
-                        SPI_ENGINE_CONFIG_CPOL,     // spi_config
-                        1000000,                    // spi_clk_hz
-                        100000000,                  // ref_clk_hz
-                        1,                          // spi_offload_rx_support_en
-                        AD738X_DMA_BASEADDR,        // spi_offload_rx_dma_baseaddr
-                        1,                          // spi_offload_tx_support_en
-                        AD738X_DMA_BASEADDR         // spi_offload_tx_dma_baseaddr
-                },
-                /* Configuration */
-				ONE_WIRE_MODE,		// conv_mode
-				INT_REF,			// ref_sel
+	/* SPI engine*/
+	{
+		AD738X_SPI_ENGINE_BASEADDR, // adc_baseaddr
+		AD738X_SPI_CS,              // chip_select
+		SPI_ENGINE_CONFIG_CPOL,     // spi_config
+		1000000,                    // spi_clk_hz
+		100000000,                  // ref_clk_hz
+		1,                          // spi_offload_rx_support_en
+		AD738X_DMA_BASEADDR,        // spi_offload_rx_dma_baseaddr
+		1,                          // spi_offload_tx_support_en
+		AD738X_DMA_BASEADDR         // spi_offload_tx_dma_baseaddr
+	},
+	/* Configuration */
+	ONE_WIRE_MODE,		// conv_mode
+	INT_REF,		// ref_sel
 };
 
 /**
@@ -83,72 +83,73 @@ ad738x_init_param ad738x_default_init_param = {
  */
 void mdelay(uint32_t msecs)
 {
-        usleep(msecs * 1000);
+	usleep(msecs * 1000);
 }
 
 #define SPI_ENGINE_OFFLOAD_EXAMPLE	0
 
 int main()
 {
-        ad738x_dev *dev;
-        uint16_t adc_data[2];
-        uint16_t *offload_data;
-    	spi_eng_msg *msg;
-    	int i;
-        uint32_t spi_eng_msg_cmds[6] = {CS_DEASSERT, TRANSFER_BYTES_W(2), CS_ASSERT,
-                                        CS_DEASSERT, TRANSFER_BYTES_R(2), CS_ASSERT};
+	ad738x_dev *dev;
+	uint16_t adc_data[2];
+	uint16_t *offload_data;
+	spi_eng_msg *msg;
+	int i;
+	uint32_t spi_eng_msg_cmds[6] = {CS_DEASSERT, TRANSFER_BYTES_W(2), CS_ASSERT,
+					CS_DEASSERT, TRANSFER_BYTES_R(2), CS_ASSERT
+				       };
 
-        Xil_ICacheEnable();
-        Xil_DCacheEnable();
+	Xil_ICacheEnable();
+	Xil_DCacheEnable();
 
-        ad738x_init(&dev, ad738x_default_init_param);
+	ad738x_init(&dev, ad738x_default_init_param);
 
-        if (SPI_ENGINE_OFFLOAD_EXAMPLE == 0) {
-        	while(1) {
-        		ad738x_spi_single_conversion(dev, adc_data);
-        		xil_printf("ADC0: %d\n", adc_data[0]);
-				if (dev->conv_mode == ONE_WIRE_MODE)
-					xil_printf("ADC1: %d\n", adc_data[1]);
-			}
-        }
-        /* Offload example */
-        else {
-    		msg = (spi_eng_msg *)malloc(sizeof(*msg));
-    		if (!msg)
-    			return -1;
+	if (SPI_ENGINE_OFFLOAD_EXAMPLE == 0) {
+		while(1) {
+			ad738x_spi_single_conversion(dev, adc_data);
+			xil_printf("ADC0: %d\n", adc_data[0]);
+			if (dev->conv_mode == ONE_WIRE_MODE)
+				xil_printf("ADC1: %d\n", adc_data[1]);
+		}
+	}
+	/* Offload example */
+	else {
+		msg = (spi_eng_msg *)malloc(sizeof(*msg));
+		if (!msg)
+			return -1;
 
-    		msg->spi_msg_cmds = malloc(sizeof(spi_eng_msg_cmds));
-    		msg->spi_msg_cmds = spi_eng_msg_cmds;
-    		msg->msg_cmd_len = ARRAY_SIZE(spi_eng_msg_cmds);
-    		msg->rx_buf_addr = 0x800000;
-    		msg->tx_buf_addr = 0xA000000;
-    	    msg->tx_buf[0] = 0x00;
+		msg->spi_msg_cmds = malloc(sizeof(spi_eng_msg_cmds));
+		msg->spi_msg_cmds = spi_eng_msg_cmds;
+		msg->msg_cmd_len = ARRAY_SIZE(spi_eng_msg_cmds);
+		msg->rx_buf_addr = 0x800000;
+		msg->tx_buf_addr = 0xA000000;
+		msg->tx_buf[0] = 0x00;
 
-    	    /* Init the rx buffer with 0s */
-    	    for (i = 0; i < ARRAY_SIZE(msg->rx_buf); i++)
-    	            msg->rx_buf[i] = 0;
+		/* Init the rx buffer with 0s */
+		for (i = 0; i < ARRAY_SIZE(msg->rx_buf); i++)
+			msg->rx_buf[i] = 0;
 
-    		spi_eng_offload_load_msg(dev->spi_desc, msg);
-    		spi_eng_transfer_multiple_msgs(dev->spi_desc, 256);
+		spi_eng_offload_load_msg(dev->spi_desc, msg);
+		spi_eng_transfer_multiple_msgs(dev->spi_desc, 256);
 
-    	    mdelay(10000);
+		mdelay(10000);
 
-    	    free(msg);
+		free(msg);
 
-    		offload_data = (uint16_t*)dev->spi_desc->rx_dma_startaddr;
-    		/* Data for ADC0 and ADC1 is interleaved */
-            for(i = 0; i < dev->spi_desc->rx_length / 2; i++) {
-            	printf("ADC%d: %d\r\n", (i % 2), *offload_data);
-        		offload_data += sizeof(uint8_t);
-            }
-        }
+		offload_data = (uint16_t*)dev->spi_desc->rx_dma_startaddr;
+		/* Data for ADC0 and ADC1 is interleaved */
+		for(i = 0; i < dev->spi_desc->rx_length / 2; i++) {
+			printf("ADC%d: %d\r\n", (i % 2), *offload_data);
+			offload_data += sizeof(uint8_t);
+		}
+	}
 
-        ad738x_remove(dev);
+	ad738x_remove(dev);
 
-        printf("Bye\n");
+	printf("Bye\n");
 
-        Xil_DCacheDisable();
-        Xil_ICacheDisable();
+	Xil_DCacheDisable();
+	Xil_ICacheDisable();
 
-        return 0;
+	return 0;
 }
