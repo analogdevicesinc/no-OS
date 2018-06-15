6dd6d76bb13405d1f17021f2870be186dbe35cf5
Dragos Bogdan <dragos.bogdan@analog.com>
2018-06-15 16:44:46 +0300

    ad9361: adc_dma_example: Allow choosing the number of channels
    Only even numbers are accepted in this version.
    For example:
    	2 for AD9364 (I,Q)
    	4 for AD9361 (I1,Q1,I2,Q2)
    	8 for FMCOMMS5
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- ad9361/scripts/adc_dma_example/xilinx/adc_dma_example.tcl
+++ ad9361/scripts/adc_dma_example/xilinx/adc_dma_example.tcl
@@ -1,30 +1,41 @@
 
+set start_addr 8388608
+
+set num_samples 16384
+
+set num_bytes_per_sample 2
+
+set num_channels [lindex $argv 0]
+if { $num_channels == "" } {
+	# Assume that AD9361 (I1,Q1,I2,Q2) is used
+	set num_channels 4
+}
+
+#Default mrd access size is word
+set mrd_size [expr {$num_samples * $num_channels * $num_bytes_per_sample / 4}]
+
 connect
 
 target 2
 
 puts "Transferring data from target to adc_dma_example.csv file..."
 
-set startAddress 8388608
-set readData [mrd $startAddress 32768]
+set read_data [mrd $start_addr $mrd_size]
 
-set fp [ open adc_dma_example.csv a ]
-for {set index 1} {$index < 65536} {incr index 4} {
-	set data [lindex $readData $index]
-	set intData [expr 0x$data]
-	
-	set sampleQ1 [expr {$intData & 0xFFFF}]
-	set sampleI1 [expr {($intData >> 16) & 0xFFFF}]
-	
-	set data [lindex $readData [expr {$index + 2}]]
-	set intData [expr 0x$data]
-	
-	set sampleQ2 [expr {$intData & 0xFFFF}]
-	set sampleI2 [expr {($intData >> 16) & 0xFFFF}]
-	
-	set line $sampleI1,$sampleQ1,$sampleI2,$sampleQ2
-	
+set fp [open adc_dma_example.csv a]
+set index 1
+while {$index < [expr {$mrd_size * 2}]} {
+	set line ""
+	for {set ch 0} {$ch < $num_channels} {incr ch 2} {
+		set data [lindex $read_data [expr {$index + $ch}]]
+		set data [expr 0x$data]
+		set sample_q [expr {$data & 0xFFFF}]
+		set sample_i [expr {($data >> 16) & 0xFFFF}]
+		append line $sample_i,$sample_q,
+	}
+	set line [string range $line 0 end-1]
 	puts $fp $line
+	incr index $num_channels
 }
 close $fp
 
