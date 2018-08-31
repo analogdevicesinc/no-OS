744d1ce9933809b6998caa3158c1d6616d455de1
Lars-Peter Clausen <lars@metafoo.de>
2018-08-31 17:11:50 +0200

    Add ad9144-fmc-ebz project
    Add a project that supports the:
    	* AD9135-FMC-EBZ
    	* AD9136-FMC-EBZ
    	* AD9144-FMC-EBZ
    
    In the default configuration the project expects a clock in the range from
    70-250 MHz to be connected to the J1 connector on the board.
    
    This clock signal will be forwarded to both the converter and the FPGA. The
    converter will be setup in a way so that the lane rate is equal to 40x the
    clock rate.
    
    The design will measure the frequency of the external clock signal and
    configure the JESD204 subsystem accordingly.
    
    The bitstream should be build from the dac_fmc_ebz HDL project.
    
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>

---
+++ ad9144-fmc-ebz/ad9144-fmc-ebz.mk
@@ -0,0 +1,29 @@
+## NOOS-DIR:
+##		is the no-OS root variable (do not override)
+##
+## M_INC_DIRS:
+##		list all your include folders (all *.h and *.c files from these folders are included
+##		in compile and passed as directives)
+##
+## M_HDR_FILES:
+##		if you want to hand pick files, use this variable to list header files.
+##
+## M_SRC_FILES:
+##		if you want to hand pick files, use this variable to list source files.
+
+## CAPTURE SCRIPT
+NR_OF_CHAN := 2
+
+M_INC_DIRS := $(NOOS-DIR)/common_drivers/platform_drivers
+M_INC_DIRS += $(NOOS-DIR)/common_drivers/dac_core
+M_INC_DIRS += $(NOOS-DIR)/common_drivers/dmac_core
+M_INC_DIRS += $(NOOS-DIR)/common_drivers/xcvr_core
+M_INC_DIRS += $(NOOS-DIR)/common_drivers/xcvr_core/xcvr_modules
+M_INC_DIRS += $(NOOS-DIR)/common_drivers/jesd_core
+M_INC_DIRS += $(NOOS-DIR)/common_drivers/dac_buffer
+M_INC_DIRS += $(NOOS-DIR)/drivers/ad9144
+M_INC_DIRS += $(NOOS-DIR)/ad9144-fmc-ebz
+
+M_HDR_FILES :=
+
+M_SRC_FILES :=
