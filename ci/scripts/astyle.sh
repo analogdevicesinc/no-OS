91209de304a3d9d804a87f5a0bdc5fe239a1e913
Antoniu Miclaus <antoniu.miclaus@analog.com>
2018-09-20 17:00:44 +0300

    general: add astyle check to travis
    Checks modified source files from pull request to determine if they are
    properly formatted.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

---
+++ ci/scripts/astyle.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+
+echo_red() { printf "\033[1;31m$*\033[m\n"; }
+
+COMMIT_RANGE=$TRAVIS_COMMIT_RANGE
+if [ -z "$TRAVIS_PULL_REQUEST_SHA" ]
+then
+	echo "Using only latest commit, since there is no Pull Request"
+	COMMIT_RANGE=HEAD~1
+fi
+
+FILES=$(git diff --name-only $COMMIT_RANGE)
+
+is_source_file() {
+	local file="$1"
+	local ext_list=".c .h"
+
+	for ext in $ext_list; do
+		[[ "${file: -2}" == "$ext" ]] && return 0
+	done;
+
+	return 1
+}
+
+for file in $FILES; do
+	if is_source_file $file
+	then 
+		astyle --options=./astyle_config $file
+	fi
+done;
+
+git diff --exit-code || {
+	echo_red "Code style issues found." 
+	exit 1
+}
