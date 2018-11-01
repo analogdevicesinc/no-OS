dffe27c4dbfee252ed1495db5d74da8c0a9adddc
Dragos Bogdan <dragos.bogdan@analog.com>
2018-11-01 18:11:15 +0000

    drivers: axi_dmac: Add ALTERA_PLATFORM support
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- projects/drivers/axi_dmac/axi_dmac.c
+++ projects/drivers/axi_dmac/axi_dmac.c
@@ -43,6 +43,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "platform_drivers.h"
+#ifdef ALTERA_PLATFORM
+#include "io.h"
+#else
+#include "xil_io.h"
+#endif
 #include "util.h"
 #include "axi_dmac.h"
 
@@ -75,7 +80,11 @@ int32_t axi_dmac_read(struct axi_dmac *dmac,
 		      uint32_t reg_addr,
 		      uint32_t *reg_data)
 {
+#ifdef ALTERA_PLATFORM
+	*reg_data = IORD_32DIRECT(dmac->base, reg_addr);
+#else
 	*reg_data = Xil_In32((dmac->base + reg_addr));
+#endif
 
 	return SUCCESS;
 }
@@ -87,7 +96,11 @@ int32_t axi_dmac_write(struct axi_dmac *dmac,
 		       uint32_t reg_addr,
 		       uint32_t reg_data)
 {
+#ifdef ALTERA_PLATFORM
+	IOWR_32DIRECT(dmac->base, reg_addr, reg_data);
+#else
 	Xil_Out32((dmac->base + reg_addr), reg_data);
+#endif
 
 	return SUCCESS;
 }
