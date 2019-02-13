55dc0b822632a9ccaac379168cedfdfe053a3949
Antoniu Miclaus <antoniu.miclaus@analog.com>
2019-02-13 14:13:59 +0200

    travis: add .cppcheckignore option
    The user can now define in the root directory of the repo a file called
    ".cppcheckignore" containing all the paths that will be ignored by the
    static analysis tool.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- ci/travis/cppcheck.sh
+++ ci/travis/cppcheck.sh
@@ -2,4 +2,6 @@
 
 set -e
 
+[[ ! -f .cppcheckignore ]] || CPPCHECK_OPTIONS="${CPPCHECK_OPTIONS} --suppressions-list=.cppcheckignore"
+	
 cppcheck --quiet --force --error-exitcode=1 $CPPCHECK_OPTIONS .
