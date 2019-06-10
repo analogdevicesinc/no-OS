analogdevicesinc/no-OS

commit e08dca5422a87ea69d36fd07fece7da9d7a91970

projects: ad9371: Add an example for using DAC DMA
Commit f9992d9bd offers an example of how to send custom data using the
DMA and adrv9009.
This patch updates the headless file in the same manner in order to work
with the ad9371 project. The main difference is that the configuration
for ad9371 uses the FIFO, while adrv9009 bypasses it. At the moment, the
DMA cyclic mode is not supported in the hdl.

Signed-off-by: Stefan Popa <stefan.popa@analog.com>

projects/ad9371/src/app/headless.c
@@ -53,6 +53,7 @@
 #include "clk_altera_a10_fpll.h"
 #include "altera_adxcvr.h"
 #else
+#include "xil_cache.h"
 #include "clk_axi_clkgen.h"
 #include "axi_adxcvr.h"
 #endif
@@ -264,6 +265,16 @@ int main(void)
 		0,
 	};
 	struct axi_dmac *rx_dmac;
+#ifdef DAC_DMA_EXAMPLE
+	struct axi_dmac_init tx_dmac_init = {
+		"tx_dmac",
+		TX_DMA_BASEADDR,
+		DMA_MEM_TO_DEV,
+		DMA_LAST,
+	};
+	struct axi_dmac *tx_dmac;
+	extern const uint32_t sine_lut_iq[1024];
+#endif
 	uint32_t i;
 
 	/* Allocating memory for the errorString */
@@ -863,6 +874,14 @@ int main(void)
 	/* Initialize the ADC core */
 	axi_adc_init(&rx_adc, &rx_adc_init);
 
+#ifdef DAC_DMA_EXAMPLE
+	axi_dac_load_custom_data(tx_dac, sine_lut_iq,
+				 ARRAY_SIZE(sine_lut_iq),
+				 DDR_MEM_BASEADDR + 0xA000000);
+	axi_dmac_init(&tx_dmac, &tx_dmac_init);
+	axi_dmac_transfer(tx_dmac, DDR_MEM_BASEADDR + 0xA000000,
+			  sizeof(sine_lut_iq) * 2);
+#endif
 	mdelay(1000);
 
 	/* Initialize the DMAC and transfer 16384 samples from ADC to MEM */
@@ -870,6 +889,9 @@ int main(void)
 	axi_dmac_transfer(rx_dmac,
 			  DDR_MEM_BASEADDR + 0x800000,
 			  16384 * 8);
+#ifndef ALTERA_PLATFORM
+	Xil_DCacheInvalidateRange(XPAR_DDR_MEM_BASEADDR + 0x800000, 16384 * 8);
+#endif
 
 	printf("Done\n");
 

commit 94f00d1f0b179ea826b39fce169bd5ec31357148

projects: ad9371: Fix MicroBlaze compilation error
Use the generic DDR_MEM_BASEADDR macro.

The issue was introduced by: https://github.com/analogdevicesinc/no-OS/commit/e08dca5422a87ea69d36fd07fece7da9d7a91970

Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

projects/ad9371/src/app/headless.c
@@ -890,7 +890,7 @@ int main(void)
 			  DDR_MEM_BASEADDR + 0x800000,
 			  16384 * 8);
 #ifndef ALTERA_PLATFORM
-	Xil_DCacheInvalidateRange(XPAR_DDR_MEM_BASEADDR + 0x800000, 16384 * 8);
+	Xil_DCacheInvalidateRange(DDR_MEM_BASEADDR + 0x800000, 16384 * 8);
 #endif
 
 	printf("Done\n");