43a162ca78a88492a16ddb862075bef7c3388133
Dragos Bogdan <dragos.bogdan@analog.com>
2019-07-11 17:38:09 +0300

    projects: ad9371: Fix SPI communication
    Extend https://github.com/analogdevicesinc/no-OS/commit/0d2fe781d389cae5d160c38db796f98eb9885cf1 .
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- projects/ad9371/src/devices/adi_hal/common.c
+++ projects/ad9371/src/devices/adi_hal/common.c
@@ -140,7 +140,7 @@ commonErr_t CMB_SPIReadByte(spiSettings_t *spiSettings, uint16_t addr,
 {
 	uint8_t buf[3];
 
-	spi_ad_desc->chip_select = spiSettings->chipSelectIndex;
+	spi_ad_desc->chip_select = spiSettings->chipSelectIndex - 1;
 
 	buf[0] = (uint8_t) ((addr >> 8) | 0x80);
 	buf[1] = (uint8_t) (addr & 0xff);
