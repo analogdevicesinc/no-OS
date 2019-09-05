8a5ab451db80f02c99060191d4b97c0503a1f850
Andrei Drimbarean <Andrei.Drimbarean@analog.com>
2019-09-05 10:48:29 +0300

    adv7511: remove deprecated sources
    No functional changes to the project at this point.
    
    Signed-off-by: Andrei Drimbarean <Andrei.Drimbarean@analog.com>

--- projects/adv7511/evaluate/zed/evaluate.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-IF "%PROCESSOR_ARCHITECTURE%"=="x86" set XMD_DIR=C:\Xilinx\14.6\ISE_DS\EDK\bin\nt
-IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" set XMD_DIR=C:\Xilinx\14.6\ISE_DS\EDK\bin\nt64
-
-%XMD_DIR%\xmd.exe -tcl evaluate.tcl
-
-pause
