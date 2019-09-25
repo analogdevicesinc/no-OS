8219624d9f949f3e58777c9b968ffaacc38b18fd
Antoniu Miclaus <antoniu.miclaus@analog.com>
2019-09-25 12:04:06 +0300

    drivers: jesd204: make driver platform agnostic
    Use `axi_io` wrapper functions to perform read/write operations.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- projects/drivers/jesd204/axi_jesd204_rx.h
+++ projects/drivers/jesd204/axi_jesd204_rx.h
@@ -63,6 +63,8 @@ struct axi_jesd204_rx {
 	struct jesd204_rx_config config;
 	uint32_t device_clk_khz;
 	uint32_t lane_clk_khz;
+	int32_t (*axi_io_read)(uint32_t, uint32_t, uint32_t*);
+	int32_t (*axi_io_write)(uint32_t, uint32_t, uint32_t);
 };
 
 struct jesd204_rx_init {
@@ -73,6 +75,8 @@ struct jesd204_rx_init {
 	uint8_t subclass;
 	uint32_t device_clk_khz;
 	uint32_t lane_clk_khz;
+	int32_t (*axi_io_read)(uint32_t, uint32_t, uint32_t*);
+	int32_t(*axi_io_write)(uint32_t, uint32_t, uint32_t);
 };
 
 /******************************************************************************/
