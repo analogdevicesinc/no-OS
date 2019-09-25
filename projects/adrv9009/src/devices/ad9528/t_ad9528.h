388998644d1ac06c2e469e95066be6b2c51a1750
Antoniu Miclaus <antoniu.miclaus@analog.com>
2019-09-25 11:24:41 +0300

    drivers: ad9528: make driver platform agnostic
    Add void pointer `extra` to AD9528 device structure, holding platform
    specific parameters.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- projects/adrv9009/src/devices/ad9528/t_ad9528.h
+++ projects/adrv9009/src/devices/ad9528/t_ad9528.h
@@ -184,6 +184,7 @@ typedef struct {
 	ad9528pll2Settings_t *pll2Settings;
 	ad9528outputSettings_t *outputSettings;
 	ad9528sysrefSettings_t *sysrefSettings;
+	void *extra;
 } ad9528Device_t;
 
 typedef enum {
