3c4cb6c0f5282b8c97dbc2ba2bc9d83d86c4cad7
Antoniu Miclaus <antoniu.miclaus@analog.com>
2019-10-30 10:42:44 +0200

    projects:drivers:jesd204: fix code style
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- projects/drivers/jesd204/xilinx_transceiver.c
+++ projects/drivers/jesd204/xilinx_transceiver.c
@@ -348,7 +348,7 @@ int32_t xilinx_xcvr_configure_lpm_dfe_mode(struct xilinx_xcvr *xcvr,
 }
 
 static void xilinx_xcvr_setup_cpll_vco_range(struct xilinx_xcvr *xcvr,
-					     uint32_t *vco_max)
+		uint32_t *vco_max)
 {
 	if  ((xcvr->type == XILINX_XCVR_TYPE_US_GTH3) |
 	     (xcvr->type == XILINX_XCVR_TYPE_US_GTH4) |
@@ -361,10 +361,10 @@ static void xilinx_xcvr_setup_cpll_vco_range(struct xilinx_xcvr *xcvr,
 }
 
 static void xilinx_xcvr_setup_qpll_vco_range(struct xilinx_xcvr *xcvr,
-					     uint32_t *vco0_min,
-					     uint32_t *vco0_max,
-					     uint32_t *vco1_min,
-					     uint32_t *vco1_max)
+		uint32_t *vco0_min,
+		uint32_t *vco0_max,
+		uint32_t *vco1_min,
+		uint32_t *vco1_max)
 {
 	switch (xcvr->type) {
 	case XILINX_XCVR_TYPE_S7_GTX2:
