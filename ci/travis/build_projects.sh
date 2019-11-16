5798a8d0b74abe280625ea15b69f4211c1084f97
AntoniuMiclaus <antoniu.miclaus@gmail.com>
2019-11-16 14:34:56 +0200

    travis: build drivers
    This patch integrates the drivers builds into TravisCI.
    
    Builds are available only on Linux OS.
    
    Platform drivers and AXI Core drivers are skipped.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- ci/travis/build_projects.sh
+++ ci/travis/build_projects.sh
@@ -4,7 +4,12 @@ set -e
 
 if [ "$TRAVIS_OS_NAME" == "linux" ] 
 then 
+	echo "Building AD9361-Linux"
 	make -C ./ad9361/sw -f Makefile.linux
+	echo "Building Drivers"
+	make -C ./drivers -f Makefile
 fi
 
+echo "Building AD9361-Generic"
 make -C ./ad9361/sw -f Makefile.generic
+
