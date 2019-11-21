d6dc4c92e29886455b756b3ef2e2523cf9fc6949
Antoniu Miclaus <antoniu.miclaus@analog.com>
2019-11-21 18:51:40 +0200

    travis: add ad9361 build jobs
    Add independent build jobs for ad9361 project (generic & linux).
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- ci/travis/run_build.sh
+++ ci/travis/run_build.sh
@@ -16,4 +16,14 @@ build_cppcheck() {
     . ./ci/travis/cppcheck.sh
 }
 
+build_ad9361_generic() {
+    sudo apt-get install libmatio-dev
+    make -C ./ad9361/sw -f Makefile.generic
+}
+
+build_ad9361_linux() {
+    sudo apt-get install libmatio-dev
+    make -C ./ad9361/sw -f Makefile.linux
+}
+
 build_${BUILD_TYPE:-default}
