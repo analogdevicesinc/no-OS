84b8006c6c98f9d42a4b9dff63cd86ec1955f492
Antoniu Miclaus <antoniu.miclaus@analog.com>
2019-11-21 18:51:03 +0200

    travis: add cppcheck build job
    Add separate build job to perform static analysis within TravisCI.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- ci/travis/cppcheck.sh
+++ ci/travis/cppcheck.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+sudo apt-get install cppcheck 
+
 [[ ! -f .cppcheckignore ]] || CPPCHECK_OPTIONS="${CPPCHECK_OPTIONS} --suppressions-list=.cppcheckignore"
 	
 cppcheck --quiet --force --error-exitcode=1 $CPPCHECK_OPTIONS .
