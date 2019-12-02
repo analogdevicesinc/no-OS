fd0be922dbad833d8e24dcf2a16073fcb1f8298a
Sergiu <sergiu.cuciurean@analog.com>
2019-12-02 11:22:48 +0200

    tools: scripts: Makefile: Add altera build
    Add altera build rules for nios2.
    
    Signed-off-by: Sergiu Cuciurean <sergiu.cuciurean@analog.com>

--- projects/adrv9009/src.mk
+++ projects/adrv9009/src.mk
@@ -37,12 +37,19 @@ SRCS := $(PROJECT)/src/app/headless.c					\
 SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c			\
 	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.c			\
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c				\
-	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c		\
-	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c			\
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c			\
-	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c			\
-	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c		\
-	$(NO-OS)/util/util.c
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c
+SRCS +=	$(NO-OS)/util/util.c
+ifeq (xilinx,$(strip $(PLATFORM)))
+SRCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c		\
+	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c			\
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c
+else
+SRCS += $(DRIVERS)/axi_core/clk_altera_a10_fpll/clk_altera_a10_fpll.c	\
+	$(DRIVERS)/axi_core/jesd204/altera_a10_atx_pll.c		\
+	$(DRIVERS)/axi_core/jesd204/altera_a10_cdr_pll.c		\
+	$(DRIVERS)/axi_core/jesd204/altera_adxcvr.c
+endif
 SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c					\
 	$(PLATFORM_DRIVERS)/spi.c					\
 	$(PLATFORM_DRIVERS)/gpio.c					\
@@ -89,11 +96,18 @@ INCS :=	$(PROJECT)/src/app/app_config.h					\
 INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h			\
 	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.h			\
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h				\
-	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h		\
-	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h			\
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h			\
-	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h			\
-	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h
+ifeq (xilinx,$(strip $(PLATFORM)))
+INCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h		\
+	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h			\
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h
+else
+INCS += $(DRIVERS)/axi_core/clk_altera_a10_fpll/clk_altera_a10_fpll.h	\
+	$(DRIVERS)/axi_core/jesd204/altera_a10_atx_pll.h		\
+	$(DRIVERS)/axi_core/jesd204/altera_a10_cdr_pll.h		\
+	$(DRIVERS)/axi_core/jesd204/altera_adxcvr.h
+endif
 INCS +=	$(PLATFORM_DRIVERS)/spi_extra.h					\
 	$(PLATFORM_DRIVERS)/gpio_extra.h
 INCS +=	$(INCLUDE)/axi_io.h						\
