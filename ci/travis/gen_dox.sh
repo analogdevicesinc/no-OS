d9e26c154e8eba3f5ce8a22f145448a9b0179070
Antoniu Miclaus <antoniu.miclaus@analog.com>
2020-01-14 18:30:26 +0200

    travis: generate .dox files
    Currently, the drivers_page.dox and projects_page.dox are populated
    manually with sections and links to different types of drivers/projects.
    
    This patch replaces the .dox files corresponding to drivers/projects and
    with gen_dox.sh script that generates these files on doxygen travis
    build.
    
    For example, when a new driver implementation is merged on master branch,
    this approach takes care of updating the doxygen files accordingly.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

---
+++ ci/travis/gen_dox.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+#Generate drivers_page.dox and projects_page.dox
+
+#Append string to specific *.dox file
+append_to_dox () {
+        echo "$1" >> ${TRAVIS_BUILD_DIR}/doc/$2
+}
+
+#.dox file generic header 
+HEADER="/**"
+
+#.dox file generic footer
+FOOTER=" */"
+
+#Generate drivers_page.dox file
+generate_drivers_page () {
+
+	> ${TRAVIS_BUILD_DIR}/doc/drivers_page.dox
+
+#Page tag and introduction for drivers
+	INTRO_DRV="
+\page drivers_page Drivers Page
+
+\tableofcontents
+
+The following sections contain code documentation for ADI no-OS drivers.
+"
+	#add header
+	append_to_dox "${HEADER}" drivers_page.dox
+
+	#add introduction
+	append_to_dox "${INTRO_DRV}" drivers_page.dox
+
+	#iterate driver types
+	for drv_type in ${TRAVIS_BUILD_DIR}/drivers/*
+	do
+		#add drivers sections
+		if [[ -d "${drv_type}" ]]
+		then
+			append_to_dox "\section $(basename -- ${drv_type}) $(basename -- ${drv_type^^})" drivers_page.dox
+			append_to_dox "" drivers_page.dox
+		fi
+		#sd-card folder does not contain any subfolder, therefore, the linking is different from other drivers
+		if [[ "${drv_type}" == *"sd-card"* ]]
+		then
+			append_to_dox "- \link_to_subdir{/drivers/sd-card \"SD Card\"}" drivers_page.dox
+		else
+			#iterate drivers per type
+			for part in ${drv_type}/*
+			do
+				if [[ -d "${part}" ]]
+				then
+					#exclude talise driver
+					if [[ ${part} != *"talise"* ]]
+					then
+						#add link to driver
+						append_to_dox "- \link_to_subdir{/drivers/$(basename -- ${drv_type})/$(basename -- ${part}) \"$(basename -- ${part^^})\"}" drivers_page.dox
+					fi
+				fi
+			done
+		fi
+		#add new line
+		append_to_dox "" drivers_page.dox
+	done
+
+	#add footer
+	append_to_dox "${FOOTER}" drivers_page.dox
+
+}
+
+#Generate projects_page.dox file
+generate_projects_page () {
+
+	> ${TRAVIS_BUILD_DIR}/doc/projects_page.dox
+
+#Page tag and introduction for projects
+	INTRO_PRJ="
+\page projects_page Projects Page
+
+\tableofcontents
+
+The following list contains code documentation for ADI no-OS projects:
+
+"
+	#dd header
+	append_to_dox "${HEADER}" projects_page.dox
+
+	#add introduction
+	append_to_dox "${INTRO_PRJ}" projects_page.dox
+
+	#iterate projects
+	for prj_type in ${TRAVIS_BUILD_DIR}/projects/*; do
+		if [[ -d "${prj_type}" ]]; then
+			#add link to project
+			append_to_dox "- \link_to_subdir{/projects/$(basename -- ${prj_type}) \"$(basename -- ${prj_type^^})\"}" projects_page.dox
+			#add new line
+			append_to_dox "" projects_page.dox
+		fi
+	done
+
+	#add footer
+	append_to_dox "${FOOTER}" projects_page.dox
+
+}
+
+generate_drivers_page
+generate_projects_page
