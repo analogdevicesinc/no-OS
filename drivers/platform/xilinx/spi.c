analogdevicesinc/no-OS

commit b0269818a2ff691f72331e34d2c00ca91ab917b2

spi driver, take max_speed_hz into account
Compute the prescaler such that the selected SPI clock will be the maximum
clock possible that is lower or equal to user defined max_speed_hz.

Signed-off-by: Darius Berghe <darius.berghe@analog.com>

drivers/platform/xilinx/spi.c
@@ -72,6 +72,11 @@ int32_t spi_init(struct spi_desc **desc,
 	struct spi_desc			*sdesc;
 	struct xil_spi_desc		*xdesc;
 	struct xil_spi_init_param	*xinit;
+	const uint32_t			input_clock = 100000000;
+	const uint32_t			prescaler_default = XSPIPS_CLK_PRESCALE_64;
+	const uint32_t			prescaler_min = XSPIPS_CLK_PRESCALE_4;
+	const uint32_t			prescaler_max = XSPIPS_CLK_PRESCALE_256;
+	uint32_t			prescaler = 0u;
 
 	sdesc = (struct spi_desc *)malloc(sizeof(*sdesc));
 	xdesc = (struct xil_spi_desc *)malloc(sizeof(*xdesc));
@@ -149,8 +154,37 @@ pl_error:
 		if(ret != SUCCESS)
 			goto ps_error;
 
-		ret = XSpiPs_SetClkPrescaler(xdesc->instance,
-					     XSPIPS_CLK_PRESCALE_64);
+		if (sdesc->max_speed_hz != 0u) {
+			uint32_t div = input_clock / sdesc->max_speed_hz;
+			uint32_t rem = input_clock % sdesc->max_speed_hz;
+
+			// find the power of two just higher than div and
+			// store the exponent in prescaler
+			while(div) {
+				prescaler += 1;
+				div >>= 1u;
+			}
+
+			// this exponent - 1 is needed because of the way
+			// xilinx stores it into registers
+			if (prescaler)
+				prescaler -= 1;
+
+			// this exponent - 1 is needed when initial div was
+			// precisely a power of two
+			if (!rem && prescaler)
+				prescaler -= 1;
+
+			if (prescaler < prescaler_min)
+				prescaler = prescaler_min;
+
+			if (prescaler > prescaler_max)
+				prescaler = prescaler_max;
+		} else
+			prescaler = prescaler_default;
+
+		ret = XSpiPs_SetClkPrescaler(xdesc->instance, prescaler);
+
 		if(ret != SUCCESS)
 			goto ps_error;
 

commit 475c6fcbf0f186e89390bcda1ca673a47cec4f10

drivers: xilinx: spi: add check for XSpiPs
Add check for XSpiPs component implementation when initializing the
prescaler min, max, default values.

The macros `XSPIPS_CLK_PRESCALE_64`, `XSPIPS_CLK_PRESCALE_64`,
`XSPIPS_CLK_PRESCALE_64` are not defined when XSpi component
implementation is used (Microblaze platform).

Fixes: b0269818a2ff691f72331e34d2c00ca91ab917b2

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/platform/xilinx/spi.c
@@ -73,9 +73,11 @@ int32_t spi_init(struct spi_desc **desc,
 	struct xil_spi_desc		*xdesc;
 	struct xil_spi_init_param	*xinit;
 	const uint32_t			input_clock = 100000000;
+#ifdef XSPIPS_H
 	const uint32_t			prescaler_default = XSPIPS_CLK_PRESCALE_64;
 	const uint32_t			prescaler_min = XSPIPS_CLK_PRESCALE_4;
 	const uint32_t			prescaler_max = XSPIPS_CLK_PRESCALE_256;
+#endif
 	uint32_t			prescaler = 0u;
 
 	sdesc = (struct spi_desc *)malloc(sizeof(*sdesc));