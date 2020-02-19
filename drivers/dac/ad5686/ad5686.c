analogdevicesinc/no-OS

commit d0e387b0adc3dc795d21a15cf0e08c823d07e04e

AD5686: Added I2C Readback
Added a simple 2-byte readback, using the same approach taken
for the SPI readback (2 bytes, single-channel)

Signed-off-by: Sean Smith <Sean.Smith@analog.com>

drivers/dac/ad5686/ad5686.c
@@ -501,12 +501,17 @@ uint16_t ad5686_read_back_register(struct ad5686_dev *dev,
 	uint16_t offset = MAX_RESOLUTION - \
 			  chip_info[dev->act_device].resolution;
 	uint8_t address = chip_info[dev->act_device].channel_addr[channel];
+	uint8_t rb_data_i2c[2] = { 0 };
 
 	if(chip_info[dev->act_device].communication == SPI) {
 		ad5686_set_shift_reg(dev, AD5686_CTRL_RB_REG, address, 0);
 		read_back_data = ad5686_set_shift_reg(dev, AD5686_CTRL_NOP, 0,
 						      0);
 		read_back_data >>= offset;
+	} else {
+		i2c_write(dev->i2c_desc, &address, 1, NO_STOP_BIT);
+		i2c_read(dev->i2c_desc, rb_data_i2c, 2, STOP_BIT);
+		read_back_data = (rb_data_i2c[1] << 8) | rb_data_i2c[0];
 	}
 
 	return read_back_data;

commit 5fcc2e09a9d07c96892566623cf9ebcd4cb729e2

drivers: dac: ad5686: Fix i2c register readback
This patch adds the readback command at the start of the register read
procedure. This command is necessary to enable the register readback
function of the chip.

Fixes: d0e387b0adc3 ("AD5686: Added I2C Readback")
Signed-off-by: Mircea Caprioru <mircea.caprioru@analog.com>

drivers/dac/ad5686/ad5686.c
@@ -514,7 +514,7 @@ uint16_t ad5686_read_back_register(struct ad5686_dev *dev,
 	uint16_t offset = MAX_RESOLUTION - \
 			  chip_info[dev->act_device].resolution;
 	uint8_t address = chip_info[dev->act_device].channel_addr[channel];
-	uint8_t rb_data_i2c[2] = { 0 };
+	uint8_t rb_data_i2c[3] = { 0 };
 
 	if(chip_info[dev->act_device].communication == SPI) {
 		ad5686_set_shift_reg(dev, AD5686_CTRL_RB_REG, address, 0);
@@ -522,9 +522,16 @@ uint16_t ad5686_read_back_register(struct ad5686_dev *dev,
 						      0);
 		read_back_data >>= offset;
 	} else {
-		i2c_write(dev->i2c_desc, &address, 1, 0);
+		if (chip_info[dev->act_device].register_map == AD5683_REG_MAP)
+			rb_data_i2c[0] = (AD5683_CTRL_RB_REG << CMD_OFFSET) |
+					 address;
+		else
+			rb_data_i2c[0] = (AD5686_CTRL_RB_REG << CMD_OFFSET) |
+					 address;
+
+		i2c_write(dev->i2c_desc, rb_data_i2c, 3, 0);
 		i2c_read(dev->i2c_desc, rb_data_i2c, 2, 1);
-		read_back_data = (rb_data_i2c[1] << 8) | rb_data_i2c[0];
+		read_back_data = (rb_data_i2c[0] << 8) | rb_data_i2c[1];
 	}
 
 	return read_back_data;