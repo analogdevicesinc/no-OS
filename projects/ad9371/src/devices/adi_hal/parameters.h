analogdevicesinc/no-OS

commit c1085e5ca8b9f8a04674c321174ede66d0423e4a

ad9371: Observation path initialization.
Initialize DMA for observation path, this will allow data capture on
sniffer channels.

Signed-off-by: Cristian Pop <cristian.pop@analog.com>

projects/ad9371/src/devices/adi_hal/parameters.h
@@ -141,6 +141,7 @@
 #define RX_OS_CORE_BASEADDR		XPAR_RX_OS_AD9371_TPL_CORE_TPL_CORE_BASEADDR
 #endif
 
+#define RX_OBS_DMA_BASEADDR		XPAR_AXI_AD9371_RX_OS_DMA_BASEADDR
 #define RX_DMA_BASEADDR			XPAR_AXI_AD9371_RX_DMA_BASEADDR
 #define TX_DMA_BASEADDR			XPAR_AXI_AD9371_TX_DMA_BASEADDR
 #endif

projects/ad9371/src/app/headless.c
@@ -295,6 +295,14 @@ int main(void)
 		4
 	};
 	struct axi_adc *rx_adc;
+
+	struct axi_adc_init rx_obs_adc_init = {
+		"rx_obs_adc",
+		RX_OS_CORE_BASEADDR,
+		2
+	};
+	struct axi_adc *rx_obs_adc;
+
 	struct axi_dmac_init rx_dmac_init = {
 		"rx_dmac",
 		RX_DMA_BASEADDR,
@@ -302,6 +310,15 @@ int main(void)
 		0
 	};
 	struct axi_dmac *rx_dmac;
+
+	struct axi_dmac_init rx_obs_dmac_init = {
+		"rx_obs_dmac",
+		RX_OBS_DMA_BASEADDR,
+		DMA_DEV_TO_MEM,
+		0
+	};
+	struct axi_dmac *rx_obs_dmac;
+
 #ifdef DAC_DMA_EXAMPLE
 	struct axi_dmac_init tx_dmac_init = {
 		"tx_dmac",
@@ -911,6 +928,14 @@ int main(void)
 	/* Initialize the ADC core */
 	axi_adc_init(&rx_adc, &rx_adc_init);
 
+	if ((mykError = MYKONOS_setObsRxPathSource(&mykDevice,
+			OBS_RX1_SNIFFERLO)) != MYKONOS_ERR_OK) {
+		errorString = getMykonosErrorMessage(mykError);
+		goto error_11;
+	}
+
+	axi_adc_init(&rx_obs_adc, &rx_obs_adc_init);
+
 #ifdef DAC_DMA_EXAMPLE
 	axi_dac_load_custom_data(tx_dac, sine_lut_iq,
 				 ARRAY_SIZE(sine_lut_iq),
@@ -926,6 +951,7 @@ int main(void)
 
 	/* Initialize the DMAC and transfer 16384 samples from ADC to MEM */
 	axi_dmac_init(&rx_dmac, &rx_dmac_init);
+	axi_dmac_init(&rx_obs_dmac, &rx_obs_dmac_init);
 
 #ifdef IIO_EXAMPLE
 

commit 33f3215330a869a9477e7e49dda4f6b5cc0731aa

projects: ad9371: parameters: add rx_obs_dma
Add `RX_OBS_DMA_BASEADDR` define for Altera Platforms.

Fixes: c1085e5ca8b9f8a04674c321174ede66d0423e4a

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

projects/ad9371/src/devices/adi_hal/parameters.h
@@ -93,6 +93,7 @@
 #define TX_CORE_BASEADDR		AXI_AD9371_BASE + 0x4000
 #define RX_OS_CORE_BASEADDR		AXI_AD9371_BASE + 0x8000
 
+#define RX_OBS_DMA_BASEADDR		AXI_AD9371_RX_OS_DMA_BASE
 #define RX_DMA_BASEADDR			AXI_AD9371_RX_DMA_BASE
 #define TX_DMA_BASEADDR			AXI_AD9371_TX_DMA_BASE
 #else