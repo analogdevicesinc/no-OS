6c7f8d851a192b97fe2d51bac3b0816c33538215
Dragos Bogdan <dragos.bogdan@analog.com>
2020-03-25 00:07:46 +0200

    tools: scripts: xilinx: Remove Vivado dependency
    Since Xilinx SDK can be installed standalone too, the Vivado dependency
    should be removed.
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- tools/scripts/platform/xilinx/environment.sh
+++ tools/scripts/platform/xilinx/environment.sh
@@ -3,7 +3,7 @@
 if [ "$#" -eq 2 ]
 then
 export PATH="$PATH\
-:$1/Vivado/$2/bin\
+:$1/SDK/$2/bin\
 :$1/SDK/$2/gnu/microblaze/lin/bin\
 :$1/SDK/$2/gnu/aarch32/lin/gcc-arm-none-eabi/bin\
 :$1/SDK/$2/gnu/aarch64/lin/aarch64-none/bin"
