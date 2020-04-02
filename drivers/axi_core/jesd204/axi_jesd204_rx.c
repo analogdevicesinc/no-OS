1050496afb6acb4fd2e9acde6e4f7a422327841a
Dragos Bogdan <dragos.bogdan@analog.com>
2020-04-02 10:56:38 +0300

    drivers: axi_jesd204_[rx|tx]: Status add LMFC rate
    The LMFC rate can be useful information for selecting the SYSREF rate.
    The SYSREF rate must be SYSREF = LMFC / N, where N must be positive
    integers of 1,2,3, ...
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- drivers/axi_core/jesd204/axi_jesd204_rx.c
+++ drivers/axi_core/jesd204/axi_jesd204_rx.c
@@ -160,12 +160,15 @@ uint32_t axi_jesd204_rx_status_read(struct axi_jesd204_rx *jesd)
 	uint32_t clock_rate;
 	uint32_t link_rate;
 	uint32_t sysref_config;
+	uint32_t link_config0;
+	uint32_t lmfc_rate;
 
 	axi_jesd204_rx_read(jesd, JESD204_RX_REG_LINK_STATE, &link_disabled);
 	axi_jesd204_rx_read(jesd, JESD204_RX_REG_LINK_STATUS, &link_status);
 	axi_jesd204_rx_read(jesd, JESD204_RX_REG_SYSREF_STATUS, &sysref_status);
 	axi_jesd204_rx_read(jesd, JESD204_RX_REG_LINK_CLK_RATIO, &clock_ratio);
 	axi_jesd204_rx_read(jesd, JESD204_RX_REG_SYSREF_CONF, &sysref_config);
+	axi_jesd204_rx_read(jesd, JESD204_RX_REG_LINK_CONF0, &link_config0);
 
 	printf("%s status:\n", jesd->name);
 
@@ -187,10 +190,13 @@ uint32_t axi_jesd204_rx_status_read(struct axi_jesd204_rx *jesd)
 	if (!link_disabled) {
 		clock_rate = jesd->lane_clk_khz;
 		link_rate = DIV_ROUND_CLOSEST(clock_rate, 40);
+		lmfc_rate = clock_rate / (10 * ((link_config0 & 0xFF) + 1));
 		printf("\tLane rate: %"PRIu32".%.3"PRIu32" MHz\n"
-		       "\tLane rate / 40: %"PRIu32".%.3"PRIu32" MHz\n",
+		       "\tLane rate / 40: %"PRIu32".%.3"PRIu32" MHz\n"
+		       "\tLMFC rate: %"PRIu32".%.3"PRIu32" MHz\n",
 		       clock_rate / 1000, clock_rate % 1000,
-		       link_rate / 1000, link_rate % 1000);
+		       link_rate / 1000, link_rate % 1000,
+		       lmfc_rate / 1000, lmfc_rate % 1000);
 
 		printf("\tLink status: %s\n"
 		       "\tSYSREF captured: %s\n"
