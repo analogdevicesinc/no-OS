ec4003e3a5c5ab2308214ab10e56ee0a15cf246d
Antoniu Miclaus <antoniu.miclaus@analog.com>
2020-04-07 18:38:38 +0300

    projects: fmcjesdadc1 initial version
    Update fmcjesdadc1 project to use latest platform drivers and AXI core
    implementations
    
    Rename `config.h` file to `app_config.h`
    
    Use `parameters.h` file for macro definition.
    
    Folder structures is kept similar to the updated projects.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

---
+++ projects/fmcjesdadc1/src/devices/adi_hal/parameters.h
@@ -0,0 +1,78 @@
+/***************************************************************************//**
+ *   @file   parameters.h
+ *   @brief  Platform dependent parameters.
+ *   @author Antoniu Miclaus (antoniu.miclaus@analog.com)
+********************************************************************************
+ * Copyright 2020(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#ifndef _PARAMETERS_H_
+#define _PARAMETERS_H_
+
+#include "app_config.h"
+#include "xparameters.h"
+
+#ifdef PLATFORM_MB
+#define SPI_DEVICE_ID				XPAR_SPI_0_DEVICE_ID
+#define GPIO_DEVICE_ID				XPAR_GPIO_0_DEVICE_ID
+
+#define GPIO_OFFSET				0
+
+#define ADC_0_DDR_BASEADDR			(XPAR_AXI_DDR_CNTRL_BASEADDR + 0x800000)
+#define ADC_1_DDR_BASEADDR			(XPAR_AXI_DDR_CNTRL_BASEADDR + 0x900000)
+#else
+#define SPI_DEVICE_ID				XPAR_XSPIPS_0_DEVICE_ID
+#define GPIO_DEVICE_ID				XPAR_XGPIOPS_0_DEVICE_ID
+
+#ifdef PLATFORM_ZYNQ
+#define GPIO_OFFSET				54
+#endif
+
+#define ADC_0_DDR_BASEADDR			(XPAR_DDR_MEM_BASEADDR + 0x800000)
+#define ADC_1_DDR_BASEADDR			(XPAR_DDR_MEM_BASEADDR + 0x900000)
+#endif
+
+#define RX_0_CORE_BASEADDR			XPAR_AXI_AD9250_0_CORE_BASEADDR
+#define RX_1_CORE_BASEADDR			XPAR_AXI_AD9250_1_CORE_BASEADDR
+
+#define RX_DMA_0_BASEADDR			XPAR_AXI_AD9250_0_DMA_BASEADDR
+#define RX_DMA_1_BASEADDR			XPAR_AXI_AD9250_1_DMA_BASEADDR
+
+#define RX_JESD_BASEADDR			XPAR_AXI_AD9250_JESD_RX_AXI_BASEADDR
+
+#define RX_XCVR_BASEADDR			XPAR_AXI_AD9250_XCVR_BASEADDR
+
+#define GPIO_JESD204_SYSREF			(GPIO_OFFSET + 32)
+
+#endif /* _PARAMETERS_H_ */
