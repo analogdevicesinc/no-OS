596d869179d9a942ac4d872b9d02697f1556441d
Dragos Bogdan <dragos.bogdan@analog.com>
2020-04-09 11:16:18 +0300

    projects: Add AD9081 project
    AD9081-FMCA-EBZ (Single MxFE) + VCU118 setup is supported.
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

---
+++ projects/ad9081/src.mk
@@ -0,0 +1,97 @@
+################################################################################
+#									       #
+#     Shared variables:							       #
+#	- PROJECT							       #
+#	- DRIVERS							       #
+#	- INCLUDE							       #
+#	- PLATFORM_DRIVERS						       #
+#	- NO-OS								       #
+#									       #
+################################################################################
+
+SRCS := $(PROJECT)/src/app.c						\
+	$(PROJECT)/src/app_clock.c					\
+	$(PROJECT)/src/app_jesd.c					\
+	$(DRIVERS)/adc/ad9081/ad9081.c					\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_adc.c			\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_dac.c			\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_device.c			\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_hal.c			\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_jesd.c			\
+	$(DRIVERS)/frequency/hmc7044/hmc7044.c				\
+	$(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c			\
+	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.c			\
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c				\
+	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c			\
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c			\
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c			\
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.c			\
+	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c		\
+	$(PLATFORM_DRIVERS)/axi_io.c					\
+	$(PLATFORM_DRIVERS)/delay.c					\
+	$(PLATFORM_DRIVERS)/gpio.c					\
+	$(PLATFORM_DRIVERS)/spi.c					\
+	$(NO-OS)/util/clk.c						\
+	$(NO-OS)/util/util.c
+ifeq (y,$(strip $(TINYIIOD)))
+SRCS += $(PROJECT)/src/app_iio.c					\
+	$(PLATFORM_DRIVERS)/uart.c					\
+	$(NO-OS)/util/xml.c						\
+	$(NO-OS)/iio/iio.c						\
+	$(NO-OS)/iio/iio_app/iio_app.c					\
+	$(NO-OS)/iio/iio_axi_adc/iio_axi_adc.c				\
+	$(NO-OS)/iio/iio_axi_dac/iio_axi_dac.c
+endif
+INCS :=	$(PROJECT)/src/app_clock.h					\
+	$(PROJECT)/src/app_jesd.h					\
+	$(PROJECT)/src/app_parameters.h					\
+	$(DRIVERS)/adc/ad9081/ad9081.h					\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_ad9081.h		\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_impala_tc.h		\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_jrxa_des.h		\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_jtx_dual_link.h		\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_jtx_qbf_ad9081.h	\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_lcpll_28nm.h		\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_main.h			\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_nb_coarse_nco.h		\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_nb_ddc_dformat.h	\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_nb_fine_nco.h		\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_rx_paging.h		\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_ser_phy.h		\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_bf_spi_only_up.h		\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_config.h			\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081.h				\
+	$(DRIVERS)/adc/ad9081/api/adi_ad9081_hal.h			\
+	$(DRIVERS)/adc/ad9081/api/adi_cms_api_common.h			\
+	$(DRIVERS)/adc/ad9081/api/adi_cms_api_config.h			\
+	$(DRIVERS)/frequency/hmc7044/hmc7044.h				\
+	$(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h			\
+	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.h			\
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h				\
+	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h			\
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h			\
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h			\
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.h			\
+	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h		\
+	$(PLATFORM_DRIVERS)/gpio_extra.h				\
+	$(PLATFORM_DRIVERS)/spi_extra.h					\
+	$(INCLUDE)/axi_io.h						\
+	$(INCLUDE)/clk.h						\
+	$(INCLUDE)/delay.h						\
+	$(INCLUDE)/error.h						\
+	$(INCLUDE)/gpio.h						\
+	$(INCLUDE)/spi.h						\
+	$(INCLUDE)/util.h
+ifeq (y,$(strip $(TINYIIOD)))
+INCS += $(PROJECT)/src/app_iio.h					\
+	$(INCLUDE)/uart.h						\
+	$(PLATFORM_DRIVERS)/uart_extra.h				\
+	$(INCLUDE)/xml.h						\
+	$(NO-OS)/iio/iio.h						\
+	$(NO-OS)/iio/iio_types.h					\
+	$(NO-OS)/iio/iio_app/iio_app.h					\
+	$(NO-OS)/iio/iio_axi_adc/iio_axi_adc.h				\
+	$(NO-OS)/iio/iio_axi_dac/iio_axi_dac.h				\
+	$(NO-OS)/libraries/libtinyiiod/compat.h				\
+	$(NO-OS)/libraries/libtinyiiod/tinyiiod.h
+endif
---
+++ projects/ad9081/src/app_clock.c
@@ -0,0 +1,165 @@
+/***************************************************************************//**
+ *   @file   app_clock.c
+ *   @brief  Application clocks initialization.
+ *   @author DBogdan (dragos.bogdan@analog.com)
+********************************************************************************
+ * Copyright 2020(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "spi.h"
+#include "spi_extra.h"
+#include "hmc7044.h"
+#include "error.h"
+#include "app_parameters.h"
+#include "app_clock.h"
+
+/******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+const char *app_clocks_names[] = {
+	"dev_refclk",
+	"dev_sysref",
+	"core_clk_tx",
+	"core_clk_rx",
+	"fpga_refclk",
+	"fpga_sysref"
+};
+
+/******************************************************************************/
+/************************ Variables Definitions *******************************/
+/******************************************************************************/
+struct hmc7044_dev* hmc7044_dev;
+
+struct clk_hw hmc7044_hw;
+
+/******************************************************************************/
+/************************** Functions Implementation **************************/
+/******************************************************************************/
+/**
+ * @brief Application clock setup.
+ * @return SUCCESS in case of success, FAILURE otherwise.
+ */
+int32_t app_clock_init(struct clk clk[NUM_APP_CLKS])
+{
+	uint32_t i;
+	int32_t ret;
+
+	struct xil_spi_init_param xil_spi_param = {
+		.type = SPI_PL,
+		.device_id = 0,
+	};
+
+	struct spi_init_param clkchip_spi_init_param = {
+		.max_speed_hz = 10000000,
+		.mode = SPI_MODE_0,
+		.chip_select = CLK_CS,
+		.extra = &xil_spi_param
+	};
+
+	struct hmc7044_chan_spec chan_spec[8] = {
+		{
+			.num = 2,		// DEV_REFCLK
+			.divider = 12,		// 250 MHz
+			.driver_mode = 2,	// LVDS
+		}, {
+			.num = 3,		// DEV_SYSREF
+			.divider = 1536,	// 1.953125 MHz
+			.driver_mode = 2,	// LVDS
+		}, {
+			.num = 6,		// CORE_CLK_TX
+			.divider = 12,		// 250 MHz
+			.driver_mode = 2,	// LVDS
+		}, {
+			.num = 8,		// CORE_CLK_RX
+			.divider = 12,		// 250 MHz
+			.driver_mode = 2,	// LVDS
+		}, {
+			.num = 12,		// FPGA_REFCLK
+			.divider = 6,		// 500 MHz
+			.driver_mode = 2,	// LVDS
+		}, {
+			.num = 13,		// FPGA_SYSREF
+			.divider = 1536,	// 1.953125 MHz
+			.driver_mode = 2,	// LVDS
+		}
+	};
+
+	struct hmc7044_init_param hmc7044_param = {
+		.spi_init = &clkchip_spi_init_param,
+		.clkin_freq = {122880000, 30720000, 0, 0},
+		.vcxo_freq = 122880000,
+		.pll2_freq = 3000000000,
+		.pll1_loop_bw = 200,
+		.sysref_timer_div = 1024,
+		.in_buf_mode = {0x07, 0x07, 0x00, 0x00, 0x15},
+		.gpi_ctrl = {0x00, 0x00, 0x00, 0x00},
+		.gpo_ctrl = {0x37, 0x33, 0x00, 0x00},
+		.num_channels = NUM_APP_CLKS,
+		.pll1_ref_prio_ctrl = 0xe4,
+		.sync_pin_mode = 0x1,
+		.high_performance_mode_clock_dist_en = false,
+		.high_performance_mode_pll_vco_en = false,
+		.pulse_gen_mode = 0x0,
+		.channels = chan_spec
+	};
+
+	ret = hmc7044_init(&hmc7044_dev, &hmc7044_param);
+	if (ret)
+		return ret;
+
+	hmc7044_hw.dev = hmc7044_dev;
+	hmc7044_hw.dev_clk_recalc_rate = hmc7044_clk_recalc_rate;
+	hmc7044_hw.dev_clk_round_rate = hmc7044_clk_round_rate;
+	hmc7044_hw.dev_clk_set_rate = hmc7044_clk_set_rate;
+
+	for (i = 0; i < NUM_APP_CLKS; i++) {
+		clk[i].hw = &hmc7044_hw;
+		clk[i].hw_ch_num = i;
+		clk[i].name = app_clocks_names[i];
+	}
+
+	return SUCCESS;
+}
+
+/**
+ * @brief Application clocking remove.
+ * @return SUCCESS in case of success, FAILURE otherwise.
+ */
+int32_t app_clock_remove(void)
+{
+	return hmc7044_remove(hmc7044_dev);
+}
