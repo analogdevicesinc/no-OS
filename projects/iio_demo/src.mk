b2b94aee1ef111e6fa1e9652d3369aad9949a763
Dragos Bogdan <dragos.bogdan@analog.com>
2020-04-09 10:27:41 +0300

    projects: iio_demo: src.mk: Add missing files
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- projects/iio_demo/src.mk
+++ projects/iio_demo/src.mk
@@ -18,10 +18,12 @@ SRCS += $(PLATFORM_DRIVERS)/uart.c					\
 	$(NO-OS)/util/util.c						\
 	$(NO-OS)/iio/iio.c						\
 	$(NO-OS)/iio/iio_app/iio_app.c					\
-        $(NO-OS)/iio/iio_demo/iio_demo.c
+	$(NO-OS)/iio/iio_demo/iio_demo.c				\
+	$(NO-OS)/libraries/libtinyiiod/parser.c				\
+	$(NO-OS)/libraries/libtinyiiod/tinyiiod.c
 
 INCS += $(PROJECT)/src/app_config.h					\
-        $(PROJECT)/src/parameters.h
+	$(PROJECT)/src/parameters.h
 
 INCS += $(INCLUDE)/xml.h						\
 	$(INCLUDE)/fifo.h						\
@@ -34,6 +36,7 @@ INCS += $(INCLUDE)/xml.h						\
 	$(NO-OS)/iio/iio.h						\
 	$(NO-OS)/iio/iio_types.h					\
 	$(NO-OS)/iio/iio_app/iio_app.h					\
-        $(NO-OS)/iio/iio_demo/iio_demo.h				\
+	$(NO-OS)/iio/iio_demo/iio_demo.h				\
+	$(NO-OS)/libraries/libtinyiiod/compat.h				\
 	$(NO-OS)/libraries/libtinyiiod/tinyiiod.h			\
-	$(NO-OS)/libraries/libtinyiiod/compat.h
+	$(NO-OS)/libraries/libtinyiiod/tinyiiod-private.h
