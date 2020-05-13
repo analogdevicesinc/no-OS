ad490647d08a0c0a772a69fc31372aa8734b30a2
Antoniu Miclaus <antoniu.miclaus@analog.com>
2020-05-13 11:56:45 +0300

    travis: cppcheck: exclude mbedtls
    Exclude mbedtls library from static analysis check.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- ci/travis/cppcheck.sh
+++ ci/travis/cppcheck.sh
@@ -6,4 +6,4 @@ sudo apt-get install cppcheck
 
 [[ ! -f .cppcheckignore ]] || CPPCHECK_OPTIONS="${CPPCHECK_OPTIONS} --suppressions-list=.cppcheckignore"
 	
-cppcheck --quiet --force --error-exitcode=1 $CPPCHECK_OPTIONS .
+cppcheck -ilibraries/mbedtls --quiet --force --error-exitcode=1 $CPPCHECK_OPTIONS .
