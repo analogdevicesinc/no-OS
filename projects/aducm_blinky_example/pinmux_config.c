2764da8f308a8fc683577b89076981c6d730d9ba
Mihail Chindris <mihail.chindris@analog.com>
2020-05-29 17:14:44 +0300

    project: aducm_blinky_example: Add aducm sample project
    This projects uses the new added makefile for aducm.
    The functionality is just to blink the leds on the evaluation board
    with a specific pattern.
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

---
+++ projects/aducm_blinky_example/pinmux_config.c
@@ -0,0 +1,28 @@
+/*
+ **
+ ** Source file generated on May 22, 2020 at 20:59:41.	
+ **
+ ** Copyright (C) 2011-2020 Analog Devices Inc., All Rights Reserved.
+ **
+ ** This file is generated automatically based upon the options selected in 
+ ** the Pin Multiplexing configuration editor. Changes to the Pin Multiplexing
+ ** configuration should be made by changing the appropriate options rather
+ ** than editing this file.
+ **
+ ** Selected Peripherals
+ ** --------------------
+ */
+//MY AWSOME PINMUX
+#include <sys/platform.h>
+#include <stdint.h>
+
+int32_t adi_initpinmux(void);
+
+/*
+ * Initialize the Port Control MUX Registers
+ */
+int32_t adi_initpinmux(void) {
+
+    return 0;
+}
+
---
+++ projects/aducm_blinky_example/src.mk
@@ -0,0 +1,31 @@
+################################################################################
+#									       #
+#     Shared variables:							       #
+#	- PROJECT							       #
+#	- DRIVERS							       #
+#	- INCLUDE							       #
+#	- PLATFORM_DRIVERS						       #
+#	- NO-OS								       #
+#									       #
+################################################################################
+
+#In aducm projects the name of the source dir should be different from src
+#Direcory where app srcs are stored
+
+SRC_DIRS +=	$(PROJECT)/app_src		\
+		$(PLATFORM_DRIVERS)		\
+		$(INCLUDE)			\
+		$(NO-OS)/util			\
+
+#Include makefiles from each source directory if they exist
+SUB_MAKES= $(wildcard $(addsuffix /src.mk, $(SRC_DIRS)))
+include $(SUB_MAKES)
+
+DIRECORIES_WITH_MAKEFILES = $(patsubst %/src.mk, %,$(SUB_MAKES))
+REMAINING_DIRECORIES = $(filter-out $(DIRECORIES_WITH_MAKEFILES), $(SRC_DIRS))
+
+# recursive wildcard
+rwildcard = $(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+
+SRCS += $(foreach dir, $(REMAINING_DIRECORIES), $(call rwildcard, $(dir),*.c))
+INCS += $(foreach dir, $(REMAINING_DIRECORIES), $(call rwildcard, $(dir),*.h))
