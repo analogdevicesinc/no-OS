3eb5aedf11e2bee7c83a282acef13bcc9ac33605
Cristian Pop <cristian.pop@analog.com>
2020-06-17 15:14:34 +0300

    linux.mk: Add libtinyiiod to the generated sdk project
    An buildable Xilinx SDK project will be created, with iio support.
    
    Signed-off-by: Cristian Pop <cristian.pop@analog.com>

--- tools/scripts/platform/xilinx/create_project.tcl
+++ tools/scripts/platform/xilinx/create_project.tcl
@@ -1,4 +1,4 @@
-if { $argc != 3 } {
+if { $argc != 6 } {
 	puts "create_project: Invalid arguments"
 	exit
 }
@@ -11,6 +11,11 @@ sdk createbsp -name bsp -hwproject hw -proc [lindex $argv 2] -os standalone
 # Create app
 sdk createapp -name app -hwproject hw -proc [lindex $argv 2] -os standalone \
 	-lang C -app {Empty Application} -bsp bsp
+
+sdk configapp -app app library-search-path [lindex $argv 3]
+sdk configapp -app app libraries [lindex $argv 4]
+sdk configapp -app app define-compiler-symbols [lindex $argv 5]
+
 # Build the project via SDK
 clean -type all
 build -type all
