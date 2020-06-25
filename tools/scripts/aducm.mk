4957bdd1a4158f31928add289eec883503a40e77
Mihail Chindris <mihail.chindris@analog.com>
2020-06-25 12:20:35 +0300

    tools:scripts:aducm.mk Move generic part from src.mk to aducm.mk
    Move generic source finding inside aducm.mk this way it shouldn't be added to each aducm src.mk
    This will be a generic approach based on getting sources from variables like SRC_DIRS
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

--- tools/scripts/aducm.mk
+++ tools/scripts/aducm.mk
@@ -137,7 +137,34 @@ endif
 #                           MAKEFILE SOURCES                              
 #------------------------------------------------------------------------------
 
+#If src.mk exist include it
+ifneq ($(wildcard src.mk,)
 include src.mk
+endif
+
+#Complete SRC_DIRS with NOOS path
+SRC_DIRS := $(addprefix  $(NO-OS)/,$(SRC_DIRS))
+
+#Add minimal needed dirs
+SRC_DIRS +=	$(PLATFORM_DRIVERS)		\
+		$(INCLUDE)			\
+		$(NO-OS)/util	
+
+## Fill SRCS and INCS from SRC_DIRS
+
+#Include makefiles from each source directory if they exist
+SUB_MAKES= $(wildcard $(addsuffix /src.mk, $(SRC_DIRS)))
+include $(SUB_MAKES)
+
+DIRECORIES_WITH_MAKEFILES = $(patsubst %/src.mk, %,$(SUB_MAKES))
+REMAINING_DIRECORIES = $(filter-out $(DIRECORIES_WITH_MAKEFILES), $(SRC_DIRS))
+
+# recursive wildcard
+rwildcard = $(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+
+SRCS += $(foreach dir, $(REMAINING_DIRECORIES), $(call rwildcard, $(dir),*.c))
+INCS += $(foreach dir, $(REMAINING_DIRECORIES), $(call rwildcard, $(dir),*.h))
+
 
 #------------------------------------------------------------------------------
 #                     SETTING LIBRARIES IF NEEDED                              
