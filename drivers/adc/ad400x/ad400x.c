209ca6df1e60daf7a0ebac887bbbae61d2ae0814
Cristian Pop <cristian.pop@analog.com>
2020-07-14 22:57:17 +0300

    ad400x: Update project with new SPI engine
    A common SPI engine implementation is used now.
    Remove old SPI engine files.
    
    Signed-off-by: Cristian Pop <cristian.pop@analog.com>

--- drivers/adc/ad400x/ad400x.c
+++ drivers/adc/ad400x/ad400x.c
@@ -61,22 +61,12 @@ int32_t ad400x_spi_reg_read(struct ad400x_dev *dev,
 {
 	int32_t ret;
 	uint8_t buf[2];
-	uint32_t spi_clk_hz_tmp = (uint32_t) -1;
 
 	buf[0] = AD400X_READ_COMMAND;
 	buf[1] = 0xFF;
 
-	// register access runs at a lower clock rate (~2MHz)
-	if (dev->spi_desc->spi_clk_hz > dev->spi_desc->spi_clk_hz_reg_access) {
-		spi_clk_hz_tmp = dev->spi_desc->spi_clk_hz;
-		dev->spi_desc->spi_clk_hz = dev->spi_desc->spi_clk_hz_reg_access;
-	}
-
 	ret = spi_write_and_read(dev->spi_desc, buf, 2);
 	*reg_data = buf[1];
-
-	if (spi_clk_hz_tmp != (uint32_t) -1)
-		dev->spi_desc->spi_clk_hz = spi_clk_hz_tmp;
 
 	return ret;
 }
@@ -92,18 +82,11 @@ int32_t ad400x_spi_reg_write(struct ad400x_dev *dev,
 {
 	int32_t ret;
 	uint8_t buf[2];
-	uint32_t spi_clk_hz_tmp;
-
-	// register access runs at a lower clock rate (~2MHz)
-	spi_clk_hz_tmp = dev->spi_desc->spi_clk_hz;
-	dev->spi_desc->spi_clk_hz = 2000000;
 
 	buf[0] = AD400X_WRITE_COMMAND;
 	buf[1] = reg_data | AD400X_RESERVED_MSK;
 
 	ret = spi_write_and_read(dev->spi_desc, buf, 2);
-
-	dev->spi_desc->spi_clk_hz = spi_clk_hz_tmp;
 
 	return ret;
 }
@@ -120,8 +103,7 @@ int32_t ad400x_spi_single_conversion(struct ad400x_dev *dev,
 	uint32_t buf = 0;
 	int32_t ret;
 
-	spi_set_transfer_length(dev->spi_desc, dev->spi_desc->max_data_width);
-	ret = spi_write_and_read(dev->spi_desc, &buf, 4);
+	ret = spi_write_and_read(dev->spi_desc, (uint8_t *)&buf, 4);
 
 	*adc_data = buf & 0xFFFFF;
 
@@ -146,19 +128,14 @@ int32_t ad400x_init(struct ad400x_dev **device,
 	if (!dev)
 		return -1;
 
-	ret = spi_init(&dev->spi_desc, init_param.spi_init);
+	ret = spi_init(&dev->spi_desc, &init_param.spi_init);
 	if (ret < 0)
 		goto error;
 
 	dev->dev_id = init_param.dev_id;
-	dev->spi_desc->cs_delay = init_param.spi_init.cs_delay;
 
-	dev->spi_desc->max_data_width = init_param.num_bits;
-
-	spi_set_transfer_length(dev->spi_desc, 16);
 	ad400x_spi_reg_read(dev, &data);
 
-	spi_set_transfer_length(dev->spi_desc, dev->spi_desc->max_data_width);
 	data = AD400X_TURBO_MODE(init_param.turbo_mode) |
 	       AD400X_HIGH_Z_MODE(init_param.high_z_mode) |
 	       AD400X_SPAN_COMPRESSION(init_param.span_compression) |
--- projects/ad400x-fmcz/src/ad400x_fmcz.c
+++ projects/ad400x-fmcz/src/ad400x_fmcz.c
@@ -8,60 +8,61 @@
 #include "xil_printf.h"
 #include "spi_engine.h"
 #include "ad400x.h"
+#include "error.h"
+#include "delay.h"
 
 /******************************************************************************/
 /********************** Macros and Constants Definitions **********************/
 /******************************************************************************/
+#define AD400x_EVB_SAMPLE_NO			1000
 #define AD400X_DMA_BASEADDR             XPAR_AXI_AD40XX_DMA_BASEADDR
-#define AD400X_SPI_ENGINE_BASEADDR      XPAR_SPI_AD40XX_AXI_BASEADDR
+#define AD400X_SPI_ENGINE_BASEADDR      XPAR_SPI_AD40XX_AXI_REGMAP_BASEADDR
 #define AD400x_SPI_CS                   0
 
 #define SPI_ENGINE_OFFLOAD_EXAMPLE	1
 
+struct spi_engine_init_param spi_eng_init_param  = {
+	.type = SPI_ENGINE,
+	.spi_engine_baseaddr = AD400X_SPI_ENGINE_BASEADDR,
+	.cs_delay = 2,
+	.data_width = 20,
+};
+
 struct ad400x_init_param ad400x_init_param = {
-	/* SPI engine*/
-	{
-		AD400X_SPI_ENGINE_BASEADDR,	/* adc_baseaddr */
-		AD400x_SPI_CS,			/* chip_select */
-		2,				/* cs_delay */
-		0,				/* spi_config */
-		83333333,			/* spi_clk_hz */
-		2000000,			/* spi_clk_hz_reg_access */
-		166666667,			/* ref_clk_hz */
-		1,				/* spi_offload_rx_support_en */
-		AD400X_DMA_BASEADDR,		/* spi_offload_rx_dma_baseaddr */
-		1,				/* spi_offload_tx_support_en */
-		AD400X_DMA_BASEADDR		/* spi_offload_tx_dma_baseaddr */
+	.spi_init = {
+		.chip_select = AD400x_SPI_CS,
+		.max_speed_hz = 200000,
+		.mode = SPI_MODE_0,
+		.platform_ops = &spi_eng_platform_ops,
+		.extra = (void*)&spi_eng_init_param,
 	},
 	ID_AD4003, 20, /* dev_id, num_bits */
 	1,0,0,0,
 };
-
-/**
- * @brief Generate miliseconds delay.
- * @param msecs - Delay in miliseconds.
- * @return None.
- */
-void mdelay(uint32_t msecs)
-{
-	usleep(msecs * 1000);
-}
 
 int main()
 {
 	struct ad400x_dev *dev;
 	uint32_t *offload_data;
 	uint32_t adc_data;
-	spi_eng_msg *msg;
+	struct spi_engine_offload_init_param spi_engine_offload_init_param = {
+			.offload_config = (OFFLOAD_RX_EN | OFFLOAD_TX_EN),
+			.rx_dma_baseaddr = AD400X_DMA_BASEADDR,
+			.tx_dma_baseaddr = AD400X_DMA_BASEADDR
+	};
+	struct spi_engine_offload_message msg;
+
+	uint8_t commands_data[2] = {0xFF, 0xFF};
 	int32_t ret, data;
 	uint32_t i;
 
 	print("Test\n\r");
 
-	uint32_t spi_eng_msg_cmds[3] = { CS_DEASSERT,
-					 TRANSFER_BYTES_R(2),
-					 CS_ASSERT
-				       };
+	uint32_t spi_eng_msg_cmds[3] = {
+		CS_LOW,
+		READ(2),
+		CS_HIGH
+	};
 
 	Xil_ICacheEnable();
 	Xil_DCacheEnable();
@@ -78,30 +79,25 @@ int main()
 	}
 	/* Offload example */
 	else {
-		msg = (spi_eng_msg *)malloc(sizeof(*msg));
-		if (!msg)
-			return -1;
+		ret = spi_engine_offload_init(dev->spi_desc, &spi_engine_offload_init_param);
+		if (ret != SUCCESS)
+			return FAILURE;
 
-		msg->spi_msg_cmds = spi_eng_msg_cmds;
-		msg->msg_cmd_len = ARRAY_SIZE(spi_eng_msg_cmds);
-		msg->rx_buf_addr = 0x800000;
-		msg->tx_buf_addr = 0xA000000;
-		msg->tx_buf[0] = 0xFF;
-		msg->tx_buf[1] = 0xFF;
+		msg.commands = spi_eng_msg_cmds;
+		msg.no_commands = ARRAY_SIZE(spi_eng_msg_cmds);
+		msg.rx_addr = 0x800000;
+		msg.tx_addr = 0xA000000;
+		msg.commands_data = commands_data;
 
-		/* Init the rx buffer with 0s */
-		memset(msg->rx_buf, 0, 4 * ARRAY_SIZE(msg->rx_buf));
-
-		spi_eng_offload_load_msg(dev->spi_desc, msg);
-		spi_eng_transfer_multiple_msgs(dev->spi_desc, 1000);
+		ret = spi_engine_offload_transfer(dev->spi_desc, msg, AD400x_EVB_SAMPLE_NO);
+		if (ret != SUCCESS)
+			return ret;
 
 		mdelay(2000);
 
-		free(msg);
+		offload_data = (uint32_t *)msg.rx_addr;
 
-		offload_data = (uint32_t *)dev->spi_desc->rx_dma_startaddr;
-
-		for(i = 0; i < dev->spi_desc->rx_length/4; i++) {
+		for(i = 0; i < AD400x_EVB_SAMPLE_NO; i++) {
 			data = *offload_data & 0xFFFFF;
 			if (data > 524287)
 				data = data - 1048576;
@@ -109,8 +105,6 @@ int main()
 			offload_data += 1;
 		}
 	}
-
-	spi_eng_write(dev->spi_desc, SPI_ENGINE_REG_OFFLOAD_CTRL(0), 0x0000);
 
 	print("Success\n\r");
 
