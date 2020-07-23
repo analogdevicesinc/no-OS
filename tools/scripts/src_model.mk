ba41039bd63e5c770e80dfcca79ae30f9509404b
Mihail Chindris <mihail.chindris@analog.com>
2020-07-23 18:49:08 +0300

    tools:scripts: Add src_model.mk
    In this file comments will be written for how variables should be
    used in src.mk.
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

---
+++ tools/scripts/src_model.mk
@@ -0,0 +1,16 @@
+################################################################################
+#									       #
+#     Shared variables:							       #
+#	- PROJECT							       #
+#	- DRIVERS							       #
+#	- INCLUDE							       #
+#	- PLATFORM_DRIVERS						       #
+#	- NO-OS								       #
+#									       #
+################################################################################
+
+# Add to SRCS source files to be build in the project
+SRCS += $(PROJECT)/src/main.c
+
+# Add to INCS inlcude files to be build in the porject
+INCS += $(PROJECT)/src/main.h
