972519759fa25901aa3b65de9f04cedfcbf528f7
Dragos Bogdan <dragos.bogdan@analog.com>
2020-08-13 19:16:50 +0300

    ad9361: Fix verify FIR coefficients call
    https://github.com/analogdevicesinc/linux/commit/c5fa9ba93140461e64f8aafdcdcbd84218e7a598
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- ad9361/sw/ad9361.c
+++ ad9361/sw/ad9361.c
@@ -5774,6 +5774,7 @@ int32_t ad9361_load_fir_filter_coef(struct ad9361_rf_phy *phy,
 {
 	struct spi_device *spi = phy->spi;
 	uint32_t val, offs = 0, fir_conf = 0, fir_enable = 0;
+	int32_t ret;
 
 	dev_dbg(&phy->spi->dev, "%s: TAPS %"PRIu32", gain %"PRId32", dest %d",
 		__func__, ntaps, gain_dB, dest);
@@ -5831,6 +5832,8 @@ int32_t ad9361_load_fir_filter_coef(struct ad9361_rf_phy *phy,
 	fir_conf &= ~FIR_START_CLK;
 	ad9361_spi_write(spi, REG_TX_FILTER_CONF + offs, fir_conf);
 
+	ret = ad9361_verify_fir_filter_coef(phy, dest, ntaps, coef);
+
 	if (dest & FIR_IS_RX)
 		ad9361_spi_writef(phy->spi, REG_RX_ENABLE_FILTER_CTRL,
 				  RX_FIR_ENABLE_DECIMATION(~0), fir_enable);
@@ -5840,7 +5843,7 @@ int32_t ad9361_load_fir_filter_coef(struct ad9361_rf_phy *phy,
 
 	ad9361_ensm_restore_prev_state(phy);
 
-	return ad9361_verify_fir_filter_coef(phy, dest, ntaps, coef);
+	return ret;
 }
 
 /**
