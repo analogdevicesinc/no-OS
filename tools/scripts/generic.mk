fc743ac110011c8450498ce141f25e5216c5efa1
Mihail Chindris <Mihail.Chindris@analog.com>
2020-09-01 14:35:27 +0300

    tools:scripts:generic.mk Add SRC_DIRS and IGNORE_FILES
    Implement functionality and add description for new variables.
    SRC_DIRS will be used to specify a full folder of sources and
    IGNORE_FILES to remove some files  or directories from the build
    
    Signed-off-by: Mihail Chindris <Mihail.Chindris@analog.com>

--- tools/scripts/generic.mk
+++ tools/scripts/generic.mk
@@ -1,3 +1,29 @@
+#------------------------------------------------------------------------------
+#                              UTIL FUNCTIONS
+#------------------------------------------------------------------------------
+# recursive wildcard
+rwildcard = $(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+
+#------------------------------------------------------------------------------
+#                          COMMON INITIALIZATION
+#------------------------------------------------------------------------------
+
+# Get all .c and .h files from SRC_DIRS
+SRCS     += $(foreach dir, $(SRC_DIRS), $(call rwildcard, $(dir),*.c))
+INCS     += $(foreach dir, $(SRC_DIRS), $(call rwildcard, $(dir),*.h))
+
+# Recursive ignored files. If a directory is in the variable IGNORED_FILES,
+# all files from inside the directory will be ignored
+ALL_IGNORED_FILES = $(foreach dir, $(IGNORED_FILES), $(call rwildcard, $(dir),*))
+
+# Remove ignored files
+SRCS     := $(filter-out $(ALL_IGNORED_FILES),$(SRCS))
+INCS     := $(filter-out $(ALL_IGNORED_FILES),$(INCS))
+
+#------------------------------------------------------------------------------
+#                          INCLUDE SPECIFIC MAKEFILES
+#------------------------------------------------------------------------------
+
 ifeq (aducm3029,$(strip $(PLATFORM)))
 #Aducm3029 makefile
 include ../../tools/scripts/aducm.mk
