a9da18ce639aab186ca97af1389e3c3b34e4e19c
Mihail Chindris <Mihail.Chindris@analog.com>
2020-09-01 14:35:27 +0300

    tools:scripts:generic.mk Add SRC_DIRS and IGNORE_FILES
    Implement functionality and add description for new variables.
    SRC_DIRS will be used to specify a full folder of sources and
    IGNORE_FILES to remove some files  or directories from the build
    
    Signed-off-by: Mihail Chindris <Mihail.Chindris@analog.com>

--- tools/scripts/src_model.mk
+++ tools/scripts/src_model.mk
@@ -14,3 +14,10 @@ SRCS += $(PROJECT)/src/main.c
 
 # Add to INCS inlcude files to be build in the porject
 INCS += $(PROJECT)/src/main.h
+
+# Add to SRC_DIRS directories to be used in the build. All .c and .h files from
+# the directory and subdirectories will be added to the build (recursively)
+SRC_DIRS += $(PLATFORM_DRIVERS)
+
+# Add to IGNORE_FILES files to be ignored from build
+IGNORE_FILES += $(PLATFORM_DRIVERS)/irq.c
