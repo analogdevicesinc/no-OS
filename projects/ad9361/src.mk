893b4afe51f45ec3253171b2d974e24700b6b34f
Dragos Bogdan <dragos.bogdan@analog.com>
2020-09-22 23:15:29 +0300

    projects: ad9361: Remove unused source/header files
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- projects/ad9361/src.mk
+++ projects/ad9361/src.mk
@@ -17,11 +17,6 @@ SRCS += $(PROJECT)/src/ad9361_api.c					\
 SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c			\
 	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.c			\
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c				\
-	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c		\
-	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c			\
-	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c			\
-	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c			\
-	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c		\
 	$(DRIVERS)/spi/spi.c						\
 	$(DRIVERS)/gpio/gpio.c						\
 	$(NO-OS)/util/util.c
@@ -53,12 +48,7 @@ INCS += $(PROJECT)/src/ad9361.h						\
 	$(PROJECT)/src/ad9361_api.h
 INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h			\
 	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.h			\
-	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h				\
-	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h		\
-	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h			\
-	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h			\
-	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h			\
-	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h
 INCS +=	$(PLATFORM_DRIVERS)/spi_extra.h					\
 	$(PLATFORM_DRIVERS)/gpio_extra.h
 INCS +=	$(INCLUDE)/axi_io.h						\
