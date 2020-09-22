a3f85793a391487ce5309533cfcbe997a6f786d4
Andrei Drimbarean <Andrei.Drimbarean@analog.com>
2020-09-22 17:41:13 +0300

    projects:iio_ad7124: example project
    Add example project with iio_ad7124.
    
    Signed-off-by: Andrei Drimbarean <Andrei.Drimbarean@analog.com>

---
+++ projects/ad7124/src.mk
@@ -0,0 +1,19 @@
+#See No-OS/tool/scripts/src_model.mk for variable description
+
+SRC_DIRS += $(PROJECT)/src
+SRC_DIRS += $(NO-OS)/iio/iio_app
+
+# Add to SRCS source files to be build in the project
+SRCS += $(NO-OS)/drivers/adc/ad7124/ad7124.c \
+	$(NO-OS)/drivers/adc/ad7124/iio_ad7124.c
+
+# Add to INCS inlcude files to be build in the porject
+INCS += $(NO-OS)/drivers/adc/ad7124/ad7124.h \
+	$(NO-OS)/drivers/adc/ad7124/iio_ad7124.h
+
+SRC_DIRS += $(PLATFORM_DRIVERS)
+SRC_DIRS += $(NO-OS)/util
+SRC_DIRS += $(INCLUDE)
+
+TINYIIOD=y
+
---
+++ projects/ad7124/src/main.c
@@ -0,0 +1,128 @@
+/***************************************************************************//**
+ *   @file   iio_ad7124/src/main.c
+ *   @brief  Implementation of Main Function.
+ *   @author Drimbarean Andrei (andrei.drimbarean@analog.com)
+********************************************************************************
+ * Copyright 2020(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+
+#include "app_config.h"
+#include "error.h"
+#include "iio.h"
+#include "irq.h"
+#include "irq_extra.h"
+#include "uart.h"
+#include "uart_extra.h"
+#include "iio_ad7124.h"
+#include "ad7124_regs.h"
+#include "spi_extra.h"
+#include "iio_app.h"
+
+#include <sys/platform.h>
+#include "adi_initialize.h"
+#include <drivers/pwr/adi_pwr.h>
+
+#define MAX_SIZE_BASE_ADDR		1024
+
+static uint8_t in_buff[MAX_SIZE_BASE_ADDR];
+
+#define ADC_DDR_BASEADDR	((uint32_t)in_buff)
+#define NUMBER_OF_DEVICES	1
+
+/***************************************************************************//**
+ * @brief main
+*******************************************************************************/
+int main(void)
+{
+	int32_t status;
+
+	/* iio descriptor. */
+	struct iio_desc  *iio_device;
+
+	/* iio initialization structure */
+	struct iio_init_param iio_inital;
+
+	/* Initialization for UART. */
+	struct uart_init_param uart_init_par;
+
+	/* IRQ initial configuration. */
+	struct irq_init_param irq_init_param;
+
+	/* IRQ instance. */
+	struct irq_ctrl_desc *irq_desc;
+
+	status = platform_init();
+	if (IS_ERR_VALUE(status))
+		return status;
+
+	struct ad7124_dev *ad7124_device;
+	struct aducm_spi_init_param aducm_spi_ini = {
+		.continuous_mode = true,
+		.dma = false,
+		.half_duplex = false,
+		.master_mode = MASTER,
+		.spi_channel = SPI1
+	};
+	struct spi_init_param spi_initial = {
+		.chip_select = 0x00,
+		.max_speed_hz = 10000000,
+		.mode = SPI_MODE_3,
+		.extra = &aducm_spi_ini
+	};
+	struct ad7124_init_param ad7124_initial = {
+		.spi_init = &spi_initial,
+		.regs = ad7124_init_regs_default,
+		.spi_rdy_poll_cnt = 1000
+	};
+	struct iio_data_buffer iio_ad7124_read_buff = {
+		.buff = ADC_DDR_BASEADDR,
+		.size = MAX_SIZE_BASE_ADDR,
+	};
+
+	status = ad7124_setup(&ad7124_device, &ad7124_initial);
+	if (status < 0)
+		return status;
+
+	struct iio_app_device devices[] = {
+		IIO_APP_DEVICE("ad7124-8", ad7124_device, &iio_ad7124_device,
+			       &iio_ad7124_read_buff, NULL)
+	};
+
+	return iio_app_run(devices, NUMBER_OF_DEVICES);
+}
+
