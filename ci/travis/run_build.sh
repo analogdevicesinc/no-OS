e41b3ea2c258a90d0f44c49df6d88af384fb2f26
Antoniu Miclaus <antoniu.miclaus@analog.com>
2020-11-09 13:02:25 +0200

    travis: remove build jobs for ad9361 project
    With #810 the old makefiles for ad9361 project were removed.
    
    The build jobs ad9361_generic and ad9361_linux are no longer valid.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- ci/travis/run_build.sh
+++ ci/travis/run_build.sh
@@ -14,16 +14,6 @@ build_cppcheck() {
     . ./ci/travis/cppcheck.sh
 }
 
-build_ad9361_generic() {
-    sudo apt-get install libmatio-dev
-    make -C ./ad9361/sw -f Makefile.generic
-}
-
-build_ad9361_linux() {
-    sudo apt-get install libmatio-dev
-    make -C ./ad9361/sw -f Makefile.linux
-}
-
 build_drivers() {
     sudo apt-get install gcc-arm-none-eabi libnewlib-arm-none-eabi
     make -C ./drivers -f Makefile
