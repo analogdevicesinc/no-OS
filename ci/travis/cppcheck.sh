aeb26133631dd51d55dd018b19697414875cc505
Antoniu Miclaus <antoniu.miclaus@analog.com>
2020-11-10 17:01:48 +0200

    cppcheckignore: move mbedtls to the ignore file
    Remove the cppcheck option to ignore the library.
    
    Move it to the .cppcheckignore file.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- ci/travis/cppcheck.sh
+++ ci/travis/cppcheck.sh
@@ -6,4 +6,4 @@ sudo apt-get install cppcheck
 
 [[ ! -f .cppcheckignore ]] || CPPCHECK_OPTIONS="${CPPCHECK_OPTIONS} --suppressions-list=.cppcheckignore"
 	
-cppcheck -ilibraries/mbedtls --quiet --force --error-exitcode=1 $CPPCHECK_OPTIONS .
+cppcheck --quiet --force --error-exitcode=1 $CPPCHECK_OPTIONS .
