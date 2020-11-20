a185353bf31757348495cf063b645f43cd6fbd15
Mihail Chindris <mihail.chindris@analog.com>
2020-11-20 14:29:09 +0200

    tools:scripts: Update makefile
    In this commit the bigger change is to move from aducm.mk to
    generic.mk all that can be shared with other makefiles. Mainly
    the build rules and utilitary function for windows and linux
    platforms.
    Then xilinx.mk was created based on
    https://github.com/analogdevicesinc/no-OS/blob/ed2871a4bcd38c3d53812d1ac9aee5c2d92317c1/tools/scripts/linux.mk
    and
    https://github.com/analogdevicesinc/no-OS/blob/ed2871a4bcd38c3d53812d1ac9aee5c2d92317c1/tools/scripts/windows.mk
    with needed rules to compleat generic.mk with the xilinx specific
    functionality.
    
    Improvments added:
     - Same file will work for both linux and windows. New features
    will be added in just one place
     - After creating a project make will build just updated files
     - Flag -j for paralel build is supported
     - Rule clean will delete only binary files
     - Rule clean_all will delete the build folder and libraries
     - Rule project_build will build the project the SDK way
     - Rule ra: clean_all then all
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

--- tools/scripts/generic_variables.mk
+++ tools/scripts/generic_variables.mk
@@ -16,3 +16,32 @@ WORKSPACE		?= $(PROJECT)/build
 INCLUDE			?= $(NO-OS)/include
 DRIVERS 		?= $(NO-OS)/drivers
 PLATFORM_DRIVERS	?= $(NO-OS)/drivers/platform/$(PLATFORM)
+
+#------------------------------------------------------------------------------
+#                          EVALUATE PLATFORM
+#------------------------------------------------------------------------------
+
+LEGACY_BUILD ?= n
+
+ifneq '$(strip $(LEGACY_BUILD))' 'y'
+
+HARDWARE ?= $(wildcard *.hdf) $(wildcard *.sopcinfo) $(wildcard pinmux_config.c)
+
+#If platform not set get it from HARDWARE file
+ifeq '' '$(PLATFORM)'
+ifneq '' '$(findstring hdf,$(HARDWARE))'
+PLATFORM = xilinx
+else
+ifneq '' '$(findstring sopcinfo,$(HARDWARE))'
+PLATFORM = altera
+else
+ifneq '' '$(findstring pinmux_config.c,$(HARDWARE))'
+PLATFORM = aducm3029
+else
+$(error No HARDWARE found)
+endif
+endif
+endif
+endif
+
+endif
