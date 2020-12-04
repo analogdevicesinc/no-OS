d19504086b47cb1b454f4254d98362453c642136
Darius Berghe <darius.berghe@analog.com>
2020-12-04 13:03:46 +0200

    environment.sh: append paths to beggining of PATH
    This is to ensure the SDK compilers are used instead of the
    linux distribution stock compiler.
    
    Signed-off-by: Darius Berghe <darius.berghe@analog.com>

--- tools/scripts/platform/xilinx/environment.sh
+++ tools/scripts/platform/xilinx/environment.sh
@@ -2,11 +2,11 @@
 
 if [ "$#" -eq 2 ]
 then
-export PATH="$PATH\
-:$1/SDK/$2/bin\
+export PATH="$1/SDK/$2/bin\
 :$1/SDK/$2/gnu/microblaze/lin/bin\
 :$1/SDK/$2/gnu/aarch32/lin/gcc-arm-none-eabi/bin\
-:$1/SDK/$2/gnu/aarch64/lin/aarch64-none/bin"
+:$1/SDK/$2/gnu/aarch64/lin/aarch64-none/bin\
+:$PATH"
 else
 echo "Usage : source xilinx_env.sh [/path/to/xilinx/directory] [version]"
 fi
