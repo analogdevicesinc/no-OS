72c499a4676a2baffa4ea2d4f98c282f94903429
Darius Berghe <darius.berghe@analog.com>
2020-12-04 13:03:46 +0200

    environment.sh: append paths to beggining of PATH
    This is to ensure the SDK compilers are used instead of the
    linux distribution stock compiler.
    
    Signed-off-by: Darius Berghe <darius.berghe@analog.com>

--- tools/scripts/platform/altera/environment.sh
+++ tools/scripts/platform/altera/environment.sh
@@ -2,11 +2,11 @@
 
 if [ "$#" -eq 2 ]
 then
-export PATH="$PATH\
-:$1/$2/quartus/bin\
+export PATH="$1/$2/quartus/bin\
 :$1/$2/nios2eds/bin\
 :$1/$2/nios2eds/bin/gnu/H-x86_64-pc-linux-gnu/bin\
-:$1/$2/nios2eds/sdk2/bin"
+:$1/$2/nios2eds/sdk2/bin\
+:$PATH"
 else
 echo "Usage : source intel_env.sh [/path/to/intel/directory] [version]"
 fi
