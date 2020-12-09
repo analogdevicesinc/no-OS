e7573185f2e02f2ddbe4b9f2703e3f630ceaffc7
Mihail Chindris <mihail.chindris@analog.com>
2020-12-09 13:30:51 +0200

    projects:iio_demo: Update project to use iio_app
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

--- projects/iio_demo/src/main.c
+++ projects/iio_demo/src/main.c
@@ -46,16 +46,7 @@
 #include "app_config.h"
 #include "parameters.h"
 #include "error.h"
-#include "iio.h"
-#include "irq.h"
-#include "irq_extra.h"
-#include "uart.h"
-#include "uart_extra.h"
-
-#ifdef USE_TCP_SOCKET
-#include "wifi.h"
-#include "tcp_socket.h"
-#endif
+#include "iio_app.h"
 
 #ifdef XILINX_PLATFORM
 #include <xparameters.h>
@@ -102,8 +93,6 @@ int32_t platform_init()
 int main(void)
 {
 	int32_t status;
-	char demo_device_output[] = "demo_device_output";
-	char demo_device_input[] = "demo_device_input";
 
 	/* iio demo configurations. */
 	struct iio_demo_init_param iio_demo_in_init_par;
@@ -111,136 +100,14 @@ int main(void)
 	/* iio demo configurations. */
 	struct iio_demo_init_param iio_demo_out_init_par;
 
-	/* iio descriptor. */
-	struct iio_desc  *iio_desc;
-
 	/* iio instance descriptor. */
 	struct iio_demo_desc *iio_demo_in_desc;
 
 	/* iio instance descriptor. */
 	struct iio_demo_desc *iio_demo_out_desc;
 
-	/* iio init param */
-	struct iio_init_param iio_init_param;
-
-	/* Initialization for UART. */
-	struct uart_init_param uart_init_par;
-
-	/* IRQ initial configuration. */
-	struct irq_init_param irq_init_param;
-
-	/* IRQ instance. */
-	struct irq_ctrl_desc *irq_desc;
-
-#ifdef USE_TCP_SOCKET
-	struct tcp_socket_init_param	socket_param;
-	struct wifi_init_param		wifi_param;
-	struct wifi_desc		*wifi;
-	struct uart_desc		*uart_desc;
-#endif
-
 	status = platform_init();
 	if (IS_ERR_VALUE(status))
-		return status;
-
-#ifdef XILINX_PLATFORM
-	/* Xilinx platform dependent initialization for IRQ. */
-	struct xil_irq_init_param platform_irq_init_par;
-
-	platform_irq_init_par = (struct xil_irq_init_param ) {
-#ifdef XPAR_INTC_SINGLE_DEVICE_ID
-		.type = IRQ_PL,
-#else
-		.type = IRQ_PS,
-#endif
-	};
-#endif // XILINX_PLATFORM
-
-#ifdef ADUCM_PLATFORM
-	/* Dummy value for Aducm platform dependent initialization for IRQ. */
-	int32_t platform_irq_init_par = 0;
-#endif //ADUCM_PLATFORM
-
-	irq_init_param = (struct irq_init_param ) {
-		.irq_ctrl_id = INTC_DEVICE_ID,
-		.extra = &platform_irq_init_par
-	};
-
-	status = irq_ctrl_init(&irq_desc, &irq_init_param);
-	if(status < 0)
-		return status;
-
-#ifdef XILINX_PLATFORM
-	/* Xilinx platform dependent initialization for UART. */
-	struct xil_uart_init_param platform_uart_init_par;
-
-	platform_uart_init_par = (struct xil_uart_init_param) {
-#ifdef XPAR_XUARTLITE_NUM_INSTANCES
-		.type = UART_PL,
-#else
-		.type = UART_PS,
-#endif
-		.irq_id = UART_IRQ_ID,
-		.irq_desc = irq_desc,
-	};
-#endif // XILINX_PLATFORM
-
-#ifdef ADUCM_PLATFORM
-
-	/* Aducm platform dependent initialization for UART. */
-	struct aducm_uart_init_param platform_uart_init_par = {
-		.parity = UART_NO_PARITY,
-		.stop_bits = UART_ONE_STOPBIT,
-		.word_length = UART_WORDLEN_8BITS
-	};
-#endif // ADUCM_PLATFORM
-	uart_init_par = (struct uart_init_param) {
-		.device_id = UART_DEVICE_ID,
-		.baud_rate = UART_BAUDRATE,
-		.extra = &platform_uart_init_par
-	};
-
-	status = irq_global_enable(irq_desc);
-	if (status < 0)
-		return status;
-
-#ifdef USE_TCP_SOCKET
-	status = uart_init(&uart_desc, &uart_init_par);
-	if (status < 0)
-		return status;
-
-	wifi_param.irq_desc = irq_desc;
-	wifi_param.uart_desc = uart_desc;
-#ifdef ADUCM_PLATFORM
-	wifi_param.uart_irq_conf = uart_desc;
-#endif //ADUCM_PLATFORM
-	wifi_param.uart_irq_id = UART_IRQ_ID;
-
-	status = wifi_init(&wifi, &wifi_param);
-	if (status < 0)
-		return status;
-
-	status = wifi_connect(wifi, WIFI_SSID, WIFI_PWD);
-	if (status < 0)
-		return status;
-
-	char buff[100];
-	wifi_get_ip(wifi, buff, 100);
-	printf("Tinyiiod ip is: %s\n", buff);
-
-	wifi_get_network_interface(wifi, &socket_param.net);
-	socket_param.max_buff_size = 0;
-
-	iio_init_param.phy_type = USE_NETWORK;
-	iio_init_param.tcp_socket_init_param = &socket_param;
-
-#else //USE_TCP_SOCKET
-	iio_init_param.phy_type = USE_UART;
-	iio_init_param.uart_init_param = &uart_init_par;
-#endif //USE_TCP_SOCKET
-
-	status = iio_init(&iio_desc, &iio_init_param);
-	if(status < 0)
 		return status;
 
 	iio_demo_out_init_par = (struct iio_demo_init_param) {
@@ -255,7 +122,6 @@ int main(void)
 		.dev_global_attr = 2200,
 		.dev_ch_attr = 2211,
 	};
-
 	status = iio_demo_dev_init(&iio_demo_in_desc, &iio_demo_in_init_par);
 	if (IS_ERR_VALUE(status))
 		return status;
@@ -264,25 +130,17 @@ int main(void)
 		.buff = (void *)ADC_DDR_BASEADDR,
 		.size = MAX_SIZE_BASE_ADDR
 	};
-	status = iio_register(iio_desc, &iio_demo_dev_in_descriptor,
-			      demo_device_input, iio_demo_in_desc,
-			      &rd_buf, NULL);
-	if (IS_ERR_VALUE(status))
-		return status;
-
 	struct iio_data_buffer wr_buf = {
 		.buff = (void *)DAC_DDR_BASEADDR,
 		.size = MAX_SIZE_BASE_ADDR
 	};
-	status = iio_register(iio_desc, &iio_demo_dev_out_descriptor,
-			      demo_device_output, iio_demo_out_desc,
-			      NULL, &wr_buf);
-	if (IS_ERR_VALUE(status))
-		return status;
 
-	do {
-		status = iio_step(iio_desc);
-	} while (true);
+	struct iio_app_device devices[] = {
+		IIO_APP_DEVICE("demo_device_output", iio_demo_out_desc,
+			       &iio_demo_dev_out_descriptor,NULL, &wr_buf),
+		IIO_APP_DEVICE("demo_device_input", iio_demo_in_desc,
+			       &iio_demo_dev_in_descriptor,&rd_buf, NULL)
+	};
 
-	return status;
+	return iio_app_run(devices, 2);
 }
