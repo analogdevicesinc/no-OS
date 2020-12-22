074f9bcbc5e3af5219760548603722c21bb043a9
Antoniu Miclaus <antoniu.miclaus@analog.com>
2020-12-22 13:21:28 +0200

    projects: ad6676-ebz: add IIO support
    Allow the user to read/write via UART interface using IIO oscilloscope
    app.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- projects/ad6676-ebz/src/app_iio.h
+++ projects/ad6676-ebz/src/app_iio.h
@@ -1,6 +1,6 @@
 /***************************************************************************//**
- *   @file   projects/ad6676_ebz/src/app_config.h
- *   @brief  Config file for AD6676 project.
+ *   @file   app_iio.h
+ *   @brief  Application IIO setup.
  *   @author Antoniu Miclaus (antoniu.miclaus@analog.com)
 ********************************************************************************
  * Copyright 2020(c) Analog Devices, Inc.
@@ -36,12 +36,20 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
+#ifndef APP_IIO_H_
+#define APP_IIO_H_
 
-#ifndef APP_CONFIG_H_
-#define APP_CONFIG_H_
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include <stdint.h>
+#include "iio_axi_adc.h"
 
-//#define XILINX_PLATFORM
-//#define IIO_SUPPORT
+/******************************************************************************/
+/************************ Functions Declarations ******************************/
+/******************************************************************************/
 
-#endif /* APP_CONFIG_H_ */
+/* @brief Application IIO setup. */
+int32_t iio_app_start(struct iio_axi_adc_init_param *adc_init);
 
+#endif
