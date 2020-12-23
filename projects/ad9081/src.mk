87826795a22e63154e4425a5c55184113e5b0af1
Dragos Bogdan <dragos.bogdan@analog.com>
2020-12-24 00:35:23 +0200

    projects: ad9081: src.mk: Fix missing separator
    src.mk:52: *** missing separator
    
    This fixes: https://github.com/analogdevicesinc/no-OS/commit/94ef1e4820d189a403ae01e393ee78a40f6e7e48
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- projects/ad9081/src.mk
+++ projects/ad9081/src.mk
@@ -49,8 +49,7 @@ SRCS += $(PROJECT)/src/app_iio.c					\
 	$(NO-OS)/iio/iio_axi_adc/iio_axi_adc.c				\
 	$(NO-OS)/iio/iio_axi_dac/iio_axi_dac.c
 endif
-INCS +
-=	$(PROJECT)/src/app_clock.h					\
+INCS +=	$(PROJECT)/src/app_clock.h					\
 	$(PROJECT)/src/app_jesd.h					\
 	$(PROJECT)/src/app_config.h					\
 	$(PROJECT)/src/app_parameters.h					\
