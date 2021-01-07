096b8f8c86720a859657c222edd29f7b0af2ec9c
Mihail Chindris <mihail.chindris@analog.com>
2021-01-07 09:29:24 +0200

    tools:scripts: Remove LEGACY Makefiles code
    This code will not be used anymore
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

--- tools/scripts/generic.mk
+++ tools/scripts/generic.mk
@@ -1,60 +1,3 @@
-#By default for the moment old makefiles will be used
-
-ifeq '$(strip $(LEGACY_BUILD))' 'y'
-
-#------------------------------------------------------------------------------
-#                              UTIL FUNCTIONS
-#------------------------------------------------------------------------------
-# recursive wildcard
-rwildcard = $(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
-
-#------------------------------------------------------------------------------
-#                          COMMON INITIALIZATION
-#------------------------------------------------------------------------------
-
-ifeq (y,$(strip $(ENABLE_IIO_NETWORK)))
-CFLAGS += -DENABLE_IIO_NETWORK
-endif
-
-include $(NO-OS)/tools/scripts/libraries.mk
-
-# Get all .c and .h files from SRC_DIRS
-SRCS     += $(foreach dir, $(SRC_DIRS), $(call rwildcard, $(dir),*.c))
-INCS     += $(foreach dir, $(SRC_DIRS), $(call rwildcard, $(dir),*.h))
-
-# Recursive ignored files. If a directory is in the variable IGNORED_FILES,
-# all files from inside the directory will be ignored
-ALL_IGNORED_FILES = $(foreach dir, $(IGNORED_FILES), $(call rwildcard, $(dir),*))
-
-# Remove ignored files
-SRCS     := $(filter-out $(ALL_IGNORED_FILES),$(SRCS))
-INCS     := $(filter-out $(ALL_IGNORED_FILES),$(INCS))
-
-#------------------------------------------------------------------------------
-#                          INCLUDE SPECIFIC MAKEFILES
-#------------------------------------------------------------------------------
-
-ifeq (aducm3029,$(strip $(PLATFORM)))
-	include $(NO-OS)/tools/scripts/aducm.mk
-else ifeq (stm32,$(strip $(PLATFORM)))
-	include $(NO-OS)/tools/scripts/stm32.mk
-else
-	ifeq ($(OS), Windows_NT)
-		include $(NO-OS)/tools/scripts/windows.mk
-	else
-		include $(NO-OS)/tools/scripts/linux.mk
-	endif
-endif
-
-list:
-ifeq ($(OS), Windows_NT)
-	@echo $(subst $(space),,$(sort $(SRCS) $(INCS)))
-else
-	@echo $(sort $(SRCS) $(INCS)) | tr ' ' '\n'
-endif
-
-else
-
 #------------------------------------------------------------------------------
 #                             EXPORTED VARIABLES                               
 #------------------------------------------------------------------------------
@@ -375,5 +318,3 @@ list:
 	$(call print_lines, $(sort $(SRCS) $(INCS)))
 
 .PHONY: $(PHONY)
-
-endif #USE_OLD_MAKFILE
