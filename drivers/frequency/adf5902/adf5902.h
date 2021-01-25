75a6c9d45cd9f5047c5db4ec81e898758435a085
Antoniu Miclaus <antoniu.miclaus@analog.com>
2021-01-25 15:53:28 +0200

    drivers: adf5902: add reg bit def
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- drivers/frequency/adf5902/adf5902.h
+++ drivers/frequency/adf5902/adf5902.h
@@ -50,27 +50,7 @@
 /********************** Macros and Constants Definitions **********************/
 /******************************************************************************/
 
-/* ADF5902 Registers */
-#define ADF5902_REG0	0x0
-#define ADF5902_REG1	0x1
-#define ADF5902_REG2	0x2
-#define ADF5902_REG3	0x3
-#define ADF5902_REG4	0x4
-#define ADF5902_REG5	0x5
-#define ADF5902_REG6	0x6
-#define ADF5902_REG7	0x7
-#define ADF5902_REG8	0x8
-#define ADF5902_REG9	0x9
-#define ADF5902_REG10	0x10
-#define ADF5902_REG11	0x11
-#define ADF5902_REG12	0x12
-#define ADF5902_REG13	0x13
-#define ADF5902_REG14	0x14
-#define ADF5902_REG15	0x15
-#define ADF5902_REG16	0x16
-#define ADF5902_REG17	0x17
-
-/* Register 0 Definitions */
+/* Register 0 Map */
 #define ADF5902_REG0_PLO(x)		(((x) & 0x1) << 5)
 #define ADF5902_REG0_PTX1(x)		(((x) & 0x1) << 6)
 #define ADF5902_REG0_PTX2(x)		(((x) & 0x1) << 7)
@@ -81,37 +61,143 @@
 #define ADF5902_REG0_TX2C(x)		(((x) & 0x1) << 12)
 #define ADF5902_REG0_RESERVED		(0x4007F << 13)
 
-/* Register 1 Definitions */
+/* Register 0 Bit Definitions */
+#define ADF5902_POWER_DOWN_LO		0x0
+#define ADF5902_POWER_UP_LO		0x1
+
+#define ADF5902_POWER_DOWN_TX1		0x0
+#define ADF5902_POWER_UP_TX1		0x1
+
+#define ADF5902_POWER_DOWN_TX2		0x0
+#define ADF5902_POWER_UP_TX2		0x1
+
+#define ADF5902_POWER_DOWN_ADC		0x0
+#define ADF5902_POWER_UP_ADC		0x1
+
+#define ADF5902_VCO_NORMAL_OP		0x0
+#define ADF5902_VCO_FULL_CAL		0x1
+
+#define ADF5902_POWER_DOWN_VCO		0x0
+#define ADF5902_POWER_UP_VCO		0x1
+
+#define ADF5902_TX1_CAL_NORMAL		0x0
+#define ADF5902_TX1_AMP_CAL		0x1
+
+#define ADF5902_TX2_CAL_NORMAL		0x0
+#define ADF5902_TX2_AMP_CAL		0x1
+
+/* Register 1 Map */
 #define ADF5902_REG1_TX_AMP_CAL_REF(x)	(((x) & 0xFF) << 5)
 #define ADF5902_REG1_RESERVED		(0x7FFBF << 13)
 
-/* Register 2 Definitions */
+/* Register 1 Bit Definitions */
+#define ADF5902_TX_AMP_CAL_MIN_REF_CODE	0x00
+#define ADF5902_TX_AMP_CAL_MAX_REF_CODE	0xFF
+
+/* Register 2 Map */
 #define ADF5902_REG2_ADC_CLK_DIV(x)	(((x) & 0xFF) << 5)
 #define ADF5902_REG2_ADC_AVG(x)		(((x) & 0x3) << 13)
 #define ADF5902_REG2_ADC_START(x)	(((x) & 0x1) << 15)
 #define ADF5902_REG2_RESERVED		(0x2 << 16)
 
-/* Register 3 Definitions */
+/* Register 2 Bit Definitions */
+#define ADF5902_ADC_MIN_CLK_DIVIDER	0x1
+#define ADF5902_ADC_MAX_CLK_DIVIDER	0x7F
+
+#define ADF5902_ADC_AVG_1		0x0
+#define ADF5902_ADC_AVG_2		0x1
+#define ADF5902_ADC_AVG_3		0x2
+#define ADF5902_ADC_AVG_4		0x3
+
+#define ADF5902_ADC_NORMAL_OP		0x0
+#define ADF5902_START_ADC_CONV		0x1
+
+/* Register 3 Map */
 #define ADF5902_REG3_READBACK_CTRL(x)	(((x) & 0x3F) << 5)
 #define ADF5902_REG3_IO_LVL(x)		(((x) & 0x1) << 11)
 #define ADF5902_REG3_MUXOUT(x)		(((x) & 0xF) << 12)
 #define ADF5902_REG3_RESERVED		(0x189 << 16)
 
-/* Register 4 Definitions */
+/* Register 3 Bit Definitions */
+#define ADF5902_REG0			0x1
+#define ADF5902_REG1			0x2
+#define ADF5902_REG2			0x3
+#define ADF5902_REG3			0x4
+#define ADF5902_REG4			0x5
+#define ADF5902_REG5			0x6
+#define ADF5902_REG6			0x7
+#define ADF5902_REG7			0x8
+#define ADF5902_REG8			0x9
+#define ADF5902_REG9			0xA
+#define ADF5902_REG10			0xB
+#define ADF5902_REG11			0xC
+#define ADF5902_REG12			0xD
+#define ADF5902_REG13_SEL_0		0xE
+#define ADF5902_REG14_SEL_0		0xF
+#define ADF5902_REG15_SEL_0		0x10
+#define ADF5902_REG16_SEL_0		0x11
+#define ADF5902_REG17			0x12
+#define ADF5902_ADC_READBACK		0x16
+#define ADF5902_FREQ_READBACK		0x1A
+#define ADF5902_REG13_SEL_1		0x33
+#define ADF5902_REG14_SEL_1		0x34
+#define ADF5902_REG15_SEL_1		0x35
+#define ADF5902_REG16_SEL_1		0x36
+#define ADF5902_REG13_SEL_2		0x37
+#define ADF5902_REG14_SEL_2		0x38
+#define ADF5902_REG15_SEL_2		0x39
+#define ADF5902_REG16_SEL_2		0x3A
+#define ADF5902_REG13_SEL_3		0x3B
+#define ADF5902_REG14_SEL_3		0x3C
+#define ADF5902_REG15_SEL_3		0x3D
+#define ADF5902_REG16_SEL_3		0x3F
+
+#define ADF5902_IO_LVL_1V8		0x0
+#define ADF5902_IO_LVL_3V3		0x1
+
+#define ADF5902_MUXOUT_TRISTATE_OUT	0x0
+#define ADF5902_MUXOUT_LOGIC_HIGH	0x1
+#define ADF5902_MUXOUT_LOGIC_LOW	0x2
+#define ADF5902_MUXOUT_R_DIV_OUT	0x3
+#define ADF5902_MUXOUT_N_DIV_OUT	0x4
+#define ADF5902_MUXOUT_CAL_BUSY		0x5
+#define ADF5902_MUXOUT_R_DIV_OUT_2	0xB
+#define ADF5902_MUXOUT_N_DIV_OUT_2	0xC
+#define ADF5902_MUXOUT_RAMP_STATUS	0xF
+
+/* Register 4 Map */
 #define ADF5902_REG4_TEST_BUS(x)	(((x) & 0x7FFF) << 5)
 #define ADF5902_REG4_RESERVED		(0x0 << 16)
 
-/* Register 5 Definitions */
+/* Register 4 Bit Definitions */
+#define ADF5902_TEST_BUS_NONE		0x0000
+#define ADF5902_RAMP_COMPL_TO_MUXOUT	0x00C0
+#define ADF5902_RAMP_DOWN_TO_MUXOUT	0x0100
+#define ADF5902_TEMP_SENS_TO_ATEST	0x0503
+#define ADF5902_TEMP_SENS_TO_ADC	0x0903
+
+/* Register 5 Map */
 #define ADF5902_REG5_FRAC_MSB_WORD(x)	(((x) & 0xFFF) << 5)
 #define ADF5902_REG5_INTEGER_WORD(x)	(((x) & 0xFFF) << 17)
 #define ADF5902_REG5_RAMP_ON(x)		(((x) & 0x1) << 29)
 #define ADF5902_REG5_RESERVED		(0x0 << 30)
 
-/* Register 6 Definitions */
+/* Register 5 Bit Definitions */
+#define ADF5902_MIN_FRAC_MSB_WORD	0x000
+#define ADF5902_MAX_FRAC_MSB_WORD	0xFFF
+
+#define ADF5902_MIN_INT_MSB_WORD	0x000
+#define ADF5902_MAX_INT_MSB_WORD	0xFFF
+
+/* Register 6 Map */
 #define ADF5902_REG6_FRAC_LSB_WORD(x)	(((x) & 0x1FFF) << 5)
 #define ADF5902_REG6_RESERVED		(0x0 << 18)
 
-/* Register 7 Definitions */
+/* Register 6 Bit Definitions */
+#define ADF5902_MIN_FRAC_LSB_WORD	0x000
+#define ADF5902_MAX_FRAC_LSB_WORD	0x1FFF
+
+/* Register 7 Map */
 #define ADF5902_REG7_R_DIVIDER(x)	(((x) & 0x1F) << 5)
 #define ADF5902_REG7_REF_DOUBLER(x)	(((x) & 0x1) << 10)
 #define ADF5902_REG7_R_DIV_2(x)		(((x) & 0x1) << 11)
@@ -119,37 +205,106 @@
 #define ADF5902_REG7_MASTER_RESET(x)	(((x) & 0x1) << 25)
 #define ADF5902_REG6_RESERVED		((0x0 << 26) & (0x1 << 24))
 
-/* Register 8 Definitions */
+/* Register 7 Bit Definitions */
+#define ADF5902_MIN_R_DIVDER		0x01
+#define ADF5902_MAX_R_DIVIER		0x1F
+
+#define ADF5902_REF_DOUBLER_DISABLE	0x0
+#define ADF5902_REF_DOUBLER_ENABLE	0x1
+
+#define ADF5902_MIN_CLK_DIVIDER		0x000
+#define ADF5902_MAX_CLK_DIVIDER		0xFFF
+
+#define ADF5902_MASTER_RESET_DISABLE	0x0
+#define ADF5902_MASTER_RESET_ENABLE	0x1
+
+/* Register 8 Map */
 #define ADF5902_REG8_FREQ_CAL_DIV(x)	(((x) & 0x3FF) << 5)
 #define ADF5902_REG8_RESERVED		(0x8000 << 15)
 
-/* Register 9 Definitions */
+/* Register 8 Bit Definitions */
+#define ADF5902_MIN_FREQ_CAL_DIV	0x000
+#define ADF5902_MAX_FREQ_CAL_DIV	0x3FF
+
+/* Register 9 Map */
 #define ADF5902_REG9_RESERVED_CALIB	(0x15105C9 << 5)
 #define ADF5902_REG9_RESERVED_NORMAL	(0x14005C9 << 5)
 
-/* Register 10 Definitions */
+/* Register 10 Map */
 #define ADF5902_REG10_RESERVED		(0xE99532 << 5)
 
-/* Register 11 Definitions */
+/* Register 11 Map */
 #define ADF5902_REG11_CNTR_RESET(x)	(((x) & 0x1) << 5)
 #define ADF5902_REG11_RAMP_MODE(x)	(((x) & 0x3) << 7)
 #define ADF5902_REG11_SING_FULL_TRI(x)	(((x) & 0x1) << 9)
 #define ADF5902_REG11_SD_RESET(x)	(((x) & 0x1) << 11)
 #define ADF5902_REG11_RESERVED		((0x0 << 6) & (0x0 << 10) & (0x0 << 12))
 
-/* Register 12 Definitions */
+/* Register 11 Bit Definitions */
+#define ADF5902_CNTR_RESET_DISABLE	0x0
+#define ADF5902_CNTR_RESET_ENABLE	0x1
+
+#define ADF5902_CONT_SAWTOOTH		0x0
+#define ADF5902_SAWTOOTH_BURST		0x1
+#define ADF5902_CONTINUOUS_TRIANGULAR	0x2
+#define ADF5902_SINGLE_RAMP_BURST	0x3
+
+#define ADF5902_SINGLE_FULL_TRI_DISBLE	0x0
+#define ADF5902_SINGLE_FULL_TRI_ENABLE	0x1
+
+#define ADF5902_SD_RESET_ENABLE		0x0
+#define ADF5902_SD_RESET_DISABLE	0x1
+
+/* Register 12 Map */
 #define ADF5902_REG12_CP_TRISTATE(x)	(((x) & 0x1) << 15)
 #define ADF5902_REG12_CHARGE_PUMP(x)	(((x) & 0xF) << 17)
 #define ADF5902_REG12_RESERVED		((0x0 << 5) & (0x1 << 16) & (0x2 << 21))
 
-/* Register 13 Definitions */
+/* Register 12 Bit Definition */
+#define ADF5902_CP_TRISTATE_DISABLE	0x0
+#define ADF5902_CP_TRISTATE_ENABLE	0x1
+
+#define ADF5902_CP_CURRENT_280UA	0x0
+#define ADF5902_CP_CURRENT_560UA	0x1
+#define ADF5902_CP_CURRENT_840UA	0x2
+#define ADF5902_CP_CURRENT_1MA12	0x3
+#define ADF5902_CP_CURRENT_1MA40	0x4
+#define ADF5902_CP_CURRENT_1MA68	0x5
+#define ADF5902_CP_CURRENT_1MA96	0x6
+#define ADF5902_CP_CURRENT_2MA24	0x7
+#define ADF5902_CP_CURRENT_2MA52	0x8
+#define ADF5902_CP_CURRENT_2MA80	0x9
+#define ADF5902_CP_CURRENT_3MA08	0xA
+#define ADF5902_CP_CURRENT_3MA36	0xB
+#define ADF5902_CP_CURRENT_3MA64	0xC
+#define ADF5902_CP_CURRENT_3MA92	0xD
+#define ADF5902_CP_CURRENT_4MA20	0xE
+#define ADF5902_CP_CURRENT_4MA48	0xF
+
+/* Register 13 Map */
 #define ADF5902_REG13_CLK_DIV_SEL(x)	(((x) & 0x3) << 5)
 #define ADF5902_REG13_CLK_DIV_2(x)	(((x) & 0xFFF) << 7)
 #define ADF5902_REG13_CLK_DIV_MODE(x)	(((x) & 0x3) << 19)
 #define ADF5902_REG13_LE_SEL(x)		(((x) & 0x1) << 21)
 #define ADF5902_REG13_RESERVED		(0x0 << 22)
 
-/* Register 14 Definitions */
+/* Register 13 Bit Definitions */
+#define ADF5902_CLK_DIV_SEL_0		0x0
+#define ADF5902_CLK_DIV_SEL_1		0x1
+#define ADF5902_CLK_DIV_SEL_2		0x2
+#define ADF5902_CLK_DIV_SEL_3		0x3
+
+#define ADF5902_MIN_CLK_DIV_2		0x000
+#define ADF5902_MAX_CLK_DIV_2		0xFFF
+
+#define ADF5902_CLK_DIV_OFF		0x0
+#define ADF5902_FREQ_MEASURE		0x2
+#define ADF5902_RAMP_DIV		0x3
+
+#define ADF5902_LE_FROM_PIN		0x0
+#define ADF5902_LE_SYNC_REFIN		0x1
+
+/* Register 14 Map */
 #define ADF5902_REG14_DEV_WORD(x)	(((x) & 0xFFFF) << 5)
 #define ADF5902_REG14_DEV_OFFSET(x)	(((x) & 0xF) << 21)
 #define ADF5902_REG14_DEV_SEL(x)	(((x) & 0x3) << 25)
@@ -157,27 +312,77 @@
 #define ADF5902_REG14_TX_DATA_INV(x)	(((x) & 0x1) << 31)
 #define ADF5902_REG14_RESERVED		(0x0 << 27)
 
-/* Register 15 Definitions */
+/* Register 14 Bit Definitions */
+#define ADF5902_MAX_DEV_WORD		0x7FFF
+#define ADF5902_MIN_DEV_WORD		0x8000
+
+#define ADF5902_DEV_OFFSET_0		0x0
+#define ADF5902_DEV_OFFSET_1		0x1
+#define ADF5902_DEV_OFFSET_2		0x2
+#define ADF5902_DEV_OFFSET_3		0x3
+#define ADF5902_DEV_OFFSET_4		0x4
+#define ADF5902_DEV_OFFSET_5		0x5
+#define ADF5902_DEV_OFFSET_6		0x6
+#define ADF5902_DEV_OFFSET_7		0x7
+#define ADF5902_DEV_OFFSET_8		0x8
+#define ADF5902_DEV_OFFSET_9		0x9
+
+#define ADF5902_DEV_SEL_0		0x0
+#define ADF5902_DEV_SEL_1		0x1
+#define ADF5902_DEV_SEL_2		0x2
+#define ADF5902_DEV_SEL_3		0x3
+
+#define ADF5902_TX_RAMP_CLK_DIV		0x0
+#define ADF5902_TX_RAMP_TX_DATA_PIN	0x1
+
+#define AD5902_TX_DATA_INV_DISABLE	0x0
+#define AD5902_TX_DATA_INV_ENABLE	0x1
+
+/* Register 15 Map */
 #define ADF5902_REG15_STEP_WORD(x)	(((x) & 0xFFFFF) << 5)
 #define ADF5902_REG15_STEP_SEL(x)	(((x) & 0x3) << 25)
 #define ADF5902_REG15_RESERVED		(0x0 << 27)
 
-/* Register 16 Definitions */
+/* Register 15 Bit Definition */
+#define ADF5902_MIN_STEP_WORD		0x00000
+#define ADF5902_MAX_STEP_WORD		0xFFFFF
+
+#define ADF5902_STEP_SEL_0		0x0
+#define ADF5902_STEP_SEL_1		0x1
+#define ADF5902_STEP_SEL_2		0x2
+#define ADF5902_STEP_SEL_3		0x3
+
+/* Register 16 Map */
 #define ADF5902_REG16_DEL_START_WORD(x)	(((x) & 0xFFF) << 5)
 #define ADF5902_REG16_RAMP_DEL(x)	(((x) & 0x1) << 19)
 #define ADF5902_REG16_TX_DATA_TRIG(x)	(((x) & 0x1) << 20)
 #define ADF5902_REG16_DEL_SEL(x)	(((x) & 0x3) << 23)
 #define ADF5902_REG16_RESERVED		((0x0 << 17) & (0x0 << 21) & (0x1 << 25))
 
-/* Register 17 Definitions */
+#define ADF5902_MIN_DELAY_STARD_WRD	0x000
+#define ADF5902_MAX_DELAY_STARD_WRD	0xFFF
+
+#define ADF5902_RAMP_DEL_DISABLE	0x0
+#define ADF5902_RAMP_DEL_ENABLE		0x1
+
+#define ADF5902_TX_DATA_TRIGGER_DISABLE	0x0
+#define ADF5902_TX_DATA_TRIGGER_ENABLE	0x1
+
+#define ADF5902_DEL_SEL_0		0x0
+#define ADF5902_DEL_SEL_1		0x1
+#define ADF5902_DEL_SEL_2		0x2
+#define ADF5902_DEL_SEL_3		0x3
+
+
+/* Register 17 Map */
 #define ADF5902_REG17_RESERVED		(0x0 << 5)
 
 /** Specifications */
-#define ADF5902_MAX_VCO_FREQ	24250000000ull
-#define ADF5902_MIN_VCO_FREQ	24000000000ull
-#define ADF5902_MIN_REFIN_FREQ	10000000
-#define ADF5902_MAX_REFIN_FREQ	260000000
-#define ADF5902_MAX_FREQ_PFD	110000000
+#define ADF5902_MAX_VCO_FREQ		24250000000ull
+#define ADF5902_MIN_VCO_FREQ		24000000000ull
+#define ADF5902_MIN_REFIN_FREQ		10000000
+#define ADF5902_MAX_REFIN_FREQ		260000000
+#define ADF5902_MAX_FREQ_PFD		110000000
 
 /******************************************************************************/
 /*************************** Types Declarations *******************************/
