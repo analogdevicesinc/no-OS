99f4e6c3dfb3c254fb284f694b597e87620c7d69
Andrei Drimbarean <Andrei.Drimbarean@analog.com>
2021-03-01 13:14:11 +0200

    drivers:adc:ad7799: move global variable declarations
    Move global variable declarations used in IIO driver from the header to the
    source.
    
    Signed-off-by: Andrei Drimbarean <Andrei.Drimbarean@analog.com>

--- drivers/adc/ad7799/iio_ad7799.h
+++ drivers/adc/ad7799/iio_ad7799.h
@@ -42,78 +42,7 @@
 
 #include "iio_types.h"
 
-/** AD7799 IIO Channel read */
-ssize_t ad7799_iio_channel_read(void *device, char *buf, size_t len,
-				const struct iio_ch_info *channel, intptr_t priv);
-/** AD7799 IIO Gain write read */
-ssize_t ad7799_iio_gain_read(void *device, char *buf, size_t len,
-			     const struct iio_ch_info *channel, intptr_t priv);
-/** AD7799 IIO Gain write */
-ssize_t ad7799_iio_gain_write(void *device, char *buf, size_t len,
-			      const struct iio_ch_info *channel, intptr_t priv);
-
-/** ADC IIO channel attributes*/
-struct iio_attribute ad7799_iio_channel_attributes[] = {
-	{
-		.name = "volts",
-		.show = ad7799_iio_channel_read,
-		.store = NULL
-	},
-	END_ATTRIBUTES_ARRAY,
-};
-
-/** Channel Scan Type */
-struct scan_type channel_scan_type = {
-	.sign = 'u',
-	.realbits = 12,
-	.storagebits = 24,
-	.shift = 0,
-	.is_big_endian = false
-};
-
-#define AD7799_IIO_CHANN_DEF(nm, ch) \
-	{ \
-		.name = nm, \
-		.ch_type = IIO_VOLTAGE, \
-		.channel = ch, \
-		.scan_type = &channel_scan_type, \
-		.attributes = ad7799_iio_channel_attributes, \
-		.ch_out = false, \
-		.indexed = 1, \
-		.diferential = true, \
-	}
-
-/** IIO ADC Channels */
-struct iio_channel ad7799_iio_channels[] = {
-	AD7799_IIO_CHANN_DEF("ch0", 0),
-	AD7799_IIO_CHANN_DEF("ch1", 1),
-	AD7799_IIO_CHANN_DEF("ch2", 2),
-	END_ATTRIBUTES_ARRAY
-};
-
-/** IIO attributes */
-struct iio_attribute ad7799_iio_attributes[] = {
-	{
-		.name = "gain",
-		.show = ad7799_iio_gain_read,
-		.store = ad7799_iio_gain_write,
-	},
-	END_ATTRIBUTES_ARRAY,
-};
-
 /** IIO Descriptor */
-struct iio_device ad7799_iio_descriptor = {
-	.num_ch = 3,
-	.channels = ad7799_iio_channels,
-	.attributes = ad7799_iio_attributes,
-	.debug_attributes = NULL,
-	.buffer_attributes = NULL,
-	.transfer_dev_to_mem = NULL,
-	.transfer_mem_to_dev = NULL,
-	.read_data = NULL,
-	.write_data = NULL,
-	.debug_reg_read = (int32_t (*)())ad7799_read,
-	.debug_reg_write = (int32_t (*)())ad7799_write,
-};
+extern struct iio_device const ad7799_iio_descriptor;
 
 #endif //IIO_AD7799_H
