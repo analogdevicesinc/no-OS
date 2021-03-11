678246dca3ce85dc44a42e1960b145366073af81
Pratyush Mallick <Pratyush.Mallick@analog.com>
2021-03-11 14:46:19 +0530

    Renamed the uart irq event macros.
    The ERROR macro in the 'irq_uart_event_e' enum conflicts with a lot of ST HAL libraries that mbed is using.
    Prefixed the irq event names with "IRQ_" as this would be more consistent and match the naming convention used
    for 'irq_trig_level' enum.
    
    Signed-off-by: Pratyush Mallick <Pratyush.Mallick@analog.com>

--- drivers/platform/aducm3029/uart.c
+++ drivers/platform/aducm3029/uart.c
@@ -229,7 +229,7 @@ static void uart_callback(void *ctx, uint32_t event, void *buff)
 		} else {
 			extra->read_desc.is_nonblocking = false;
 			if (desc->callback)
-				desc->callback(desc->callback_ctx, READ_DONE,
+				desc->callback(desc->callback_ctx, IRQ_READ_DONE,
 					       NULL);
 		}
 		break;
@@ -247,7 +247,7 @@ static void uart_callback(void *ctx, uint32_t event, void *buff)
 		} else {
 			extra->write_desc.is_nonblocking = false;
 			if (desc->callback)
-				desc->callback(desc->callback_ctx, WRITE_DONE,
+				desc->callback(desc->callback_ctx, IRQ_WRITE_DONE,
 					       NULL);
 		}
 		break;
@@ -256,7 +256,7 @@ static void uart_callback(void *ctx, uint32_t event, void *buff)
 		extra->read_desc.is_nonblocking = false;
 		extra->write_desc.is_nonblocking = false;
 		if (desc->callback)
-			desc->callback(desc->callback_ctx, ERROR, buff);
+			desc->callback(desc->callback_ctx, IRQ_ERROR, buff);
 		break;
 	}
 }
