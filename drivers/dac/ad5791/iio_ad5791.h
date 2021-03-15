cb533ff39ee1b44a1572a01a6c14200c3a606719
Andrei Drimbarean <Andrei.Drimbarean@analog.com>
2021-03-15 16:21:27 +0200

    drivers:dac:ad5791: update IIO driver
    Add the necessary functionality for the driver to be identical in
    functionality to the Linux one. This is done to use the pyadi-iio scripts on
    both environments.
    
    Signed-off-by: Andrei Drimbarean <Andrei.Drimbarean@analog.com>

--- drivers/dac/ad5791/iio_ad5791.h
+++ drivers/dac/ad5791/iio_ad5791.h
@@ -46,6 +46,44 @@
 
 #define AD5791_CH_NO 1
 
+/**
+ * @enum ad5791_iio_powerdown_modes
+ * @brief AD5791 channel power down modes.
+ */
+enum ad5791_iio_powerdown_modes {
+	AD5791_6kOHMS_TO_GND,
+	AD5791_THREE_STATE
+};
+
+/**
+ * @struct ad5791_iio_desc
+ * @brief D5791 IIO driver handler.
+ */
+struct ad5791_iio_desc {
+	struct ad5791_dev *ad5791_handle;
+	struct iio_device *ad5791_iio_dev;
+	enum ad5791_iio_powerdown_modes curr_mode;
+	uint32_t vref_mv;
+	uint32_t vref_neg_mv;
+};
+
+/**
+ * @struct ad5791_iio_init_param
+ * @brief AD5791 IIO driver initialization structure.
+ */
+struct ad5791_iio_init_param {
+	struct ad5791_init_param *ad5791_initial;
+	uint32_t vref_mv;
+	uint32_t vref_neg_mv;
+};
+
+/* Initialize the AD5791 IIO driver. */
+int32_t ad5791_iio_init(struct ad5791_iio_desc **iio_dev,
+			struct ad5791_iio_init_param *init_param);
+
+/* Free memory allocated by ad5791_iio_init(). */
+int32_t ad5791_iio_remove(struct ad5791_iio_desc *desc);
+
 /*****************************************************************************/
 /***************************** Constant definition ***************************/
 /*****************************************************************************/
