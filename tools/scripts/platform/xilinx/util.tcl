analogdevicesinc/no-OS

commit de1396b8f548dad484502abedbc2b15b4f5c3223

tools: scripts: xilinx: Move specific functions
This change concentrates all the Xilinx SDK specific tasks into a single
tcl script, keeping the project creation backwards compatible.

Signed-off-by: Sergiu Cuciurean <sergiu.cuciurean@analog.com>

tools/scripts/platform/xilinx/util.tcl
@@ -1,0 +1,248 @@
+proc _file_is_xsa {} {
+	if {[string first .hdf $::hw ] != -1} {
+		return 0
+	} else {
+		return 1
+	}
+}
+
+proc _get_processor {} {
+	if {[_file_is_xsa] == 1} {
+		set processor [hsi::get_cells * -filter {IP_TYPE==PROCESSOR}]
+	} else {
+		set processor [lindex \
+		[hsi::get_cells * -filter {IP_TYPE==PROCESSOR}] 0]
+	}
+	
+	if {[llength $processor] != 0} {
+		return $processor
+	}
+	return 0
+}
+
+proc _replace_heap {} {
+	set file_name "$::env(WORKSPACE)/app/src/lscript.ld"
+	set temp_name "$file_name.tmp"
+	set file [open [lindex $file_name] r]
+	set temp [open [lindex $temp_name] w+]
+
+	while {[gets $file line] >= 0} {
+		if {[string first _HEAP_SIZE $line ] != -1} {
+			puts $temp "_HEAP_SIZE = 0x100000;"
+		}
+		puts $temp $line
+	}
+
+	close $file
+	close $temp
+
+	file rename -force $temp_name $file_name
+}
+
+proc _project_config {cmd {arg}} {
+	if {$cmd == "sdk"} {
+		set arg2 "app"
+	} else {
+		set arg2 "name"
+	}
+
+	set paths [lsort -unique  $::env(EXTRA_INC_PATHS)]
+
+	foreach path $paths {
+		set new_path [string map [list $::env(PROJECT_BUILD)	\
+				  \${ProjDirPath}] $path]
+		$cmd $arg -$arg2 app include-path $new_path
+	}
+	foreach symbol $::env(FLAGS_WITHOUT_D) {
+		$cmd $arg -$arg2 app define-compiler-symbols $symbol
+	}
+	foreach lib $::env(EXTRA_LIBS_NAMES) {
+		$cmd $arg -$arg2 app libraries $lib
+	}
+	foreach lib_path $::env(EXTRA_LIBS_PATHS) {
+		$cmd $arg -$arg2 app library-search-path $lib_path
+	}
+}
+
+proc _vitis_project {} {
+	openhw $::hw
+	set cpu [_get_processor]
+	
+	# Create bsp
+	hsi::generate_bsp						\
+		-dir bsp						\
+		-proc $cpu						\
+		-os standalone						\
+		-compile
+	closehw $::hw
+	
+	# Create app
+	app create							\
+		-name app						\
+		-hw $::hw						\
+		-proc $cpu						\
+		-os standalone						\
+		-template  {Empty Application}
+
+	# Increase heap size
+	_replace_heap
+
+	# Configure the project
+	_project_config "app" "config"
+
+	# Build the project
+	app build -name app
+}
+
+proc _xsdk_project {} {
+	openhw $::hw
+	set cpu [_get_processor]
+
+	# # Create hwspec
+	sdk createhw							\
+		-name hw						\
+		-hwspec $::hw
+	# Create bsp
+	sdk createbsp							\
+		-name bsp						\
+		-hwproject hw						\
+		-proc $cpu						\
+		-os standalone  
+	# Create app
+	sdk createapp							\
+		-name app						\
+		-hwproject hw						\
+		-proc $cpu						\
+		-os standalone						\
+		-lang C							\
+		-app {Empty Application}				\
+		-bsp bsp
+	
+	closehw $::hw
+
+	# Increase heap size
+	_replace_heap
+
+	# Configure the project
+	_project_config "sdk" "configapp"
+
+	# Build the project via SDK
+	clean -type all
+	build -type all
+
+	projects -build -name app -type app
+}
+
+proc get_arch {} {
+	openhw $::hw
+	set cpu [_get_processor]
+	set file [open [lindex "$::env(WORKSPACE)/tmp/arch.txt"] w+]
+	puts $file $cpu
+	close $file
+	closehw $::hw
+}
+
+proc create_project {} {
+	cd $::env(WORKSPACE)
+	setws ./
+
+	if {[_file_is_xsa] == 1} {
+		_vitis_project
+	} else {
+		_xsdk_project
+	}
+}
+
+set pl_dict [dict create					\
+	"ps7_cortexa9_0" "xc7z*"				\
+	"psu_cortexa53_0" "PSU"					\
+	"sys_mb" "xc*"]
+
+set ps_dict [dict create					\
+	"ps7_cortexa9_0" "*Cortex-A9 MPCore #0*"		\
+	"psu_cortexa53_0" "*Cortex-A53 #0*"			\
+	"sys_mb" "*MicroBlaze #*"]
+
+proc _cpu_reset {cpu} {
+	targets -set -filter {name =~ "APU*"}
+	stop
+	if {$cpu == "ps7_cortexa9_0"} {
+		rst 
+	} elseif {$arch == "psu_cortexa53_0"} {
+		rst -system
+	}
+}
+
+proc _write_pl {cpu {bitstream}} {
+	set name [dict get $::pl_dict $cpu]
+	targets -set -filter {name =~ "$name"}
+	fpga -file [file normalize $bitstream]
+}
+
+proc _init_ps {cpu} {
+	openhw $::hw
+	switch $cpu {
+		"ps7_cortexa9_0" {
+			targets -set -filter {name =~ "APU*"}
+			ps7_init
+			ps7_post_config
+		}
+
+		"psu_cortexa53_0" {
+			# Configure PSU
+			targets -set -nocase -filter {name =~ "PSU"}
+			psu_init
+			psu_post_config
+			psu_ps_pl_reset_config
+			psu_ps_pl_isolation_removal
+			# write bootloop and release A53-0 reset
+			mwr 0xffff0000 0x14000000
+			mwr 0xFD1A0104 0x380E
+			targets -set -filter {name =~ "Cortex-A53 #0"}
+		}
+
+		"sys_mb" {
+			targets -set -filter {[dict get $::pl_dict $cpu]}
+
+		}
+	}
+}
+
+proc _write_ps {cpu} {
+	set name [dict get $::ps_dict $cpu]
+	targets -set -filter {name =~  "$name"}
+	dow "[file normalize $::env(BINARY)]"
+	con
+	disconnect
+}
+
+proc upload {} {
+	openhw $::hw
+	set bitstream $::env(WORKSPACE)/tmp/system_top.bit
+	set cpu [_get_processor]
+
+	# Connect to the fpga
+	connect
+
+	# Reset and stop the ARM CPU before we re-program the FPGA if we are on a ZYNQ.
+	# Otherwise undefined behavior can occur.
+	_cpu_reset $cpu
+
+	_write_pl $cpu $bitstream
+
+	_init_ps $cpu
+
+	_write_ps $cpu
+}
+
+set function	[lindex $argv 0]
+set hw_path	$::env(WORKSPACE)/tmp
+set hw		$::hw_path/$::env(HARDWARE)
+
+if {[file exists $::hw_path/ps7_init.tcl]} {
+	source "[file normalize $::hw_path/ps7_init.tcl]"
+} elseif {[file exists $::hw_path/psu_init.tcl]} {
+	source "[file normalize $::hw_path/psu_init.tcl]"
+}
+
+$function

tools/scripts/generic.mk
@@ -6,6 +6,10 @@ export CFLAGS
 export CC
 export AR
 
+# Used by the build utils
+export EXTRA_INC_PATHS
+export FLAGS_WITHOUT_D
+export PROJECT_BUILD
 #------------------------------------------------------------------------------
 #                              UTIL FUNCTIONS
 #------------------------------------------------------------------------------

tools/scripts/libraries.mk
@@ -1,5 +1,9 @@
 # File where libraries are handled
 
+# Used by the build utils
+export EXTRA_LIBS_NAMES
+export EXTRA_LIBS_PATHS
+
 #	IIO
 ifeq (y,$(strip $(TINYIIOD)))
 

tools/scripts/platform/xilinx/create_project.tcl
@@ -1,19 +1,0 @@
-if { $argc != 3 } {
-	puts "create_project: Invalid arguments"
-	exit
-}
-# Set the workspace
-sdk setws [lindex $argv 0]
-# Create hwspec
-sdk createhw -name hw -hwspec [lindex $argv 1]
-# Create bsp
-sdk createbsp -name bsp -hwproject hw -proc [lindex $argv 2] -os standalone  
-# Create app
-sdk createapp -name app -hwproject hw -proc [lindex $argv 2] -os standalone \
-	-lang C -app {Empty Application} -bsp bsp
-
-# Build the project via SDK
-clean -type all
-build -type all
-
-exit

tools/scripts/platform/xilinx/read_hdf.tcl
@@ -1,14 +1,0 @@
-if { $argc != 2 } {
-	puts "read_hdf: Invalid arguments"
-	exit
-}
-cd [lindex $argv 0]
-# Open the design
-hsi open_hw_design [lindex $argv 1]
-# Save the cpu name in a variable
-set cpu_name [lindex [hsi get_cells -filter {IP_TYPE==PROCESSOR}] 0]
-# Open a file
-set fp [open "arch.txt" w+]
-# Write the cpu name in the file
-puts $fp $cpu_name
-close $fp

tools/scripts/xilinx.mk
@@ -2,24 +2,23 @@
 #                           ENVIRONMENT VARIABLES                              
 #------------------------------------------------------------------------------
 
+# Used by the build utils
+export HARDWARE
+export WORKSPACE
+export TEMP_DIR
+export BINARY
+
 TEMP_DIR	= $(BUILD_DIR)/tmp
 BINARY		= $(BUILD_DIR)/$(PROJECT_NAME).elf
 
-#	WINDOWS
-ifeq ($(OS), Windows_NT)
 
-replace_heap = powershell \( \( Get-Content	-Path $3 -Raw \) 	\
-		-Replace \'_HEAP_SIZE : $1\', \'_HEAP_SIZE : $2\' \) \| \
-		Set-Content -Path $3
+define tcl_util
+	xsct $(PLATFORM_TOOLS)/util.tcl					\
+	     $(1)							\
+	     $(WORKSPACE) $(WORKSPACE)/tmp/$(HARDWARE)
+endef
 
-#	LINUX
-else
-
-replace_heap = sed -i "s/_HEAP_SIZE : $1/_HEAP_SIZE : $2/g"	$3
-
-endif
-
-ARCH = $(shell $(call read_file, $(BUILD_DIR)/tmp/arch.txt))
+ARCH = $(shell $(call read_file, $(TEMP_DIR)/arch.txt))
 
 # Define the platform compiler switch
 CFLAGS += -DXILINX_PLATFORM						\
@@ -121,18 +120,13 @@ $(BINARY): $(TEMP_DIR)/arch.txt
 
 PHONY += xilinx_run
 xilinx_run: all
-	$(MUTE) xsdb $(PLATFORM_TOOLS)/upload.tcl\
-		$(ARCH)\
-		$(BUILD_DIR)/hw/system_top.bit\
-		$(BINARY)\
-		$(BUILD_DIR)/hw $(HIDE)
+	$(MUTE) $(call tcl_util, upload) $(HIDE)
 
-#do copy of HARDWARE to tmp is needed?
 $(TEMP_DIR)/arch.txt: $(HARDWARE)
 	$(MUTE) $(call mk_dir,$(BUILD_DIR)/app $(BUILD_DIR)/app/src $(OBJECTS_DIR) $(TEMP_DIR)) $(HIDE)
 	$(MUTE) $(call copy_fun,$(HARDWARE),$(TEMP_DIR)) $(HIDE)
 	$(call print,Evaluating hardware: $(HARDWARE))
-	$(MUTE) xsct $(PLATFORM_TOOLS)/read_hdf.tcl $(TEMP_DIR) $(TEMP_DIR)/$(notdir $(HARDWARE)) $(HIDE)
+	$(MUTE)	$(call tcl_util, get_arch) $(HIDE)
 
 xilinx_project: $(BUILD_DIR)/.bsp.target
 
@@ -141,29 +135,9 @@ xilinx_project_build: all
 	$(MUTE) $(MUTE) xsct $(NO-OS)/tools/scripts/platform/xilinx/build_project.tcl $(WORKSPACE) $(HIDE)
 
 
-ADD_INCLUDE_PATHS=$(foreach dir, $(patsubst $(PROJECT_BUILD)/%,%,$(EXTRA_INC_PATHS)),\
-		sdk configapp -app app include-path $(addprefix \$${ProjDirPath}/,$(dir));)
-ADD_COMPILER_DEFINES = $(foreach flag, $(FLAGS_WITHOUT_D),\
-		sdk configapp -app app define-compiler-symbols $(flag);)
-ADD_LIBRARIES = $(foreach lib, $(EXTRA_LIBS_NAMES), \
-		sdk configapp -app app libraries $(lib);)
-ADD_LIBRARIES_PATH = $(foreach path, $(EXTRA_LIBS_PATHS), \
-		sdk configapp -app app library-search-path $(path);)
-
-ifeq ($(OS), Windows_NT)
-UDPATE_TCL_CONTENT = setws $(WORKSPACE);$(ADD_INCLUDE_PATHS) $(ADD_COMPILER_DEFINES) $(ADD_LIBRARIES_PATH) $(ADD_LIBRARIES)
-else
-UDPATE_TCL_CONTENT = 'setws $(WORKSPACE);$(ADD_INCLUDE_PATHS) $(ADD_COMPILER_DEFINES) $(ADD_LIBRARIES_PATH) $(ADD_LIBRARIES)'
-endif
-
 $(BUILD_DIR)/.bsp.target: $(LIB_TARGETS) $(TEMP_DIR)/arch.txt
-	$(call print,Creating IDE project)
-	$(MUTE) xsdk -batch -source $(PLATFORM_TOOLS)/create_project.tcl $(WORKSPACE) $(HARDWARE) $(ARCH) $(HIDE)
-	$(MUTE) echo $(UDPATE_TCL_CONTENT) > $(TEMP_DIR)/update_sdk.tcl
-	$(call print,Configuring project)
-	$(MUTE) xsct $(TEMP_DIR)/update_sdk.tcl $(HIDE)
-	$(MUTE)$(call replace_heap,0x800,0x100000,$(BUILD_DIR)/app/src/lscript.ld)
-	$(MUTE)$(call replace_heap,0x2000,0x100000,$(BUILD_DIR)/app/src/lscript.ld)
+	$(call print,Creating and configuring the IDE project)
+	$(MUTE)	$(call tcl_util, create_project)  $(HIDE)
 	$(MUTE) $(MAKE) --no-print-directory update_srcs
 	$(MUTE) $(call set_one_time_rule,$@) $(HIDE)
 

commit c3b1607aa2bba1c83c3fd4fb2d387c4ff9166791

xilinx scripts: fix typo in util.tcl
make run was broken on xilinx in de1396b8f.

Signed-off-by: Darius Berghe <darius.berghe@analog.com>

tools/scripts/platform/xilinx/util.tcl
@@ -169,7 +169,7 @@ proc _cpu_reset {cpu} {
 	stop
 	if {$cpu == "ps7_cortexa9_0"} {
 		rst 
-	} elseif {$arch == "psu_cortexa53_0"} {
+	} elseif {$cpu == "psu_cortexa53_0"} {
 		rst -system
 	}
 }