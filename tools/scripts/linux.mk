21aa01d8b302e94c4cb5143d4f1577e1399f0cc9
Mihail Chindris <mihail.chindris@analog.com>
2021-04-21 08:43:39 +0100

    tools:scripts:linux.mk: Add support for debugging
    After running make, the folder build/app can be selected
    from vscode in order to start developing and debugging the
    project.
    Includes path and flags are set by the config_build.py
    script in order to have correct linting.
    Files from tools/scripts/platform/linux/.vscode are copied to
    the build/app folder and the updated with config_build.py
    with the project specific parameters
    tools:scripts:linux.mk: Add support for debugging
    
    After running make, the folder build/app can be selected
    from vscode in order to start developing and debugging the
    project.
    Includes path and flags are set by the config_build.py
    script in order to have correct linting.
    Files from tools/scripts/platform/linux/.vscode are copied to
    the build/app folder and the updated with config_build.py
    with the project specific parameters
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

--- tools/scripts/linux.mk
+++ tools/scripts/linux.mk
@@ -12,5 +12,11 @@ $(PROJECT_TARGET):
 	$(MUTE) $(call mk_dir, $(BUILD_DIR)) $(HIDE)
 	$(MUTE) $(call set_one_time_rule,$@)
 
+develop:
+	$(MUTE) $(call mk_dir, $(PROJECT_BUILD)) $(HIDE)
+	$(MUTE) $(call copy_folder, $(PLATFORM_TOOLS)/.vscode, $(PROJECT_BUILD)/.vscode) $(HIDE)
+	$(MUTE) python $(PLATFORM_TOOLS)/config_build.py $(NO-OS) $(PROJECT) $(BINARY) $(HIDE)
+	$(call print, Workspace ready. Open $(PROJECT_BUILD) directory in VSCode for debug)
+
 linux_run: $(BINARY)
 	$(BINARY)
