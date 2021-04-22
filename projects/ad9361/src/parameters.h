8b771d95521034be8c0b59763e1da6c7ece8280f
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2021-04-22 17:16:08 +0300

    projects: ad9361: use iio_app Update project to use iio_app.
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- projects/ad9361/src/parameters.h
+++ projects/ad9361/src/parameters.h
@@ -49,7 +49,6 @@
 /********************** Macros and Constants Definitions **********************/
 /******************************************************************************/
 #define UART_BAUDRATE 921600
-
 #ifdef XPAR_AXI_AD9361_0_BASEADDR
 #define AD9361_RX_0_BASEADDR		XPAR_AXI_AD9361_0_BASEADDR
 #define AD9361_TX_0_BASEADDR		XPAR_AXI_AD9361_0_BASEADDR + 0x4000
@@ -151,6 +150,14 @@
 
 #define SPI_DEVICE_ID		0
 #define SPI_CS			0
+
+#define MAX_SIZE_BASE_ADDR 0x1000
+
+static uint8_t in_buff[MAX_SIZE_BASE_ADDR];
+static uint8_t out_buff[MAX_SIZE_BASE_ADDR];
+
+#define DAC_DDR_BASEADDR	((uint32_t)out_buff)
+#define ADC_DDR_BASEADDR	((uint32_t)in_buff)
 
 #define GPIO_RESET_PIN	1006
 #endif
