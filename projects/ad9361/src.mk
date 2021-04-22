569a68b798112bc48a1c8edd080eb0a98cee189e
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2021-04-22 17:11:52 +0300

    projects: ad9361: make Update src.mk to add linux_sockets and iio_app.
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

--- projects/ad9361/src.mk
+++ projects/ad9361/src.mk
@@ -30,13 +30,24 @@ SRCS +=	$(PLATFORM_DRIVERS)/delay.c
 endif
 ifeq (y,$(strip $(TINYIIOD)))
 LIBRARIES += iio
-SRCS += $(PLATFORM_DRIVERS)/uart.c					\
-	$(NO-OS)/util/fifo.c						\
+
+ifeq (linux,$(strip $(PLATFORM)))
+CFLAGS += -DENABLE_IIO_NETWORK 	\
+		-DDISABLE_SECURE_SOCKET
+SRCS += $(NO-OS)/network/linux_socket/linux_socket.c	\
+		$(NO-OS)/network/tcp_socket.c	\
+	$(PLATFORM_DRIVERS)/linux_uart.c
+else
+SRCS += $(PLATFORM_DRIVERS)/uart.c
+endif
+
+SRCS += $(NO-OS)/util/fifo.c						\
 	$(NO-OS)/util/list.c						\
 	$(NO-OS)/iio/iio_ad9361/iio_ad9361.c				\
 	$(NO-OS)/iio/iio_axi_adc/iio_axi_adc.c				\
-	$(NO-OS)/iio/iio_axi_dac/iio_axi_dac.c	\
-	$(NO-OS)/iio/iio_app/iio_app.c
+	$(NO-OS)/iio/iio_axi_dac/iio_axi_dac.c		\
+	$(NO-OS)/iio/iio_app/iio_app.c		\
+	$(NO-OS)/util/circular_buffer.c
 endif
 INCS += $(DRIVERS)/rf-transceiver/ad9361/common.h			\
 	$(PROJECT)/src/app_config.h
@@ -49,8 +60,10 @@ INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h			\
 	$(INCLUDE)/irq.h						\
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h
 ifeq (linux,$(strip $(PLATFORM)))
+CFLAGS += -DPLATFORM_MB
 INCS +=	$(PLATFORM_DRIVERS)/linux_spi.h					\
-	$(PLATFORM_DRIVERS)/linux_gpio.h
+	$(PLATFORM_DRIVERS)/linux_gpio.h	\
+	$(PLATFORM_DRIVERS)/linux_uart.h
 endif
 INCS +=	$(INCLUDE)/axi_io.h						\
 	$(INCLUDE)/spi.h						\
@@ -59,18 +72,28 @@ INCS +=	$(INCLUDE)/axi_io.h						\
 	$(INCLUDE)/delay.h						\
 	$(INCLUDE)/util.h
 ifeq (y,$(strip $(TINYIIOD)))
+
+ifeq (linux,$(strip $(PLATFORM)))
+INCS += $(NO-OS)/network/tcp_socket.h	\
+	$(NO-OS)/network/linux_socket/linux_socket.h	\
+	$(NO-OS)/network/network_interface.h	\
+	$(NO-OS)/network/noos_mbedtls_config.h
+endif
+
 INCS += $(INCLUDE)/fifo.h						\
 	$(INCLUDE)/uart.h						\
-	$(INCLUDE)/list.h						\
-	$(PLATFORM_DRIVERS)/uart_extra.h				\
+	$(INCLUDE)/list.h										\
 	$(NO-OS)/iio/iio_ad9361/iio_ad9361.h				\
 	$(NO-OS)/iio/iio_axi_adc/iio_axi_adc.h				\
 	$(NO-OS)/iio/iio_axi_dac/iio_axi_dac.h	\
-	$(NO-OS)/iio/iio_app/iio_app.h
+	$(NO-OS)/iio/iio_app/iio_app.h	\
+	$(INCLUDE)/circular_buffer.h
 endif
 ifeq (xilinx,$(strip $(PLATFORM)))
-SRCS +=	$(PLATFORM_DRIVERS)/irq.c
+SRCS +=	$(PLATFORM_DRIVERS)/irq.c	
+	
 INCS += $(PLATFORM_DRIVERS)/irq_extra.h				\
 	$(PLATFORM_DRIVERS)/spi_extra.h					\
-	$(PLATFORM_DRIVERS)/gpio_extra.h
+	$(PLATFORM_DRIVERS)/gpio_extra.h	\
+	$(PLATFORM_DRIVERS)/uart_extra.h	
 endif
