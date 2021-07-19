analogdevicesinc/no-OS

commit 6b7f049b4a57919835a52f0c9de6d360db65d95c

ad9081: Fix 4x4 Crossbar Mux0 Mappings for AD9081
Corresponds to linux commit:
18fa16da2d3cf5c78f6c13ec651a667313102813

Signed-off-by: Darius Berghe <darius.berghe@analog.com>

drivers/adc/ad9081/ad9081.c
@@ -595,6 +595,17 @@ static int32_t ad9081_setup(struct ad9081_phy *phy)
 	if (ret != 0)
 		return ret;
 
+	/* Fix: 4x4 Crossbar Mux0 Mappings for AD9081 */
+	ret  = adi_ad9081_adc_pfir_din_select_set(&phy->ad9081,
+		AD9081_ADC_PFIR_ADC_PAIR0, 0, 1);
+	if (ret != 0)
+		return ret;
+
+	ret  = adi_ad9081_adc_pfir_din_select_set(&phy->ad9081,
+		AD9081_ADC_PFIR_ADC_PAIR1, 3, 0);
+	if (ret != 0)
+		return ret;
+
 	/* setup txfe dac channel gain */
 	ret = adi_ad9081_dac_duc_nco_gains_set(&phy->ad9081,
 					       phy->dac_cache.chan_gain);

commit 75984383b2175c8b003cb385765f50ee59e8cc05

drivers/adc/ad9081: minor build fix
Fix build issue propagated into master branch.

Fixes: 6b7f049

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad9081/ad9081.c
@@ -597,12 +597,12 @@ static int32_t ad9081_setup(struct ad9081_phy *phy)
 
 	/* Fix: 4x4 Crossbar Mux0 Mappings for AD9081 */
 	ret  = adi_ad9081_adc_pfir_din_select_set(&phy->ad9081,
-		AD9081_ADC_PFIR_ADC_PAIR0, 0, 1);
+			AD9081_ADC_PFIR_ADC_PAIR0, 0, 1);
 	if (ret != 0)
 		return ret;
 
 	ret  = adi_ad9081_adc_pfir_din_select_set(&phy->ad9081,
-		AD9081_ADC_PFIR_ADC_PAIR1, 3, 0);
+			AD9081_ADC_PFIR_ADC_PAIR1, 3, 0);
 	if (ret != 0)
 		return ret;
 