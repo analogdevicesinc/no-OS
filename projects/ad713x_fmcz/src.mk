9de0e41cf9a83c791906f909069e72bf31bc174f
Andrei Drimbarean <Andrei.Drimbarean@analog.com>
2021-08-24 13:20:40 +0300

    projects:ad713x_fmcz: add src.mk paths
    Add paths to the new PWM driver in the makefile sources.
    
    Signed-off-by: Andrei Drimbarean <Andrei.Drimbarean@analog.com>

--- projects/ad713x_fmcz/src.mk
+++ projects/ad713x_fmcz/src.mk
@@ -15,6 +15,7 @@ SRC_DIRS += $(DRIVERS)/adc/ad713x
 SRCS += $(DRIVERS)/spi/spi.c						\
 	$(DRIVERS)/gpio/gpio.c						\
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c				\
+	$(DRIVERS)/axi_core/axi_pwmgen/axi_pwm.c			\
 	$(DRIVERS)/axi_core/spi_engine/spi_engine.c			\
 	$(NO-OS)/util/util.c
 SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c					\
@@ -30,6 +31,7 @@ SRCS += $(PLATFORM_DRIVERS)/uart.c					\
 	$(NO-OS)/util/list.c	
 endif
 INCS += $(DRIVERS)/axi_core/axi_dmac/axi_dmac.h				\
+	$(DRIVERS)/axi_core/axi_pwmgen/axi_pwm_extra.h			\
 	$(DRIVERS)/axi_core/spi_engine/spi_engine.h			\
 	$(DRIVERS)/axi_core/spi_engine/spi_engine_private.h
 INCS +=	$(PLATFORM_DRIVERS)/spi_extra.h					\
@@ -43,6 +45,7 @@ INCS +=	$(INCLUDE)/axi_io.h						\
 	$(INCLUDE)/delay.h						\
 	$(INCLUDE)/irq.h						\
 	$(INCLUDE)/uart.h						\
+	$(INCLUDE)/pwm.h						\
 	$(INCLUDE)/util.h
 ifeq (y,$(strip $(TINYIIOD)))
 INCS += $(INCLUDE)/fifo.h						\
