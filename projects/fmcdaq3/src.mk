0f187d852d6a965c941df984ba99f2e4bfbba75c
APorumb <andrei.porumb@analog.com>
2021-09-09 16:36:24 +0300

    fmcdaq3 build fix using new irq_platform_ops

--- projects/fmcdaq3/src.mk
+++ projects/fmcdaq3/src.mk
@@ -41,8 +41,9 @@ SRCS += $(NO-OS)/util/fifo.c					\
 	$(DRIVERS)/axi_core/iio_axi_dac/iio_axi_dac.c		\
 	$(DRIVERS)/adc/ad9680/iio_ad9680.c				\
 	$(DRIVERS)/dac/ad9152/iio_ad9152.c				\
+	$(DRIVERS)/irq/irq.c						\
 	$(PLATFORM_DRIVERS)/uart.c					\
-	$(PLATFORM_DRIVERS)/irq.c
+	$(PLATFORM_DRIVERS)/$(PLATFORM)_irq.c
 endif
 INCS +=	$(PROJECT)/src/app/app_config.h					\
 	$(PROJECT)/src/devices/adi_hal/parameters.h
