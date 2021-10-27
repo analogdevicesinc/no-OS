98d758eebca2ff51f387ef06da3d0a4e382427cb
Antoniu Miclaus <antoniu.miclaus@analog.com>
2021-10-27 18:27:38 +0300

    drivers:ad463x: iio support
    Add initial IIO support for the ad463x driver.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

---
+++ drivers/adc/ad463x/iio_ad463x.c
@@ -0,0 +1,161 @@
+/***************************************************************************//**
+ *   @file   iio_ad463x.c
+ *   @brief  Implementation of iio_ad463x.c.
+ *   @author Antoniu Miclaus (antoniu.miclaus@analog.com)
+********************************************************************************
+ * Copyright 2021(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifdef IIO_SUPPORT
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+
+#include "ad463x.h"
+#include "iio_ad463x.h"
+#include "error.h"
+
+/******************************************************************************/
+/*************************** Types Declarations *******************************/
+/******************************************************************************/
+
+#define BITS_PER_SAMPLE 32
+
+static struct scan_type adc_scan_type = {
+	.sign = 'u',
+	.realbits = BITS_PER_SAMPLE,
+	.storagebits = BITS_PER_SAMPLE,
+	.shift = 0,
+	.is_big_endian = false
+};
+
+#define IIO_AD463x_CHANNEL(_idx) {\
+	.name = "ch" # _idx,\
+	.ch_type = IIO_VOLTAGE,\
+	.channel = _idx,\
+	.scan_index = _idx,\
+	.indexed = true,\
+	.scan_type = &adc_scan_type,\
+	.ch_out = false,\
+}
+
+static struct iio_channel iio_adc_channels[] = {
+	IIO_AD463x_CHANNEL(0),
+	IIO_AD463x_CHANNEL(1),
+};
+
+/******************************************************************************/
+/************************ Functions Definitions *******************************/
+/******************************************************************************/
+
+static int32_t _iio_ad463x_prepare_transfer(struct iio_ad463x *desc,
+		uint32_t mask)
+{
+	if (!desc)
+		return -EINVAL;
+
+	desc->mask = mask;
+
+	return SUCCESS;
+}
+
+static int32_t _iio_ad463x_read_dev(struct iio_ad463x *desc, uint32_t *buff,
+				    uint32_t nb_samples)
+{
+	int ret;
+	uint32_t data[nb_samples], i, j, ch;
+
+	if (!desc)
+		return -EINVAL;
+
+	ret = ad463x_read_data(desc->ad463x_desc, data, nb_samples);
+	if(ret)
+		return ret;
+
+	for(i = 0, j = 0; i < nb_samples; i++)
+		for (ch = 0; ch < desc->iio_dev_desc.num_ch; ch++)
+			if (desc->mask & BIT(ch)) {
+				buff[j++] = data[i];
+			}
+
+	return nb_samples;
+}
+
+/**
+ * @brief Init for reading/writing and parameterization of a
+ * ad463x device.
+ * @param desc - Descriptor.
+ * @param param - Configuration structure.
+ * @return SUCCESS in case of success, FAILURE otherwise.
+ */
+int32_t iio_ad463x_init(struct iio_ad463x **desc,
+			struct ad463x_dev *dev)
+{
+	struct iio_ad463x *iio_ad463x;
+
+	iio_ad463x = (struct iio_ad463x *)calloc(1, sizeof(struct iio_ad463x));
+	if (!iio_ad463x)
+		return FAILURE;
+
+	iio_ad463x->ad463x_desc = dev;
+	iio_ad463x->iio_dev_desc = ad463x_iio_desc;
+
+	*desc = iio_ad463x;
+
+	return SUCCESS;
+}
+
+/**
+ * @brief Release resources.
+ * @param desc - Descriptor.
+ * @return SUCCESS in case of success, FAILURE otherwise.
+ */
+int32_t iio_ad463x_remove(struct iio_ad463x *desc)
+{
+	if (!desc)
+		return FAILURE;
+
+	free(desc);
+
+	return SUCCESS;
+}
+
+struct iio_device ad463x_iio_desc = {
+	.channels = iio_adc_channels,
+	.num_ch = 2,
+	.prepare_transfer = (int32_t (*)())_iio_ad463x_prepare_transfer,
+	.read_dev = (int32_t (*)())_iio_ad463x_read_dev
+};
+
+#endif /* IIO_SUPPORT */
---
+++ drivers/adc/ad463x/iio_ad463x.h
@@ -0,0 +1,80 @@
+/***************************************************************************//**
+*   @file   iio_ad463x.h
+*   @brief  Header file of iio_ad463x
+*   @author Antoniu Miclaus (antoniu.miclaus@analog.com)
+********************************************************************************
+* Copyright 2021(c) Analog Devices, Inc.
+*
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*  - Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+*  - Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+*  - Neither the name of Analog Devices, Inc. nor the names of its
+*    contributors may be used to endorse or promote products derived
+*    from this software without specific prior written permission.
+*  - The use of this software may or may not infringe the patent rights
+*    of one or more patent holders.  This license does not release you
+*    from the requirement that you obtain separate licenses from these
+*    patent holders to use this software.
+*  - Use of the software either in source or binary form, must be run
+*    on or directly connected to an Analog Devices Inc. component.
+*
+* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+* IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef IIO_AD463x
+#define IIO_AD463x
+
+#ifdef IIO_SUPPORT
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+
+#include <stdio.h>
+#include "iio_types.h"
+#include "spi.h"
+
+/******************************************************************************/
+/*************************** Types Declarations *******************************/
+/******************************************************************************/
+
+struct iio_ad463x {
+	/* Mask of active ch */
+	uint32_t mask;
+	/** iio device descriptor */
+	struct iio_device iio_dev_desc;
+	/** Device Descriptor */
+	struct ad463x_dev *ad463x_desc;
+};
+
+extern struct iio_device ad463x_iio_desc;
+
+/******************************************************************************/
+/************************ Functions Declarations ******************************/
+/******************************************************************************/
+
+/* Init function. */
+int32_t iio_ad463x_init(struct iio_ad463x **desc,
+			struct ad463x_dev *dev);
+
+/* Free the resources allocated by iio_ad463x_init(). */
+int32_t iio_ad463x_remove(struct iio_ad463x *desc);
+
+#endif /* IIO_SUPPORT */
+
+#endif /* IIO_AD463x */
