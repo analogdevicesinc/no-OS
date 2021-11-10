58b6939ed2a74d16879a629a37514f3ce631943e
Antoniu Miclaus <antoniu.miclaus@analog.com>
2021-11-10 11:04:15 +0200

    drivers:afe:ad4110: format code
    Update driver to the latest code formatting standard.
    
    No functional changes.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- drivers/afe/ad4110/ad4110.c
+++ drivers/afe/ad4110/ad4110.c
@@ -1,3 +1,44 @@
+/***************************************************************************//**
+ *   @file   ad4110.c
+ *   @brief  Implementation of AD4110 Driver.
+ *   @author Stefan Popa (stefan.popa@analog.com)
+ * 	     Andrei Porump (andrei.porumb@analog.com)
+ * 	     Mihail Chindris (mihail.chindris@analog.com)
+********************************************************************************
+ * Copyright 2021(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
 /******************************************************************************/
 /***************************** Include Files **********************************/
 /******************************************************************************/
@@ -110,9 +151,9 @@ int32_t ad4110_spi_int_reg_write_msk(struct ad4110_dev *dev,
 int32_t ad4110_set_adc_mode(struct ad4110_dev *dev, enum ad4110_adc_mode mode)
 {
 	if(mode == AD4110_SYS_OFFSET_CAL)
-		printf("Assuming that the applied analog input is the zero scale point. \n");
+		pr_info("Assuming that the applied analog input is the zero scale point. \n");
 	else if(mode == AD4110_SYS_GAIN_CAL)
-		printf("Assuming that the applied analog input is the full scale point. \n");
+		pr_info("Assuming that the applied analog input is the full scale point. \n");
 
 	return ad4110_spi_int_reg_write_msk(dev,
 					    A4110_ADC,
@@ -663,8 +704,9 @@ static void irq_adc_read(struct ad4110_callback_ctx *ctx)
 		ctx->buffer++;
 		if(irq_enable(dev->irq_desc, dev->nready_pin))
 			pr_err("IRQ_enable error \n");
-	} else if(irq_disable(dev->irq_desc, dev->nready_pin))
+	} else if(irq_disable(dev->irq_desc, dev->nready_pin)) {
 		pr_err("IRQ_disable error \n");
+	}
 }
 
 /***************************************************************************//**
@@ -683,7 +725,7 @@ int32_t ad4110_setup(struct ad4110_dev **device,
 	int32_t ret;
 	dev = (struct ad4110_dev *)malloc(sizeof(*dev));
 	if (!dev)
-		return -1;
+		return FAILURE;
 
 	/* SPI */
 	ret = spi_init(&dev->spi_dev, &init_param.spi_init);
@@ -790,7 +832,7 @@ int32_t ad4110_setup(struct ad4110_dev **device,
 
 	*device = dev;
 
-	printf("AD4110 successfully initialized\n");
+	pr_info("AD4110 successfully initialized\n");
 	return SUCCESS;
 
 err_spi:
