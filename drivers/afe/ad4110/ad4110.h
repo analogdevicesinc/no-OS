18cee031137c432e1d653ea6f0f8bce6580a1d95
Antoniu Miclaus <antoniu.miclaus@analog.com>
2021-11-10 11:04:15 +0200

    drivers:afe:ad4110: format code
    Update driver to the latest code formatting standard.
    
    No functional changes.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- drivers/afe/ad4110/ad4110.h
+++ drivers/afe/ad4110/ad4110.h
@@ -1,3 +1,44 @@
+/***************************************************************************//**
+ *   @file   ad4110.h
+ *   @brief  Header file of AD4110 Driver.
+ *   @author Stefan Popa (stefan.popa@analog.com)
+ * 	     Andrei Porump (andrei.porumb@analog.com)
+ * 	     Mihail Chindris (mihail.chindris@analog.com)
+********************************************************************************
+ * Copyright 2021(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
 #ifndef AD4110_H_
 #define AD4110_H_
 
@@ -13,7 +54,6 @@
 /******************************************************************************/
 /********************** Macros and Constants Definitions **********************/
 /******************************************************************************/
-
 #define AD4110_CMD_WR_COM_REG(x)   (0x00 | ((x) & 0xF)) // Write to Register x
 #define AD4110_CMD_READ_COM_REG(x) (0x40 | ((x) & 0xF)) // Read from Register x
 #define AD4110_DEV_ADDR_MASK	   (0x30) // Device address mask
@@ -23,7 +63,6 @@
 #define A4110_AFE 0x01
 
 /****************************** AFE Register Map ******************************/
-
 #define AD4110_REG_AFE_TOP_STATUS 	     0x0
 #define AD4110_REG_AFE_CNTRL1		     0x1
 #define AD4110_REG_AFE_CLK_CTRL		     0x3
@@ -36,9 +75,7 @@
 #define AD4110_REG_AFE_NO_PWR_DEFAULT_SEL    0xE
 #define AD4110_REG_AFE_NO_PWR_DEFAULT_STATUS 0xF
 
-
 /***************************** ADC Register Map *******************************/
-
 #define AD4110_REG_ADC_STATUS 			0x0
 #define AD4110_REG_ADC_MODE			0x1
 #define AD4110_REG_ADC_INTERFACE		0x2
@@ -142,7 +179,6 @@
 /******************************************************************************/
 /*************************** Types Declarations *******************************/
 /******************************************************************************/
-
 /* If AD4110_AFE_ADC_CLOCKED selected, select AD4110_ADC_INT_CLK_CLKIO */
 enum ad4110_adc_clk_sel {
 	AD4110_ADC_INT_CLK,
