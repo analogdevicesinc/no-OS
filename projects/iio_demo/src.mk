analogdevicesinc/no-OS

commit 195fbc0d8f033eeaa0e078a74dd82926f6fe5bd5

drivers: adc_demo&dac_demo: Add buffers Add buffer for communication between adc&dac. Add default values to load in adc if used separately. Make number of device channels configurable from main.
Signed-off-by: Ramona Nechita <Ramona.Nechita@analog.com>

projects/iio_demo/src.mk
@@ -8,12 +8,23 @@ SRCS +=	$(NO-OS)/util/xml.c						\
 	$(NO-OS)/util/fifo.c						\
 	$(NO-OS)/util/util.c
 
+#drivers
+SRCS += $(DRIVERS)/adc/adc_demo/adc_demo.c				\
+		$(DRIVERS)/dac/dac_demo/dac_demo.c				\
+		$(DRIVERS)/dac/dac_demo/iio_dac_demo.c				\
+		$(DRIVERS)/adc/adc_demo/iio_adc_demo.c				\
+
 INCS += $(INCLUDE)/xml.h						\
 	$(INCLUDE)/fifo.h						\
 	$(INCLUDE)/uart.h						\
 	$(INCLUDE)/list.h						\
 	$(INCLUDE)/util.h						\
 	$(INCLUDE)/error.h
+
+INCS += $(DRIVERS)/adc/adc_demo/iio_adc_demo.h			\
+		$(DRIVERS)/dac/dac_demo/dac_demo.h		\
+		$(DRIVERS)/dac/dac_demo/iio_dac_demo.h			\
+		$(DRIVERS)/adc/adc_demo/adc_demo.h		\
 
 ifeq ($(PLATFORM),$(filter $(PLATFORM),xilinx aducm3029))
 # For the moment there is support only for aducm for iio with network backend
@@ -75,4 +86,3 @@ INCS += $(PROJECT)/src/hal/stm32/main.h \
 
 LSCRIPT = $(PROJECT)/src/linker/STM32F446RETX_FLASH.ld
 endif
-

drivers/adc/adc_demo/adc_demo.c
@@ -41,10 +41,33 @@
 /***************************** Include Files **********************************/
 /******************************************************************************/
 
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include "iio_types.h"
 #include "adc_demo.h"
 #include "error.h"
 #include "util.h"
+
+/* Default sine values if the user wants to use only ADC*/
+const uint16_t sine_lut[128] = {
+	0x000, 0x064, 0x0C8, 0x12C, 0x18F, 0x1F1, 0x252, 0x2B1,
+	0x30F, 0x36B, 0x3C5, 0x41C, 0x471, 0x4C3, 0x512, 0x55F,
+	0x5A7, 0x5ED, 0x62E, 0x66C, 0x6A6, 0x6DC, 0x70D, 0x73A,
+	0x763, 0x787, 0x7A7, 0x7C2, 0x7D8, 0x7E9, 0x7F5, 0x7FD,
+	0x7FF, 0x7FD, 0x7F5, 0x7E9, 0x7D8, 0x7C2, 0x7A7, 0x787,
+	0x763, 0x73A, 0x70D, 0x6DC, 0x6A6, 0x66C, 0x62E, 0x5ED,
+	0x5A7, 0x55F, 0x512, 0x4C3, 0x471, 0x41C, 0x3C5, 0x36B,
+	0x30F, 0x2B1, 0x252, 0x1F1, 0x18F, 0x12C, 0xC8,  0x64,
+	0x000, 0xF9B, 0xF37, 0xED3, 0xE70, 0xE0E, 0xDAD, 0xD4E,
+	0xCF0, 0xC94, 0xC3A, 0xBE3, 0xB8E, 0xB3C, 0xAED, 0xAA0,
+	0xA58, 0xA12, 0x9D1, 0x993, 0x959, 0x923, 0x8F2, 0x8C5,
+	0x89C, 0x878, 0x858, 0x83D, 0x827, 0x816, 0x80A, 0x802,
+	0x800, 0x802, 0x80A, 0x816, 0x827, 0x83D, 0x858, 0x878,
+	0x89C, 0x8C5, 0x8F2, 0x923, 0x959, 0x993, 0x9D1, 0xA12,
+	0xA58, 0xAA0, 0xAED, 0xB3C, 0xB8E, 0xBE3, 0xC3A, 0xC94,
+	0xCF0, 0xD4E, 0xDAD, 0xE0E, 0xE70, 0xED3, 0xF37, 0xF9B
+};
 
 /******************************************************************************/
 /************************ Functions Definitions *******************************/
@@ -65,6 +88,11 @@ int32_t adc_demo_init(struct adc_demo_desc **desc,
 	if(!adesc)
 		return -ENOMEM;
 
+	adesc->loopback_buffers = param->loopback_buffers;
+	adesc->active_ch = param->channel_no;
+	for(int i = 0; i < adesc->active_ch; i++)
+		adesc->adc_ch_attr[i] = param->dev_ch_attr[i];
+	adesc->adc_global_attr = param->dev_global_attr;
 	*desc = adesc;
 
 	return SUCCESS;
@@ -85,6 +113,166 @@ int32_t adc_demo_remove(struct adc_demo_desc *desc)
 	return SUCCESS;
 }
 
+int32_t update_adc_channels(void *dev, uint32_t mask)
+{
+	struct adc_demo_desc *desc;
+
+	if(!dev)
+		return -ENODEV;
+
+	desc = dev;
+
+	desc->active_ch = mask;
+	/* If a real device. Here needs to be selected the channels to be read*/
+
+	return SUCCESS;
+}
+
+/*
+* @brief close all channels
+* @param dev - physical instance of an adc device
+* @return SUCCESS in case of success.
+******************************************************************************/
+int32_t close_adc_channels(void* dev)
+{
+	struct adc_demo_desc *desc;
+
+	if(!dev)
+		return -ENODEV;
+
+	desc = dev;
+
+	desc->active_ch = 0;
+
+	return SUCCESS;
+}
+
+/***************************************************************************//**
+ * @brief utility function for computing next upcoming channel
+ * @param ch_mask - active channels .
+ * @param last_idx -  previous index.
+ * @param new_idx - upcoming channel index, return param.
+ * @return 1 if there are more channels, 0 if done.
+*******************************************************************************/
+static bool get_next_ch_idx(uint32_t ch_mask, uint32_t last_idx,
+			    uint32_t *new_idx)
+{
+	last_idx++;
+	ch_mask >>= last_idx;
+	if (!ch_mask) {
+		*new_idx = -1;
+		return 0;
+	}
+	while (!(ch_mask & 1)) {
+		last_idx++;
+		ch_mask >>= 1;
+	}
+	*new_idx = last_idx;
+
+	return 1;
+}
+
+/***************************************************************************//**
+ * @brief function for reading samples
+ * @param dev - physical instance of adc device
+ * @param buff - buffer for reading samples
+ * @param samples - number of samples to receive
+ * @return the number of samples.
+*******************************************************************************/
+int32_t adc_read_samples(void* dev, uint16_t* buff, uint32_t samples)
+{
+	struct adc_demo_desc *desc;
+	uint32_t k = 0;
+	uint32_t ch = -1;
+
+	if(!dev)
+		return -ENODEV;
+
+	desc = dev;
+
+	if(desc->loopback_buffers == NULL) {
+		//default sin function
+		int offset_per_ch = ARRAY_SIZE(sine_lut) / TOTAL_CHANNEL_NO;
+		for(int i = 0; i < samples; i++) {
+			while(get_next_ch_idx(desc->active_ch, ch, &ch))
+				buff[k++] = sine_lut[(i + ch * offset_per_ch ) % ARRAY_SIZE(sine_lut)];
+		}
+
+		return samples;
+	}
+
+	for(int i = 0; i < samples; i++) {
+		while(get_next_ch_idx(desc->active_ch, ch, &ch))
+			buff[k++] = desc->loopback_buffers[ch][i % DEFAULT_LOCAL_SAMPLES];
+	}
+	return samples;
+}
+
+/**
+ * @brief get attributes for adc.
+ * @param device- Physical instance of a iio_demo_device.
+ * @param buf - Where value is stored.
+ * @param len - Maximum length of value to be stored in buf.
+ * @param channel - Channel properties.
+ * @param attr_id - Attribute ID
+ * @return Length of chars written in buf, or negative value on failure.
+ */
+ssize_t get_adc_demo_attr(void *device, char *buf, size_t len,
+			  const struct iio_ch_info *channel, intptr_t attr_id)
+{
+	struct adc_demo_desc *desc;
+
+	if(!device)
+		return -ENODEV;
+
+	desc = device;
+
+	switch(attr_id) {
+	case ADC_GLOBAL_ATTR:
+		return snprintf(buf,len,"%"PRIu32"",desc->adc_global_attr);
+	case ADC_CHANNEL_ATTR:
+		return snprintf(buf,len,"%"PRIu32"",desc->adc_ch_attr[channel->ch_num]);
+	default:
+		return -EINVAL;
+	}
+
+	return -EINVAL;
+}
+
+/**
+ * @brief set attributes for adc.
+ * @param device - Physical instance of a iio_demo_device.
+ * @param buf - Value to be written to attribute.
+ * @param len -	Length of the data in "buf".
+ * @param channel - Channel properties.
+ * @param attr_id - Attribute ID
+ * @return: Number of bytes written to device, or negative value on failure.
+ */
+ssize_t set_adc_demo_attr(void *device, char *buf, size_t len,
+			  const struct iio_ch_info *channel, intptr_t attr_id)
+{
+	struct adc_demo_desc *desc;
+	uint32_t value = srt_to_uint32(buf);
+
+	if(!device)
+		return -ENODEV;
+
+	desc = device;
+
+	switch(attr_id) {
+	case ADC_GLOBAL_ATTR:
+		desc->adc_global_attr = value;
+		return len;
+	case ADC_CHANNEL_ATTR:
+		desc->adc_ch_attr[channel->ch_num] = value;
+		return len;
+	default:
+		return -EINVAL;
+	}
+
+	return -EINVAL;
+}
+
 /***************************************************************************//**
  * @brief read function for the adc demo driver
  * @param desc - descriptor for the adc
@@ -98,7 +286,7 @@ int32_t adc_demo_reg_read(struct adc_demo_desc *desc, uint8_t reg_index,
 	if(!desc || reg_index >= ARRAY_SIZE(desc->reg))
 		return -EINVAL;
 
-	*readval =  desc->reg[reg_index];
+	*readval = desc->reg[reg_index];
 
 	return SUCCESS;
 }

drivers/adc/adc_demo/adc_demo.h
@@ -50,15 +50,24 @@
 /*************************** Types Declarations *******************************/
 /******************************************************************************/
 
-#define MAX_REG_ADDR		16
-
+#define MAX_ADC_ADDR		16
+#define DEFAULT_LOCAL_SAMPLES 128
+#define TOTAL_CHANNEL_NO 16
 /**
  * @struct iio_demo_adc_desc
  * @brief Desciptor.
  */
 struct adc_demo_desc {
 	/** Dummy registers of device for testing */
-	uint8_t reg[MAX_REG_ADDR];
+	uint8_t reg[MAX_ADC_ADDR];
+	/** Demo global device attribute */
+	uint32_t adc_global_attr;
+	/** Demo device channel attribute */
+	uint32_t adc_ch_attr[TOTAL_CHANNEL_NO];
+	/** Active channel**/
+	uint32_t active_ch;
+	/** Array of buffers for each channel*/
+	uint16_t **loopback_buffers;
 };
 
 /**
@@ -66,6 +75,19 @@ struct adc_demo_desc {
  * @brief iio demo adc configuration.
  */
 struct adc_demo_init_param {
+	/** Demo global dac attribute */
+	uint32_t dev_global_attr;
+	/** Demo dac channel attribute */
+	uint32_t dev_ch_attr[TOTAL_CHANNEL_NO];
+	/* NUmber of channels*/
+	uint32_t channel_no;
+	/**Array of buffers for each channel*/
+	uint16_t **loopback_buffers;
+};
+
+enum iio_adc_demo_attributes {
+	ADC_CHANNEL_ATTR,
+	ADC_GLOBAL_ATTR,
 };
 
 /******************************************************************************/
@@ -76,6 +98,18 @@ int32_t adc_demo_init(struct adc_demo_desc **desc,
 		      struct adc_demo_init_param *param);
 
 int32_t adc_demo_remove(struct adc_demo_desc *desc);
+
+ssize_t get_adc_demo_attr(void *device, char *buf, size_t len,
+			  const struct iio_ch_info *channel, intptr_t attr_id);
+
+ssize_t set_adc_demo_attr(void *device, char *buf, size_t len,
+			  const struct iio_ch_info *channel, intptr_t attr_id);
+
+int32_t update_adc_channels(void *dev, uint32_t mask);
+
+int32_t close_adc_channels(void* dev);
+
+int32_t adc_read_samples(void* dev, uint16_t* buff, uint32_t samples);
 
 int32_t adc_demo_reg_read(struct adc_demo_desc *desc, uint8_t reg_index,
 			  uint8_t *readval);

drivers/adc/adc_demo/iio_adc_demo.h
@@ -42,10 +42,84 @@
 
 #include <stdlib.h>
 #include "adc_demo.h"
+#include "iio_types.h"
 
-struct iio_device adc_demo_iio_descriptor = {
-	.debug_reg_read = (int32_t (*)()) adc_demo_reg_read,
-	.debug_reg_write = (int32_t (*)()) adc_demo_reg_write
+#define DEFAULT_CHANNEL_NO 2
+#define ADC_CHANNEL_NO changeable_channel_number
+#define MAX_SAMPLES_PER_CHANNEL 800
+
+static int changeable_channel_number = 2;
+
+#define ADC_DEMO_ATTR(_name, _priv) {\
+	.name = _name,\
+	.priv = _priv,\
+	.show = get_adc_demo_attr,\
+	.store = set_adc_demo_attr \
+}
+
+static struct scan_type adc_scan_type = {
+	.sign = 's',
+	.realbits = 16,
+	.storagebits = 16,
+	.shift = 0,
+	.is_big_endian = false
 };
+
+static struct iio_attribute adc_channel_attributes[] = {
+	ADC_DEMO_ATTR("adc_channel_attr", ADC_CHANNEL_ATTR),
+	END_ATTRIBUTES_ARRAY,
+};
+
+static struct iio_attribute iio_adc_global_attributes[] = {
+	ADC_DEMO_ATTR("adc_global_attr", ADC_GLOBAL_ATTR),
+	END_ATTRIBUTES_ARRAY,
+};
+
+#define IIO_DEMO_ADC_CHANNEL(_idx) {\
+	.name = "adc_in_ch",\
+	.ch_type = IIO_VOLTAGE,\
+	.channel = _idx,\
+	.scan_index = _idx,\
+	.indexed = true,\
+	.scan_type = &adc_scan_type,\
+	.attributes = adc_channel_attributes,\
+	.ch_out = false,\
+}
+
+static struct iio_channel iio_adc_channels[] = {
+	IIO_DEMO_ADC_CHANNEL(0),
+	IIO_DEMO_ADC_CHANNEL(1),
+#ifdef MODIFIED_CHANNEL_VALUE
+	IIO_DEMO_ADC_CHANNEL(2),
+	IIO_DEMO_ADC_CHANNEL(3),
+	IIO_DEMO_ADC_CHANNEL(4),
+	IIO_DEMO_ADC_CHANNEL(5),
+	IIO_DEMO_ADC_CHANNEL(6),
+	IIO_DEMO_ADC_CHANNEL(7),
+	IIO_DEMO_ADC_CHANNEL(8),
+	IIO_DEMO_ADC_CHANNEL(9),
+	IIO_DEMO_ADC_CHANNEL(10),
+	IIO_DEMO_ADC_CHANNEL(11),
+	IIO_DEMO_ADC_CHANNEL(12),
+	IIO_DEMO_ADC_CHANNEL(13),
+	IIO_DEMO_ADC_CHANNEL(14),
+	IIO_DEMO_ADC_CHANNEL(15),
+#endif
+};
+
+#define ADC_DEMO_DEV(_numch) {\
+	.num_ch = _numch, \
+	.channels = iio_adc_channels, \
+	.attributes = iio_adc_global_attributes,	\
+	.debug_attributes = NULL,	\
+	.buffer_attributes = NULL,	\
+	.prepare_transfer = update_adc_channels,	\
+	.end_transfer = close_adc_channels,	\
+	.read_dev = (int32_t (*)())adc_read_samples,	\
+	.debug_reg_read = (int32_t (*)()) adc_demo_reg_read,	\
+	.debug_reg_write = (int32_t (*)()) adc_demo_reg_write	\
+}
+
+//static struct iio_device adc_demo_iio_descriptor = ADC_DEMO_DEV(MODIFIED_CHANNEL_VALUE);
 
 #endif /* IIO_DEMO_ADC */

drivers/dac/dac_demo/dac_demo.c
@@ -41,7 +41,10 @@
 /***************************** Include Files **********************************/
 /******************************************************************************/
 
-#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include "iio_types.h"
 #include "dac_demo.h"
 #include "error.h"
 #include "util.h"
@@ -65,16 +68,20 @@ int32_t dac_demo_init(struct dac_demo_desc **desc,
 	if(!adesc)
 		return -ENOMEM;
 
+	adesc->loopback_buffers = param->loopback_buffers;
+	adesc->active_ch = param->channel_no;
+	for(int i = 0; i < adesc->active_ch; i++)
+		adesc->dac_ch_attr[i] = param->dev_ch_attr[i];
+	adesc->dac_global_attr = param->dev_global_attr;
 	*desc = adesc;
-
 	return SUCCESS;
 }
 
-/***************************************************************************//**
+/****************************************************************************//**
  * @brief free allocated resources
  * @param desc - descriptor for the dac to be removed
  * @return SUCCESS in case of success, FAILURE otherwise.
-*******************************************************************************/
+******************************************************************************/
 int32_t dac_demo_remove(struct dac_demo_desc *desc)
 {
 	if(!desc)
@@ -86,6 +93,157 @@ int32_t dac_demo_remove(struct dac_demo_desc *desc)
 }
 
 /***************************************************************************//**
+ * @brief update number of active channels
+ * @param dev - physical instance of a dac device
+ * @param mask - the new number of active channels
+ * @return SUCCESS in case of success.
+*******************************************************************************/
+int32_t update_dac_channels(void *dev, int32_t mask)
+{
+	struct dac_demo_desc *desc;
+	if(!dev)
+		return -ENODEV;
+
+	desc = dev;
+
+	desc->active_ch = mask;
+
+	return SUCCESS;
+}
+
+/**********************************************************************//**
+ * @brief close all channels
+ * @param dev - physical instance of an adc device
+ * @return SUCCESS in case of success.
+**************************************************************************/
+int32_t close_dac_channels(void* dev)
+{
+	struct dac_demo_desc *desc;
+	if(!dev)
+		return -ENODEV;
+
+	desc = dev;
+
+	desc->active_ch = 0;
+
+	return SUCCESS;
+}
+
+/*********************************************************************//**
+ * @brief utility function for computing next upcoming channel
+ * @param ch_mask - active channels .
+ * @param last_idx -  previous index.
+ * @param new_idx - upcoming channel index, return param.
+ * @return 1 if there are more channels, 0 if done.
+**************************************************************************/
+static bool get_next_ch_idx(uint32_t ch_mask, uint32_t last_idx,
+			    uint32_t *new_idx)
+{
+	last_idx++;
+	ch_mask >>= last_idx;
+	if (!ch_mask) {
+		*new_idx = -1;
+		return 0;
+	}
+	while (!(ch_mask & 1)) {
+		last_idx++;
+		ch_mask >>= 1;
+	}
+	*new_idx = last_idx;
+
+	return 1;
+}
+
+/**********************************************************************//**
+ * @brief function for reading samples
+ * @param dev - physical instance of adc device
+ * @param buff - buffer for reading samples
+ * @param samples - number of samples to receive
+ * @return the number of samples.
+**************************************************************************/
+int32_t dac_write_samples(void* dev, uint16_t* buff, uint32_t samples)
+{
+	struct dac_demo_desc *desc;
+	uint32_t k = 0;
+	uint32_t ch = -1;
+
+	if(!dev)
+		return -ENODEV;
+
+	desc = dev;
+
+	for(int i = 0; i < samples; i++)
+		while (get_next_ch_idx(desc->active_ch, ch, &ch))
+			desc->loopback_buffers[ch][i % DEFAULT_LOCAL_SAMPLES] = buff[k++];
+
+	return samples;
+}
+
+/**
+ * @brief get attributes for dac.
+ * @param device- Physical instance of a iio_demo_device.
+ * @param buf - Where value is stored.
+ * @param len - Maximum length of value to be stored in buf.
+ * @param channel - Channel properties.
+ * @param attr_id - Attribute ID
+ * @return Length of chars written in buf, or negative value on failure.
+ */
+ssize_t get_dac_demo_attr(void *device, char *buf, size_t len,
+			  const struct iio_ch_info *channel, intptr_t attr_id)
+{
+	struct dac_demo_desc *desc;
+
+	if(!device)
+		return -ENODEV;
+
+	desc = device;
+
+	switch(attr_id) {
+	case DAC_GLOBAL_ATTR:
+		return snprintf(buf,len,"%"PRIu32"",desc->dac_global_attr);
+	case DAC_CHANNEL_ATTR:
+		return snprintf(buf,len,"%"PRIu32"",desc->dac_ch_attr[channel->ch_num]);
+	default:
+		return -EINVAL;
+	}
+
+	return -EINVAL;
+}
+
+/**
+ * @brief set attributes for dac.
+ * @param device - Physical instance of a iio_demo_device.
+ * @param buf - Value to be written to attribute.
+ * @param len -	Length of the data in "buf".
+ * @param channel - Channel properties.
+ * @param attr_id - Attribute ID
+ * @return: Number of bytes written to device, or negative value on failure.
+ */
+ssize_t set_dac_demo_attr(void *device, char *buf, size_t len,
+			  const struct iio_ch_info *channel, intptr_t attr_id)
+{
+	struct dac_demo_desc *desc;
+	uint32_t value = srt_to_uint32(buf);
+
+	if(!device)
+		return -ENODEV;
+
+	desc = device;
+
+	switch(attr_id) {
+	case DAC_GLOBAL_ATTR:
+		desc->dac_global_attr = value;
+		return len;
+	case DAC_CHANNEL_ATTR:
+		desc->dac_ch_attr[channel->ch_num] = value;
+		return len;
+	default:
+		return -EINVAL;
+	}
+
+	return -EINVAL;
+}
+/**********************************************************************//**
  * @brief read function for the dac demo driver
  * @param desc - descriptor for the dac
  * @param reg_index - the address at which we want to read

drivers/dac/dac_demo/dac_demo.h
@@ -50,7 +50,9 @@
 /*************************** Types Declarations *******************************/
 /******************************************************************************/
 
-#define MAX_REG_ADDR		16
+#define MAX_DAC_ADDR		16
+#define DEFAULT_LOCAL_SAMPLES 128
+#define TOTAL_DAC_CHANNELS 16
 
 /**
  * @struct iio_demo_dac_desc
@@ -58,7 +60,15 @@
  */
 struct dac_demo_desc {
 	/** Dummy registers of device for testing */
-	uint8_t reg[MAX_REG_ADDR];
+	uint8_t reg[MAX_DAC_ADDR];
+	/** Demo global device attribute */
+	uint32_t dac_global_attr;
+	/** Demo device channel attribute */
+	uint32_t dac_ch_attr[TOTAL_DAC_CHANNELS];
+	/** Active channel**/
+	uint32_t active_ch;
+	/** Array of buffers for each channel*/
+	uint16_t **loopback_buffers;
 };
 
 /**
@@ -66,6 +76,19 @@ struct dac_demo_desc {
  * @brief iio demo dac configuration.
  */
 struct dac_demo_init_param {
+	/** Demo global dac attribute */
+	uint32_t dev_global_attr;
+	/** Demo dac channel attribute */
+	uint32_t dev_ch_attr[TOTAL_DAC_CHANNELS];
+	/** Buffer for adc/dac communication*/
+	uint16_t **loopback_buffers;
+	/* initializing number of channels*/
+	uint32_t channel_no;
+};
+
+enum iio_dac_demo_attributes {
+	DAC_CHANNEL_ATTR,
+	DAC_GLOBAL_ATTR,
 };
 
 /******************************************************************************/
@@ -76,6 +99,18 @@ int32_t dac_demo_init(struct dac_demo_desc **desc,
 		      struct dac_demo_init_param *param);
 
 int32_t dac_demo_remove(struct dac_demo_desc *desc);
+
+int32_t update_dac_channels(void *dev, int32_t mask);
+
+int32_t close_dac_channels(void* dev);
+
+int32_t dac_write_samples(void* dev, uint16_t* buff, uint32_t samples);
+
+ssize_t get_dac_demo_attr(void *device, char *buf, size_t len,
+			  const struct iio_ch_info *channel, intptr_t priv);
+
+ssize_t set_dac_demo_attr(void *device, char *buf, size_t len,
+			  const struct iio_ch_info *channel, intptr_t priv);
 
 int32_t dac_demo_reg_read(struct dac_demo_desc *desc, uint8_t reg_index,
 			  uint8_t *readval);

drivers/dac/dac_demo/iio_dac_demo.h
@@ -42,10 +42,84 @@
 
 #include <stdlib.h>
 #include "dac_demo.h"
+#include "iio.h"
 
-struct iio_device dac_demo_iio_descriptor = {
-	.debug_reg_read = (int32_t (*)()) dac_demo_reg_read,
-	.debug_reg_write = (int32_t (*)()) dac_demo_reg_write
+#define MAX_NR_CHANNELS 10
+#define MAX_NR_ATTRIBUTES 5
+#define DAC_CHANNEL_NO changeable_dac_number
+
+static int changeable_dac_number = 2;
+
+#define DAC_DEMO_ATTR(_name, _priv) {\
+	.name = _name,\
+	.priv = _priv,\
+	.show = get_dac_demo_attr,\
+	.store = set_dac_demo_attr\
+}
+
+#define IIO_DEMO_DAC_CHANNEL(_idx) {\
+	.name = "dac_out_ch",\
+	.ch_type = IIO_VOLTAGE,\
+	.channel = _idx,\
+	.scan_index = _idx,\
+	.indexed = true,\
+	.scan_type = &dac_scan_type,\
+	.attributes = dac_channel_attributes,\
+	.ch_out = true,\
+}
+
+#define DAC_DEMO_DEV(_numch) {\
+	.num_ch = _numch, \
+	.channels = iio_dac_channels, \
+	.attributes = dac_global_attributes,	\
+	.debug_attributes = NULL,	\
+	.buffer_attributes = NULL,	\
+	.prepare_transfer = update_dac_channels,	\
+	.end_transfer = close_dac_channels,	\
+	.read_dev = (int32_t (*)())dac_write_samples,	\
+	.debug_reg_read = (int32_t (*)()) dac_demo_reg_read,	\
+	.debug_reg_write = (int32_t (*)()) dac_demo_reg_write	\
+}
+
+static struct scan_type dac_scan_type = {
+	.sign = 's',
+	.realbits = 16,
+	.storagebits = 16,
+	.shift = 0,
+	.is_big_endian = false
 };
+
+static struct iio_attribute dac_channel_attributes[] = {
+	DAC_DEMO_ATTR("dac_channel_attr", DAC_CHANNEL_ATTR),
+	END_ATTRIBUTES_ARRAY,
+};
+
+static struct iio_attribute dac_global_attributes[] = {
+	DAC_DEMO_ATTR("dac_global_attr", DAC_GLOBAL_ATTR),
+	END_ATTRIBUTES_ARRAY,
+};
+
+static struct iio_channel iio_dac_channels[] = {
+	IIO_DEMO_DAC_CHANNEL(0),
+	IIO_DEMO_DAC_CHANNEL(1),
+	IIO_DEMO_DAC_CHANNEL(2),
+#ifdef DAC_CHANNEL_NO
+	IIO_DEMO_DAC_CHANNEL(3),
+	IIO_DEMO_DAC_CHANNEL(4),
+	IIO_DEMO_DAC_CHANNEL(5),
+	IIO_DEMO_DAC_CHANNEL(6),
+	IIO_DEMO_DAC_CHANNEL(7),
+	IIO_DEMO_DAC_CHANNEL(8),
+	IIO_DEMO_DAC_CHANNEL(9),
+	IIO_DEMO_DAC_CHANNEL(10),
+	IIO_DEMO_DAC_CHANNEL(11),
+	IIO_DEMO_DAC_CHANNEL(12),
+	IIO_DEMO_DAC_CHANNEL(13),
+	IIO_DEMO_DAC_CHANNEL(14),
+	IIO_DEMO_DAC_CHANNEL(15)
+#endif
+};
+
+//static struct iio_device dac_demo_iio_descriptor = DAC_DEMO_DEV(2);
 
 #endif /* IIO_DEMO_DAC */

projects/iio_demo/src/app/main.c
@@ -46,6 +46,9 @@
 #include "app_config.h"
 #include "parameters.h"
 #include "iio_app.h"
+#include "iio_adc_demo.h"
+#include "iio_dac_demo.h"
+#include "util.h"
 
 #ifdef XILINX_PLATFORM
 #include <xparameters.h>
@@ -146,6 +149,8 @@ int32_t platform_init()
 	return 0;
 }
 
+uint16_t buf[DEFAULT_CHANNEL_NO][MAX_SAMPLES_PER_CHANNEL];
+
 /***************************************************************************//**
  * @brief main
 *******************************************************************************/
@@ -159,14 +164,26 @@ int main(void)
 	/* iio demo configurations. */
 	struct iio_demo_init_param iio_demo_out_init_par;
 
+	/* adc demo configurations. */
+	struct adc_demo_init_param adc_init_par;
+
+	/* dac demo configurations. */
+	struct dac_demo_init_param dac_init_par;
+
 	/* iio instance descriptor. */
 	struct iio_demo_desc *iio_demo_in_desc;
 
 	/* iio instance descriptor. */
 	struct iio_demo_desc *iio_demo_out_desc;
 
+	/* adc instance descriptor. */
+	struct adc_demo_desc *adc_desc;
+
+	/* dac instance descriptor. */
+	struct dac_demo_desc *dac_desc;
+
 	status = platform_init();
-	if (status != 0)
+	if (status != SUCCESS)
 		return status;
 
 	iio_demo_out_init_par = (struct iio_demo_init_param) {
@@ -174,7 +191,7 @@ int main(void)
 		.dev_ch_attr = 1111,
 	};
 	status = iio_demo_dev_init(&iio_demo_out_desc, &iio_demo_out_init_par);
-	if (status < 0)
+	if (status < SUCCESS)
 		return status;
 
 	iio_demo_in_init_par = (struct iio_demo_init_param) {
@@ -182,7 +199,7 @@ int main(void)
 		.dev_ch_attr = 2211,
 	};
 	status = iio_demo_dev_init(&iio_demo_in_desc, &iio_demo_in_init_par);
-	if (status != 0)
+	if (status != SUCCESS)
 		return status;
 
 	struct iio_data_buffer rd_buf = {
@@ -194,12 +211,48 @@ int main(void)
 		.size = MAX_SIZE_BASE_ADDR
 	};
 
-	struct iio_app_device devices[] = {
-		IIO_APP_DEVICE("demo_device_output", iio_demo_out_desc,
-			       &iio_demo_dev_out_descriptor,NULL, &wr_buf),
-		IIO_APP_DEVICE("demo_device_input", iio_demo_in_desc,
-			       &iio_demo_dev_in_descriptor,&rd_buf, NULL)
+	struct iio_data_buffer adc_buff = {
+		.buff = (void *)ADC_DDR_BASEADDR,
+		.size = MAX_SIZE_BASE_ADDR
 	};
 
-	return iio_app_run(devices, 2);
+	struct iio_data_buffer dac_buff = {
+		.buff = (void *)DAC_DDR_BASEADDR,
+		.size = MAX_SIZE_BASE_ADDR
+	};
+
+	adc_init_par = (struct adc_demo_init_param) {
+		.loopback_buffers = NULL,
+		.channel_no = 2,
+		.dev_global_attr = 3333,
+		.dev_ch_attr = {1111,1112,1113,1114,1115,1116,1117,1118,1119,1120,1121,1122,1123,1124,1125,1126}
+	};
+	status = adc_demo_init(&adc_desc, &adc_init_par);
+	if (status != SUCCESS)
+		return status;
+
+	dac_init_par = (struct dac_demo_init_param) {
+		.loopback_buffers = NULL,
+		.channel_no = 2,
+		.dev_global_attr = 4444,
+		.dev_ch_attr = {1111,1112,1113,1114,1115,1116,1117,1118,1119,1120,1121,1122,1123,1124,1125,1126}
+	};
+	status = dac_demo_init(&dac_desc, &dac_init_par);
+	if (status != SUCCESS)
+		return status;
+	ADC_CHANNEL_NO = adc_desc->active_ch;
+	uint32_t adc_channels = ADC_CHANNEL_NO;
+	struct iio_device adc_demo_iio_descriptor = ADC_DEMO_DEV(adc_channels);
+	DAC_CHANNEL_NO = dac_desc->active_ch;
+	uint32_t dac_channels = DAC_CHANNEL_NO;
+	struct iio_device dac_demo_iio_descriptor = DAC_DEMO_DEV(dac_channels);
+
+	struct iio_app_device devices[] = {
+		IIO_APP_DEVICE("adc_demo", adc_desc,
+			       &adc_demo_iio_descriptor,&adc_buff, NULL),
+		IIO_APP_DEVICE("dac_demo", dac_desc,
+			       &dac_demo_iio_descriptor,NULL, &dac_buff)
+	};
+
+	return iio_app_run(devices, ARRAY_SIZE(devices));
 }

commit f036fe77324d4e1af18afad2a7083995c7c04ee2

projects:iio_demo: Makefile corrected
Fixes: 195fbc0 (drivers: adc_demo&dac_demo: Add buffers)

Signed-off-by: Andrei Porumb <andrei.porumb@analog.com>

projects/iio_demo/src.mk
@@ -22,7 +22,7 @@ INCS += $(INCLUDE)/fifo.h \
 INCS += $(DRIVERS)/adc/adc_demo/iio_adc_demo.h \
 		$(DRIVERS)/dac/dac_demo/dac_demo.h \
 		$(DRIVERS)/dac/dac_demo/iio_dac_demo.h \
-		$(DRIVERS)/adc/adc_demo/adc_demo.h \
+		$(DRIVERS)/adc/adc_demo/adc_demo.h
 
 ifeq ($(PLATFORM),$(filter $(PLATFORM),xilinx aducm3029))
 SRCS += $(PLATFORM_DRIVERS)/delay.c \