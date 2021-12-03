c54e0d466553f1de3d56d021fe2263734e38dfda
Darius Berghe <darius.berghe@analog.com>
2021-12-03 15:15:38 +0200

    adapt iio_demo project to newest stm32 build
    - Remove hal files from project, these are now auto-generated from
      the .ioc file
    - Remove the -D flags from Makefile, these are now auto-computed
    - Remove the stm32_hal.h from project, this has moved to
      drivers/platform/stm32/stm32_hal.h
    
    Signed-off-by: Darius Berghe <darius.berghe@analog.com>

--- projects/iio_demo/src.mk
+++ projects/iio_demo/src.mk
@@ -10,8 +10,7 @@ SRCS +=	$(NO-OS)/util/list.c \
 SRCS += $(DRIVERS)/adc/adc_demo/adc_demo.c \
 	$(DRIVERS)/adc/adc_demo/iio_adc_demo.c \
 	$(DRIVERS)/dac/dac_demo/iio_dac_demo.c \
-	$(DRIVERS)/dac/dac_demo/dac_demo.c \
-	$(DRIVERS)/api/irq.c
+	$(DRIVERS)/dac/dac_demo/dac_demo.c
 
 INCS += $(INCLUDE)/fifo.h \
 	$(INCLUDE)/uart.h \
@@ -62,31 +61,12 @@ endif
 
 # stm32
 ifeq (stm32, $(PLATFORM))
-SRCS += $(STM32CUBE)/STM32CubeF4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c \
-	$(STM32CUBE)/STM32CubeF4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c \
-	$(STM32CUBE)/STM32CubeF4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c \
-	$(STM32CUBE)/STM32CubeF4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_uart.c \
-	$(STM32CUBE)/STM32CubeF4/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c
-
 SRCS += $(PLATFORM_DRIVERS)/stm32_delay.c \
 	$(PLATFORM_DRIVERS)/stm32_uart.c \
-	$(PLATFORM_DRIVERS)/stm32_uart_stdio.c \
-	$(PROJECT)/src/hal/stm32/stm32f4xx_hal_msp.c \
-	$(PROJECT)/src/hal/stm32/stm32f4xx_it.c \
-	$(PROJECT)/src/hal/stm32/sysmem.c \
-	$(PROJECT)/src/hal/stm32/system_stm32f4xx.c
-
-ASM_SRCS = $(PROJECT)/src/startup/startup_stm32f446retx.s
-
-INCS += $(PROJECT)/src/hal/stm32/main.h \
-	$(INCLUDE)/delay.h \
-	$(PLATFORM_DRIVERS)/stm32_uart_stdio.h \
-	$(PROJECT)/src/hal/stm32/stm32f4xx_hal_conf.h \
-	$(PROJECT)/src/hal/stm32/stm32f4xx_it.h \
-	$(PROJECT)/src/hal/stm32/stm32_hal.h \
-	$(PLATFORM_DRIVERS)/stm32_uart.h
-
-LSCRIPT = $(PROJECT)/src/linker/STM32F446RETX_FLASH.ld
+	$(PLATFORM_DRIVERS)/stm32_uart_stdio.c
+INCS += $(PLATFORM_DRIVERS)/stm32_uart_stdio.h \
+	$(PLATFORM_DRIVERS)/stm32_uart.h \
+	$(PLATFORM_DRIVERS)/stm32_hal.h
 endif
 
 ifeq (linux,$(PLATFORM))
--- projects/iio_demo/src/hal/stm32/stm32_hal.h
+++
@@ -1,7 +0,0 @@
-#ifndef _STM32_HAL_H
-#define _STM32_HAL_H
-
-#include <stm32f4xx.h>
-#include "stm32f4xx_hal_conf.h"
-
-#endif
