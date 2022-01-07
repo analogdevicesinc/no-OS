20479a385d2c4c7f0ea557b9da4a25ddc2592430
Mihail Chindris <mihail.chindris@analog.com>
2022-01-07 16:51:43 +0200

    iio:Create non-blocking iiod
    This is an improvement of libtinyiiod.
    https://github.com/analogdevicesinc/libtinyiiod
    
    The main improvments are:
     - Non-blocking processing function.
       This is needed in order to support triggers in single threaded
       enviroment like No-OS.
     - Handle multiple connection instances (e.g. Sockets)
     - Connections and application instance are passed as ctx
       to functions.
    
    Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

---
+++ iio/iiod.h
@@ -0,0 +1,197 @@
+/***************************************************************************//**
+ *   @file   iiod.h
+ *   @brief  Header file of iiod
+ *   @author Mihail Chindris (mihail.chindris@analog.com)
+********************************************************************************
+ * Copyright 2022(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#ifndef IIOD_H
+#define IIOD_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+/* Maximum nomber of iiod connections to allocate simultaneously */
+#define IIOD_MAX_CONNECTIONS	10
+#define IIOD_VERSION		"1.1.0000000"
+#define IIOD_VERSION_LEN	(sizeof(IIOD_VERSION) - 1)
+
+enum iio_attr_type {
+	IIO_ATTR_TYPE_DEBUG,
+	IIO_ATTR_TYPE_BUFFER,
+	IIO_ATTR_TYPE_CH_OUT,
+	IIO_ATTR_TYPE_CH_IN,
+	IIO_ATTR_TYPE_DEVICE,
+};
+
+struct iiod_attr {
+	enum iio_attr_type type;
+	/*
+	 * Attribute name.
+	 * If empty (""), all attributes of the specified iio_attr_type must
+	 * be read/wrote
+	 */
+	const char *name;
+	const char *channel;
+};
+
+struct iiod_ctx {
+	/* Value specified in iiod_init_param.instance in iiod_init */
+	void *instance;
+	/* Value specified in iiod_conn_data.conn in iiod_conn_add */
+	void *conn;
+};
+
+struct iiod_conn_data {
+	/* Value to be used in iiod_ctx */
+	void *conn;
+	/* Buffer to store attributes or buffer data for a connection. */
+	char *buf;
+	/* Size of the provided buffer. It must fit the max attribute size */
+	uint32_t len;
+};
+
+/* Functions should return a negative error code on failure */
+struct iiod_ops {
+	/*
+	 * I/O operations
+	 * Send and recv are used to send or receive data to/from a connection.
+	 * They should send/receive the at maximum len bytes.
+	 * They must return the number of bytes sent/received.
+	 * They can return 0 or -EAGAIN when no data was processed.
+	 * They can do not block. They will be called again if there is still
+	 * data to be sent/recevied
+	 */
+	int (*send)(struct iiod_ctx *ctx, uint8_t *buf, uint32_t len);
+	int (*recv)(struct iiod_ctx *ctx, uint8_t *buf, uint32_t len);
+
+	/*
+	 * This is the equivalent of libiio iio_device_create_buffer.
+	 * Called in order to create a buffer to read or write data.
+	 * read_buffer or write_buffer will follow with a maximum of samples
+	 * (depending on the internal buffer).
+	 * All calls with the same ctx will refer to this buffer until close is
+	 * called.
+	 */
+	int (*open)(struct iiod_ctx *ctx, const char *device, uint32_t samples,
+		    uint32_t mask, bool cyclic);
+	/* Equivalent of iio_buffer_destroy */
+	int (*close)(struct iiod_ctx *ctx, const char *device);
+
+	/*
+	 * Read data from created buffer.
+	 * If buffer is empty it should be fully refilled first.
+	 * ?? Should I create a refill function instead ??
+	 * Must not block and process only available data.
+	 * It must return the number of read bytes
+	 */
+	int (*read_buffer)(struct iiod_ctx *ctx, const char *device, char *buf,
+			   uint32_t bytes);
+
+	/*
+	 * Write data to created buffer.
+	 * If buffer is full it should be flashed to the hw.
+	 * ?? Should I create a buffer_push function instead ??
+	 * Must not block and process only available data.
+	 * It must return the number of read bytes
+	 */
+	int (*write_buffer)(struct iiod_ctx *ctx, const char *device,
+			    const char *buf, uint32_t bytes);
+
+	/*
+	 * Attribute has to be read in buf and return the number of bytes
+	 * written.
+	 */
+	int (*read_attr)(struct iiod_ctx *ctx, const char *device,
+			 struct iiod_attr *attr, char *buf, uint32_t len);
+	/* Attribute buf is filled with the attribute value. */
+	int (*write_attr)(struct iiod_ctx *ctx, const char *device,
+			  struct iiod_attr *attr, char *buf, uint32_t len);
+	/* Simular with read_attr but trigger must be filled */
+	int (*get_trigger)(struct iiod_ctx *ctx, const char *device,
+			   char *trigger, uint32_t len);
+	/*
+	 * Simular with write_attr but trigger name is in trigger.
+	 * If trigger is equals to "". Trigger must be removed.
+	 */
+	int (*set_trigger)(struct iiod_ctx *ctx, const char *device,
+			   const char *trigger, uint32_t len);
+
+	/* I don't know what this should be used for :) */
+	int (*set_timeout)(struct iiod_ctx *ctx, uint32_t timeout);
+
+	/* I don't know what this should be used for :) */
+	int (*set_buffers_count)(struct iiod_ctx *ctx, const char *device,
+				 uint32_t buffers_count);
+};
+
+/*
+ * Internal structure.
+ * It is created in iiod_init and must be passed to all fucntions
+ */
+struct iiod_desc;
+
+/* Parameter to initialize iiod_desc */
+struct iiod_init_param {
+	struct iiod_ops *ops;
+	/* Value to be send in each iiod_ctx from iiod_ops functions */
+	void *instance;
+	/*
+	 * Xml description of the context and devices. It should exist until
+	 * iiod_remove is called
+	 */
+	char *xml;
+	/* Size of xml in bytes */
+	uint32_t xml_len;
+};
+
+/* Initialize desc. */
+int32_t iiod_init(struct iiod_desc **desc, struct iiod_init_param *param);
+/* Remove desc resources */
+void iiod_remove(struct iiod_desc *desc);
+
+/*
+ * Notify iiod about a new connection in order to store context for it.
+ * new_conn_id is set in order to reference the connection in iiod_conn_step
+ */
+int32_t iiod_conn_add(struct iiod_desc *desc, struct iiod_conn_data *data,
+		      uint32_t *new_conn_id);
+/* Remove conn_id from iiod. Provided data is returned in data */
+int32_t iiod_conn_remove(struct iiod_desc *desc, uint32_t conn_id,
+			 struct iiod_conn_data *data);
+/* Advance in the state machine of a connection. Will not block */
+int32_t iiod_conn_step(struct iiod_desc *desc, uint32_t conn_id);
+
+#endif //IIOD_H
---
+++ iio/iiod_private.h
@@ -0,0 +1,176 @@
+/***************************************************************************//**
+ *   @file   iiod_private.h
+ *   @brief  Private header file of iiod
+ *   @author Mihail Chindris (mihail.chindris@analog.com)
+********************************************************************************
+ * Copyright 2022(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#ifndef IIOD_PRIVATE_H
+#define IIOD_PRIVATE_H
+
+#define IIOD_WR				0x1
+#define IIOD_ENDL			0x2
+#define IIOD_RD				0x4
+#define IIOD_PARSER_MAX_BUF_SIZE	128
+
+#define IIOD_STR(cmd) {(cmd), sizeof(cmd) - 1}
+
+#define IIOD_CTX(desc, conn) {.instance = (desc)->app_instance,\
+			      .conn = (conn)->conn}
+
+
+/* Used to store a string and its size */
+struct iiod_str {
+	char *str;
+	uint32_t len;
+};
+
+/*
+ * Commads are the ones documented int the link:
+ * https://wiki.analog.com/resources/tools-software/linux-software/libiio_internals#the_network_backend_and_iio_daemon
+ */
+enum iiod_cmd {
+	IIOD_CMD_HELP,
+	IIOD_CMD_EXIT,
+	IIOD_CMD_PRINT,
+	IIOD_CMD_VERSION,
+	IIOD_CMD_TIMEOUT,
+	IIOD_CMD_OPEN,
+	IIOD_CMD_CLOSE,
+	IIOD_CMD_READ,
+	IIOD_CMD_WRITE,
+	IIOD_CMD_READBUF,
+	IIOD_CMD_WRITEBUF,
+	IIOD_CMD_GETTRIG,
+	IIOD_CMD_SETTRIG,
+	IIOD_CMD_SET
+};
+
+/*
+ * Structure to be filled after a command is parsed.
+ * Depending of cmd some fields are set or not
+ */
+struct comand_desc {
+	enum iiod_cmd cmd;
+	uint32_t mask;
+	uint32_t timeout;
+	uint32_t sample_count;
+	uint32_t bytes_count;
+	uint32_t count;
+	bool cyclic;
+	const char *device;
+	const char *channel;
+	const char *attr;
+	const char *trigger;
+	enum iio_attr_type type;
+};
+
+/* Used to store buffer indexes for non blocking transfers */
+struct iiod_buff {
+	char *buf;
+	uint32_t idx;
+	uint32_t len;
+};
+
+/* Result after executing a command. */
+struct iiod_run_cmd_result {
+	uint32_t val;
+	/* If set. Val needs to be sent */
+	bool write_val;
+	/* If buf.len != 0 buf has to be sent */
+	struct iiod_buff buf;
+};
+
+/* Internal structure to handle a connection state */
+struct iiod_conn_priv {
+	/* User instance of the connection to be sent in iiod_ctx */
+	void *conn;
+	/* Unset when can be used from the connection pool */
+	bool used;
+
+	/* Command data after parsed */
+	struct comand_desc cmd_data;
+	/* Result of an executed cmd */
+	struct iiod_run_cmd_result res;
+	/* IIOD States */
+	enum {
+		/* Reading line until \n */
+		IIOD_READING_LINE,
+		/* Execut cmd without I/O operations */
+		IIOD_RUNNING_CMD,
+		/* Write result of executed cmd */
+		IIOD_WRITING_CMD_RESULT,
+		/* I/O operations for READBUF and WRITEBUF cmds */
+		IIOD_RW_BUF,
+		/* I/O operations for WRITE cmd */
+		IIOD_READING_WRITE_DATA,
+		/* Set when a operation is finalized */
+		IIOD_LINE_DONE
+	} state;
+
+	/* Buffer to store received line */
+	char parser_buf[IIOD_PARSER_MAX_BUF_SIZE];
+	/* Index in parser_buf. For nonblocking operation */
+	uint32_t parser_idx;
+	/* Buffer to store raw data (attributes or buffer data).*/
+	char *payload_buf;
+	/* Length of payload_buf_len */
+	uint32_t payload_buf_len;
+	/* Used in nonbloking transfers to save indexes */
+	struct iiod_buff nb_buf;
+
+	/* Mask of current opened buffer */
+	uint32_t mask;
+	/* Buffer to store mask as a string */
+	char buf_mask[10];
+	/* Context for strtok_r function */
+	char *strtok_ctx;
+};
+
+/* Private iiod information */
+struct iiod_desc {
+	/* Pool of iiod connections */
+	struct iiod_conn_priv conns[IIOD_MAX_CONNECTIONS];
+	/* Application operations */
+	struct iiod_ops ops;
+	/* Application instance */
+	void *app_instance;
+	/* Address of xml */
+	char *xml;
+	/* XML length in bytes */
+	uint32_t xml_len;
+};
+
+#endif //IIOD_PRIVATE_H
