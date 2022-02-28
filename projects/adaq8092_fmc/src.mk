20b7e3e777c3331c8f3a8ddf7b9d639b4e85c363
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-02-28 13:10:31 +0200

    projects: adaq8092: add application
    Add application example for the ADAQ8092.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

---
+++ projects/adaq8092_fmc/src.mk
@@ -0,0 +1,57 @@
+################################################################################
+#									       #
+#     Shared variables:							       #
+#	- PROJECT							       #
+#	- DRIVERS							       #
+#	- INCLUDE							       #
+#	- PLATFORM_DRIVERS						       #
+#	- NO-OS								       #
+#									       #
+################################################################################
+
+SRCS := $(PROJECT)/src/adaq8092_fmc.c
+ifeq (y,$(strip $(TINYIIOD)))
+LIBRARIES += iio
+SRC_DIRS += $(NO-OS)/iio/iio_app
+endif
+SRCS += $(DRIVERS)/adc/adaq8092/adaq8092.c \
+	$(DRIVERS)/api/spi.c \
+	$(DRIVERS)/api/gpio.c \
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c \
+	$(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
+	$(NO-OS)/util/util.c
+ifeq (y,$(strip $(TINYIIOD)))
+SRCS += $(NO-OS)/util/fifo.c \
+	$(DRIVERS)/axi_core/iio_axi_adc/iio_axi_adc.c \
+	$(DRIVERS)/api/irq.c \
+	$(NO-OS)/util/list.c \
+	$(PLATFORM_DRIVERS)/uart.c \
+	$(PLATFORM_DRIVERS)/$(PLATFORM)_irq.c
+endif
+SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c \
+	$(PLATFORM_DRIVERS)/xilinx_spi.c \
+	$(PLATFORM_DRIVERS)/xilinx_gpio.c \
+	$(PLATFORM_DRIVERS)/delay.c
+INCS += $(PROJECT)/src/parameters.h \
+	$(PROJECT)/src/app_config.h \
+	$(DRIVERS)/adc/adaq8092/adaq8092.h \
+	$(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h
+INCS +=	$(PLATFORM_DRIVERS)/spi_extra.h \
+	$(PLATFORM_DRIVERS)/gpio_extra.h
+INCS +=	$(INCLUDE)/no-os/axi_io.h \
+	$(INCLUDE)/no-os/spi.h \
+	$(INCLUDE)/no-os/gpio.h \
+	$(INCLUDE)/no-os/error.h \
+	$(INCLUDE)/no-os/delay.h \
+	$(INCLUDE)/no-os/print_log.h \
+	$(INCLUDE)/no-os/util.h
+ifeq (y,$(strip $(TINYIIOD)))
+INCS +=	$(INCLUDE)/no-os/fifo.h \
+	$(INCLUDE)/no-os/irq.h \
+	$(INCLUDE)/no-os/uart.h \
+	$(INCLUDE)/no-os/list.h \
+	$(PLATFORM_DRIVERS)/irq_extra.h \
+	$(PLATFORM_DRIVERS)/uart_extra.h \
+	$(DRIVERS)/axi_core/iio_axi_adc/iio_axi_adc.h
+endif
