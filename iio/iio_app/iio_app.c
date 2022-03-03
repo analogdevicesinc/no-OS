analogdevicesinc/no-OS

commit 9b3bfa04bff8d7375be5aa3b5ec5d5b31faf63c3

iio: iio_app: ENOMEM is used independent of the platform
Include error.h for all the platforms.

Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

iio/iio_app/iio_app.c
@@ -48,10 +48,10 @@
 #include "parameters.h"
 #include "no-os/uart.h"
 #include "no-os/delay.h"
+#include "no-os/error.h"
 
 #if defined(ADUCM_PLATFORM) || defined(XILINX_PLATFORM)
 #include "no-os/irq.h"
-#include "no-os/error.h"
 #include "irq_extra.h"
 #include "uart_extra.h"
 #endif

commit 9df35562c70dcd472b839d6571c03383ed7e9a79

iio: iio_app: Remove error.h inclusion for all plaforms
Include error.h only for ADUCM_PLATFORM, XILINX_PLATFORM and
LINUX_PLATFORM platforms because for STM32_PLATFORM it
generates a compilation error due to redefinition of SUCCESS

Fixes: 9b3bfa04bff8 (iio: iio_app: ENOMEM is used independent of the platform)
Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

iio/iio_app/iio_app.c
@@ -48,10 +48,10 @@
 #include "parameters.h"
 #include "no-os/uart.h"
 #include "no-os/delay.h"
-#include "no-os/error.h"
 
 #if defined(ADUCM_PLATFORM) || defined(XILINX_PLATFORM)
 #include "no-os/irq.h"
+#include "no-os/error.h"
 #include "irq_extra.h"
 #include "uart_extra.h"
 #endif
@@ -70,6 +70,7 @@
 #ifdef LINUX_PLATFORM
 #include "linux_socket.h"
 #include "tcp_socket.h"
+#include "no-os/error.h"
 #endif
 
 // The default baudrate iio_app will use to print messages to console.