analogdevicesinc/no-OS

commit 0db1a92e8e1061fa2a96475d8a7d5b6b7d43e5cd

ad469x: Add channel sequencer implementation.
This will automatically select configured channels selected for data
acquisition.

Signed-off-by: Cristian Pop <cristian.pop@analog.com>

drivers/adc/ad469x/ad469x.c
@@ -324,6 +324,89 @@ int32_t ad469x_set_channel_sequence(struct ad469x_dev *dev,
 }
 
 /**
+ * @brief Configure advanced sequencer number of slots
+ * @param [in] dev - ad469x_dev device handler.
+ * @param [in] num_slots - Number of slots, max value = 0x7f
+ * @return \ref SUCCESS in case of success, \ref FAILURE otherwise.
+ */
+int32_t ad469x_adv_sequence_set_num_slots(struct ad469x_dev *dev,
+		uint8_t num_slots)
+{
+	return ad469x_spi_reg_write(dev,
+				    AD469x_REG_SEQ_CTRL,
+				    AD469x_SEQ_CTRL_NUM_SLOTS_AS(num_slots));
+}
+
+/**
+ * @brief Advanced sequencer, assign channel to a slot
+ * @param [in] dev - ad469x_dev device handler.
+ * @param [in] slot - Slot number [0x00, 0x7f]
+ * @param [in] channel - Assigned channel [0x00, 0x0f].
+ * @return \ref SUCCESS in case of success, \ref FAILURE otherwise.
+ */
+int32_t ad469x_adv_sequence_set_slot(struct ad469x_dev *dev,
+				     uint8_t slot,
+				     uint8_t channel)
+{
+	return ad469x_spi_reg_write(dev,
+				    AD469x_REG_AS_SLOT(slot),
+				    AD469x_REG_AS_SLOT_INX(channel));
+}
+
+/**
+ * @brief Configure standard sequencer channels
+ * @param [in] dev - ad469x_dev device handler.
+ * @param [in] ch_mask - Extra channels to activate.
+ * @return \ref SUCCESS in case of success, \ref FAILURE otherwise.
+ */
+int32_t ad469x_std_sequence_ch(struct ad469x_dev *dev, uint16_t ch_mask)
+{
+	int32_t ret;
+
+	ret = ad469x_spi_reg_write(dev,
+				   AD469x_REG_STD_SEQ_CONFIG,
+				   0x0f & ch_mask);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = ad469x_spi_reg_write(dev,
+				   AD469x_REG_STD_SEQ_CONFIG + 1,
+				   ch_mask >> 8);
+	if (ret != SUCCESS)
+		return ret;
+
+	return ret;
+}
+
+/**
+ * @brief Enable temperature read at the end of the sequence, for standard and
+ * advanced sequencer
+ * @param [in] dev - ad469x_dev device handler.
+ * @return \ref SUCCESS in case of success, \ref FAILURE otherwise.
+ */
+int32_t ad469x_sequence_enable_temp(struct ad469x_dev *dev)
+{
+	return ad469x_spi_write_mask(dev,
+				     AD469x_REG_TEMP_CTRL,
+				     AD469x_REG_TEMP_CTRL_TEMP_EN_MASK,
+				     AD469x_REG_TEMP_CTRL_TEMP_EN(1));
+}
+
+/**
+ * @brief Disable temperature read at the end of the sequence, for standard and
+ * advanced sequencer
+ * @param [in] dev - ad469x_dev device handler.
+ * @return \ref SUCCESS in case of success, \ref FAILURE otherwise.
+ */
+int32_t ad469x_sequence_disable_temp(struct ad469x_dev *dev)
+{
+	return ad469x_spi_write_mask(dev,
+				     AD469x_REG_TEMP_CTRL,
+				     AD469x_REG_TEMP_CTRL_TEMP_EN_MASK,
+				     AD469x_REG_TEMP_CTRL_TEMP_EN(0));
+}
+
+/**
  * @brief Configure converter busy indicator to the output of the specified port
  * @param [in] dev - ad469x_dev device handler.
  * @param [in] gp_sel - Port.
@@ -407,6 +490,21 @@ int32_t ad469x_exit_conversion_mode(struct ad469x_dev *dev)
 		return ret;
 
 	return SUCCESS;
+}
+
+/**
+ * @brief Read from device when converter has the channel sequencer activated.
+ *        Enter register mode to read/write registers
+ * @param [in] dev - ad469x_dev device handler.
+ * @param [out] buf - data buffer.
+ * @param [in] samples - sample number.
+ * @return \ref SUCCESS in case of success, \ref FAILURE otherwise.
+ */
+int32_t ad469x_seq_read_data(struct ad469x_dev *dev,
+			     uint32_t *buf,
+			     uint16_t samples)
+{
+	return ad469x_read_data(dev, 0, buf, samples);
 }
 
 /**

drivers/adc/ad469x/ad469x.h
@@ -79,6 +79,7 @@
 #define AD469x_REG_GP_MODE		0x027
 #define AD469x_REG_GPIO_STATE		0x028
 #define AD469x_REG_TEMP_CTRL		0x029
+#define AD469x_REG_AS_SLOT(x)		((x & 0x7F) | 0x100)
 
 /* 5-bit SDI Conversion Mode Commands */
 #define AD469x_CMD_REG_CONFIG_MODE		(0x0A << 3)
@@ -103,6 +104,18 @@
 #define AD469x_SEQ_CTRL_NUM_SLOTS_AS_MASK	(0x7f << 0)
 #define AD469x_SEQ_CTRL_NUM_SLOTS_AS(x)		((x & 0x7f) << 0)
 
+/* AD469x_REG_TEMP_CTRL */
+#define AD469x_REG_TEMP_CTRL_TEMP_EN_MASK	(0x01 << 0)
+#define AD469x_REG_TEMP_CTRL_TEMP_EN(x)		((x & 0x01) << 0)
+
+/* AD469x_REG_AS_SLOT */
+#define AD469x_REG_AS_SLOT_INX(x)		((x & 0x0f) << 0)
+
+/* AD469x_REG_IF_CONFIG_C */
+#define AD469x_REG_IF_CONFIG_C_MB_STRICT_MASK	(0x01 << 5)
+#define AD469x_REG_IF_CONFIG_C_MB_STRICT(x)	((x & 0x01) << 5)
+
+#define AD469x_CHANNEL(x)			(BIT(x) & 0xFFFF)
 /******************************************************************************/
 /*************************** Types Declarations *******************************/
 /******************************************************************************/
@@ -238,9 +251,33 @@ int32_t ad469x_read_data(struct ad469x_dev *dev,
 			 uint32_t *buf,
 			 uint16_t samples);
 
+/* Read from device when converter has the channel sequencer activated */
+int32_t ad469x_seq_read_data(struct ad469x_dev *dev,
+			     uint32_t *buf,
+			     uint16_t samples);
+
 /* Set channel sequence */
 int32_t ad469x_set_channel_sequence(struct ad469x_dev *dev,
 				    enum ad469x_channel_sequencing seq);
+
+/* Configure standard sequencer enabled channels */
+int32_t ad469x_std_sequence_ch(struct ad469x_dev *dev,
+			       uint16_t ch_mask);
+
+/* Configure advanced sequencer number of slots */
+int32_t ad469x_adv_sequence_set_num_slots(struct ad469x_dev *dev,
+		uint8_t num_slots);
+
+/* Advanced sequencer, assign channel to a slot */
+int32_t ad469x_adv_sequence_set_slot(struct ad469x_dev *dev,
+				     uint8_t slot,
+				     uint8_t channel);
+
+/* Enable temperature read at the end of the sequence, for standard and */
+int32_t ad469x_sequence_enable_temp(struct ad469x_dev *dev);
+
+/* Disable temperature read at the end of the sequence, for standard and */
+int32_t ad469x_sequence_disable_temp(struct ad469x_dev *dev);
 
 /* Enter conversion mode */
 int32_t ad469x_enter_conversion_mode(struct ad469x_dev *dev);

commit 7d10b87953dda04869aa040cd9bf0357f06472be

drivers: adc: ad469x: fix channel mask
Fixes mask for the number of active channels when writing the
AD469x_REG_STD_SEQ_CONFIG register.

Fix issue reported at #1294.

Fixes: 0db1a92 ("ad469x: Add channel sequencer implementation")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad469x/ad469x.c
@@ -484,7 +484,7 @@ int32_t ad469x_std_sequence_ch(struct ad469x_dev *dev, uint16_t ch_mask)
 
 	ret = ad469x_spi_reg_write(dev,
 				   AD469x_REG_STD_SEQ_CONFIG,
-				   0x0f & ch_mask);
+				   0xff & ch_mask);
 	if (ret != 0)
 		return ret;
 