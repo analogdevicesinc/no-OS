3230fdea5d3866e355fbe096567070cb7ead1d9e
Ramona Bolboaca <ramona.bolboaca@analog.com>
2022-04-05 11:18:12 +0300

    projects:eval-adxl355-pmdz: Add support for UART RX interrupt usage
    Add support for UART RX interrupt usage since by default
    iio_app is using .asynchronous_rx = true.
    
    Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

--- projects/eval-adxl355-pmdz/src/platform/stm32/parameters.h
+++ projects/eval-adxl355-pmdz/src/platform/stm32/parameters.h
@@ -56,7 +56,7 @@ extern UART_HandleTypeDef huart5;
 
 #ifdef IIO_SUPPORT
 #define IIO_APP_HUART	(&huart5)
-#define UART_IRQ_ID	0
+#define UART_IRQ_ID     UART5_IRQn
 #endif
 #define UART_DEVICE_ID      5
 #define UART_BAUDRATE  115200
