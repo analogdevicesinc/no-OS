analogdevicesinc/no-OS

commit 108ffbaa7ab99fabafedba6c9237d59a02ef3e29

include:no_os_util.h Add no_os_prefix to functions, enums, structures.
Added the "no_os_" prefix to functions declarations, enums, and
structs in no_os_util.h and changed all connected files.

Signed-off-by: George Mois <george.mois@analog.com>

drivers/adc/ad9081/api/adi_ad9081_adc.c
@@ -1922,7 +1922,7 @@ int32_t adi_ad9081_adc_ddc_fine_nco_set(adi_ad9081_device_t *device,
 				adi_ad9081_adc_ddc_coarse_dcm_decode(cddc_dcm);
 			adc_freq_hz = device->dev_info.adc_freq_hz;
 #ifdef __KERNEL__
-			adc_freq_hz = div_u64(adc_freq_hz, cddc_dcm);
+			adc_freq_hz = no_os_div_u64(adc_freq_hz, cddc_dcm);
 #else
 			adc_freq_hz = adc_freq_hz / cddc_dcm;
 #endif
@@ -2514,7 +2514,7 @@ int32_t adi_ad9081_adc_config(adi_ad9081_device_t *device, uint8_t cddcs,
 				cddc_dcm[j]);
 			adc_freq_hz = device->dev_info.adc_freq_hz;
 #ifdef __KERNEL__
-			adc_freq_hz = div_u64(adc_freq_hz, cddc_dcm_value);
+			adc_freq_hz = no_os_div_u64(adc_freq_hz, cddc_dcm_value);
 #else
 			adc_freq_hz = adc_freq_hz / cddc_dcm_value;
 #endif

drivers/adc/ad9081/api/adi_ad9081_device.c
@@ -369,7 +369,7 @@ int32_t adi_ad9081_device_clk_pll_startup(adi_ad9081_device_t *device,
 	/* find divider */
 	for (ref_div = 1; ref_div <= 4; ref_div++) {
 #ifdef __KERNEL__
-		pfd_clk_hz = div_u64(ref_clk_hz, ref_div);
+		pfd_clk_hz = no_os_div_u64(ref_clk_hz, ref_div);
 #else
 		pfd_clk_hz = ref_clk_hz / ref_div;
 #endif
@@ -385,7 +385,7 @@ int32_t adi_ad9081_device_clk_pll_startup(adi_ad9081_device_t *device,
 				n_div = n_div_vals[i];
 #ifdef __KERNEL__
 				total_feedback =
-					div_u64(vco_clk_hz, pfd_clk_hz);
+					no_os_div_u64(vco_clk_hz, pfd_clk_hz);
 #else
 				total_feedback = vco_clk_hz / pfd_clk_hz;
 #endif
@@ -451,7 +451,7 @@ int32_t adi_ad9081_device_clk_up_div_set(adi_ad9081_device_t *device,
 	cdiv = cdiv > 31 ? 31 : cdiv;
 	cdiv = cdiv < 2 ? 2 : cdiv;
 #ifdef __KERNEL__
-	cclk = div_u64(in_clk, cdiv);
+	cclk = no_os_div_u64(in_clk, cdiv);
 	sdiv = (uint8_t)div64_u64((cclk + 250000000ULL - 1),
 				  250000000ULL); /* ceil(cclk / 250e6) */
 #else
@@ -462,7 +462,7 @@ int32_t adi_ad9081_device_clk_up_div_set(adi_ad9081_device_t *device,
 	sdiv = sdiv > 4 ? 4 : sdiv;
 	sdiv = sdiv < 2 ? 2 : sdiv;
 #ifdef __KERNEL__
-	sclk = div_u64(cclk, sdiv);
+	sclk = no_os_div_u64(cclk, sdiv);
 	pdiv = (uint8_t)div64_u64((sclk + 125000000ULL - 1),
 				  125000000ULL); /* ceil(sclk / 125e6) */
 #else

drivers/adc/ad9081/api/adi_ad9081_jesd.c
@@ -992,7 +992,7 @@ int32_t adi_ad9081_jesd_rx_bit_rate_get(adi_ad9081_device_t *device,
 	bit_rate = (jesd_m * jesd_np * (jesd204b_en > 0 ? 10 : 66) *
 		    device->dev_info.dac_freq_hz);
 #ifdef __KERNEL__
-	bit_rate = div_u64(bit_rate, jesd_l * (jesd204b_en > 0 ? 8 : 64) *
+	bit_rate = no_os_div_u64(bit_rate, jesd_l * (jesd204b_en > 0 ? 8 : 64) *
 					     main_interp * ch_interp);
 #else
 	bit_rate = bit_rate / (jesd_l * (jesd204b_en > 0 ? 8 : 64) *
@@ -1827,7 +1827,7 @@ uint64_t adi_ad9081_jesd_tx_calc_bit_rate(adi_ad9081_device_t *device,
 	bit_rate = np * m * device->dev_info.adc_freq_hz *
 		   (jesd204b_en > 0 ? 10 : 66);
 #ifdef __KERNEL__
-	bit_rate = div_u64(bit_rate, l * dcm * (jesd204b_en > 0 ? 8 : 64));
+	bit_rate = no_os_div_u64(bit_rate, l * dcm * (jesd204b_en > 0 ? 8 : 64));
 #else
 	bit_rate = bit_rate / (l * dcm * (jesd204b_en > 0 ? 8 : 64));
 #endif

commit df23026fea47055553b1ff4e38ca34a5a214eaac

adc: ad9081: no_os_div_u64() -> div_u64()
In the __KERNEL__ use case, no_os_ functions are not available.

Fixes: 108ffbaa7ab9 (Add no_os_prefix)
Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

drivers/adc/ad9081/api/adi_ad9081_adc.c
@@ -1922,7 +1922,7 @@ int32_t adi_ad9081_adc_ddc_fine_nco_set(adi_ad9081_device_t *device,
 				adi_ad9081_adc_ddc_coarse_dcm_decode(cddc_dcm);
 			adc_freq_hz = device->dev_info.adc_freq_hz;
 #ifdef __KERNEL__
-			adc_freq_hz = no_os_div_u64(adc_freq_hz, cddc_dcm);
+			adc_freq_hz = div_u64(adc_freq_hz, cddc_dcm);
 #else
 			adc_freq_hz = adc_freq_hz / cddc_dcm;
 #endif
@@ -2514,7 +2514,7 @@ int32_t adi_ad9081_adc_config(adi_ad9081_device_t *device, uint8_t cddcs,
 				cddc_dcm[j]);
 			adc_freq_hz = device->dev_info.adc_freq_hz;
 #ifdef __KERNEL__
-			adc_freq_hz = no_os_div_u64(adc_freq_hz, cddc_dcm_value);
+			adc_freq_hz = div_u64(adc_freq_hz, cddc_dcm_value);
 #else
 			adc_freq_hz = adc_freq_hz / cddc_dcm_value;
 #endif

drivers/adc/ad9081/api/adi_ad9081_device.c
@@ -369,7 +369,7 @@ int32_t adi_ad9081_device_clk_pll_startup(adi_ad9081_device_t *device,
 	/* find divider */
 	for (ref_div = 1; ref_div <= 4; ref_div++) {
 #ifdef __KERNEL__
-		pfd_clk_hz = no_os_div_u64(ref_clk_hz, ref_div);
+		pfd_clk_hz = div_u64(ref_clk_hz, ref_div);
 #else
 		pfd_clk_hz = ref_clk_hz / ref_div;
 #endif
@@ -385,7 +385,7 @@ int32_t adi_ad9081_device_clk_pll_startup(adi_ad9081_device_t *device,
 				n_div = n_div_vals[i];
 #ifdef __KERNEL__
 				total_feedback =
-					no_os_div_u64(vco_clk_hz, pfd_clk_hz);
+					div_u64(vco_clk_hz, pfd_clk_hz);
 #else
 				total_feedback = vco_clk_hz / pfd_clk_hz;
 #endif
@@ -451,7 +451,7 @@ int32_t adi_ad9081_device_clk_up_div_set(adi_ad9081_device_t *device,
 	cdiv = cdiv > 31 ? 31 : cdiv;
 	cdiv = cdiv < 2 ? 2 : cdiv;
 #ifdef __KERNEL__
-	cclk = no_os_div_u64(in_clk, cdiv);
+	cclk = div_u64(in_clk, cdiv);
 	sdiv = (uint8_t)div64_u64((cclk + 250000000ULL - 1),
 				  250000000ULL); /* ceil(cclk / 250e6) */
 #else
@@ -462,7 +462,7 @@ int32_t adi_ad9081_device_clk_up_div_set(adi_ad9081_device_t *device,
 	sdiv = sdiv > 4 ? 4 : sdiv;
 	sdiv = sdiv < 2 ? 2 : sdiv;
 #ifdef __KERNEL__
-	sclk = no_os_div_u64(cclk, sdiv);
+	sclk = div_u64(cclk, sdiv);
 	pdiv = (uint8_t)div64_u64((sclk + 125000000ULL - 1),
 				  125000000ULL); /* ceil(sclk / 125e6) */
 #else

drivers/adc/ad9081/api/adi_ad9081_jesd.c
@@ -992,7 +992,7 @@ int32_t adi_ad9081_jesd_rx_bit_rate_get(adi_ad9081_device_t *device,
 	bit_rate = (jesd_m * jesd_np * (jesd204b_en > 0 ? 10 : 66) *
 		    device->dev_info.dac_freq_hz);
 #ifdef __KERNEL__
-	bit_rate = no_os_div_u64(bit_rate, jesd_l * (jesd204b_en > 0 ? 8 : 64) *
+	bit_rate = div_u64(bit_rate, jesd_l * (jesd204b_en > 0 ? 8 : 64) *
 					     main_interp * ch_interp);
 #else
 	bit_rate = bit_rate / (jesd_l * (jesd204b_en > 0 ? 8 : 64) *
@@ -1827,7 +1827,7 @@ uint64_t adi_ad9081_jesd_tx_calc_bit_rate(adi_ad9081_device_t *device,
 	bit_rate = np * m * device->dev_info.adc_freq_hz *
 		   (jesd204b_en > 0 ? 10 : 66);
 #ifdef __KERNEL__
-	bit_rate = no_os_div_u64(bit_rate, l * dcm * (jesd204b_en > 0 ? 8 : 64));
+	bit_rate = div_u64(bit_rate, l * dcm * (jesd204b_en > 0 ? 8 : 64));
 #else
 	bit_rate = bit_rate / (l * dcm * (jesd204b_en > 0 ? 8 : 64));
 #endif