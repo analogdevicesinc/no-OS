e121669c52b9fde05f2efd1bc270351b53aa9b4c
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-04-08 13:22:05 +0300

    axi_core: jesd204: axi_jesd204_tx: add param doc
    Add documentation for the parameters of the initialization and device
    structures.
    
    No functional changes.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- drivers/axi_core/jesd204/axi_jesd204_tx.h
+++ drivers/axi_core/jesd204/axi_jesd204_tx.h
@@ -76,29 +76,50 @@ enum jesd204_tx_encoder {
 };
 
 struct axi_jesd204_tx {
+	/** Device Name */
 	const char *name;
+	/** Base address */
 	uint32_t base;
+	/** Number of lanes */
 	uint32_t num_lanes;
+	/** Data path width */
 	uint32_t data_path_width;
+	/** TPL data path width */
 	uint32_t tpl_data_path_width;
+	/** Tx coniguration */
 	struct jesd204_tx_config config;
+	/** Device Clock in KHz */
 	uint32_t device_clk_khz;
+	/** Lane Clock in KHz */
 	uint32_t lane_clk_khz;
+	/** Selected Encoder */
 	enum jesd204_tx_encoder encoder;
 };
 
 struct jesd204_tx_init {
+	/** Device Name */
 	const char *name;
+	/** Base address */
 	uint32_t base;
+	/** Number of octets per frame (F) */
 	uint8_t octets_per_frame;
+	/** Number of frames per multi-frame (K) */
 	uint16_t frames_per_multiframe;
+	/**  Number of converter per device (M) */
 	uint8_t converters_per_device;
+	/** Converter resolution (N) */
 	uint8_t converter_resolution;
+	/** Number of bits per sample (N') */
 	uint8_t bits_per_sample;
+	/** If specified the JESD204B link is configured for high density (HD) operation. */
 	bool high_density;
+	/**  Number of control bits per conversion sample (CS) */
 	uint8_t control_bits_per_sample;
+	/** The JESD204B subclass */
 	uint8_t subclass;
+	/** Device Clock in KHz for the JESD204 interface */
 	uint32_t device_clk_khz;
+	/** Lane Clock in KHz */
 	uint32_t lane_clk_khz;
 };
 
