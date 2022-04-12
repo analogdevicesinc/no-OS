cf79487b69f590de652e4643ca697930c3f7cfb9
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-04-12 15:36:12 +0300

    axi_core: axi_adc_core: add code doc
    Add documentation for the axi_adc structures and functions.
    
    No functional changes.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- drivers/axi_core/axi_adc_core/axi_adc_core.h
+++ drivers/axi_core/axi_adc_core/axi_adc_core.h
@@ -117,17 +117,34 @@
 /******************************************************************************/
 /*************************** Types Declarations *******************************/
 /******************************************************************************/
+
+/**
+ * @struct axi_adc
+ * @brief AXI ADC Device Descriptor.
+ */
 struct axi_adc {
+	/** Device Name */
 	const char *name;
+	/** Base Address */
 	uint32_t base;
+	/** Number of channels */
 	uint8_t	num_channels;
+	/** AXI ADC Clock */
 	uint64_t clock_hz;
+	/** AXI ADC Channel Mask*/
 	uint32_t mask;
 };
 
+/**
+ * @struct axi_adc_init
+ * @brief AXI ADC Initialization Parameters structure.
+ */
 struct axi_adc_init {
+	/** Device Name */
 	const char *name;
+	/** Base Address */
 	uint32_t base;
+	/** Number of channels */
 	uint8_t	num_channels;
 };
 
@@ -147,59 +164,78 @@ enum axi_adc_pn_sel {
 /******************************************************************************/
 /************************ Functions Declarations ******************************/
 /******************************************************************************/
+/** Begin AXI ADC Initialization */
 int32_t axi_adc_init_begin(struct axi_adc **adc_core,
 			   const struct axi_adc_init *init);
+/** Finish AXI ADC Initialization */
 int32_t axi_adc_init_finish(struct axi_adc *adc);
+/** AXI ADC Main Initialization */
 int32_t axi_adc_init(struct axi_adc **adc_core,
 		     const struct axi_adc_init *init);
+/** Resources Deallocation */
 int32_t axi_adc_remove(struct axi_adc *adc);
+/** AXI ADC Read */
 int32_t axi_adc_read(struct axi_adc *adc,
 		     uint32_t reg_addr,
 		     uint32_t *reg_data);
+/** AXI ADC Write */
 int32_t axi_adc_write(struct axi_adc *adc,
 		      uint32_t reg_addr,
 		      uint32_t reg_data);
+/** Set AXI ADC PN sequence */
 int32_t axi_adc_set_pnsel(struct axi_adc *adc,
 			  uint32_t chan,
 			  enum axi_adc_pn_sel sel);
+/** Monitor the AXI ADC PN Sequence */
 int32_t axi_adc_pn_mon(struct axi_adc *adc,
 		       enum axi_adc_pn_sel sel,
 		       uint32_t delay_ms);
+/** Get the AXI ADC Sampling Frequency */
 int32_t axi_adc_get_sampling_freq(struct axi_adc *adc,
 				  uint32_t chan,
 				  uint64_t *sampling_freq);
+/** Set input/output delay primitive for specific interface line */
 void axi_adc_idelay_set(struct axi_adc *adc,
 			uint32_t lane,
 			uint32_t val);
+/** Set input/output delay primitive for number of lines */
 int32_t axi_adc_delay_set(struct axi_adc *adc,
 			  uint32_t no_of_lanes,
 			  uint32_t delay);
+/** Calibrate Delay using specific PN sequence */
 int32_t axi_adc_delay_calibrate(struct axi_adc *core,
 				uint32_t no_of_lanes,
 				enum axi_adc_pn_sel sel);
+/** Calibrate phase for specific AXI ADC channel */
 int32_t axi_adc_set_calib_phase(struct axi_adc *adc,
 				uint32_t chan,
 				int32_t val,
 				int32_t val2);
+/** Get the phase calibration values for AXI ADC channel */
 int32_t axi_adc_get_calib_phase(struct axi_adc *adc,
 				uint32_t chan,
 				int32_t *val,
 				int32_t *val2);
+/** Calibrate scale for specific AXI ADC channel */
 int32_t axi_adc_set_calib_scale(struct axi_adc *adc,
 				uint32_t chan,
 				int32_t val,
 				int32_t val2);
+/** Get the scale calibration values for AXI ADC channel */
 int32_t axi_adc_get_calib_scale(struct axi_adc *adc,
 				uint32_t chan,
 				int32_t *val,
 				int32_t *val2);
+/** Calibrate bias for specific AXI ADC channel */
 int32_t axi_adc_set_calib_bias(struct axi_adc *adc,
 			       uint32_t chan,
 			       int32_t val,
 			       int32_t val2);
+/** Get the bias calibration values for AXI ADC channel */
 int32_t axi_adc_get_calib_bias(struct axi_adc *adc,
 			       uint32_t chan,
 			       int32_t *val,
 			       int32_t *val2);
+/** Update active AXI ADC channels */
 int32_t axi_adc_update_active_channels(struct axi_adc *adc, uint32_t mask);
 #endif
