fe3126ea988fc34d1178e5f5c8cebd64d36bca14
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-04-14 11:00:08 +0300

    axi_core: axi_dac: add code documentation
    Add documentation for the device and initialization structures.
    Add functions documentation.
    
    No functional changes.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- drivers/axi_core/axi_dac_core/axi_dac_core.h
+++ drivers/axi_core/axi_dac_core/axi_dac_core.h
@@ -47,19 +47,32 @@
 /******************************************************************************/
 /*************************** Types Declarations *******************************/
 /******************************************************************************/
+/**
+ * @struct axi_dac
+ * @brief AXI DAC Device Descriptor.
+ */
 struct axi_dac {
+	/** Device Name */
 	const char *name;
+	/** Base Address */
 	uint32_t base;
+	/** Number of channels */
 	uint8_t	num_channels;
+	/** AXI DAC Clock */
 	uint64_t clock_hz;
-	struct axi_dac_channel *channels; //dac channels manual configuration
+	/** DAC channels manual configuration */
+	struct axi_dac_channel *channels;
 };
 
 struct axi_dac_init {
+	/** Device Name */
 	const char *name;
+	/** Base Address */
 	uint32_t base;
+	/** Number of channels */
 	uint8_t	num_channels;
-	struct axi_dac_channel *channels; //dac channels manual configuration
+	/** DAC channels manual configuration */
+	struct axi_dac_channel *channels;
 };
 
 enum axi_dac_data_sel {
@@ -94,55 +107,74 @@ extern const uint32_t sine_lut_iq[1024];
 /******************************************************************************/
 /************************ Functions Declarations ******************************/
 /******************************************************************************/
+/** Begin AXI DAC Initialization */
 int32_t axi_dac_init_begin(struct axi_dac **dac_core,
 			   const struct axi_dac_init *init);
+/** Finish AXI DAC Initialization */
 int32_t axi_dac_init_finish(struct axi_dac *dac);
+/** AXI DAC Main Initialization */
 int32_t axi_dac_init(struct axi_dac **dac_core,
 		     const struct axi_dac_init *init);
+/** Resources Deallocation */
 int32_t axi_dac_remove(struct axi_dac *dac);
+/** AXI DAC Data Select */
 int32_t axi_dac_set_datasel(struct axi_dac *dac,
 			    int32_t chan,
 			    enum axi_dac_data_sel sel);
+/** AXI DAC Set DDS Frequency */
 int32_t axi_dac_dds_set_frequency(struct axi_dac *dac,
 				  uint32_t chan, uint32_t freq_hz);
+/** AXI DAC Get DDS Frequency */
 int32_t axi_dac_dds_get_frequency(struct axi_dac *dac,
 				  uint32_t chan, uint32_t *freq);
+/** AXI DAC Set DDS Phase */
 int32_t axi_dac_dds_set_phase(struct axi_dac *dac,
 			      uint32_t chan, uint32_t phase);
+/** AXI DAC Get DDS Phase */
 int32_t axi_dac_dds_get_phase(struct axi_dac *dac,
 			      uint32_t chan, uint32_t *phase);
+/** AXI DAC Set DDS Scale */
 int32_t axi_dac_dds_set_scale(struct axi_dac *dac,
 			      uint32_t chan,
 			      int32_t scale_micro_units);
+/** AXI DAC Get DDS Phase */
 int32_t axi_dac_dds_get_scale(struct axi_dac *dac,
 			      uint32_t chan,
 			      int32_t *scale_micro_units);
+/** AXI DAC Set Buffer */
 int32_t axi_dac_set_buff(struct axi_dac *dac,
 			 uint32_t address,
 			 uint16_t *buff,
 			 uint32_t buff_size);
+/** AXI DAC Set Sine Lookup Table */
 uint32_t axi_dac_set_sine_lut(struct axi_dac *dac,
 			      uint32_t address);
+/** Get the scale calibration values for AXI DAC channel */
 int32_t axi_dac_dds_get_calib_scale(struct axi_dac *dac,
 				    uint32_t chan,
 				    int32_t *val,
 				    int32_t *val2);
+/** Get the phase calibration values for AXI DAC channel */
 int32_t axi_dac_dds_get_calib_phase(struct axi_dac *dac,
 				    uint32_t chan,
 				    int32_t *val,
 				    int32_t *val2);
+/** Calibrate scale for specific AXI DAC channel */
 int32_t axi_dac_dds_set_calib_scale(struct axi_dac *dac,
 				    uint32_t chan,
 				    int32_t val,
 				    int32_t val2);
+/** Calibrate phase for specific AXI DAC channel */
 int32_t axi_dac_dds_set_calib_phase(struct axi_dac *dac,
 				    uint32_t chan,
 				    int32_t val,
 				    int32_t val2);
+/** AXI DAC Load Custom Data */
 int32_t axi_dac_load_custom_data(struct axi_dac *dac,
 				 const uint32_t *custom_data_iq,
 				 uint32_t custom_tx_count,
 				 uint32_t address);
+/** Setup the AXI DAC Data */
 int32_t axi_dac_data_setup(struct axi_dac *dac);
 
 #endif
