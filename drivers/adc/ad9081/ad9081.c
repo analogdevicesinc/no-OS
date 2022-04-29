2c758e62721593cba6fb86c3e9a4c29109c89ddb
Dragos Bogdan <dragos.bogdan@analog.com>
2022-04-29 12:41:07 +0300

    adc: ad9081: Add AD9082 to the compatible list
    The AD9081 is a highly integrated, RF mixed signal front-end
    (MxFE) that features four 16-bit, 12 GSPS DAC cores and four
    12-bit, 4 GSPS ADC cores. The AD9082 features four 16-bit,
    12 GSPS DAC cores and two 12-bit, 6 GSPS ADC cores. Aside from
    the different ADC options, both devices are nearly identical
    in all other aspects.
    
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- drivers/adc/ad9081/ad9081.c
+++ drivers/adc/ad9081/ad9081.c
@@ -52,6 +52,7 @@
 #include <inttypes.h>
 
 #define CHIPID_AD9081	0x9081
+#define CHIPID_AD9082	0x9082
 #define CHIPID_MASK	0xFFFF
 
 #define for_each_cddc(bit, mask) \
@@ -1150,7 +1151,8 @@ int32_t ad9081_init(struct ad9081_phy **dev,
 		goto error_3;
 	}
 
-	if ((chip_id.prod_id & CHIPID_MASK) != CHIPID_AD9081) {
+	if (((chip_id.prod_id & CHIPID_MASK) != CHIPID_AD9081) &&
+		((chip_id.prod_id & CHIPID_MASK) != CHIPID_AD9082)) {
 		printf("%s: Unrecognized CHIP_ID 0x%X\n", __func__,
 		       chip_id.prod_id);
 		ret = -1;
