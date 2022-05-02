719b0393456e654cb6469e588ad912bc449f8d77
George Mois <george.mois@analog.com>
2022-05-02 14:19:10 +0300

    projects:eval-adxl313z: EVAL-ADXL313Z project creation
    The initial project for showcasing the use of the ADXL313Z
    or the ADXL314Z development board.
    
    Signed-off-by: George Mois <george.mois@analog.com>

---
+++ projects/eval-adxl313z/src/examples/examples_src.mk
@@ -0,0 +1,20 @@
+ifeq (y,$(strip $(IIO_EXAMPLE)))
+TINYIIOD=y
+CFLAGS += -DIIO_EXAMPLE=1
+SRCS += $(PROJECT)/src/examples/iio_example/iio_example.c
+INCS += $(PROJECT)/src/examples/iio_example/iio_example.h
+endif
+
+ifeq (y,$(strip $(BASIC_EXAMPLE)))
+CFLAGS += -DBASIC_EXAMPLE=1
+SRCS += $(PROJECT)/src/examples/basic/basic_example.c
+INCS += $(PROJECT)/src/examples/basic/basic_example.h
+endif
+
+ifeq (y,$(strip $(TINYIIOD)))
+SRC_DIRS += $(NO-OS)/iio/iio_app
+INCS += $(DRIVERS)/accel/adxl313/iio_adxl313.h
+
+INCS += $(INCLUDE)/no_os_list.h \
+		$(PLATFORM_DRIVERS)/uart_extra.h
+endif
