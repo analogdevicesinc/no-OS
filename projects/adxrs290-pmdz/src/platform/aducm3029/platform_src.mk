a03ec13816687c0826b4ee8675ec1a00c396c6a4
Ramona Bolboaca <ramona.bolboaca@analog.com>
2022-05-10 13:50:00 +0300

    projects:adxrs290-pmdz: Add platforms support
    Add stm32, maxim and aducm platforms support for
    adxrs290-pmdz project.
    
    Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

---
+++ projects/adxrs290-pmdz/src/platform/aducm3029/platform_src.mk
@@ -0,0 +1,15 @@
+ifeq '$(USE_TCP_SOCKET)' 'y'
+ENABLE_IIO_NETWORK ?= y
+CFLAGS += -DUSE_TCP_SOCKET
+else
+ENABLE_IIO_NETWORK ?= n
+endif
+
+ifeq (y,$(strip $(ENABLE_IIO_NETWORK)))
+DISABLE_SECURE_SOCKET ?= y
+SRC_DIRS += $(NO-OS)/network
+SRCS	 += $(NO-OS)/util/no_os_circular_buffer.c
+INCS	 += $(INCLUDE)/no_os_circular_buffer.h
+endif
+SRC_DIRS += $(INCLUDE)
+SRC_DIRS += $(PLATFORM_DRIVERS)
