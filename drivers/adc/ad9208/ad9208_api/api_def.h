analogdevicesinc/no-OS

commit 108ffbaa7ab99fabafedba6c9237d59a02ef3e29

include:no_os_util.h Add no_os_prefix to functions, enums, structures.
Added the "no_os_" prefix to functions declarations, enums, and
structs in no_os_util.h and changed all connected files.

Signed-off-by: George Mois <george.mois@analog.com>

drivers/adc/ad9208/ad9208_api/api_def.h
@@ -26,11 +26,11 @@
 #define ADI_GET_BYTE(w, p) (uint8_t)(((w) >> (p)) & 0xFF)
 #define ALL -1
 
-#define DIV_U64_REM(x, y, r) div64_u64_rem(x, y, r)
+#define DIV_U64_REM(x, y, r) no_os_div64_u64_rem(x, y, r)
 #ifndef DIV_U64
-#define DIV_U64(x, y) div_u64(x, y)
+#define NO_OS_DIV_U64(x, y) no_os_div_u64(x, y)
 #endif
-#define DIV_S64(x, y) div_s64(x, y)
+#define DIV_S64(x, y) no_os_div_s64(x, y)
 
 /**
  * \brief Platform dependent SPI access functions.

commit 9a4c2eacfee7faf831ec334a239d89da39a3cb25

drivers: adc: ad9208: fix ifdef statement
Fix ifdef statement for `NO_OS_DIV_U64`.

Fixes: 108ffba ("include:no_os_util.h Add no_os_prefix to functions, enums, structures.")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad9208/ad9208_api/api_def.h
@@ -27,7 +27,7 @@
 #define ALL -1
 
 #define DIV_U64_REM(x, y, r) no_os_div64_u64_rem(x, y, r)
-#ifndef DIV_U64
+#ifndef NO_OS_DIV_U64
 #define NO_OS_DIV_U64(x, y) no_os_div_u64(x, y)
 #endif
 #define DIV_S64(x, y) no_os_div_s64(x, y)