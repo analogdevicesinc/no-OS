analogdevicesinc/no-OS

commit 1737aec61f6509f70c7c06571bfe6afd269d59a6

drivers:adc:ad7799_iio: read raw data
Insead of processing the channel data in the IIO layer of the driver,
just send the raw data in the upper layers to be processed.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad7799/iio_ad7799.c
@@ -59,7 +59,7 @@ static int ad7799_iio_channel_read(void *device, char *buf, uint32_t len,
 	struct ad7799_dev *dev = (struct ad7799_dev *)device;
 	int32_t ret, data;
 
-	ret = ad7799_read_channel(dev, channel->ch_num, &data);
+	ret = ad7799_get_channel(dev, channel->ch_num, &data);
 	if (ret != SUCCESS)
 		return ret;
 

commit 632b439a0abd43aab7ad307e08100b1b2a803d64

drivers: ad7799: iio: fix variable type
After the latest changes of the iio driver, the `data` variable should
be of unsigned type.

Fixes: 1737aec ("drivers:adc:ad7799_iio: read raw data")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad7799/iio_ad7799.c
@@ -57,7 +57,8 @@ static int ad7799_iio_channel_read(void *device, char *buf, uint32_t len,
 				   const struct iio_ch_info *channel)
 {
 	struct ad7799_dev *dev = (struct ad7799_dev *)device;
-	int32_t ret, data;
+	int32_t ret;
+	uint32_t data;
 
 	ret = ad7799_get_channel(dev, channel->ch_num, &data);
 	if (ret != 0)