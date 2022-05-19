analogdevicesinc/no-OS

commit 5f50a6da59ca8d90bf50814832e4411a479d0364

display_demo : adding display driver using ssd_1306 controller

drivers/display/ssd_1306/ssd_1306.c
@@ -1,0 +1,259 @@
+/***************************************************************************//**
+ *   @file   ssd_1306.c
+ *   @brief  Implementation of ssd_1306 Driver.
+ *   @author Andrei Porumb (andrei.porumb@analog.com)
+********************************************************************************
+ * Copyright 2021(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include <stdbool.h>
+#include "ssd_1306.h"
+#include "error.h"
+#include "spi.h"
+
+/******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+/* @defines for ssd_1306 pins signal level */
+#define SSD1306_RST_ON     	0U
+#define SSD1306_RST_OFF    	1U
+#define SSD1306_DC_DATA    	1U
+#define SSD1306_DC_CMD     	0U
+#define SSD1306_DISP_ON    	0xAFU
+#define SSD1306_DISP_OFF   	0xAEU
+#define SSD1306_CHARSZ  	8U
+
+const struct display_controller_ops ssd1306_ops = {
+	.init = &ssd_1306_init,
+	.display_on_off = &ssd_1306_display_on_off,
+	.move_cursor = &ssd_1306_move_cursor,
+	.print_char = &ssd_1306_print_ascii,
+	.remove = &ssd_1306_remove
+};
+
+extern const uint8_t chr_8x8[128][8];
+
+/******************************************************************************/
+/************************** Functions Implementation **************************/
+/******************************************************************************/
+
+/***************************************************************************//**
+ * @brief Initializes ssd_1306 for display screening.
+ *
+ * @param device - The device structure.
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t ssd_1306_init(struct display_dev *device)
+{
+	int32_t	ret;
+	uint8_t command[3];
+	ssd_1306_extra *extra;
+
+	extra = device->extra;
+	ret = spi_init(&extra->spi_desc, extra->spi_ip);
+	if (ret != SUCCESS)
+		return FAILURE;
+	ret = gpio_get(&extra->dc_pin, extra->dc_pin_ip);
+	if (ret != SUCCESS)
+		return FAILURE;
+	ret = gpio_get(&extra->reset_pin, extra->reset_pin_ip);
+	if (ret != SUCCESS)
+		return FAILURE;
+
+	// initial pin state
+	ret = gpio_direction_output(extra->dc_pin, SSD1306_DC_CMD);
+	if (ret != SUCCESS)
+		return FAILURE;
+	ret = gpio_direction_output(extra->reset_pin, SSD1306_RST_OFF);
+	if (ret != SUCCESS)
+		return FAILURE;
+
+	command[0] = 0xAE;
+	ret = spi_write_and_read(extra->spi_desc, command, 1U);
+	if (ret != SUCCESS)
+		return FAILURE;
+	ret = gpio_set_value(extra->reset_pin, SSD1306_RST_ON);
+	if (ret != SUCCESS)
+		return FAILURE;
+	// Post reset delay, Treset=3us (See datasheet -> power on sequence)
+	usleep(3U);
+	ret = gpio_set_value(extra->reset_pin, SSD1306_RST_OFF);
+	if (ret != SUCCESS)
+		return FAILURE;
+	// charge pump
+	command[0] = 0x8D;
+	command[1] = 0x14;
+	ret = spi_write_and_read(extra->spi_desc, command, 2U);
+	if (ret != SUCCESS)
+		return FAILURE;
+	// pre-charge
+	command[0] = 0xD9;
+	command[1] = 0xF1;
+	ret = spi_write_and_read(extra->spi_desc, command, 2U);
+	if (ret != SUCCESS)
+		return FAILURE;
+	// set contrast
+	command[0] = 0x81;
+	command[1] = 0xFF;
+	ret = spi_write_and_read(extra->spi_desc, command, 2U);
+	if (ret != SUCCESS)
+		return FAILURE;
+	// set segment remap
+	command[0] = 0xA0;
+	ret = spi_write_and_read(extra->spi_desc, command, 1U);
+	if (ret != SUCCESS)
+		return FAILURE;
+	// set scan direction
+	command[0] = 0xC0;
+	ret = spi_write_and_read(extra->spi_desc, command, 1U);
+	if (ret != SUCCESS)
+		return FAILURE;
+	// set COM pin
+	command[0] = 0xDA;
+	command[1] = 0x00;
+	ret = spi_write_and_read(extra->spi_desc, command, 2U);
+	if (ret != SUCCESS)
+		return FAILURE;
+	// show written memory on screen
+	command[0] = 0xA4;
+	ret = spi_write_and_read(extra->spi_desc, command, 1U);
+	if (ret != SUCCESS)
+		return FAILURE;
+	command[0] = 0x20;	// memory addressing mode
+	command[1] = 0x00;	// horizontal addressing
+	return spi_write_and_read(extra->spi_desc, command, 2U);
+}
+
+/***************************************************************************//**
+ * @brief  ssd_1306 turns display on/off.
+ *
+ * @param device - The device structure
+ * @param on_off - Display state
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t ssd_1306_display_on_off(struct display_dev *device, uint8_t on_off)
+{
+	int32_t	ret;
+	uint8_t command;
+	ssd_1306_extra *extra;
+
+	extra = device->extra;
+	ret = gpio_set_value(extra->dc_pin, SSD1306_DC_CMD);
+	if (ret != SUCCESS)
+		return FAILURE;
+	command = (on_off == true) ? SSD1306_DISP_ON : SSD1306_DISP_OFF;
+	return spi_write_and_read(extra->spi_desc, &command, 1U);
+}
+
+/***************************************************************************//**
+ * @brief  Moves cursor to desired row/column.
+ *
+ * @param device - The device structure
+ * @param row    - row
+ * @param column - column
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t ssd_1306_move_cursor(struct display_dev *device, uint8_t row,
+			     uint8_t column)
+{
+	int32_t	ret;
+	uint8_t command[3];
+	ssd_1306_extra *extra;
+
+	extra = device->extra;
+	ret = gpio_set_value(extra->dc_pin, SSD1306_DC_CMD);
+	if (ret != SUCCESS)
+		return FAILURE;
+	command[0] = 0x21;
+	command[1] = column*8;
+	command[2] = device->cols_nb * 8 - 1U;
+	ret = spi_write_and_read(extra->spi_desc, &command, 3U);
+	if (ret != SUCCESS)
+		return FAILURE;
+	command[0] = 0x22;
+	command[1] = row;
+	command[2] = device->rows_nb - 1U;
+	return spi_write_and_read(extra->spi_desc, &command, 3U);;
+}
+
+/***************************************************************************//**
+ * @brief Prints character at selected position.
+ *
+ * @param device - The device structure.
+ * @param ascii  - corresponding number to ascii table
+ * @param row    - row
+ * @param column - column
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t ssd_1306_print_ascii(struct display_dev *device, uint8_t ascii,
+			     uint8_t row, uint8_t column)
+{
+	int32_t ret;
+	ssd_1306_extra *extra;
+	uint8_t ch[SSD1306_CHARSZ];
+	memcpy(ch, chr_8x8[ascii], SSD1306_CHARSZ);
+
+	extra = device->extra;
+	ret = ssd_1306_move_cursor(device, row, column);
+	if (ret != SUCCESS)
+		return FAILURE;
+	ret = gpio_set_value(extra->dc_pin, SSD1306_DC_DATA);
+	if (ret != SUCCESS)
+		return FAILURE;
+	return spi_write_and_read(extra->spi_desc, &ch, SSD1306_CHARSZ);
+}
+
+/***************************************************************************//**
+ * @brief Removes resources allocated by device.
+ *
+ * @param device - The device structure.
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t ssd_1306_remove(struct display_dev *device)
+{
+	int32_t ret;
+	ssd_1306_extra *extra;
+
+	extra = device->extra;
+	ret = gpio_remove(extra->reset_pin);
+	if (ret != SUCCESS)
+		return FAILURE;
+	ret = gpio_remove(extra->dc_pin);
+	if (ret != SUCCESS)
+		return FAILURE;
+	return spi_remove(extra->spi_desc);
+}

drivers/Makefile
@@ -20,7 +20,8 @@ INCLUDES = -I../include/ \
 	 -I./platform/xilinx \
 	 -I../projects/adrv9009/src/devices/adi_hal \
 	 -I../iio \
-	 -I../libraries/iio/libtinyiiod
+	 -I../libraries/iio/libtinyiiod \
+	 -I./display
 
 IIOD_FLAGS += -DTINYIIOD_VERSION_MAJOR=0	 \
 	   -DTINYIIOD_VERSION_MINOR=1		 \

drivers/display/display.c
@@ -1,0 +1,236 @@
+/***************************************************************************//**
+ *   @file   display.c
+ *   @brief  Implementation of display Driver.
+ *   @author Andrei Porumb (andrei.porumb@analog.com)
+********************************************************************************
+ * Copyright 2021(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include <stdlib.h>
+#include <stdbool.h>
+#include "display.h"
+#include "error.h"
+
+/******************************************************************************/
+/************************** Functions Implementation **************************/
+/******************************************************************************/
+
+/***************************************************************************//**
+ * @brief Initializes the display peripheral.
+ *
+ * @param device     - The device structure.
+ * @param param      - The structure that contains the device initial
+ * 		               parameters.
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t display_init(struct display_dev **device,
+		     const struct display_init_param *param)
+{
+	struct display_dev *dev;
+	uint32_t ret;
+
+	if (!device || !param)
+		return -EINVAL;
+
+	dev = (struct display_dev *)malloc(sizeof(*dev));
+	if (!dev)
+		return FAILURE;
+	dev->cols_nb = param->cols_nb;
+	dev->rows_nb = param->rows_nb;
+	dev->controller_ops = param->controller_ops;
+	dev->extra = param->extra;
+
+	ret = dev->controller_ops->init(dev);
+	if (ret != SUCCESS) {
+		free(dev);
+		return FAILURE;
+	}
+
+	*device = dev;
+
+	return ret;
+}
+
+/****************************************************************************//**
+ * @brief Frees the resources allocated by display_init().
+ *
+ * @param device - The device structure.
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t display_remove(struct display_dev *device)
+{
+	int32_t ret;
+
+	if (!device)
+		return -EINVAL;
+
+	ret = device->controller_ops->remove(device);
+	if (ret != SUCCESS)
+		return FAILURE;
+	free(device);
+
+	return ret;
+}
+
+/***************************************************************************//**
+ * @brief Turns display on.
+ *
+ * @param device - The device structure.
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t display_on(struct display_dev *device)
+{
+	if (!device)
+		return -EINVAL;
+
+	return device->controller_ops->display_on_off(device, true);
+}
+
+/***************************************************************************//**
+ * @brief Turns display off.
+ *
+ * @param device - The device structure.
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t display_off(struct display_dev *device)
+{
+	if (!device)
+		return -EINVAL;
+
+	return device->controller_ops->display_on_off(device, false);
+}
+
+/***************************************************************************//**
+ * @brief Moves cursor to desired position.
+ *
+ * @param device - The device structure.
+ * @param row    - row
+ * @param column - column
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t display_move_cursor(struct display_dev *device, uint8_t row,
+			    uint8_t column)
+{
+	if (!device)
+		return -EINVAL;
+
+	return device->controller_ops->move_cursor(device, row, column);
+}
+
+/***************************************************************************//**
+ * @brief Clears data on display.
+ *
+ * @param device - The device structure.
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t display_clear(struct display_dev *device)
+{
+	int32_t ret;
+	uint8_t i, j;
+
+	if (!device)
+		return -EINVAL;
+
+	for(i = 0; i < device->rows_nb; i++)
+		for(j = 0; j < device->cols_nb; j++) {
+			ret = device->controller_ops->print_char(device, ' ', i, j);
+			if (ret != SUCCESS)
+				return FAILURE;
+		}
+	return ret;
+}
+
+/***************************************************************************//**
+ * @brief Prints char string at selected position.
+ *
+ * @param device - The device structure.
+ * @param msg    - char string pointer
+ * @param row    - row
+ * @param column - column
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t display_print_string(struct display_dev *device, char *msg,
+			     uint8_t row, uint8_t column)
+{
+	int32_t ret;
+	int32_t len;
+	int32_t i, j;
+	int32_t r = row;
+	int32_t c = column;
+
+	if (!device || !msg)
+		return -EINVAL;
+
+	len = strlen(msg);
+	for(i = 0; i < len; i++) {
+		if(r < device->rows_nb) {
+			if(c < device->cols_nb) {
+				ret = device->controller_ops->print_char(device, msg[i], r, c);
+				if (ret != SUCCESS)
+					return FAILURE;
+				c++;
+			} else {
+				c=0U;
+				r++;
+				ret = device->controller_ops->print_char(device, msg[i], r, c);
+				if (ret != SUCCESS)
+					return FAILURE;
+				c++;
+			}
+		}
+	}
+
+	return ret;
+}
+
+/***************************************************************************//**
+ * @brief Prints single char at selected position.
+ *
+ * @param device - The device structure.
+ * @param chr    - char to be printed
+ * @param row    - row
+ * @param column - column
+ * @return Returns SUCCESS in case of success or negative error code otherwise.
+*******************************************************************************/
+int32_t display_print_char(struct display_dev *device, char chr,
+			   uint8_t row, uint8_t column)
+{
+	if (!device)
+		return -EINVAL;
+
+	return device->controller_ops->print_char(device, chr, row, column);
+}

drivers/display/display.h
@@ -1,0 +1,134 @@
+/***************************************************************************//**
+ *   @file   display.h
+ *   @brief  Header file for display Driver.
+ *   @author Andrei Porumb (andrei.porumb@analog.com)
+********************************************************************************
+ * Copyright 2021(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include <stdint.h>
+#include "gpio.h"
+#include "spi.h"
+
+/******************************************************************************/
+/*************************** Types Declarations *******************************/
+/******************************************************************************/
+/**
+ * @struct display_dev
+ * @brief Display Device Descriptor.
+ */
+struct display_dev {
+	/** Number of columns */
+	uint8_t                    cols_nb;
+	/** Number of rows */
+	uint8_t                    rows_nb;
+	/** DISPLAY controller specific ops */
+	const struct display_controller_ops *controller_ops;
+	/**  Display extra parameters (device specific) */
+	void		               *extra;
+};
+
+/**
+ * @struct display_init_param
+ * @brief Display Device Initial Parameters.
+ */
+struct display_init_param {
+	/** Number of columns */
+	uint8_t                    cols_nb;
+	/** Number of rows */
+	uint8_t                    rows_nb;
+	/** DISPLAY controller specific ops */
+	const struct display_controller_ops *controller_ops;
+	/**  Display extra parameters (device specific) */
+	void		               *extra;
+};
+
+/**
+ * @struct display_controller_ops
+ * @brief Structure holding display controller function pointers that point to the controller
+ * specific function
+ */
+struct display_controller_ops {
+	/** Initialize controller for display usage. */
+	int32_t (*init)(struct display_dev *);
+	/** Turn display on/off */
+	int32_t (*display_on_off)(struct display_dev *, uint8_t);
+	/** Move cursor */
+	int32_t (*move_cursor)(struct display_dev *, uint8_t, uint8_t);
+	/** Print character by ascii number */
+	int32_t (*print_char)(struct display_dev *, uint8_t, uint8_t,
+			      uint8_t);
+	/** Removes resources allocated by device */
+	int32_t (*remove)(struct display_dev *);
+};
+
+/******************************************************************************/
+/************************ Functions Declarations ******************************/
+/******************************************************************************/
+
+/** Initializes the display peripheral. */
+int32_t display_init(struct display_dev **device,
+		     const struct display_init_param *param);
+
+/* Frees the resources allocated by display_init(). */
+int32_t display_remove(struct display_dev *device);
+
+/** Turns display on */
+int32_t display_on(struct display_dev *device);
+
+/** Turns display off */
+int32_t display_off(struct display_dev *device);
+
+/** Moves cursor to desired position. */
+int32_t display_move_cursor(struct display_dev *device, uint8_t row,
+			    uint8_t column);
+
+/** Clears data on display. */
+int32_t display_clear(struct display_dev *device);
+
+/** Prints char string at selected position. */
+int32_t display_print_string(struct display_dev *device, char *msg,
+			     uint8_t row, uint8_t column);
+
+/** Prints single char at selected position. */
+int32_t display_print_char(struct display_dev *device, char chr,
+			   uint8_t row, uint8_t column);
+
+#endif

commit 7062255444f707eaf5958ce8ab0af4e28ced58ec

display: ssd_1306: properly pass array to function
Correctly pass buffers to the spi write/read functions.

Fixes: 5f50a6d ("display_demo : adding display driver using ssd_1306 controller")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/display/ssd_1306/ssd_1306.c
@@ -203,13 +203,13 @@ int32_t ssd_1306_move_cursor(struct display_dev *device, uint8_t row,
 	command[0] = 0x21;
 	command[1] = column*8;
 	command[2] = device->cols_nb * 8 - 1U;
-	ret = no_os_spi_write_and_read(extra->spi_desc, &command, 3U);
+	ret = no_os_spi_write_and_read(extra->spi_desc, command, 3U);
 	if (ret != 0)
 		return -1;
 	command[0] = 0x22;
 	command[1] = row;
 	command[2] = device->rows_nb - 1U;
-	return no_os_spi_write_and_read(extra->spi_desc, &command, 3U);;
+	return no_os_spi_write_and_read(extra->spi_desc, command, 3U);;
 }
 
 /***************************************************************************//**
@@ -236,7 +236,7 @@ int32_t ssd_1306_print_ascii(struct display_dev *device, uint8_t ascii,
 	ret = no_os_gpio_set_value(extra->dc_pin, SSD1306_DC_DATA);
 	if (ret != 0)
 		return -1;
-	return no_os_spi_write_and_read(extra->spi_desc, &ch, SSD1306_CHARSZ);
+	return no_os_spi_write_and_read(extra->spi_desc, ch, SSD1306_CHARSZ);
 }
 
 /***************************************************************************//**