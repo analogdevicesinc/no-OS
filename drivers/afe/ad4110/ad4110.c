analogdevicesinc/no-OS

commit 8b68f1c42484dbd124386cd6c6725a2d1c75b73a

drivers:afe:ad4110: Add continuous_read function
Add continuous read function which sets the adc's mode to
continuous conversion and fills a buffer with buffer_size
number of samples using gpio irq.

Signed-off-by: Andrei Porumb <andrei.porumb@analog.com>

drivers/afe/ad4110/ad4110.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include "ad4110.h"
 #include "error.h"
+#include "irq.h"
 #include "print_log.h"
 
 /******************************************************************************/
@@ -660,6 +661,23 @@ int32_t ad4110_spi_int_reg_read(struct ad4110_dev *dev,
 }
 
 /***************************************************************************//**
+ * IRQ handler for ADC continuous read.
+*******************************************************************************/
+static void irq_adc_read(struct ad4110_callback_ctx *ctx)
+{
+	struct ad4110_dev *dev = ctx->dev;
+	if(ctx->buffer_size > 0) {
+		if(ad4110_spi_int_data_reg_read(ctx->dev, ctx->buffer))
+			pr_err("DATA reg could not be read \n");
+		ctx->buffer_size--;
+		ctx->buffer++;
+		if(irq_enable(dev->irq_desc, dev->nready_pin))
+			pr_err("IRQ_enable error \n");
+	} else if(irq_disable(dev->irq_desc, dev->nready_pin))
+		pr_err("IRQ_disable error \n");
+}
+
+/***************************************************************************//**
  * Initialize the device.
  *
  * @param device     - The device structure.
@@ -685,6 +703,8 @@ int32_t ad4110_setup(struct ad4110_dev **device,
 	dev->afe_crc_en = AD4110_AFE_CRC_DISABLE;
 	dev->adc_crc_en = AD4110_ADC_CRC_DISABLE;
 	dev->addr = init_param.addr;
+	dev->nready_pin = init_param.nready_pin;
+	dev->irq_desc = init_param.irq_desc;
 	dev->data_stat = init_param.data_stat;
 	dev->data_length = init_param.data_length;
 	dev->sync = init_param.sync;
@@ -787,6 +807,53 @@ err_init:
 	free(dev);
 	pr_err("AD4110 initialization error (%d)\n", ret);
 	return ret;
+}
+
+/***************************************************************************//**
+ * ADC continuous read fills buffer with buffer_size number of samples.
+ *
+ * @param dev          - The device structure.
+ * @param buffer       - The buffer.
+ * @param buffer_size  - The buffer size = number of samples.
+ *
+ * @return SUCCESS in case of success, negative error code otherwise.
+*******************************************************************************/
+int32_t ad4110_continuous_read(struct ad4110_dev *dev, int32_t *buffer,
+			       int32_t buffer_size)
+{
+	int32_t ret;
+
+	struct ad4110_callback_ctx ctx = {
+		.dev = dev,
+		.buffer = buffer,
+		.buffer_size = buffer_size,
+	};
+
+	struct callback_desc irq_callback = {
+		.callback = &irq_adc_read,
+		.ctx = &ctx
+	};
+
+	ret = irq_trigger_level_set(dev->irq_desc, dev->nready_pin, IRQ_LEVEL_LOW);
+	if (ret)
+		return ret;
+	ret = irq_register_callback(dev->irq_desc, dev->nready_pin, &irq_callback);
+	if (ret)
+		return ret;
+
+
+	ret = ad4110_set_adc_mode(dev, AD4110_CONTINOUS_CONV_MODE);
+	if (ret)
+		return ret;
+	// make sure adc is fully initialized before irq enabling
+	mdelay(2U);
+	ret = irq_enable(dev->irq_desc, dev->nready_pin);
+	if (ret)
+		return ret;
+
+	while(ctx.buffer_size != 0U);
+
+	return SUCCESS;
 }
 
 /***************************************************************************//**

drivers/afe/ad4110/ad4110.h
@@ -8,6 +8,7 @@
 #include "delay.h"
 #include "gpio.h"
 #include "spi.h"
+#include "irq.h"
 
 /******************************************************************************/
 /********************** Macros and Constants Definitions **********************/
@@ -240,6 +241,9 @@ struct ad4110_dev {
 	enum ad4110_afe_clk_cfg		afe_clk;
 	enum ad4110_adc_clk_sel		adc_clk;
 	uint8_t				addr;
+	/* GPIO - used only for continuous mode */
+	struct irq_ctrl_desc *irq_desc;
+	uint32_t nready_pin;
 };
 
 struct ad4110_init_param {
@@ -257,6 +261,15 @@ struct ad4110_init_param {
 	enum ad4110_afe_clk_cfg		afe_clk;
 	enum ad4110_adc_clk_sel		adc_clk;
 	uint8_t				addr;
+	/* GPIO - used only for continuous mode */
+	struct irq_ctrl_desc *irq_desc;
+	uint32_t nready_pin;
+};
+
+struct ad4110_callback_ctx {
+	struct ad4110_dev *dev;
+	int32_t *buffer;
+	int32_t buffer_size;
 };
 
 /******************************************************************************/
@@ -313,6 +326,10 @@ int32_t ad4110_spi_int_reg_read(struct ad4110_dev *dev,
 				uint8_t reg_map,
 				uint8_t reg_addr,
 				uint32_t *reg_data);
+
+/* Fills buffer with buffer_size number of samples using irq */
+int32_t ad4110_continuous_read(struct ad4110_dev *dev, int32_t *buffer,
+			       int32_t buffer_size);
 
 /* SPI internal DATA register read from device. */
 int32_t ad4110_spi_int_data_reg_read(struct ad4110_dev *dev,

commit 001d9f1bdad33354619ae20ded15b7c6f51d4ea6

ad4110: change buffer type
Switch from int32_t to uint32_t since the adc resolution is 24-bit.

Fix incompatible types warnings.

Fixes: 8b68f1c ("drivers:afe:ad4110: Add continuous_read function")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/afe/ad4110/ad4110.c
@@ -1034,8 +1034,8 @@ err_dev:
  *
  * @return 0 in case of success, negative error code otherwise.
 *******************************************************************************/
-int32_t ad4110_continuous_read(struct ad4110_dev *dev, int32_t *buffer,
-			       int32_t buffer_size)
+int32_t ad4110_continuous_read(struct ad4110_dev *dev, uint32_t *buffer,
+			       uint32_t buffer_size)
 {
 	int32_t ret;
 

drivers/afe/ad4110/ad4110.h
@@ -355,8 +355,8 @@ struct ad4110_init_param {
 
 struct ad4110_callback_ctx {
 	struct ad4110_dev *dev;
-	int32_t *buffer;
-	int32_t buffer_size;
+	uint32_t *buffer;
+	uint32_t buffer_size;
 };
 
 /******************************************************************************/
@@ -415,8 +415,8 @@ int32_t ad4110_spi_int_reg_read(struct ad4110_dev *dev,
 				uint32_t *reg_data);
 
 /* Fills buffer with buffer_size number of samples using irq */
-int32_t ad4110_continuous_read(struct ad4110_dev *dev, int32_t *buffer,
-			       int32_t buffer_size);
+int32_t ad4110_continuous_read(struct ad4110_dev *dev, uint32_t *buffer,
+			       uint32_t buffer_size);
 
 /* SPI internal DATA register read from device. */
 int32_t ad4110_spi_int_data_reg_read(struct ad4110_dev *dev,

projects/ad4110/src/app/main.c
@@ -54,7 +54,7 @@
 #include <inttypes.h>
 #include <xil_cache.h>
 
-static int32_t data_buf[BUF_LENGTH];
+static uint32_t data_buf[BUF_LENGTH];
 
 /***************************************************************************//**
  * @brief main