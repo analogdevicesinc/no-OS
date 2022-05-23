f55d79da5d7fdbffa216292a807d155581129283
Ramona Bolboaca <ramona.bolboaca@analog.com>
2022-05-23 16:56:36 +0300

    projects: Remove no_os_timer.c from projects
    ad7768-evb, iio_aducm3029 and aducm_blinky_example include the
    whole drivers\api\ directory, which now also includes
    no_os_timer.c file. However, aducm3029 and xilinx platforms
    use their own implementation of no_os_timer.c so the
    directory inclusion was moddified such that no_os_timer.c
    is not included in these projects.
    
    Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

--- projects/aducm_blinky_example/src.mk
+++ projects/aducm_blinky_example/src.mk
@@ -15,8 +15,7 @@
 SRC_DIRS +=	$(PROJECT)/app_src \
 		$(PLATFORM_DRIVERS) \
 		$(INCLUDE) \
-		$(NO-OS)/util \
-		$(DRIVERS)/api
+		$(NO-OS)/util
 
 #Include makefiles from each source directory if they exist
 SUB_MAKES= $(wildcard $(addsuffix /src.mk, $(SRC_DIRS)))
@@ -27,6 +26,11 @@ REMAINING_DIRECORIES = $(filter-out $(DIRECORIES_WITH_MAKEFILES), $(SRC_DIRS))
 
 # recursive wildcard
 rwildcard = $(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
+
+SRCS += $(DRIVERS)/api/no_os_gpio.c \
+        $(DRIVERS)/api/no_os_i2c.c  \
+        $(DRIVERS)/api/no_os_irq.c  \
+        $(DRIVERS)/api/no_os_spi.c
 
 SRCS += $(foreach dir, $(REMAINING_DIRECORIES), $(call rwildcard, $(dir),*.c))
 INCS += $(foreach dir, $(REMAINING_DIRECORIES), $(call rwildcard, $(dir),*.h))
