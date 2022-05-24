analogdevicesinc/no-OS

commit d2018ba14809caeea8dea8da7c2fa422fdc75aa3

tools:scripts:build_projects.py: Always regenerate bsp for stm32
For stm32 platform always set new_hdf to true in order to
regenerate bsp at each build to avoid situations in which
the already existing bsp is not the correct one due to
HAL changes.

Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

tools/scripts/build_projects.py
@@ -143,8 +143,12 @@ def get_hardware(hardware, platform, builds_dir):
 	if os.path.isfile(filename):
 		#If equal
 		if filecmp.cmp(filename, tmp_filename):
-			log("Same hardware from last build, use existing bsp")
-			return (filename, 0, 0)
+			if platform == 'stm32':
+				log("For stm32 platform always regenerate bsp")
+				return (filename, 1, 0)
+			else:
+				log("Same hardware from last build, use existing bsp")
+				return (filename, 0, 0)
 	
 	err = run_cmd('cp %s %s' % (tmp_filename, filename))
 	if err != 0:

commit b50588efec3b9b1c5f63b1871681b4e60584f0ef

tools:scripts:build_projects.py: Fixes bsp regeneration for stm32
If platform is stm32, always perform make reset.

Fixes: d2018ba ("tools:scripts:build_projects.py: Always regenerate bsp for stm32")

Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

tools/scripts/build_projects.py
@@ -143,12 +143,8 @@ def get_hardware(hardware, platform, builds_dir):
 	if os.path.isfile(filename):
 		#If equal
 		if filecmp.cmp(filename, tmp_filename):
-			if platform == 'stm32':
-				log("For stm32 platform always regenerate bsp")
-				return (filename, 1, 0)
-			else:
-				log("Same hardware from last build, use existing bsp")
-				return (filename, 0, 0)
+			log("Same hardware from last build, use existing bsp")
+			return (filename, 0, 0)
 	
 	err = run_cmd('cp %s %s' % (tmp_filename, filename))
 	if err != 0:
@@ -214,21 +210,28 @@ class BuildConfig:
 				new_hdf = False
 			else:
 				new_hdf = True
-		
-		if new_hdf:
+		if self.platform == 'stm32':
 			err = run_cmd(cmd + ' reset')
 			if err != 0:
 				return err
+			err = run_cmd(cmd + ' -j%d all' % (multiprocessing.cpu_count() - 1))
+			if err != 0:
+				return err
+		else:
+			if new_hdf:
+				err = run_cmd(cmd + ' reset')
+				if err != 0:
+					return err
 
-		err = run_cmd(cmd + ' update')
-		if err != 0:
-			return err
-		err = run_cmd(cmd + ' clean')
-		if err != 0:
-			return err
-		err = run_cmd(cmd + ' -j%d all' % (multiprocessing.cpu_count() - 1))
-		if err != 0:
-			return err
+			err = run_cmd(cmd + ' update')
+			if err != 0:
+				return err
+			err = run_cmd(cmd + ' clean')
+			if err != 0:
+				return err
+			err = run_cmd(cmd + ' -j%d all' % (multiprocessing.cpu_count() - 1))
+			if err != 0:
+				return err
 		
 		log_success("DONE")
 		log_file = DEFAULT_LOG_FILE