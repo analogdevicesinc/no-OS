60e7ee57032ad3608840dafecefcc637543b9e1c
Darius Berghe <darius.berghe@analog.com>
2022-06-08 11:44:08 +0300

    fix iio_aducm3029 project build
    a recent change in aducm3029 platform drivers created a linker
    error when building this project.
    
    undefined reference to `no_os_irq_unregister_callback'
    ... (undefined references to all used no_os_irq_* functions)
    
    Signed-off-by: Darius Berghe <darius.berghe@analog.com>

--- projects/iio_aducm3029/src.mk
+++ projects/iio_aducm3029/src.mk
@@ -7,11 +7,11 @@ SRC_DIRS += $(PROJECT)/src \
 		$(PLATFORM_DRIVERS) \
 		$(NO-OS)/util \
 		$(NO-OS)/include
+SRCS += $(NO-OS)/drivers/api/no_os_irq.c
 
 ifeq '$(TINYIIOD)' 'y'
 SRC_DIRS += $(NO-OS)/iio/iio_app
 SRCS += $(NO-OS)/drivers/api/no_os_gpio.c \
         $(NO-OS)/drivers/api/no_os_i2c.c  \
-        $(NO-OS)/drivers/api/no_os_irq.c  \
         $(NO-OS)/drivers/api/no_os_spi.c
 endif
