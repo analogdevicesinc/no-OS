f16fa83f5756370dd95fcbacf095b7fb4f4347ca
Ciprian Regus <ciprian.regus@analog.com>
2022-06-21 18:10:13 +0300

    projects:ad74413r: Demo project for AD74413r
    Implemented a basic example for the AD74413R adc-dac, which
    exemplifies the GPO comparator, voltage input, voltage output
    and current input functionalities.
    
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

---
+++ projects/ad74413r/src.mk
@@ -0,0 +1,36 @@
+include $(PROJECT)/src/platform/$(PLATFORM)/platform_src.mk
+include $(PROJECT)/src/examples/examples_src.mk
+
+SRCS += $(PROJECT)/src/platform/$(PLATFORM)/main.c
+
+INCS += $(PROJECT)/src/common/common_data.h
+SRCS += $(PROJECT)/src/common/common_data.c
+	
+INCS += $(PROJECT)/src/platform/platform_includes.h
+
+INCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.h
+SRCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.c 
+
+INCS += $(INCLUDE)/no_os_delay.h     \
+		$(INCLUDE)/no_os_error.h     \
+		$(INCLUDE)/no_os_gpio.h      \
+		$(INCLUDE)/no_os_print_log.h \
+		$(INCLUDE)/no_os_spi.h       \
+		$(INCLUDE)/no_os_irq.h      \
+		$(INCLUDE)/no_os_list.h      \
+		$(INCLUDE)/no_os_crc8.h      \
+		$(INCLUDE)/no_os_uart.h      \
+		$(INCLUDE)/no_os_lf256fifo.h \
+		$(INCLUDE)/no_os_util.h \
+
+SRCS += $(DRIVERS)/api/no_os_gpio.c \
+		$(NO-OS)/util/no_os_lf256fifo.c \
+		$(DRIVERS)/api/no_os_irq.c  \
+		$(DRIVERS)/api/no_os_spi.c  \
+		$(NO-OS)/util/no_os_list.c \
+		$(NO-OS)/util/no_os_crc8.c \
+		$(NO-OS)/util/no_os_util.c
+
+INCS += $(DRIVERS)/adc-dac/ad74413r/ad74413r.h
+SRCS += $(DRIVERS)/adc-dac/ad74413r/ad74413r.c
+
---
+++ projects/ad74413r/src/platform/maxim/platform_src.mk
@@ -0,0 +1,20 @@
+INCS += $(INCLUDE)/no_os_rtc.h
+
+INCS += $(PLATFORM_DRIVERS)/maxim_delay.h     \
+        $(PLATFORM_DRIVERS)/gpio_extra.h      \
+        $(PLATFORM_DRIVERS)/maxim_hal.h       \
+        $(PLATFORM_DRIVERS)/spi_extra.h       \
+        $(PLATFORM_DRIVERS)/gpio_irq_extra.h  \
+        $(PLATFORM_DRIVERS)/irq_extra.h       \
+        $(PLATFORM_DRIVERS)/rtc_extra.h       \
+        $(PLATFORM_DRIVERS)/maxim_uart.h      \
+        $(PLATFORM_DRIVERS)/maxim_stdio.h
+
+SRCS += $(PLATFORM_DRIVERS)/maxim_delay.c     \
+        $(PLATFORM_DRIVERS)/maxim_gpio.c      \
+        $(PLATFORM_DRIVERS)/maxim_spi.c       \
+        $(PLATFORM_DRIVERS)/maxim_rtc.c       \
+        $(PLATFORM_DRIVERS)/maxim_gpio_irq.c  \
+        $(PLATFORM_DRIVERS)/maxim_irq.c       \
+        $(PLATFORM_DRIVERS)/maxim_uart.c      \
+        $(PLATFORM_DRIVERS)/maxim_stdio.c
