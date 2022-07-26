09803176419a6177e66afd85c9e4f93f6bddfe26
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2022-07-26 10:59:54 +0300

    drivers: adt7420: add iio support for adt7420
    Added iio_adt7420.c and iio_adt7420.h
    
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

---
+++ drivers/temperature/adt7420/iio_adt7420.h
@@ -0,0 +1,79 @@
+/***************************************************************************//**
+ *   @file   iio_adt7420.h
+ *   @brief  Header file of IIO ADT7420 Driver.
+ *   @author RNechita (ramona.nechita@analog.com)
+********************************************************************************
+ * Copyright 2022(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef IIO_ADT7420_H
+#define IIO_ADT7420_H
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include <stdint.h>
+#include <stdlib.h>
+#include "iio.h"
+
+/******************************************************************************/
+/*************************** Types Declarations *******************************/
+/******************************************************************************/
+/**
+ * @struct adt7420_iio_dev
+ * @brief iio device descriptor.
+ */
+struct adt7420_iio_dev {
+	struct adt7420_dev *adt7420_dev;
+	struct iio_dev *iio_dev;
+	uint32_t active_channels;
+	uint8_t no_active_channels;
+};
+
+/**
+ * @struct adt7420_iio_init_param
+ * @brief Configuration structure.
+ */
+struct adt7420_iio_init_param {
+	struct adt7420_init_param *adt7420_dev_init;
+};
+
+/******************************************************************************/
+/************************ Functions Declarations ******************************/
+/******************************************************************************/
+int adt7420_iio_init(struct adt7420_iio_dev **iio_dev,
+		     struct adt7420_iio_init_param *init_param);
+
+int adt7420_iio_remove(struct adt7420_iio_dev *desc);
+
+#endif /** IIO_ADT7420_H **/
