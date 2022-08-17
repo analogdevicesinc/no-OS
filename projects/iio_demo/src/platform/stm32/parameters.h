8387f3b1459510d6d04be106ca294f5b59ba6109
Antoniu Miclaus <antoniu.miclaus@analog.com>
2022-08-17 11:58:39 +0300

    platform: stm32: timer: remove typedefs
    Aliases using `typedef` are no longer supported on the no-os repository.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- projects/iio_demo/src/platform/stm32/parameters.h
+++ projects/iio_demo/src/platform/stm32/parameters.h
@@ -68,7 +68,7 @@ extern UART_HandleTypeDef huart5;
 
 #ifdef IIO_TIMER_TRIGGER_EXAMPLE
 /* Adc Demo Timer settings */
-extern stm32_timer_init_param adc_demo_xtip;
+extern struct stm32_timer_init_param adc_demo_xtip;
 #define ADC_DEMO_TIMER_DEVICE_ID    13
 #define ADC_DEMO_TIMER_FREQ_HZ      1000000
 #define ADC_DEMO_TIMER_TICKS_COUNT  2000
@@ -86,7 +86,7 @@ extern TIM_HandleTypeDef htim13;
 #define ADC_DEMO_TIMER_TRIG_IRQ_ID  TIM8_UP_TIM13_IRQn
 
 /* Dac Demo Timer settings */
-extern stm32_timer_init_param dac_demo_xtip;
+extern struct stm32_timer_init_param dac_demo_xtip;
 #define DAC_DEMO_TIMER_DEVICE_ID    14
 #define DAC_DEMO_TIMER_FREQ_HZ      1000000
 #define DAC_DEMO_TIMER_TICKS_COUNT  2000
