analogdevicesinc/no-OS

commit 3d616520f6fe03f8c34ea8e009eb9e942f1d1dd3

Remove pull field from STM32 GPIO extra param
Use the pull field from the no_os_gpio_param to initialize the GPIO
controller instead of the one from stm32_gpio_init_param.

Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/platform/stm32/stm32_gpio.c
@@ -110,15 +110,28 @@ static int32_t _gpio_init(struct no_os_gpio_desc *desc,
 
 	/* copy the settings to gpio descriptor */
 	desc->number = param->number;
+	desc->pull = param->pull;
 	extra->port = pextra->port;
 	extra->mode = pextra->mode;
-	extra->pull = pextra->pull;
 	extra->speed = pextra->speed;
+
+	switch (param->pull) {
+	case NO_OS_PULL_NONE:
+		gis.Pull = GPIO_PuPd_NOPULL;
+		break;
+	case NO_OS_PULL_UP:
+		gis.Pull = GPIO_PdPu_UP;
+		break;
+	case NO_OS_PULL_DOWN:
+		gis.Pull = GPIO_PdPu_DOWN;
+		break;
+	default:
+		return -EINVAL;
+	}
 
 	/* configure gpio with user configuration */
 	gis.Pin = NO_OS_BIT(param->number);
 	gis.Mode = extra->mode;
-	gis.Pull = extra->pull;
 	gis.Speed = extra->speed;
 	HAL_GPIO_Init(extra->port, &gis);
 

drivers/platform/stm32/stm32_gpio.h
@@ -52,8 +52,6 @@ typedef struct stm32_gpio_init_param {
 	GPIO_TypeDef *port;
 	/** Output mode */
 	uint32_t mode;
-	/** Pull-up, Pull-down mode */
-	uint32_t pull;
 	/** Speed grade */
 	uint32_t speed;
 } stm32_gpio_init_param;
@@ -67,8 +65,6 @@ typedef struct stm32_gpio_desc {
 	GPIO_TypeDef *port;
 	/** Output mode */
 	uint32_t mode;
-	/** Pull-up, Pull-down mode */
-	uint32_t pull;
 	/** Speed grade */
 	uint32_t speed;
 } stm32_gpio_desc;

projects/adxrs290-pmdz/src/platform/stm32/parameters.c
@@ -52,6 +52,5 @@ struct stm32_spi_init_param adxrs290_spi_extra_ip  = {
 struct stm32_gpio_desc adxrs290_gpio_extra_ip = {
 	.mode = GPIO_MODE_IT_RISING,
 	.port = GPIOA,
-	.pull = GPIO_NOPULL,
 	.speed = GPIO_SPEED_FREQ_VERY_HIGH,
 };

commit a120ed4227f64239da93b36a34c5758920577aac

drivers:platform:stm32:Restructure extra gpio desc
Add a field of type GPIO_InitTypeDef for the stm32 platform specific
gpio descriptor, instead of storing each of the structure's fields.

Fixes: 3d616520f6fe ("Remove pull field from STM32 GPIO extra param")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/platform/stm32/stm32_gpio.c
@@ -57,8 +57,7 @@ static int32_t _gpio_init(struct no_os_gpio_desc *desc,
 {
 	int32_t ret = 0;
 	struct stm32_gpio_desc *extra = desc->extra;
-	struct stm32_gpio_desc *pextra = param->extra;
-	GPIO_InitTypeDef gis;
+	struct stm32_gpio_init_param *pextra = param->extra;
 
 	if (!param)
 		return -EINVAL;
@@ -112,28 +111,29 @@ static int32_t _gpio_init(struct no_os_gpio_desc *desc,
 	desc->number = param->number;
 	desc->pull = param->pull;
 	extra->port = pextra->port;
-	extra->mode = pextra->mode;
-	extra->speed = pextra->speed;
 
 	switch (param->pull) {
 	case NO_OS_PULL_NONE:
-		gis.Pull = GPIO_PuPd_NOPULL;
+		extra->gpio_config.Pull = GPIO_NOPULL;
 		break;
 	case NO_OS_PULL_UP:
-		gis.Pull = GPIO_PdPu_UP;
+	case NO_OS_PULL_UP_WEAK:
+		extra->gpio_config.Pull = GPIO_PULLUP;
 		break;
 	case NO_OS_PULL_DOWN:
-		gis.Pull = GPIO_PdPu_DOWN;
+	case NO_OS_PULL_DOWN_WEAK:
+		extra->gpio_config.Pull = GPIO_PULLDOWN;
 		break;
 	default:
 		return -EINVAL;
 	}
 
 	/* configure gpio with user configuration */
-	gis.Pin = NO_OS_BIT(param->number);
-	gis.Mode = extra->mode;
-	gis.Speed = extra->speed;
-	HAL_GPIO_Init(extra->port, &gis);
+	extra->gpio_config.Pin = NO_OS_BIT(param->number);
+	extra->gpio_config.Mode = pextra->mode;
+	extra->gpio_config.Speed = pextra->speed;
+
+	HAL_GPIO_Init(extra->port, &extra->gpio_config);
 
 	return ret;
 }
@@ -219,7 +219,6 @@ int32_t stm32_gpio_remove(struct no_os_gpio_desc *desc)
  */
 int32_t stm32_gpio_direction_input(struct no_os_gpio_desc *desc)
 {
-	GPIO_InitTypeDef gis = {0};
 	struct stm32_gpio_desc	*extra;
 
 	if (!desc)
@@ -231,14 +230,8 @@ int32_t stm32_gpio_direction_input(struct no_os_gpio_desc *desc)
 	extra = desc->extra;
 
 	/* configure gpio with user configuration */
-	gis.Pin = NO_OS_BIT(desc->number);
-	gis.Mode = GPIO_MODE_INPUT;
-	gis.Pull = extra->pull;
-	gis.Speed = extra->speed;
-	HAL_GPIO_Init(extra->port, &gis);
-
-	/* copy the settings to gpio descriptor */
-	extra->mode = gis.Mode;
+	extra->gpio_config.Mode = GPIO_MODE_INPUT;
+	HAL_GPIO_Init(extra->port, &extra->gpio_config);
 
 	return 0;
 }
@@ -254,7 +247,6 @@ int32_t stm32_gpio_direction_input(struct no_os_gpio_desc *desc)
 int32_t stm32_gpio_direction_output(struct no_os_gpio_desc *desc,
 				    uint8_t value)
 {
-	GPIO_InitTypeDef gis = {0};
 	struct stm32_gpio_desc	*extra;
 
 	if (!desc)
@@ -269,14 +261,8 @@ int32_t stm32_gpio_direction_output(struct no_os_gpio_desc *desc,
 	HAL_GPIO_WritePin(extra->port, NO_OS_BIT(desc->number), (GPIO_PinState)value);
 
 	/* configure gpio with user configuration */
-	gis.Pin = NO_OS_BIT(desc->number);
-	gis.Mode = GPIO_MODE_OUTPUT_PP;
-	gis.Pull = extra->pull;
-	gis.Speed = extra->speed;
-	HAL_GPIO_Init(extra->port, &gis);
-
-	/* copy the settings to gpio descriptor */
-	extra->mode = gis.Mode;
+	extra->gpio_config.Mode = GPIO_MODE_OUTPUT_PP;
+	HAL_GPIO_Init(extra->port, &extra->gpio_config);
 
 	return 0;
 }
@@ -296,7 +282,7 @@ int32_t stm32_gpio_get_direction(struct no_os_gpio_desc *desc,
 		return -EINVAL;
 
 	struct stm32_gpio_desc *extra = desc->extra;
-	*direction = (extra->mode & GPIO_MODE_OUTPUT_PP) ? NO_OS_GPIO_OUT :
+	*direction = (extra->gpio_config.Mode & GPIO_MODE_OUTPUT_PP) ? NO_OS_GPIO_OUT :
 		     NO_OS_GPIO_IN;
 	return 0;
 }

drivers/platform/stm32/stm32_gpio.h
@@ -63,10 +63,8 @@ struct stm32_gpio_init_param {
 struct stm32_gpio_desc {
 	/** Port */
 	GPIO_TypeDef *port;
-	/** Output mode */
-	uint32_t mode;
-	/** Speed grade */
-	uint32_t speed;
+	/** GPIO configuration */
+	GPIO_InitTypeDef gpio_config;
 };
 
 /**

drivers/platform/stm32/stm32_spi.c
@@ -84,10 +84,10 @@ int32_t stm32_spi_init(struct no_os_spi_desc **desc,
 	sinit = param->extra;
 
 	csip_extra.port = sinit->chip_select_port;
-	csip_extra.pull = GPIO_NOPULL;
 	csip_extra.mode = GPIO_MODE_OUTPUT_PP;
 	csip_extra.speed = GPIO_SPEED_FREQ_LOW;
 	csip.number = param->chip_select;
+	csip.pull = NO_OS_PULL_NONE;
 	csip.extra = &csip_extra;
 	csip.platform_ops = &stm32_gpio_ops;
 	ret = no_os_gpio_get(&sdesc->chip_select, &csip);