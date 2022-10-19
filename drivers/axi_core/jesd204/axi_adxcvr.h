4569c05a590290b12804a08aa0780e8fa101c8a0
George Mois <george.mois@analog.com>
2022-10-19 10:27:59 +0300

    drivers:axi_core:jesd204:axi_adxcvr.* Add FSM ops
    Add JESD204 FSM ops for axi_adxcvr.c and axi_adxcvr.h.
    
    Signed-off-by: George Mois <george.mois@analog.com>

--- drivers/axi_core/jesd204/axi_adxcvr.h
+++ drivers/axi_core/jesd204/axi_adxcvr.h
@@ -127,21 +127,21 @@ struct adxcvr_init {
 /************************ Functions Declarations ******************************/
 /******************************************************************************/
 /** AXI ADXCVR DPR Port Read */
-int32_t adxcvr_drp_read(struct adxcvr *xcvr,
-			uint32_t drp_port,
-			uint32_t reg,
-			uint32_t *val);
+int adxcvr_drp_read(struct adxcvr *xcvr,
+		    unsigned int drp_port,
+		    unsigned int reg,
+		    unsigned int *val);
 /** AXI ADXCVR DPR Port Write */
-int32_t adxcvr_drp_write(struct adxcvr *xcvr,
-			 uint32_t drp_port,
-			 uint32_t reg,
-			 uint32_t val);
+int adxcvr_drp_write(struct adxcvr *xcvr,
+		     unsigned int drp_port,
+		     unsigned int reg,
+		     unsigned int val);
 /** AXI ADXCVRS Status Read */
 int32_t adxcvr_status_error(struct adxcvr *xcvr);
 /** AXI ADXCVR Clock Enable */
-int32_t adxcvr_clk_enable(struct adxcvr *xcvr);
+int adxcvr_clk_enable(struct adxcvr *xcvr);
 /** AXI ADXCVR Clock Disable */
-int32_t adxcvr_clk_disable(struct adxcvr *xcvr);
+int adxcvr_clk_disable(struct adxcvr *xcvr);
 /** AXI ADXCVR Device Initialization */
 int32_t adxcvr_init(struct adxcvr **ad_xcvr,
 		    const struct adxcvr_init *init);
