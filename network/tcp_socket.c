analogdevicesinc/no-OS

commit 44cdc3e89608f76a3eb5a59451aa648ef502d811

include:*.h Add "no_os_" prefix to /include files.
The modifications to the repo so that the files in
the "include" folder are renamed to have the
"no_os_" prefix.

Signed-off-by: George Mois <george.mois@analog.com>

network/tcp_socket.c
@@ -43,14 +43,14 @@
 /******************************************************************************/
 
 #include <stdlib.h>
-#include "no-os/error.h"
+#include "no_os_error.h"
 #include "tcp_socket.h"
-#include "no-os/util.h"
+#include "no_os_util.h"
 
 #ifndef DISABLE_SECURE_SOCKET
 #include "mbedtls/ssl.h"
 #include "noos_mbedtls_config.h"
-#include "no-os/trng.h"
+#include "no_os_trng.h"
 #endif /* DISABLE_SECURE_SOCKET */
 
 /******************************************************************************/

commit 57c5da340a81276e8344609c73d437bf1c5bcfe7

network: tcp_socket: fix config rng
Update the true random generator fill buffer function with the correct
naming.

Fixes: 44cdc3e ("include:*.h Add "no_os_" prefix to /include files")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

network/tcp_socket.c
@@ -234,7 +234,7 @@ static int32_t stcp_socket_init(struct secure_socket_desc **desc,
 	/* Config Random number generator */
 	mbedtls_ssl_conf_rng(&ldesc->conf,
 			     (int (*)(void *, unsigned char *, size_t))
-			     trng_fill_buffer,
+			     no_os_trng_fill_buffer,
 			     (void *)ldesc->trng);
 
 	/* Set the resulting protocol configuration */