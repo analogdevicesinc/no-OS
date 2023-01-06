bae757bc9b8869791d9a6a557db95f2f3fab5ca2
George Mois <george.mois@analog.com>
2023-01-06 15:13:35 +0200

    include:no_os_delay.h Add time struct & function
    Add a structure for storing time value for debug purposes (s, us) and
    get time function prototype.
    
    Signed-off-by: George Mois <george.mois@analog.com>

--- include/no_os_delay.h
+++ include/no_os_delay.h
@@ -46,6 +46,14 @@
 
 #include <stdint.h>
 
+/**
+ * @struct no_os_time
+ * @brief Structure holding time data (seconds, microseconds).
+ */
+struct no_os_time {
+	unsigned int s, us;
+};
+
 /******************************************************************************/
 /************************ Functions Declarations ******************************/
 /******************************************************************************/
@@ -55,5 +63,8 @@ void no_os_udelay(uint32_t usecs);
 
 /* Generate miliseconds delay. */
 void no_os_mdelay(uint32_t msecs);
+
+/* Get current time */
+struct no_os_time no_os_get_time(void);
 
 #endif // _NO_OS_DELAY_H_
