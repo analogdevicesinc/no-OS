a0d2e4a8f6dd09dd833523acfb5914cc936bd4e8
Stanca Pop <stanca.pop@analog.com>
2023-01-10 12:04:47 +0200

    ad7616: Remove spi engine from the ad7616 IP core

--- drivers/adc/ad7616/ad7616.c
+++ drivers/adc/ad7616/ad7616.c
@@ -44,7 +44,6 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "spi_engine.h"
 #include "axi_dmac.h"
 #include "no_os_gpio.h"
 #include "ad7616.h"
@@ -52,6 +51,10 @@
 #include "no_os_delay.h"
 #include "no_os_axi_io.h"
 #include "no_os_alloc.h"
+#include "no_os_pwm.h"
+#if (HDL_AD7616_PARALLEL == 0)
+#include "spi_engine.h"
+#endif
 
 /**
  * Read from device.
@@ -393,9 +396,13 @@ int32_t ad7616_read_data_serial(struct ad7616_dev *dev,
 	struct spi_engine_offload_message msg;
 	uint32_t spi_eng_msg_cmds[3] = {
 		CS_LOW,
-		READ(2),
+		READ(1),
 		CS_HIGH,
 	};
+
+	ret = no_os_pwm_enable(dev->trigger_pwm_desc);
+	if (ret != 0)
+		return ret;
 
 	dev->spi_desc->mode = NO_OS_SPI_MODE_3;
 	spi_engine_set_speed(dev->spi_desc, dev->spi_desc->max_speed_hz);
@@ -497,13 +504,11 @@ int32_t ad7616_core_setup(struct ad7616_dev *dev)
 	no_os_axi_io_write(dev->core_baseaddr, AD7616_REG_UP_CTRL, AD7616_CTRL_RESETN);
 	no_os_mdelay(10);
 
-	no_os_axi_io_read(dev->core_baseaddr, AD7616_REG_UP_IF_TYPE, &type);
-
-	if (type)
+	#if (HDL_AD7616_PARALLEL == 1)
 		dev->interface = AD7616_PARALLEL;
-	else
+	#else
 		dev->interface = AD7616_SERIAL;
-
+	#endif
 	return 0;
 }
 
@@ -593,10 +598,47 @@ int32_t ad7616_setup(struct ad7616_dev **device,
 	if (ret != 0)
 		return ret;
 
+	ret = no_os_pwm_init(&dev->trigger_pwm_desc, init_param->trigger_pwm_init);
+	if (ret != 0)
+		goto error_spi;
+
 	*device = dev;
 
 	if (!ret)
 		printf("AD7616 successfully initialized\n");
+
+	return ret;
+
+error_spi:
+	no_os_spi_remove(dev->spi_desc);
+
+error_dev:
+	free(dev);
+
+	return -1;
+}
+
+/**
+ * @brief Free the memory allocated by ad7616_setup().
+ * @param [in] dev - Pointer to the device handler.
+ * @return 0 in case of success, -1 otherwise
+ */
+int32_t ad7616_remove(struct ad7616_dev *dev)
+{
+	int32_t ret;
+
+	if (!dev)
+		return -1;
+
+	ret = no_os_pwm_remove(dev->trigger_pwm_desc);
+	if (ret != 0)
+		return ret;
+
+	ret = no_os_spi_remove(dev->spi_desc);
+	if (ret != 0)
+		return ret;
+
+	free(dev);
 
 	return ret;
 }
--- drivers/adc/ad7616/ad7616.h
+++ drivers/adc/ad7616/ad7616.h
@@ -41,6 +41,7 @@
 #define AD7616_H_
 
 #include "no_os_gpio.h"
+#include "no_os_pwm.h"
 
 /******************************************************************************/
 /********************** Macros and Constants Definitions **********************/
@@ -89,6 +90,9 @@
 #define AD7616_STATUS_A(x)				(((x) & 0xF) << 12)
 #define AD7616_STATUS_B(x)				(((x) & 0xF) << 8)
 #define AD7616_STATUS_CRC(x)			(((x) & 0xFF) << 0)
+
+/* AD7616_REG_PWM */
+#define AD7616_TRIGGER_PULSE_WIDTH_NS	        50
 
 /******************************************************************************/
 /*************************** Types Declarations *******************************/
@@ -151,6 +155,8 @@ struct ad7616_dev {
 	struct no_os_gpio_desc	*gpio_os0;
 	struct no_os_gpio_desc	*gpio_os1;
 	struct no_os_gpio_desc	*gpio_os2;
+	/** PWM */
+	struct no_os_pwm_desc *trigger_pwm_desc;
 	/* AXI Core */
 	uint32_t core_baseaddr;
 	/* Device Settings */
@@ -174,6 +180,8 @@ struct ad7616_init_param {
 	struct no_os_gpio_init_param		*gpio_os0_param;
 	struct no_os_gpio_init_param		*gpio_os1_param;
 	struct no_os_gpio_init_param		*gpio_os2_param;
+	/** PWM */
+	struct no_os_pwm_init_param             *trigger_pwm_init;
 	/* Core */
 	uint32_t			core_baseaddr;
 	/* Device Settings */
