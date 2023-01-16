904d2c2b4943d2ecc191cf5dd1b60c56351a1330
George Mois <george.mois@analog.com>
2023-01-16 17:26:11 +0200

    eval-adxl355-pmdz:src.mk Add init incs, srcs
    Add no_os_init.h to INCS and maxim_init.c to SRCS.
    
    Signed-off-by: George Mois <george.mois@analog.com>
    (cherry picked from commit bc295de5f82c280997b0e448dc368dc292cf2f86)

--- projects/eval-adxl355-pmdz/src.mk
+++ projects/eval-adxl355-pmdz/src.mk
@@ -23,7 +23,8 @@ INCS += $(INCLUDE)/no_os_delay.h     \
 		$(INCLUDE)/no_os_uart.h      \
 		$(INCLUDE)/no_os_lf256fifo.h \
 		$(INCLUDE)/no_os_util.h \
-		$(INCLUDE)/no_os_units.h
+		$(INCLUDE)/no_os_units.h \
+		$(INCLUDE)/no_os_init.h
 
 SRCS += $(DRIVERS)/api/no_os_gpio.c \
 		$(DRIVERS)/api/no_os_i2c.c  \
