analogdevicesinc/no-OS

commit 22ffa3c1376fdd68e2f518d5d987a097d34f7d5c

drivers: accel: adxl355: Add delay after soft reset
Add delay after softreset before reading shadows registers.

Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

drivers/accel/adxl355/adxl355.c
@@ -311,6 +311,9 @@ int adxl355_soft_reset(struct adxl355_dev *dev)
 	if ((!nb_of_retries) || ret)
 		return -EAGAIN;
 
+	// Delay is needed between soft reset command and shadow registers reading
+	no_os_mdelay(1);
+
 	// Read the shadow registers
 	ret = adxl355_read_device_data(dev,
 				       ADXL355_ADDR(ADXL355_SHADOW_REGISTER_BASE_ADDR),

commit 900c6b7e72ebd9efda32993d5ede031d6b35e021

drivers: accel: adxl355: Include no_os_delay.h
Include no_os_delay.h in adxl355.c

Fixes: 22ffa3c1 ("drivers: accel: adxl355: Add delay after soft reset")
Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

drivers/accel/adxl355/adxl355.c
@@ -43,6 +43,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include "adxl355.h"
+#include "no_os_delay.h"
 
 /******************************************************************************/
 /************************ Variable Declarations ******************************/