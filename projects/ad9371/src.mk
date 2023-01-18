da6dd7b1efa37e01650dd044fa899288e0fabc7e
George Mois <george.mois@analog.com>
2023-01-18 22:38:55 +0200

    ad9371:src.mk Add jesd204.h to incs
    Add the file jesd204.h to incs.
    
    Signed-off-by: George Mois <george.mois@analog.com>

--- projects/ad9371/src.mk
+++ projects/ad9371/src.mk
@@ -31,7 +31,9 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
 	$(NO-OS)/util/no_os_util.c \
 	$(DRIVERS)/api/no_os_spi.c \
-	$(DRIVERS)/api/no_os_gpio.c
+	$(DRIVERS)/api/no_os_gpio.c \
+	$(NO-OS)/jesd204/jesd204-core.c \
+	$(NO-OS)/jesd204/jesd204-fsm.c
 ifeq (xilinx,$(strip $(PLATFORM)))
 SRCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
@@ -102,7 +104,9 @@ INCS +=	$(INCLUDE)/no_os_axi_io.h \
 	$(INCLUDE)/no_os_error.h \
 	$(INCLUDE)/no_os_delay.h \
 	$(INCLUDE)/no_os_util.h \
-	$(INCLUDE)/no_os_print_log.h
+	$(INCLUDE)/no_os_print_log.h \
+	$(INCLUDE)/jesd204.h \
+	$(NO-OS)/jesd204/jesd204-priv.h
 ifeq (y,$(strip $(TINYIIOD)))
 INCS += $(INCLUDE)/no_os_fifo.h \
 	$(INCLUDE)/no_os_irq.h \
