ff008a389e526d5be38e00bfaba9acd9f71526e6
George Mois <george.mois@analog.com>
2023-01-20 14:08:59 +0200

    eval-adxl313z:src:platform:maxim Maxim support
    Add Maxim platform files for the eval-adxl313z project.
    
    Signed-off-by: George Mois <george.mois@analog.com>

---
+++ projects/eval-adxl313z/src/platform/maxim/parameters.h
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ *   @file   parameters.h
+ *   @brief  Definitions specific to Maxim platform used by eval-adxl313z
+ *           project.
+ *   @author GMois (george.mois@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef __PARAMETERS_H__
+#define __PARAMETERS_H__
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "maxim_irq.h"
+#include "maxim_spi.h"
+#include "maxim_gpio.h"
+#include "maxim_uart.h"
+#include "maxim_uart_stdio.h"
+
+/******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+#ifdef IIO_SUPPORT
+#define INTC_DEVICE_ID  0
+#define UART_IRQ_ID     UART0_IRQn
+#endif
+
+#define UART_DEVICE_ID  0
+#define UART_BAUDRATE   57600
+#define UART_EXTRA      &xuip
+#define UART_OPS        &max_uart_ops
+
+#if (TARGET_NUM == 78000)
+#define SPI_DEVICE_ID   1
+#define SPI_CS          1
+#else
+#define SPI_DEVICE_ID   1
+#define SPI_CS          0
+#endif
+
+#define SPI_BAUDRATE    1000000
+#define SPI_OPS         &max_spi_ops
+#define SPI_EXTRA       &xsip
+
+extern struct max_uart_init_param xuip;
+extern struct max_spi_init_param xsip;
+
+#endif /* __PARAMETERS_H__ */
