analogdevicesinc/no-OS

commit 2b2c0bb68c7f69337c7268009e8ab701556f897a

drivers:platform:maxim: Allow SPI vddio selection
Add the vssel field, so that the VDDIO may be configured for each SPI
interface. Adapt the projects accordingly.

Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/platform/maxim/max32650/maxim_spi.c
@@ -64,18 +64,21 @@
 /******************************************************************************/
 
 /**
- * @brief Enable the chip select gpio on MAX32665 or MAX32650
- * @param spi - The SPI port registers
- * @param chip_select - cs index
+ * @brief Configure the VDDIO level for a SPI interface
+ * @param desc - the SPI descriptor
  * @return 0 in case of success, -EINVAL otherwise
  */
-static int32_t _max_spi_enable_ss(uint32_t id, uint32_t chip_select)
+static int32_t _max_spi_config_pins(struct no_os_spi_desc *desc)
 {
+	struct max_spi_state *st = desc->extra;
+	mxc_gpio_cfg_t spi_pins;
 	mxc_gpio_cfg_t cs;
 
-	switch(id) {
+	switch(desc->device_id) {
 	case 0:
-		switch(chip_select) {
+		spi_pins = gpio_cfg_spi0_1;
+
+		switch(desc->chip_select) {
 		case 0:
 			cs = gpio_cfg_spi0_0;
 			break;
@@ -84,7 +87,9 @@ static int32_t _max_spi_enable_ss(uint32_t id, uint32_t chip_select)
 		}
 		break;
 	case 1:
-		switch(chip_select) {
+		spi_pins = gpio_cfg_spi1;
+
+		switch(desc->chip_select) {
 		case 0:
 			cs = gpio_cfg_spi1_ss0;
 			break;
@@ -103,7 +108,9 @@ static int32_t _max_spi_enable_ss(uint32_t id, uint32_t chip_select)
 		break;
 
 	case 2:
-		switch(chip_select) {
+		spi_pins = gpio_cfg_spi2;
+
+		switch(desc->chip_select) {
 		case 0:
 			cs = gpio_cfg_spi2_ss0;
 			break;
@@ -121,7 +128,9 @@ static int32_t _max_spi_enable_ss(uint32_t id, uint32_t chip_select)
 		}
 		break;
 	case 3:
-		switch(chip_select) {
+		spi_pins = gpio_cfg_spi3;
+
+		switch(desc->chip_select) {
 		case 0:
 			cs = gpio_cfg_spi2_ss0;
 			break;
@@ -142,6 +151,9 @@ static int32_t _max_spi_enable_ss(uint32_t id, uint32_t chip_select)
 		return -EINVAL;
 	}
 
+	spi_pins.vssel = st->init_param->vssel;
+	spi_pins.cs = st->init_param->vssel;
+	MXC_GPIO_Config(&spi_pins);
 	MXC_GPIO_Config(&cs);
 
 	return 0;
@@ -240,11 +252,9 @@ static int _max_spi_config(struct no_os_spi_desc *desc)
 		goto err_init;
 	}
 
-	ret = _max_spi_enable_ss(desc->device_id, desc->chip_select);
-	if (ret) {
-		ret = -EINVAL;
+	ret = _max_spi_config_pins(desc);
+	if (ret)
 		goto err_init;
-	}
 
 	ret = MXC_SPI_SetMode(MXC_SPI_GET_SPI(desc->device_id), desc->mode);
 	if (ret) {

drivers/platform/maxim/max32655/maxim_spi.c
@@ -134,6 +134,11 @@ error:
 	spi->sstime = sstime_cache;
 }
 
+/**
+ * @brief Configure a SPI peripheral
+ * @param spi - The SPI descriptor which stores the configuration parameters.
+ * @return 0 in case of success, a negative error code otherwise
+ */
 static int _max_spi_config(struct no_os_spi_desc *desc)
 {
 	struct max_spi_init_param *eparam;
@@ -152,7 +157,7 @@ static int _max_spi_config(struct no_os_spi_desc *desc)
 		.mosi = true,
 		.sdio2 = false,
 		.sdio3 = false,
-		.vddioh = true,
+		.vddioh = st->init_param->vssel,
 	};
 
 	ret = MXC_SPI_Init(MXC_SPI_GET_SPI(desc->device_id), SPI_MASTER_MODE,

drivers/platform/maxim/max32660/maxim_spi.c
@@ -134,11 +134,17 @@ error:
 	spi->ss_time = sstime_cache;
 }
 
+/**
+ * @brief Configure the VDDIO level for a SPI interface
+ * @param desc - the SPI descriptor
+ * @return 0 in case of success, -EINVAL otherwise
+ */
 static int _max_spi_config(struct no_os_spi_desc *desc)
 {
 	int32_t ret;
 	struct max_spi_state *st;
 	struct max_spi_init_param *eparam;
+	mxc_gpio_cfg_t spi_pins = gpio_cfg_spi0;
 
 	st = desc->extra;
 	eparam = st->init_param;
@@ -150,6 +156,9 @@ static int _max_spi_config(struct no_os_spi_desc *desc)
 		ret = -EINVAL;
 		goto err_init;
 	}
+
+	spi_pins.vssel = eparam->vssel;
+	MXC_GPIO_Config(&spi_pins);
 
 	ret = MXC_SPI_SetMode(MXC_SPI_GET_SPI(desc->device_id),
 			      (mxc_spi_mode_t)desc->mode);

drivers/platform/maxim/max32665/maxim_spi.c
@@ -64,18 +64,21 @@
 /******************************************************************************/
 
 /**
- * @brief Enable the chip select gpio on MAX32665 or MAX32650
- * @param spi - The SPI port registers
- * @param chip_select - cs index
+ * @brief Configure the VDDIO level for a SPI interface
+ * @param desc - the SPI descriptor
  * @return 0 in case of success, -EINVAL otherwise
  */
-static int32_t _max_spi_enable_ss(uint32_t id, uint32_t chip_select)
+static int32_t _max_spi_config_pins(struct no_os_spi_desc *desc)
 {
+	struct max_spi_state *st = desc->extra;
+	mxc_gpio_cfg_t spi_pins;
 	mxc_gpio_cfg_t cs;
 
-	switch(id) {
+	switch(desc->device_id) {
 	case 0:
-		switch(chip_select) {
+		spi_pins = gpio_cfg_spi0a;
+
+		switch(desc->chip_select) {
 		case 0:
 			cs = gpio_cfg_spi0_ss0a;
 			break;
@@ -90,7 +93,9 @@ static int32_t _max_spi_enable_ss(uint32_t id, uint32_t chip_select)
 		}
 		break;
 	case 1:
-		switch(chip_select) {
+		spi_pins = gpio_cfg_spi1;
+
+		switch(desc->chip_select) {
 		case 0:
 			cs = gpio_cfg_spi1_ss0;
 			break;
@@ -106,7 +111,9 @@ static int32_t _max_spi_enable_ss(uint32_t id, uint32_t chip_select)
 		break;
 
 	case 2:
-		switch(chip_select) {
+		spi_pins = gpio_cfg_spi2;
+
+		switch(desc->chip_select) {
 		case 0:
 			cs = gpio_cfg_spi2_ss0;
 			break;
@@ -122,7 +129,7 @@ static int32_t _max_spi_enable_ss(uint32_t id, uint32_t chip_select)
 		break;
 #ifdef MXC_SPI3
 	case 3:
-		switch(chip_select) {
+		switch(desc->chip_select) {
 		case 0:
 			cs = gpio_cfg_spi2_ss0;
 			break;
@@ -141,6 +148,9 @@ static int32_t _max_spi_enable_ss(uint32_t id, uint32_t chip_select)
 		return -EINVAL;
 	}
 
+	spi_pins.vssel = st->init_param->vssel;
+	cs.vssel = st->init_param->vssel;
+	MXC_GPIO_Config(&spi_pins);
 	MXC_GPIO_Config(&cs);
 
 	return 0;
@@ -234,11 +244,9 @@ static int _max_spi_config(struct no_os_spi_desc *desc)
 		goto err_init;
 	}
 
-	ret = _max_spi_enable_ss(desc->device_id, desc->chip_select);
-	if (ret) {
-		ret = -EINVAL;
+	ret = _max_spi_config_pins(desc);
+	if (ret)
 		goto err_init;
-	}
 
 	ret = MXC_SPI_SetMode(MXC_SPI_GET_SPI(desc->device_id),
 			      (mxc_spi_mode_t)desc->mode);

drivers/platform/maxim/max78000/maxim_spi.c
@@ -152,7 +152,7 @@ static int _max_spi_config(struct no_os_spi_desc *desc)
 		.mosi = true,
 		.sdio2 = false,
 		.sdio3 = false,
-		.vddioh = true,
+		.vddioh = eparam->vssel,
 	};
 
 	ret = MXC_SPI_Init(MXC_SPI_GET_SPI(desc->device_id), SPI_MASTER_MODE,

drivers/platform/maxim/max32650/maxim_spi.h
@@ -41,6 +41,7 @@
 #define MAXIM_SPI_H_
 
 #include <stdint.h>
+#include "max32650.h"
 
 /**
  * @brief maxim specific SPI platform ops structure
@@ -55,6 +56,7 @@ enum spi_ss_polarity {
 struct max_spi_init_param {
 	uint32_t numSlaves;
 	enum spi_ss_polarity polarity;
+	mxc_gpio_vssel_t vssel;
 };
 
 struct max_spi_state {

drivers/platform/maxim/max32655/maxim_spi.h
@@ -41,6 +41,7 @@
 #define MAXIM_SPI_H_
 
 #include <stdint.h>
+#include "max32655.h"
 
 /**
  * @brief maxim specific SPI platform ops structure
@@ -55,6 +56,7 @@ enum spi_ss_polarity {
 struct max_spi_init_param {
 	uint32_t numSlaves;
 	enum spi_ss_polarity polarity;
+	mxc_gpio_vssel_t vssel;
 };
 
 struct max_spi_state {

drivers/platform/maxim/max32660/maxim_spi.h
@@ -41,6 +41,7 @@
 #define MAXIM_SPI_H_
 
 #include <stdint.h>
+#include "max32660.h"
 
 /**
  * @brief maxim specific SPI platform ops structure
@@ -55,6 +56,7 @@ enum spi_ss_polarity {
 struct max_spi_init_param {
 	uint32_t numSlaves;
 	enum spi_ss_polarity polarity;
+	mxc_gpio_vssel_t vssel;
 };
 
 struct max_spi_state {

commit c873d1398247ce55fa36798afc9198a79f2508d3

platform: maxim: max32650: CS vssel fix
Fix the CS pin vssel selection for the MAX32650.

Fixes: 2b2c0bb6 ("drivers:platform:maxim: Allow SPI vddio selection")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/platform/maxim/max32650/maxim_spi.c
@@ -152,7 +152,7 @@ static int32_t _max_spi_config_pins(struct no_os_spi_desc *desc)
 	}
 
 	spi_pins.vssel = st->init_param->vssel;
-	spi_pins.cs = st->init_param->vssel;
+	cs.vssel = st->init_param->vssel;
 	MXC_GPIO_Config(&spi_pins);
 	MXC_GPIO_Config(&cs);
 