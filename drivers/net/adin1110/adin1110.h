6f55d51001a4c0383e66e39f8b6dea1f99b21519
Ciprian Regus <ciprian.regus@analog.com>
2023-01-27 15:07:09 +0200

    rx and tx working on adin2111

--- drivers/net/adin1110/adin1110.h
+++ drivers/net/adin1110/adin1110.h
@@ -1,3 +1,42 @@
+/******************************************************************************
+ *   @file   adin1110.h
+ *   @brief  Header file of the ADIN1110 driver.
+ *   @author Ciprian Regus (ciprian.regus@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
 #ifndef _ADIN1110_H
 #define _ADIN1110_H
 
@@ -6,13 +45,18 @@
 #include "no_os_gpio.h"
 #include "no_os_util.h"
 
+/* The maximum length of an Ethernet frame */
+#define ADIN1110_BUFF_LEN			1522
+#define ADIN1110_ETH_ALEN			6
+#define ADIN1110_ETHERTYPE_LEN			2
+
 #define ADIN1110_FCS_LEN			4
 #define ADIN1110_MAC_LEN			6
 
 #define ADIN1110_ADDR_MASK			NO_OS_GENMASK(12, 0)
 #define ADIN1110_RD_FRAME_SIZE			7
 #define ADIN1110_WR_FRAME_SIZE			6
-#define ADIN1110_PHY_ID_REG			0x1
+#define ADIN1110_PHY_ID_REG			1
 
 #define ADIN1110_PHY_ID				0x0283BC91
 #define ADIN2111_PHY_ID				0x0283BCA1
@@ -24,7 +68,7 @@
 #define ADIN1110_RW_MASK			NO_OS_BIT(13)
 
 #define ADIN1110_SOFT_RST_REG			0x3C
-#define ADIN1110_RESET				0x03
+#define ADIN1110_RESET_REG			0x03
 #define ADIN1110_SWRESET			NO_OS_BIT(0)
 #define ADIN1110_SWRESET_KEY1			0x4F1C
 #define ADIN1110_SWRESET_KEY2			0xC1F4
@@ -34,24 +78,24 @@
 #define ADIN1110_SPI_CD				NO_OS_BIT(7)
 #define ADIN1110_SPI_RW             		NO_OS_BIT(5)
 
-#define ADIN1110_CONFIG1			0x04
+#define ADIN1110_CONFIG1_REG			0x04
 #define ADIN1110_CONFIG1_SYNC			NO_OS_BIT(15)
 
-#define ADIN1110_CONFIG2			0x06
+#define ADIN1110_CONFIG2_REG			0x06
 #define ADIN2111_P2_FWD_UNK2HOST_MASK		NO_OS_BIT(12)
 #define ADIN2111_PORT_CUT_THRU_EN		NO_OS_BIT(11)
 #define ADIN1110_CRC_APPEND			NO_OS_BIT(5)
 #define ADIN1110_FWD_UNK2HOST_MASK		NO_OS_BIT(2)
 
-#define ADIN1110_STATUS0			0x08
+#define ADIN1110_STATUS0_REG			0x08
 #define ADIN1110_RESETC_MASK			NO_OS_BIT(6)
 
-#define ADIN1110_STATUS1			0x09
+#define ADIN1110_STATUS1_REG			0x09
 #define ADIN2111_P2_RX_RDY			NO_OS_BIT(17)
 #define ADIN1110_SPI_ERR			NO_OS_BIT(10)
 #define ADIN1110_RX_RDY			NO_OS_BIT(4)
 
-#define ADIN1110_IMASK1				0x0D
+#define ADIN1110_IMASK1_REG				0x0D
 #define ADIN2111_RX_RDY_IRQ			NO_OS_BIT(17)
 #define ADIN1110_SPI_ERR_IRQ			NO_OS_BIT(10)
 #define ADIN1110_RX_RDY_IRQ			NO_OS_BIT(4)
@@ -69,16 +113,16 @@
 #define ADIN1110_MMD_ACR_DEVAD_MASK		NO_OS_GENMASK(4, 0)
 #define ADIN1110_MMD_ACR_FUNCTION_MASK		NO_OS_GENMASK(15, 14)
 #define ADIN1110_MMD_ACCESS_MASK		NO_OS_GENMASK(15, 0)
-#define ADIN1110_MMD_ACCESS_CNTRL		0x0D
-#define ADIN1110_MMD_ACCESS			0x0E
+#define ADIN1110_MMD_ACCESS_CTRL_REG		0x0D
+#define ADIN1110_MMD_ACCESS_REG			0x0E
 
 #define ADIN1110_MI_SFT_PD_MASK			NO_OS_BIT(11)
 #define ADIN1110_MDIO_PHY_ID(x)			((x) + 1)
 #define ADIN1110_MI_CONTROL_REG			0x0
 
-#define ADIN1110_TX_FSIZE			0x30
-#define ADIN1110_TX				0x31
-#define ADIN1110_TX_SPACE			0x32
+#define ADIN1110_TX_FSIZE_REG			0x30
+#define ADIN1110_TX_REG				0x31
+#define ADIN1110_TX_SPACE_REG			0x32
 
 #define ADIN1110_MAC_RST_STATUS_REG		0x3B
 
@@ -87,28 +131,28 @@
 #define ADIN2111_MAC_ADDR_TO_OTHER_PORT		NO_OS_BIT(17)
 #define ADIN1110_MAC_ADDR_TO_HOST		NO_OS_BIT(16)
 
-#define ADIN1110_MAC_ADDR_FILTER_UPR		0x50
-#define ADIN1110_MAC_ADDR_FILTER_LWR		0x51
+#define ADIN1110_MAC_ADDR_FILTER_UPR_REG	0x50
+#define ADIN1110_MAC_ADDR_FILTER_LWR_REG	0x51
 
-#define ADIN1110_MAC_ADDR_MASK_UPR		0x70
-#define ADIN1110_MAC_ADDR_MASK_LWR		0x71
+#define ADIN1110_MAC_ADDR_MASK_UPR_REG		0x70
+#define ADIN1110_MAC_ADDR_MASK_LWR_REG		0x71
 #define ADIN1110_MAC_P1_ADDR_SLOT		2
 #define ADIN2110_MAC_P2_ADDR_SLOT		3
 
-#define ADIN1110_RX_FRM_CNT		0xA0
-#define ADIN1110_RX_CRC_ERR_CNT		0xA4
-#define ADIN1110_RX_ALGN_ERR_CNT	0xA5
-#define ADIN1110_RX_LS_ERR_CNT		0xA6
-#define ADIN1110_RX_PHY_ERR_CNT		0xA7
+#define ADIN1110_RX_FRM_CNT_REG			0xA0
+#define ADIN1110_RX_CRC_ERR_CNT_REG		0xA4
+#define ADIN1110_RX_ALGN_ERR_CNT_REG		0xA5
+#define ADIN1110_RX_LS_ERR_CNT_REG		0xA6
+#define ADIN1110_RX_PHY_ERR_CNT_REG		0xA7
 
-#define ADIN1110_RX_DROP_FULL_CNT   0xAC
-#define ADIN1110_RX_DROP_FILT_CNT   0xAD
+#define ADIN1110_RX_DROP_FULL_CNT_REG		0xAC
+#define ADIN1110_RX_DROP_FILT_CNT_REG		0xAD
 
-#define ADIN1110_RX_FSIZE			0x90
-#define ADIN1110_RX				0x91
+#define ADIN1110_RX_FSIZE_REG			0x90
+#define ADIN1110_RX_REG				0x91
 
-#define ADIN2111_RX_P2_FSIZE			0xC0
-#define ADIN2111_RX_P2				0xC1
+#define ADIN2111_RX_P2_FSIZE_REG		0xC0
+#define ADIN2111_RX_P2_REG			0xC1
 
 #define ADIN1110_CLEAR_STATUS0			0xFFF
 
@@ -140,12 +184,6 @@ enum adin1110_chip_id {
     ADIN2111,
 };
 
-enum adin1110_ethertype {
-	ADIN1110_IPV4 = 0x0800,
-	ADIN1110_ARP = 0x0806,
-	ADIN1110_IPV6 = 0x86DD,
-};
-
 enum adin1110_mmd_acr_func {
 	adin1110_address_func,
 	adin1110_data_func,
@@ -153,12 +191,10 @@ enum adin1110_mmd_acr_func {
 
 struct adin1110_desc {
     enum adin1110_chip_id chip_type;
-    uint32_t ports;
-
     struct no_os_spi_desc *comm_desc;
-    uint8_t mac_address[6];
-    uint8_t rx_buff[1700];
-    uint8_t tx_buff[1700];
+    uint8_t mac_address[ADIN1110_ETH_ALEN];
+    uint8_t rx_buff[ADIN1110_BUFF_LEN];
+    uint8_t tx_buff[ADIN1110_BUFF_LEN];
 
     struct no_os_gpio_desc *reset_gpio;
     bool append_crc;
@@ -168,23 +204,21 @@ struct adin1110_init_param {
 	enum adin1110_chip_id chip_type; 
 	struct no_os_spi_init_param comm_param;
 	struct no_os_gpio_init_param reset_param;
-	uint8_t mac_address[6];
-
+	uint8_t mac_address[ADIN1110_ETH_ALEN];
 	bool append_crc;
 };
 
-struct adin1110_sk_buff {
-	uint8_t mac_source[6];
-	uint8_t mac_destination[6];
-	enum adin1110_ethertype ethertype;
-	uint32_t buff_len;
+struct adin1110_eth_buff {
+	uint8_t mac_source[ADIN1110_ETH_ALEN];
+	uint8_t mac_destination[ADIN1110_ETH_ALEN];
+	uint16_t ethertype;
+	uint32_t payload_len;
 	uint8_t *payload;
 };
 
-int adin1110_mdio_write_c45(struct adin1110_desc *desc, uint32_t phy_id,
-				   uint32_t dev_id, uint32_t reg, uint32_t data);
-int adin1110_mdio_read_c45(struct adin1110_desc *desc, uint32_t phy_id,
-				  uint32_t dev_id, uint32_t reg, uint32_t *data);
+int adin1110_sw_reset(struct adin1110_desc *desc);
+int adin1110_mdio_write_c45(struct adin1110_desc *, uint32_t, uint32_t, uint32_t, uint32_t);
+int adin1110_mdio_read_c45(struct adin1110_desc *, uint32_t, uint32_t, uint32_t, uint32_t *);
 int adin1110_write_mmd(struct adin1110_desc *, uint32_t , uint32_t , uint32_t, uint32_t);
 int adin1110_read_mmd(struct adin1110_desc *, uint32_t , uint32_t , uint32_t, uint32_t *);
 int adin1110_mdio_write(struct adin1110_desc *, uint32_t, uint32_t, uint32_t);
@@ -192,8 +226,8 @@ int adin1110_mdio_read(struct adin1110_desc *, uint32_t, uint32_t, uint32_t *);
 int adin1110_reg_update(struct adin1110_desc *, uint16_t, uint32_t, uint32_t);
 int adin1110_reg_write(struct adin1110_desc *, uint16_t, uint32_t);
 int adin1110_reg_read(struct adin1110_desc *, uint16_t, uint32_t *);
-int adin1110_write_fifo(struct adin1110_desc *, struct adin1110_sk_buff *);
-int adin1110_read_fifo(struct adin1110_desc *, struct adin1110_sk_buff *);
+int adin1110_write_fifo(struct adin1110_desc *, uint32_t, struct adin1110_eth_buff *);
+int adin1110_read_fifo(struct adin1110_desc *, uint32_t, struct adin1110_eth_buff *);
 int adin1110_set_promisc(struct adin1110_desc *, uint32_t , bool);
 int adin1110_init(struct adin1110_desc **, struct adin1110_init_param *);
 int adin1110_remove(struct adin1110_desc *);
