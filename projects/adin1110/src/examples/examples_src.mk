49983f47acd4800d1ac8ec0323dab9f7bca5ebf2
Ciprian Regus <ciprian.regus@analog.com>
2023-01-30 11:06:06 +0200

    projects: adin1110: Add ADIN1110 example project
    Add an example project which showcases the ethernet frames receive and
    transmit functionality. Every 5 seconds a frame (payload filled with 0x44)
    is broadcasted, and incoming frames are read (the device is set in
    promiscuous mode, so there are no mac filters).
    
    This is implemented for the ADIN2111. The targets are MAX32650 and
    STM32F7.
    
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

---
+++ projects/adin1110/src/examples/examples_src.mk
@@ -0,0 +1,8 @@
+ifeq (y,$(strip $(FRAME_RX_TX)))
+CFLAGS += -DFRAME_RX_TX=1
+SRCS += $(PROJECT)/src/examples/frame_rx_tx/frame_rx_tx_example.c
+INCS += $(PROJECT)/src/examples/frame_rx_tx/frame_rx_tx_example.h
+endif
+
+INCS += $(DRIVERS)/net/adin1110/adin1110.h
+SRCS += $(DRIVERS)/net/adin1110/adin1110.c
