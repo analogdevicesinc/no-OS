analogdevicesinc/no-OS

commit d8428150ffa73d729fb4f5eea78c150b1d8422a2

tools:scripts: Move libraries code to libraries.mk
libraries.mk will be then included by generic.mk and all libraries can
be used with the other makefiles.

Signed-off-by: Mihail Chindris <mihail.chindris@analog.com>

tools/scripts/libraries.mk
@@ -1,0 +1,117 @@
+# File where libraries are handled
+
+#	IIO
+ifneq ($(if $(findstring iio, $(LIBRARIES)), 1),)
+# Generic part
+IIO_DIR						= $(NO-OS)/libraries/iio
+IIO_LIB						= $(IIO_DIR)/libiio.a
+EXTRA_LIBS					+= $(IIO_LIB)
+EXTRA_LIBS_PATHS			+= $(IIO_DIR)
+EXTRA_LIBS_INCLUDE_PATHS	+= $(IIO_DIR)
+ifeq ($(wildcard $(IIO_DIR)/libtinyiiod/.git),)
+INIT_SUBMODULES				+= git submodule update --init --remote -- $(IIO_DIR)/libtinyiiod;
+endif
+
+# Rules
+CLEAN_IIO	= $(MAKE) -C $(IIO_DIR) clean
+$(IIO_LIB):
+	$(MAKE) -C $(IIO_DIR)
+
+# Custom settings
+CFLAGS += -DIIO_SUPPORT
+endif
+
+#	MBEDTLS
+ifneq ($(if $(findstring mbedtls, $(LIBRARIES)), 1),)
+# Generic part
+MBEDTLS_DIR					= $(NO-OS)/libraries/mbedtls
+MBEDTLS_LIB_DIR				= $(MBEDTLS_DIR)/library
+MBEDTLS_LIB_NAMES			= libmbedtls.a libmbedx509.a libmbedcrypto.a
+MBEDTLS_LIBS				= $(addprefix $(MBEDTLS_LIB_DIR)/,$(MBEDTLS_LIB_NAMES))
+EXTRA_LIBS					+= $(MBEDTLS_LIBS)
+EXTRA_LIBS_PATHS			+= $(MBEDTLS_LIB_DIR)
+EXTRA_LIBS_INCLUDE_PATHS	+= $(MBEDTLS_DIR)/include
+ifeq ($(wildcard $(MBEDTLS_DIR)/.git),)
+INIT_SUBMODULES				+= git submodule update --init --remote -- $(MBEDTLS_DIR);
+endif
+
+#Rules
+MBED_TLS_CONFIG_FILE = $(NO-OS)/network/noos_mbedtls_config.h
+CLEAN_MBEDTLS	= $(call remove_fun,$(MBEDTLS_LIB_DIR)/*.o $(MBEDTLS_LIBS))
+$(MBEDTLS_LIB_DIR)/libmbedcrypto.a: $(MBED_TLS_CONFIG_FILE)
+	-$(CLEAN_MBEDTLS)
+	$(MAKE) -C $(MBEDTLS_LIB_DIR)
+$(MBEDTLS_LIB_DIR)/libmbedx509.a: $(MBEDTLS_LIB_DIR)/libmbedcrypto.a
+$(MBEDTLS_LIB_DIR)/libmbedtls.a: $(MBEDTLS_LIB_DIR)/libmbedx509.a
+
+# Custom settings
+CFLAGS 		+= -I$(dir $(MBED_TLS_CONFIG_FILE)) \
+			-DMBEDTLS_CONFIG_FILE=\"$(notdir $(MBED_TLS_CONFIG_FILE))\"
+else
+DISABLE_SECURE_SOCKET ?= y
+endif
+
+#	FATFS
+ifneq ($(if $(findstring fatfs, $(LIBRARIES)), 1),)
+# Generic part
+FATFS_DIR					= $(NO-OS)/libraries/fatfs
+FATFS_LIB					= $(FATFS_DIR)/libfatfs.a
+EXTRA_LIBS					+= $(FATFS_LIB)
+EXTRA_LIBS_PATHS			+= $(FATFS_DIR)
+EXTRA_LIBS_INCLUDE_PATHS	+= $(FATFS_DIR)/source
+
+# Rules
+CLEAN_FATFS	= $(MAKE) -C $(NO-OS)/libraries/fatfs clean
+$(FATFS_LIB):
+	$(MAKE) -C $(FATFS_DIR)
+
+# Custom settings
+CFLAGS		+= -I$(DRIVERS)/sd-card -I$(INCLUDE)
+
+endif
+
+#	MQTT
+ifneq ($(if $(findstring mqtt, $(LIBRARIES)), 1),)
+# Generic part
+MQTT_DIR					= $(NO-OS)/libraries/mqtt
+MQTT_LIB					= $(MQTT_DIR)/libmqtt.a
+EXTRA_LIBS					+= $(MQTT_LIB)
+EXTRA_LIBS_PATHS			+= $(MQTT_DIR)
+EXTRA_LIBS_INCLUDE_PATHS	+= $(MQTT_DIR)
+
+CLEAN_MQTT	= $(MAKE) -C $(MQTT_DIR) clean
+$(MQTT_LIB):
+	$(MAKE) -C $(MQTT_DIR)
+
+endif
+
+LIB_TARGETS			+= $(IIO_LIB) $(MBEDTLS_TARGETS) $(FATFS_LIB) $(MQTT_LIB)
+EXTRA_LIBS_NAMES	= $(subst lib,,$(basename $(notdir $(EXTRA_LIBS))))
+LIB_FLAGS			= $(addprefix -l,$(EXTRA_LIBS_NAMES))
+LIB_DIR_FLAGS		= $(addprefix -L,$(EXTRA_LIBS_PATHS))
+INCLUDE_DIRS		+= $(EXTRA_LIBS_INCLUDE_PATHS)
+
+#Convert variable to linux.mk naming
+ifneq (aducm3029,$(strip $(PLATFORM)))
+INC_PATHS += $(addprefix -I,$(INCLUDE_DIRS))
+LIB_PATHS += $(LIB_DIR_FLAGS)
+LIBS += $(LIB_FLAGS)
+endif
+
+# Build project Release Configuration
+PHONY := libs
+ifneq ($(INIT_SUBMODULES),)
+libs:
+	$(INIT_SUBMODULES)
+	@$(MAKE) libs
+else
+libs: $(LIB_TARGETS)
+	
+endif
+
+PHONY += clean_libs
+clean_libs:
+	-$(CLEAN_MBEDTLS)
+	-$(CLEAN_FATFS)
+	-$(CLEAN_MQTT)
+	-$(CLEAN_IIO)

tools/scripts/aducm.mk
@@ -144,94 +144,6 @@ include src.mk
 endif
 
 #------------------------------------------------------------------------------
-#                     SETTING LIBRARIES IF NEEDED                              
-#------------------------------------------------------------------------------
-
-#	MBEDTLS
-ifneq (y,$(strip $(DISABLE_SECURE_SOCKET)))
-#If network dir is included, mbedtls will be used
-ifneq ($(if $(findstring $(NO-OS)/network, $(SRC_DIRS)), 1),)
-
-MBEDTLS_PATH		= $(NO-OS)/libraries/mbedtls
-MBEDTLS_LIB_DIR		= $(MBEDTLS_PATH)/library
-MBEDTLS_LIB_NAMES	= libmbedtls.a libmbedx509.a  libmbedcrypto.a
-
-LIBS_DIRS	+= "$(MBEDTLS_LIB_DIR)"
-LIBS		+= $(MBEDTLS_LIB_NAMES)
-INCLUDE_DIRS 	+= $(MBEDTLS_PATH)/include
-
-MBED_TLS_CONFIG_FILE = $(NO-OS)/network/noos_mbedtls_config.h
-
-#Add to CFAGS the config file name and the directory where it is located
-CFLAGS 		+= -I$(dir $(MBED_TLS_CONFIG_FILE)) \
-			-DMBEDTLS_CONFIG_FILE=\"$(notdir $(MBED_TLS_CONFIG_FILE))\"
-
-MBEDTLS_TARGETS	= $(addprefix $(MBEDTLS_LIB_DIR)/,$(MBEDTLS_LIB_NAMES))
-
-ifeq ($(wildcard $(MBEDTLS_PATH)/LICENSE),)
-MBED_TLS_INIT = git submodule update --init --remote -- $(MBEDTLS_PATH)
-endif
-
-#Workaround for cleaning because mbedtls clean don't work on windows enviroment
-CLEAN_MBEDTLS	= $(call remove_fun,$(MBEDTLS_LIB_DIR)/*.o $(MBEDTLS_TARGETS))
-
-#Executed only once if one of the libs needs update
-$(MBEDTLS_LIB_DIR)/libmbedcrypto.a: $(MBED_TLS_CONFIG_FILE)
-	-$(CLEAN_MBEDTLS)
-	$(MAKE) -C $(MBEDTLS_LIB_DIR)
-
-$(MBEDTLS_LIB_DIR)/libmbedx509.a: $(MBEDTLS_LIB_DIR)/libmbedcrypto.a
-
-$(MBEDTLS_LIB_DIR)/libmbedtls.a: $(MBEDTLS_LIB_DIR)/libmbedx509.a
-
-
-endif
-endif
-
-#	FATFS
-#If fatfs is found in SRC_DIRS
-ifneq ($(if $(findstring $(NO-OS)/libraries/fatfs, $(SRC_DIRS)), 1),)
-#Remove fatfs from srcdirs because it is a library
-SRC_DIRS := $(filter-out $(NO-OS)/libraries/fatfs, $(SRC_DIRS))
-
-FATFS_LIB	= $(NO-OS)/libraries/fatfs/libfatfs.a
-
-LIBS_DIRS	+= "$(dir $(FATFS_LIB))"
-LIBS		+= $(notdir $(FATFS_LIB))
-INCLUDE_DIRS	+= $(NO-OS)/libraries/fatfs/source
-
-CFLAGS		+= -I$(DRIVERS)/sd-card -I$(INCLUDE)
-
-CLEAN_FATFS	= $(MAKE) -C $(NO-OS)/libraries/fatfs clean
-$(FATFS_LIB):
-	$(MAKE) -C $(NO-OS)/libraries/fatfs
-
-endif
-
-#	MQTT
-MQTT_DIR = $(NO-OS)/libraries/mqtt
-#If mqtt is found in SRC_DIRS
-ifneq ($(if $(findstring $(MQTT_DIR), $(SRC_DIRS)), 1),)
-#Remove fatfs from srcdirs because it is a library
-SRC_DIRS := $(filter-out $(MQTT_DIR), $(SRC_DIRS))
-
-MQTT_LIB	= $(MQTT_DIR)/libmqtt_client.a
-
-LIBS_DIRS	+= "$(MQTT_DIR)"
-LIBS		+= $(notdir $(MQTT_LIB))
-INCLUDE_DIRS	+= $(MQTT_DIR)
-
-CLEAN_MQTT	= $(MAKE) -C $(MQTT_DIR) clean
-$(MQTT_LIB):
-	$(MAKE) -C $(MQTT_DIR)
-
-endif
-
-LIB_FLAGS = $(addprefix -l,$(subst lib,,$(basename $(LIBS))))
-LIB_DIR_FLAGS = $(addprefix -L,$(LIBS_DIRS))
-LIB_TARGETS += $(MBEDTLS_TARGETS) $(FATFS_LIB) $(MQTT_LIB)
-
-#------------------------------------------------------------------------------
 #                           UTIL FUNCTIONS                              
 #------------------------------------------------------------------------------
 
@@ -391,9 +303,6 @@ run: all
 		-s "$(ADUCM_DFP)/openocd/scripts" -f target/aducm3029.cfg \
 		-c "program  $(BINARY) verify reset exit"
 
-PHONY += libs
-libs: $(LIB_TARGETS)
-
 # Remove project binaries
 PHONY += clean
 clean:
@@ -408,13 +317,6 @@ re: clean
 PHONY += ra
 ra: clean_all
 	@$(MAKE) all
-
-PHONY += clean_libs
-clean_libs:
-	-$(CLEAN_MBEDTLS)
-	-$(CLEAN_FATFS)
-	-$(CLEAN_MQTT)
-	-$(CLEAN_IIO)
 
 # Remove workspace data and project directory
 PHONY += clean_all

commit 9a920240c87783bc3e205bdfc41ae1a2cab5c586

tools: scripts: fix mbedtls integration
Use the proper lib target variable for the mbedtls library.

Fixes: d842815 ("tools:scripts: Move libraries code to libraries.mk")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

tools/scripts/libraries.mk
@@ -71,7 +71,7 @@ include $(NO-OS)/tools/scripts/mqtt_srcs.mk
 
 endif
 
-LIB_TARGETS			+= $(IIO_LIB) $(MBEDTLS_TARGETS) $(FATFS_LIB) $(MQTT_LIB)
+LIB_TARGETS			+= $(IIO_LIB) $(MBEDTLS_LIBS) $(FATFS_LIB) $(MQTT_LIB)
 EXTRA_LIBS_NAMES	= $(subst lib,,$(basename $(notdir $(EXTRA_LIBS))))
 LIB_FLAGS			+= $(addprefix -l,$(EXTRA_LIBS_NAMES))
 LIB_PATHS			+= $(addprefix -L,$(EXTRA_LIBS_PATHS))