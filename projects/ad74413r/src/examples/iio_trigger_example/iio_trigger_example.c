analogdevicesinc/no-OS

commit 62db1106f1724ffd1e95cc97eea1298426aec27e

projects Update iio examples with iio_app_desc
Update iio examples in projects so that they use iio_app_desc.

Signed-off-by: George Mois <george.mois@analog.com>

projects/ad74413r/src/examples/iio_trigger_example/iio_trigger_example.c
@@ -71,6 +71,8 @@ int iio_trigger_example_main()
 	struct iio_hw_trig *ad74413r_trig_desc;
 	struct no_os_irq_ctrl_desc *ad74413r_irq_desc;
 	struct iio_desc *iio_desc;
+	struct iio_app_desc *app;
+	struct iio_app_init_param app_init_param = { 0 };
 
 	struct ad74413r_iio_desc *ad74413r_iio_desc;
 	struct ad74413r_iio_desc_init_param ad74413r_iio_ip;
@@ -137,8 +139,20 @@ int iio_trigger_example_main()
 				&ad74413r_iio_trig_desc)
 	};
 
-	return iio_app_run_with_trigs(NULL, 0, iio_devices,
-				      NO_OS_ARRAY_SIZE(iio_devices),
-				      trigs, NO_OS_ARRAY_SIZE(trigs), ad74413r_irq_desc, &iio_desc);
+	app_init_param.devices = iio_devices;
+	app_init_param.nb_devices = NO_OS_ARRAY_SIZE(iio_devices);
+	app_init_param.uart_init_params = ad74413r_uart_ip;
+	app_init_param.trigs = trigs;
+	app_init_param.nb_trigs = NO_OS_ARRAY_SIZE(trigs);
+	app_init_param.irq_desc = ad74413r_irq_desc;
+
+	ret = iio_app_init(&app, app_init_param);
+	if (ret)
+		return ret;
+
+	// update the reference to iio_desc
+	ad74413r_trig_desc->iio_desc = app->iio_desc;
+
+	return iio_app_run(app);
 
 }

projects/adxrs290-pmdz/src/examples/iio_timer_trigger_example/iio_timer_trigger_example.c
@@ -67,6 +67,8 @@ int iio_timer_trigger_example_main()
 {
 	int ret;
 	struct adxrs290_dev *adxrs290_desc;
+	struct iio_app_desc *app;
+	struct iio_app_init_param app_init_param = { 0 };
 	struct iio_data_buffer rd_buf = {
 		.buff = (void *)GYRO_DDR_BASEADDR,
 		.size = MAX_SIZE_BASE_ADDR
@@ -124,7 +126,19 @@ int iio_timer_trigger_example_main()
 				&adxrs290_iio_trig_desc)
 	};
 
-	return iio_app_run_with_trigs(NULL, 0, iio_devices,
-				      NO_OS_ARRAY_SIZE(iio_devices),
-				      trigs, NO_OS_ARRAY_SIZE(trigs), adxrs290_timer_irq_desc, &iio_desc);
+	app_init_param.devices = iio_devices;
+	app_init_param.nb_devices = NO_OS_ARRAY_SIZE(iio_devices);
+	app_init_param.uart_init_params = adxrs290_uart_ip;
+	app_init_param.trigs = trigs;
+	app_init_param.nb_trigs = NO_OS_ARRAY_SIZE(trigs);
+	app_init_param.irq_desc = adxrs290_timer_irq_desc;
+
+	ret = iio_app_init(&app, app_init_param);
+	if (ret)
+		return ret;
+
+	// update the reference to iio_desc
+	adxrs290_trig_desc->iio_desc = app->iio_desc;
+
+	return iio_app_run(app);
 }

projects/adxrs290-pmdz/src/examples/iio_trigger_example/iio_trigger_example.c
@@ -66,6 +66,8 @@ int iio_trigger_example_main()
 {
 	int ret;
 	struct adxrs290_dev *adxrs290_desc;
+	struct iio_app_desc *app;
+	struct iio_app_init_param app_init_param = { 0 };
 	struct iio_data_buffer rd_buf = {
 		.buff = (void *)GYRO_DDR_BASEADDR,
 		.size = MAX_SIZE_BASE_ADDR
@@ -115,7 +117,19 @@ int iio_trigger_example_main()
 				&adxrs290_iio_trig_desc)
 	};
 
-	return iio_app_run_with_trigs(NULL, 0, iio_devices,
-				      NO_OS_ARRAY_SIZE(iio_devices),
-				      trigs, NO_OS_ARRAY_SIZE(trigs), adxrs290_irq_desc, &iio_desc);
+	app_init_param.devices = iio_devices;
+	app_init_param.nb_devices = NO_OS_ARRAY_SIZE(iio_devices);
+	app_init_param.uart_init_params = adxrs290_uart_ip;
+	app_init_param.trigs = trigs;
+	app_init_param.nb_trigs = NO_OS_ARRAY_SIZE(trigs);
+	app_init_param.irq_desc = adxrs290_irq_desc;
+
+	ret = iio_app_init(&app, app_init_param);
+	if (ret)
+		return ret;
+
+	// update the reference to iio_desc
+	adxrs290_trig_desc->iio_desc = app->iio_desc;
+
+	return iio_app_run(app);
 }

projects/iio_demo/src/examples/iio_timer_trigger_example/iio_timer_trigger_example.c
@@ -87,6 +87,9 @@ int iio_timer_trigger_example_main()
 	/* iio desc */
 	struct iio_desc *iio_desc;
 
+	/* iio application desc */
+	struct iio_app_desc *app;
+
 	struct iio_data_buffer adc_buff = {
 		.buff = (void *)ADC_DDR_BASEADDR,
 		.size = MAX_SIZE_BASE_ADDR
@@ -96,6 +99,8 @@ int iio_timer_trigger_example_main()
 		.buff = (void *)DAC_DDR_BASEADDR,
 		.size = MAX_SIZE_BASE_ADDR
 	};
+
+	struct iio_app_init_param app_init_param = { 0 };
 
 	ret = adc_demo_init(&adc_desc, &adc_init_par);
 	if (ret)
@@ -168,6 +173,20 @@ int iio_timer_trigger_example_main()
 				&dac_iio_timer_trig_desc)
 	};
 
-	return iio_app_run_with_trigs(NULL, 0, devices, NO_OS_ARRAY_SIZE(devices),
-				      trigs, NO_OS_ARRAY_SIZE(trigs), NULL, &iio_desc);
+	app_init_param.devices = devices;
+	app_init_param.nb_devices = NO_OS_ARRAY_SIZE(devices);
+	app_init_param.uart_init_params = iio_demo_uart_ip;
+	app_init_param.trigs = trigs;
+	app_init_param.nb_trigs = NO_OS_ARRAY_SIZE(trigs);
+	app_init_param.irq_desc = NULL;
+
+	ret = iio_app_init(&app, app_init_param);
+	if (ret)
+		return ret;
+
+	// update the reference to iio_desc
+	adc_timer_trig_desc->iio_desc = app->iio_desc;
+	dac_timer_trig_desc->iio_desc = app->iio_desc;
+
+	return iio_app_run(app);
 }

projects/eval-adxl355-pmdz/src/examples/iio_trigger_example/iio_trigger_example.c
@@ -76,7 +76,8 @@ int iio_trigger_example_main()
 	};
 	struct iio_hw_trig *adxl355_trig_desc;
 	struct no_os_irq_ctrl_desc *adxl355_irq_desc;
-	struct iio_desc *iio_desc;
+	struct iio_app_desc *app;
+	struct iio_app_init_param app_init_param = { 0 };
 
 	/* Initialize IIO device */
 	adxl355_iio_ip.adxl355_dev_init = &adxl355_ip;
@@ -96,7 +97,6 @@ int iio_trigger_example_main()
 	adxl355_gpio_trig_ip.irq_ctrl = adxl355_irq_desc;
 
 	/* Initialize hardware trigger */
-	adxl355_gpio_trig_ip.iio_desc = &iio_desc,
 	ret = iio_hw_trig_init(&adxl355_trig_desc, &adxl355_gpio_trig_ip);
 	if (ret)
 		return ret;
@@ -117,7 +117,19 @@ int iio_trigger_example_main()
 				&adxl355_iio_trig_desc)
 	};
 
-	return iio_app_run_with_trigs(NULL, 0, iio_devices,
-				      NO_OS_ARRAY_SIZE(iio_devices),
-				      trigs, NO_OS_ARRAY_SIZE(trigs), adxl355_irq_desc, &iio_desc);
+	app_init_param.devices = iio_devices;
+	app_init_param.nb_devices = NO_OS_ARRAY_SIZE(iio_devices);
+	app_init_param.uart_init_params = adxl355_uart_ip;
+	app_init_param.trigs = trigs;
+	app_init_param.nb_trigs = NO_OS_ARRAY_SIZE(trigs);
+	app_init_param.irq_desc = adxl355_irq_desc;
+
+	ret = iio_app_init(&app, app_init_param);
+	if (ret)
+		return ret;
+
+	// update the reference to iio_desc
+	adxl355_trig_desc->iio_desc = app->iio_desc;
+
+	return iio_app_run(app);
 }

commit c6b8926e3d81b1034b9dc8cf2a58dd901d36e25f

projects: Remove iio_desc pointer assignment
Remove iio_desc pointer assignment: double pointer was removed in
2df74e4 ("iio::iio_trigger.* Rem double pointer in trig struct"),
leave simple pointer assignation after iio_app_init is performed.

Fixes: 62db110 ("projects Update iio examples with iio_app_desc")
Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

projects/ad74413r/src/examples/iio_trigger_example/iio_trigger_example.c
@@ -70,7 +70,6 @@ int iio_trigger_example_main()
 	int ret;
 	struct iio_hw_trig *ad74413r_trig_desc;
 	struct no_os_irq_ctrl_desc *ad74413r_irq_desc;
-	struct iio_desc *iio_desc;
 	struct iio_app_desc *app;
 	struct iio_app_init_param app_init_param = { 0 };
 
@@ -120,7 +119,6 @@ int iio_trigger_example_main()
 	ad74413r_gpio_trig_ip.irq_ctrl = ad74413r_irq_desc;
 
 	/* Initialize hardware trigger */
-	ad74413r_gpio_trig_ip.iio_desc = &iio_desc,
 	ret = iio_hw_trig_init(&ad74413r_trig_desc, &ad74413r_gpio_trig_ip);
 	if (ret)
 		return ret;

projects/adxrs290-pmdz/src/examples/iio_timer_trigger_example/iio_timer_trigger_example.c
@@ -76,7 +76,6 @@ int iio_timer_trigger_example_main()
 	struct iio_hw_trig *adxrs290_trig_desc;
 	struct no_os_timer_desc *adxrs290_timer_desc;
 	struct no_os_irq_ctrl_desc *adxrs290_timer_irq_desc;
-	struct iio_desc *iio_desc;
 
 	ret = adxrs290_init(&adxrs290_desc, &adxrs290_ip);
 	if (ret)
@@ -101,7 +100,6 @@ int iio_timer_trigger_example_main()
 	adxrs290_timer_trig_ip.cb_info.handle = adxrs290_timer_desc;
 
 	/* Initialize hardware trigger */
-	adxrs290_timer_trig_ip.iio_desc = &iio_desc,
 	ret = iio_hw_trig_init(&adxrs290_trig_desc, &adxrs290_timer_trig_ip);
 	if (ret)
 		return ret;

projects/adxrs290-pmdz/src/examples/iio_trigger_example/iio_trigger_example.c
@@ -74,7 +74,6 @@ int iio_trigger_example_main()
 	};
 	struct iio_hw_trig *adxrs290_trig_desc;
 	struct no_os_irq_ctrl_desc *adxrs290_irq_desc;
-	struct iio_desc *iio_desc;
 
 	ret = adxrs290_init(&adxrs290_desc, &adxrs290_ip);
 	if (ret)
@@ -96,7 +95,6 @@ int iio_trigger_example_main()
 		return ret;
 
 	/* Initialize hardware trigger */
-	adxrs290_gpio_trig_ip.iio_desc = &iio_desc,
 	ret = iio_hw_trig_init(&adxrs290_trig_desc, &adxrs290_gpio_trig_ip);
 	if (ret)
 		return ret;

projects/iio_demo/src/examples/iio_timer_trigger_example/iio_timer_trigger_example.c
@@ -84,9 +84,6 @@ int iio_timer_trigger_example_main()
 	/* dac demo irq instance descriptor */
 	struct no_os_irq_ctrl_desc *dac_demo_irq_desc;
 
-	/* iio desc */
-	struct iio_desc *iio_desc;
-
 	/* iio application desc */
 	struct iio_app_desc *app;
 
@@ -141,12 +138,10 @@ int iio_timer_trigger_example_main()
 	dac_demo_timer_trig_ip.irq_ctrl = dac_demo_irq_desc;
 
 	/* Initialize hardware trigger */
-	adc_demo_timer_trig_ip.iio_desc = &iio_desc,
 	ret = iio_hw_trig_init(&adc_timer_trig_desc, &adc_demo_timer_trig_ip);
 	if (ret)
 		return ret;
 
-	dac_demo_timer_trig_ip.iio_desc = &iio_desc,
 	ret = iio_hw_trig_init(&dac_timer_trig_desc, &dac_demo_timer_trig_ip);
 	if (ret)
 		return ret;