30dc4abab62e6b9b836409361e56e6f5c607094f
Chandrakant Minajigi <chandrakant.minajigi@analog.com>
2023-02-22 14:49:57 +0530

    projects:adt7420-pmdz: mbed platform code support
    Signed-off-by: Chandrakant Minajigi <chandrakant.minajigi@analog.com>

--- projects/adt7420-pmdz/src/platform/mbed/main.c
+++ projects/adt7420-pmdz/src/platform/mbed/main.c
@@ -1,9 +1,9 @@
 /***************************************************************************//**
- *   @file   platform_includes.h
- *   @brief  Includes for used platforms used by adt7420-pmdz project.
- *   @author RNechita (ramona.nechita@analog.com)
+ *   @file   main.c
+ *   @brief  Main file for Mbed platform of adt7420-pmdz project.
+ *   @author CMinajigi (chandrakant.minajigi@analog.com)
 ********************************************************************************
- * Copyright 2022(c) Analog Devices, Inc.
+ * Copyright 2023(c) Analog Devices, Inc.
  *
  * All rights reserved.
  *
@@ -36,20 +36,54 @@
  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
-#ifndef __PLATFORM_INCLUDES_H__
-#define __PLATFORM_INCLUDES_H__
 
 /******************************************************************************/
 /***************************** Include Files **********************************/
 /******************************************************************************/
-#ifdef MAXIM_PLATFORM
-#include "maxim/parameters.h"
-#elif  MBED_PLATFORM
-#include "mbed/parameters.h"
+#include "platform_includes.h"
+#include "common_data.h"
+
+#ifdef IIO_EXAMPLE
+#include "iio_example.h"
 #endif
 
-#ifdef IIO_SUPPORT
-#include "iio_app.h"
+#ifdef DUMMY_EXAMPLE
+#include "dummy_example.h"
 #endif
 
-#endif /* __PLATFORM_INCLUDES_H__ */
+/***************************************************************************//**
+ * @brief Main function for Mbed platform.
+ *
+ * @return ret - Result of the enabled examples.
+*******************************************************************************/
+
+int main()
+{
+	int ret;
+#ifdef IIO_EXAMPLE
+	ret = iio_example_main();
+	if (ret)
+		return ret;
+#endif
+
+#ifdef DUMMY_EXAMPLE
+	struct no_os_uart_desc* uart;
+	ret = no_os_uart_init(&uart, &uip);
+	if (ret) {
+		no_os_uart_remove(uart);
+		return ret;
+	}
+	no_os_uart_stdio(uart);
+	ret = dummy_example_main();
+	if (ret) {
+		no_os_uart_remove(uart);
+		return ret;
+	}
+#endif
+
+#if (IIO_EXAMPLE+DUMMY_EXAMPLE != 1)
+#error Selected example projects cannot be enabled at the same time. \
+Please enable only one example and re-build the project.
+#endif
+	return 0;
+}
