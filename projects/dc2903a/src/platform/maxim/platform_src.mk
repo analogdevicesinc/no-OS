eee239cf6181214137a4b2344cee394aa46f7402
Marc Paolo Sosa <marcpaolo.sosa@analog.com>
2023-02-27 16:28:22 +0800

    projects: add dc2903a example project
    Add example project for dc2903a/ltc2672 eval board
    
    Signed-off-by: Marc Paolo Sosa <marcpaolo.sosa@analog.com>

---
+++ projects/dc2903a/src/platform/maxim/platform_src.mk
@@ -0,0 +1,15 @@
+INCS += $(INCLUDE)/no_os_rtc.h
+
+INCS += $(PLATFORM_DRIVERS)/maxim_delay.h     \
+        $(PLATFORM_DRIVERS)/maxim_spi.h       \
+        $(PLATFORM_DRIVERS)/maxim_irq.h       \
+        $(PLATFORM_DRIVERS)/maxim_rtc.h       \
+        $(PLATFORM_DRIVERS)/maxim_uart.h      \
+        $(PLATFORM_DRIVERS)/maxim_uart_stdio.h
+
+SRCS += $(PLATFORM_DRIVERS)/maxim_delay.c     \
+        $(PLATFORM_DRIVERS)/maxim_spi.c       \
+        $(PLATFORM_DRIVERS)/maxim_rtc.c       \
+        $(PLATFORM_DRIVERS)/maxim_irq.c       \
+        $(PLATFORM_DRIVERS)/maxim_uart.c      \
+        $(PLATFORM_DRIVERS)/maxim_uart_stdio.c
