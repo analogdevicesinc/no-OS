3ac01436f0473083b95dde4cd8fe074d881ef28d
Antoniu Miclaus <antoniu.miclaus@analog.com>
2023-03-01 12:02:12 +0200

    ci: cppcheck: add config file
    Add custom config file for checking the no-os memory
    allocation/deallocation functions.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- ci/cppcheck.sh
+++ ci/cppcheck.sh
@@ -5,5 +5,6 @@ set -e
 sudo apt-get install cppcheck 
 
 [[ ! -f .cppcheckignore ]] || CPPCHECK_OPTIONS="${CPPCHECK_OPTIONS} --suppressions-list=.cppcheckignore"
+[[ ! -f ./ci/config.cppcheck ]] || CPPCHECK_OPTIONS="${CPPCHECK_OPTIONS} --library=./ci/config.cppcheck"
 	
 cppcheck --quiet --force --error-exitcode=1 $CPPCHECK_OPTIONS .
