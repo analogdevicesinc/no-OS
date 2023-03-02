analogdevicesinc/no-OS

commit fa348f73d04678cf32680944e73c41bb179e219a

drivers:platform:pico:pico_spi: Add spi_transfer API
Added spi_transfer API for pico platform which allows
to send multiple spi messages with chip select either
asserted or de-asserted.

Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

drivers/platform/pico/pico_spi.c
@@ -242,10 +242,53 @@ int32_t pico_spi_write_and_read(struct no_os_spi_desc *desc,
 }
 
 /**
+ * @brief Write/read multiple messages to/from SPI.
+ * @param desc - The SPI descriptor.
+ * @param msgs - The messages array.
+ * @param len - Number of messages.
+ * @return 0 in case of success, errno codes otherwise.
+ */
+int32_t pico_spi_transfer(struct no_os_spi_desc *desc,
+			  struct no_os_spi_msg *msgs,
+			  uint32_t len)
+{
+	struct pico_spi_desc *pico_spi;
+	bool cs_level;
+
+	if (!desc || !desc->extra || !msgs)
+		return -EINVAL;
+
+	pico_spi = desc->extra;
+	cs_level = gpio_get(pico_spi->spi_cs_pin);
+
+	for (uint32_t i = 0; i < len; i++) {
+		gpio_set_function(pico_spi->spi_cs_pin, GPIO_FUNC_NULL);
+		gpio_set_dir(pico_spi->spi_cs_pin, true);
+
+		if (cs_level)
+			gpio_pull_down(pico_spi->spi_cs_pin);
+		else
+			gpio_pull_up(pico_spi->spi_cs_pin);
+
+		spi_write_read_blocking(pico_spi->spi_instance, msgs[i].tx_buff,
+					msgs[i].rx_buff, msgs[i].bytes_number);
+
+		if (msgs[i].cs_change) {
+			gpio_set_function(pico_spi->spi_cs_pin, GPIO_FUNC_SPI);
+		}
+	}
+
+	gpio_set_function(pico_spi->spi_cs_pin, GPIO_FUNC_SPI);
+
+	return 0;
+}
+
+/**
  * @brief pico platform specific SPI platform ops structure
  */
 const struct no_os_spi_platform_ops pico_spi_ops = {
 	.init = &pico_spi_init,
 	.write_and_read = &pico_spi_write_and_read,
+	.transfer = &pico_spi_transfer,
 	.remove = &pico_spi_remove
 };

commit 074a8281b9de9a98325986c80c8dc952fdfe6de2

pico: pico_spi: handle spi_transfer null buffers
Use rx_buff and tx_buff in spi_transfer only if they are not
null.

Fixes: fa348f7 ("drivers:platform:pico:pico_spi: Add spi_transfer API")
Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

drivers/platform/pico/pico_spi.c
@@ -240,14 +240,24 @@ int32_t pico_spi_transfer(struct no_os_spi_desc *desc,
 
 	for (uint32_t i = 0; i < len; i++) {
 
+		if (!msgs[i].tx_buff && !msgs[i].rx_buff)
+			return -EINVAL;
+
 		/* Assert CS */
 		gpio_put(pico_spi->spi_cs_pin, 0);
 
-		if(msgs[i].cs_delay_first)
+		if (msgs[i].cs_delay_first)
 			no_os_udelay(msgs[i].cs_delay_first);
 
-		spi_write_read_blocking(pico_spi->spi_instance, msgs[i].tx_buff,
-					msgs[i].rx_buff, msgs[i].bytes_number);
+		if (!msgs[i].tx_buff)
+			spi_read_blocking(pico_spi->spi_instance, 0, msgs[i].rx_buff,
+					  msgs[i].bytes_number);
+		else if (!msgs[i].rx_buff)
+			spi_write_blocking(pico_spi->spi_instance, msgs[i].tx_buff,
+					   msgs[i].bytes_number);
+		else
+			spi_write_read_blocking(pico_spi->spi_instance, msgs[i].tx_buff,
+						msgs[i].rx_buff, msgs[i].bytes_number);
 
 		if(msgs[i].cs_delay_last)
 			no_os_udelay(msgs[i].cs_delay_last);