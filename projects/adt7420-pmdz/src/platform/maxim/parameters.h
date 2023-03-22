d211ea5376157ea7d26166dec9aca91fbb031a52
Ciprian Regus <ciprian.regus@analog.com>
2023-03-22 08:41:15 +0200

    projects:adt7420-pmdz: Add support for max32690
    Implement support for the max32690 target.
    
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

--- projects/adt7420-pmdz/src/platform/maxim/parameters.h
+++ projects/adt7420-pmdz/src/platform/maxim/parameters.h
@@ -61,12 +61,17 @@
 #define I2C_DEVICE_ID    1
 #elif (TARGET_NUM == 32655)
 #define I2C_DEVICE_ID    2
-#elif (TARGET_NUM == 32665) || (TARGET_NUM == 32660)
+#elif (TARGET_NUM == 32665) || (TARGET_NUM == 32660) || (TARGET_NUM == 32690)
 #define I2C_DEVICE_ID    0
 #endif
 
+#if (TARGET_NUM == 32690)
+#define UART_IRQ_ID     UART2_IRQn
+#define UART_DEVICE_ID  2
+#else
 #define UART_IRQ_ID     UART0_IRQn
 #define UART_DEVICE_ID  0
+#endif
 #define UART_BAUDRATE   57600
 #define UART_OPS        &max_uart_ops
 
