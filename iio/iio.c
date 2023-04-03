analogdevicesinc/no-OS

commit 05ea622fd6106b34cc2693217e0cf40692245693

iio: modify the buffer size allocated to support data continuity
Corrected the size of the circular buffer allocated in the
iio_open_dev function to support data continuity.

Signed-off-by: Gudla <Saikiran.Gudla@analog.com>

iio/iio.c
@@ -1070,6 +1070,7 @@ static int iio_open_dev(struct iiod_ctx *ctx, const char *device,
 	uint32_t ch_mask;
 	int32_t ret;
 	int8_t *buf;
+	uint32_t buf_size;
 
 	dev = get_iio_device(ctx->instance, device);
 	if (!dev)
@@ -1095,6 +1096,7 @@ static int iio_open_dev(struct iiod_ctx *ctx, const char *device,
 			/* Need a bigger buffer or to allocate */
 			return -ENOMEM;
 
+		buf_size = dev->buffer.raw_buf_len;
 		buf = dev->buffer.raw_buf;
 	} else {
 		if (dev->buffer.allocated) {
@@ -1102,13 +1104,14 @@ static int iio_open_dev(struct iiod_ctx *ctx, const char *device,
 			free(dev->buffer.cb.buff);
 			dev->buffer.allocated = 0;
 		}
+		buf_size = dev->buffer.public.size;
 		buf = (int8_t *)calloc(dev->buffer.public.size, sizeof(*buf));
 		if (!buf)
 			return -ENOMEM;
 		dev->buffer.allocated = 1;
 	}
 
-	ret = no_os_cb_cfg(&dev->buffer.cb, buf, dev->buffer.public.size);
+	ret = no_os_cb_cfg(&dev->buffer.cb, buf, buf_size);
 	if (NO_OS_IS_ERR_VALUE(ret)) {
 		if (dev->buffer.allocated) {
 			free(dev->buffer.cb.buff);

commit dcc512902c89a19ff4d4bbd40b6f0553315efe6f

iio: iio.c: Fix buffer maximum length based on received request
When using the statically allocated buffer with circular buffer,
make sure the used size is a multiple of the total requested
number of bytes from the iio client.
This has to be done because the circular buffer cannot handle
memory wrapping.

Fixes: 05ea622("iio: modify the buffer size allocated to support data continuity")
Signed-off-by: Ramona Bolboaca <ramona.bolboaca@analog.com>

iio/iio.c
@@ -1133,12 +1133,13 @@ static int iio_open_dev(struct iiod_ctx *ctx, const char *device,
 	dev->buffer.public.bytes_per_scan =
 		bytes_per_scan(dev->dev_descriptor->channels, mask);
 	dev->buffer.public.size = dev->buffer.public.bytes_per_scan * samples;
+	dev->buffer.public.samples = samples;
 	if (dev->buffer.raw_buf && dev->buffer.raw_buf_len) {
 		if (dev->buffer.raw_buf_len < dev->buffer.public.size)
 			/* Need a bigger buffer or to allocate */
 			return -ENOMEM;
-
-		buf_size = dev->buffer.raw_buf_len;
+		buf_size = dev->buffer.raw_buf_len - (dev->buffer.raw_buf_len %
+						      dev->buffer.public.size);
 		buf = dev->buffer.raw_buf;
 	} else {
 		if (dev->buffer.allocated) {
@@ -1243,7 +1244,6 @@ static int iio_call_submit(struct iiod_ctx *ctx, const char *device,
 		     dev->dev_descriptor->write_dev && dev->trig_idx==NO_TRIGGER)) {
 		/* Code used to don't break devices using read_dev */
 		int32_t ret;
-		uint32_t nb_scans;
 		void *buff;
 		struct iio_buffer *buffer = &dev->buffer.public;
 
@@ -1251,13 +1251,12 @@ static int iio_call_submit(struct iiod_ctx *ctx, const char *device,
 		if (NO_OS_IS_ERR_VALUE(ret))
 			return ret;
 
-		nb_scans = buffer->size / buffer->bytes_per_scan;
 		if (dir == IIO_DIRECTION_INPUT)
 			ret = dev->dev_descriptor->read_dev(dev->dev_instance,
-							    buff, nb_scans);
+							    buff, buffer->samples);
 		else
 			ret = dev->dev_descriptor->write_dev(dev->dev_instance,
-							     buff, nb_scans);
+							     buff, buffer->samples);
 		if (NO_OS_IS_ERR_VALUE(ret))
 			return ret;
 
@@ -1361,35 +1360,15 @@ static int iio_write_buffer(struct iiod_ctx *ctx, const char *device,
 
 int iio_buffer_get_block(struct iio_buffer *buffer, void **addr)
 {
-	int32_t ret;
 	uint32_t size;
 
 	if (!buffer)
 		return -EINVAL;
 
 	if (buffer->dir == IIO_DIRECTION_INPUT)
-		ret = no_os_cb_prepare_async_write(buffer->buf, buffer->size, addr,
-						   &size);
-	else
-		ret = no_os_cb_prepare_async_read(buffer->buf, buffer->size, addr,
-						  &size);
-	if (NO_OS_IS_ERR_VALUE(ret))
-		/* ToDo: Implement async cancel. And cancel transaction here.
-		 * Also cancel may be needed for a posible future abort callback
-		 * If this is not done, after the first error all future calls
-		 * to async will fail.
-		 * An other option will be to call cb_cfg but then data is lost
-		 */
-		return ret;
+		return no_os_cb_prepare_async_write(buffer->buf, buffer->size, addr, &size);
 
-	/* This function is exepected to be called for a DMA transaction of the
-	 * full buffer. But if can't do in one transaction won't work.
-	 * This behavior is not expected anyway.
-	 */
-	if (size != buffer->size)
-		return -ENOMEM;
-
-	return 0;
+	return no_os_cb_prepare_async_read(buffer->buf, buffer->size, addr, &size);
 }
 
 int iio_buffer_block_done(struct iio_buffer *buffer)

iio/iio_types.h
@@ -215,6 +215,8 @@ struct iio_buffer {
 	uint32_t size;
 	/* Number of bytes per sample * number of active channels */
 	uint32_t bytes_per_scan;
+	/* Number of requested samples */
+	uint32_t samples;
 	/* Buffer direction */
 	enum iio_buffer_direction dir;
 	/* Buffer where data is stored */