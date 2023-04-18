analogdevicesinc/no-OS

commit ce402f8ca7756efdbba7fa3717eb5f7e9db448d4

drivers/platform/chibios: chibios_alloc
no_os memory allocation implementation in ChibiOS

Signed-off-by: rbudai <robert.budai@analog.com>

drivers/platform/chibios/chibios_alloc.c
@@ -1,0 +1,77 @@
+/*******************************************************************************
+ *   @file   chibios/chibios_alloc.c
+ *   @brief  Implementation of no-OS memory allocation functions.
+ *   @author Robert Budai (robert.budai@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include "no_os_alloc.h"
+#include "hal.h"
+
+/**
+ * @brief Allocate memory and return a pointer to it.
+ * @param size - Size of the memory block, in bytes.
+ * @return Pointer to the allocated memory, or NULL if the request fails.
+ */
+__attribute__((weak)) void *no_os_malloc(size_t size)
+{
+	return chHeapAlloc(NULL, size);
+}
+
+/**
+ * @brief Allocate memory and return a pointer to it, set memory to 0.
+ * @param nitems - Number of elements to be allocated.
+ * @param size - Size of elements.
+ * @return Pointer to the allocated memory, or NULL if the request fails.
+ */
+__attribute__((weak)) void *no_os_calloc(size_t nitems, size_t size)
+{
+	void *ptr = chHeapAlloc(NULL, size);
+	if (ptr != NULL)
+		memset(ptr, 0, size);
+	return ptr;
+}
+
+/**
+ * @brief Deallocate memory previously allocated by a call to no_os_calloc
+ * 		  or no_os_malloc.
+ * @param ptr - Pointer to a memory block previously allocated by a call
+ * 		  to no_os_calloc or no_os_malloc.
+ * @return None.
+ */
+__attribute__((weak)) void no_os_free(void *ptr)
+{
+	chHeapFree(ptr);
+}

commit 2347873161e1c8135a61be8c78040e15a4aa46cf

platform/chibios/chibi_alloc.c: param fix
Added missing library for chibos_alloc.c

Fixes: ce402f8("drivers/platform/chibios: chibios_alloc")
Signed-off-by: rbudai <robert.budai@analog.com>

drivers/platform/chibios/chibios_alloc.c
@@ -39,6 +39,7 @@
 
 #include "no_os_alloc.h"
 #include "hal.h"
+#include <string.h>
 
 /**
  * @brief Allocate memory and return a pointer to it.