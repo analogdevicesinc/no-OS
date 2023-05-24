5d160b4e1b34fdaf003f99a6f17bab2dd231d63a
Andreea Andrisan <Andreea.Andrisan@analog.com>
2023-05-24 10:39:35 +0300

    tools: scripts: Creating BOOT.BIN for Xilinx projects except microblaze
    Modify xilinx.mk so that when make all is run to create also BOOT.BIN
    not just app.elf specific to the project. This is possible for Xilinx
    projects except those with sys_mb processor type.
    
    Signed-off-by: Andreea Andrisan <Andreea.Andrisan@analog.com>

---
+++ tools/scripts/platform/xilinx/create_fsbl_project.tcl
@@ -0,0 +1,6 @@
+set build_dir [lindex $argv 0]
+set hardware [lindex $argv 1]
+hsi open_hw_design $::hardware
+set cpu_name [lindex [hsi get_cells -filter {IP_TYPE==PROCESSOR}] 0]
+platform create -name hw0 -hw $::hardware -os standalone -out $::build_dir/tmp/output -proc $cpu_name
+platform generate
