fe5309737add0d344c837d23e2f819c162a35681
Antoniu Miclaus <antoniu.miclaus@analog.com>
2023-05-30 10:54:31 +0300

    Update documentation to 163f74e

---
+++ doxygen/search/enumvalues_2.js
@@ -0,0 +1,20 @@
+var searchData=
+[
+  ['bad_5finput_5fparameters_0',['BAD_INPUT_PARAMETERS',['../aducm3029__uart_8h.html#a07f8ef0410af663fab0dcf0c0f8f7fb8a29915f84b0db8513a0975d3e2be0861b',1,'aducm3029_uart.h']]],
+  ['bat_5fin_5fipeak_5foff_1',['BAT_IN_IPEAK_OFF',['../ltc3337_8h.html#a1fe9a48acedb0aecd2418b4cb28d3ea1ae2d04fd754ab6411fcbf1f4e801f5cb0',1,'ltc3337.h']]],
+  ['bat_5fin_5fipeak_5fon_2',['BAT_IN_IPEAK_ON',['../ltc3337_8h.html#a1fe9a48acedb0aecd2418b4cb28d3ea1a843596c5354211872c4d9943806d3abf',1,'ltc3337.h']]],
+  ['bat_5fout_5fipeak_5foff_3',['BAT_OUT_IPEAK_OFF',['../ltc3337_8h.html#a1fe9a48acedb0aecd2418b4cb28d3ea1a388f28a8e7a4e17c28d6a2175a0e07fd',1,'ltc3337.h']]],
+  ['bat_5fout_5fipeak_5fon_4',['BAT_OUT_IPEAK_ON',['../ltc3337_8h.html#a1fe9a48acedb0aecd2418b4cb28d3ea1a7b0663633539e944374501ad7e66f254',1,'ltc3337.h']]],
+  ['bb_5frefclk_5',['BB_REFCLK',['../ad9361_8h.html#ab0582a29cdd033f3ab558329eea89238aa21b49684565e7886555367cac8275b8',1,'ad9361.h']]],
+  ['bbpll_5fclk_6',['BBPLL_CLK',['../ad9361_8h.html#ab0582a29cdd033f3ab558329eea89238aeaaeabdd80e44c5f93fb6dc792ddcb69',1,'ad9361.h']]],
+  ['bbpll_5ffreq_7',['BBPLL_FREQ',['../ad9361_8h.html#a0591af9b291561a550b762f859e1b95aafaca286393cf7c1b858dd6dff5c1c36d',1,'ad9361.h']]],
+  ['be_5fmoreverbose_8',['BE_MOREVERBOSE',['../ad9361_8h.html#a40b28352800d3af6826c214db46cd4e8aa22e795d69a75402f75f9cb9d43f935f',1,'ad9361.h']]],
+  ['be_5fverbose_9',['BE_VERBOSE',['../ad9361_8h.html#a40b28352800d3af6826c214db46cd4e8a718b92affc99f68d4eb56f5228fc3b8a',1,'ad9361.h']]],
+  ['bipolar_10',['bipolar',['../ad5446_8h.html#a327c82847b9e6b68850b40dfaeebb63baaf941ae00aea56c2981c12f5848e4df5',1,'ad5446.h']]],
+  ['bist_5fdisable_11',['BIST_DISABLE',['../ad9361_8h.html#a04433bf30c400363fcf49c60845fcc16a48fb311ecb3de1bfe3a75b438fda581b',1,'ad9361.h']]],
+  ['bist_5finj_5frx_12',['BIST_INJ_RX',['../ad9361_8h.html#a04433bf30c400363fcf49c60845fcc16ae004e25d6eacb2fec6bbe10729355174',1,'ad9361.h']]],
+  ['bist_5finj_5ftx_13',['BIST_INJ_TX',['../ad9361_8h.html#a04433bf30c400363fcf49c60845fcc16a61348340aa696125d86641d5fc04a368',1,'ad9361.h']]],
+  ['break_5finterrupt_14',['BREAK_INTERRUPT',['../aducm3029__uart_8h.html#a07f8ef0410af663fab0dcf0c0f8f7fb8a8ef7d00637e2acb4e387ade6dac4c9ff',1,'aducm3029_uart.h']]],
+  ['buffered_5fxtaln_5fdcxo_15',['BUFFERED_XTALN_DCXO',['../ad9361_8h.html#a552145d04632dbcf2aa9fd2131bf70f5a83ef68f1d4eb77decf6ebf40b5418f99',1,'ad9361.h']]],
+  ['bus_16',['BUS',['../ad5293_8h.html#a4dd0057f3dd624cfa8556f28956901b8a885a6a40e3fde5dfec3db7fefea61f9b',1,'ad5293.h']]]
+];
---
+++ doxygen/search/files_7.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['generic_5faxi_5fio_2ec_0',['generic_axi_io.c',['../generic__axi__io_8c.html',1,'']]],
+  ['generic_5fdelay_2ec_1',['generic_delay.c',['../generic__delay_8c.html',1,'']]],
+  ['generic_5fgpio_2ec_2',['generic_gpio.c',['../generic__gpio_8c.html',1,'']]],
+  ['generic_5fi2c_2ec_3',['generic_i2c.c',['../generic__i2c_8c.html',1,'']]],
+  ['generic_5fspi_2ec_4',['generic_spi.c',['../generic__spi_8c.html',1,'']]],
+  ['generic_5fspi_5fexample_2ec_5',['generic_spi_example.c',['../generic__spi__example_8c.html',1,'']]],
+  ['generic_5ftimer_2ec_6',['generic_timer.c',['../generic__timer_8c.html',1,'']]],
+  ['generic_5ftrng_2ec_7',['generic_trng.c',['../generic__trng_8c.html',1,'']]],
+  ['generic_5fuart_2ec_8',['generic_uart.c',['../generic__uart_8c.html',1,'']]]
+];
