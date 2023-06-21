4b4970799acb94ea9d680b4c1b5d04a0293f08f1
Antoniu Miclaus <antoniu.miclaus@analog.com>
2023-06-21 15:51:21 +0300

    projects: demo_esp: include no_os_units
    Required for maxim platform support.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- projects/demo_esp/src.mk
+++ projects/demo_esp/src.mk
@@ -22,6 +22,7 @@ INCS += $(INCLUDE)/no_os_delay.h		\
 	$(INCLUDE)/no_os_irq.h			\
 	$(INCLUDE)/no_os_uart.h			\
 	$(INCLUDE)/no_os_lf256fifo.h		\
+	$(INCLUDE)/no_os_units.h		\
 	$(INCLUDE)/no_os_util.h			\
 	$(INCLUDE)/no_os_list.h			\
 	$(INCLUDE)/no_os_circular_buffer.h	\
