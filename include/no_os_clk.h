analogdevicesinc/no-OS

commit db56745f48d8cddb4a7171ad18b46eb154af6e22

include:no_os_clk.h util:no_os_clk.c clk ops
Add clk ops similar to spi no-OS implementation.

Signed-off-by: George Mois <george.mois@analog.com>

include/no_os_clk.h
@@ -1,4 +1,4 @@
-/***************************************************************************//**
+/*******************************************************************************
  *   @file   no_os_clk.h
  *   @brief  Header file of Clock Driver.
  *   @author DBogdan (dragos.bogdan@analog.com)
@@ -47,6 +47,17 @@
 /******************************************************************************/
 /************************* Structure Declarations *****************************/
 /******************************************************************************/
+struct no_os_clk_init_param {
+	/** Device name */
+	const char	*name;
+	/** Channel number */
+	uint8_t		hw_ch_num;
+	/** CLK function pointers */
+	const struct no_os_clk_platform_ops *platform_ops;
+	/**  CLK hardware device descriptor */
+	void		*dev_desc;
+};
+
 struct no_os_clk_hw {
 	void	*dev;
 	int32_t (*dev_clk_enable)();
@@ -60,29 +71,74 @@ struct no_os_clk {
 	struct no_os_clk_hw	*hw;
 	uint32_t	hw_ch_num;
 	const char	*name;
+	const struct no_os_clk_desc *clk_desc;
+};
+
+/**
+ * @struct no_os_clk_desc
+ * @brief Structure holding CLK descriptor.
+ */
+typedef struct no_os_clk_desc {
+	/** Device name */
+	const char	*name;
+	/** Channel number */
+	uint8_t		hw_ch_num;
+	/** CLK function pointers */
+	const struct no_os_clk_platform_ops *platform_ops;
+	/**  CLK hardware device descriptor */
+	void		*dev_desc;
+} no_os_clk_desc;
+
+/**
+ * @struct no_os_clk_platform_ops
+ * @brief Structure holding CLK function pointers that point to the platform
+ * specific function
+ */
+struct no_os_clk_platform_ops {
+	/** Initialize CLK function pointer. */
+	int (*init)(struct no_os_clk_desc **, const struct no_os_clk_init_param *);
+	/** Start CLK function pointer. */
+	int (*clk_enable)(struct no_os_clk_desc *);
+	/** Stop CLK function pointer. */
+	int (*clk_disable)(struct no_os_clk_desc *);
+	/** Get the current frequency of CLK function pointer. */
+	int (*clk_recalc_rate)(struct no_os_clk_desc *, uint64_t *);
+	/* Round the desired frequency to a rate that CLK can actually output. */
+	int (*clk_round_rate)(struct no_os_clk_desc *, uint64_t, uint64_t *);
+	/* Change CLK frequency function pointer. */
+	int (*clk_set_rate)(struct no_os_clk_desc *, uint64_t);
+	/** CLK remove function pointer */
+	int (*remove)(struct no_os_clk_desc *);
 };
 
 /******************************************************************************/
 /************************ Functions Declarations ******************************/
 /******************************************************************************/
 
+/* Initialize CLK ops. */
+int32_t no_os_clk_init(struct no_os_clk_desc **desc,
+		       const struct no_os_clk_init_param *param);
+
+/* Free the resources allocated by no_os_clk_init(). */
+int32_t no_os_clk_remove(struct no_os_clk_desc *desc);
+
 /* Start the clock. */
-int32_t no_os_clk_enable(struct no_os_clk * clk);
+int32_t no_os_clk_enable(struct no_os_clk_desc *desc);
 
 /* Stop the clock. */
-int32_t no_os_clk_disable(struct no_os_clk * clk);
+int32_t no_os_clk_disable(struct no_os_clk_desc *desc);
 
 /* Get the current frequency of the clock. */
-int32_t no_os_clk_recalc_rate(struct no_os_clk *clk,
+int32_t no_os_clk_recalc_rate(struct no_os_clk_desc *desc,
 			      uint64_t *rate);
 
 /* Round the desired frequency to a rate that the clock can actually output. */
-int32_t no_os_clk_round_rate(struct no_os_clk *clk,
+int32_t no_os_clk_round_rate(struct no_os_clk_desc *desc,
 			     uint64_t rate,
 			     uint64_t *rounded_rate);
 
 /* Change the frequency of the clock. */
-int32_t no_os_clk_set_rate(struct no_os_clk *clk,
+int32_t no_os_clk_set_rate(struct no_os_clk_desc *desc,
 			   uint64_t rate);
 
 #endif // _NO_OS_CLK_H_

util/no_os_clk.c
@@ -1,4 +1,4 @@
-/***************************************************************************//**
+/*******************************************************************************
  *   @file   no_os_clk.c
  *   @brief  Implementation of Clock Driver.
  *   @author DBogdan (dragos.bogdan@analog.com)
@@ -46,83 +46,132 @@
 /******************************************************************************/
 /************************** Functions Implementation **************************/
 /******************************************************************************/
+/**
+ * Initialize clock.
+ * @param desc - CLK descriptor.
+ * @param param - The structure that contains CLK parameters.
+ * @return 0 in case of success, -1 otherwise.
+ */
+int32_t no_os_clk_init(struct no_os_clk_desc **desc,
+		       const struct no_os_clk_init_param *param)
+{
+	int ret;
+
+	if (!desc || !param || !param->platform_ops)
+		return -EINVAL;
+
+	if (!param->platform_ops->init)
+		return -ENOSYS;
+
+	ret = param->platform_ops->init(desc, param);
+	if (ret)
+		return -EINVAL;
+
+	(*desc)->platform_ops = param->platform_ops;
+
+	return 0;
+}
+
+/**
+ * @brief Free the resources allocated by no_os_clk_init().
+ * @param desc - The clock descriptor.
+ * @return 0 in case of success, -1 otherwise.
+ */
+int32_t no_os_clk_remove(struct no_os_clk_desc *desc)
+{
+	if (!desc || !desc->platform_ops)
+		return -EINVAL;
+
+	if (!desc->platform_ops->remove)
+		return -ENOSYS;
+
+	return desc->platform_ops->remove(desc);
+}
 
 /**
  * Start the clock.
- * @param clk - The clock structure.
+ * @param clk - The clock descriptor.
  * @return 0 in case of success, negative error code otherwise.
  */
-int32_t no_os_clk_enable(struct no_os_clk * clk)
+int32_t no_os_clk_enable(struct no_os_clk_desc *desc)
 {
-	if (clk->hw->dev_clk_enable)
-		return clk->hw->dev_clk_enable(clk->hw->dev);
-	else
-		return -1;
+	if (!desc || !desc->platform_ops)
+		return -EINVAL;
+
+	if (!desc->platform_ops->clk_enable)
+		return -ENOSYS;
+
+	return desc->platform_ops->clk_enable(desc);
 }
 
 /**
  * Stop the clock.
- * @param clk - The clock structure.
+ * @param clk - The clock descriptor.
  * @return 0 in case of success, negative error code otherwise.
  */
-int32_t no_os_clk_disable(struct no_os_clk * clk)
+int32_t no_os_clk_disable(struct no_os_clk_desc *desc)
 {
-	if (clk->hw->dev_clk_disable)
-		return clk->hw->dev_clk_disable(clk->hw->dev);
-	else
-		return -1;
+	if (!desc || !desc->platform_ops)
+		return -EINVAL;
+
+	if (!desc->platform_ops->clk_disable)
+		return -ENOSYS;
+
+	return desc->platform_ops->clk_disable(desc);
 }
 
 /**
  * Get the current frequency of the clock.
- * @param clk - The clock structure.
+ * @param clk - The clock descriptor.
  * @param rate - The current frequency.
  * @return 0 in case of success, negative error code otherwise.
  */
-int32_t no_os_clk_recalc_rate(struct no_os_clk *clk,
+int32_t no_os_clk_recalc_rate(struct no_os_clk_desc *desc,
 			      uint64_t *rate)
 {
-	if (clk->hw->dev_clk_recalc_rate)
-		return clk->hw->dev_clk_recalc_rate(clk->hw->dev,
-						    clk->hw_ch_num,
-						    rate);
-	else
-		return -1;
+	if (!desc || !desc->platform_ops || !rate)
+		return -EINVAL;
+
+	if (!desc->platform_ops->clk_recalc_rate)
+		return -ENOSYS;
+
+	return desc->platform_ops->clk_recalc_rate(desc, rate);
 }
 
 /**
  * Round the desired frequency to a rate that the clock can actually output.
- * @param clk - The clock structure.
+ * @param clk - The clock descriptor.
  * @param rate - The desired frequency.
  * @param rounded_rate - The rounded frequency.
  * @return 0 in case of success, negative error code otherwise.
  */
-int32_t no_os_clk_round_rate(struct no_os_clk *clk,
+int32_t no_os_clk_round_rate(struct no_os_clk_desc *desc,
 			     uint64_t rate,
 			     uint64_t *rounded_rate)
 {
-	if (clk->hw->dev_clk_round_rate)
-		return clk->hw->dev_clk_round_rate(clk->hw->dev,
-						   clk->hw_ch_num,
-						   rate,
-						   rounded_rate);
-	else
-		return -1;
+	if (!desc || !desc->platform_ops || !rounded_rate)
+		return -EINVAL;
+
+	if (!desc->platform_ops->clk_round_rate)
+		return -ENOSYS;
+
+	return desc->platform_ops->clk_round_rate(desc, rate, rounded_rate);
 }
 
 /**
  * Change the frequency of the clock.
- * @param clk - The clock structure.
+ * @param clk - The clock descriptor.
  * @param rate - The desired frequency.
  * @return 0 in case of success, negative error code otherwise.
  */
-int32_t no_os_clk_set_rate(struct no_os_clk *clk,
+int32_t no_os_clk_set_rate(struct no_os_clk_desc *desc,
 			   uint64_t rate)
 {
-	if (clk->hw->dev_clk_set_rate)
-		return clk->hw->dev_clk_set_rate(clk->hw->dev,
-						 clk->hw_ch_num,
-						 rate);
-	else
-		return -1;
+	if (!desc || !desc->platform_ops)
+		return -EINVAL;
+
+	if (!desc->platform_ops->clk_set_rate)
+		return -ENOSYS;
+
+	return desc->platform_ops->clk_set_rate(desc, rate);
 }

commit cfeacee306b55461c6219d753fc04bdbc5154c33

include:no_os_clk.h Restore Doxygen comments generation
Restore Doxygen comments generation for the no_os_clk files.

Fixes: db56745

Signed-off-by: George Mois <george.mois@analog.com>

include/no_os_clk.h
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/***************************************************************************//**
  *   @file   no_os_clk.h
  *   @brief  Header file of Clock Driver.
  *   @author DBogdan (dragos.bogdan@analog.com)

util/no_os_clk.c
@@ -1,4 +1,4 @@
-/*******************************************************************************
+/***************************************************************************//**
  *   @file   no_os_clk.c
  *   @brief  Implementation of Clock Driver.
  *   @author DBogdan (dragos.bogdan@analog.com)