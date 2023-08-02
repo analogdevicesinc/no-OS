f09ab0f5a3492690adf4043aeebbe578d8552e66
Stanca Pop <stanca.pop@analog.com>
2023-08-02 11:51:32 +0300

    ad7616: Fix main

--- projects/ad7616-sdz/src/ad7616_sdz.c
+++ projects/ad7616-sdz/src/ad7616_sdz.c
@@ -66,108 +66,107 @@
 #define AD7616_SDZ_SAMPLE_NO 10
 #define AD7616_SDZ_CH_NO 1
 
-#if (HDL_AD7616_PARALLEL == 0)
-struct spi_engine_offload_init_param spi_engine_offload_init_param = {
-	.offload_config = OFFLOAD_RX_EN,
-	.rx_dma_baseaddr = AD7616_DMA_BASEADDR,
-};
-
-struct spi_engine_init_param spi_eng_init_param  = {
-	.ref_clk_hz = 100000000,
-	.type = SPI_ENGINE,
-	.spi_engine_baseaddr = AD7616_SPI_ENGINE_BASEADDR,
-	.cs_delay = 1,
-	.data_width = 16,
-};
-
-struct no_os_spi_init_param ad7616_spi_init = {
-	.chip_select = SPI_AD7616_CS,
-	.max_speed_hz = 50000000,
-	.mode = NO_OS_SPI_MODE_2,
-	.platform_ops = &spi_eng_platform_ops,
-	.extra = (void*)&spi_eng_init_param,
-};
-#endif
-
-struct xil_gpio_init_param xil_gpio_param = {
-	.device_id = GPIO_DEVICE_ID,
-	.type = GPIO_PS,
-};
-struct no_os_gpio_init_param ad7616_gpio_reset = {
-	.number = GPIO_ADC_RESET_N,
-	.platform_ops = &xil_gpio_ops,
-	.extra = &xil_gpio_param
-};
-struct axi_pwm_init_param axi_pwm_init = {
-	.base_addr = AXI_PWMGEN_BASEADDR,
-	.ref_clock_Hz = 100000000,
-};
-
-struct no_os_pwm_init_param trigger_pwm_init = {
-	.period_ns = 1000,	/* 1MHz */
-	.duty_cycle_ns = AD7616_TRIGGER_PULSE_WIDTH_NS,  /* pulse_width = 50 */
-	.polarity = NO_OS_PWM_POLARITY_HIGH,
-	.extra = &axi_pwm_init,
-};
-struct ad7616_init_param init_param = {
-	/* GPIO */
-	.gpio_hw_rngsel0_param = NULL,
-	.gpio_hw_rngsel1_param = NULL,
-	.gpio_os0_param = NULL,
-	.gpio_os1_param = NULL,
-	.gpio_os2_param = NULL,
-	.gpio_reset_param = &ad7616_gpio_reset,
-	#if (HDL_AD7616_PARALLEL == 1)
-	/* AXI Core */
-	.core_baseaddr = AD7616_CORE_BASEADDR,
-	#else
-	/* SPI */
-        .spi_param = &ad7616_spi_init,
-        .offload_init_param = &spi_engine_offload_init_param,
-        .reg_access_speed = 1000000,
-	#endif
-
-	/* Device Settings */
-	.mode = AD7616_SW,
-	.trigger_pwm_init = &trigger_pwm_init,
-	.va = {
-		AD7616_10V, AD7616_10V, AD7616_10V, AD7616_10V,
-		AD7616_10V, AD7616_10V, AD7616_10V, AD7616_10V
-	},
-	.vb = {
-		AD7616_10V, AD7616_10V, AD7616_10V, AD7616_10V,
-		AD7616_10V, AD7616_10V, AD7616_10V, AD7616_10V
-	},
-	.osr = AD7616_OSR_0,
-	.seq_addr = {
-		AD7616_ADP, AD7616_ADP, AD7616_ADP,
-		AD7616_ADP, AD7616_ADP, AD7616_ADP,
-		AD7616_ADP, AD7616_ADP
-	},
-	.seq_ssren = {
-		AD7616_SRREN_0, AD7616_SRREN_0, AD7616_SRREN_0,
-		AD7616_SRREN_0, AD7616_SRREN_0, AD7616_SRREN_0,
-		AD7616_SRREN_0, AD7616_SSREN_1
-	},
-	.seq_bsel = {
-		AD7616_CH_BP, AD7616_CH_BP, AD7616_CH_BP,
-		AD7616_CH_BP, AD7616_CH_BP, AD7616_CH_BP,
-		AD7616_CH_BP, AD7616_CH_BP
-	},
-	.seq_asel = {
-		AD7616_CH_AP, AD7616_CH_AP, AD7616_CH_AP,
-		AD7616_CH_AP, AD7616_CH_AP, AD7616_CH_AP,
-		AD7616_CH_AP, AD7616_CH_AP
-	},
-	.dcache_invalidate_range =
-	(void (*)(uint32_t, uint32_t))Xil_DCacheInvalidateRange,
-};
-
-/***************************************************************************//**
-* @brief main
-*******************************************************************************/
 int main(void)
 {
+
+	#if (HDL_AD7616_PARALLEL == 0)
+	struct spi_engine_offload_init_param spi_engine_offload_init_param = {
+		.offload_config = OFFLOAD_RX_EN,
+		.rx_dma_baseaddr = AD7616_DMA_BASEADDR,
+	};
+
+	struct spi_engine_init_param spi_eng_init_param  = {
+		.ref_clk_hz = 100000000,
+		.type = SPI_ENGINE,
+		.spi_engine_baseaddr = AD7616_SPI_ENGINE_BASEADDR,
+		.cs_delay = 1,
+		.data_width = 16,
+	};
+
+	struct no_os_spi_init_param ad7616_spi_init = {
+		.chip_select = SPI_AD7616_CS,
+		.max_speed_hz = 50000000,
+		.mode = NO_OS_SPI_MODE_2,
+		.platform_ops = &spi_eng_platform_ops,
+		.extra = (void*)&spi_eng_init_param,
+	};
+	#endif
+
+	struct xil_gpio_init_param xil_gpio_param = {
+		.device_id = GPIO_DEVICE_ID,
+		.type = GPIO_PS,
+	};
+	struct no_os_gpio_init_param ad7616_gpio_reset = {
+		.number = GPIO_ADC_RESET_N,
+		.platform_ops = &xil_gpio_ops,
+		.extra = &xil_gpio_param
+	};
+	struct axi_pwm_init_param axi_pwm_init = {
+		.base_addr = AXI_PWMGEN_BASEADDR,
+		.ref_clock_Hz = 100000000,
+	};
+
+	struct no_os_pwm_init_param trigger_pwm_init = {
+		.period_ns = 1000,	/* 1MHz */
+		.duty_cycle_ns = AD7616_TRIGGER_PULSE_WIDTH_NS,  /* pulse_width = 50 */
+		.polarity = NO_OS_PWM_POLARITY_HIGH,
+		.platform_ops = &axi_pwm_ops,
+		.extra = &axi_pwm_init,
+	};
+	struct ad7616_init_param init_param = {
+		/* GPIO */
+		.gpio_hw_rngsel0_param = NULL,
+		.gpio_hw_rngsel1_param = NULL,
+		.gpio_os0_param = NULL,
+		.gpio_os1_param = NULL,
+		.gpio_os2_param = NULL,
+		.gpio_reset_param = &ad7616_gpio_reset,
+		#if (HDL_AD7616_PARALLEL == 1)
+		/* AXI Core */
+		.core_baseaddr = AD7616_CORE_BASEADDR,
+		#else
+		/* SPI */
+			.spi_param = &ad7616_spi_init,
+			.offload_init_param = &spi_engine_offload_init_param,
+			.reg_access_speed = 1000000,
+		#endif
+
+		/* Device Settings */
+		.mode = AD7616_SW,
+		.trigger_pwm_init = &trigger_pwm_init,
+		.va = {
+			AD7616_10V, AD7616_10V, AD7616_10V, AD7616_10V,
+			AD7616_10V, AD7616_10V, AD7616_10V, AD7616_10V
+		},
+		.vb = {
+			AD7616_10V, AD7616_10V, AD7616_10V, AD7616_10V,
+			AD7616_10V, AD7616_10V, AD7616_10V, AD7616_10V
+		},
+		.osr = AD7616_OSR_0,
+		.seq_addr = {
+			AD7616_ADP, AD7616_ADP, AD7616_ADP,
+			AD7616_ADP, AD7616_ADP, AD7616_ADP,
+			AD7616_ADP, AD7616_ADP
+		},
+		.seq_ssren = {
+			AD7616_SRREN_0, AD7616_SRREN_0, AD7616_SRREN_0,
+			AD7616_SRREN_0, AD7616_SRREN_0, AD7616_SRREN_0,
+			AD7616_SRREN_0, AD7616_SSREN_1
+		},
+		.seq_bsel = {
+			AD7616_CH_BP, AD7616_CH_BP, AD7616_CH_BP,
+			AD7616_CH_BP, AD7616_CH_BP, AD7616_CH_BP,
+			AD7616_CH_BP, AD7616_CH_BP
+		},
+		.seq_asel = {
+			AD7616_CH_AP, AD7616_CH_AP, AD7616_CH_AP,
+			AD7616_CH_AP, AD7616_CH_AP, AD7616_CH_AP,
+			AD7616_CH_AP, AD7616_CH_AP
+		},
+		.dcache_invalidate_range =
+		(void (*)(uint32_t, uint32_t))Xil_DCacheInvalidateRange,
+	};
+
 	struct ad7616_dev	*dev;
 	uint32_t buf[AD7616_SDZ_SAMPLE_NO] __attribute__ ((aligned));
 	//uint32_t i;
@@ -183,52 +182,30 @@ int main(void)
 
 	ad7616_setup(&dev, &init_param);
 
-	//ad7616_spi_write(dev, 0x20, 0x41BB);
-	ad7616_write_mask(dev, 0x2, 0x20, 0x20); // SEQ EN
-	ad7616_spi_write(dev, 0x20, 0x41BB);
-	uint16_t val;
-	ad7616_spi_read(dev, 0x20, &val);
-	printf("reg 0x20: 0x%x\n", val);
-	uint16_t val2;
-	ad7616_spi_read(dev, 0x3, &val2);
-	printf("reg 0x3: 0x%x\n", val2);
-	uint16_t val3;
-	ad7616_spi_read(dev, AD7616_CTRL_RESETN, &val3);
-	printf("reg AD7616_CTRL_RESETN: 0x%x\n", val3);
-
-
 	if(dev->interface == AD7616_PARALLEL)
 		ad7616_read_data_parallel(dev, buf, AD7616_SDZ_SAMPLE_NO);
 	else
-		ad7616_read_data_serial(dev, buf, AD7616_SDZ_SAMPLE_NO * AD7616_SDZ_CH_NO);
+        ad7616_read_data_serial(dev, buf, AD7616_SDZ_SAMPLE_NO * AD7616_SDZ_CH_NO);
 
-	//for (i = 0; i < AD7616_SDZ_SAMPLE_NO/2; i++) {
-	//	pr_info("ADC sample %lu : %lu \n", i * 2, buf[i] >> 16);
-	//	pr_info("ADC sample %lu : %lu \n", (i * 2) + 1, buf[i] & 0xFFFF);
-	//}
-
-	for(i = 0; i < AD7616_SDZ_SAMPLE_NO; i++) {
-		ch = 0;
-		printf("%lu:\n", i);
-		for(ch = 0; ch < AD7616_SDZ_CH_NO; ch++) {
-//			buf[AD7616_SDZ_CH_NO*i+ch] >>= 16;
-//			//buf[AD7616_SDZ_CH_NO*i+j] &= 0xffff;
-//			data = lsb * (int32_t)buf[AD7616_SDZ_CH_NO*i+ch];
-//			if(data > 2.5)
-//				data = data - 5;
-//			printf("CH%lu: 0x%08lx = %+1.5fV ", ch,
-//					buf[AD7616_SDZ_CH_NO*i+ch], data);
-//			if(ch == (AD7616_SDZ_CH_NO - 1))
-//				printf("\n");
-//			ch++;
-			uint32_t vab = buf[ch * AD7616_SDZ_CH_NO + i];
-			uint16_t va = vab >> 16;
-			double va_volts= (double)va * 10 * 2 / 65536 - 10;
-			uint16_t vb = (uint16_t)vab;
-			double vb_volts= (double)vb * 10 * 2 / 65536 - 10;
-			printf(" VA%d: 0x%.4x %.2lfV VB%d 0x%.4x %.2lfV\n", ch, va, va_volts, ch, vb, vb_volts);
-		}
+	for (i = 0; i < AD7616_SDZ_SAMPLE_NO/2; i++) {
+		pr_info("ADC sample %lu : %lu \n", i * 2, buf[i] >> 16);
+		pr_info("ADC sample %lu : %lu \n", (i * 2) + 1, buf[i] & 0xFFFF);
 	}
+
+//  Example for printing voltages instead of codes
+//
+//	for(i = 0; i < AD7616_SDZ_SAMPLE_NO; i++) {
+//		ch = 0;
+//		printf("%lu:\n", i);
+//		for(ch = 0; ch < AD7616_SDZ_CH_NO; ch++) {
+//			uint32_t vab = buf[ch * AD7616_SDZ_CH_NO + i];
+//			uint16_t va = vab >> 16;
+//			double va_volts= (double)va * 10 * 2 / 65536 - 10;
+//			uint16_t vb = (uint16_t)vab;
+//			double vb_volts= (double)vb * 10 * 2 / 65536 - 10;
+//			printf(" VA%d: 0x%.4x %.2lfV VB%d 0x%.4x %.2lfV\n", ch, va, va_volts, ch, vb, vb_volts);
+//		}
+//	}
 
 	pr_info("Capture done. \n");
 
