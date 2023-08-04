f00f1633f7332ad12ae807c87714aa0a3d4a6aa1
George Mois <george.mois@analog.com>
2023-08-04 16:11:49 +0300

    ad9081:src:app_config.h Add JESD params for Versal
    Add the JESD parameters for the Versal project.
    
    Signed-off-by: George Mois <george.mois@analog.com>

--- projects/ad9081/src/app_config.h
+++ projects/ad9081/src/app_config.h
@@ -45,27 +45,112 @@
 #define MULTIDEVICE_INSTANCE_COUNT	1
 #endif
 
-#define AD9081_DAC_FREQUENCY	4000000000
-#define AD9081_ADC_FREQUENCY	2000000000
+#define AD9081_TX_DAC_1X_NON1X_CROSSBAR	{0x1, 0x0, 0x0, 0x0}
+#define AD9081_RX_CDDC_NCO_CHANNEL_SELECT_MODE	{0, 0, 0, 0}
+
+#define AD9081_TX_FSC	{0, 0, 0, 0}
+
 #ifdef QUAD_MXFE
 #define AD9081_ADC_NYQUIST_ZONE	{1, 1, 1, 1}
 #else
 #define AD9081_ADC_NYQUIST_ZONE	{0, 0, 0, 0}
 #endif
 
+// ----------------------------------------ZCU102----------------------------------------
+//
+//#define ADXCVR_REF_CLK_KHZ		500000
+//#define ADXCVR_RX_DEV_CLK_KHZ	250000
+//#define ADXCVR_TX_DEV_CLK_KHZ	250000
+//#define ADXCVR_RX_LANE_CLK_KHZ	10000000
+//#define ADXCVR_TX_LANE_CLK_KHZ	10000000
+//
+//#define AD9081_DAC_FREQUENCY	12000000000
+//#define AD9081_ADC_FREQUENCY	4000000000
+//
+///* TX path */
+//
+//#define AD9081_TX_JESD_MODE		9
+//#define AD9081_TX_JESD_SUBCLASS		1
+//#define AD9081_TX_JESD_VERSION		1
+//#define AD9081_TX_JESD_M		8
+//#define AD9081_TX_JESD_F		4
+//#define AD9081_TX_JESD_K		32
+//#define AD9081_TX_JESD_N		16
+//#define AD9081_TX_JESD_NP		16
+//#define AD9081_TX_JESD_CS		0
+//#define AD9081_TX_JESD_L		4
+//#define AD9081_TX_JESD_S		1
+//#define AD9081_TX_JESD_HD		1
+//#ifdef QUAD_MXFE
+//#define AD9081_TX_LOGICAL_LANE_MAPPING	{0, 1, 2, 3, 4, 5, 6, 7}
+//#else
+//#define AD9081_TX_LOGICAL_LANE_MAPPING	{0, 2, 7, 7, 1, 7, 7, 3}
+//#endif
+//
+//#define AD9081_JRX_TPL_PHASE_ADJUST		15
+//
+//#define AD9081_TX_MAIN_INTERPOLATION	6
+//#define AD9081_TX_CHAN_INTERPOLATION	8
+//#define AD9081_TX_MAIN_NCO_SHIFT	{1000000000, 1100000000, 1200000000, 1300000000}
+//#define AD9081_TX_CHAN_NCO_SHIFT	{0, 0, 0, 0, 0, 0, 0, 0}
+//#define AD9081_TX_CHAN_GAIN		{2048, 2048, 2048, 2048, 0, 0, 0, 0}
+//
+//#define AD9081_TX_DAC_CHAN_CROSSBAR	{0x1, 0x2, 0x4, 0x8}
+//
+///* RX path */
+//
+//#define AD9081_RX_JESD_MODE		10
+//#define AD9081_RX_JESD_SUBCLASS		1
+//#define AD9081_RX_JESD_VERSION		1
+//#define AD9081_RX_JESD_M		8
+//#define AD9081_RX_JESD_F		4
+//#define AD9081_RX_JESD_K		32
+//#define AD9081_RX_JESD_N		16
+//#define AD9081_RX_JESD_NP		16
+//#define AD9081_RX_JESD_CS		0
+//#define AD9081_RX_JESD_L		4
+//#define AD9081_RX_JESD_S		1
+//#define AD9081_RX_JESD_HD		1
+//#ifdef QUAD_MXFE
+//#define AD9081_RX_LOGICAL_LANE_MAPPING	{0, 1, 2, 3, 4, 5, 6, 7}
+//#else
+//#define AD9081_RX_LOGICAL_LANE_MAPPING	{2, 0, 7, 7, 7, 7, 3, 1}
+//#endif
+//#define AD9081_RX_LINK_CONVERTER_SELECT	{0, 1, 2, 3, 8, 9, 10, 11, 0, 0, 0, 0, 0, 0, 0, 0}
+//
+//#define AD9081_RX_MAIN_DECIMATION	{4, 4, 4, 4}
+//#define AD9081_RX_CHAN_DECIMATION	{4, 4, 0, 0, 4, 4, 0, 0}
+//#define AD9081_RX_MAIN_ENABLE		{1, 1, 1, 1}
+//#define AD9081_RX_CHAN_ENABLE		{1, 1, 0, 0, 1, 1, 0, 0}
+//#define AD9081_RX_MAIN_NCO_SHIFT	{1000000000, -400000000, 100000000, 100000000}
+//#define AD9081_RX_CHAN_NCO_SHIFT	{0, 0, 0, 0, 0, 0, 0, 0}
+//
+//// END----------------------------------------ZCU102----------------------------------------
+
+// ----------------------------------------Versal----------------------------------------
+
+#define ADXCVR_REF_CLK_KHZ		500000
+#define ADXCVR_RX_DEV_CLK_KHZ		375000
+#define ADXCVR_TX_DEV_CLK_KHZ		375000
+#define ADXCVR_RX_LANE_CLK_KHZ	24750000
+#define ADXCVR_TX_LANE_CLK_KHZ	24750000
+
+#define AD9081_DAC_FREQUENCY	4000000000
+#define AD9081_ADC_FREQUENCY	2000000000
+
 /* TX path */
 
-#define AD9081_TX_JESD_MODE		24
+#define AD9081_TX_JESD_MODE		35
 #define AD9081_TX_JESD_SUBCLASS		1
 #define AD9081_TX_JESD_VERSION		2
-#define AD9081_TX_JESD_M		8
+#define AD9081_TX_JESD_M		4
 #define AD9081_TX_JESD_F		3
 #define AD9081_TX_JESD_K		256
 #define AD9081_TX_JESD_N		12
 #define AD9081_TX_JESD_NP		12
 #define AD9081_TX_JESD_CS		0
 #define AD9081_TX_JESD_L		8
-#define AD9081_TX_JESD_S		2
+#define AD9081_TX_JESD_S		4
 #define AD9081_TX_JESD_HD		0
 #ifdef QUAD_MXFE
 #define AD9081_TX_LOGICAL_LANE_MAPPING	{0, 1, 2, 3, 4, 5, 6, 7}
@@ -85,17 +170,17 @@
 
 /* RX path */
 
-#define AD9081_RX_JESD_MODE		26
+#define AD9081_RX_JESD_MODE		27
 #define AD9081_RX_JESD_SUBCLASS		1
 #define AD9081_RX_JESD_VERSION		2
-#define AD9081_RX_JESD_M		8
+#define AD9081_RX_JESD_M		4
 #define AD9081_RX_JESD_F		3
 #define AD9081_RX_JESD_K		256
 #define AD9081_RX_JESD_N		12
 #define AD9081_RX_JESD_NP		12
 #define AD9081_RX_JESD_CS		0
 #define AD9081_RX_JESD_L		8
-#define AD9081_RX_JESD_S		2
+#define AD9081_RX_JESD_S		4
 #define AD9081_RX_JESD_HD		0
 #ifdef QUAD_MXFE
 #define AD9081_RX_LOGICAL_LANE_MAPPING	{0, 1, 2, 3, 4, 5, 6, 7}
