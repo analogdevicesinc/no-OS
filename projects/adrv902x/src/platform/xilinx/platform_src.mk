ab393ac22c5ff651ba604dfad099724e5eb8efda
George Mois <george.mois@analog.com>
2023-09-21 10:33:01 +0300

    projects: adrv9025: Madura project
    Add Madura sample project.
    
    Remove Madura driver from drivers build check, cppcheck, and
    astyle check.
    
    Signed-off-by: George Mois <george.mois@analog.com>

---
+++ projects/adrv902x/src/platform/xilinx/platform_src.mk
@@ -0,0 +1,23 @@
+SRCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
+	$(PLATFORM_DRIVERS)/xilinx_axi_io.c \
+	$(PLATFORM_DRIVERS)/xilinx_spi.c \
+	$(PLATFORM_DRIVERS)/xilinx_irq.c \
+	$(PLATFORM_DRIVERS)/xilinx_gpio.c \
+	$(PLATFORM_DRIVERS)/xilinx_delay.c
+	
+ifeq (y,$(strip $(TINYIIOD)))
+SRCS += $(PLATFORM_DRIVERS)/$xilinx_uart.c
+endif
+	
+INCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \
+	$(PLATFORM_DRIVERS)/xilinx_spi.h \
+	$(PLATFORM_DRIVERS)/xilinx_irq.h \
+	$(PLATFORM_DRIVERS)/xilinx_gpio.h
+	
+ifeq (y,$(strip $(TINYIIOD)))
+INCS += $(PLATFORM_DRIVERS)/xilinx_uart.h
+endif	
+	
+	
+	
+	
