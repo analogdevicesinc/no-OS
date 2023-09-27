analogdevicesinc/no-OS

commit 43da1f0153340d2f849ee3872f842bc860b7d59a

axi_core: jesd204: axi_jesd204: Add lane clock
Add support for controlling the lane clock for axi_jesd204_rx and
axi_jesd204_tx.

Add lane clock control support in app_jesd.c.

Add no_os_clk to the sources of projects using axi_jesd204_rx and
axi_jesd204_tx.

Modify the fmcdaq2 project so that it correctly initializes lane
clocks.

Signed-off-by: George Mois <george.mois@analog.com>

drivers/axi_core/jesd204/axi_jesd204_rx.c
@@ -43,6 +43,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include "no_os_clk.h"
 #include "no_os_error.h"
 #include "no_os_delay.h"
 #include "no_os_util.h"
@@ -669,6 +670,16 @@ static int axi_jesd204_rx_jesd204_link_pre_setup(struct jesd204_dev *jdev,
 	pr_debug("%s: Link%u set lane rate %lu kHz\n",
 		 __func__, lnk->link_id, lane_rate);
 
+	ret = no_os_clk_set_rate(jesd->lane_clk->clk_desc, lane_rate);
+	if (ret) {
+		pr_err("%s: Link%u set lane rate %lu kHz failed (%d)\n",
+		       __func__, lnk->link_id, lane_rate, ret);
+		return ret;
+	} else {
+		pr_debug("%s: Link%u set lane rate %lu kHz\n",
+			 __func__, lnk->link_id, lane_rate);
+	}
+
 	return JESD204_STATE_CHANGE_DONE;
 }
 
@@ -725,9 +736,7 @@ static int axi_jesd204_rx_jesd204_link_enable(struct jesd204_dev *jdev,
 {
 	struct axi_jesd204_rx_jesd204_priv *priv = jesd204_dev_priv(jdev);
 	struct axi_jesd204_rx *jesd = priv->jesd;
-#if 0
 	int ret;
-#endif
 
 	pr_debug("%s:%d link_num %u reason %s\n", __func__, __LINE__,
 		 lnk->link_id, jesd204_state_op_reason_str(reason));
@@ -761,6 +770,12 @@ static int axi_jesd204_rx_jesd204_link_enable(struct jesd204_dev *jdev,
 		return ret;
 	}
 #endif
+	ret = no_os_clk_enable(jesd->lane_clk->clk_desc);
+	if (ret) {
+		pr_err("%s: Link%u enable lane clock failed (%d)\n",
+		       __func__, lnk->link_id, ret);
+		return ret;
+	}
 	axi_jesd204_rx_write(jesd, JESD204_RX_REG_SYSREF_STATUS, 0x3);
 	axi_jesd204_rx_write(jesd, JESD204_RX_REG_LINK_DISABLE, 0x0);
 #if 0
@@ -913,6 +928,8 @@ int32_t axi_jesd204_rx_init_jesd_fsm(struct axi_jesd204_rx **jesd204,
 				     const struct jesd204_rx_init *init)
 {
 	struct axi_jesd204_rx_jesd204_priv *priv;
+	struct no_os_clk_desc *clk_desc;
+	struct no_os_clk *lane_clock;
 	struct axi_jesd204_rx *jesd;
 	uint32_t synth_1;
 	uint32_t magic;
@@ -960,9 +977,39 @@ int32_t axi_jesd204_rx_init_jesd_fsm(struct axi_jesd204_rx **jesd204,
 	else if (jesd->encoder >= JESD204_ENCODER_MAX)
 		goto err;
 
+	jesd->config.octets_per_frame = init->octets_per_frame;
+	jesd->config.frames_per_multiframe = init->frames_per_multiframe;
+	jesd->config.subclass_version = init->subclass;
+
+	lane_clock = (struct no_os_clk *)no_os_calloc(1, sizeof(*lane_clock));
+	if (!lane_clock) {
+		ret = -ENOMEM;
+		goto err_1;
+	}
+
+	lane_clock->hw_ch_num = init->lane_clk.hw_ch_num;
+	lane_clock->name = init->lane_clk.name;
+
+	clk_desc = (struct no_os_clk_desc *)no_os_calloc(1, sizeof(*clk_desc));
+	if (!clk_desc) {
+		ret = -ENOMEM;
+		goto err_1;
+	}
+
+	ret = no_os_clk_init(&clk_desc, &init->lane_clk);
+	if (ret)
+		goto err_2;
+
+	lane_clock->clk_desc = clk_desc;
+	jesd->lane_clk = lane_clock;
+
+	ret = no_os_clk_disable(jesd->lane_clk->clk_desc);
+	if (ret)
+		goto err_2;
+
 	ret = jesd204_dev_register(&jesd->jdev, &jesd204_axi_jesd204_rx_init);
 	if (ret)
-		goto err;
+		goto err_2;
 
 	priv = jesd204_dev_priv(jesd->jdev);
 	priv->jesd = jesd;
@@ -971,6 +1018,10 @@ int32_t axi_jesd204_rx_init_jesd_fsm(struct axi_jesd204_rx **jesd204,
 
 	return 0;
 
+err_2:
+	no_os_free(clk_desc);
+err_1:
+	no_os_free(lane_clock);
 err:
 	no_os_free(jesd);
 

drivers/axi_core/jesd204/axi_jesd204_tx.c
@@ -43,6 +43,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <inttypes.h>
+#include "no_os_clk.h"
 #include "no_os_error.h"
 #include "no_os_util.h"
 #include "no_os_alloc.h"
@@ -572,6 +573,16 @@ static int axi_jesd204_tx_jesd204_link_pre_setup(struct jesd204_dev *jdev,
 	pr_debug("%s: Link%u set lane rate %lu kHz\n",
 		 __func__, lnk->link_id, lane_rate);
 
+	ret = no_os_clk_set_rate(jesd->lane_clk->clk_desc, lane_rate);
+	if (ret) {
+		pr_err("%s: Link%u set lane rate %lu kHz failed (%d)\n",
+		       __func__, lnk->link_id, lane_rate, ret);
+		return ret;
+	} else {
+		pr_debug("%s: Link%u set lane rate %lu kHz\n",
+			 __func__, lnk->link_id, lane_rate);
+	}
+
 	return JESD204_STATE_CHANGE_DONE;
 }
 
@@ -606,6 +617,14 @@ static int axi_jesd204_tx_jesd204_link_setup(struct jesd204_dev *jdev,
 		       __func__, lnk->link_id, ret);
 		return ret;
 	}
+
+	ret = no_os_clk_enable(jesd->lane_clk->clk_desc);
+	if (ret) {
+		pr_err("%s: Link%u enable lane clock failed (%d)\n",
+		       __func__, lnk->link_id, ret);
+		return ret;
+	}
+
 	return JESD204_STATE_CHANGE_DONE;
 }
 
@@ -803,6 +822,8 @@ int32_t axi_jesd204_tx_init_jesd_fsm(struct axi_jesd204_tx **jesd204,
 				     const struct jesd204_tx_init *init)
 {
 	struct axi_jesd204_tx_jesd204_priv *priv;
+	struct no_os_clk_desc *clk_desc;
+	struct no_os_clk *lane_clock;
 	struct axi_jesd204_tx *jesd;
 	uint32_t synth_1;
 	uint32_t magic;
@@ -851,9 +872,52 @@ int32_t axi_jesd204_tx_init_jesd_fsm(struct axi_jesd204_tx **jesd204,
 	else if (jesd->encoder >= JESD204_ENCODER_MAX)
 		goto err;
 
+	jesd->config.version = version;
+	jesd->config.device_id = 0;
+	jesd->config.bank_id = 0;
+	jesd->config.enable_scrambling = true;
+	jesd->config.lanes_per_device = jesd->num_lanes;
+	jesd->config.jesd_version = 1;
+	jesd->config.samples_per_frame = 1;
+
+	jesd->config.octets_per_frame = init->octets_per_frame;
+	jesd->config.frames_per_multiframe = init->frames_per_multiframe;
+	jesd->config.converters_per_device = init->converters_per_device;
+	jesd->config.resolution = init->converter_resolution;
+	jesd->config.bits_per_sample = init->bits_per_sample;
+	jesd->config.high_density = init->high_density;
+	jesd->config.control_bits_per_sample = init->control_bits_per_sample;
+	jesd->config.subclass_version = init->subclass;
+
+	lane_clock = (struct no_os_clk *)no_os_calloc(1, sizeof(*lane_clock));
+	if (!lane_clock) {
+		ret = -ENOMEM;
+		goto err;
+	}
+
+	lane_clock->hw_ch_num = init->lane_clk.hw_ch_num;
+	lane_clock->name = init->lane_clk.name;
+
+	clk_desc = (struct no_os_clk_desc *)no_os_calloc(1, sizeof(*clk_desc));
+	if (!clk_desc) {
+		ret = -ENOMEM;
+		goto err_1;
+	}
+
+	ret = no_os_clk_init(&clk_desc, &init->lane_clk);
+	if (ret)
+		goto err_2;
+
+	lane_clock->clk_desc = clk_desc;
+	jesd->lane_clk = lane_clock;
+
+	ret = no_os_clk_disable(jesd->lane_clk->clk_desc);
+	if (ret)
+		goto err_2;
+
 	ret = jesd204_dev_register(&jesd->jdev, &jesd204_axi_jesd204_tx_init);
 	if (ret)
-		goto err;
+		goto err_2;
 
 	priv = jesd204_dev_priv(jesd->jdev);
 	priv->jesd = jesd;
@@ -862,6 +926,10 @@ int32_t axi_jesd204_tx_init_jesd_fsm(struct axi_jesd204_tx **jesd204,
 
 	return 0;
 
+err_2:
+	no_os_free(clk_desc);
+err_1:
+	no_os_free(lane_clock);
 err:
 	no_os_free(jesd);
 

projects/ad6676-ebz/src.mk
@@ -24,6 +24,7 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/api/no_os_spi.c \
 	$(DRIVERS)/api/no_os_gpio.c \
 	$(DRIVERS)/adc/ad6676/ad6676.c \
+	$(NO-OS)/util/no_os_clk.c \
 	$(NO-OS)/util/no_os_util.c \
 	$(NO-OS)/util/no_os_alloc.c \
 	$(NO-OS)/util/no_os_mutex.c \
@@ -61,6 +62,7 @@ INCS +=	$(INCLUDE)/no_os_axi_io.h \
 	$(INCLUDE)/no_os_gpio.h \
 	$(INCLUDE)/no_os_error.h \
 	$(INCLUDE)/no_os_delay.h \
+	$(INCLUDE)/no_os_clk.h \
 	$(INCLUDE)/no_os_util.h \
 	$(INCLUDE)/no_os_print_log.h \
 	$(INCLUDE)/no_os_alloc.h \

projects/ad9371/src.mk
@@ -29,6 +29,7 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(NO-OS)/util/no_os_clk.c \
 	$(NO-OS)/util/no_os_util.c \
 	$(NO-OS)/util/no_os_alloc.c \
 	$(NO-OS)/util/no_os_mutex.c\
@@ -105,6 +106,7 @@ INCS +=	$(INCLUDE)/no_os_axi_io.h \
 	$(INCLUDE)/no_os_gpio.h \
 	$(INCLUDE)/no_os_error.h \
 	$(INCLUDE)/no_os_delay.h \
+	$(INCLUDE)/no_os_clk.h \
 	$(INCLUDE)/no_os_util.h \
 	$(INCLUDE)/no_os_print_log.h \
 	$(INCLUDE)/no_os_alloc.h \

projects/ad9656_fmc/src.mk
@@ -26,6 +26,7 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
         $(DRIVERS)/frequency/ad9553/ad9553.c \
         $(DRIVERS)/frequency/ad9508/ad9508.c \
         $(DRIVERS)/api/no_os_spi.c \
+        $(NO-OS)/util/no_os_clk.c \
         $(NO-OS)/util/no_os_util.c \
         $(NO-OS)/util/no_os_alloc.c \
 	$(NO-OS)/util/no_os_mutex.c \
@@ -60,6 +61,7 @@ INCS +=	$(INCLUDE)/no_os_axi_io.h \
         $(INCLUDE)/no_os_spi.h \
         $(INCLUDE)/no_os_error.h \
         $(INCLUDE)/no_os_delay.h \
+        $(INCLUDE)/no_os_clk.h \
         $(INCLUDE)/no_os_util.h \
         $(INCLUDE)/no_os_units.h \
         $(INCLUDE)/no_os_print_log.h \

projects/fmcadc2/src.mk
@@ -21,6 +21,7 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/adc/ad9625/ad9625.c \
 	$(DRIVERS)/api/no_os_spi.c \
 	$(DRIVERS)/api/no_os_gpio.c \
+	$(NO-OS)/util/no_os_clk.c \
 	$(NO-OS)/util/no_os_util.c \
 	$(NO-OS)/util/no_os_alloc.c \
 	$(NO-OS)/util/no_os_mutex.c \
@@ -58,6 +59,7 @@ INCS +=	$(INCLUDE)/no_os_axi_io.h \
 	$(INCLUDE)/no_os_gpio.h \
 	$(INCLUDE)/no_os_error.h \
 	$(INCLUDE)/no_os_delay.h \
+	$(INCLUDE)/no_os_clk.h \
 	$(INCLUDE)/no_os_util.h \
 	$(INCLUDE)/no_os_alloc.h \
 	$(INCLUDE)/no_os_mutex.h \

projects/fmcadc5/src.mk
@@ -22,6 +22,7 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/adc/ad9625/ad9625.c \
 	$(DRIVERS)/api/no_os_spi.c \
 	$(DRIVERS)/api/no_os_gpio.c \
+	$(NO-OS)/util/no_os_clk.c \
 	$(NO-OS)/util/no_os_util.c \
 	$(NO-OS)/util/no_os_alloc.c \
 	$(NO-OS)/util/no_os_mutex.c \
@@ -58,6 +59,7 @@ INCS +=	$(INCLUDE)/no_os_axi_io.h \
 	$(INCLUDE)/no_os_gpio.h \
 	$(INCLUDE)/no_os_error.h \
 	$(INCLUDE)/no_os_delay.h \
+	$(INCLUDE)/no_os_clk.h \
 	$(INCLUDE)/no_os_util.h \
 	$(INCLUDE)/no_os_print_log.h \
 	$(INCLUDE)/no_os_alloc.h \

projects/fmcdaq3/src.mk
@@ -25,6 +25,7 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/dac/ad9152/ad9152.c \
 	$(DRIVERS)/api/no_os_spi.c \
 	$(DRIVERS)/api/no_os_gpio.c \
+	$(NO-OS)/util/no_os_clk.c \
 	$(NO-OS)/util/no_os_util.c \
 	$(NO-OS)/util/no_os_alloc.c \
 	$(NO-OS)/util/no_os_mutex.c \
@@ -73,6 +74,7 @@ INCS +=	$(INCLUDE)/no_os_axi_io.h \
 	$(INCLUDE)/no_os_gpio.h \
 	$(INCLUDE)/no_os_error.h \
 	$(INCLUDE)/no_os_delay.h \
+	$(INCLUDE)/no_os_clk.h \
 	$(INCLUDE)/no_os_util.h \
 	$(INCLUDE)/no_os_alloc.h \
 	$(INCLUDE)/no_os_mutex.h \

commit 8d01212b59995a8e38956f4f5704011020cbebce

axi_core: jesd204: axi_jesd204_*x.c Fix lane clock
Fix the lane clock initialization in axi_jesd204_rx.c and
axi_jesd204_tx.c (set the jesd_rx or jesd_tx member of the
jesd204_clock structure to the corresponding AXI jesd structure.

Fixes: 43da1f015 ("axi_core: jesd204: axi_jesd204: Add lane clock")

Signed-off-by: George Mois <george.mois@analog.com>

drivers/axi_core/jesd204/axi_jesd204_rx.c
@@ -51,6 +51,7 @@
 #include "axi_jesd204_rx.h"
 #include "no_os_axi_io.h"
 #include "no_os_print_log.h"
+#include "jesd204_clk.h"
 
 /******************************************************************************/
 /********************** Macros and Constants Definitions **********************/
@@ -995,6 +996,10 @@ int32_t axi_jesd204_rx_init_jesd_fsm(struct axi_jesd204_rx **jesd204,
 		ret = -ENOMEM;
 		goto err_1;
 	}
+
+	struct jesd204_clk *clk1 = init->lane_clk.dev_desc;
+	clk1->jesd_tx = NULL;
+	clk1->jesd_rx = jesd;
 
 	ret = no_os_clk_init(&clk_desc, &init->lane_clk);
 	if (ret)

drivers/axi_core/jesd204/axi_jesd204_tx.c
@@ -51,6 +51,7 @@
 #include "no_os_axi_io.h"
 #include "no_os_delay.h"
 #include "no_os_print_log.h"
+#include "jesd204_clk.h"
 
 /******************************************************************************/
 /********************** Macros and Constants Definitions **********************/
@@ -907,6 +908,10 @@ int32_t axi_jesd204_tx_init_jesd_fsm(struct axi_jesd204_tx **jesd204,
 		ret = -ENOMEM;
 		goto err_1;
 	}
+
+	struct jesd204_clk *clk1 = init->lane_clk.dev_desc;
+	clk1->jesd_tx = jesd;
+	clk1->jesd_rx = NULL;
 
 	ret = no_os_clk_init(&clk_desc, &init->lane_clk);
 	if (ret)

projects/ad6676-ebz/src.mk
@@ -20,6 +20,8 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.c \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
 	$(DRIVERS)/api/no_os_spi.c \
 	$(DRIVERS)/api/no_os_gpio.c \
@@ -52,6 +54,8 @@ INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
 	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.h \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \
 	$(DRIVERS)/io-expander/demux_spi/demux_spi.h \
 	$(DRIVERS)/adc/ad6676/ad6676.h

projects/ad9172/src.mk
@@ -28,6 +28,7 @@ SRCS += $(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.c \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.c \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
 	$(NO-OS)/util/no_os_util.c \
 	$(NO-OS)/util/no_os_alloc.c \
@@ -63,6 +64,7 @@ INCS += $(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.h \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.h \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h
 INCS +=	$(PLATFORM_DRIVERS)/$(PLATFORM)_spi.h \
 	$(PLATFORM_DRIVERS)/$(PLATFORM)_gpio.h

projects/ad9208/src.mk
@@ -40,6 +40,7 @@ SRCS += $(DRIVERS)/api/no_os_spi.c \
 SRCS += $(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.c \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
 	$(NO-OS)/util/no_os_util.c \
 	$(NO-OS)/util/no_os_alloc.c \
@@ -56,6 +57,7 @@ INCS += $(DRIVERS)/frequency/hmc7044/hmc7044.h \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.h \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h
 INCS +=	$(PLATFORM_DRIVERS)/$(PLATFORM)_spi.h \
 	$(PLATFORM_DRIVERS)/$(PLATFORM)_gpio.h

projects/ad9371/src.mk
@@ -29,6 +29,7 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.c \
 	$(NO-OS)/util/no_os_clk.c \
 	$(NO-OS)/util/no_os_util.c \
 	$(NO-OS)/util/no_os_alloc.c \
@@ -88,7 +89,8 @@ INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
 	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.h \
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
-	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.h
 ifeq (xilinx,$(strip $(PLATFORM)))
 INCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \

projects/ad9656_fmc/src.mk
@@ -21,6 +21,8 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
         $(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c \
         $(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
         $(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
+        $(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+        $(DRIVERS)/axi_core/jesd204/jesd204_clk.c \
         $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
         $(DRIVERS)/adc/ad9656/ad9656.c \
         $(DRIVERS)/frequency/ad9553/ad9553.c \
@@ -52,6 +54,8 @@ INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
         $(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h \
         $(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \
         $(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
+        $(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+        $(DRIVERS)/axi_core/jesd204/jesd204_clk.h \
         $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \
         $(DRIVERS)/frequency/ad9553/ad9553.h \
         $(DRIVERS)/frequency/ad9508/ad9508.h \

projects/adrv9009/src.mk
@@ -48,6 +48,7 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.c \
 	$(DRIVERS)/api/no_os_spi.c \
 	$(DRIVERS)/api/no_os_gpio.c \
 	$(NO-OS)/jesd204/jesd204-core.c \
@@ -129,7 +130,8 @@ INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
 	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.h \
 	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
-	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.h
 ifeq (xilinx,$(strip $(PLATFORM)))
 INCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \

projects/fmcadc2/src.mk
@@ -17,6 +17,8 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.c \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
 	$(DRIVERS)/adc/ad9625/ad9625.c \
 	$(DRIVERS)/api/no_os_spi.c \
@@ -50,6 +52,8 @@ INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
 	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.h \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \
 	$(DRIVERS)/adc/ad9625/ad9625.h					
 INCS +=	$(PLATFORM_DRIVERS)/$(PLATFORM)_spi.h \

projects/fmcadc5/src.mk
@@ -18,6 +18,8 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.c \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
 	$(DRIVERS)/adc/ad9625/ad9625.c \
 	$(DRIVERS)/api/no_os_spi.c \
@@ -50,6 +52,8 @@ INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
 	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.h \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \
 	$(DRIVERS)/adc/ad9625/ad9625.h					
 INCS +=	$(PLATFORM_DRIVERS)/$(PLATFORM)_spi.h \

projects/fmcdaq3/src.mk
@@ -19,6 +19,7 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.c \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
 	$(DRIVERS)/frequency/ad9528/ad9528.c \
 	$(DRIVERS)/adc/ad9680/ad9680.c \
@@ -63,6 +64,7 @@ INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.h \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \
 	$(DRIVERS)/frequency/ad9528/ad9528.h \
 	$(DRIVERS)/adc/ad9680/ad9680.h \

projects/fmcjesdadc1/src.mk
@@ -17,6 +17,8 @@ SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
 	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.c \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
 	$(DRIVERS)/io-expander/demux_spi/demux_spi.c \
 	$(DRIVERS)/api/no_os_spi.c \
@@ -52,6 +54,8 @@ INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
 	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h \
 	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+	$(DRIVERS)/axi_core/jesd204/jesd204_clk.h \
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \
 	$(DRIVERS)/io-expander/demux_spi/demux_spi.h \
 	$(DRIVERS)/adc/ad9250/ad9250.h \