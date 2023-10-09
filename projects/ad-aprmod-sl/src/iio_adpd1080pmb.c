f08f9de6c5706dab93e5c30845bd9a5203dc4601
Darius Berghe <darius.berghe@analog.com>
2023-10-09 11:05:53 +0300

    gestures over iio
    import iio
    import time
    
    frequency = 10  # Hz
    gestures = [
    	"click",
    	"up",
    	"down",
    	"left",
    	"right"
    ]
    
    ctx = iio.Context("ip:169.254.97.40")
    dev = ctx.find_device("adpd1080pmb")
    
    while True:
        g = int(dev.attrs['gestures'].value)
    
        for bit in range(len(gestures)):
            if g & (1 << bit):
                print(gestures[bit] + ' ')
    
        time.sleep(1/frequency)

---
+++ projects/ad-aprmod-sl/src/iio_adpd1080pmb.c
@@ -0,0 +1,281 @@
+/***************************************************************************//**
+ *   @file   iio_adpd1080pmb.c
+ *   @brief  Implementation of the ADPD1080 IIO driver.
+ *   @author Darius Berghe (darius.berghe@analog.com)
+********************************************************************************
+ * Copyright 2021(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#include <stdlib.h>
+#include <stdint.h>
+#include "no_os_error.h"
+#include "no_os_util.h"
+#include "no_os_alloc.h"
+#include "iio.h"
+#include "iio_adpd1080pmb.h"
+
+enum adpd1080pmb_dev_attrs {
+	ADPD1080PMB_DEV_ATTR_GESTURES,
+	ADPD1080PMB_DEV_ATTR_TH_INTENSITY,
+	ADPD1080PMB_DEV_ATTR_TH_CLICK,
+};
+
+int adpd1080pmb_get_fifo_data(struct adpd188_dev *desc, int32_t *buff)
+{
+	uint8_t byte_no;
+	uint32_t s;
+	uint16_t data_buff[16];
+	int32_t ret;
+
+	ret = adpd188_fifo_status_get(desc, &byte_no);
+	if (NO_OS_IS_ERR_VALUE(ret))
+		return ret;
+	
+	if (byte_no < 16)
+		return -EAGAIN; // nothing to do (yet)
+
+	for (s = 0; s < 16; s++) {
+		ret = adpd188_reg_read(desc, ADPD188_REG_FIFO_ACCESS,
+					&data_buff[s]);
+		if (NO_OS_IS_ERR_VALUE(ret))
+			return ret;
+		
+		if (s & 0x1)
+			buff[s/2] = ((uint16_t)data_buff[s] << 16) | data_buff[s - 1];
+	}
+
+	return 0;
+}
+
+enum gesture {
+	click,
+	up,
+	down,
+	left,
+	right
+};
+
+char *gestures[6] = {
+	"click",
+	"up",
+	"down",
+	"left",
+	"right"
+};
+
+static unsigned isqrt(unsigned long val) {
+    unsigned long temp, g=0, b = 0x8000, bshft = 15;
+    do {
+        if (val >= (temp = (((g << 1) + b)<<bshft--))) {
+           g += b;
+           val -= temp;
+        }
+    } while (b >>= 1);
+    return g;
+}
+
+int adpd1080pmb_gesture_detection(struct adpd1080pmb_iio_desc *iiodev)
+{
+	struct adpd188_dev *desc = iiodev->dev;
+	int32_t buff[8];
+	int x, y, dx, dy, L, m, d;
+	int gestureStopX, gestureStopY;
+	enum gesture gesture;
+
+	if (adpd1080pmb_get_fifo_data(desc, buff) == -EAGAIN) {
+		// no data, no problem, let iiod run
+		return 0;
+	}
+	
+	// This algorithm is provided in the adpd2140 datasheet
+	x = (buff[1] - buff[0]) * 1000 / (buff[1] + buff[0]);
+	y = (buff[3] - buff[2]) * 1000 / (buff[3] + buff[2]);
+	L = (buff[0] + buff[1] + buff[2] + buff[3]) * 1000;
+
+	if (!iiodev->event && L > iiodev->th_intensity)
+	{
+		iiodev->evc = 0;
+		iiodev->event = true;
+		iiodev->gestureStartX = x;
+		iiodev->gestureStartY = y;
+	}
+
+	if (iiodev->event) {
+		iiodev->evc += 1;
+		if (iiodev->evc >= 5 && L < iiodev->th_intensity) {
+			iiodev->event = false;
+			gestureStopX = x;
+			gestureStopY = y;
+			dx = iiodev->gestureStartX - gestureStopX;
+			dy = iiodev->gestureStartY - gestureStopY;
+			m = dy * 1000 / dx;
+			d = isqrt(dx * dx + dy * dy);
+		}
+		if (d < iiodev->th_click)
+			gesture = click;
+		else {
+			if (abs(m) > 1000) {
+				if (iiodev->gestureStartY > gestureStopY)
+					gesture = up;
+				else
+					gesture = down;
+			}
+			else {
+				if (iiodev->gestureStartX > gestureStopX)
+					gesture = left;
+				else
+					gesture = right;
+			}
+		}
+		if (!iiodev->event)
+			iiodev->gestures |= NO_OS_BIT(gesture);
+	}
+
+	return 0;
+}
+
+int32_t adpd1080pmb_iio_init(struct adpd1080pmb_iio_desc **iiodev,
+			 struct adpd1080pmb_iio_init_param *iiodevconfig)
+{
+	int ret;
+	struct adpd1080pmb_iio_desc *d = (struct adpd1080pmb_iio_desc *)no_os_calloc(1, sizeof(*d));
+	if (!d)
+		return -ENOMEM;
+
+	d->dev = iiodevconfig->dev;
+	d->event = false;
+	d->evc = 0;
+	d->th_click = iiodevconfig->th_click;
+	d->th_intensity = iiodevconfig->th_intensity;
+
+	*iiodev = d;
+
+	return 0;
+}
+
+int32_t adpd1080pmb_iio_remove(struct adpd1080pmb_iio_desc *iiodev)
+{
+	no_os_free(iiodev);
+	return 0;
+}
+
+static int adpd1080pmb_attr_read(void *device, char *buf, uint32_t len,
+				     const struct iio_ch_info *channel,
+				     intptr_t priv)
+{
+	int ret;
+	struct adpd1080pmb_iio_desc *iiodev = (struct adpd1080pmb_iio_desc *)device;
+	int32_t vals[2];
+	int type;
+	int valcount;
+
+
+	switch (priv) {
+	case ADPD1080PMB_DEV_ATTR_GESTURES:
+		vals[0] = iiodev->gestures;
+		iiodev->gestures = 0; // clear on read
+		type = IIO_VAL_INT;
+		valcount = 1;
+		break;
+	case ADPD1080PMB_DEV_ATTR_TH_INTENSITY:
+		vals[0] = iiodev->th_intensity;
+		type = IIO_VAL_INT;
+		valcount = 1;
+		break;
+	case ADPD1080PMB_DEV_ATTR_TH_CLICK:
+		vals[0] = iiodev->th_click;
+		type = IIO_VAL_INT;
+		valcount = 1;
+		break;
+	default:
+		return -EINVAL;
+	};
+
+	return iio_format_value(buf, len, type, valcount, vals);
+}
+
+static int adpd1080pmb_attr_write(void *device, char *buf,
+				  uint32_t len, const struct iio_ch_info *channel,
+				  intptr_t priv)
+{
+	int ret;
+	int32_t val;
+	struct adpd1080pmb_iio_desc *iiodev = (struct adpd1080pmb_iio_desc *)device;
+
+	ret = iio_parse_value(buf, IIO_VAL_INT, &val, NULL);
+	if (ret < 0)
+		return ret;
+
+	switch (priv) {
+	case ADPD1080PMB_DEV_ATTR_TH_INTENSITY:
+		if (val > 2000000)
+			return -EINVAL;
+		iiodev->th_intensity = val;
+		break;
+	case ADPD1080PMB_DEV_ATTR_TH_CLICK:
+		if (val > 1000)
+			return -EINVAL;
+		iiodev->th_click = val;
+		break;
+	default:
+		return -EINVAL;
+	};
+
+	return 0;
+}
+
+static struct iio_attribute adpd1080pmb_device_attributes[] = {
+	{
+		.name = "gestures",
+		.priv = ADPD1080PMB_DEV_ATTR_GESTURES,
+		.show = adpd1080pmb_attr_read,
+	},
+	{
+		.name = "th_intensity",
+		.priv = ADPD1080PMB_DEV_ATTR_TH_INTENSITY,
+		.show = adpd1080pmb_attr_read,
+		.store = adpd1080pmb_attr_write,
+	},
+	{
+		.name = "th_click",
+		.priv = ADPD1080PMB_DEV_ATTR_TH_CLICK,
+		.show = adpd1080pmb_attr_read,
+		.store = adpd1080pmb_attr_write,
+	},
+	END_ATTRIBUTES_ARRAY
+};
+
+struct iio_device iio_adpd1080pmb_device = {
+	.attributes = adpd1080pmb_device_attributes,
+};
+
---
+++ projects/ad-aprmod-sl/src/main.c
@@ -0,0 +1,525 @@
+/***************************************************************************//**
+ *   @file   main.c
+ *   @brief  Implementation of Main Function.
+ *   @author Darius Berghe (darius.berghe@analog.com)
+********************************************************************************
+ * Copyright 2020(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#include <stdio.h>
+#include <math.h>
+#include "no_os_error.h"
+#include "no_os_util.h"
+#include "iio_adpd188.h"
+#include "iio_app.h"
+#include "maxim_gpio.h"
+#include "no_os_gpio.h"
+#include "no_os_timer.h"
+#include "maxim_irq.h"
+#include "maxim_i2c.h"
+#include "maxim_spi.h"
+#include "maxim_uart.h"
+#include "maxim_uart_stdio.h"
+#include "maxim_gpio_irq.h"
+#include "maxim_timer.h"
+#include "parameters.h"
+#include "adin1110.h"
+#include "lwip_adin1110.h"
+#include "iio_adpd1080pmb.h"
+
+uint8_t adin1110_mac_address[6] = {0x00, 0x18, 0x80, 0x03, 0x25, 0x80};
+
+const struct no_os_gpio_init_param adin1110_reset_ip = {
+	.port = 0,
+	.number = 15,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = &(struct max_gpio_init_param) {
+		.vssel = MXC_GPIO_VSSEL_VDDIOH,
+	},
+};
+
+const struct no_os_spi_init_param adin1110_spi_ip = {
+	.device_id = 3,
+	.max_speed_hz = 25000000,
+	.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
+	.mode = NO_OS_SPI_MODE_0,
+	.platform_ops = &max_spi_ops,
+	.chip_select = 0,
+	.extra = &(struct max_spi_init_param) {
+		.num_slaves = 1,
+		.polarity = SPI_SS_POL_LOW,
+		.vssel = MXC_GPIO_VSSEL_VDDIOH,
+	},
+};
+
+struct adin1110_init_param adin1110_ip = {
+	.chip_type = ADIN1110,
+	.comm_param = adin1110_spi_ip,
+	.reset_param = adin1110_reset_ip,
+	.append_crc = false,
+};
+
+static uint8_t in_buff[1024];
+
+static void adpd1080_sync_gpio_cb(void *ctx)
+{
+	int32_t *cnt = ctx;
+
+	*cnt += 1;
+}
+
+static int32_t adpd1080pmod_32k_calib(struct adpd188_dev *adpd1080_dev)
+{
+	int32_t status;
+	uint16_t temp_reg;
+	uint32_t t_start, t_stop = 0;
+	volatile int32_t sync_gpio_pulse_no;
+	struct no_os_timer_desc *cal_timer;
+	struct no_os_timer_init_param cal_timer_init = {
+		.id = 0,
+		.ticks_count = 0,
+		.freq_hz = 1000000,
+		.platform_ops = &max_timer_ops,
+	};
+	struct no_os_irq_ctrl_desc *nvic;
+	struct no_os_irq_init_param nvic_param = {
+		.irq_ctrl_id = 0,
+		.platform_ops = &max_irq_ops,
+	};
+	struct no_os_irq_ctrl_desc *gpio0_irqc;
+	struct no_os_irq_init_param gpio0_irqc_config = {
+		.irq_ctrl_id = GPIO0_PORT,
+		.platform_ops = &max_gpio_irq_ops,
+		.extra = NULL
+	};
+
+	status = no_os_timer_init(&cal_timer, &cal_timer_init);
+	if(status != 0)
+		return -1;
+	status = no_os_timer_start(cal_timer);
+	if(status != 0)
+		goto timer_finish;
+
+	status = no_os_irq_ctrl_init(&nvic, &nvic_param);
+	if (status)
+		return status;
+	status = no_os_irq_ctrl_init(&gpio0_irqc, &gpio0_irqc_config);
+	if(status != 0)
+		goto finish;
+
+	struct no_os_callback_desc sync_gpio_cb = {
+		.callback = adpd1080_sync_gpio_cb,
+		.ctx = &sync_gpio_pulse_no,
+		.event = NO_OS_EVT_GPIO,
+		.peripheral = NO_OS_GPIO_IRQ
+	};
+	status = no_os_irq_register_callback(gpio0_irqc, GPIO0_PIN, &sync_gpio_cb);
+	if(status != 0)
+		goto finish;
+
+	status = no_os_irq_trigger_level_set(gpio0_irqc, GPIO0_PIN, NO_OS_IRQ_EDGE_FALLING);
+	if(status != 0)
+		goto finish;
+
+	status = no_os_irq_set_priority(gpio0_irqc, GPIO0_PIN, 1);
+	if (status != 0)
+		goto finish;
+
+	status = adpd188_adc_fsample_set(adpd1080_dev, 1000);
+	if(status != 0)
+		goto finish;
+
+	status = adpd188_reg_write(adpd1080_dev, ADPD188_REG_GPIO_DRV,
+				   ADPD188_GPIO_DRV_GPIO0_ENA_MASK);
+	if(status != 0)
+		goto finish;
+
+	status = adpd188_reg_write(adpd1080_dev, ADPD188_REG_GPIO_CTRL, 2);
+	if(status != 0)
+		goto finish;
+
+	status = adpd188_mode_set(adpd1080_dev, ADPD188_NORMAL);
+	if (status != 0)
+		goto finish;
+
+	status = no_os_irq_enable(gpio0_irqc, GPIO0_PIN);
+	if(status != 0)
+		goto finish;
+
+	status = no_os_irq_enable(nvic, GPIO0_IRQn);
+	if (status)
+		goto finish;
+
+	while (true) {
+		status = adpd188_reg_read(adpd1080_dev, ADPD188_REG_SAMPLE_CLK, &temp_reg);
+		if(status != 0)
+			goto finish;
+		status = no_os_timer_counter_get(cal_timer, &t_start);
+		if(status != 0)
+			goto finish;
+		sync_gpio_pulse_no = 0;
+		no_os_mdelay(100);
+		status = no_os_timer_counter_get(cal_timer, &t_stop);
+		if(status != 0)
+			goto finish;
+		if (abs(sync_gpio_pulse_no - 100) < 1)
+			break;
+		if (sync_gpio_pulse_no < 100)
+			temp_reg -= 1;
+		else if (sync_gpio_pulse_no > 100)
+			temp_reg += 1;
+		status = adpd188_reg_write(adpd1080_dev, ADPD188_REG_SAMPLE_CLK, temp_reg);
+		if(status != 0)
+			goto finish;
+	}
+
+	status = no_os_irq_disable(gpio0_irqc, GPIO0_PIN);
+	if(status != 0)
+		goto finish;
+
+	status = no_os_irq_unregister_callback(gpio0_irqc, GPIO0_PIN, &sync_gpio_cb);
+	if(status != 0)
+		goto finish;
+
+	status = adpd188_mode_set(adpd1080_dev, ADPD188_PROGRAM);
+	if (status != 0)
+		goto finish;
+
+	status = adpd188_adc_fsample_set(adpd1080_dev, 16);
+	if(status != 0)
+		goto finish;
+
+	status = adpd188_reg_write(adpd1080_dev, ADPD188_REG_GPIO_DRV, 0);
+	if(status != 0)
+		goto finish;
+
+	status = adpd188_reg_write(adpd1080_dev, ADPD188_REG_GPIO_CTRL, 0);
+	if(status != 0)
+		goto finish;
+
+finish:
+	// TODO FIX THIS: no_os_irq_ctrl_remove(gpio0_irqc);
+timer_finish:
+	no_os_timer_remove(cal_timer);
+
+	return status;
+}
+
+int app_step(void *arg)
+{
+	static int cnt1 = 0;
+
+	cnt1++;
+	if (cnt1 == 100) {
+		cnt1 = 0;
+		adpd1080pmb_gesture_detection(arg);
+	}
+
+	return 0;
+}
+
+int main(void)
+{
+	int32_t status;
+	uint16_t reg_data;
+	struct iio_app_desc *app;
+	struct iio_app_init_param app_init_param = { 0 };
+
+	struct no_os_uart_init_param adpd1080_uart_ip = {
+		.device_id = UART_DEVICE_ID,
+		.irq_id = UART_IRQ_ID,
+		.asynchronous_rx = true,
+		.baud_rate = UART_BAUDRATE,
+		.size = NO_OS_UART_CS_8,
+		.parity = NO_OS_UART_PAR_NO,
+		.stop = NO_OS_UART_STOP_1_BIT,
+		.extra = &(struct max_uart_init_param) {
+			.flow = UART_FLOW_DIS,
+			.vssel = MXC_GPIO_VSSEL_VDDIOH,
+		},
+		.platform_ops = &max_uart_ops,
+	};
+
+	struct adpd188_iio_desc *adpd1080_iio_device;
+	struct adpd188_iio_init_param adpd1080_iio_inital = {
+		.drv_init_param.device = ADPD1080,
+		.drv_init_param.phy_opt = ADPD188_I2C,
+		.drv_init_param.phy_init.i2c_phy =
+		{
+			.slave_address = 0x64,
+			.max_speed_hz = 400000,
+			.platform_ops = &max_i2c_ops,
+			.extra = &(struct max_i2c_init_param) {
+				.vssel = MXC_GPIO_VSSEL_VDDIOH,
+			},
+		},
+		.drv_init_param.gpio0_init =
+		{
+			.port = GPIO0_PORT,
+			.number = GPIO0_PIN,
+			.platform_ops = &max_gpio_ops,
+			.extra = &(struct max_gpio_init_param) {
+				.vssel = MXC_GPIO_VSSEL_VDDIOH,
+			},
+		},
+		.drv_init_param.gpio1_init =
+		{
+			.port = GPIO1_PORT,
+			.number = GPIO1_PIN,
+			.platform_ops = &max_gpio_ops,
+			.extra = &(struct max_gpio_init_param) {
+				.vssel = MXC_GPIO_VSSEL_VDDIOH,
+			},
+		}
+	};
+	struct iio_data_buffer iio_adpd1080_read_buff = {
+		.buff = in_buff,
+		.size = NO_OS_ARRAY_SIZE(in_buff),
+	};
+
+	no_os_init();
+
+	status = adpd188_iio_init(&adpd1080_iio_device, &adpd1080_iio_inital);
+	if (status < 0)
+		return status;
+
+	status = adpd188_adc_fsample_set(adpd1080_iio_device->drv_dev, 16);
+	if(status != 0)
+		return -1;
+
+	status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_SLOT_EN,
+				  &reg_data);
+	if(status != 0)
+		return -1;
+	reg_data |= ADPD188_SLOT_EN_RDOUT_MODE_MASK |
+		    ADPD188_SLOT_EN_FIFO_OVRN_PREVENT_MASK;
+	status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_SLOT_EN,
+				   reg_data);
+	if(status != 0)
+		return -1;
+
+	/* Slot A chop mode is inverted, non-inverted, non-inverted, inverted */
+	status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_A,
+				  &reg_data);
+	if(status != 0)
+		return -1;
+	reg_data |= 0x9 & ADPD188_INT_SEQ_A_INTEG_ORDER_A_MASK;
+	status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_A,
+				   reg_data);
+	if(status != 0)
+		return -1;
+	/* Slot B chop mode is inverted, non-inverted, non-inverted, inverted */
+	status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_B,
+				  &reg_data);
+	if(status != 0)
+		return -1;
+	reg_data |= 0x9 & ADPD188_INT_SEQ_B_INTEG_ORDER_B_MASK;
+	status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_B,
+				   reg_data);
+	if(status != 0)
+		return -1;
+
+	/* Set blue LED 1 power */
+	status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
+				  ADPD188_REG_ILED1_COARSE, &reg_data);
+	if(status != 0)
+		return -1;
+	reg_data &= ~ADPD188_ILED1_COARSE_ILED1_COARSE_MASK;
+	reg_data |= (0x6 << ADPD188_ILED1_COARSE_ILED1_COARSE_POS) &
+		    ADPD188_ILED1_COARSE_ILED1_COARSE_MASK;
+	reg_data &= ~ADPD188_ILED1_COARSE_ILED1_SLEW_MASK;
+	reg_data |= (0x3 << ADPD188_ILED1_COARSE_ILED1_SLEW_POS) &
+		    ADPD188_ILED1_COARSE_ILED1_SLEW_MASK;
+	reg_data |= ADPD188_ILED1_COARSE_ILED1_SCALE_MASK;
+	status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
+				   ADPD188_REG_ILED1_COARSE, reg_data);
+	if(status != 0)
+		return -1;
+
+	/* Slot A 4 LED pulses with 15us period */
+	status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
+				  ADPD188_REG_SLOTA_NUMPULSES, &reg_data);
+	if(status != 0)
+		return -1;
+	reg_data &= ~ADPD188_SLOTA_NUMPULSES_SLOTA_PULSES_MASK;
+	reg_data |= (0x4 << ADPD188_SLOTA_NUMPULSES_SLOTA_PULSES_POS) &
+		    ADPD188_SLOTA_NUMPULSES_SLOTA_PULSES_MASK;
+	reg_data &= ~ADPD188_SLOTA_NUMPULSES_SLOTA_PERIOD_MASK;
+	reg_data |= (0xE << ADPD188_SLOTA_NUMPULSES_SLOTA_PERIOD_POS) &
+		    ADPD188_SLOTA_NUMPULSES_SLOTA_PERIOD_MASK;
+	status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
+				   ADPD188_REG_SLOTA_NUMPULSES, reg_data);
+	if(status != 0)
+		return -1;
+
+	/* Slot B 4 LED pulses with 15us period */
+	status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
+				  ADPD188_REG_SLOTB_NUMPULSES, &reg_data);
+	if(status != 0)
+		return -1;
+	reg_data &= ~ADPD188_SLOTB_NUMPULSES_SLOTB_PULSES_MASK;
+	reg_data |= (0x4 << ADPD188_SLOTB_NUMPULSES_SLOTB_PULSES_POS) &
+		    ADPD188_SLOTB_NUMPULSES_SLOTB_PULSES_MASK;
+	reg_data &= ~ADPD188_SLOTB_NUMPULSES_SLOTB_PERIOD_MASK;
+	reg_data |= (0xE << ADPD188_SLOTB_NUMPULSES_SLOTB_PERIOD_POS) &
+		    ADPD188_SLOTB_NUMPULSES_SLOTB_PERIOD_MASK;
+	status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
+				   ADPD188_REG_SLOTB_NUMPULSES, reg_data);
+	if(status != 0)
+		return -1;
+
+	/* Slot A integrator window */
+	status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
+				  ADPD188_REG_SLOTA_AFE_WINDOW, &reg_data);
+	if(status != 0)
+		return -1;
+	reg_data &= ~ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_WIDTH_MASK;
+	reg_data |= (0x4 << ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_WIDTH_POS) &
+		    ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_WIDTH_MASK;
+	reg_data &= ~ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_OFFSET_MASK;
+	reg_data |= (0x2F0 << ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_OFFSET_POS) &
+		    ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_OFFSET_MASK;
+	status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
+				   ADPD188_REG_SLOTA_AFE_WINDOW, reg_data);
+	if(status != 0)
+		return -1;
+
+	/* Slot B integrator window */
+	status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
+				  ADPD188_REG_SLOTB_AFE_WINDOW, &reg_data);
+	if(status != 0)
+		return -1;
+	reg_data &= ~ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_WIDTH_MASK;
+	reg_data |= (0x4 << ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_WIDTH_POS) &
+		    ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_WIDTH_MASK;
+	reg_data &= ~ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_OFFSET_MASK;
+	reg_data |= (0x2F0 << ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_OFFSET_POS) &
+		    ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_OFFSET_MASK;
+	status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
+				   ADPD188_REG_SLOTB_AFE_WINDOW, reg_data);
+	if(status != 0)
+		return -1;
+
+	/* Math for chop mode is inverted, non-inverted, non-inverted, inverted */
+	status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_MATH,
+				  &reg_data);
+	if(status != 0)
+		return -1;
+	reg_data &= ~ADPD188_MATH_FLT_MATH34_B_MASK;
+	reg_data |= (0x01 << ADPD188_MATH_FLT_MATH34_B_POS) &
+		    ADPD188_MATH_FLT_MATH34_B_MASK;
+	reg_data &= ~ADPD188_MATH_FLT_MATH34_A_MASK;
+	reg_data |= (0x01 << ADPD188_MATH_FLT_MATH34_A_POS) &
+		    ADPD188_MATH_FLT_MATH34_A_MASK;
+	reg_data &= ~ADPD188_MATH_FLT_MATH12_B_MASK;
+	reg_data |= (0x02 << ADPD188_MATH_FLT_MATH12_B_POS) &
+		    ADPD188_MATH_FLT_MATH12_B_MASK;
+	reg_data &= ~ADPD188_MATH_FLT_MATH12_A_MASK;
+	reg_data |= (0x02 << ADPD188_MATH_FLT_MATH12_A_POS) &
+		    ADPD188_MATH_FLT_MATH12_A_MASK;
+
+	status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_MATH,
+				   reg_data);
+	if(status != 0)
+		return -1;
+
+	status = adpd1080pmod_32k_calib(adpd1080_iio_device->drv_dev);
+	if (status < 0)
+		return status;
+
+	status = adpd188_clk32mhz_cal(adpd1080_iio_device->drv_dev);
+	if(status != 0)
+		return status;
+
+	status = adpd188_adc_fsample_set(adpd1080_iio_device->drv_dev, 512.0);
+	if(status != 0)
+		return status;
+	
+	int ch;
+	int32_t raw[8];
+	while(adpd1080pmb_get_fifo_data(adpd1080_iio_device->drv_dev, raw) == -EAGAIN)
+		; // blocking wait
+	for (ch = 0; ch < 8; ch++) {
+		uint16_t offset;
+		uint8_t offset_reg = ch / 4 ? ADPD188_REG_SLOTB_CH1_OFFSET : ADPD188_REG_SLOTA_CH1_OFFSET + (ch % 4);
+		status = adpd188_reg_read(adpd1080_iio_device->drv_dev, offset_reg,
+					&offset);
+		if (status != 0)
+			return status;
+		status = adpd188_reg_write(adpd1080_iio_device->drv_dev, offset_reg,
+					raw[ch] + offset);
+		if (status != 0)
+			return status;
+	}
+
+	status = adpd188_mode_set(adpd1080_iio_device->drv_dev, ADPD188_NORMAL);
+	if (status != 0)
+		return status;
+	
+	struct adpd1080pmb_iio_desc *adpd1080pmb;
+	struct adpd1080pmb_iio_init_param adpd1080pmb_config = {
+		.dev = adpd1080_iio_device->drv_dev,
+		.th_intensity = 1200000,
+		.th_click = 60,
+	};
+	status = adpd1080pmb_iio_init(&adpd1080pmb, &adpd1080pmb_config);
+
+	struct iio_app_device devices[] = {
+		IIO_APP_DEVICE("adpd1080", adpd1080_iio_device, &iio_adpd188_device,
+			       &iio_adpd1080_read_buff, NULL, NULL),
+		IIO_APP_DEVICE("adpd1080pmb", adpd1080pmb, &iio_adpd1080pmb_device,
+				NULL, NULL, NULL)
+	};
+
+	memcpy(adin1110_ip.mac_address, adin1110_mac_address, NETIF_MAX_HWADDR_LEN);
+	app_init_param.devices = devices;
+	app_init_param.nb_devices = NO_OS_ARRAY_SIZE(devices);
+	app_init_param.uart_init_params = adpd1080_uart_ip;
+	app_init_param.post_step_callback = app_step;
+	app_init_param.arg = adpd1080pmb;
+	app_init_param.lwip_param.platform_ops = &adin1110_lwip_ops;
+	app_init_param.lwip_param.mac_param = &adin1110_ip;
+	app_init_param.lwip_param.extra = NULL;
+	memcpy(app_init_param.lwip_param.hwaddr, adin1110_mac_address,
+	       NETIF_MAX_HWADDR_LEN);
+
+	status = iio_app_init(&app, app_init_param);
+	if (status)
+		return status;
+	
+	//no_os_uart_stdio(app->uart_desc);
+
+	return iio_app_run(app);
+}
+
