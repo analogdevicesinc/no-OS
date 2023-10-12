88a33ca0bf94471d7318e14c135eeaa963a2b427
Radu Sabau <radu.sabau@analog.com>
2023-10-12 17:15:08 +0300

    projects: max14906 : Add initial project files for MAX14906
    Add initial header and source files for MAX14906 project.
    
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

---
+++ projects/max14906/src/examples/iio_example/iio_example.c
@@ -0,0 +1,113 @@
+/***************************************************************************//**
+ *   @file   max14906/src/examples/iio_example/iio_example.c
+ *   @brief  Source file for iio example.
+ *   @author Radu Sabau (radu.sabau@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#include "iio_example.h"
+#include "iio_max14906.h"
+#include "common_data.h"
+#include "no_os_print_log.h"
+#include "iio_app.h"
+
+int iio_example_main()
+{
+	int ret;
+
+	/** IIO descriptor and initialization parameter. */
+	struct max14906_iio_desc *max14906_iio_desc;
+
+	struct max14906_iio_desc_init_param max14906_iio_ip = {
+		.max14906_init_param = &max14906_ip,
+		/** Channel configuration to be used in the example. */
+		.channel_configs = {
+			{
+				.enabled = true,
+				.function = MAX14906_OUT,
+			},
+			{
+				.enabled = true,
+				.function = MAX14906_IN,
+			},
+			{
+				.enabled = true,
+				.function = MAX14906_OUT,
+			},
+			{
+				.enabled = true,
+				.function = MAX14906_IN,
+			},
+		},
+	};
+
+	/** IIO app. */
+	struct iio_app_desc *app;
+	struct iio_app_init_param app_init_param = { 0 };
+
+	ret = max14906_iio_init(&max14906_iio_desc, &max14906_iio_ip);
+	if(ret)
+		goto exit;
+
+	/** Declaring iio_devices structure */
+	struct iio_app_device iio_devices[] = {
+		{
+			.name = "max14906",
+			.dev = max14906_iio_desc,
+			.dev_descriptor = max14906_iio_desc->iio_dev,
+		}
+	};
+
+	/** Initializing IIO app init param. */
+	app_init_param.devices = iio_devices;
+	app_init_param.nb_devices = NO_OS_ARRAY_SIZE(iio_devices);
+	app_init_param.uart_init_params = max14906_uart_ip;
+
+	/** Initializing IIO app. */
+	ret = iio_app_init(&app, app_init_param);
+	if(ret)
+		goto iio_max14906_remove;
+
+	/** Running the IIO app (use iio_info or osc in terminal). */
+	ret = iio_app_run(app);
+
+	iio_app_remove(app);
+
+iio_max14906_remove:
+	max14906_iio_remove(max14906_iio_desc);
+exit:
+	if (ret)
+		pr_info("Error!\n");
+	return ret;
+}
