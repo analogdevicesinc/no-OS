4b59a5d5561c4e829ff32bfa352645dbe76325dd
Radu Sabau <radu.sabau@analog.com>
2023-10-12 18:45:50 +0300

    projects: max14916 : Add project files for MAX14916
    Add initial header and source files for MAX14916
    
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

---
+++ projects/max14916/src/platform/maxim/platform_src.mk
@@ -0,0 +1,11 @@
+INCS += $(PLATFORM_DRIVERS)/maxim_spi.h		\
+	$(PLATFORM_DRIVERS)/maxim_uart.h	\
+	$(PLATFORM_DRIVERS)/maxim_uart_stdio.h	\
+	$(PLATFORM_DRIVERS)/maxim_irq.h
+
+
+SRCS += $(PLATFORM_DRIVERS)/maxim_spi.c		\
+	$(PLATFORM_DRIVERS)/maxim_uart.c	\
+	$(PLATFORM_DRIVERS)/maxim_uart_stdio.c	\
+	$(PLATFORM_DRIVERS)/maxim_irq.c		\
+	$(PLATFORM_DRIVERS)/maxim_delay.c	
