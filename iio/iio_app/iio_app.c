549e0901001765836a62de6e87855538ca9d5146
Darius Berghe <darius.berghe@analog.com>
2023-10-25 15:19:24 +0300

    asynchrounous_rx = false in iio_app

--- iio/iio_app/iio_app.c
+++ iio/iio_app/iio_app.c
@@ -246,7 +246,7 @@ static int32_t uart_setup(struct no_os_uart_desc **uart_desc,
 		/* TODO: remove this ifdef when asynchrounous rx is implemented on every platform. */
 #if defined(STM32_PLATFORM) || defined(MAXIM_PLATFORM) || defined(ADUCM_PLATFORM) || defined(PICO_PLATFORM)
 		.irq_id = uart_init_par->irq_id,
-		.asynchronous_rx = true,
+		.asynchronous_rx = false,
 #endif
 		.baud_rate = UART_BAUDRATE_DEFAULT,
 		.size = NO_OS_UART_CS_8,
