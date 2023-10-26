analogdevicesinc/no-OS

commit 8c5e44f16bf9fdb1c9ce5aee0cf8256dc7f23d75

ci: use all cpu cores
Add NUM_JOBS variable holding the available number of cpu cores on a
build agent.

Use all avaliable cores for all ci builds.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

ci/astyle.sh
@@ -96,7 +96,7 @@ build_astyle() {
 		wget --no-check-certificate "https://sourceforge.net/projects/astyle/files/astyle/astyle 3.1/astyle_3.1_linux.tar.gz"
 		tar -xzf astyle_3.1_linux.tar.gz
 		pushd ./astyle/build/gcc
-		make -j3
+		make -j${NUM_JOBS}
 		popd
 		popd
 	fi
@@ -106,7 +106,7 @@ run_astyle() {
 	git diff --name-only --diff-filter=d $COMMIT_RANGE | while read -r file; do
 		if is_source_file "$file" && is_valid_file "$file"
 		then
-			./build/astyle/build/gcc/bin/astyle --options="$(get_script_path astyle_config)" "$file"
+			./build/astyle/build/gcc/bin/astyle -j${NUM_JOBS} --options="$(get_script_path astyle_config)" "$file"
 		fi
 	done;
 

ci/cppcheck.sh
@@ -54,7 +54,7 @@ parse_cppcheck_options() {
 }
 
 run_cppcheck() {
-	cppcheck --quiet --force --error-exitcode=1 $CPPCHECK_OPTIONS .
+	cppcheck -j${NUM_JOBS} --quiet --force --error-exitcode=1 $CPPCHECK_OPTIONS .
 }
 
 build_cppheck

ci/documentation.sh
@@ -37,7 +37,7 @@
 ############################################################################
 build_doxygen() {
         pushd ${TOP_DIR}/doc/doxygen
-        (cd build && ! make doc 2>&1 | grep -E "warning:|error:") || {
+        (cd build && ! make -j${NUM_JOBS} doc 2>&1 | grep -E "warning:|error:") || {
                 echo_red "Documentation incomplete or errors in the generation of it have occured!"
                 exit 1
         }
@@ -49,7 +49,7 @@ build_doxygen() {
 build_sphinx() {
         pushd ${TOP_DIR}/doc/sphinx
 
-        sphinx-build -W ./source/ ./build
+        sphinx-build -j${NUM_JOBS} -W ./source/ ./build
 
         popd
 }

ci/run_build.sh
@@ -39,6 +39,7 @@ sudo apt-get update
 
 TOP_DIR="$(pwd)"
 DEPS_DIR="${TOP_DIR}/deps"
+NUM_JOBS=$(nproc)
 
 . ./ci/lib.sh
 
@@ -53,7 +54,7 @@ build_cppcheck() {
 build_drivers() {
     sudo apt-get install gcc-arm-none-eabi libnewlib-arm-none-eabi
     git submodule update --init libraries/iio/libtinyiiod
-    make -C ./drivers -f Makefile -j
+    make -j${NUM_JOBS} -C ./drivers -f Makefile
 }
 
 build_documentation() {

commit 2bffb4e49cae1767a4de9b415e115b8361a4642f

ci: drop -j parameter for the astyle command
Astyle command does not support -j<nr_of_threads> option.

The -j option is actually a shortcut for `--add-braces` option for
astyle tool.

Threrefore drop it.

Fixes: 8c5e44f ("ci: use all cpu cores")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

ci/astyle.sh
@@ -106,7 +106,7 @@ run_astyle() {
 	git diff --name-only --diff-filter=d $COMMIT_RANGE | while read -r file; do
 		if is_source_file "$file" && is_valid_file "$file"
 		then
-			./build/astyle/build/gcc/bin/astyle -j${NUM_JOBS} --options="$(get_script_path astyle_config)" "$file"
+			./build/astyle/build/gcc/bin/astyle --options="$(get_script_path astyle_config)" "$file"
 		fi
 	done;
 