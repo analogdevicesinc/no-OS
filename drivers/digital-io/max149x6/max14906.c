analogdevicesinc/no-OS

commit d7b9c6856351ae3004d61b74b80794c342af19b4

drivers: digital-io: max149x6 : Add GPIOs for FAULT, READY and SYNCH
Add GPIO init_param and desc for FAULT, READY and SYNCH pins
for both MAX14906 and MAX14916.
Initialize them properly in the max14906_init and max14916 init.

Signed-off-by: Radu Sabau <radu.sabau@analog.com>

drivers/digital-io/max149x6/max14906.c
@@ -199,6 +199,40 @@ int max14906_init(struct max149x6_desc **desc,
 			goto gpio_err;
 	}
 
+	ret = no_os_gpio_get_optional(&descriptor->fault_gpio,
+				      param->fault_gpio_param);
+	if (ret)
+		goto spi_err;
+
+	if (descriptor->fault_gpio) {
+		ret = no_os_gpio_direction_input(descriptor->fault_gpio);
+		if (ret)
+			goto gpio_err;
+	}
+
+	ret = no_os_gpio_get_optional(&descriptor->ready_gpio,
+				      param->ready_gpio_param);
+	if (ret)
+		goto spi_err;
+
+	if (descriptor->ready_gpio) {
+		ret = no_os_gpio_direction_input(descriptor->ready_gpio);
+		if (ret)
+			goto gpio_err;
+	}
+
+	ret = no_os_gpio_get_optional(&descriptor->synch_gpio,
+				      param->synch_gpio_param);
+	if (ret)
+		goto spi_err;
+
+	if (descriptor->synch_gpio) {
+		ret = no_os_gpio_direction_output(descriptor->synch_gpio,
+						  NO_OS_GPIO_HIGH);
+		if (ret)
+			goto gpio_err;
+	}
+
 	/* Clear the latched faults generated at power up */
 	ret = max149x6_reg_read(descriptor, MAX14906_OVR_LD_REG, &reg_val);
 	if (ret)

drivers/digital-io/max149x6/max14916.c
@@ -235,6 +235,40 @@ int max14916_init(struct max149x6_desc **desc,
 			goto gpio_err;
 	}
 
+	ret = no_os_gpio_get_optional(&descriptor->fault_gpio,
+				      param->fault_gpio_param);
+	if (ret)
+		goto spi_err;
+
+	if (descriptor->fault_gpio) {
+		ret = no_os_gpio_direction_input(descriptor->fault_gpio);
+		if (ret)
+			goto gpio_err;
+	}
+
+	ret = no_os_gpio_get_optional(&descriptor->ready_gpio,
+				      param->ready_gpio_param);
+	if (ret)
+		goto spi_err;
+
+	if (descriptor->ready_gpio) {
+		ret = no_os_gpio_direction_input(descriptor->ready_gpio);
+		if (ret)
+			goto gpio_err;
+	}
+
+	ret = no_os_gpio_get_optional(&descriptor->synch_gpio,
+				      param->synch_gpio_param);
+	if (ret)
+		goto spi_err;
+
+	if (descriptor->synch_gpio) {
+		ret = no_os_gpio_direction_output(descriptor->synch_gpio,
+						  NO_OS_GPIO_HIGH);
+		if (ret)
+			goto gpio_err;
+	}
+
 	/* Clear the latched faults generated at power up */
 	ret = max149x6_reg_read(descriptor, MAX14916_OVR_LD_REG, &reg_val);
 	if (ret)

drivers/digital-io/max149x6/max149x6-base.h
@@ -61,6 +61,9 @@ struct max149x6_init_param {
 	uint32_t chip_address;
 	struct no_os_spi_init_param *comm_param;
 	struct no_os_gpio_init_param *en_gpio_param;
+	struct no_os_gpio_init_param *fault_gpio_param;
+	struct no_os_gpio_init_param *ready_gpio_param;
+	struct no_os_gpio_init_param *synch_gpio_param;
 	bool crc_en;
 };
 
@@ -71,6 +74,9 @@ struct max149x6_desc {
 	uint32_t chip_address;
 	struct no_os_spi_desc *comm_desc;
 	struct no_os_gpio_desc *en_gpio;
+	struct no_os_gpio_desc *fault_gpio;
+	struct no_os_gpio_desc *ready_gpio;
+	struct no_os_gpio_desc *synch_gpio;
 	uint8_t buff[MAX149X6_FRAME_SIZE + 1];
 	bool crc_en;
 };

commit dc132b22eb2dcec9f8e544c3a268a1aeea6ae227

drivers: digital-io: max149x6 : Fix max14906 remove function
Fix max14906 remove function to remove all GPIO's allocated at init.

Fixes: d7b9c68 ("drivers: digital-io: max149x6 : Add GPIOs for FAULT, READY and SYNCH")
Signed-off-by: Radu Sabau <radu.sabau@analog.com>

drivers/digital-io/max149x6/max14906.c
@@ -202,64 +202,70 @@ int max14906_init(struct max149x6_desc **desc,
 	ret = no_os_gpio_get_optional(&descriptor->fault_gpio,
 				      param->fault_gpio_param);
 	if (ret)
-		goto spi_err;
+		goto gpio_err;
 
 	if (descriptor->fault_gpio) {
 		ret = no_os_gpio_direction_input(descriptor->fault_gpio);
 		if (ret)
-			goto gpio_err;
+			goto fault_gpio_err;
 	}
 
 	ret = no_os_gpio_get_optional(&descriptor->ready_gpio,
 				      param->ready_gpio_param);
 	if (ret)
-		goto spi_err;
+		goto fault_gpio_err;
 
 	if (descriptor->ready_gpio) {
 		ret = no_os_gpio_direction_input(descriptor->ready_gpio);
 		if (ret)
-			goto gpio_err;
+			goto ready_gpio_err;
 	}
 
 	ret = no_os_gpio_get_optional(&descriptor->synch_gpio,
 				      param->synch_gpio_param);
 	if (ret)
-		goto spi_err;
+		goto ready_gpio_err;
 
 	if (descriptor->synch_gpio) {
 		ret = no_os_gpio_direction_output(descriptor->synch_gpio,
 						  NO_OS_GPIO_HIGH);
 		if (ret)
-			goto gpio_err;
+			goto synch_gpio_err;
 	}
 
 	/* Clear the latched faults generated at power up */
 	ret = max149x6_reg_read(descriptor, MAX14906_OVR_LD_REG, &reg_val);
 	if (ret)
-		goto gpio_err;
+		goto synch_gpio_err;
 
 	ret = max149x6_reg_read(descriptor, MAX14906_OPN_WIR_FLT_REG, &reg_val);
 	if (ret)
-		goto gpio_err;
+		goto synch_gpio_err;
 
 	ret = max149x6_reg_read(descriptor, MAX14906_SHD_VDD_FLT_REG, &reg_val);
 	if (ret)
-		goto gpio_err;
+		goto synch_gpio_err;
 
 	ret = max149x6_reg_read(descriptor, MAX14906_GLOBAL_FLT_REG, &reg_val);
 	if (ret)
-		goto gpio_err;
+		goto synch_gpio_err;
 
 	for (i = 0; i < MAX14906_CHANNELS; i++) {
 		ret = max14906_ch_func(descriptor, i, MAX14906_HIGH_Z);
 		if (ret)
-			goto gpio_err;
+			goto synch_gpio_err;
 	}
 
 	*desc = descriptor;
 
 	return 0;
 
+synch_gpio_err:
+	no_os_gpio_remove(descriptor->synch_gpio);
+ready_gpio_err:
+	no_os_gpio_remove(descriptor->ready_gpio);
+fault_gpio_err:
+	no_os_gpio_remove(descriptor->fault_gpio);
 gpio_err:
 	no_os_gpio_remove(descriptor->en_gpio);
 spi_err:
@@ -290,9 +296,10 @@ int max14906_remove(struct max149x6_desc *desc)
 	}
 
 	no_os_spi_remove(desc->comm_desc);
-
-	if (desc->en_gpio)
-		no_os_gpio_remove(desc->en_gpio);
+	no_os_gpio_remove(desc->en_gpio);
+	no_os_gpio_remove(desc->fault_gpio);
+	no_os_gpio_remove(desc->ready_gpio);
+	no_os_gpio_remove(desc->synch_gpio);
 
 	no_os_free(desc);
 