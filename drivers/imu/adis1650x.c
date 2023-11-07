analogdevicesinc/no-OS

commit 9650f8de4517587e622ba5ac90764ff3bd5329e5

drivers: imu: Rewrite scale values for adis chips
Rewrite scale values for adis1650x and adis1657x.

Signed-off-by: Ramona Gradinariu <ramona.gradinariu@analog.com>

drivers/imu/adis1650x.c
@@ -138,23 +138,23 @@ static const struct adis_clk_freq_limit adis1650x_sampling_clk_limits = {
 
 /* Values from datasheet for 32-bit data */
 static const struct adis_scale_fractional adis1650x_anglvel_scale[] = {
-	[ADIS16500]   = {1, RAD_TO_DEGREE(655360)},
-	[ADIS16505_1] = {1, RAD_TO_DEGREE(10485760)},
-	[ADIS16505_2] = {1, RAD_TO_DEGREE(2621440)},
-	[ADIS16505_3] = {1, RAD_TO_DEGREE(655360)},
-	[ADIS16507_1] = {1, RAD_TO_DEGREE(10485760)},
-	[ADIS16507_2] = {1, RAD_TO_DEGREE(2621440)},
-	[ADIS16507_3] = {1, RAD_TO_DEGREE(655360)},
+	[ADIS16500]   = {1, RAD_TO_DEGREE(10 << 16)},
+	[ADIS16505_1] = {1, RAD_TO_DEGREE(160 << 16)},
+	[ADIS16505_2] = {1, RAD_TO_DEGREE(40 << 16)},
+	[ADIS16505_3] = {1, RAD_TO_DEGREE(10 << 16)},
+	[ADIS16507_1] = {1, RAD_TO_DEGREE(160 << 16)},
+	[ADIS16507_2] = {1, RAD_TO_DEGREE(40 << 16)},
+	[ADIS16507_3] = {1, RAD_TO_DEGREE(10 << 16)},
 };
 
 static const struct adis_scale_fractional adis1650x_accl_scale[] = {
-	[ADIS16500]   = {1, 5349877},
-	[ADIS16505_1] = {1, 26783550},
-	[ADIS16505_2] = {1, 26783550},
-	[ADIS16505_3] = {1, 26783550},
-	[ADIS16507_1] = {1, 5349877},
-	[ADIS16507_2] = {1, 5349877},
-	[ADIS16507_3] = {1, 5349877},
+	[ADIS16500]   = {1, M_S_2_TO_G(800 << 16)},
+	[ADIS16505_1] = {78, 32000 << 16},
+	[ADIS16505_2] = {78, 32000 << 16},
+	[ADIS16505_3] = {78, 32000 << 16},
+	[ADIS16507_1] = {1, M_S_2_TO_G(800 << 16)},
+	[ADIS16507_2] = {1, M_S_2_TO_G(800 << 16)},
+	[ADIS16507_3] = {1, M_S_2_TO_G(800 << 16)},
 };
 
 static const struct adis_scale_fractional_log2 adis1650x_deltaangl_scale[] = {

drivers/imu/adis1657x.c
@@ -161,24 +161,21 @@ static const struct adis_clk_freq_limit adis1657x_sampling_clk_limits = {
 
 /* Values from datasheet for 32-bit data */
 static const struct adis_scale_fractional adis1657x_anglvel_scale[] = {
-	[ADIS16575_2] = {1, RAD_TO_DEGREE(2621440)},
-	[ADIS16575_3] = {1, RAD_TO_DEGREE(655360)},
-	[ADIS16576_2] = {1, RAD_TO_DEGREE(2621440)},
-	[ADIS16576_3] = {1, RAD_TO_DEGREE(655360)},
-	[ADIS16577_2] = {1, RAD_TO_DEGREE(2621440)},
-	[ADIS16577_3] = {1, RAD_TO_DEGREE(655360)},
+	[ADIS16575_2] = {1, RAD_TO_DEGREE(40 << 16)},
+	[ADIS16575_3] = {1, RAD_TO_DEGREE(10 << 16)},
+	[ADIS16576_2] = {1, RAD_TO_DEGREE(40 << 16)},
+	[ADIS16576_3] = {1, RAD_TO_DEGREE(10 << 16)},
+	[ADIS16577_2] = {1, RAD_TO_DEGREE(40 << 16)},
+	[ADIS16577_3] = {1, RAD_TO_DEGREE(10 << 16)},
 };
 
 static const struct adis_scale_fractional adis1657x_accl_scale[] = {
-	/* datasheet scale is in LSB/g = 262144000, need to convert to m/s^2 262144000/9.80665 = 26731249 */
-	[ADIS16575_2] = {1, 26731249},
-	[ADIS16575_3] = {1, 26731249},
-	/* datasheet scale is in LSB/g = 52428800, need to convert to m/s^2 52428800/9.80665 = 5346250 */
-	[ADIS16576_2] = {1, 5346250},
-	[ADIS16576_3] = {1, 5346250},
-	/* datasheet scale is in LSB/g = 52428800, need to convert to m/s^2 52428800/9.80665 = 5346250 */
-	[ADIS16577_2] = {1, 5346250},
-	[ADIS16577_3] = {1, 5346250},
+	[ADIS16575_2] = {1, M_S_2_TO_G(4000 << 16)},
+	[ADIS16575_3] = {1, M_S_2_TO_G(4000 << 16)},
+	[ADIS16576_2] = {1, M_S_2_TO_G(800 << 16)},
+	[ADIS16576_3] = {1, M_S_2_TO_G(800 << 16)},
+	[ADIS16577_2] = {1, M_S_2_TO_G(800 << 16)},
+	[ADIS16577_3] = {1, M_S_2_TO_G(800 << 16)},
 };
 
 static const struct adis_scale_fractional_log2 adis1657x_deltaangl_scale[] = {

commit e31ebfd9bf440ecb8573c9d2e38b4d2ba199306c

driver: imu: adis1650x: Fix accel scale
Fix accel scale in adis1650x driver for adis16500 and
adis16507 devices.

Fixes: 9650f8d ("drivers: imu: Rewrite scale values for adis chips")
Signed-off-by: Ramona Gradinariu <ramona.gradinariu@analog.com>

drivers/imu/adis1650x.c
@@ -149,14 +149,14 @@ static const struct adis_scale_fractional adis1650x_anglvel_scale[] = {
 };
 
 static const struct adis_scale_fractional adis1650x_accl_scale[] = {
-	[ADIS16500]   = {1, M_S_2_TO_G(800 << 16)},
+	[ADIS16500]   = {392, 32000 << 16},
 	[ADIS16501]   = {1, M_S_2_TO_G(800 << 16)},
 	[ADIS16505_1] = {78, 32000 << 16},
 	[ADIS16505_2] = {78, 32000 << 16},
 	[ADIS16505_3] = {78, 32000 << 16},
-	[ADIS16507_1] = {1, M_S_2_TO_G(800 << 16)},
-	[ADIS16507_2] = {1, M_S_2_TO_G(800 << 16)},
-	[ADIS16507_3] = {1, M_S_2_TO_G(800 << 16)},
+	[ADIS16507_1] = {392, 32000 << 16},
+	[ADIS16507_2] = {392, 32000 << 16},
+	[ADIS16507_3] = {392, 32000 << 16},
 };
 
 static const struct adis_scale_fractional_log2 adis1650x_deltaangl_scale[] = {