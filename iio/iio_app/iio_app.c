analogdevicesinc/no-OS

commit 9998beb55ccc1cd9f448cd976f4de948b691de86

iio_app: Don't init UART when using lwip network
There is no need to initialize an UART descriptor when using the lwip
network backend, since UART is not used by iio. Also, the hello message
shouldn't be printed.

Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

iio/iio_app/iio_app.c
@@ -115,7 +115,7 @@ static int32_t iio_print_uart_info_message(struct no_os_uart_desc **uart_desc,
 static int32_t print_uart_hello_message(struct no_os_uart_desc **uart_desc,
 					struct no_os_uart_init_param *user_uart_params)
 {
-#if defined(LINUX_PLATFORM) || defined(NO_OS_NETWORKING)
+#if defined(LINUX_PLATFORM) || defined(NO_OS_NETWORKING) || defined(NO_OS_LWIP_NETWORKING)
 	return 0;
 #else
 	const char *uart_data_size[] = { "5", "6", "7", "8", "9" };
@@ -150,7 +150,7 @@ static int32_t print_uart_error_message(struct no_os_uart_desc **uart_desc,
 	uint32_t msglen = sprintf(message,
 				  "TinyIIOD server failed with code %d.\n",
 				  (int)status);
-#if defined(LINUX_PLATFORM) || defined(NO_OS_NETWORKING)
+#if defined(LINUX_PLATFORM) || defined(NO_OS_NETWORKING) || defined(NO_OS_LWIP_NETWORKING)
 	(void)msglen;
 	printf("%s", message);
 	return 0;
@@ -237,7 +237,7 @@ static int32_t network_setup(struct iio_init_param *iio_init_param,
 static int32_t uart_setup(struct no_os_uart_desc **uart_desc,
 			  struct no_os_uart_init_param *uart_init_par)
 {
-#ifdef LINUX_PLATFORM
+#if defined(LINUX_PLATFORM) || defined(NO_OS_LWIP_NETWORKING)
 	*uart_desc = NULL;
 	return 0;
 #endif

commit f770cbd08cfd59dd65bdd22f787bc8fad5234de6

iio: iio_app: Check if uart_desc is allocated
Only free the uart decriptor if it was previously allocated in the init
function. Otherwise, this causes the remove to fail.

Fixes: 9998beb("iio_app: Don't init UART when using lwip network")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

iio/iio_app/iio_app.c
@@ -461,9 +461,11 @@ int iio_app_remove(struct iio_app_desc *app)
 		return ret;
 #endif
 
-	ret = no_os_uart_remove(app->uart_desc);
-	if (ret)
-		return ret;
+	if (app->uart_desc) {
+		ret = no_os_uart_remove(app->uart_desc);
+		if (ret)
+			return ret;
+	}
 
 	ret = iio_remove(app->iio_desc);
 	if (ret)