analogdevicesinc/no-OS

commit 2b0d85de3b93f02b63397ed61493d9e2a6cf4967

drivers:axi_core:jesd204:axi_adxcvr.* Add FSM ops
Add JESD204 FSM ops for axi_adxcvr.c and axi_adxcvr.h.

Signed-off-by: George Mois <george.mois@analog.com>

drivers/axi_core/jesd204/axi_adxcvr.c
@@ -162,10 +162,10 @@ int32_t adxcvr_drp_wait_idle(struct adxcvr *xcvr,
  * @param val - Data read.
  * @return Returns 0 in case of success or negative error code otherwise.
  */
-int32_t adxcvr_drp_read(struct adxcvr *xcvr,
-			uint32_t drp_port,
-			uint32_t reg,
-			uint32_t *val)
+int adxcvr_drp_read(struct adxcvr *xcvr,
+		    unsigned int drp_port,
+		    unsigned int reg,
+		    unsigned int *val)
 {
 	uint32_t drp_sel, drp_addr;
 	int32_t ret;
@@ -197,10 +197,10 @@ int32_t adxcvr_drp_read(struct adxcvr *xcvr,
  * @param val - Data to be written.
  * @return Returns 0 in case of success or negative error code otherwise.
  */
-int32_t adxcvr_drp_write(struct adxcvr *xcvr,
-			 uint32_t drp_port,
-			 uint32_t reg,
-			 uint32_t val)
+int adxcvr_drp_write(struct adxcvr *xcvr,
+		     unsigned int drp_port,
+		     unsigned int reg,
+		     unsigned int val)
 {
 	uint32_t drp_sel, drp_addr;
 	int32_t ret;
@@ -223,6 +223,101 @@ int32_t adxcvr_drp_write(struct adxcvr *xcvr,
 	return 0;
 }
 
+static const struct xilinx_xcvr_drp_ops adxcvr_drp_ops = {
+	.read = adxcvr_drp_read,
+	.write = adxcvr_drp_write,
+};
+
+static unsigned long adxcvr_clk_recalc_rate(struct adxcvr *xcvr,
+		unsigned long parent_rate)
+{
+	unsigned int *rx_out_div;
+	unsigned int *tx_out_div;
+	unsigned int out_div;
+
+	pr_debug("%s: Parent Rate %lu Hz", __func__, parent_rate);
+
+	if (xcvr->tx_enable) {
+		rx_out_div = NULL;
+		tx_out_div = &out_div;
+	} else {
+		rx_out_div = &out_div;
+		tx_out_div = NULL;
+	}
+
+	xilinx_xcvr_read_out_div(&xcvr->xlx_xcvr, ADXCVR_DRP_PORT_CHANNEL(0),
+				 rx_out_div, tx_out_div);
+
+	if (xcvr->cpll_enable) {
+		struct xilinx_xcvr_cpll_config cpll_conf;
+
+		xilinx_xcvr_cpll_read_config(&xcvr->xlx_xcvr, ADXCVR_DRP_PORT_CHANNEL(0),
+					     &cpll_conf);
+		return xilinx_xcvr_cpll_calc_lane_rate(&xcvr->xlx_xcvr, parent_rate,
+						       &cpll_conf, out_div);
+
+	} else {
+		struct xilinx_xcvr_qpll_config qpll_conf;
+
+		if (!xcvr->qpll_enable)
+			return xcvr->lane_rate_khz;
+
+		xilinx_xcvr_qpll_read_config(&xcvr->xlx_xcvr, xcvr->sys_clk_sel,
+					     ADXCVR_DRP_PORT_COMMON(0), &qpll_conf);
+		return xilinx_xcvr_qpll_calc_lane_rate(&xcvr->xlx_xcvr,
+						       xcvr->sys_clk_sel, &qpll_conf, out_div);
+	}
+}
+
+static long adxcvr_clk_round_rate(struct adxcvr *xcvr,
+				  unsigned long rate,
+				  unsigned long *prate)
+{
+	int ret;
+
+	if (xcvr->ref_rate_khz % 40 == 0)
+		*prate = rate * (1000 / 40);
+
+	pr_debug("%s: Rate %lu kHz Parent Rate %lu Hz",
+		 __func__, rate, *prate);
+
+	/* Just check if we can support the requested rate */
+	if (xcvr->cpll_enable)
+		ret = xilinx_xcvr_calc_cpll_config(&xcvr->xlx_xcvr, *prate, rate,
+						   NULL, NULL);
+	else
+		ret = xilinx_xcvr_calc_qpll_config(&xcvr->xlx_xcvr,
+						   xcvr->sys_clk_sel, *prate, rate,	NULL, NULL);
+
+	return ret < 0 ? ret : rate;
+}
+
+static const struct clk_ops clkout_ops = {
+	.recalc_rate = adxcvr_clk_recalc_rate,
+	.enable = adxcvr_clk_enable,
+	.disable = adxcvr_clk_disable,
+	.round_rate = adxcvr_clk_round_rate,
+	.set_rate = adxcvr_clk_set_rate,
+};
+
+static unsigned long adxcvr_qpll_recalc_rate(struct adxcvr *xcvr,
+		unsigned long parent_rate)
+{
+	struct xilinx_xcvr_qpll_config qpll_conf;
+
+	pr_debug("%s: Parent Rate %lu Hz", __func__, parent_rate);
+
+	xilinx_xcvr_qpll_read_config(&xcvr->xlx_xcvr,
+				     ADXCVR_DRP_PORT_COMMON(0), xcvr->sys_clk_sel, &qpll_conf);
+
+	return xilinx_xcvr_qpll_calc_lane_rate(&xcvr->xlx_xcvr,
+					       parent_rate, &qpll_conf, 1);
+}
+
+static const struct clk_ops qpll_ops = {
+	.recalc_rate = adxcvr_qpll_recalc_rate,
+};
+
 /**
  * @brief AXI ADXCVR Clock Set Rate
  * @param xcvr - The device structure.
@@ -231,9 +326,8 @@ int32_t adxcvr_drp_write(struct adxcvr *xcvr,
  * @return Returns 0 in case of success or negative error code otherwise.
  */
 int adxcvr_clk_set_rate(struct adxcvr *xcvr,
-			uint32_t rate,
-			uint32_t parent_rate
-		       )
+			unsigned long rate,
+			unsigned long parent_rate)
 {
 	struct xilinx_xcvr_cpll_config cpll_conf;
 	struct xilinx_xcvr_qpll_config qpll_conf;
@@ -401,7 +495,7 @@ static int adxcvr_reset(struct adxcvr *xcvr)
  * @param xcvr - The device structure.
  * @return Returns 0 in case of success or negative error code otherwise.
  */
-int32_t adxcvr_clk_enable(struct adxcvr *xcvr)
+int adxcvr_clk_enable(struct adxcvr *xcvr)
 {
 	int ret, retry = 10;
 	unsigned int status;
@@ -451,7 +545,7 @@ int32_t adxcvr_clk_enable(struct adxcvr *xcvr)
  * @param xcvr - The device structure.
  * @return Returns 0 in case of success or negative error code otherwise.
  */
-int32_t adxcvr_clk_disable(struct adxcvr *xcvr)
+int adxcvr_clk_disable(struct adxcvr *xcvr)
 {
 	pr_debug("%s: %s", __func__, xcvr->tx_enable ? "TX" : "RX");
 

drivers/axi_core/jesd204/axi_adxcvr.h
@@ -127,21 +127,21 @@ struct adxcvr_init {
 /************************ Functions Declarations ******************************/
 /******************************************************************************/
 /** AXI ADXCVR DPR Port Read */
-int32_t adxcvr_drp_read(struct adxcvr *xcvr,
-			uint32_t drp_port,
-			uint32_t reg,
-			uint32_t *val);
+int adxcvr_drp_read(struct adxcvr *xcvr,
+		    unsigned int drp_port,
+		    unsigned int reg,
+		    unsigned int *val);
 /** AXI ADXCVR DPR Port Write */
-int32_t adxcvr_drp_write(struct adxcvr *xcvr,
-			 uint32_t drp_port,
-			 uint32_t reg,
-			 uint32_t val);
+int adxcvr_drp_write(struct adxcvr *xcvr,
+		     unsigned int drp_port,
+		     unsigned int reg,
+		     unsigned int val);
 /** AXI ADXCVRS Status Read */
 int32_t adxcvr_status_error(struct adxcvr *xcvr);
 /** AXI ADXCVR Clock Enable */
-int32_t adxcvr_clk_enable(struct adxcvr *xcvr);
+int adxcvr_clk_enable(struct adxcvr *xcvr);
 /** AXI ADXCVR Clock Disable */
-int32_t adxcvr_clk_disable(struct adxcvr *xcvr);
+int adxcvr_clk_disable(struct adxcvr *xcvr);
 /** AXI ADXCVR Device Initialization */
 int32_t adxcvr_init(struct adxcvr **ad_xcvr,
 		    const struct adxcvr_init *init);
@@ -149,8 +149,8 @@ int32_t adxcvr_init(struct adxcvr **ad_xcvr,
 int32_t adxcvr_remove(struct adxcvr *xcvr);
 /** AXI ADXCVR Clock Set Rate */
 int adxcvr_clk_set_rate(struct adxcvr *xcvr,
-			uint32_t rate,
-			uint32_t parent_rate);
+			unsigned long rate,
+			unsigned long parent_rate);
 /** AXI ADXCVR Write */
 int32_t adxcvr_write(struct adxcvr *xcvr, uint32_t reg_addr, uint32_t reg_val);
 /** AXI ADXCVR Read */

drivers/axi_core/jesd204/xilinx_transceiver.h
@@ -188,6 +188,66 @@ struct xilinx_xcvr {
 	uint32_t vco1_max; // kHz
 };
 
+struct xilinx_xcvr_drp_ops {
+	int (*write)(struct adxcvr *xcvr, unsigned int drp_port,
+		     unsigned int reg, unsigned int val);
+	int (*read)(struct adxcvr *xcvr, unsigned int drp_port,
+		    unsigned int reg, unsigned int *val);
+};
+
+/**
+ * struct clk_ops -  Callback operations for hardware clocks; these are to
+ * be provided by the clock implementation, and will be called by drivers
+ * through the clk_* api.
+ *
+ * @param enable:	Enable the clock atomically. This must not return until the
+ *		clock is generating a valid clock signal, usable by consumer
+ *		devices. Called with enable_lock held. This function must not
+ *		sleep.
+ *
+ * @param disable:	Disable the clock atomically. Called with enable_lock held.
+ *		This function must not sleep.
+ *
+ * @param recalc_rate	Recalculate the rate of this clock, by querying hardware. The
+ *		parent rate is an input parameter.  It is up to the caller to
+ *		ensure that the prepare_mutex is held across this call.
+ *		Returns the calculated rate.  Optional, but recommended - if
+ *		this op is not set then clock rate will be initialized to 0.
+ *
+ * @param round_rate:	Given a target rate as input, returns the closest rate actually
+ *		supported by the clock. The parent rate is an input/output
+ *		parameter.
+ *
+ * @param set_rate:	Change the rate of this clock. The requested rate is specified
+ *		by the second argument, which should typically be the return
+ *		of .round_rate call.  The third argument gives the parent rate
+ *		which is likely helpful for most .set_rate implementation.
+ *		Returns 0 on success, -EERROR otherwise.
+ *
+ * The clk_enable/clk_disable and clk_prepare/clk_unprepare pairs allow
+ * implementations to split any work between atomic (enable) and sleepable
+ * (prepare) contexts.  If enabling a clock requires code that might sleep,
+ * this must be done in clk_prepare.  Clock enable code that will never be
+ * called in a sleepable context may be implemented in clk_enable.
+ *
+ * Typically, drivers will call clk_prepare when a clock may be needed later
+ * (eg. when a device is opened), and clk_enable when the clock is actually
+ * required (eg. from an interrupt). Note that clk_prepare MUST have been
+ * called before clk_enable.
+ */
+struct clk_ops {
+	int (*enable)(struct adxcvr *xcvr);
+	int (*disable)(struct adxcvr *xcvr);
+	unsigned long (*recalc_rate)(struct adxcvr *xcvr,
+				     unsigned long parent_rate);
+	long (*round_rate)(struct adxcvr *xcvr,
+			   unsigned long parent_rate,
+			   unsigned long *prate);
+	int (*set_rate)(struct adxcvr *xcvr,
+			unsigned long rate,
+			unsigned long parent_rate);
+};
+
 /**
  * @struct xilinx_xcvr_cpll_config
  * @brief Structure holding CPLL configuration.

commit f05debd24c436f48c174474325714f290948930d

axi_core: jesd204: axi_adxcvr.c: Fix debug message
Fix debug message for the adxcvr_clk_round_rate function.

Fixes: 2b0d85d ("drivers:axi_core:jesd204:axi_adxcvr.* Add FSM ops")

Signed-off-by: George Mois <george.mois@analog.com>

drivers/axi_core/jesd204/axi_adxcvr.c
@@ -281,7 +281,7 @@ static long adxcvr_clk_round_rate(struct adxcvr *xcvr,
 		parent_rate = rate * (1000 / 40);
 
 	pr_debug("%s: Rate %lu kHz Parent Rate %lu Hz",
-		 __func__, rate, *prate);
+		 __func__, rate, parent_rate);
 
 	/* Just check if we can support the requested rate */
 	if (xcvr->cpll_enable)