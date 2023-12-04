analogdevicesinc/no-OS

commit ab393ac22c5ff651ba604dfad099724e5eb8efda

projects: adrv9025: Madura project
Add Madura sample project.

Remove Madura driver from drivers build check, cppcheck, and
astyle check.

Signed-off-by: George Mois <george.mois@analog.com>

projects/adrv902x/src/platform/xilinx/platform_src.mk
@@ -1,0 +1,23 @@
+SRCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
+	$(PLATFORM_DRIVERS)/xilinx_axi_io.c \
+	$(PLATFORM_DRIVERS)/xilinx_spi.c \
+	$(PLATFORM_DRIVERS)/xilinx_irq.c \
+	$(PLATFORM_DRIVERS)/xilinx_gpio.c \
+	$(PLATFORM_DRIVERS)/xilinx_delay.c
+	
+ifeq (y,$(strip $(TINYIIOD)))
+SRCS += $(PLATFORM_DRIVERS)/$xilinx_uart.c
+endif
+	
+INCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \
+	$(PLATFORM_DRIVERS)/xilinx_spi.h \
+	$(PLATFORM_DRIVERS)/xilinx_irq.h \
+	$(PLATFORM_DRIVERS)/xilinx_gpio.h
+	
+ifeq (y,$(strip $(TINYIIOD)))
+INCS += $(PLATFORM_DRIVERS)/xilinx_uart.h
+endif	
+	
+	
+	
+	

.astyleignore
@@ -10,5 +10,8 @@ drivers/adc/ad9081/api
 drivers/rf-transceiver/navassa/common/
 drivers/rf-transceiver/navassa/devices/
 drivers/rf-transceiver/navassa/third_party/
+drivers/rf-transceiver/madura/common/
+drivers/rf-transceiver/madura/devices/
+drivers/rf-transceiver/madura/platforms/
 drivers/adc/ad9083/ad9083_api
 legacy/

.cppcheckignore
@@ -16,6 +16,9 @@
 *:drivers/rf-transceiver/navassa/common/*
 *:drivers/rf-transceiver/navassa/devices/*
 *:drivers/rf-transceiver/navassa/third_party/*
+*:drivers/rf-transceiver/madura/common/*
+*:drivers/rf-transceiver/madura/devices/*
+*:drivers/rf-transceiver/madura/platforms/*
 *:drivers/adc/ad9083/ad9083_api
 *:libraries/fatfs/*
 *:libraries/mbedtls/*

ci/gen_dox.sh
@@ -33,7 +33,7 @@
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #List of excluded driver folders for the documentation generation.
-EXCLUDE_DRV="talise navassa ad9083_api ad5940"
+EXCLUDE_DRV="talise navassa ad9083_api ad5940 madura"
 
 #Generate drivers_page.dox and projects_page.dox
 

doc/doxygen/CMakeLists.txt
@@ -26,6 +26,7 @@ set(DOCUMENTED_FILES
 set(EXCLUDE_FILES
     "${SOURCES_DIR}/drivers/rf-transceiver/talise \\
     ${SOURCES_DIR}/drivers/rf-transceiver/navassa \\
+    ${SOURCES_DIR}/drivers/rf-transceiver/madura \\
     ${SOURCES_DIR}/drivers/adc/ad9081/api \\
     ${SOURCES_DIR}/drivers/adc/ad9083/ad9083_api \\
     ${SOURCES_DIR}/drivers/afe/ad5940 \\

drivers/Makefile
@@ -3,7 +3,8 @@ CC = arm-none-eabi-gcc
 SKIPDIR = -path ./platform -prune -o \
 	-path ./axi_core -prune -o \
 	-path ./rf-transceiver/navassa -prune -o \
-	-path ./adc/ad9081 -prune -o
+	-path ./adc/ad9081 -prune -o \
+	-path ./rf-transceiver/madura -prune -o
 
 INCLUDES = -I../include/ \
 	 -I../projects/drivers/util/ \

projects/adrv902x/Makefile
@@ -1,0 +1,13 @@
+# Select the example you want to enable by choosing y for enabling and n for disabling
+BASIC_EXAMPLE = n
+DMA_EXAMPLE = n
+IIO_EXAMPLE = y
+
+# Uncomment to use the desired platform
+PLATFORM = xilinx
+
+include ../../tools/scripts/generic_variables.mk
+
+include src.mk
+
+include ../../tools/scripts/generic.mk

projects/adrv902x/src.mk
@@ -1,0 +1,96 @@
+################################################################################
+#									       #
+#     Shared variables:							       #
+#	- PROJECT							       #
+#	- DRIVERS							       #
+#	- INCLUDE							       #
+#	- PLATFORM_DRIVERS						       #
+#	- NO-OS								       #
+#									       #
+################################################################################
+include $(PROJECT)/src/platform/$(PLATFORM)/platform_src.mk
+include $(PROJECT)/src/examples/examples_src.mk
+
+SRCS += $(PROJECT)/src/platform/$(PLATFORM)/main.c
+
+INCS += $(PROJECT)/src/common/common_data.h \
+	$(PROJECT)/src/common/app_config.h \
+	$(PROJECT)/src/common/ADRV9025_RxGainTable.h \
+	$(PROJECT)/src/common/ADRV9025_TxAttenTable.h \
+	$(PROJECT)/src/common/hal/no_os_platform.h \
+	$(PROJECT)/src/common/firmware/ADRV9025_DPDCORE_FW.h \
+	$(PROJECT)/src/common/firmware/ADRV9025_FW.h \
+	$(PROJECT)/src/common/firmware/stream_image_6E3E00EFB74FE7D465FA88A171B81B8F.h \
+	$(PROJECT)/src/common/firmware/ActiveUseCase_profile.h \
+	$(PROJECT)/src/common/firmware/ActiveUtilInit_profile.h
+
+SRCS += $(PROJECT)/src/common/common_data.c \
+	$(PROJECT)/src/common/hal/no_os_platform.c
+
+INCS += $(PROJECT)/src/platform/platform_includes.h \
+	$(PROJECT)/src/platform/$(PLATFORM)/parameters.h
+SRCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.c
+
+SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
+	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.c \
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(DRIVERS)/frequency/ad9528/ad9528.c \
+	$(NO-OS)/util/no_os_util.c \
+	$(NO-OS)/util/no_os_clk.c \
+	$(NO-OS)/util/no_os_alloc.c \
+	$(NO-OS)/util/no_os_mutex.c\
+	$(DRIVERS)/api/no_os_spi.c \
+	$(DRIVERS)/api/no_os_gpio.c \
+	$(NO-OS)/jesd204/jesd204-core.c \
+	$(NO-OS)/jesd204/jesd204-fsm.c
+SRCS += $(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c
+ifeq (y,$(strip $(TINYIIOD)))
+LIBRARIES += iio
+SRCS += $(NO-OS)/util/no_os_lf256fifo.c \
+	$(NO-OS)/util/no_os_fifo.c \
+	$(NO-OS)/util/no_os_list.c \
+	$(DRIVERS)/axi_core/iio_axi_adc/iio_axi_adc.c \
+	$(DRIVERS)/axi_core/iio_axi_dac/iio_axi_dac.c \
+	$(NO-OS)/iio/iio_app/iio_app.c \
+	$(DRIVERS)/api/no_os_uart.c \
+	$(DRIVERS)/api/no_os_irq.c
+endif
+
+# Madura API sources
+SRC_DIRS += $(DRIVERS)/rf-transceiver/madura
+
+INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
+	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.h \
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+	$(DRIVERS)/frequency/ad9528/ad9528.h
+INCS += $(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h
+INCS +=	$(INCLUDE)/no_os_axi_io.h \
+	$(INCLUDE)/no_os_spi.h \
+	$(INCLUDE)/no_os_irq.h \
+	$(INCLUDE)/no_os_gpio.h \
+	$(INCLUDE)/no_os_error.h \
+	$(INCLUDE)/no_os_delay.h \
+	$(INCLUDE)/no_os_util.h \
+	$(INCLUDE)/no_os_clk.h \
+	$(INCLUDE)/no_os_print_log.h \
+	$(INCLUDE)/no_os_alloc.h \
+	$(INCLUDE)/no_os_mutex.h \
+	$(INCLUDE)/jesd204.h \
+	$(NO-OS)/jesd204/jesd204-priv.h
+ifeq (y,$(strip $(TINYIIOD)))
+INCS += $(INCLUDE)/no_os_fifo.h \
+	$(INCLUDE)/no_os_irq.h \
+	$(INCLUDE)/no_os_uart.h \
+	$(INCLUDE)/no_os_lf256fifo.h \
+	$(INCLUDE)/no_os_list.h \
+	$(DRIVERS)/axi_core/iio_axi_adc/iio_axi_adc.h \
+	$(NO-OS)/iio/iio_app/iio_app.h \
+	$(DRIVERS)/axi_core/iio_axi_dac/iio_axi_dac.h
+endif
+

commit 127e37e97981a346969d5474402a0a9bae9df9c1

adrv902x: Fix uart src in platform_src.mk
Fixes: ab393ac ("projects: adrv9025: Madura project")
Signed-off-by: George Mois <george.mois@analog.com>

projects/adrv902x/src/platform/xilinx/platform_src.mk
@@ -6,7 +6,7 @@ SRCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c \
 	$(PLATFORM_DRIVERS)/xilinx_delay.c
 	
 ifeq (y,$(strip $(IIOD)))
-SRCS += $(PLATFORM_DRIVERS)/$xilinx_uart.c
+SRCS += $(PLATFORM_DRIVERS)/xilinx_uart.c
 endif
 	
 INCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \