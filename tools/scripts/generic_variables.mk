analogdevicesinc/no-OS

commit ded4e851c417a55908fd610e4121b9fc48c3a5fd

tools: scripts: generic_variables: handle maxim
If a TARGET is specified as parameter to the make command and has the
`max` prefix, then assume that the PLATFORM=maxim.

This patch allows building maxim platform projects without specifying
explicitly the platform if there is a maxim TARGET set.

Example:
`make PLATFORM=maxim TARGET=max32650`
can be called now simply
`make TARGET=max32650`

A similar approach is already implemented for the xilinx projects where
if a user sets the HARDWARE variable, the PLATFORM is set automatically
to `xilinx`.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

tools/scripts/generic_variables.mk
@@ -57,7 +57,11 @@ else
 ifneq '' '$(findstring pinmux_config.c,$(HARDWARE))'
 PLATFORM = aducm3029
 else
+ifneq '' '$(findstring max,$(TARGET))'
+PLATFORM = maxim
+else
 $(error No HARDWARE found)
+endif
 endif
 endif
 endif

commit ae4f4a6652b83b538c10f1ed73d7208dc29aa48d

tools: scripts: fix maxim target handling
If a hardware file exists (.xsa, .hdf, .ioc, etc) even though the max
target is specified the build will perform for the first hardware file
that is found.

Fixes: ded4e85 ("tools: scripts: generic_variables: handle maxim")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

tools/scripts/generic_variables.mk
@@ -42,6 +42,9 @@ LINK_SRCS ?= y
 
 HARDWARE ?= $(wildcard *.xsa) $(wildcard *.hdf) $(wildcard *.sopcinfo) $(wildcard *.ioc) $(wildcard pinmux_config.c)
 #If platform not set get it from HARDWARE file
+ifneq '' '$(findstring max,$(TARGET))'
+PLATFORM = maxim
+else
 ifeq '' '$(PLATFORM)'
 ifneq '' '$(findstring .xsa,$(HARDWARE))'
 PLATFORM = xilinx
@@ -57,9 +60,6 @@ PLATFORM = stm32
 else
 ifneq '' '$(findstring pinmux_config.c,$(HARDWARE))'
 PLATFORM = aducm3029
-else
-ifneq '' '$(findstring max,$(TARGET))'
-PLATFORM = maxim
 else
 $(error No HARDWARE or TARGET found. Please specify one of them.)
 endif