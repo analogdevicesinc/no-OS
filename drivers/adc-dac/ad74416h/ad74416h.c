analogdevicesinc/no-OS

commit 44aa88ff764d8fdc54313e08373d88129b1bd216

adc-dac: ad74416h: add dac resolution for ad74414h
The dac resolution for ad74414h device is 14 bits. Update the conversion
functions accordingly.

Fixes: f3f0d00670c7 ("adc-dac: ad74416h: add support for ad74414h")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc-dac/ad74416h/ad74416h.c
@@ -86,7 +86,16 @@ static const uint32_t conv_rate_ad74416h[] = {
 int ad74416h_dac_voltage_to_code(struct ad74416h_desc *desc, int32_t mvolts,
 				 uint16_t *code, uint32_t ch)
 {
-	uint32_t range, offset;
+	uint32_t range, offset, res;
+
+	switch(desc->id) {
+	case ID_AD74414H:
+		res = AD74414H_DAC_RESOLUTION;
+	case ID_AD74416H:
+		res = AD74416H_DAC_RESOLUTION;
+	default:
+		return -EINVAL;
+	}
 
 	switch (desc->channel_configs[ch].vout_range) {
 	case AD74416H_VOUT_RANGE_0_12V:
@@ -105,22 +114,35 @@ int ad74416h_dac_voltage_to_code(struct ad74416h_desc *desc, int32_t mvolts,
 		return -EINVAL;
 	}
 
-	*code = (mvolts + offset) * NO_OS_BIT(AD74416H_DAC_RESOLUTION) / range;
+	*code = (mvolts + offset) * NO_OS_BIT(res) / range;
 
 	return 0;
 }
 
 /**
  * @brief Convers a microamp value in the corresponding DAC 16 bit code
+ * @param desc - The device structure.
  * @param uamps - The microamps value
  * @param code - The resulting DAC code
  * @return 0 in case of success, -EINVAL otherwise
  */
-int ad74416h_dac_current_to_code(uint32_t uamps, uint16_t *code)
+int ad74416h_dac_current_to_code(struct ad74416h_desc *desc, uint32_t uamps,
+				 uint16_t *code)
 {
+	uint32_t res;
+
+	switch(desc->id) {
+	case ID_AD74414H:
+		res = AD74414H_DAC_RESOLUTION;
+	case ID_AD74416H:
+		res = AD74416H_DAC_RESOLUTION;
+	default:
+		return -EINVAL;
+	}
+
 	if (uamps > AD74416H_DAC_CURRENT_RANGE)
 		return -EINVAL;
-	*code = uamps * NO_OS_BIT(AD74416H_DAC_RESOLUTION) / AD74416H_DAC_CURRENT_RANGE;
+	*code = uamps * NO_OS_BIT(res) / AD74416H_DAC_CURRENT_RANGE;
 	return 0;
 }
 

drivers/adc-dac/ad74416h/ad74416h.h
@@ -425,6 +425,7 @@
 #define AD74416H_DAC_RANGE			12000
 #define AD74416H_DAC_CURRENT_RANGE		25000
 #define AD74416H_DAC_RESOLUTION			16
+#define AD74414H_DAC_RESOLUTION			14
 #define AD74116H_CONV_TIME_US			1000000
 
 /******************************************************************************/
@@ -649,7 +650,7 @@ int ad74416h_dac_voltage_to_code(struct ad74416h_desc *, int32_t,
 				 uint16_t *, uint32_t);
 
 /** Converts a microamp value in the corresponding DAC 16 bit code */
-int ad74416h_dac_current_to_code(uint32_t, uint16_t *);
+int ad74416h_dac_current_to_code(struct ad74416h_desc *, uint32_t, uint16_t *);
 
 /** Write a register's value */
 int ad74416h_reg_write(struct ad74416h_desc *, uint32_t, uint16_t);

commit 60bc0a462ff16354350b285bbd4c048c6056cd54

adc-dac: ad74416h: add missing breaks
Add missing break statements in the switch cases for device id.

Fixes: 44aa88f ("adc-dac: ad74416h: add dac resolution for ad74414h")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc-dac/ad74416h/ad74416h.c
@@ -91,8 +91,10 @@ int ad74416h_dac_voltage_to_code(struct ad74416h_desc *desc, int32_t mvolts,
 	switch(desc->id) {
 	case ID_AD74414H:
 		res = AD74414H_DAC_RESOLUTION;
+		break;
 	case ID_AD74416H:
 		res = AD74416H_DAC_RESOLUTION;
+		break;
 	default:
 		return -EINVAL;
 	}
@@ -134,8 +136,10 @@ int ad74416h_dac_current_to_code(struct ad74416h_desc *desc, uint32_t uamps,
 	switch(desc->id) {
 	case ID_AD74414H:
 		res = AD74414H_DAC_RESOLUTION;
+		break;
 	case ID_AD74416H:
 		res = AD74416H_DAC_RESOLUTION;
+		break;
 	default:
 		return -EINVAL;
 	}