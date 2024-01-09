analogdevicesinc/no-OS

commit 746d823f27cf4b6581fb3bba129af2365c9ee705

drivers: imu: iio_adis: Use channel address instead of index
Use channel address instead of channel index to differentiate
the channels.

Signed-off-by: Ramona Gradinariu <ramona.gradinariu@analog.com>

drivers/imu/iio_adis.c
@@ -127,7 +127,7 @@ static int adis_iio_read_raw(void *dev, char *buf, uint32_t len,
 
 	adis = iio_adis->adis_dev;
 
-	switch(channel->ch_num) {
+	switch(channel->address) {
 	case ADIS_GYRO_X:
 		ret = adis_read_x_gyro(adis, &res);
 		break;

drivers/imu/iio_adis.h
@@ -209,8 +209,8 @@ struct adis_iio_dev {
 #define ADIS_ACCEL_CHAN(mod, idx, adis_nb) { \
 	.ch_type = IIO_ACCEL, \
 	.modified = true, \
-	.channel = idx, \
 	.channel2 = IIO_MOD_##mod, \
+	.address = idx, \
 	.scan_index = idx, \
 	.scan_type = &adis##adis_nb##_iio_accel_scan_type,  \
         .attributes = adis_iio_accel_attrs,  \
@@ -219,8 +219,8 @@ struct adis_iio_dev {
 #define ADIS_GYRO_CHAN(mod, idx, adis_nb) { \
 	.ch_type = IIO_ANGL_VEL, \
 	.modified = true, \
-	.channel = idx, \
 	.channel2 = IIO_MOD_##mod, \
+	.address = idx, \
 	.scan_index = idx, \
 	.scan_type = &adis##adis_nb##_iio_anglvel_scan_type,  \
         .attributes = adis_iio_anglvel_attrs,  \
@@ -229,8 +229,8 @@ struct adis_iio_dev {
 #define ADIS_DELTA_ANGL_CHAN(mod, idx, adis_nb) { \
 	.ch_type = IIO_DELTA_ANGL, \
 	.modified = true, \
-	.channel = idx, \
 	.channel2 = IIO_MOD_##mod, \
+	.address = idx, \
 	.scan_index = idx, \
 	.scan_type = &adis##adis_nb##_iio_delta_angl_scan_type,  \
         .attributes = adis_iio_delta_angl_attrs,  \
@@ -239,8 +239,8 @@ struct adis_iio_dev {
 #define ADIS_DELTA_VEL_CHAN(mod, idx, adis_nb) { \
 	.ch_type = IIO_DELTA_VELOCITY, \
 	.modified = true, \
-	.channel = idx, \
 	.channel2 = IIO_MOD_##mod, \
+	.address = idx, \
 	.scan_index = idx, \
 	.scan_type = &adis##adis_nb##_iio_delta_vel_scan_type,  \
         .attributes = adis_iio_delta_vel_attrs,  \
@@ -248,7 +248,7 @@ struct adis_iio_dev {
 
 #define ADIS_TEMP_CHAN(idx, adis_nb) { \
 	.ch_type = IIO_TEMP, \
-	.channel = idx, \
+	.address = idx, \
 	.scan_index = idx, \
 	.scan_type = &adis##adis_nb##_iio_temp_scan_type,  \
         .attributes = adis_iio_temp_attrs,  \

commit 57acd7fdf2cee604043bfbb0803a893468ba8108

Use channel address instead of channel number
Channel number is no longer used, instead channel address has
to be used to differentiate between channels.

Fixes: 746d823 ("drivers: imu: iio_adis: Use channel address instead of index")
Signed-off-by: Ramona Gradinariu <ramona.gradinariu@analog.com>

drivers/imu/iio_adis.c
@@ -275,7 +275,7 @@ static int adis_iio_read_calibbias(void *dev, char *buf, uint32_t len,
 
 	adis = iio_adis->adis_dev;
 
-	switch (channel->ch_num) {
+	switch (channel->address) {
 	case ADIS_GYRO_X:
 		ret = adis_read_xg_bias(adis, &res);
 		break;
@@ -335,7 +335,7 @@ static int adis_iio_write_calibbias(void *dev, char *buf, uint32_t len,
 	if (ret)
 		return ret;
 
-	switch (channel->ch_num) {
+	switch (channel->address) {
 	case ADIS_GYRO_X:
 		return adis_write_xg_bias(adis, calibbias);
 	case ADIS_GYRO_Y: