analogdevicesinc/no-OS

commit b6efc453df4c44a6a1429855d9f8d4fbecd5e057

SPI: added bus desc to api and initialization
Signed-off-by: rbudai <robert.budai@analog.com>

drivers/api/no_os_spi.c
@@ -41,6 +41,13 @@
 #include "no_os_spi.h"
 #include <stdlib.h>
 #include "no_os_error.h"
+#include "no_os_mutex.h"
+#include "no_os_alloc.h"
+
+/**
+ * @brief spi_table contains the pointers towards the SPI buses
+*/
+static void *spi_table[SPI_MAX_BUS_NUMBER + 1];
 
 /**
  * @brief Initialize the SPI communication peripheral.
@@ -58,13 +65,53 @@ int32_t no_os_spi_init(struct no_os_spi_desc **desc,
 
 	if (!param->platform_ops->init)
 		return -ENOSYS;
-
+	if (param->device_id > SPI_MAX_BUS_NUMBER)
+		return -EINVAL;
+	// Initializing BUS descriptor
+	if (!spi_table[param->device_id]) {
+		ret = no_os_spibus_init(param);
+		if (ret)
+			return ret;
+	}
+	// Initilize SPI descriptor
 	ret = param->platform_ops->init(desc, param);
 	if (ret)
 		return ret;
-
+	(*desc)->bus = spi_table[param->device_id];
 	(*desc)->platform_ops = param->platform_ops;
 	(*desc)->parent = param->parent;
+
+	return 0;
+}
+
+/**
+ * @brief Initialize the SPI bus communication peripheral.
+ * @param param - The structure that containes the SPI bus parameters
+ * @return 0 in case of success, error code otherwise
+*/
+int32_t no_os_spibus_init(const struct no_os_spi_init_param *param)
+{
+	struct no_os_spibus_desc *bus = (struct no_os_spibus_desc *)no_os_calloc(1,
+					sizeof(struct no_os_spibus_desc));
+
+	if (!bus)
+		return -ENOMEM;
+
+	no_os_mutex_init(bus->mutex);
+
+	if (!bus->mutex) {
+		no_os_free(bus);
+		return -ENOMEM;
+	}
+
+	bus->device_id = param->device_id;
+	bus->max_speed_hz = param->max_speed_hz;
+	bus->mode = param->mode;
+	bus->bit_order = param->bit_order;
+	bus->platform_ops = param->platform_ops;
+	bus->extra = param->extra;
+
+	spi_table[param->device_id] = bus;
 
 	return 0;
 }
@@ -76,13 +123,34 @@ int32_t no_os_spi_init(struct no_os_spi_desc **desc,
  */
 int32_t no_os_spi_remove(struct no_os_spi_desc *desc)
 {
+	// Remove SPI bus
+	no_os_spibus_remove(desc->device_id);
+
 	if (!desc || !desc->platform_ops)
 		return -EINVAL;
 
 	if (!desc->platform_ops->remove)
 		return -ENOSYS;
-
 	return desc->platform_ops->remove(desc);
+}
+
+/**
+ * @brief Removes SPI bus instance
+ * @param bus_number - SPI bus number
+*/
+void no_os_spibus_remove(uint32_t bus_number)
+{
+	struct no_os_spibus_desc *spi_bus_desriptor = (struct no_os_spibus_desc *)
+			spi_table[bus_number];
+
+	if (spi_bus_desriptor->mutex) {
+		no_os_mutex_remove(spi_bus_desriptor->mutex);
+		spi_bus_desriptor->mutex = 0;
+	}
+	if (spi_bus_desriptor) {
+		no_os_free(spi_bus_desriptor);
+		spi_bus_desriptor = 0;
+	}
 }
 
 /**

include/no_os_spi.h
@@ -52,6 +52,7 @@
 
 #define	NO_OS_SPI_CPHA	0x01
 #define	NO_OS_SPI_CPOL	0x02
+#define SPI_MAX_BUS_NUMBER 8
 
 /******************************************************************************/
 /*************************** Types Declarations *******************************/
@@ -137,11 +138,34 @@ struct no_os_spi_init_param {
 };
 
 /**
+ * @struct no_os_spibus_desc
+ * @brief SPI bus descriptor
+*/
+struct no_os_spibus_desc {
+	/** SPI bus mutex (lock) */
+	void 		*mutex;
+	/** SPI bus device id */
+	uint32_t	device_id;
+	/** SPI bus max speed */
+	uint32_t	max_speed_hz;
+	/** SPI bus mode */
+	enum no_os_spi_mode	mode;
+	/** SPI bus bit order */
+	enum no_os_spi_bit_order	bit_order;
+	/** SPI bus platform ops */
+	const struct no_os_spi_platform_ops *platform_ops;
+	/** SPI bus extra */
+	void		*extra;
+};
+
+/**
  * @struct no_os_spi_desc
  * @brief Structure holding SPI descriptor.
  */
 struct no_os_spi_desc {
-	/** Device ID */
+	/** SPI bus address */
+	struct no_os_spibus_desc	*bus;
+	/** SPI bus number (0 for SPI0, 1 for SPI1, ...) */
 	uint32_t	device_id;
 	/** maximum transfer speed */
 	uint32_t	max_speed_hz;
@@ -194,6 +218,12 @@ int32_t no_os_spi_write_and_read(struct no_os_spi_desc *desc,
 int32_t no_os_spi_transfer(struct no_os_spi_desc *desc,
 			   struct no_os_spi_msg *msgs,
 			   uint32_t len);
+
+/* Initialize SPI bus descriptor*/
+int32_t no_os_spibus_init(const struct no_os_spi_init_param *param);
+
+/* Free the resources allocated for SPI bus desc*/
+void no_os_spibus_remove(uint32_t bus_number);
 
 
 #endif // _NO_OS_SPI_H_

commit 9d431b961e32e23e8faf9263c62a27318cd189b9

drivers: api: no_os_spi: Fix remove API
Call spibus_remove only if desc and desc->bus are not null to
avoid undefined behavior.

Fixes: b6efc45 ("SPI: added bus desc to api and initialization")
Signed-off-by: Ramona Gradinariu <ramona.gradinariu@analog.com>

drivers/api/no_os_spi.c
@@ -121,11 +121,11 @@ int32_t no_os_spibus_init(const struct no_os_spi_init_param *param)
  */
 int32_t no_os_spi_remove(struct no_os_spi_desc *desc)
 {
-	// Remove SPI bus
-	no_os_spibus_remove(desc->bus->device_id);
-
 	if (!desc || !desc->platform_ops)
 		return -EINVAL;
+
+	if (desc->bus)
+		no_os_spibus_remove(desc->bus->device_id);
 
 	if (!desc->platform_ops->remove)
 		return -ENOSYS;