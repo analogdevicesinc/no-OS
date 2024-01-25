analogdevicesinc/no-OS

commit b52797090b586944a3688b954e55d5745332e9d8

drivers: dac: ad5791: Add linearity error compensation API
Implement set linearity error compensation API, add enum for possible voltage spans.

Signed-off-by: SGudla <Saikiran.Gudla@analog.com>

drivers/dac/ad5791/ad5791.c
@@ -381,3 +381,24 @@ int ad5791_spi_write_mask(struct ad5791_dev *dev,
 	return ad5791_set_register_value(dev, register_address, reg_data);
 }
 
+/***************************************************************************//**
+ * @brief	Set Linearity error compensation based on the reference voltage span.
+ *
+ * @param	dev		- The device structure.
+ * @param	v_span	- voltage span to set the corresponding lin_comp value.
+ * @return	0 in case of success, negative error code otherwise.
+ */
+int ad5791_set_lin_comp(struct ad5791_dev *dev,
+			enum ad5791_lin_comp_select v_span)
+{
+	if(!dev || (dev->act_device != ID_AD5781 && dev->act_device != ID_AD5791))
+		return -EINVAL;
+
+	if (!v_span)
+		v_span |= NO_OS_BIT(4);
+
+	return ad5791_spi_write_mask(dev,
+				     AD5791_REG_CTRL,
+				     AD5791_CTRL_LINCOMP_MASK,
+				     AD5791_CTRL_LINCOMP(v_span));
+}

drivers/dac/ad5791/ad5791.h
@@ -62,6 +62,16 @@ enum ad5791_type {
 	ID_AD5791
 };
 
+/* Possible voltage spans for linearity error compensation */
+enum ad5791_lin_comp_select {
+	AD5781_SPAN_UPTO_10V = 0,
+	AD5781_SPAN_10V_TO_20V = 4,
+	AD5791_SPAN_10V_TO_12V = 1,
+	AD5791_SPAN_12V_TO_16V = 2,
+	AD5791_SPAN_16V_TO_19V = 3,
+	AD5791_SPAN_19V_TO_20V = 4
+};
+
 struct ad5791_chip_info {
 	uint32_t resolution;
 };
@@ -196,5 +206,9 @@ int ad5791_spi_write_mask(struct ad5791_dev *dev,
 			  uint8_t register_address,
 			  uint32_t mask,
 			  uint32_t value);
+
+/*! Set Linearity error compensation based on the reference voltage span. */
+int ad5791_set_lin_comp(struct ad5791_dev *dev,
+			enum ad5791_lin_comp_select v_span);
 
 #endif /* __AD5791_H__ */

commit 83ff6c0122479196b867dce9b9339a25f7ff9471

dac: ad5791: Fix set_lin_comp API
Fix v_span condition check in the set_lin_comp api

Fixes: b527970 ("drivers: dac: ad5791: Add linearity error compensation API")
Signed-off-by: SGudla <Saikiran.Gudla@analog.com>

drivers/dac/ad5791/ad5791.c
@@ -408,7 +408,7 @@ int ad5791_set_lin_comp(struct ad5791_dev *dev,
 		return -EINVAL;
 	}
 
-	if (!v_span)
+	if (v_span)
 		v_span |= NO_OS_BIT(3);
 
 	return ad5791_spi_write_mask(dev,