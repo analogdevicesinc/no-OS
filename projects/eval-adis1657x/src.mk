2202ffb9fc6956f65ef0e55efb0b594b5680a3bd
Ramona Gradinariu <ramona.gradinariu@analog.com>
2024-03-08 15:50:43 +0200

    projects: eval-adis1657x: Remove white spaces from src.mk
    Remove white spaces from src.mk from eval-adis1657x project.
    
    Signed-off-by: Ramona Gradinariu <ramona.gradinariu@analog.com>

--- projects/eval-adis1657x/src.mk
+++ projects/eval-adis1657x/src.mk
@@ -5,11 +5,11 @@ SRCS += $(PROJECT)/src/platform/$(PLATFORM)/main.c
 
 INCS += $(PROJECT)/src/common/common_data.h
 SRCS += $(PROJECT)/src/common/common_data.c
-	
+
 INCS += $(PROJECT)/src/platform/platform_includes.h
 
 INCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.h
-SRCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.c 
+SRCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.c
 
 INCS += $(INCLUDE)/no_os_delay.h     \
 		$(INCLUDE)/no_os_error.h     \
