analogdevicesinc/no-OS

commit e1161faea74a7b22098d63524f510b6b2801336b

drivers: adc: ad713x: add ad4134 data format
The output data frame for the ad4134 device was missing.

Fixes: 6eda224 ("drivers:adc:ad713x: update driver to work with new silicon")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad713x/ad713x.c
@@ -84,6 +84,15 @@ static const int ad713x_output_data_frame[3][9][2] = {
 		{ADC_16_BIT_DATA, CRC_8},
 		{INVALID}
 	},
+	{
+		{ADC_16_BIT_DATA, NO_CRC},
+		{ADC_16_BIT_DATA, CRC_6},
+		{ADC_24_BIT_DATA, NO_CRC},
+		{ADC_24_BIT_DATA, CRC_6},
+		{ADC_16_BIT_DATA, CRC_8},
+		{ADC_24_BIT_DATA, CRC_8},
+		{INVALID}
+	},
 };
 
 /******************************************************************************/

commit 06793b0ef42a66f99b193648f5b3c51dfbe0e5c2

drivers:adc:ad713x:Bug fix with AD7134 driver
Bug fix to ensure that number of array elements dont exceed
the array size

fixes: e1161fa ("drivers:adc:ad713x:add ad7134 data format")
Signed-off-by: Janani Sunil <janani.sunil@analog.com>

drivers/adc/ad713x/ad713x.c
@@ -58,7 +58,7 @@
 /***************************** Variable definition ****************************/
 /******************************************************************************/
 
-static const int ad713x_output_data_frame[3][9][2] = {
+static const int ad713x_output_data_frame[4][9][2] = {
 	{
 		{ADC_16_BIT_DATA, CRC_6},
 		{ADC_24_BIT_DATA, CRC_6},