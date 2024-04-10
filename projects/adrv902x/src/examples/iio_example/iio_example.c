analogdevicesinc/no-OS

commit ab393ac22c5ff651ba604dfad099724e5eb8efda

projects: adrv9025: Madura project
Add Madura sample project.

Remove Madura driver from drivers build check, cppcheck, and
astyle check.

Signed-off-by: George Mois <george.mois@analog.com>

projects/adrv902x/src/examples/iio_example/iio_example.c
@@ -1,0 +1,528 @@
+/***************************************************************************//**
+ *   @file   iio_example.c
+ *   @brief  IIO example for the adrv902x project
+ *   @author GMois (george.mois@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "iio_example.h"
+#include "common_data.h"
+#include "parameters.h"
+#include "app_config.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "no_os_print_log.h"
+#include "no_os_error.h"
+#include "no_os_util.h"
+#include "no_os_spi.h"
+
+#include "axi_jesd204_rx.h"
+#include "axi_jesd204_tx.h"
+#include "axi_adc_core.h"
+#include "axi_dac_core.h"
+#include "axi_adxcvr.h"
+#include "axi_dmac.h"
+
+#include "xilinx_gpio.h"
+#include "xilinx_uart.h"
+#include "xilinx_spi.h"
+#include "xil_cache.h"
+
+#include "adrv9025.h"
+#include "ad9528.h"
+
+#include "iio_axi_adc.h"
+#include "iio_axi_dac.h"
+#include "iio_app.h"
+
+
+uint32_t dac_buffer[DAC_BUFFER_SAMPLES] __attribute__ ((aligned));
+uint16_t adc_buffer[ADC_BUFFER_SAMPLES * ADC_CHANNELS] __attribute__ ((
+			aligned));
+
+/******************************************************************************/
+/************************ Functions Declarations ******************************/
+/******************************************************************************/
+/***************************************************************************//**
+ * @brief IIO example main execution.
+ *
+ * @return ret - Result of the example execution.
+*******************************************************************************/
+int iio_example_main(void)
+{
+	struct adrv9025_init_param adrv9025_init_par = { 0 };
+	struct adi_adrv9025_Device adrv9025_device = { 0 };
+	struct ad9528_platform_data ad9528_pdata = { 0 };
+	struct ad9528_channel_spec ad9528_channels[14];
+	struct ad9528_init_param ad9528_param;
+	struct ad9528_dev* ad9528_device;
+	struct adrv9025_rf_phy *phy;
+	int status;
+
+	Xil_ICacheEnable();
+	Xil_DCacheEnable();
+
+	struct axi_dmac_init rx_dmac_init = {
+		"rx_dmac",
+		RX_DMA_BASEADDR,
+		IRQ_DISABLED
+	};
+	struct axi_dmac *rx_dmac;
+	struct axi_dmac_init tx_dmac_init = {
+		"tx_dmac",
+		TX_DMA_BASEADDR,
+		IRQ_DISABLED
+	};
+	struct axi_dmac *tx_dmac;
+
+	ad9528_param.spi_init = ad9528_spi_param;
+
+	// Export no_os_clk_desc for each channel
+	ad9528_param.export_no_os_clk = true;
+
+	// ad9528 defaults
+	ad9528_param.gpio_resetb = &clkchip_gpio_init_param;
+	ad9528_param.pdata = &ad9528_pdata;
+	ad9528_param.pdata->num_channels = 14;
+	ad9528_param.pdata->channels = &ad9528_channels[0];
+	ad9528_init(&ad9528_param);
+
+	// channel 0
+	ad9528_channels[0].channel_num = 0;
+	ad9528_channels[0].driver_mode = DRIVER_MODE_LVDS;
+	ad9528_channels[0].divider_phase = 0;
+	ad9528_channels[0].channel_divider = 5;
+	ad9528_channels[0].signal_source = SOURCE_SYSREF_VCO;
+	ad9528_channels[0].output_dis = 0;
+
+	// channel 1
+	ad9528_channels[1].channel_num = 1;
+	ad9528_channels[1].driver_mode = DRIVER_MODE_LVDS;
+	ad9528_channels[1].divider_phase = 0;
+	ad9528_channels[1].channel_divider = 5;
+	ad9528_channels[1].signal_source = SOURCE_VCO;
+	ad9528_channels[1].output_dis = 0;
+
+	// channel 3
+	ad9528_channels[3].channel_num = 3;
+	ad9528_channels[3].driver_mode = DRIVER_MODE_LVDS;
+	ad9528_channels[3].divider_phase = 0;
+	ad9528_channels[3].channel_divider = 5;
+	ad9528_channels[3].signal_source = SOURCE_VCO;
+	ad9528_channels[3].output_dis = 0;
+
+	// channel 12
+	ad9528_channels[12].channel_num = 12;
+	ad9528_channels[12].driver_mode = DRIVER_MODE_LVDS;
+	ad9528_channels[12].divider_phase = 0;
+	ad9528_channels[12].channel_divider = 5;
+	ad9528_channels[12].signal_source = SOURCE_SYSREF_VCO;
+	ad9528_channels[12].output_dis = 0;
+
+	// channel 13
+	ad9528_channels[13].channel_num = 13;
+	ad9528_channels[13].driver_mode = DRIVER_MODE_LVDS;
+	ad9528_channels[13].divider_phase = 0;
+	ad9528_channels[13].channel_divider = 5;
+	ad9528_channels[13].signal_source = SOURCE_VCO;
+	ad9528_channels[13].output_dis = 0;
+
+	// pllx settings
+	ad9528_param.pdata->spi3wire = 0;
+	ad9528_param.pdata->vcxo_freq = 122880000;
+	ad9528_param.pdata->osc_in_diff_en = 0;
+	ad9528_param.pdata->refa_en = 1;
+	ad9528_param.pdata->refa_diff_rcv_en = 1;
+	ad9528_param.pdata->refb_diff_rcv_en = 0;
+	ad9528_param.pdata->osc_in_diff_en = 0;
+	/* JESD */
+	ad9528_param.pdata->jdev_desired_sysref_freq = 0;
+	/* PLL1 config */
+	ad9528_param.pdata->pll1_feedback_div = 4;
+	ad9528_param.pdata->pll1_charge_pump_current_nA = 5000;
+	ad9528_param.pdata->ref_mode = REF_MODE_EXT_REF;
+	/* PLL2 config */
+	ad9528_param.pdata->pll2_charge_pump_current_nA = 805000;
+	ad9528_param.pdata->pll2_vco_div_m1 = 3;
+	ad9528_param.pdata->pll2_r1_div = 1;
+	ad9528_param.pdata->pll2_ndiv_a_cnt = 3;
+	ad9528_param.pdata->pll2_ndiv_b_cnt = 27;
+	ad9528_param.pdata->pll2_n2_div = 10;
+	/* SYSREF config */
+	ad9528_param.pdata->sysref_src = SYSREF_SRC_INTERNAL;
+	ad9528_param.pdata->sysref_k_div = 512;
+	ad9528_param.pdata->sysref_pattern_mode = SYSREF_PATTERN_NSHOT;
+	ad9528_param.pdata->sysref_nshot_mode = SYSREF_NSHOT_8_PULSES;
+	ad9528_param.pdata->sysref_req_trigger_mode = SYSREF_LEVEL_HIGH;
+	ad9528_param.pdata->sysref_req_en = false;
+	ad9528_param.pdata->rpole2 = RPOLE2_900_OHM;
+	ad9528_param.pdata->rzero = RZERO_1850_OHM;
+	ad9528_param.pdata->cpole1 = CPOLE1_16_PF;
+	ad9528_param.pdata->pll1_bypass_en = false;
+	ad9528_param.pdata->pll2_bypass_en = false;
+	ad9528_param.pdata->stat0_pin_func_sel = 1; /* PLL1 & PLL2 Locked */
+	ad9528_param.pdata->stat1_pin_func_sel = 7; /* REFA Correct */
+
+	status = ad9528_setup(&ad9528_device, ad9528_param);
+	if (status != 0) {
+		printf("error: ad9528_setup() failed\n");
+		goto error;
+	}
+
+	struct axi_adc_init rx_adc_init = {
+		.name = "rx_adc",
+		.base = RX_CORE_BASEADDR,
+		.num_channels = 8
+	};
+	struct axi_dac_init tx_dac_init = {
+		.name = "tx_dac",
+		.base = TX_CORE_BASEADDR,
+		.channels = NULL,
+		.rate = 3,
+		.num_channels = 8
+	};
+
+	struct jesd204_tx_init tx_jesd_init = {
+		.name = "tx_jesd",
+		.base = TX_JESD_BASEADDR,
+		.octets_per_frame = ADRV9025_TX_JESD_OCTETS_PER_FRAME,
+		.frames_per_multiframe = ADRV9025_TX_JESD_FRAMES_PER_MULTIFRAME,
+		.converters_per_device = ADRV9025_TX_JESD_CONVS_PER_DEVICE,
+		.converter_resolution = ADRV9025_TX_JESD_CONV_RESOLUTION,
+		.bits_per_sample = ADRV9025_TX_JESD_BITS_PER_SAMPLE,
+		.high_density = ADRV9025_TX_JESD_HIGH_DENSITY,
+		.control_bits_per_sample = ADRV9025_TX_JESD_CTRL_BITS_PER_SAMPLE,// optional
+		.subclass = ADRV9025_TX_JESD_SUBCLASS,
+		.device_clk_khz = ADRV9025_DEVICE_CLK_KHZ,
+		.lane_clk_khz = ADRV9025_LANE_RATE_KHZ
+	};
+
+	struct jesd204_rx_init rx_jesd_init = {
+		.name = "rx_jesd",
+		.base = RX_JESD_BASEADDR,
+		.octets_per_frame = ADRV9025_RX_JESD_OCTETS_PER_FRAME,
+		.frames_per_multiframe = ADRV9025_RX_JESD_FRAMES_PER_MULTIFRAME,
+		.subclass = ADRV9025_RX_JESD_SUBCLASS,
+		.device_clk_khz = ADRV9025_DEVICE_CLK_KHZ,
+		.lane_clk_khz = ADRV9025_LANE_RATE_KHZ
+	};
+
+	struct adxcvr_init tx_adxcvr_init = {
+		.name = "tx_adxcvr",
+		.base = TX_XCVR_BASEADDR,
+		.sys_clk_sel = ADXCVR_SYS_CLK_QPLL0,
+		.out_clk_sel = ADXCVR_REFCLK,
+		.lpm_enable = 0,
+		.lane_rate_khz = ADRV9025_LANE_RATE_KHZ,
+		.ref_rate_khz = ADRV9025_DEVICE_CLK_KHZ,
+		.export_no_os_clk = true
+	};
+	struct adxcvr *tx_adxcvr;
+
+	struct adxcvr_init rx_adxcvr_init = {
+		.name = "rx_adxcvr",
+		.base = RX_XCVR_BASEADDR,
+		.sys_clk_sel = ADXCVR_SYS_CLK_CPLL,
+		.out_clk_sel = ADXCVR_REFCLK,
+		.lpm_enable = 1,
+		.lane_rate_khz = ADRV9025_LANE_RATE_KHZ,
+		.ref_rate_khz = ADRV9025_DEVICE_CLK_KHZ,
+		.export_no_os_clk = true
+	};
+	struct adxcvr *rx_adxcvr;
+
+	status = adxcvr_init(&tx_adxcvr, &tx_adxcvr_init);
+	if (status)
+		goto error_1;
+
+	status = adxcvr_init(&rx_adxcvr, &rx_adxcvr_init);
+	if (status)
+		goto error_2;
+
+	struct axi_jesd204_rx *rx_jesd;
+	struct axi_jesd204_tx *tx_jesd;
+
+	rx_jesd_init.lane_clk = rx_adxcvr->clk_out;
+	tx_jesd_init.lane_clk = tx_adxcvr->clk_out;
+
+	status = axi_jesd204_tx_init(&tx_jesd, &tx_jesd_init);
+	if (status)
+		goto error_3;
+
+	status = axi_jesd204_rx_init(&rx_jesd, &rx_jesd_init);
+	if (status)
+		goto error_4;
+
+	adrv9025_init_par.adrv9025_device = &adrv9025_device;
+	adrv9025_init_par.dev_clk = ad9528_device->clk_desc[1];
+	adrv9025_init_par.streamImageFile = ADRV9025_STREAM_IMAGE_FILE;
+
+	status = adrv9025_init(&phy, &adrv9025_init_par);
+	if (status) {
+		pr_err("error: adrv9025_init() failed\n");
+		goto error_5;
+	}
+
+	status = axi_dac_init_begin(&phy->tx_dac, &tx_dac_init);
+	if (status)
+		goto error_6;
+	status = axi_adc_init_begin(&phy->rx_adc, &rx_adc_init);
+	if (status)
+		goto error_7;
+
+	// Reset Tx DAC
+	axi_adc_write(phy->rx_adc, 0x4040, 0);
+	axi_adc_write(phy->rx_adc, 0x4040,
+		      NO_OS_BIT(1) | NO_OS_BIT(0));
+
+	// Reset Rx ADC
+	axi_adc_write(phy->rx_adc, AXI_ADC_REG_RSTN, 0);
+	axi_adc_write(phy->rx_adc, AXI_ADC_REG_RSTN,
+		      AXI_ADC_MMCM_RSTN | AXI_ADC_RSTN);
+
+	status = adrv9025_post_setup(phy);
+	if (status) {
+		pr_err("error: adrv9025_post_setup() failed\n");
+		goto error_8;
+	}
+
+	status = axi_dmac_init(&tx_dmac, &tx_dmac_init);
+	if (status) {
+		printf("axi_dmac_init tx init error: %d\n", status);
+		goto error_8;
+	}
+	status = axi_dmac_init(&rx_dmac, &rx_dmac_init);
+	if (status) {
+		printf("axi_dmac_init rx init error: %d\n", status);
+		goto error_9;
+	}
+
+	struct jesd204_topology *topology;
+	struct jesd204_topology_dev devs[] = {
+		{
+			.jdev = ad9528_device->jdev,
+			.link_ids = {DEFRAMER0_LINK_TX, FRAMER0_LINK_RX},
+			.links_number = 2,
+			.is_sysref_provider = true,
+		},
+		{
+			.jdev = rx_jesd->jdev,
+			.link_ids = {FRAMER0_LINK_RX},
+			.links_number = 1,
+		},
+		{
+			.jdev = tx_jesd->jdev,
+			.link_ids = {DEFRAMER0_LINK_TX},
+			.links_number = 1,
+		},
+		{
+			.jdev = phy->jdev,
+			.link_ids = {DEFRAMER0_LINK_TX, FRAMER0_LINK_RX},
+			.links_number = 2,
+			.is_top_device = true,
+		},
+	};
+
+	jesd204_topology_init(&topology, devs,
+			      sizeof(devs)/sizeof(*devs));
+
+	jesd204_fsm_start(topology, JESD204_LINKS_ALL);
+
+	axi_jesd204_tx_status_read(tx_jesd);
+	axi_jesd204_rx_status_read(rx_jesd);
+
+	/**
+	 * UART init params
+	 */
+	struct xil_uart_init_param platform_uart_init_par = {
+#ifdef XPAR_XUARTLITE_NUM_INSTANCES
+		.type = UART_PL,
+#else
+		.type = UART_PS,
+		.irq_id = UART_IRQ_ID
+#endif
+	};
+
+	struct no_os_uart_init_param iio_uart_ip = {
+		.device_id = UART_DEVICE_ID,
+		.irq_id = UART_IRQ_ID,
+		.baud_rate = UART_BAUDRATE,
+		.size = NO_OS_UART_CS_8,
+		.parity = NO_OS_UART_PAR_NO,
+		.stop = NO_OS_UART_STOP_1_BIT,
+		.extra = &platform_uart_init_par,
+		.platform_ops = &xil_uart_ops
+	};
+
+	/**
+	 * IIO application descriptor
+	 */
+	struct iio_app_desc *app;
+
+	/**
+	 * IIO application initialization parameters
+	 */
+	struct iio_app_init_param app_init_param = { 0 };
+
+	/**
+	 * iio axi adc configurations.
+	 */
+	struct iio_axi_adc_init_param iio_axi_adc_init_par;
+
+	/**
+	 * iio axi dac configurations.
+	 */
+	struct iio_axi_dac_init_param iio_axi_dac_init_par;
+
+	/**
+	 * iio instance descriptor.
+	 */
+	struct iio_axi_adc_desc *iio_axi_adc_desc;
+
+	/**
+	 * iio instance descriptor.
+	 */
+	struct iio_axi_dac_desc *iio_axi_dac_desc;
+
+	/**
+	 * iio device instance descriptor.
+	 */
+	struct iio_device *adc_dev_desc, *dac_dev_desc;
+
+	iio_axi_adc_init_par = (struct iio_axi_adc_init_param) {
+		.rx_adc = phy->rx_adc,
+		.rx_dmac = rx_dmac,
+#ifndef PLATFORM_MB
+		.dcache_invalidate_range = (void (*)(uint32_t,
+						     uint32_t))Xil_DCacheInvalidateRange,
+#endif
+	};
+
+	status = iio_axi_adc_init(&iio_axi_adc_desc, &iio_axi_adc_init_par);
+	if(status < 0)
+		goto error_10;
+
+	struct iio_data_buffer read_buff = {
+		.buff = (void *)adc_buffer,
+		.size = sizeof(adc_buffer),
+	};
+	iio_axi_adc_get_dev_descriptor(iio_axi_adc_desc, &adc_dev_desc);
+
+	iio_axi_dac_init_par = (struct iio_axi_dac_init_param) {
+		.tx_dac = phy->tx_dac,
+		.tx_dmac = tx_dmac,
+#ifndef PLATFORM_MB
+		.dcache_flush_range = (void (*)(uint32_t, uint32_t))Xil_DCacheFlushRange,
+#endif
+	};
+
+	status = iio_axi_dac_init(&iio_axi_dac_desc, &iio_axi_dac_init_par);
+	if(status < 0)
+		goto error_11;
+	struct iio_data_buffer write_buff = {
+		.buff = (void *)dac_buffer,
+		.size = sizeof(dac_buffer),
+	};
+	iio_axi_dac_get_dev_descriptor(iio_axi_dac_desc, &dac_dev_desc);
+	struct iio_app_device devices[] = {
+		IIO_APP_DEVICE("cf-adrv9025-lpc", iio_axi_adc_desc, adc_dev_desc, &read_buff, NULL, NULL),
+		IIO_APP_DEVICE("cf-adrv9025-dds-core-lpc", iio_axi_dac_desc, dac_dev_desc, NULL, &write_buff, NULL)
+	};
+
+	app_init_param.devices = devices;
+	app_init_param.nb_devices = NO_OS_ARRAY_SIZE(devices);
+	app_init_param.uart_init_params = iio_uart_ip;
+
+	/* Wait to display previous messages */
+	no_os_mdelay(100);
+
+	status = iio_app_init(&app, app_init_param);
+	if (status)
+		goto error_12;
+
+	return iio_app_run(app);
+
+error_12:
+	status = iio_axi_dac_remove(iio_axi_dac_desc);
+	if (status)
+		goto error;
+error_11:
+	status = iio_axi_adc_remove(iio_axi_adc_desc);
+	if (status)
+		goto error;
+
+error_10:
+	axi_dmac_remove(rx_dmac);
+error_9:
+	axi_dmac_remove(tx_dmac);
+error_8:
+	axi_adc_remove(phy->rx_adc);
+error_7:
+	axi_dac_remove(phy->tx_dac);
+error_6:
+	adi_adrv9025_HwClose(phy->madDevice);
+	adrv9025_remove(phy);
+error_5:
+	axi_jesd204_rx_remove(rx_jesd);
+error_4:
+	axi_jesd204_tx_remove(tx_jesd);
+error_3:
+	adxcvr_remove(rx_adxcvr);
+error_2:
+	adxcvr_remove(tx_adxcvr);
+error_1:
+	status = ad9528_remove(ad9528_device);
+	if (status)
+		goto error;
+
+	printf("Bye \n");
+
+	/* Disable the instruction cache. */
+	Xil_ICacheDisable();
+	/* Disable the data cache. */
+	Xil_DCacheDisable();
+
+	return 0;
+
+error:
+	printf("Error %d.\n", status);
+	return status;
+}

.astyleignore
@@ -10,5 +10,8 @@ drivers/adc/ad9081/api
 drivers/rf-transceiver/navassa/common/
 drivers/rf-transceiver/navassa/devices/
 drivers/rf-transceiver/navassa/third_party/
+drivers/rf-transceiver/madura/common/
+drivers/rf-transceiver/madura/devices/
+drivers/rf-transceiver/madura/platforms/
 drivers/adc/ad9083/ad9083_api
 legacy/

.cppcheckignore
@@ -16,6 +16,9 @@
 *:drivers/rf-transceiver/navassa/common/*
 *:drivers/rf-transceiver/navassa/devices/*
 *:drivers/rf-transceiver/navassa/third_party/*
+*:drivers/rf-transceiver/madura/common/*
+*:drivers/rf-transceiver/madura/devices/*
+*:drivers/rf-transceiver/madura/platforms/*
 *:drivers/adc/ad9083/ad9083_api
 *:libraries/fatfs/*
 *:libraries/mbedtls/*

ci/gen_dox.sh
@@ -33,7 +33,7 @@
 # THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #List of excluded driver folders for the documentation generation.
-EXCLUDE_DRV="talise navassa ad9083_api ad5940"
+EXCLUDE_DRV="talise navassa ad9083_api ad5940 madura"
 
 #Generate drivers_page.dox and projects_page.dox
 

doc/doxygen/CMakeLists.txt
@@ -26,6 +26,7 @@ set(DOCUMENTED_FILES
 set(EXCLUDE_FILES
     "${SOURCES_DIR}/drivers/rf-transceiver/talise \\
     ${SOURCES_DIR}/drivers/rf-transceiver/navassa \\
+    ${SOURCES_DIR}/drivers/rf-transceiver/madura \\
     ${SOURCES_DIR}/drivers/adc/ad9081/api \\
     ${SOURCES_DIR}/drivers/adc/ad9083/ad9083_api \\
     ${SOURCES_DIR}/drivers/afe/ad5940 \\

drivers/Makefile
@@ -3,7 +3,8 @@ CC = arm-none-eabi-gcc
 SKIPDIR = -path ./platform -prune -o \
 	-path ./axi_core -prune -o \
 	-path ./rf-transceiver/navassa -prune -o \
-	-path ./adc/ad9081 -prune -o
+	-path ./adc/ad9081 -prune -o \
+	-path ./rf-transceiver/madura -prune -o
 
 INCLUDES = -I../include/ \
 	 -I../projects/drivers/util/ \

projects/adrv902x/Makefile
@@ -1,0 +1,13 @@
+# Select the example you want to enable by choosing y for enabling and n for disabling
+BASIC_EXAMPLE = n
+DMA_EXAMPLE = n
+IIO_EXAMPLE = y
+
+# Uncomment to use the desired platform
+PLATFORM = xilinx
+
+include ../../tools/scripts/generic_variables.mk
+
+include src.mk
+
+include ../../tools/scripts/generic.mk

projects/adrv902x/src.mk
@@ -1,0 +1,96 @@
+################################################################################
+#									       #
+#     Shared variables:							       #
+#	- PROJECT							       #
+#	- DRIVERS							       #
+#	- INCLUDE							       #
+#	- PLATFORM_DRIVERS						       #
+#	- NO-OS								       #
+#									       #
+################################################################################
+include $(PROJECT)/src/platform/$(PLATFORM)/platform_src.mk
+include $(PROJECT)/src/examples/examples_src.mk
+
+SRCS += $(PROJECT)/src/platform/$(PLATFORM)/main.c
+
+INCS += $(PROJECT)/src/common/common_data.h \
+	$(PROJECT)/src/common/app_config.h \
+	$(PROJECT)/src/common/ADRV9025_RxGainTable.h \
+	$(PROJECT)/src/common/ADRV9025_TxAttenTable.h \
+	$(PROJECT)/src/common/hal/no_os_platform.h \
+	$(PROJECT)/src/common/firmware/ADRV9025_DPDCORE_FW.h \
+	$(PROJECT)/src/common/firmware/ADRV9025_FW.h \
+	$(PROJECT)/src/common/firmware/stream_image_6E3E00EFB74FE7D465FA88A171B81B8F.h \
+	$(PROJECT)/src/common/firmware/ActiveUseCase_profile.h \
+	$(PROJECT)/src/common/firmware/ActiveUtilInit_profile.h
+
+SRCS += $(PROJECT)/src/common/common_data.c \
+	$(PROJECT)/src/common/hal/no_os_platform.c
+
+INCS += $(PROJECT)/src/platform/platform_includes.h \
+	$(PROJECT)/src/platform/$(PLATFORM)/parameters.h
+SRCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.c
+
+SRCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.c \
+	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.c \
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.c \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.c \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c \
+	$(DRIVERS)/frequency/ad9528/ad9528.c \
+	$(NO-OS)/util/no_os_util.c \
+	$(NO-OS)/util/no_os_clk.c \
+	$(NO-OS)/util/no_os_alloc.c \
+	$(NO-OS)/util/no_os_mutex.c\
+	$(DRIVERS)/api/no_os_spi.c \
+	$(DRIVERS)/api/no_os_gpio.c \
+	$(NO-OS)/jesd204/jesd204-core.c \
+	$(NO-OS)/jesd204/jesd204-fsm.c
+SRCS += $(DRIVERS)/axi_core/jesd204/axi_adxcvr.c \
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.c
+ifeq (y,$(strip $(TINYIIOD)))
+LIBRARIES += iio
+SRCS += $(NO-OS)/util/no_os_lf256fifo.c \
+	$(NO-OS)/util/no_os_fifo.c \
+	$(NO-OS)/util/no_os_list.c \
+	$(DRIVERS)/axi_core/iio_axi_adc/iio_axi_adc.c \
+	$(DRIVERS)/axi_core/iio_axi_dac/iio_axi_dac.c \
+	$(NO-OS)/iio/iio_app/iio_app.c \
+	$(DRIVERS)/api/no_os_uart.c \
+	$(DRIVERS)/api/no_os_irq.c
+endif
+
+# Madura API sources
+SRC_DIRS += $(DRIVERS)/rf-transceiver/madura
+
+INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
+	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.h \
+	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
+	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
+	$(DRIVERS)/frequency/ad9528/ad9528.h
+INCS += $(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \
+	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h
+INCS +=	$(INCLUDE)/no_os_axi_io.h \
+	$(INCLUDE)/no_os_spi.h \
+	$(INCLUDE)/no_os_irq.h \
+	$(INCLUDE)/no_os_gpio.h \
+	$(INCLUDE)/no_os_error.h \
+	$(INCLUDE)/no_os_delay.h \
+	$(INCLUDE)/no_os_util.h \
+	$(INCLUDE)/no_os_clk.h \
+	$(INCLUDE)/no_os_print_log.h \
+	$(INCLUDE)/no_os_alloc.h \
+	$(INCLUDE)/no_os_mutex.h \
+	$(INCLUDE)/jesd204.h \
+	$(NO-OS)/jesd204/jesd204-priv.h
+ifeq (y,$(strip $(TINYIIOD)))
+INCS += $(INCLUDE)/no_os_fifo.h \
+	$(INCLUDE)/no_os_irq.h \
+	$(INCLUDE)/no_os_uart.h \
+	$(INCLUDE)/no_os_lf256fifo.h \
+	$(INCLUDE)/no_os_list.h \
+	$(DRIVERS)/axi_core/iio_axi_adc/iio_axi_adc.h \
+	$(NO-OS)/iio/iio_app/iio_app.h \
+	$(DRIVERS)/axi_core/iio_axi_dac/iio_axi_dac.h
+endif
+

commit fde295d6ec3751d75dbcd8d56a569a52eb5a8b7d

adrv902x: iio_example.c: Set DDS initial parameters
Set DDS initial parameters for the IIO example.

Fixes: ab393ac ("projects: adrv9025: Madura project")
Signed-off-by: George Mois <george.mois@analog.com>

projects/adrv902x/src/examples/iio_example/iio_example.c
@@ -324,6 +324,9 @@ int iio_example_main(void)
 		goto error_8;
 	}
 
+	// Set DDS data
+	axi_dac_data_setup(phy->tx_dac);
+
 	status = axi_dmac_init(&tx_dmac, &tx_dmac_init);
 	if (status) {
 		printf("axi_dmac_init tx init error: %d\n", status);