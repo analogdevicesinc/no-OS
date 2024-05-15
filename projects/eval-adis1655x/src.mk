d847c8773d6aed9d8c96dcd2ec730e2a364541b6
Ramona Gradinariu <ramona.gradinariu@analog.com>
2024-05-15 10:42:12 +0300

    projects: eval-adis1655x: Add example project for adis1655x
    Add example project for devices supported by adis1655x
    driver.
    
    Signed-off-by: Ramona Gradinariu <ramona.gradinariu@analog.com>

---
+++ projects/eval-adis1655x/src.mk
@@ -0,0 +1,51 @@
+include $(PROJECT)/src/platform/$(PLATFORM)/platform_src.mk
+include $(PROJECT)/src/examples/examples_src.mk
+
+SRCS += $(PROJECT)/src/platform/$(PLATFORM)/main.c
+
+INCS += $(PROJECT)/src/common/common_data.h
+SRCS += $(PROJECT)/src/common/common_data.c
+
+INCS += $(PROJECT)/src/platform/platform_includes.h
+
+INCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.h
+SRCS += $(PROJECT)/src/platform/$(PLATFORM)/parameters.c
+
+INCS += $(INCLUDE)/no_os_delay.h             \
+		$(INCLUDE)/no_os_error.h     \
+		$(INCLUDE)/no_os_gpio.h      \
+		$(INCLUDE)/no_os_i2c.h       \
+		$(INCLUDE)/no_os_print_log.h \
+		$(INCLUDE)/no_os_spi.h       \
+		$(INCLUDE)/no_os_irq.h       \
+		$(INCLUDE)/no_os_list.h      \
+		 $(INCLUDE)/no_os_dma.h      \
+		$(INCLUDE)/no_os_timer.h     \
+		$(INCLUDE)/no_os_uart.h      \
+		$(INCLUDE)/no_os_lf256fifo.h \
+		$(INCLUDE)/no_os_util.h      \
+		$(INCLUDE)/no_os_units.h     \
+		$(INCLUDE)/no_os_crc8.h      \
+		$(INCLUDE)/no_os_alloc.h     \
+        	$(INCLUDE)/no_os_mutex.h
+
+SRCS += $(DRIVERS)/api/no_os_gpio.c             \
+		$(DRIVERS)/api/no_os_i2c.c      \
+		$(NO-OS)/util/no_os_lf256fifo.c \
+		$(DRIVERS)/api/no_os_irq.c      \
+		$(DRIVERS)/api/no_os_spi.c      \
+		$(DRIVERS)/api/no_os_dma.c      \
+		$(DRIVERS)/api/no_os_timer.c    \
+		$(NO-OS)/util/no_os_list.c      \
+		$(DRIVERS)/api/no_os_uart.c     \
+		$(NO-OS)/util/no_os_crc8.c      \
+		$(NO-OS)/util/no_os_util.c      \
+		$(NO-OS)/util/no_os_alloc.c     \
+		$(NO-OS)/util/no_os_mutex.c
+
+INCS += $(DRIVERS)/imu/adis.h           \
+	$(DRIVERS)/imu/adis_internals.h \
+	$(DRIVERS)/imu/adis1655x.h
+SRCS += $(DRIVERS)/imu/adis.c           \
+	$(DRIVERS)/imu/adis1655x.c
+
---
+++ projects/eval-adis1655x/src/platform/pico/parameters.h
@@ -0,0 +1,96 @@
+/***************************************************************************//**
+ *   @file   parameters.h
+ *   @brief  Definitions specific to pico platform used by eval-adis1655x
+ *           project.
+ *   @author RBolboac (ramona.gradinariu@analog.com)
+********************************************************************************
+ * Copyright 2024(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#ifndef __PARAMETERS_H__
+#define __PARAMETERS_H__
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+
+#include "common_data.h"
+#include "no_os_util.h"
+#include "pico_uart.h"
+#include "pico_spi.h"
+#include "pico_gpio.h"
+#include "pico_gpio_irq.h"
+#include "pico_irq.h"
+#include "pico_timer.h"
+
+/******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+
+#define UART_DEVICE_ID  0
+#define UART_BAUDRATE   115200
+#define UART_IRQ_ID     20
+#define UART_EXTRA      &adis1655x_uart_extra_ip
+#define UART_OPS        &pico_uart_ops
+
+#define UART_TX_PIN     UART0_TX_GP0
+#define UART_RX_PIN     UART0_RX_GP1
+
+#define SPI_DEVICE_ID   0
+#define SPI_BAUDRATE    15000000
+#define SPI_CS          SPI0_CS_GP17
+#define SPI_OPS         &pico_spi_ops
+#define SPI_EXTRA       &adis1655x_spi_extra_ip
+
+extern struct pico_spi_init_param adis1655x_spi_extra_ip;
+extern struct pico_uart_init_param adis1655x_uart_extra_ip;
+
+#define GPIO_RESET_PIN_NUM   20
+#define GPIO_RESET_PORT_NUM  0
+#define GPIO_OPS            &pico_gpio_ops
+#define GPIO_EXTRA          NULL /* Not used for pico platform */
+
+#ifdef IIO_TRIGGER_EXAMPLE
+#define GPIO_DRDY_PIN_NUM   21
+#define GPIO_DRDY_PORT_NUM  0 /* Not used for pico platform */
+
+#define ADIS1655X_GPIO_TRIG_IRQ_ID     GPIO_DRDY_PIN_NUM
+#define ADIS1655X_GPIO_CB_HANDLE       NULL /* Not used in pico platform */
+
+#define GPIO_IRQ_ID             GPIO_DRDY_PIN_NUM
+#define GPIO_IRQ_OPS            &pico_gpio_irq_ops
+#define GPIO_IRQ_EXTRA          NULL /* Not used for pico platform */
+#endif
+
+#endif /* __PARAMETERS_H__ */
