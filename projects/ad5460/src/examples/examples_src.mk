027e7ceca64ce6fdc7c4e31c595c7aa79fdc354b
Antoniu Miclaus <antoniu.miclaus@analog.com>
2024-05-21 13:49:32 +0300

    projects: ad5460: add voltage output example
    Add application example for the voltage output of ad5460.
    
    Signed-off-by: Akila Marimuthu <akila.marimuthu@analog.com>
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- projects/ad5460/src/examples/examples_src.mk
+++ projects/ad5460/src/examples/examples_src.mk
@@ -10,5 +10,11 @@ SRCS += $(PROJECT)/src/examples/channel_output_example/channel_output_example.c
 INCS += $(PROJECT)/src/examples/channel_output_example/channel_output_example.h
 endif
 
+ifeq (y,$(strip $(VOLTAGE_OUTPUT_EXAMPLE)))
+CFLAGS += -DVOLTAGE_OUTPUT_EXAMPLE=1
+SRCS += $(PROJECT)/src/examples/voltage_output_example/voltage_output_example.c
+INCS += $(PROJECT)/src/examples/voltage_output_example/voltage_output_example.h
+endif
+
 INCS += $(INCLUDE)/no_os_list.h \
 	$(PLATFORM_DRIVERS)/$(PLATFORM)_uart.h
