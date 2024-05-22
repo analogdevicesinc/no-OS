0c90e44431b4a73b4e8d52abc0c101da76bf8efc
Darius Berghe <darius.berghe@analog.com>
2024-05-22 19:21:56 +0300

    stm32 usb serial implementation
    Use the STM32 HAL to create a no_os_uart device that initializes
    and manages a USB CDC connection instead of a normal UART.
    
    In order to use this feature:
    - USB CDC has to be enabled in the .ioc
    - build with NO_OS_USB_UART=y
    
    Signed-off-by: Darius Berghe <darius.berghe@analog.com>

---
+++ drivers/platform/stm32/stm32_usb_uart.h
@@ -0,0 +1,60 @@
+/***************************************************************************//**
+ *   @file   stm32/stm32_usb_uart.h
+ *   @brief  UART over USB driver for stm32 as a no_os_uart implementation.
+ *   @author Darius Berghe (darius.berghe@analog.com)
+********************************************************************************
+ * Copyright 2024(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef _STM32_USB_UART_H_
+#define _STM32_USB_UART_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "no_os_uart.h"
+#include "stm32_hal.h"
+
+/**
+ * @struct stm32_uart_desc
+ * @brief stm32 platform specific UART over USB descriptor.
+ */
+struct stm32_usb_uart_desc {
+	struct lf256_fifo *fifo;
+};
+
+/**
+ * @brief stm32 specific UART over USB platform ops structure.
+ */
+extern const struct no_os_uart_platform_ops stm32_usb_uart_ops;
+
+#endif
