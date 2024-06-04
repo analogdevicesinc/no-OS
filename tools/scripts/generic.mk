analogdevicesinc/no-OS

commit a096809c02d1ba2fe862b8759860be26c248b068

tools: scripts: Delete .vscode on reset
After 86d407b("tools:scripts: Don't overwrite .vscode config if it already
exists") the vscode configuration files (contents of the .vscode
directory) are no longer overwritten on every make build. This may
lead to confusion if the build was accidentally done for a different
platform than the one intended.

Such a situation may occur if the user runs "make build" (without the
PLATFORM variable), resulting in generating the vscode configurations
for the STM32 platform. From this point there is no way to change the
target to another platform using the build system. Fix this by removing
the .vscode directory on "make reset".

Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

tools/scripts/generic.mk
@@ -400,7 +400,7 @@ clean:
 
 # Remove the whole build directory
 PHONY += reset
-reset: clean_libs
+reset: clean_libs sdkclean
 	$(call print,[Delete] $(BUILD_DIR))
 	$(call remove_dir,$(BUILD_DIR))
 

tools/scripts/maxim.mk
@@ -153,6 +153,7 @@ $(PLATFORM)_sdkopen:
 	code $(PROJECT)
 
 $(PLATFORM)_sdkclean: clean
+	$(call remove_dir,$(VSCODE_CFG_DIR))
 
 $(PLATFORM)_sdkbuild: build
 

tools/scripts/stm32.mk
@@ -214,6 +214,7 @@ $(PLATFORM)_sdkclean:
 	$(call remove_dir,$(DEBUG_DIR)) $(HIDE)
 	$(call print,[Delete] SDK artefacts from $(RELEASE_DIR))
 	$(call remove_dir,$(RELEASE_DIR)) $(HIDE)
+	$(call remove_dir,$(VSCODE_CFG_DIR))
 
 clean_hex:
 	@$(call print,[Delete] $(HEX))

commit 0372fe9c6c1a86adccf75d96a1fcf4b58023b2c4

tools: scripts: Delete .vscode in $(PLATFORM)_reset
$(PLATFORM)_sdkclean is not defined for some platforms, so commit
a096809 ("tools: scripts: Delete .vscode on reset") broke the reset
target for them. Delete the .vscode directory in the platform specific
reset target instead of sdkclean.

Fixes: 86d407b ("tools: scripts: Delete .vscode on reset")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

tools/scripts/generic.mk
@@ -400,7 +400,7 @@ clean:
 
 # Remove the whole build directory
 PHONY += reset
-reset: clean_libs sdkclean $(PLATFORM)_reset
+reset: clean_libs $(PLATFORM)_reset
 	$(call print,[Delete] $(BUILD_DIR))
 	$(call remove_dir,$(BUILD_DIR))
 

tools/scripts/maxim.mk
@@ -153,9 +153,9 @@ $(PLATFORM)_sdkopen:
 	code $(PROJECT)
 
 $(PLATFORM)_sdkclean: clean
-	$(call remove_dir,$(VSCODE_CFG_DIR))
 
 $(PLATFORM)_reset:
+	$(call remove_dir,$(VSCODE_CFG_DIR))
 
 $(PLATFORM)_sdkbuild: build
 

tools/scripts/stm32.mk
@@ -225,7 +225,6 @@ $(PLATFORM)_sdkclean:
 	$(call remove_dir,$(DEBUG_DIR)) $(HIDE)
 	$(call print,[Delete] SDK artefacts from $(RELEASE_DIR))
 	$(call remove_dir,$(RELEASE_DIR)) $(HIDE)
-	$(call remove_dir,$(VSCODE_CFG_DIR))
 
 clean_hex:
 	@$(call print,[Delete] $(HEX))
@@ -245,3 +244,4 @@ debug: all $(BINARY).openocd $(BINARY).gdb
 	$(GDB) --command=$(BINARY).gdb
 
 $(PLATFORM)_reset:
+	$(call remove_dir,$(VSCODE_CFG_DIR))