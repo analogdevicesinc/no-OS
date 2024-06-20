74024a023235895457d2d11a40d6f7660713774a
Ciprian Regus <ciprian.regus@analog.com>
2024-06-20 10:01:41 +0300

    projects: cn0583: Include maxim_dma.* files
    The Maxim SPI driver requires for the DMA to also be compiled. This is
    not an ideal solution, but work around it while a better fix is applied.
    
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

--- projects/cn0583/src.mk
+++ projects/cn0583/src.mk
@@ -27,6 +27,7 @@ SRCS += $(PLATFORM_DRIVERS)/maxim_uart.c 			\
 	$(PLATFORM_DRIVERS)/maxim_uart_stdio.c			\
 	$(PLATFORM_DRIVERS)/maxim_gpio.c			\
 	$(PLATFORM_DRIVERS)/maxim_irq.c				\
+	$(PLATFORM_DRIVERS)/../common/maxim_dma.c		\
 	$(PLATFORM_DRIVERS)/maxim_gpio_irq.c			\
 	$(PLATFORM_DRIVERS)/maxim_rtc.c				\
 	$(PLATFORM_DRIVERS)/maxim_spi.c				\
@@ -37,6 +38,7 @@ INCS += $(PLATFORM_DRIVERS)/maxim_uart.h			\
 	$(PLATFORM_DRIVERS)/maxim_gpio.h			\
 	$(PLATFORM_DRIVERS)/maxim_uart_stdio.h			\
 	$(PLATFORM_DRIVERS)/maxim_irq.h				\
+	$(PLATFORM_DRIVERS)/../common/maxim_dma.h		\
 	$(PLATFORM_DRIVERS)/maxim_init.h			\
 	$(PLATFORM_DRIVERS)/maxim_gpio_irq.h			\
 	$(PLATFORM_DRIVERS)/maxim_spi.h				\
