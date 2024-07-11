analogdevicesinc/no-OS

commit c24331264729012d23e379dfd29412500f4a2fbc

drivers: axi_core: Fix offload xfer size
spi_engine_offload_transfer should xfer number of samples,
not number of bytes.

Drivers are sometimes sending number of bytes, causing
a buffer over run. because the dma buffer is cyclic,
the overrun migh not be visible with a simple sine wave
or DC input as feed signal to the part.

The bytes that are transfered on dma per sample depend
on the DMA_DATA_WIDTH_SRC of the dma IP not on the data_width
bytes sent by the spi_engine on the spi bus.

Example:
for a 24 bit part (data_width of 3) if spi_engine_offload_transfer
asks for 100 samples, only 75 are done.

This is because 3 * 1 * 100 = 300 bytes,
but since the dma core is doing 4 byte xfers and the 4th byte
is counted too:
  300/4 = 75 total samples.

Fix this by using the acutal src size of the the dma ip to
calculate the number of bytes needed to complete n samples.

Signed-off-by: Axel Haslam <ahaslam@baylibre.com>

drivers/axi_core/spi_engine/spi_engine.c
@@ -844,12 +844,10 @@ int32_t spi_engine_offload_transfer(struct no_os_spi_desc *desc,
 
 	/* Start transfer */
 	spi_engine_write(eng_desc, SPI_ENGINE_REG_OFFLOAD_CTRL(0), 0x0001);
-
-	word_length = spi_get_word_lenght(eng_desc);
 	if(eng_desc->offload_config & OFFLOAD_TX_EN) {
 		struct axi_dma_transfer tx_transfer = {
 			// Number of bytes to write/read
-			.size = word_length * eng_desc->offload_tx_len * no_samples,
+			.size = eng_desc->offload_tx_dma->width_src * eng_desc->offload_tx_len * no_samples,
 			// Transfer done flag
 			.transfer_done = 0,
 			// Signal transfer mode
@@ -865,7 +863,7 @@ int32_t spi_engine_offload_transfer(struct no_os_spi_desc *desc,
 	if(eng_desc->offload_config & OFFLOAD_RX_EN) {
 		struct axi_dma_transfer rx_transfer = {
 			// Number of bytes to write/read
-			.size = word_length * eng_desc->offload_tx_len * no_samples,
+			.size = 4 * eng_desc->offload_tx_len * no_samples,
 			// Transfer done flag
 			.transfer_done = 0,
 			// Signal transfer mode

drivers/axi_core/axi_dmac/axi_dmac.c
@@ -285,9 +285,12 @@ static int32_t axi_dmac_detect_caps(struct axi_dmac *dmac)
 	uint32_t reg_val, initial_reg_val = 0;
 	uint32_t src_mem_mapped = 0;
 	uint32_t dest_mem_mapped = 0;
+	uint32_t intf_desc = 0;
 
 	dmac->max_length = -1;
 	dmac->direction = INVALID_DIR;
+	dmac->width_dst = -1;
+	dmac->width_src = -1;
 
 	/* Check if HW cyclic possible */
 	axi_dmac_read(dmac, AXI_DMAC_REG_FLAGS, &initial_reg_val);
@@ -307,6 +310,11 @@ static int32_t axi_dmac_detect_caps(struct axi_dmac *dmac)
 	axi_dmac_read(dmac, AXI_DMAC_REG_DEST_ADDRESS, &dest_mem_mapped);
 	axi_dmac_write(dmac, AXI_DMAC_REG_SRC_ADDRESS, 0xffffffff);
 	axi_dmac_read(dmac, AXI_DMAC_REG_SRC_ADDRESS, &src_mem_mapped);
+	axi_dmac_read(dmac, AXI_DMAC_REG_INTF_DESC, &intf_desc);
+	dmac->width_dst = no_os_field_get(AXI_DMAC_DMA_BPB_DEST, intf_desc);
+	dmac->width_dst = (1 << dmac->width_dst);
+	dmac->width_src = no_os_field_get(AXI_DMAC_DMA_BPB_SRC, intf_desc);
+	dmac->width_src = (1 << dmac->width_src);
 
 	if (dest_mem_mapped && !src_mem_mapped) {
 		dmac->direction = DMA_DEV_TO_MEM;

drivers/axi_core/axi_dmac/axi_dmac.h
@@ -54,7 +54,9 @@
 #define AXI_DMAC_IRQ_EOT			NO_OS_BIT(1)
 
 #define AXI_DMAC_REG_INTF_DESC		0x010
+#define AXI_DMAC_DMA_BPB_DEST		NO_OS_GENMASK(3,0)
 #define AXI_DMAC_DMA_TYPE_DEST		NO_OS_GENMASK(5,4)
+#define AXI_DMAC_DMA_BPB_SRC		NO_OS_GENMASK(11,8)
 #define AXI_DMAC_DMA_TYPE_SRC		NO_OS_GENMASK(13,12)
 //Define macro for src and dest
 
@@ -118,6 +120,8 @@ struct axi_dmac {
 	enum dma_direction direction;
 	bool hw_cyclic;
 	uint32_t max_length;
+	uint32_t width_dst;
+	uint32_t width_src;
 	volatile struct axi_dma_transfer transfer;
 	//Current sub-transfer properties
 	uint32_t init_addr;

commit bf3201ce5fe110ba5329e97148d810de862691a7

drivers: spi-engine: remove unused variable
During commit c24331264 ("drivers: axi_core: Fix offload xfer size") this
was omitted (for removal).

Fixes: c24331264 ("drivers: axi_core: Fix offload xfer size")
Signed-off-by: Alexandru Ardelean <aardelean@baylibre.com>

drivers/axi_core/spi_engine/spi_engine.c
@@ -811,7 +811,6 @@ int32_t spi_engine_offload_transfer(struct no_os_spi_desc *desc,
 	struct spi_engine_msg	transfer;
 	struct spi_engine_desc	*eng_desc;
 	uint32_t 		i;
-	uint8_t 		word_length;
 
 	eng_desc = desc->extra;
 