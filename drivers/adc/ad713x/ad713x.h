9623300c3ff3e521f87496215a1fc10633554c01
Pop Ioan Daniel <pop.ioan-daniel@analog.com>
2024-07-22 09:42:16 +0300

    ad7134: Add multidevice synchronization feature
    Signed-off-by: Pop Ioan Daniel <pop.ioan-daniel@analog.com>

--- drivers/adc/ad713x/ad713x.h
+++ drivers/adc/ad713x/ad713x.h
@@ -696,6 +696,8 @@ struct ad713x_dev {
 	struct no_os_gpio_desc		*gpio_resetn;
 	/** PDN GPIO handler. */
 	struct no_os_gpio_desc		*gpio_pnd;
+	/** CS Sync */
+	struct no_os_spi_desc *gpio_cs_sync;
 	/** ID of supported device. */
 	enum ad713x_supported_dev_ids dev_id;
 	/** Length of data in bits. */
@@ -723,6 +725,8 @@ struct ad713x_init_param {
 	struct no_os_gpio_init_param *gpio_resetn;
 	/** PDN GPIO initialization structure. */
 	struct no_os_gpio_init_param *gpio_pnd;
+	/** CS_GPIO */
+	struct no_os_gpio_init_param *gpio_cs_sync;
 	/** MODE GPIO starting value */
 	bool mode_master_nslave;
 	/** DCLKMODE GPIO starting value */
@@ -801,5 +805,8 @@ int32_t ad713x_remove(struct ad713x_dev *dev);
 
 /** Print reg dump for the specific reg map of AD4134 dev*/
 int32_t ad713x_spi_reg_dump(struct ad713x_dev *dev);
+
+/** Multidevice synchronization */
+int32_t ad713x_channel_sync(struct ad713x_dev *dev);
 
 #endif /* SRC_AD713X_H_ */
