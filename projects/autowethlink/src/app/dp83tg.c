87f4cb11bc4b1ecf18ccf6052156f49d9005d13b
Darius Berghe <darius.berghe@analog.com>
2024-08-02 14:19:15 +0300

    sw reset

--- projects/autowethlink/src/app/dp83tg.c
+++ projects/autowethlink/src/app/dp83tg.c
@@ -90,13 +90,13 @@ int dp83tg_remove(struct dp83tg_desc *dev)
 int dp83tg_soft_reset(struct dp83tg_desc *dev)
 {
 	int ret;
-	// TODO
-	// ret = dp83tg_write_bits(dev, DP83TG_GPIOCR1, DP83TG_RST_MASK,
-	// 			  DP83TG_RST_MASK);
-	// if (ret < 0)
-	// 	return ret;
 
-	// no_os_mdelay(1);
+	ret = dp83tg_write_bits(dev, DP83TG_MII_REG_1F, DP83TG_SW_GLOBAL_RESET_MASK,
+				  DP83TG_SW_GLOBAL_RESET_MASK);
+	if (ret < 0)
+		return ret;
+
+	no_os_mdelay(1);
 
 	return 0;
 }
@@ -108,7 +108,7 @@ int dp83tg_hard_reset(struct dp83tg_desc *dev)
 	ret = no_os_gpio_direction_output(dev->reset, NO_OS_GPIO_LOW);
 	if (ret)
 		return ret;
-	no_os_udelay(100);
+	no_os_udelay(5);
 	ret = no_os_gpio_direction_output(dev->reset, NO_OS_GPIO_HIGH);
 	if (ret)
 		return ret;
--- projects/autowethlink/src/app/dp83tg.h
+++ projects/autowethlink/src/app/dp83tg.h
@@ -5,8 +5,12 @@
 #include "no_os_util.h"
 #include "no_os_mdio.h"
 
-#define DP83TG_PHY_ID_1		0x2
-#define DP83TG_PHY_ID_2		0x3
+#define DP83TG_PHY_ID_1				0x2
+#define DP83TG_PHY_ID_2				0x3
+
+#define DP83TG_MII_REG_1F			0x1f
+#define DP83TG_SW_GLOBAL_RESET_MASK	NO_OS_BIT(15)
+#define DP83TG_DIGITAL_RESET_MASK	NO_OS_BIT(14)
 
 struct dp83tg_init_param {
 	struct no_os_gpio_init_param *reset;
@@ -21,5 +25,15 @@ struct dp83tg_desc {
 int dp83tg_init(struct dp83tg_desc **dev,
 		  struct dp83tg_init_param *param);
 int dp83tg_remove(struct dp83tg_desc *dev);
+
+int dp83tg_write(struct dp83tg_desc *dev, uint32_t addr,
+			  uint16_t val);
+int dp83tg_read(struct dp83tg_desc *dev, uint32_t addr,
+			 uint16_t *val);
+int dp83tg_write_bits(struct dp83tg_desc *dev, uint8_t addr, uint16_t val,
+			uint16_t bitmask);
+
+int dp83tg_soft_reset(struct dp83tg_desc *dev);
+int dp83tg_hard_reset(struct dp83tg_desc *dev);
 
 #endif
