analogdevicesinc/no-OS

commit e3d5d7158b68fd96027a17d142efd5990fa6ffcf

ad7606: Added support to 16-bit version of AD7606C
Signed-off-by: Darius Berghe <darius.berghe@analog.com>

drivers/adc/ad7606/ad7606.c
@@ -156,7 +156,19 @@ static const struct ad7606_chip_info ad7606_chip_info_tbl[] = {
 		.sw_range_table = ad7606b_range_table,
 		.sw_range_table_sz = ARRAY_SIZE(ad7606b_range_table),
 	},
-	[ID_AD7606C] = {
+	[ID_AD7606C_16] = {
+		.num_channels = 8,
+		.bits = 16,
+		.max_dout_lines = AD7606_8_DOUT,
+		.has_oversampling = true,
+		.has_registers = true,
+		.device_id = 0x3,
+		.hw_range_table = ad7606_range_table,
+		.hw_range_table_sz = ARRAY_SIZE(ad7606_range_table),
+		.sw_range_table = ad7606c_range_table,
+		.sw_range_table_sz = ARRAY_SIZE(ad7606c_range_table),
+	},
+	[ID_AD7606C_18] = {
 		.num_channels = 8,
 		.bits = 18,
 		.max_dout_lines = AD7606_8_DOUT,

drivers/adc/ad7606/ad7606.h
@@ -136,8 +136,10 @@ enum ad7606_device_id {
 	ID_AD7606_8,
 	/** 8-Channel DAS with 16-Bit, 800 kSPS, Bipolar Input, Simultaneous Sampling ADC */
 	ID_AD7606B,
+	/** 8-Channel DAS with 16-Bit, 1 MSPS, Bipolar Input, Simultaneous Sampling ADC */
+	ID_AD7606C_16,
 	/** 8-Channel DAS with 18-Bit, 1 MSPS, Bipolar Input, Simultaneous Sampling ADC */
-	ID_AD7606C,
+	ID_AD7606C_18,
 	/** 8-Channel DAS with 18-Bit, Bipolar, Simultaneous Sampling ADC */
 	ID_AD7608,
 	/** 8-Channel Differential DAS with 18-Bit, Bipolar, Simultaneous Sampling ADC */

commit 575cbae3a085fd08cedfdf9eceef2eac702d723b

adc: ad7606: fix device id for ad7606c-16
The value stated in the datasheet is wrong. Based on testing and
information provided via community forum the device id for ad7606c-16 is
0x2.

Fixes: e3d5d71 ("ad7606: Added support to 16-bit version of AD7606C")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad7606/ad7606.c
@@ -170,7 +170,7 @@ static const struct ad7606_chip_info ad7606_chip_info_tbl[] = {
 		.max_dout_lines = AD7606_8_DOUT,
 		.has_oversampling = true,
 		.has_registers = true,
-		.device_id = 0x3,
+		.device_id = 0x2,
 		.hw_range_table = ad7606_range_table,
 		.hw_range_table_sz = NO_OS_ARRAY_SIZE(ad7606_range_table),
 		.sw_range_table = ad7606c_range_table,