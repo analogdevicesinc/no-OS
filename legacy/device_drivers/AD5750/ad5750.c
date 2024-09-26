548a6db34980793f1f9cf43a2a901b346d52a6d1
Dragos Bogdan <dragos.bogdan@analog.com>
2024-09-26 11:30:11 +0300

    ADI BSD -> 3-Clause BSD
    Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

--- legacy/device_drivers/AD5750/ad5750.c
+++ legacy/device_drivers/AD5750/ad5750.c
@@ -6,37 +6,30 @@
 *******************************************************************************
 * Copyright 2013(c) Analog Devices, Inc.
 *
-* All rights reserved.
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
 *
-* Redistribution and use in source and binary forms, with or without modification,
-* are permitted provided that the following conditions are met:
-*  - Redistributions of source code must retain the above copyright
-*    notice, this list of conditions and the following disclaimer.
-*  - Redistributions in binary form must reproduce the above copyright
-*    notice, this list of conditions and the following disclaimer in
-*    the documentation and/or other materials provided with the
-*    distribution.
-*  - Neither the name of Analog Devices, Inc. nor the names of its
-*    contributors may be used to endorse or promote products derived
-*    from this software without specific prior written permission.
-*  - The use of this software may or may not infringe the patent rights
-*    of one or more patent holders.  This license does not release you
-*    from the requirement that you obtain separate licenses from these
-*    patent holders to use this software.
-*  - Use of the software either in source or binary form, must be run
-*    on or directly connected to an Analog Devices Inc. component.
+* 1. Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
 *
-* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR IMPLIED
-* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT, MERCHANTABILITY
-* AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-* IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-* INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
 *
+* 3. Neither the name of Analog Devices, Inc. nor the names of its
+*    contributors may be used to endorse or promote products derived from this
+*    software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. ‚ÄúAS IS‚Äù AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+* EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+* OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
 
 /******************************************************************************/
@@ -48,21 +41,21 @@ const unsigned char RANGE_CONFIG[22]  = {0x05, // 0 V to 5 V.
                                   0x09, // 0 V to 6.0 V (20% overrange).
                                   0x06, // 0 V to 10 V.
                                   0x0A, // 0 V to 12.0 V (20% overrange).
-                                  0x0D, // ±2.5 V.
-                                  0x07, // ±5 V.
-                                  0x0B, // ±6.0 V (20% overrange).
-                                  0x08, // ±10 V.
-                                  0x0C, // ±12.0 V (20% overrange).
+                                  0x0D, // ÔøΩ2.5 V.
+                                  0x07, // ÔøΩ5 V.
+                                  0x0B, // ÔøΩ6.0 V (20% overrange).
+                                  0x08, // ÔøΩ10 V.
+                                  0x0C, // ÔøΩ12.0 V (20% overrange).
                                   0x10, // 4 mA to 20 mA (internal current sense resistor).
                                   0x00, // 4 mA to 20 mA (external 15 k current sense resistor).
                                   0x11, // 0 mA to 20 mA (internal current sense resistor).
                                   0x01, // 0 mA to 20 mA (external 15 k current sense resistor).
                                   0x12, // 0 mA to 24 mA (internal current sense resistor).
                                   0x02, // 0 mA to 24 mA (external 15 k current sense resistor).
-                                  0x13, // ±20 mA (internal current sense resistor).
-                                  0x03, // ±20 mA (external 15 k current sense resistor).
-                                  0x14, // ±24 mA (internal current sense resistor).
-                                  0x04, // ±24 mA (external 15 k current sense resistor).
+                                  0x13, // ÔøΩ20 mA (internal current sense resistor).
+                                  0x03, // ÔøΩ20 mA (external 15 k current sense resistor).
+                                  0x14, // ÔøΩ24 mA (internal current sense resistor).
+                                  0x04, // ÔøΩ24 mA (external 15 k current sense resistor).
                                   0x1D, // 3.92 mA to 20.4 mA (internal current sense resistor).
                                   0x1E, // 0 mA to 20.4 mA (internal current sense resistor).
                                   0x1F};// 0 mA to 24.5 mA (internal current sense resistor).
