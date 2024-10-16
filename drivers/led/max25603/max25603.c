42b88c45240bc8982e122088bdae7e6ad8d0cb68
Ciprian Regus <ciprian.regus@analog.com>
2024-10-16 12:30:06 +0300

    max25603: Add PWM for DRL and turn indicators
    The standard application using the MAX25603 for headlamp control,
    involves PWM signals for low/high beam, day time running light and turn
    indicators. Include the last 2 and refactor the code wherever needed to
    reduce repetition.
    
    It's worth noting the PWM signals other than EN1 and EN2 are not directly
    controlled by the MAX25603, thus would also make sense to not be part of
    the driver at all.
    
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

--- drivers/led/max25603/max25603.c
+++ drivers/led/max25603/max25603.c
@@ -136,7 +136,9 @@ int max25603_set_enable(struct max25603_desc *desc, enum max25603_enable enable,
 int max25603_set_beam(struct max25603_desc *desc, enum max25603_beam beam,
 		      uint32_t freq_hz, uint8_t duty)
 {
+	struct no_os_pwm_desc *beam_pwm;
 	uint32_t period_ns;
+	bool enable = true;
 	int ret;
 
 	if (duty > 100)
@@ -146,62 +148,77 @@ int max25603_set_beam(struct max25603_desc *desc, enum max25603_beam beam,
 
 	switch (beam) {
 	case MAX25603_DISABLE_BEAM:
-		ret = max25603_set_beam(desc, MAX25603_LOW_BEAM,
-					NO_OS_DIV_ROUND_CLOSEST_ULL(NANO, desc->lb_desc->period_ns), 100);
-		if (ret)
-			return ret;
-
-		ret = max25603_set_beam(desc, MAX25603_HIGH_BEAM,
-					NO_OS_DIV_ROUND_CLOSEST_ULL(NANO, desc->hb_desc->period_ns), 100);
-		if (ret)
-			return ret;
+		enable = false;
 
 		break;
 	case MAX25603_HIGH_BEAM:
-		if (!duty)
-			return no_os_pwm_set_duty_cycle(desc->hb_desc, desc->hb_desc->period_ns);
-
-		ret = no_os_pwm_disable(desc->hb_desc);
-		if (ret)
-			return ret;
-
-		ret = no_os_pwm_set_period(desc->hb_desc, period_ns);
-		if (ret)
-			return ret;
-
-		ret = no_os_pwm_set_duty_cycle(desc->hb_desc, (period_ns * (100 - duty)) / 100);
-		if (ret)
-			return ret;
-
-		ret = no_os_pwm_enable(desc->hb_desc);
-		if (ret)
-			return ret;
+		beam_pwm = desc->hb_desc;
 
 		break;
 	case MAX25603_LOW_BEAM:
-		if (!duty)
-			return no_os_pwm_set_duty_cycle(desc->lb_desc, desc->lb_desc->period_ns);
+		beam_pwm = desc->lb_desc;
 
-		ret = no_os_pwm_disable(desc->lb_desc);
-		if (ret)
-			return ret;
+		break;
+	case MAX25603_DRL_BEAM:
+		beam_pwm = desc->drl_desc;
 
-		ret = no_os_pwm_set_period(desc->lb_desc, period_ns);
-		if (ret)
-			return ret;
-
-		ret = no_os_pwm_set_duty_cycle(desc->lb_desc, (period_ns * (100 - duty)) / 100);
-		if (ret)
-			return ret;
-
-		ret = no_os_pwm_enable(desc->lb_desc);
-		if (ret)
-			return ret;
+		break;
+	case MAX25603_TURN_BEAM:
+		beam_pwm = desc->turn_desc;
 
 		break;
 	default:
 		return -EINVAL;
 	}
+
+	if (!enable) {
+		if (desc->lb_desc) {
+			ret = no_os_pwm_disable(desc->lb_desc);
+			if (ret)
+				return ret;
+		}
+
+		if (desc->hb_desc) {
+			ret = no_os_pwm_disable(desc->hb_desc);
+			if (ret)
+				return ret;
+		}
+
+		if (desc->drl_desc) {
+			ret = no_os_pwm_disable(desc->drl_desc);
+			if (ret)
+				return ret;
+		}
+
+		if (desc->turn_desc) {
+			ret = no_os_pwm_disable(desc->turn_desc);
+			if (ret)
+				return ret;
+		}
+
+		desc->beam = beam;
+
+		return 0;
+	}
+
+	if (!duty)
+		return no_os_pwm_set_duty_cycle(beam_pwm, beam_pwm->period_ns);
+
+	ret = no_os_pwm_disable(beam_pwm);
+	if (ret)
+		return ret;
+
+	ret = no_os_pwm_set_period(beam_pwm, period_ns);
+	if (ret)
+		return ret;
+
+	ret = no_os_pwm_set_duty_cycle(beam_pwm, (period_ns * duty) / 100);
+	if (ret)
+		return ret;
+
+	ret = no_os_pwm_enable(beam_pwm);
+	if (ret)
+		return ret;
 
 	desc->beam = beam;
 
@@ -241,18 +258,26 @@ int max25603_init(struct max25603_desc **desc,
 	if (ret)
 		goto hb_err;
 
-	ret = max25603_set_enable(descriptor, MAX25603_DISABLE_EN, 0, 0);
+	ret = no_os_pwm_init(&descriptor->drl_desc, init_param->drl_param);
 	if (ret)
 		goto lb_err;
 
+	ret = no_os_pwm_init(&descriptor->turn_desc, init_param->turn_param);
+	if (ret)
+		goto drl_err;
+
+	ret = max25603_set_enable(descriptor, MAX25603_DISABLE_EN, 0, 0);
+	if (ret)
+		goto turn_err;
+
 	ret = max25603_set_beam(descriptor, MAX25603_DISABLE_BEAM, 0, 0);
 	if (ret)
-		goto hb_err;
+		goto turn_err;
 
 	ret = no_os_gpio_get_optional(&descriptor->shunt_desc,
 				      init_param->shunt_param);
 	if (ret)
-		goto hb_err;
+		goto turn_err;
 
 	if (descriptor->shunt_desc) {
 		ret = no_os_gpio_direction_output(descriptor->shunt_desc,
@@ -282,6 +307,10 @@ flt_err:
 	no_os_gpio_remove(descriptor->flt_desc);
 shunt_err:
 	no_os_gpio_remove(descriptor->shunt_desc);
+turn_err:
+	no_os_pwm_remove(descriptor->turn_desc);
+drl_err:
+	no_os_pwm_remove(descriptor->drl_desc);
 lb_err:
 	no_os_pwm_remove(descriptor->lb_desc);
 hb_err:
@@ -310,6 +339,8 @@ int max25603_remove(struct max25603_desc *desc)
 	no_os_gpio_remove(desc->shunt_desc);
 	no_os_pwm_remove(desc->lb_desc);
 	no_os_pwm_remove(desc->hb_desc);
+	no_os_pwm_remove(desc->drl_desc);
+	no_os_pwm_remove(desc->turn_desc);
 	no_os_pwm_remove(desc->en2_desc);
 	no_os_pwm_remove(desc->en1_desc);
 	no_os_free(desc);
