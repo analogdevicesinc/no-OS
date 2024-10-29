7878949d905c2399c351ca090af7c43a9e3a93d0
Ciprian Regus <ciprian.regus@analog.com>
2024-10-29 11:45:38 +0200

    do not use the ADPD1080
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

--- projects/sps/src/main.c
+++ projects/sps/src/main.c
@@ -399,11 +399,11 @@ int app_step(void *arg)
 	static int cnt2 = 0;
 	static int cnt3 = 0;
 
-	cnt1++;
-	if (cnt1 == 100) {
-		cnt1 = 0;
-		sps_gesture_detection(arg);
-	}
+	// cnt1++;
+	// if (cnt1 == 100) {
+	// 	cnt1 = 0;
+	// 	sps_gesture_detection(arg);
+	// }
 
 	cnt2++;
 	if (cnt2 == 1000) {
@@ -558,182 +558,182 @@ int main(void)
 	status = ad7799_read_channel(ad7793_iiodev->ad7793_desc, 1, &reg_val);
 	status = ad7799_read_channel(ad7793_iiodev->ad7793_desc, 1, &reg_val);
 
-	status = adpd188_iio_init(&adpd1080_iio_device, &adpd1080_iio_inital);
-	if (status < 0)
-		return status;
+	// status = adpd188_iio_init(&adpd1080_iio_device, &adpd1080_iio_inital);
+	// if (status < 0)
+	// 	return status;
 
-	status = adpd188_adc_fsample_set(adpd1080_iio_device->drv_dev, 16);
-	if(status != 0)
-		return -1;
+	// status = adpd188_adc_fsample_set(adpd1080_iio_device->drv_dev, 16);
+	// if(status != 0)
+	// 	return -1;
 
-	status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_SLOT_EN,
-				  &reg_data);
-	if(status != 0)
-		return -1;
-	reg_data |= ADPD188_SLOT_EN_RDOUT_MODE_MASK |
-		    ADPD188_SLOT_EN_FIFO_OVRN_PREVENT_MASK;
-	status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_SLOT_EN,
-				   reg_data);
-	if(status != 0)
-		return -1;
+	// status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_SLOT_EN,
+	// 			  &reg_data);
+	// if(status != 0)
+	// 	return -1;
+	// reg_data |= ADPD188_SLOT_EN_RDOUT_MODE_MASK |
+	// 	    ADPD188_SLOT_EN_FIFO_OVRN_PREVENT_MASK;
+	// status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_SLOT_EN,
+	// 			   reg_data);
+	// if(status != 0)
+	// 	return -1;
 
-	/* Slot A chop mode is inverted, non-inverted, non-inverted, inverted */
-	status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_A,
-				  &reg_data);
-	if(status != 0)
-		return -1;
-	reg_data |= 0x9 & ADPD188_INT_SEQ_A_INTEG_ORDER_A_MASK;
-	status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_A,
-				   reg_data);
-	if(status != 0)
-		return -1;
-	/* Slot B chop mode is inverted, non-inverted, non-inverted, inverted */
-	status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_B,
-				  &reg_data);
-	if(status != 0)
-		return -1;
-	reg_data |= 0x9 & ADPD188_INT_SEQ_B_INTEG_ORDER_B_MASK;
-	status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_B,
-				   reg_data);
-	if(status != 0)
-		return -1;
+	// /* Slot A chop mode is inverted, non-inverted, non-inverted, inverted */
+	// status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_A,
+	// 			  &reg_data);
+	// if(status != 0)
+	// 	return -1;
+	// reg_data |= 0x9 & ADPD188_INT_SEQ_A_INTEG_ORDER_A_MASK;
+	// status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_A,
+	// 			   reg_data);
+	// if(status != 0)
+	// 	return -1;
+	// /* Slot B chop mode is inverted, non-inverted, non-inverted, inverted */
+	// status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_B,
+	// 			  &reg_data);
+	// if(status != 0)
+	// 	return -1;
+	// reg_data |= 0x9 & ADPD188_INT_SEQ_B_INTEG_ORDER_B_MASK;
+	// status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_INT_SEQ_B,
+	// 			   reg_data);
+	// if(status != 0)
+	// 	return -1;
 
-	/* Set blue LED 1 power */
-	status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
-				  ADPD188_REG_ILED1_COARSE, &reg_data);
-	if(status != 0)
-		return -1;
-	reg_data &= ~ADPD188_ILED1_COARSE_ILED1_COARSE_MASK;
-	reg_data |= (0xf << ADPD188_ILED1_COARSE_ILED1_COARSE_POS) &
-		    ADPD188_ILED1_COARSE_ILED1_COARSE_MASK;
-	reg_data &= ~ADPD188_ILED1_COARSE_ILED1_SLEW_MASK;
-	reg_data |= (0x5 << ADPD188_ILED1_COARSE_ILED1_SLEW_POS) &
-		    ADPD188_ILED1_COARSE_ILED1_SLEW_MASK;
-	reg_data |= ADPD188_ILED1_COARSE_ILED1_SCALE_MASK;
-	status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
-				   ADPD188_REG_ILED1_COARSE, reg_data);
-	if(status != 0)
-		return -1;
+	// /* Set blue LED 1 power */
+	// status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
+	// 			  ADPD188_REG_ILED1_COARSE, &reg_data);
+	// if(status != 0)
+	// 	return -1;
+	// reg_data &= ~ADPD188_ILED1_COARSE_ILED1_COARSE_MASK;
+	// reg_data |= (0xf << ADPD188_ILED1_COARSE_ILED1_COARSE_POS) &
+	// 	    ADPD188_ILED1_COARSE_ILED1_COARSE_MASK;
+	// reg_data &= ~ADPD188_ILED1_COARSE_ILED1_SLEW_MASK;
+	// reg_data |= (0x5 << ADPD188_ILED1_COARSE_ILED1_SLEW_POS) &
+	// 	    ADPD188_ILED1_COARSE_ILED1_SLEW_MASK;
+	// reg_data |= ADPD188_ILED1_COARSE_ILED1_SCALE_MASK;
+	// status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
+	// 			   ADPD188_REG_ILED1_COARSE, reg_data);
+	// if(status != 0)
+	// 	return -1;
 
-	/* Slot A 4 LED pulses with 15us period */
-	status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
-				  ADPD188_REG_SLOTA_NUMPULSES, &reg_data);
-	if(status != 0)
-		return -1;
-	reg_data &= ~ADPD188_SLOTA_NUMPULSES_SLOTA_PULSES_MASK;
-	reg_data |= (0x4 << ADPD188_SLOTA_NUMPULSES_SLOTA_PULSES_POS) &
-		    ADPD188_SLOTA_NUMPULSES_SLOTA_PULSES_MASK;
-	reg_data &= ~ADPD188_SLOTA_NUMPULSES_SLOTA_PERIOD_MASK;
-	reg_data |= (0xE << ADPD188_SLOTA_NUMPULSES_SLOTA_PERIOD_POS) &
-		    ADPD188_SLOTA_NUMPULSES_SLOTA_PERIOD_MASK;
-	status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
-				   ADPD188_REG_SLOTA_NUMPULSES, reg_data);
-	if(status != 0)
-		return -1;
+	// /* Slot A 4 LED pulses with 15us period */
+	// status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
+	// 			  ADPD188_REG_SLOTA_NUMPULSES, &reg_data);
+	// if(status != 0)
+	// 	return -1;
+	// reg_data &= ~ADPD188_SLOTA_NUMPULSES_SLOTA_PULSES_MASK;
+	// reg_data |= (0x4 << ADPD188_SLOTA_NUMPULSES_SLOTA_PULSES_POS) &
+	// 	    ADPD188_SLOTA_NUMPULSES_SLOTA_PULSES_MASK;
+	// reg_data &= ~ADPD188_SLOTA_NUMPULSES_SLOTA_PERIOD_MASK;
+	// reg_data |= (0xE << ADPD188_SLOTA_NUMPULSES_SLOTA_PERIOD_POS) &
+	// 	    ADPD188_SLOTA_NUMPULSES_SLOTA_PERIOD_MASK;
+	// status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
+	// 			   ADPD188_REG_SLOTA_NUMPULSES, reg_data);
+	// if(status != 0)
+	// 	return -1;
 
-	/* Slot B 4 LED pulses with 15us period */
-	status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
-				  ADPD188_REG_SLOTB_NUMPULSES, &reg_data);
-	if(status != 0)
-		return -1;
-	reg_data &= ~ADPD188_SLOTB_NUMPULSES_SLOTB_PULSES_MASK;
-	reg_data |= (0x4 << ADPD188_SLOTB_NUMPULSES_SLOTB_PULSES_POS) &
-		    ADPD188_SLOTB_NUMPULSES_SLOTB_PULSES_MASK;
-	reg_data &= ~ADPD188_SLOTB_NUMPULSES_SLOTB_PERIOD_MASK;
-	reg_data |= (0xE << ADPD188_SLOTB_NUMPULSES_SLOTB_PERIOD_POS) &
-		    ADPD188_SLOTB_NUMPULSES_SLOTB_PERIOD_MASK;
-	status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
-				   ADPD188_REG_SLOTB_NUMPULSES, reg_data);
-	if(status != 0)
-		return -1;
+	// /* Slot B 4 LED pulses with 15us period */
+	// status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
+	// 			  ADPD188_REG_SLOTB_NUMPULSES, &reg_data);
+	// if(status != 0)
+	// 	return -1;
+	// reg_data &= ~ADPD188_SLOTB_NUMPULSES_SLOTB_PULSES_MASK;
+	// reg_data |= (0x4 << ADPD188_SLOTB_NUMPULSES_SLOTB_PULSES_POS) &
+	// 	    ADPD188_SLOTB_NUMPULSES_SLOTB_PULSES_MASK;
+	// reg_data &= ~ADPD188_SLOTB_NUMPULSES_SLOTB_PERIOD_MASK;
+	// reg_data |= (0xE << ADPD188_SLOTB_NUMPULSES_SLOTB_PERIOD_POS) &
+	// 	    ADPD188_SLOTB_NUMPULSES_SLOTB_PERIOD_MASK;
+	// status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
+	// 			   ADPD188_REG_SLOTB_NUMPULSES, reg_data);
+	// if(status != 0)
+	// 	return -1;
 
-	/* Slot A integrator window */
-	status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
-				  ADPD188_REG_SLOTA_AFE_WINDOW, &reg_data);
-	if(status != 0)
-		return -1;
-	reg_data &= ~ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_WIDTH_MASK;
-	reg_data |= (0x4 << ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_WIDTH_POS) &
-		    ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_WIDTH_MASK;
-	reg_data &= ~ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_OFFSET_MASK;
-	reg_data |= (0x2F0 << ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_OFFSET_POS) &
-		    ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_OFFSET_MASK;
-	status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
-				   ADPD188_REG_SLOTA_AFE_WINDOW, reg_data);
-	if(status != 0)
-		return -1;
+	// /* Slot A integrator window */
+	// status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
+	// 			  ADPD188_REG_SLOTA_AFE_WINDOW, &reg_data);
+	// if(status != 0)
+	// 	return -1;
+	// reg_data &= ~ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_WIDTH_MASK;
+	// reg_data |= (0x4 << ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_WIDTH_POS) &
+	// 	    ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_WIDTH_MASK;
+	// reg_data &= ~ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_OFFSET_MASK;
+	// reg_data |= (0x2F0 << ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_OFFSET_POS) &
+	// 	    ADPD188_SLOTA_AFE_WINDOW_SLOTA_AFE_OFFSET_MASK;
+	// status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
+	// 			   ADPD188_REG_SLOTA_AFE_WINDOW, reg_data);
+	// if(status != 0)
+	// 	return -1;
 
-	/* Slot B integrator window */
-	status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
-				  ADPD188_REG_SLOTB_AFE_WINDOW, &reg_data);
-	if(status != 0)
-		return -1;
-	reg_data &= ~ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_WIDTH_MASK;
-	reg_data |= (0x4 << ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_WIDTH_POS) &
-		    ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_WIDTH_MASK;
-	reg_data &= ~ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_OFFSET_MASK;
-	reg_data |= (0x2F0 << ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_OFFSET_POS) &
-		    ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_OFFSET_MASK;
-	status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
-				   ADPD188_REG_SLOTB_AFE_WINDOW, reg_data);
-	if(status != 0)
-		return -1;
+	// /* Slot B integrator window */
+	// status = adpd188_reg_read(adpd1080_iio_device->drv_dev,
+	// 			  ADPD188_REG_SLOTB_AFE_WINDOW, &reg_data);
+	// if(status != 0)
+	// 	return -1;
+	// reg_data &= ~ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_WIDTH_MASK;
+	// reg_data |= (0x4 << ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_WIDTH_POS) &
+	// 	    ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_WIDTH_MASK;
+	// reg_data &= ~ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_OFFSET_MASK;
+	// reg_data |= (0x2F0 << ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_OFFSET_POS) &
+	// 	    ADPD188_SLOTB_AFE_WINDOW_SLOTB_AFE_OFFSET_MASK;
+	// status = adpd188_reg_write(adpd1080_iio_device->drv_dev,
+	// 			   ADPD188_REG_SLOTB_AFE_WINDOW, reg_data);
+	// if(status != 0)
+	// 	return -1;
 
-	/* Math for chop mode is inverted, non-inverted, non-inverted, inverted */
-	status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_MATH,
-				  &reg_data);
-	if(status != 0)
-		return -1;
-	reg_data &= ~ADPD188_MATH_FLT_MATH34_B_MASK;
-	reg_data |= (0x01 << ADPD188_MATH_FLT_MATH34_B_POS) &
-		    ADPD188_MATH_FLT_MATH34_B_MASK;
-	reg_data &= ~ADPD188_MATH_FLT_MATH34_A_MASK;
-	reg_data |= (0x01 << ADPD188_MATH_FLT_MATH34_A_POS) &
-		    ADPD188_MATH_FLT_MATH34_A_MASK;
-	reg_data &= ~ADPD188_MATH_FLT_MATH12_B_MASK;
-	reg_data |= (0x02 << ADPD188_MATH_FLT_MATH12_B_POS) &
-		    ADPD188_MATH_FLT_MATH12_B_MASK;
-	reg_data &= ~ADPD188_MATH_FLT_MATH12_A_MASK;
-	reg_data |= (0x02 << ADPD188_MATH_FLT_MATH12_A_POS) &
-		    ADPD188_MATH_FLT_MATH12_A_MASK;
+	// /* Math for chop mode is inverted, non-inverted, non-inverted, inverted */
+	// status = adpd188_reg_read(adpd1080_iio_device->drv_dev, ADPD188_REG_MATH,
+	// 			  &reg_data);
+	// if(status != 0)
+	// 	return -1;
+	// reg_data &= ~ADPD188_MATH_FLT_MATH34_B_MASK;
+	// reg_data |= (0x01 << ADPD188_MATH_FLT_MATH34_B_POS) &
+	// 	    ADPD188_MATH_FLT_MATH34_B_MASK;
+	// reg_data &= ~ADPD188_MATH_FLT_MATH34_A_MASK;
+	// reg_data |= (0x01 << ADPD188_MATH_FLT_MATH34_A_POS) &
+	// 	    ADPD188_MATH_FLT_MATH34_A_MASK;
+	// reg_data &= ~ADPD188_MATH_FLT_MATH12_B_MASK;
+	// reg_data |= (0x02 << ADPD188_MATH_FLT_MATH12_B_POS) &
+	// 	    ADPD188_MATH_FLT_MATH12_B_MASK;
+	// reg_data &= ~ADPD188_MATH_FLT_MATH12_A_MASK;
+	// reg_data |= (0x02 << ADPD188_MATH_FLT_MATH12_A_POS) &
+	// 	    ADPD188_MATH_FLT_MATH12_A_MASK;
 
-	status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_MATH,
-				   reg_data);
-	if(status != 0)
-		return -1;
+	// status = adpd188_reg_write(adpd1080_iio_device->drv_dev, ADPD188_REG_MATH,
+	// 			   reg_data);
+	// if(status != 0)
+	// 	return -1;
 
-	status = adpd1080pmod_32k_calib(adpd1080_iio_device->drv_dev);
-	if (status < 0)
-		return status;
+	// status = adpd1080pmod_32k_calib(adpd1080_iio_device->drv_dev);
+	// if (status < 0)
+	// 	return status;
 
-	status = adpd188_clk32mhz_cal(adpd1080_iio_device->drv_dev);
-	if(status != 0)
-		return status;
+	// status = adpd188_clk32mhz_cal(adpd1080_iio_device->drv_dev);
+	// if(status != 0)
+	// 	return status;
 
-	status = adpd188_adc_fsample_set(adpd1080_iio_device->drv_dev, 512.0);
-	if(status != 0)
-		return status;
+	// status = adpd188_adc_fsample_set(adpd1080_iio_device->drv_dev, 512.0);
+	// if(status != 0)
+	// 	return status;
 	
-	int ch;
-	int32_t raw[8];
-	while(sps_get_fifo_data(adpd1080_iio_device->drv_dev, raw) == -EAGAIN)
-		; // blocking wait
-	for (ch = 0; ch < 8; ch++) {
-		uint16_t offset;
-		uint8_t offset_reg = ch / 4 ? ADPD188_REG_SLOTB_CH1_OFFSET : ADPD188_REG_SLOTA_CH1_OFFSET + (ch % 4);
-		status = adpd188_reg_read(adpd1080_iio_device->drv_dev, offset_reg,
-					&offset);
-		if (status != 0)
-			return status;
-		status = adpd188_reg_write(adpd1080_iio_device->drv_dev, offset_reg,
-					raw[ch] + offset);
-		if (status != 0)
-			return status;
-	}
+	// int ch;
+	// int32_t raw[8];
+	// while(sps_get_fifo_data(adpd1080_iio_device->drv_dev, raw) == -EAGAIN)
+	// 	; // blocking wait
+	// for (ch = 0; ch < 8; ch++) {
+	// 	uint16_t offset;
+	// 	uint8_t offset_reg = ch / 4 ? ADPD188_REG_SLOTB_CH1_OFFSET : ADPD188_REG_SLOTA_CH1_OFFSET + (ch % 4);
+	// 	status = adpd188_reg_read(adpd1080_iio_device->drv_dev, offset_reg,
+	// 				&offset);
+	// 	if (status != 0)
+	// 		return status;
+	// 	status = adpd188_reg_write(adpd1080_iio_device->drv_dev, offset_reg,
+	// 				raw[ch] + offset);
+	// 	if (status != 0)
+	// 		return status;
+	// }
 
-	status = adpd188_mode_set(adpd1080_iio_device->drv_dev, ADPD188_NORMAL);
-	if (status != 0)
-		return status;
+	// status = adpd188_mode_set(adpd1080_iio_device->drv_dev, ADPD188_NORMAL);
+	// if (status != 0)
+	// 	return status;
 	
 	struct sps_iio_desc *sps;
 	struct sps_iio_init_param sps_config = {
@@ -745,8 +745,8 @@ int main(void)
 	status = sps_iio_init(&sps, &sps_config);
 
 	struct iio_app_device devices[] = {
-		IIO_APP_DEVICE("adpd1080", adpd1080_iio_device, &iio_adpd188_device,
-			       &iio_adpd1080_read_buff, NULL, NULL),
+		// IIO_APP_DEVICE("adpd1080", adpd1080_iio_device, &iio_adpd188_device,
+		// 	       &iio_adpd1080_read_buff, NULL, NULL),
 		IIO_APP_DEVICE("ad7793", ad7793_iiodev->ad7793_desc, &ad7799_iio_descriptor,
 				NULL, NULL, NULL),
 		IIO_APP_DEVICE("sps", sps, &iio_sps_device,
@@ -771,7 +771,6 @@ int main(void)
 	
 	LCD_Init(U2D_R2L, 800);
 	LCD_Clear(WHITE);
-	print_line(0, 0, 10, "Gesture  :");
 	print_line(1, 0, 10, "IO1  (mA):");
 	print_line(2, 0, 10, "IO2      :");
 	print_line(3, 0, 10, "IO3   (V):");
