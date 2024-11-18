b79abd8acdac957583c8126bb54c1c8dc6233df1
sirac kucukarabacioglu <sirac.kucukarabacioglu@analog.com>
2024-11-18 17:25:29 +0300

    project: adf4368: Add project for ADF4368
    Add initial project files for both basic and IIO examples for ADF4368.

---
+++ projects/adf4368/src/examples/examples_src.mk
@@ -0,0 +1,24 @@
+EXAMPLE ?= basic
+
+ifeq (basic,$(strip $(EXAMPLE)))
+CFLAGS += -DBASIC_EXAMPLE
+SRCS += $(PROJECT)/src/examples/basic/basic_example.c
+INCS += $(PROJECT)/src/examples/basic/basic_example.h
+endif
+
+ifeq (iio_example,$(strip $(EXAMPLE)))
+IIOD=y
+CFLAGS += -DIIO_EXAMPLE
+SRCS += $(PROJECT)/src/examples/iio_example/iio_example.c
+INCS += $(PROJECT)/src/examples/iio_example/iio_example.h
+endif
+
+ifeq (y,$(strip $(IIOD)))
+SRC_DIRS += $(NO-OS)/iio/iio_app
+
+INCS += $(DRIVERS)/frequency/adf4368/iio_adf4368.h
+SRCS += $(DRIVERS)/frequency/adf4368/iio_adf4368.c
+
+INCS += $(INCLUDE)/no_os_list.h \
+		$(PLATFORM_DRIVERS)/$(PLATFORM)_uart.h
+endif
