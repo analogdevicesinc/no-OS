analogdevicesinc/no-OS

commit 93473c4f02b9dace816b348aa9dd02ebbf852299

tools: generic.mk: re-generate object files if flags.txt files have changed
In case CFLAGS,CPPFLAGS,ASFLAGS and OBJS lists change, their respective
files will also change. This should re-trigger a build.

The 'pre_build' rule is run via the 'update' build rule, which is now
getting called everything we build 'all'.
When the 'pre_build' gets run, we re-generate all flags.txt files, and
overwrite them if they changed.
When they change, the object file will rebuild.

Signed-off-by: Alexandru Ardelean <aardelean@baylibre.com>

tools/scripts/generic.mk
@@ -301,32 +301,41 @@ LSCRIPT_FLAG = -T$(LSCRIPT)
 endif
 
 define generate_cflags_func
-echo $(subst \",\\\",$(1)) >> $(CFLAGS_FILE)
+echo $(subst \",\\\",$(1)) >> $(CFLAGS_FILE).tmp
 endef
 
 define generate_cppflags_func
-echo $(subst \",\\\",$(1)) >> $(CPPFLAGS_FILE)
+echo $(subst \",\\\",$(1)) >> $(CPPFLAGS_FILE).tmp
 endef
 
 define generate_asflags_func
-echo $(subst \",\\\",$(1)) >> $(ASFLAGS_FILE)
+echo $(subst \",\\\",$(1)) >> $(ASFLAGS_FILE).tmp
 endef
 
 define generate_objs_func
-echo $(1) >> $(OBJS_FILE)
+echo $(1) >> $(OBJS_FILE).tmp
+endef
+
+define overwrite_file_if_different
+	diff $(1) $(2) &> /dev/null || mv -f $(1) $(2)
+endef
+
+define generate_flags_file
+	echo -n > $(1).tmp
+	$(call process_items_in_chunks,$(2),10,$(3))
+	$(call overwrite_file_if_different,$(1).tmp,$(1))
+	rm -f $(1).tmp
 endef
 
 PHONY += pre_build
 pre_build:
 	$(call print,Generating build flags)
-	echo -n > $(CFLAGS_FILE)
-	$(call process_items_in_chunks,$(CFLAGS),10,generate_cflags_func)
-	echo -n > $(CPPFLAGS_FILE)
-	$(call process_items_in_chunks,$(CPPFLAGS),10,generate_cppflags_func)
-	echo -n > $(ASFLAGS_FILE)
-	$(call process_items_in_chunks,$(ASFLAGS),10,generate_asflags_func)
-	echo -n > $(OBJS_FILE)
-	$(call process_items_in_chunks,$(sort $(OBJS)),10,generate_objs_func)
+	$(call generate_flags_file,$(CFLAGS_FILE),$(CFLAGS),generate_cflags_func)
+	$(call generate_flags_file,$(CPPFLAGS_FILE),$(CPPFLAGS),generate_cppflags_func)
+	$(call generate_flags_file,$(ASFLAGS_FILE),$(ASFLAGS),generate_asflags_func)
+	$(call generate_flags_file,$(OBJS_FILE),$(OBJS),generate_objs_func)
+
+$(OBJS): $(CFLAGS_FILE) $(CPPFLAGS_FILE) $(ASFLAGS_FILE) $(OBJS_FILE)
 
 $(BINARY): $(LIB_TARGETS) $(OBJS) $(ASM_OBJS) $(LSCRIPT) $(BOOTOBJ)
 	$(call print,[LD] $(notdir $(OBJS)))

commit 134b63e93bf233f630232e7a89506135075dd799

tools: generic.mk: re-generate object files if flags.txt files have changed (V2)
This is a V2 for commit 93473c4f0 ("tools: generic.mk: re-generate object
files if flags.txt files have changed"), which was reverted via commit
66afd04a9.

What was changed since V1:
  - inverted condition for 'diff'; 'diff' returns true when files are
    equal.
  - removed build rule '$(OBJS): $(CFLAGS_FILE) $(CPPFLAGS_FILE) $(ASFLAGS_FILE) $(OBJS_FILE)'
    this seems to generate some flags.txt generation to occur more than
    once.

Fixes: 93473c4f0 ("tools: generic.mk: re-generate object files if flags.txt files have changed")
Signed-off-by: Alexandru Ardelean <aardelean@baylibre.com>

tools/scripts/generic.mk
@@ -301,32 +301,41 @@ LSCRIPT_FLAG = -T$(LSCRIPT)
 endif
 
 define generate_cflags_func
-echo $(subst \",\\\",$(1)) >> $(CFLAGS_FILE)
+echo $(subst \",\\\",$(1)) >> $(CFLAGS_FILE).tmp
 endef
 
 define generate_cppflags_func
-echo $(subst \",\\\",$(1)) >> $(CPPFLAGS_FILE)
+echo $(subst \",\\\",$(1)) >> $(CPPFLAGS_FILE).tmp
 endef
 
 define generate_asflags_func
-echo $(subst \",\\\",$(1)) >> $(ASFLAGS_FILE)
+echo $(subst \",\\\",$(1)) >> $(ASFLAGS_FILE).tmp
 endef
 
 define generate_objs_func
-echo $(1) >> $(OBJS_FILE)
+echo $(1) >> $(OBJS_FILE).tmp
+endef
+
+define overwrite_file_if_different
+	if ! diff $(1) $(2) &> /dev/null ; then \
+		mv -f $(1) $(2) ; \
+	fi
+endef
+
+define generate_flags_file
+	echo -n > $(1).tmp
+	$(call process_items_in_chunks,$(2),10,$(3))
+	$(call overwrite_file_if_different,$(1).tmp,$(1))
+	rm -f $(1).tmp
 endef
 
 PHONY += pre_build
 pre_build:
 	$(call print,Generating build flags)
-	echo -n > $(CFLAGS_FILE)
-	$(call process_items_in_chunks,$(CFLAGS),10,generate_cflags_func)
-	echo -n > $(CPPFLAGS_FILE)
-	$(call process_items_in_chunks,$(CPPFLAGS),10,generate_cppflags_func)
-	echo -n > $(ASFLAGS_FILE)
-	$(call process_items_in_chunks,$(ASFLAGS),10,generate_asflags_func)
-	echo -n > $(OBJS_FILE)
-	$(call process_items_in_chunks,$(sort $(OBJS)),10,generate_objs_func)
+	$(call generate_flags_file,$(CFLAGS_FILE),$(CFLAGS),generate_cflags_func)
+	$(call generate_flags_file,$(CPPFLAGS_FILE),$(CPPFLAGS),generate_cppflags_func)
+	$(call generate_flags_file,$(ASFLAGS_FILE),$(ASFLAGS),generate_asflags_func)
+	$(call generate_flags_file,$(OBJS_FILE),$(OBJS),generate_objs_func)
 
 $(BINARY): $(LIB_TARGETS) $(OBJS) $(ASM_OBJS) $(LSCRIPT) $(BOOTOBJ)
 	$(call print,[LD] $(notdir $(OBJS)))