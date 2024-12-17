92a05ed2205537118b372234acbd238bbb6df89f
George Mois <george.mois@analog.com>
2024-12-17 12:09:16 +0200

    projects: adrv9009: *.mk: Use NO_OS_INC_DIRS
    Use NO_OS_INC_DIRS build var for pulling header files tools.
    
    Signed-off-by: George Mois <george.mois@analog.com>

--- projects/adrv9009/mb_src.mk
+++ projects/adrv9009/mb_src.mk
@@ -72,52 +72,27 @@ endif
 # Talise API sources
 SRC_DIRS += $(DRIVERS)/rf-transceiver/talise
 
-INCS +=	$(PROJECT)/src/app/app_config.h \
-	$(DRIVERS)/frequency/ad9528/ad9528.h \
-	$(PROJECT)/src/devices/adi_hal/adi_hal.h \
-	$(PROJECT)/src/devices/adi_hal/common.h \
-	$(PROJECT)/src/devices/adi_hal/parameters.h \
-	$(DRIVERS)/frequency/hmc7044/hmc7044.h \
-	$(PROJECT)/profiles/$(PROFILE)/talise_config.h
-INCS += $(DRIVERS)/axi_core/axi_adc_core/axi_adc_core.h \
-	$(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.h \
-	$(DRIVERS)/axi_core/axi_dmac/axi_dmac.h \
-	$(DRIVERS)/axi_core/jesd204/axi_jesd204_rx.h \
-	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.h \
-	$(DRIVERS)/axi_core/jesd204/jesd204_clk.h
+NO_OS_INC_DIRS := \
+	$(INCLUDE)/ \
+	$(PROJECT)/src/app/ \
+	$(PROJECT)/src/devices/adi_hal/ \
+	$(PROJECT)/profiles/$(PROFILE)/ \
+	$(PLATFORM_DRIVERS)/ \
+	$(NO-OS)/jesd204/ \
+	$(DRIVERS)/frequency/ad9528/ \
+	$(DRIVERS)/frequency/hmc7044/ \
+	$(DRIVERS)/axi_core/axi_adc_core/ \
+	$(DRIVERS)/axi_core/axi_dac_core/ \
+	$(DRIVERS)/axi_core/axi_dmac/ \
+	$(DRIVERS)/axi_core/jesd204/
+
 ifeq (xilinx,$(strip $(PLATFORM)))
-INCS += $(DRIVERS)/axi_core/jesd204/xilinx_transceiver.h \
-	$(DRIVERS)/axi_core/jesd204/axi_adxcvr.h \
-	$(DRIVERS)/axi_core/clk_axi_clkgen/clk_axi_clkgen.h
+NO_OS_INC_DIRS += $(DRIVERS)/axi_core/clk_axi_clkgen/
 else
-INCS += $(DRIVERS)/axi_core/clk_altera_a10_fpll/clk_altera_a10_fpll.h \
-	$(DRIVERS)/axi_core/jesd204/altera_a10_atx_pll.h \
-	$(DRIVERS)/axi_core/jesd204/altera_a10_cdr_pll.h \
-	$(DRIVERS)/axi_core/jesd204/altera_adxcvr.h
+NO_OS_INC_DIRS += $(DRIVERS)/axi_core/clk_altera_a10_fpll/
 endif
-INCS +=	$(PLATFORM_DRIVERS)/$(PLATFORM)_spi.h \
-	$(PLATFORM_DRIVERS)/$(PLATFORM)_gpio.h
-INCS +=	$(INCLUDE)/no_os_axi_io.h \
-	$(INCLUDE)/no_os_spi.h \
-	$(INCLUDE)/no_os_gpio.h \
-	$(INCLUDE)/no_os_error.h \
-	$(INCLUDE)/no_os_delay.h \
-	$(INCLUDE)/no_os_util.h \
-	$(INCLUDE)/no_os_alloc.h \
-	$(INCLUDE)/no_os_mutex.h \
-	$(INCLUDE)/no_os_units.h \
-	$(INCLUDE)/no_os_print_log.h \
-	$(INCLUDE)/no_os_clk.h \
-	$(INCLUDE)/jesd204.h \
-	$(NO-OS)/jesd204/jesd204-priv.h
 ifeq (y,$(strip $(IIOD)))
-INCS +=	$(INCLUDE)/no_os_fifo.h \
-	$(INCLUDE)/no_os_irq.h \
-	$(INCLUDE)/no_os_uart.h \
-	$(INCLUDE)/no_os_lf256fifo.h \
-	$(INCLUDE)/no_os_list.h \
-	$(PLATFORM_DRIVERS)/$(PLATFORM)_irq.h \
-	$(PLATFORM_DRIVERS)/$(PLATFORM)_uart.h \
-	$(DRIVERS)/axi_core/iio_axi_adc/iio_axi_adc.h \
-	$(DRIVERS)/axi_core/iio_axi_dac/iio_axi_dac.h
+NO_OS_INC_DIRS += \
+	$(DRIVERS)/axi_core/iio_axi_adc/ \
+	$(DRIVERS)/axi_core/iio_axi_dac/
 endif
