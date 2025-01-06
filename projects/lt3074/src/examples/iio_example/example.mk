a12ac47f9c658698bc11dc8922a832bcd21b5d13
Cedric Encarnacion <cedricjustine.encarnacion@analog.com>
2025-01-06 15:11:03 +0800

    projects: lt3074: add examples for LT3074
    Add initial project files for both basic and IIO examples for LT3074.
    
    Signed-off-by: Cedric Encarnacion <cedricjustine.encarnacion@analog.com>

---
+++ projects/lt3074/src/examples/iio_example/example.mk
@@ -0,0 +1,4 @@
+IIOD=y
+
+INCS += $(DRIVERS)/power/lt3074/iio_lt3074.h
+SRCS += $(DRIVERS)/power/lt3074/iio_lt3074.c
---
+++ projects/lt3074/src/platform/maxim/platform_src.mk
@@ -0,0 +1,12 @@
+NO_OS_INC_DIRS += \
+        $(PLATFORM_DRIVERS) \
+        $(PLATFORM_DRIVERS)/../common/
+
+SRCS += $(PLATFORM_DRIVERS)/maxim_delay.c \
+        $(PLATFORM_DRIVERS)/maxim_gpio.c \
+        $(PLATFORM_DRIVERS)/maxim_gpio_irq.c \
+        $(PLATFORM_DRIVERS)/maxim_irq.c \
+        $(PLATFORM_DRIVERS)/../common/maxim_dma.c \
+        $(PLATFORM_DRIVERS)/maxim_i2c.c \
+        $(PLATFORM_DRIVERS)/maxim_uart.c \
+        $(PLATFORM_DRIVERS)/maxim_uart_stdio.c
