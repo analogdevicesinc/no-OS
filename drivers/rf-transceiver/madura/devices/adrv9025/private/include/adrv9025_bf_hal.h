analogdevicesinc/no-OS

commit 98ae8c483dc70da9a1677ed7070931236c59a00c

Remove the wrong SPDX details
They were added by mistake.

Signed-off-by: Dragos Bogdan <dragos.bogdan@analog.com>

drivers/rf-transceiver/madura/devices/adrv9025/private/include/adrv9025_bf_hal.h
@@ -1,5 +1,4 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef ADRV9025_BF_HAL_H_
+/*#ifndef ADRV9025_BF_HAL_H_
 #define ADRV9025_BF_HAL_H_
 
 #ifdef __KERNEL__

commit 95c73d6b7555503cb3fd0d248e07e1544e9643eb

madura: adrv9025_bf_hal.h: Remove comment
Remove commented includes.

Fixes: 98ae8c4 ("Remove the wrong SPDX details")

Signed-off-by: George Mois <george.mois@analog.com>

drivers/rf-transceiver/madura/devices/adrv9025/private/include/adrv9025_bf_hal.h
@@ -1,4 +1,4 @@
-/*#ifndef ADRV9025_BF_HAL_H_
+#ifndef ADRV9025_BF_HAL_H_
 #define ADRV9025_BF_HAL_H_
 
 #ifdef __KERNEL__