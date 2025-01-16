3e0adb87376ea9d884cac38e2ddc8b05294389a9
Radu Etz <radu.etz@analog.com>
2025-01-16 12:45:20 +0200

    projects: eval-ade7953: example project
    Example project for ADE7953. Tested using the EVAL-ADE7953.
    The example includes measurements of the rms values,
    power values, power factor, period, energy etc.
    The example is tested with the AD-APARD32690-SL.
    
    Signed-off-by: Radu Etz <radu.etz@analog.com>

---
+++ projects/eval-ade7953/src/interrupt/interrupt.h
@@ -0,0 +1,62 @@
+/***************************************************************************//**
+ *   @file   interrupt.h
+ *   @brief  Data ready interrupt file.
+ *   @author REtz (radu.etz@analog.com)
+********************************************************************************
+ * Copyright (c) 2025 Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef __INTER_H__
+#define __INTER_H__
+
+#include "no_os_irq.h"
+
+/* IRQ config */
+#define GPIO_IRQ_OPS                &max_gpio_irq_ops
+#define GPIO_CTRL_IRQ_ID            0
+#define GPIO_IRQ_EXTRA              &gpio_extra_ip
+#define NVIC_GPIO_IRQ               GPIO2_IRQn
+
+/*! Get IRQN flag value. */
+int get_irq_flag_state(void);
+
+/*! Reset IRQN flag value. */
+void reset_irq_flag_state(void);
+
+/*! Initialize IRQN interrupt */
+int inter_init_irq(struct ade7953_dev *dev);
+
+/*! Get ZX flag value */
+int get_zx_flag_state(void);
+
+/*! Reset ZX flag value */
+void reset_zx_flag_state(void);
+
+/*! Initialize ZX interrupt */
+int inter_init_zx(struct ade7953_dev *dev);
+
+#endif /* __INTER__H__ */
