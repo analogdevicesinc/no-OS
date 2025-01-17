f350189dc778f1a019d1def4f2b045f823373b6e
Ciprian Hegbeli <ciprian.hegbeli@analog.com>
2025-01-17 13:00:12 +0200

    project: Add AD9084 project
    This is the  initial commit for the Apollo project. It contains the
    general structure of the project and the init of the ADF4382.
    
    Signed-off-by: Ciprian Hegbeli <ciprian.hegbeli@analog.com>

---
+++ projects/ad9084/src/common/common_data.c
@@ -0,0 +1,86 @@
+/***************************************************************************//**
+ *   @file   common_data.c
+ *   @brief  Defines common data to be used by adf4382 examples.
+ *   @author CHegbeli (ciprian.hegbeli@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include "common_data.h"
+
+struct no_os_uart_init_param platform_uart_ip = {
+	.device_id = UART_DEVICE_ID,
+	.irq_id = UART_IRQ_ID,
+	.asynchronous_rx = true,
+	.baud_rate = UART_BAUDRATE,
+	.size = NO_OS_UART_CS_8,
+	.parity = NO_OS_UART_PAR_NO,
+	.stop = NO_OS_UART_STOP_1_BIT,
+	.extra = UART_EXTRA,
+	.platform_ops = UART_OPS,
+};
+
+struct no_os_spi_init_param adf4382_spi_ip = {
+	.device_id = SPI_DEVICE_ID,
+	.max_speed_hz = 1500000,
+	.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
+	.mode = NO_OS_SPI_MODE_0,
+	.platform_ops = SPI_OPS_ADF4382,
+	.chip_select = SPI_CS_ADF4382,
+	.extra = SPI_EXTRA_ADF4382,
+};
+
+// struct no_os_spi_init_param ltc6952_spi_param = {
+// 	.device_id = SPI_DEVICE_ID,
+// 	.max_speed_hz = 1500000,
+// 	.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
+// 	.mode = NO_OS_SPI_MODE_0,
+// 	.platform_ops = SPI_OPS_LTC6952,
+// 	.chip_select = SPI_CS_LTC6952,
+// 	.extra = SPI_EXTRA_LTC6952,
+// };
+
+// struct no_os_gpio_init_param gpio_reset_param = {
+// 	.platform_ops = GPIO_OPS,
+// 	.extra = GPIO_EXTRA,
+// 	.number = GPIO_OFFSET + GPIO_RESET_N,
+// };
+
+struct adf4382_init_param adf4382_ip = {
+	.spi_init = &adf4382_spi_ip,
+	.spi_3wire_en = false,
+	.cmos_3v3 = false,
+	.ref_freq_hz = 125000000,
+	.freq = 20000000000,
+	.ref_doubler_en = 0,
+	.ref_div = 1,
+	.cp_i = 15,
+	.bleed_word = 0,
+	.ld_count = 10,
+	.id = ID_ADF4382A,
+};
