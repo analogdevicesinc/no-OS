7e26a0a7fc937a39c95184dab2bfbf3ac7e7df18
Darius Berghe <darius.berghe@analog.com>
2025-01-22 14:06:27 +0200

    projects/ad74416h: use the new example infrastructure
    Make the necessary changes to use the new example infrastructure.
    
    Signed-off-by: Darius Berghe <darius.berghe@analog.com>

--- projects/ad74416h/src/platform/stm32/main.c
+++ projects/ad74416h/src/platform/stm32/main.c
@@ -34,7 +34,7 @@
 /******************************************************************************/
 /***************************** Include Files **********************************/
 /******************************************************************************/
-#include "platform_includes.h"
+#include "parameters.h"
 #include "common_data.h"
 #include "no_os_error.h"
 
@@ -49,31 +49,10 @@
 *******************************************************************************/
 int main()
 {
-	int ret = -EINVAL;
 	ad74416h_ip.spi_ip = ad74416h_spi_ip;
 	ad74416h_spi_extra_ip.get_input_clock = HAL_RCC_GetPCLK1Freq;
 
 	stm32_init();
 
-#ifdef BASIC_EXAMPLE
-	struct no_os_uart_desc *uart_desc;
-
-	ret = no_os_uart_init(&uart_desc, &ad74416h_uart_ip);
-	if (ret)
-		return ret;
-
-	no_os_uart_stdio(uart_desc);
-	ret = basic_example_main();
-	if (ret) {
-		no_os_uart_remove(uart_desc);
-		return ret;
-	}
-
-#endif
-
-#if (BASIC_EXAMPLE == 0)
-#error At least one example has to be selected using y value in Makefile.
-#endif
-
-	return 0;
+	return example_main();
 }
