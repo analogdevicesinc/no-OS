analogdevicesinc/no-OS

commit b5692509d26b5c6680609123e53ff4e3ad7a1a8e

tools/scripts: added general makefile
Add FreeRTOS impementation for mutex, semaphore and delay
Include freeRTOS and linking to no-os in libraries

Signed-off-by: rbudai <robert.budai@analog.com>

drivers/platform/freeRTOS/freertos_delay.c
@@ -1,0 +1,86 @@
+/***************************************************************************//**
+ *   @file   freeRTOS/freertos_delay.c
+ *   @brief  Implementation of freertos delay functions.
+ *   @author Robert Budai (robert.budai@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#include <stdbool.h>
+#include "FreeRTOS.h"
+#include "no_os_delay.h"
+#include "task.h"
+
+/**
+ * @brief Generate microseconds delay.
+ * @param usecs - Delay in microseconds.
+ * @return None.
+ */
+
+void no_os_udelay(uint32_t usecs)
+{
+
+	vTaskDelay((((double)usecs / 1000000) * configTICK_RATE_HZ));
+}
+
+/**
+ * @brief Generate miliseconds delay.
+ * @param msecs - Delay in miliseconds.
+ * @return None.
+ */
+void no_os_mdelay(uint32_t msecs)
+{
+	vTaskDelay((((double)msecs / 1000) * configTICK_RATE_HZ));
+}
+
+/**
+ * @brief Get current time.
+ * @return Current time structure from system start (seconds, microseconds).
+ */
+struct no_os_time no_os_get_time(void)
+{
+	unsigned long long Xtime_Global;
+	double fractional_part = 0;
+	struct no_os_time t;
+
+	unsigned long long _system_ticks  = (unsigned long long)xTaskGetTickCount();
+
+	Xtime_Global = HAL_GetTick();
+	t.s = Xtime_Global / 1000; // milliseconds
+
+	fractional_part = (float)Xtime_Global / 1000 - Xtime_Global / 1000;
+	t.us = fractional_part * 1000;
+	t.us *= 1000;
+
+	return t;
+}

drivers/platform/freeRTOS/freertos_mutex.c
@@ -1,0 +1,90 @@
+/*******************************************************************************
+ *   @file   freeRTOS/freertos_mutex.c
+ *   @brief  Implementation of no-OS mutex funtionality.
+ *   @author Robert Budai (robert.budai@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include <FreeRTOS.h>
+#include "no_os_mutex.h"
+#include "semphr.h"
+
+/**
+ * @brief Initialize mutex.
+ * mutex - Pointer toward the mutex.
+ * @return None.
+ */
+__attribute__((weak)) inline void no_os_mutex_init(void **mutex)
+{
+	if ((*mutex) == NULL) {
+		(*mutex) = (SemaphoreHandle_t *)no_os_malloc(sizeof(SemaphoreHandle_t));
+		(*mutex) = xSemaphoreCreateMutex();
+	}
+}
+
+/**
+ * @brief Lock mutex.
+ * mutex - Pointer toward the mutex.
+ * @return None.
+ */
+__attribute__((weak)) inline void no_os_mutex_lock(void *mutex)
+{
+	if (mutex != NULL)
+		xSemaphoreTake(mutex,
+			       freeRTOS_WAIT_TIME); //replace with define from freertos config file
+}
+
+/**
+ * @brief Unlock mutex.
+ * mutex - Pointer toward the mutex.
+ * @return None.
+ */
+__attribute((weak)) inline void no_os_mutex_unlock(void *mutex)
+{
+	if (mutex != NULL)
+		xSemaphoreGive(mutex);
+}
+
+/**
+ * @brief Remove mutex.
+ * mutex - Pointer toward the mutex.
+ * @return None.
+ */
+__attribute__((weak)) inline void no_os_mutex_remove(void *mutex)
+{
+	if (mutex != NULL)
+		no_os_free(mutex);
+}
+

drivers/platform/freeRTOS/freertos_semaphore.c
@@ -1,0 +1,83 @@
+/*******************************************************************************
+ *   @file   freeRTOS/freertos_semaphore.c
+ *   @brief  Implementation of no-OS semaphore funtionality.
+ *   @author Robert Budai (robert.budai@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include <FreeRTOS.h>
+#include "no_os_semaphore.h"
+#include "semphr.h"
+
+/**
+ * @brief Initialize semaphore.
+ * semaphore - Pointer toward the semaphore.
+ * @return None.
+ */
+__attribute__((weak)) inline void no_os_semaphore_init(void **semaphore)
+{
+	*semaphore = (SemaphoreHandle_t *)no_os_malloc(sizeof(SemaphoreHandle_t));
+}
+
+/**
+ * @brief Take token from semaphore.
+ * semaphore - Pointer toward the semaphore.
+ * @return None.
+ */
+__attribute__((weak)) inline void no_os_semaphore_take(void *semaphore)
+{
+	xSemaphoreTake(semaphore,
+		       freeRTOS_WAIT_TIME);
+}
+
+/**
+ * @brief Give token to semaphore
+ * semaphore - Pointer toward the semaphore.
+ * @return None.
+ */
+__attribute((weak)) inline void no_os_semaphore_give(void *semaphore)
+{
+	xSemaphoreGive(semaphore);
+}
+
+/**
+ * @brief Remove semaphore.
+ * semaphore - Pointer toward the semaphore.
+ * @return None.
+ */
+__attribute__((weak)) inline void no_os_semaphore_remove(void *semaphore)
+{
+	no_os_free(semaphore);
+}

tools/scripts/freertos.mk
@@ -1,0 +1,34 @@
+ifeq ($(FREERTOS_KERNEL),)
+$(error $(ENDL)$(ENDL)FREERTOS_KERNEL not defined\
+		$(ENDL)$(ENDL)\
+		Please run command "export FREERTOS_KERNEL=/path/to/FreeRTOS-Kernel"$(ENDL))
+endif # FREERTOS_KERNEL check
+
+# link freeRTOS kernel files
+RTOS_INCS = $(FREERTOS_KERNEL)/include/projdefs.h \
+        $(FREERTOS_KERNEL)/include/FreeRTOS.h \
+        $(FREERTOS_KERNEL)/include/list.h \
+        $(FREERTOS_KERNEL)/include/portable.h \
+        $(FREERTOS_KERNEL)/include/deprecated_definitions.h \
+        $(FREERTOS_KERNEL)/include/mpu_wrappers.h \
+        $(FREERTOS_KERNEL)/include/newlib-freertos.h \
+        $(FREERTOS_KERNEL)/include/queue.h \
+        $(FREERTOS_KERNEL)/include/stack_macros.h \
+        $(FREERTOS_KERNEL)/include/semphr.h \
+        $(FREERTOS_KERNEL)/include/task.h \
+        $(FREERTOS_KERNEL)/include/timers.h \
+        $(FREERTOS_KERNEL)/portable/GCC/ARM_CM4F/portmacro.h # TO DO: make port finder generic, based on platform
+
+
+#Link heap implementation
+RTOS_SRCS = $(FREERTOS_KERNEL)/portable/MemMang/heap_2.c
+
+RTOS_SRCS += $(FREERTOS_KERNEL)/tasks.c \
+        $(FREERTOS_KERNEL)/list.c \
+        $(FREERTOS_KERNEL)/queue.c \
+        $(FREERTOS_KERNEL)/timers.c 
+
+RTOS_SRCS += $(FREERTOS_KERNEL)/portable/GCC/ARM_CM4F/port.c
+
+SRCS += $(RTOS_SRCS)
+INCS += $(RTOS_INCS)

tools/scripts/libraries.mk
@@ -18,6 +18,12 @@ CFLAGS += -DTINYIIOD_VERSION_MAJOR=0	 \
 CFLAGS += -DIIO_SUPPORT
 endif
 
+# FreeRTOS
+ifeq '$(FREERTOS)' 'y'
+CFLAGS += -DFREERTOS
+include $(NO-OS)/tools/scripts/freertos.mk
+endif
+
 #	MBEDTLS
 ifneq ($(if $(findstring mbedtls, $(LIBRARIES)), 1),)
 # Generic part

commit 4c4530c5cdd621bcb51c224c32d75fbc54861648

drivers: platform: freeRTOS: Fix no_os_get_time()
Currently, the no_os_get_time() function only works on STM32 because
it's using HAL_GetTick(). Make this platform agnostic, by using the
tick counter (incremented by FreeRTOS at every xPortSysTickHandler()
call).

Fixes: b569250 ("tools/scripts: added general makefile")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/platform/freeRTOS/freertos_delay.c
@@ -63,18 +63,12 @@ void no_os_mdelay(uint32_t msecs)
  */
 struct no_os_time no_os_get_time(void)
 {
-	unsigned long long Xtime_Global;
-	double fractional_part = 0;
 	struct no_os_time t;
 
-	unsigned long long _system_ticks  = (unsigned long long)xTaskGetTickCount();
+	TickType_t _system_ticks  = xTaskGetTickCount();
 
-	Xtime_Global = HAL_GetTick();
-	t.s = Xtime_Global / 1000; // milliseconds
-
-	fractional_part = (float)Xtime_Global / 1000 - Xtime_Global / 1000;
-	t.us = fractional_part * 1000;
-	t.us *= 1000;
+	t.s = _system_ticks / configTICK_RATE_HZ;
+	t.us = 1000000ULL * (_system_ticks % configTICK_RATE_HZ) / configTICK_RATE_HZ;
 
 	return t;
 }