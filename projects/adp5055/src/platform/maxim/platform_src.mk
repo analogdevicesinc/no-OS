b3888cac042b495bc496d52a278c368ec9910e70
Angelo Catapang <Angelo.Catapang@analog.com>
2025-02-18 00:59:13 +0800

    projects: adp5055: Add project for ADP5055
    Adding basic and IIO example projects using the ADP5055 no-OS drivers.
    
    Signed-off-by: Angelo Catapang <Angelo.Catapang@analog.com>

---
+++ projects/adp5055/src/platform/maxim/platform_src.mk
@@ -0,0 +1,11 @@
+NO_OS_INC_DIRS += \
+        $(PLATFORM_DRIVERS) \
+        $(PLATFORM_DRIVERS)/../common/
+
+SRCS += $(PLATFORM_DRIVERS)/maxim_delay.c     \
+        $(PLATFORM_DRIVERS)/maxim_irq.c       \
+        $(PLATFORM_DRIVERS)/../common/maxim_dma.c       \
+        $(PLATFORM_DRIVERS)/maxim_i2c.c       \
+        $(PLATFORM_DRIVERS)/maxim_uart.c      \
+        $(PLATFORM_DRIVERS)/maxim_uart_stdio.c
+ 
