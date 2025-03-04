c3455afdf070cdc130b8379049c94751d9a89159
Antoniu Miclaus <antoniu.miclaus@analog.com>
2025-03-04 16:33:53 +0200

    doc: sphinx: conf.py: disable smartquotes
    The documentation we provide contains also examples of commands which
    take input parameters with the double dash prefix (--). Due to the
    smartquotes being enabled by default in the sphinx configuration, the
    double dash is converted to typographical en-dash (–).
    
    Disable smartquotes such that the commands provided into the
    documentation can be used properly without any confusion.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- doc/sphinx/source/conf.py
+++ doc/sphinx/source/conf.py
@@ -52,3 +52,6 @@ html_theme = 'cosmic'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+# Enable/Disable Smart Quotes used to convert quotation marks and dashes.
+smartquotes = False
