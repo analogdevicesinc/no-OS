6edf8cd4a01dba6c2183cd807aacc15aefba6d78
Robert Budai <robert.budai@analog.com>
2025-04-09 10:21:42 +0300

    project: iio_demo_freertos: correct maxim dma includes
    Signed-off-by: Robert Budai <robert.budai@analog.com>

--- projects/iio_demo_freeRTOS/src/platform/maxim/platform_src.mk
+++ projects/iio_demo_freeRTOS/src/platform/maxim/platform_src.mk
@@ -1,10 +1,12 @@
 SRCS += $(PLATFORM_DRIVERS)/maxim_irq.c		\
+	$(PLATFORM_DRIVERS)/../common/maxim_dma.c       \
 	$(PLATFORM_DRIVERS)/maxim_timer.c	\
 	$(PLATFORM_DRIVERS)/maxim_uart.c	\
 	$(PLATFORM_DRIVERS)/maxim_i2c.c 	\
 	$(PLATFORM_DRIVERS)/maxim_gpio.c
 
 INCS += $(PLATFORM_DRIVERS)/maxim_irq.h		\
+	$(PLATFORM_DRIVERS)/../common/maxim_dma.h       \
 	$(PLATFORM_DRIVERS)/maxim_uart.h	\
 	$(PLATFORM_DRIVERS)/maxim_timer.h	\
 	$(PLATFORM_DRIVERS)/maxim_i2c.h		\
