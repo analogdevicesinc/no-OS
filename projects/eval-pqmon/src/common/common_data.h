b1d2bfd116de2554ec3f1d3cb07a45afaa1076da
Alisa-Dariana Roman <alisa.roman@analog.com>
2025-04-11 13:19:11 +0300

    projects: eval-pqmon: add W5500-based Ethernet connectivity
    Add support for standard Ethernet connectivity to the eval-pqmon project
    using the W5500 Ethernet controller. This introduces a new "ethernet"
    build option alongside the existing USB, serial, and ethernet_t1l options.
    Configure necessary hardware parameters, SPI interface, and network stack
    integration for the WIZ850io module.
    
    Signed-off-by: Alisa-Dariana Roman <alisa.roman@analog.com>

--- projects/eval-pqmon/src/common/common_data.h
+++ projects/eval-pqmon/src/common/common_data.h
@@ -34,6 +34,10 @@
 #ifndef __COMMON_DATA_H__
 #define __COMMON_DATA_H__
 
+#if defined(PQM_CONN_ETH)
+#include "w5500_network.h"
+#endif
+
 #if defined(PQM_CONN_T1L)
 #include "lwip_socket.h"
 #include "lwip_adin1110.h"
@@ -69,6 +73,13 @@
 #define MAX_EVENT_NUMBER 6
 
 extern IIO_BUFF_TYPE iio_data_buffer_loc[MAX_SIZE_BASE_ADDR];
+
+#if defined(PQM_CONN_ETH)
+extern const struct no_os_spi_init_param w5500_spi_init_params;
+
+extern struct w5500_init_param w5500_ip;
+extern struct w5500_network_init_param w5500_network_ip;
+#endif
 
 #if defined(PQM_CONN_USB)
 extern struct no_os_uart_init_param iio_demo_usb_ip;
