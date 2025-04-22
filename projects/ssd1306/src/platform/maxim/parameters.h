0f133ab5a15893ff3c97958bb333fe2542c3cb6d
Robert Budai <robert.budai@analog.com>
2025-04-22 14:28:29 +0300

    projects: ssd1306: add initial example
    Example project for lvgl library  and ssd1306 usage with max78000,
    using I2C protocol.
    
    Signed-off-by: Robert Budai <robert.budai@analog.com>

---
+++ projects/ssd1306/src/platform/maxim/parameters.h
@@ -0,0 +1,58 @@
+/***************************************************************************//**
+ *   @file   parameters.h
+ *   @brief  Definitions specific to Maxim platform used by ssd1306
+ *           project.
+ *   @author Robert Budai (robert.budai@analog.com)
+********************************************************************************
+ * Copyright 2025(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef __PARAMETERS_H__
+#define __PARAMETERS_H__
+
+#include "maxim_irq.h"
+#include "maxim_uart.h"
+#include "maxim_uart_stdio.h"
+#include "maxim_timer.h"
+#include "common_data.h"
+#include "no_os_util.h"
+#include "no_os_timer.h"
+
+// Used for debug purposes
+#define UART_DEVICE_ID	0
+#define UART_BAUDRATE	115200
+#define UART_IRQ_ID    	UART0_IRQn
+#define UART_EXTRA      &demo_uart_extra_ip
+#define UART_OPS        &max_uart_ops
+
+#define I2C_EXTRA      &oled_display_i2c_maxim_extra_param
+#define I2C_OPS        &max_i2c_ops
+
+extern struct max_uart_init_param demo_uart_extra_ip;
+extern struct max_i2c_init_param oled_display_i2c_maxim_extra_param;
+
+#endif /* __PARAMETERS_H__ */
---
+++ projects/ssd1306/src/platform/maxim/platform_src.mk
@@ -0,0 +1,35 @@
+SRCS += $(PLATFORM_DRIVERS)/maxim_irq.c			\
+	$(PLATFORM_DRIVERS)/maxim_timer.c			\
+	$(PLATFORM_DRIVERS)/maxim_delay.c			\
+	$(PLATFORM_DRIVERS)/maxim_uart.c			\
+	$(PLATFORM_DRIVERS)/maxim_i2c.c				\
+	$(PLATFORM_DRIVERS)/maxim_spi.c				\
+	$(PLATFORM_DRIVERS)/maxim_gpio.c			\
+	$(PLATFORM_DRIVERS)/maxim_init.c			\
+	$(PLATFORM_DRIVERS)/../common/maxim_dma.c	\
+	$(PLATFORM_DRIVERS)/maxim_uart_stdio.c
+	
+INCS += $(PLATFORM_DRIVERS)/maxim_irq.h			\
+	$(PLATFORM_DRIVERS)/maxim_uart.h			\
+	$(PLATFORM_DRIVERS)/maxim_i2c.h				\
+	$(PLATFORM_DRIVERS)/maxim_spi.h				\
+	$(PLATFORM_DRIVERS)/maxim_gpio.h			\
+	$(PLATFORM_DRIVERS)/../common/maxim_dma.h	\
+	$(PLATFORM_DRIVERS)/maxim_timer.h			\
+	$(PLATFORM_DRIVERS)/maxim_uart_stdio.h
+
+INCS += $(INCLUDE)/no_os_irq.h
+
+SRCS += $(DRIVERS)/api/no_os_irq.c
+SRCS += $(DRIVERS)/api/no_os_timer.c
+SRCS += $(DRIVERS)/api/no_os_i2c.c
+SRCS += $(DRIVERS)/api/no_os_spi.c
+SRCS += $(DRIVERS)/api/no_os_gpio.c
+
+SRCS += $(NO-OS)/util/no_os_lf256fifo.c
+
+ifeq '$(NO_OS_USB_UART)' 'y'
+SRCS += $(PLATFORM_DRIVERS)/maxim_usb_uart.c
+INCS += $(PLATFORM_DRIVERS)/maxim_usb_uart.h \
+	$(PLATFORM_DRIVERS)/maxim_usb_uart_descriptors.h
+endif
