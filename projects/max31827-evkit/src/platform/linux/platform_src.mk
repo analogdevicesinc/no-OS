0179dc5ed4eae52909dc3f25dbd11f4c97f23a4a
Radu Sabau <radu.sabau@analog.com>
2025-04-28 11:32:37 +0300

    projects: max31827-evkit : Fix project related CI errors
    Fix erroneous compilation for other platforms by moving ftd2xx
    drivers to supported platforms_src.mk only.
    Fix previous commit warnings of the platform update that comes
    with the new project structure.
    
    Fixes : 2231bd3 ("projects: max31827-evkit : Add support for FTD2XX platform")
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

--- projects/max31827-evkit/src/platform/linux/platform_src.mk
+++ projects/max31827-evkit/src/platform/linux/platform_src.mk
@@ -1,7 +1,12 @@
-
+ifneq ($(if $(findstring ftd2xx, $(LIBRARIES)), 1),)
+INCS += $(DRIVERS)/platform/ftd2xx/mpsse/ftd2xx_i2c.h
+SRCS += $(DRIVERS)/platform/ftd2xx/mpsse/ftd2xx_i2c.c	\
+	$(DRIVERS)/platform/ftd2xx/mpsse/ftd2xx_delay.c
+else
 INCS += $(PLATFORM_DRIVERS)/$(PLATFORM)_i2c.h		\
 	$(PLATFORM_DRIVERS)/$(PLATFORM)_uart.h
 
 SRCS += $(PLATFORM_DRIVERS)/$(PLATFORM)_delay.c		\
         $(PLATFORM_DRIVERS)/$(PLATFORM)_i2c.c		\
 	$(PLATFORM_DRIVERS)/$(PLATFORM)_uart.c
+endif
