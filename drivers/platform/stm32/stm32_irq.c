ea1e22a093ff9ad0035d2e9ea34c717efdad70f4
Naga Himanshu Indraganti <naga.indraganti@analog.com>
2025-05-28 12:44:25 +0530

    drivers:stm32:irq: Add support for Set and Clear Pending interrupt
    Added implementation for Set and Clear Pending interrupt APIs.
    
    Signed-off-by: Naga Himanshu Indraganti <naga.indraganti@analog.com>

--- drivers/platform/stm32/stm32_irq.c
+++ drivers/platform/stm32/stm32_irq.c
@@ -626,6 +626,34 @@ static int stm32_irq_get_priority(struct no_os_irq_ctrl_desc *desc,
 }
 
 /**
+ * @brief Clear the pending interrupt for an interrupt
+ * @param desc - Interrupt controller descriptor.
+ * @param irq_id - The interrupt vector entry id of the peripheral.
+ * @return 0
+ */
+static int stm32_irq_clear_pending(struct no_os_irq_ctrl_desc* desc,
+				   uint32_t irq_id)
+{
+	HAL_NVIC_ClearPendingIRQ(irq_id);
+
+	return 0;
+}
+
+/**
+ * @brief Set pending interrupt for an interrupt
+ * @param desc - Interrupt controller descriptor.
+ * @param irq_id - The interrupt vector entry id of the peripheral.
+ * @return 0
+ */
+static int stm32_irq_set_pending(struct no_os_irq_ctrl_desc* desc,
+				 uint32_t irq_id)
+{
+	HAL_NVIC_SetPendingIRQ(irq_id);
+
+	return 0;
+}
+
+/**
  * @brief stm32 specific IRQ platform ops structure
  */
 const struct no_os_irq_platform_ops stm32_irq_ops = {
@@ -639,5 +667,7 @@ const struct no_os_irq_platform_ops stm32_irq_ops = {
 	.disable = &stm32_irq_disable,
 	.set_priority = &stm32_irq_set_priority,
 	.get_priority = &stm32_irq_get_priority,
-	.remove = &stm32_irq_ctrl_remove
+	.remove = &stm32_irq_ctrl_remove,
+	.clear_pending = &stm32_irq_clear_pending,
+	.set_pending = &stm32_irq_set_pending
 };
