7cb3e9728f961b8759ff7e067a82203fcb76bb66
Darius Berghe <darius.berghe@analog.com>
2025-05-29 12:00:04 +0300

    add script to create all the .elf and .bin files needed for evaluation
    ./downloader.py /dev/ttyACM0 115200 A-bad.bin 0x10008000
    ./downloader.py /dev/ttyACM0 115200 A-slow.bin 0x10008000
    ./downloader.py /dev/ttyACM0 115200 B-fast.bin 0x10028000

---
+++ projects/dual-loader-app/binaries.sh
@@ -0,0 +1,40 @@
+make reset
+make -j BLINKING=slow LSCRIPT=A.ld bin
+cp build/dual-loader-app-A.elf A-slow.elf
+cp build/dual-loader-app-A.bin A-slow.bin
+
+make reset
+make -j BLINKING=slow LSCRIPT=B.ld bin
+cp build/dual-loader-app-B.elf B-slow.elf
+cp build/dual-loader-app-B.bin B-slow.bin
+
+
+make reset
+make -j BLINKING=normal LSCRIPT=A.ld bin
+cp build/dual-loader-app-A.elf A-normal.elf
+cp build/dual-loader-app-A.bin A-normal.bin
+
+make reset
+make -j BLINKING=normal LSCRIPT=B.ld bin
+cp build/dual-loader-app-B.elf B-normal.elf
+cp build/dual-loader-app-B.bin B-normal.bin
+
+make reset
+make -j BLINKING=fast LSCRIPT=A.ld bin
+cp build/dual-loader-app-A.elf A-fast.elf
+cp build/dual-loader-app-A.bin A-fast.bin
+
+make reset
+make -j BLINKING=fast LSCRIPT=B.ld bin
+cp build/dual-loader-app-B.elf B-fast.elf
+cp build/dual-loader-app-B.bin B-fast.bin
+
+cp A-slow.elf A-bad.elf
+dd if=/dev/urandom of=A-bad.elf bs=1 count=100 seek=1000 conv=notrunc
+cp A-slow.bin A-bad.bin
+dd if=/dev/urandom of=A-bad.bin bs=1 count=100 seek=1000 conv=notrunc
+
+cp B-slow.elf B-bad.elf
+dd if=/dev/urandom of=B-bad.elf bs=1 count=100 seek=1000 conv=notrunc
+cp B-slow.bin B-bad.bin
+dd if=/dev/urandom of=B-bad.bin bs=1 count=100 seek=1000 conv=notrunc
