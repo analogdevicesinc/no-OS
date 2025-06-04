3095226363e5e0e590414229df5ab1f8fcf66a8d
Darius Berghe <darius.berghe@analog.com>
2025-06-04 14:06:35 +0300

    use default .ld and .S and extend them

--- projects/dual-loader/src.mk
+++ projects/dual-loader/src.mk
@@ -1,4 +1,9 @@
 SRCS += $(PROJECT)/loader.c
 INCS += $(PROJECT)/loader.h
 LSCRIPT=DualImageLoader.ld
+
+# Add our loader function table
 ASM_SRCS=$(PROJECT)/DualImageLoader.S
+
+# The CMSIS startup (startup_max32672.S) will be included automatically 
+# by the build system through the platform makefiles
