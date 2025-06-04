936dff94936a8473c26e18f89a00e934d65fe807
Darius Berghe <darius.berghe@analog.com>
2025-06-04 14:05:31 +0300

    fix bug in binaries.sh

--- projects/dual-loader-app/binaries.sh
+++ projects/dual-loader-app/binaries.sh
@@ -30,11 +30,11 @@ cp build/dual-loader-app-B.elf B-fast.elf
 cp build/dual-loader-app-B.bin B-fast.bin
 
 cp A-slow.elf A-bad.elf
-dd if=/dev/urandom of=A-bad.elf bs=1 count=100 seek=1000 conv=notrunc
+dd if=/dev/urandom of=A-bad.elf bs=1 count=100000 seek=1000 conv=notrunc
 cp A-slow.bin A-bad.bin
 dd if=/dev/urandom of=A-bad.bin bs=1 count=100 seek=1000 conv=notrunc
 
 cp B-slow.elf B-bad.elf
-dd if=/dev/urandom of=B-bad.elf bs=1 count=100 seek=1000 conv=notrunc
+dd if=/dev/urandom of=B-bad.elf bs=1 count=100000 seek=1000 conv=notrunc
 cp B-slow.bin B-bad.bin
 dd if=/dev/urandom of=B-bad.bin bs=1 count=100 seek=1000 conv=notrunc
