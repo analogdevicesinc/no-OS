analogdevicesinc/no-OS

commit 54c826c18282fb0dd737fd357b23edfe5100301d

fix incremental build from Git Bash
The make that currently is being installed in Git Bash is unable
to look at the timestamp of the files pointed to by symlinks and
compare that to .o timestamps.

Instead, it's comparing the timestamp of the symlink themselves
to the timestamp of .o files.

Because everytime "make all" is invoked, "make update" is called
too, and because "make update" was redoing the symlinks under build/,
a full rebuild was triggered every time.

Fix this by:
 - not removing directories under build/, it's unnecessary
 - keep regenerating symlinks, as before, but make sure to change
   their timestamp to match the one of the source files they point
   to.

The proper make on any linux will ignore the timestamps of the symlinks
so this doesn't break the existing linux workflow. However it improves
the Windows workflow by allowing incremental builds.

It's not an ideal fix, it'd be better to change the tool but at this
point it's harder to change the tool than to do this fix in makefiles.

Signed-off-by: Darius Berghe <darius.berghe@analog.com>

tools/scripts/generic.mk
@@ -42,11 +42,11 @@ get_full_path = $(patsubst root/%,$(ROOT_DRIVE)%,$(FULL_PATH))
 ifeq 'y' '$(strip $(LINK_SRCS))'
 update_file = $(make_link)
 update_dir = $(make_dir_link)
-ACTION = Linking
+ACTION = symlinks to
 else
 update_file = $(call copy_file,$1,$(dir $2))
 update_dir = $(copy_dir)
-ACTION = Copying
+ACTION = copies of
 endif
 
 # Display the platform for which build is launched
@@ -379,17 +379,23 @@ define process_items_in_chunks
 	)
 endef
 
-define update_file_func
-$(call update_file,$(1),$(call relative_to_project,$(1)))
-endef
+SRC_FILES = $(sort $(SRCS) $(INCS) $(ASM_SRCS))
+BUILD_FILES = $(foreach file,$(SRC_FILES),$(call relative_to_project,$(file)))
+
+$(foreach file,$(BUILD_FILES), \
+	$(eval $(file): $(filter %/$(notdir $(file)),$(SRC_FILES))) \
+)
+
+$(BUILD_FILES): % :
+	$(call update_file,$<,$@)
+	@TS=$(shell stat --format=%Y $<); touch -h -d @$${TS} $@
+
+make_dirs:
+	$(call mk_dir,$(DIRS_TO_CREATE))
 
 PHONY += update
-update:
-	$(call print,$(ACTION) srcs to created project)
-	$(call remove_dir,$(DIRS_TO_REMOVE))
-	$(call mk_dir,$(DIRS_TO_CREATE))
-	$(call process_items_in_chunks,$(sort $(SRCS) $(INCS) $(ASM_SRCS)),10,update_file_func)
-	$(MAKE) --no-print-directory pre_build
+update: make_dirs $(BUILD_FILES) pre_build
+	$(call print,Creating $(ACTION) source files)
 
 standalone:
 	$(MAKE) --no-print-directory project LINK_SRCS=n

commit a244d7c26d01957c267a8a01d135fec609dc1b63

tools: scripts: Add dependency on make_dirs
The correct sequence in the build process is to first create the
directory structure for all the source files (in build/app), and then
create the symlinks for those files.

Since 54c826c1 ("fix incremental build from Git Bash"), the directory
and symlink creation have been moved to different make rules, however
there is no dependency between them. This results in compilation errors
whenever a multithreaded build is done:

[16:08:56] Building for maxim
[16:08:56] Building for target max32690
[16:08:56] Creating IDE project
ln: failed to create symbolic link 'Downloads/no-OS/projects/apard32690/build/app/noos/drivers/api/no_os_dma.c': No such file or directory
make[1]: *** [../../tools/scripts/generic.mk:391: Downloads/no-OS/projects/apard32690/build/app/noos/drivers/api/no_os_dma.c] Error 1
make[1]: *** Waiting for unfinished jobs....
make: *** [../../tools/scripts/generic.mk:267: all] Error 2

Fix the issue by adding the dependency on make_dirs.

Fixes: 54c826c1 ("fix incremental build from Git Bash")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

tools/scripts/generic.mk
@@ -386,7 +386,7 @@ $(foreach file,$(BUILD_FILES), \
 	$(eval $(file): $(filter %/$(notdir $(file)),$(SRC_FILES))) \
 )
 
-$(BUILD_FILES): % :
+$(BUILD_FILES): % : | make_dirs
 	$(call update_file,$<,$@)
 	@TS=$(shell stat --format=%Y $<); touch -h -d @$${TS} $@
 