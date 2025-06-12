c495838c443bf3a5dd9c09470ca8077ccc51b6f8
Ramona Alexandra Nechita <ramona.nechita@analog.com>
2025-06-12 14:51:45 +0300

    projects: ad9081: add different profile configurations
    Replace app_config.h in src folders with various config
    profiles in the profiles/ folder. Naming of the profile
    folders is the same as linux dts for same profile.
    
    Signed-off-by: Ramona Alexandra Nechita <ramona.nechita@analog.com>

---
+++ projects/ad9081/profiles/vcu118_quad_ad9081_204b_txmode_9_rxmode_10_revc/app_config.h
@@ -0,0 +1,111 @@
+/***************************************************************************//**
+ *   @file   app_config.h
+ *   @brief  AD9081 application example.
+ *   @author DBogdan (dragos.bogdan@analog.com)
+ *   @author Ramona Nechita (ramona.nechita@analog.com)
+********************************************************************************
+ * Copyright 2025(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef APP_CONFIG_H_
+#define APP_CONFIG_H_
+
+#define MULTIDEVICE_INSTANCE_COUNT	4
+
+#define CLKIN_J41_FREQ_HZ		500000000
+#define ADF4371_RFx_FREQUENCY_HZ	12000000000
+
+#define HMC7043_FPGA_XCVR_CLKDIV	1
+#define HMC7043_FPGA_LINK_CLKDIV_TX	2
+#define HMC7043_FPGA_LINK_CLKDIV_RX	2
+#define HMC7043_SYSREF_CLKDIV		256
+
+#define ADXCVR_REF_CLK_KHZ	CLKIN_J41_FREQ_HZ/1000
+#define ADXCVR_RX_DEV_CLK_KHZ	250000
+#define ADXCVR_TX_DEV_CLK_KHZ	250000
+#define ADXCVR_RX_LANE_CLK_KHZ	10000000
+#define ADXCVR_TX_LANE_CLK_KHZ	10000000
+
+#define AD9081_DAC_FREQUENCY	ADF4371_RFx_FREQUENCY_HZ
+#define AD9081_ADC_FREQUENCY	(ADF4371_RFx_FREQUENCY_HZ / 3)
+
+#define AD9081_ADC_NYQUIST_ZONE	{1, 1, 1, 1}
+
+/* TX path */
+
+#define AD9081_TX_JESD_MODE		9
+#define AD9081_TX_JESD_SUBCLASS		1
+#define AD9081_TX_JESD_VERSION		1
+#define AD9081_TX_JESD_M		8
+#define AD9081_TX_JESD_F		4
+#define AD9081_TX_JESD_K		32
+#define AD9081_TX_JESD_N		16
+#define AD9081_TX_JESD_NP		16
+#define AD9081_TX_JESD_CS		0
+#define AD9081_TX_JESD_L		4
+#define AD9081_TX_JESD_S		1
+#define AD9081_TX_JESD_HD		0
+#define AD9081_TX_LOGICAL_LANE_MAPPING	{0, 1, 2, 3, 4, 5, 6, 7}
+
+#define AD9081_JRX_TPL_PHASE_ADJUST	15
+
+#define AD9081_TX_MAIN_INTERPOLATION	8
+#define AD9081_TX_CHAN_INTERPOLATION	6
+#define AD9081_TX_MAIN_NCO_SHIFT	{2800000000, 2800000000, 2800000000, 2800000000}
+#define AD9081_TX_CHAN_NCO_SHIFT	{0, 0, 0, 0, 0, 0, 0, 0}
+#define AD9081_TX_CHAN_GAIN		{2048, 2048, 2048, 2048, 0, 0, 0, 0}
+
+#define AD9081_TX_FSC	{0, 0, 0, 0}
+
+#define AD9081_TX_DAC_CHAN_CROSSBAR	{0x1, 0x2, 0x4, 0x8}
+#define AD9081_TX_DAC_1X_NON1X_CROSSBAR	{0x1, 0x2, 0x4, 0x8}
+
+/* RX path */
+
+#define AD9081_RX_JESD_MODE		10
+#define AD9081_RX_JESD_SUBCLASS		1
+#define AD9081_RX_JESD_VERSION		1
+#define AD9081_RX_JESD_M		8
+#define AD9081_RX_JESD_F		4
+#define AD9081_RX_JESD_K		32
+#define AD9081_RX_JESD_N		16
+#define AD9081_RX_JESD_NP		16
+#define AD9081_RX_JESD_CS		0
+#define AD9081_RX_JESD_L		4
+#define AD9081_RX_JESD_S		1
+#define AD9081_RX_JESD_HD		0
+#define AD9081_RX_LOGICAL_LANE_MAPPING	{0, 1, 2, 3, 4, 5, 6, 7}
+#define AD9081_RX_LINK_CONVERTER_SELECT	{0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+
+#define AD9081_RX_MAIN_DECIMATION	{4, 4, 4, 4}
+#define AD9081_RX_CHAN_DECIMATION	{4, 4, 0, 0, 4, 4, 0, 0}
+#define AD9081_RX_MAIN_ENABLE		{1, 1, 1, 1}
+#define AD9081_RX_CHAN_ENABLE		{1, 1, 0, 0, 1, 1, 0, 0}
+#define AD9081_RX_MAIN_NCO_SHIFT	{1000000000, 1000000000, 1000000000, 1000000000}
+#define AD9081_RX_CHAN_NCO_SHIFT	{0, 0, 0, 0, 0, 0, 0, 0}
+
+#endif
