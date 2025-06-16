0f63829b6ac0d1d1d3ebb8483e24ca9d4e4addb8
Darius Berghe <darius.berghe@analog.com>
2025-06-16 13:48:16 +0300

    maxim.mk: allow multiple linker scripts
    The LSCRIPT was a simple variable, treat it as a list of .ld script
    filenames.
    
    Signed-off-by: Darius Berghe <darius.berghe@analog.com>

--- tools/scripts/maxim.mk
+++ tools/scripts/maxim.mk
@@ -106,7 +106,7 @@ endif
 
 INCS += $(foreach dir,$(DRIVER_INCLUDE_DIR), $(wildcard $(dir)/*.h))
 
-LSCRIPT = $(MAXIM_LIBRARIES)/CMSIS/Device/Maxim/$(TARGET_UCASE)/Source/GCC/$(TARGET_LCASE).ld
+LSCRIPT += $(MAXIM_LIBRARIES)/CMSIS/Device/Maxim/$(TARGET_UCASE)/Source/GCC/$(TARGET_LCASE).ld
 ASM_SRCS += $(MAXIM_LIBRARIES)/CMSIS/Device/Maxim/$(TARGET_UCASE)/Source/GCC/startup_$(TARGET_LCASE).S
 SRCS += $(MAXIM_LIBRARIES)/CMSIS/Device/Maxim/$(TARGET_UCASE)/Source/heap.c
 SRCS += $(MAXIM_LIBRARIES)/CMSIS/Device/Maxim/$(TARGET_UCASE)/Source/system_$(TARGET_LCASE).c
