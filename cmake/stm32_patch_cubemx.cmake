512cb3e897de87bacc57691ee9554878b6609ea2
Ciprian Regus <ciprian.regus@analog.com>
2025-07-16 17:17:16 +0300

    stm32 build fixed, still some things hardcoded

--- cmake/stm32_patch_cubemx.cmake
+++ cmake/stm32_patch_cubemx.cmake
@@ -10,4 +10,14 @@ string(REPLACE "\${CMAKE_SOURCE_DIR}" "/mnt/drive/testing/no-OS/build/test_build
 # Write the corrected content back to the file
 file(WRITE ${CMAKE_FILE_TO_PATCH} "${PATCHED_CONTENTS}")
 
+file(READ "/mnt/drive/testing/no-OS/build/test_build/app/Core/Src/main.c" MAIN_FILE_CONTENTS)
+
+# Replace all instances of the incorrect variable with the correct one
+string(REPLACE "main(" "stm32_init("
+     MAIN_PATCHED_CONTENTS "${MAIN_FILE_CONTENTS}")
+
+file(WRITE "/mnt/drive/testing/no-OS/build/test_build/app/Core/Src/main.c" "${MAIN_PATCHED_CONTENTS}")
+
+# Write the corrected content back to the file
+
 message(STATUS "Patched ${CMAKE_FILE_TO_PATCH} to use CMAKE_CURRENT_SOURCE_DIR.")
