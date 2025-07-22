6eacb75210a81b364c50713e4763ab5133c3ecc3
Ciprian Regus <ciprian.regus@analog.com>
2025-07-22 14:34:20 +0300

    Move stm32 cubemx logic in cmake/
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

--- cmake/stm32_patch_cubemx_config.cmake
+++ cmake/stm32_patch_cubemx_config.cmake
@@ -1,6 +1,15 @@
 file(READ ${STM32_TEMPLATE_FILE} OLD_CONTENT)
 
-set(CUBEMX_CONFIG_NAME "config load ${SOURCE_DIR}/${BOARD}.ioc\n")
+if(${STM32_IOC_PATH})
+        if(IS_ABSOLUTE ${STM32_IOC_PATH})
+                set(CUBEMX_CONFIG_NAME "config load ${STM32_IOC_PATH}\n")
+        else()
+                set(CUBEMX_CONFIG_NAME "config load ${SOURCE_DIR}/${STM32_IOC_PATH}\n")
+        endif()
+else()
+        set(CUBEMX_CONFIG_NAME "config load ${SOURCE_DIR}/${BOARD}.ioc\n")
+endif()
+
 set(CUBEMX_PROJECT_PATH "project path ${STM32_TARGET_BUILD}/${BOARD}_build\n")
 
 file(WRITE "${STM32_TARGET_BUILD}/stm32cubemx_config.cubemx" "${CUBEMX_CONFIG_NAME}${CUBEMX_PROJECT_PATH}${OLD_CONTENT}")
