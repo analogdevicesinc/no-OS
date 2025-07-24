2ed123983d8410cc061e627ef605daafaa82dfe8
Radu Sabau <radu.sabau@analog.com>
2025-07-24 15:52:03 +0300

    drivers: adc: ad7490 : Add IIO support for AD7490
    Add initial header and source files for AD7490 no-OS IIO driver.
    
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

---
+++ drivers/adc/ad7490/iio_ad7490.h
@@ -0,0 +1,62 @@
+/**************************************************************************//**
+*   @file   iio_ad7490.h
+*   @brief  Header file of iio_ad7490
+*   @author Radu Sabau (radu.sabau@analog.com)
+*******************************************************************************
+* Copyright 2025(c) Analog Devices, Inc.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+*
+* 3. Neither the name of Analog Devices, Inc. nor the names of its
+*    contributors may be used to endorse or promote products derived from this
+*    software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+* EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+* OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#ifndef IIO_AD7490_H
+#define IIO_AD7490_H
+
+#include "iio.h"
+#include "iio_types.h"
+#include "ad7490.h"
+
+struct ad7490_iio_desc {
+	struct ad7490_desc *ad7490_desc;
+	struct iio_device *iio_dev;
+
+	uint32_t active_channels;
+	uint8_t no_of_active_channels;
+};
+
+struct ad7490_iio_init_param {
+	struct ad7490_init_param *ad7490_init;
+
+	enum ad7490_dout_state dout_state;
+	enum ad7490_range range;
+	enum ad7490_coding coding;
+};
+
+int ad7490_iio_init(struct ad7490_iio_desc **iio_desc,
+		    struct ad7490_iio_init_param *init_param);
+
+int ad7490_iio_remove(struct ad7490_iio_desc *iio_desc);
+
+#endif /** IIO_AD7490_H */
