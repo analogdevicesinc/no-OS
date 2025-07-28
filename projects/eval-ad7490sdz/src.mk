e44240ad650ea24b6d44d5085c5f7f15f2016e10
Radu Sabau <radu.sabau@analog.com>
2025-07-28 11:39:27 +0300

    projects: eval-ad7490sdz : Add project files for EVAL-AD7490SDZ
    Add initial header and source files alongside Makefiles for
    EVAL-AD7490SDZ project.
    
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

---
+++ projects/eval-ad7490sdz/src.mk
@@ -0,0 +1,28 @@
+INCS += $(INCLUDE)/no_os_delay.h	\
+	$(INCLUDE)/no_os_error.h	\
+	$(INCLUDE)/no_os_gpio.h		\
+	$(INCLUDE)/no_os_print_log.h	\
+	$(INCLUDE)/no_os_spi.h		\
+	$(INCLUDE)/no_os_alloc.h	\
+	$(INCLUDE)/no_os_irq.h		\
+	$(INCLUDE)/no_os_list.h		\
+	$(INCLUDE)/no_os_dma.h		\
+	$(INCLUDE)/no_os_uart.h		\
+	$(INCLUDE)/no_os_lf256fifo.h	\
+	$(INCLUDE)/no_os_util.h		\
+	$(INCLUDE)/no_os_units.h	\
+	$(INCLUDE)/no_os_mutex.h
+
+SRCS += $(DRIVERS)/api/no_os_gpio.c	\
+	$(NO-OS)/util/no_os_lf256fifo.c	\
+	$(DRIVERS)/api/no_os_irq.c	\
+	$(DRIVERS)/api/no_os_spi.c	\
+	$(DRIVERS)/api/no_os_uart.c	\
+	$(DRIVERS)/api/no_os_dma.c	\
+	$(NO-OS)/util/no_os_list.c	\
+	$(NO-OS)/util/no_os_util.c	\
+	$(NO-OS)/util/no_os_alloc.c	\
+	$(NO-OS)/util/no_os_mutex.c
+
+INCS += $(DRIVERS)/adc/ad7490/ad7490.h
+SRCS += $(DRIVERS)/adc/ad7490/ad7490.c
