b06dfdd569fbf4c3693e1f5841a14d5f4cbf05b9
Wilbert Jethro Limjoco <Wilbertjethro.Limjoco@analog.com>
2025-09-18 10:33:43 +0800

    projects: max20362: Add project for MAX20362
    We add a basic example running on MAX32690EVKIT MCU to show how the
    MAX20362's features might be used.
    
    Co-authored-by: Wilbert Jethro Limjoco <Wilbertjethro.Limjoco@analog.com>
    Signed-off-by: Wilford Abilay <Wilford.Abilay@analog.com>

---
+++ projects/max20362/src/platform/maxim/parameters.h
@@ -0,0 +1,39 @@
+/*******************************************************************************
+ *   @file   parameters.h
+ *   @brief  Definitions used by the MAX20362 project.
+ *   @author Wilbert Jethro R. Limjoco (Wilbertjethro.Limjoco@analog.com)
+********************************************************************************
+* Copyright 2026(c) Analog Devices, Inc.
+*
+* SPDX-License-Identifier: BSD-3-Clause
+*******************************************************************************/
+#ifndef __PARAMETERS_H__
+#define __PARAMETERS_H__
+
+#include "maxim_gpio.h"
+#include "maxim_i2c.h"
+#include "maxim_uart.h"
+
+#define UART_DEVICE_ID                         3
+#define UART_ASYNC_RX                          false
+#define UART_BAUDRATE                          115200
+#define UART_OPS                               &max_uart_ops
+
+#define I2C_OPS                                &max_i2c_ops
+#define GPIO_OPS                               &max_gpio_ops
+
+/* MAX20362 parameters */
+#define MAX20362_I2C_DEVICE_ID                 0
+#define MAX20362_I2C_CLK_SPEED                 400000
+#define MAX20362_BBOUT_DEFAULT_UV              3300000      /* 3.3V default */
+#define MAX20362_VCAP_DEFAULT_UV               5000000  	/* 5V default */
+#define MAX20362_LDO_DEFAULT_UV                1800000  	/* 1.8V default */
+#define MAX20362_ILIM_DEFAULT_MA               20  			/* 20mA default */
+#define MAX20362_BBOUT_ENABLE_DEFAULT          false  		/* Start disabled for testing */
+#define MAX20362_LDO_ENABLE_DEFAULT            false  		/* Start disabled for testing */
+
+extern struct max_uart_init_param uart_extra_ip;
+extern const struct max_i2c_init_param max20362_i2c_extra;
+extern const struct max_gpio_init_param max20362_gpio_extra;
+
+#endif // __PARAMETERS_H__
