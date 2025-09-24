b7bae3d10c03bf45cff2d6587ed2ad9c0af42bb8
Radu Sabau <radu.sabau@analog.com>
2025-09-24 15:36:36 +0300

    projects: adf4371 : Add example project for ADF4371
    Add project folder including all required files for an example project
    regarding ADF4371.
    
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

---
+++ projects/adf4371/src.mk
@@ -0,0 +1,34 @@
+SRCS += $(DRIVERS)/api/no_os_uart.c     \
+        $(DRIVERS)/api/no_os_gpio.c     \
+	$(DRIVERS)/api/no_os_spi.c  	\
+	$(DRIVERS)/api/no_os_irq.c 	\
+	$(DRIVERS)/api/no_os_timer.c	\
+        $(DRIVERS)/api/no_os_dma.c      \
+        $(NO-OS)/util/no_os_clk.c      \
+        $(NO-OS)/util/no_os_fifo.c      \
+	$(NO-OS)/util/no_os_mutex.c     \
+	$(NO-OS)/util/no_os_lf256fifo.c \
+        $(NO-OS)/util/no_os_list.c      \
+        $(NO-OS)/util/no_os_util.c      \
+        $(NO-OS)/util/no_os_alloc.c
+
+INCS += $(INCLUDE)/no_os_clk.h     \
+        $(INCLUDE)/no_os_delay.h     \
+        $(INCLUDE)/no_os_error.h     \
+	$(INCLUDE)/no_os_gpio.h      \
+	$(INCLUDE)/no_os_mutex.h     \
+	$(INCLUDE)/no_os_spi.h       \
+        $(INCLUDE)/no_os_fifo.h      \
+        $(INCLUDE)/no_os_irq.h       \
+        $(INCLUDE)/no_os_lf256fifo.h \
+        $(INCLUDE)/no_os_list.h      \
+	$(INCLUDE)/no_os_print_log.h \
+        $(INCLUDE)/no_os_timer.h     \
+        $(INCLUDE)/no_os_uart.h      \
+        $(INCLUDE)/no_os_util.h      \
+        $(INCLUDE)/no_os_units.h     \
+        $(INCLUDE)/no_os_dma.h       \
+        $(INCLUDE)/no_os_alloc.h
+
+INCS += $(DRIVERS)/frequency/adf4371/adf4371.h
+SRCS += $(DRIVERS)/frequency/adf4371/adf4371.c
