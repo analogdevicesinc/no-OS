deb398126e533164066e4386572afa3eb3ab0d6f
Cherrence Sarip <cherrence.sarip@analog.com>
2025-10-20 19:39:47 +0800

    projects: ltc2378: Add project for LTC2378
    Add initial project files for both basic and IIO examples for LTC2378.
    
    Signed-off-by: Cherrence Sarip <cherrence.sarip@analog.com>

---
+++ projects/ltc2378/src/platform/maxim/parameters.h
@@ -0,0 +1,73 @@
+/********************************************************************************
+ *   @file   parameters.h
+ *   @brief  Definitions specific to Maxim platform used by ltc2378 project.
+ *   @author Cherrence Sarip (cherrence.sarip@analog.com)
+ ********************************************************************************
+ * Copyright 2025(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *******************************************************************************/
+#ifndef __PARAMETERS_H__
+#define __PARAMETERS_H__
+
+#include "maxim_uart.h"
+#include "maxim_uart_stdio.h"
+#include "maxim_spi.h"
+#include "maxim_gpio.h"
+
+#ifdef IIO_SUPPORT
+#define INTC_DEVICE_ID	0
+#endif
+
+#define UART_DEVICE_ID		1
+#define UART_IRQ_ID		UART1_IRQn
+#define UART_BAUDRATE		115200
+#define	UART_OPS		&max_uart_ops
+#define UART_EXTRA		&max_uart_extra
+
+#define SPI_DEVICE_ID	1
+#define SPI_CS		0
+
+#define SPI_MAX_SPEED	1000000  /* 1 MHz SPI clock */
+#define SPI_OPS		&max_spi_ops
+#define SPI_EXTRA	&max_spi_extra
+
+/* Hardware configuration constants */
+#define LTC2378_DEFAULT_VREF_UV	2500000  /* 2.5V reference voltage in microvolts */
+#define BASIC_EXAMPLE_DELAY_MS	500      /* Delay between measurements in basic example */
+
+#define GPIO_CNV_PORT_NUM  0
+#define GPIO_CNV_PIN_NUM   9
+#define GPIO_BUSY_PORT_NUM 0
+#define GPIO_BUSY_PIN_NUM  10
+#define GPIO_OPS	&max_gpio_ops
+#define GPIO_EXTRA 	&max_gpio_extra
+
+extern struct max_uart_init_param max_uart_extra;
+extern struct max_spi_init_param max_spi_extra;
+extern struct max_gpio_init_param max_gpio_extra;
+
+#endif /* __PARAMETERS_H__ */
