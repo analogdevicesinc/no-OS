679b83a172cdefe6f6e421ab795885f1724f6230
Jan Carlo Roleda <jancarlo.roleda@analog.com>
2025-11-24 23:01:23 +0800

    projects: led: ltc3220: add project for LTC3220
    Created LTC3220 project example for Maxim platform
    Includes tests for the different features of the LTC3220
    
    Signed-off-by: Jan Carlo Roleda <jancarlo.roleda@analog.com>

---
+++ projects/ltc3220/src.mk
@@ -0,0 +1,28 @@
+INCS += $(INCLUDE)/no_os_error.h	\
+		$(INCLUDE)/no_os_gpio.h		\
+		$(INCLUDE)/no_os_irq.h		\
+		$(INCLUDE)/no_os_list.h		\
+		$(INCLUDE)/no_os_uart.h		\
+		$(INCLUDE)/no_os_lf256fifo.h\
+		$(INCLUDE)/no_os_dma.h		\
+		$(INCLUDE)/no_os_init.h		\
+		$(INCLUDE)/no_os_util.h		\
+		$(INCLUDE)/no_os_i2c.h		\
+		$(INCLUDE)/no_os_mutex.h	\
+		$(INCLUDE)/no_os_alloc.h	\
+		$(INCLUDE)/no_os_delay.h
+
+SRCS += $(DRIVERS)/api/no_os_gpio.c		\
+		$(DRIVERS)/api/no_os_irq.c		\
+		$(NO-OS)/util/no_os_list.c		\
+		$(DRIVERS)/api/no_os_uart.c		\
+		$(NO-OS)/util/no_os_lf256fifo.c	\
+		$(DRIVERS)/api/no_os_dma.c		\
+		$(DRIVERS)/api/no_os_i2c.c		\
+		$(NO-OS)/util/no_os_util.c		\
+		$(NO-OS)/util/no_os_mutex.c		\
+		$(NO-OS)/util/no_os_alloc.c
+		
+
+INCS += $(DRIVERS)/led/ltc3220/ltc3220.h
+SRCS += $(DRIVERS)/led/ltc3220/ltc3220.c
---
+++ projects/ltc3220/src/platform/maxim/parameters.h
@@ -0,0 +1,69 @@
+/***************************************************************************//**
+ *   @file   parameters.h
+ *   @brief  Definition header of Maxim platform data used by LTC3220 project
+ *   @author Jan Carlo Roleda (jancarlo.roleda@analog.com)
+********************************************************************************
+ * Copyright 2025(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef __PARAMETERS_H__
+#define __PARAMETERS_H__
+
+#include "maxim_i2c.h"
+#include "maxim_gpio.h"
+
+#ifdef DEVICE_VARIANT_1
+#define DEV_I2C_ADDRESS     0x1D
+#else
+#define DEV_I2C_ADDRESS     0x1C
+#endif
+
+#if (TARGET_NUM == 32650) || (TARGET_NUM == 78000)
+#define I2C_DEVICE_ID    1
+#elif (TARGET_NUM == 32655)
+#define I2C_DEVICE_ID    2
+#elif (TARGET_NUM == 32665) || (TARGET_NUM == 32660) || (TARGET_NUM == 32690)
+#define I2C_DEVICE_ID    0
+#endif
+
+#define I2C_OPS         &max_i2c_ops
+
+#define GPIO_OPS        &max_gpio_ops
+
+#if (TARGET_NUM == 32690)
+#define GPIO_RST_PIN_NUM    14
+#define GPIO_RST_PIN_PORT   0
+#elif (TARGET_NUM == 32655)
+#define GPIO_RST_PIN_NUM    9
+#define GPIO_RST_PIN_PORT   1
+#endif
+
+extern struct max_gpio_init_param ltc3220_gpio_extra;
+
+extern struct max_i2c_init_param ltc3220_i2c_extra;
+
+#endif /* __PARAMETERS_H__ */
