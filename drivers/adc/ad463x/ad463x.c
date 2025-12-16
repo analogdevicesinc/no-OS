analogdevicesinc/no-OS

commit fe417e15292bc52fef1e1ae4f56bf3be27e357cd

adc: ad463x: initial commit
Add AD463x driver.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad463x/ad463x.c
@@ -1,0 +1,543 @@
+/***************************************************************************//**
+ *   @file   ad463x.c
+ *   @brief  Implementation of AD463x Driver.
+ *   @author Antoniu Miclaus (antoniu.miclaus@analog.com)
+********************************************************************************
+ * Copyright 2021(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include "error.h"
+#include "delay.h"
+#include "ad463x.h"
+#include "print_log.h"
+
+/******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+#define AD463x_TEST_DATA 0xAA
+
+/******************************************************************************/
+/************************* Functions Definitions ******************************/
+/******************************************************************************/
+
+/**
+ * @brief Read device register.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param reg_data - The data read from the register.
+ * @return SUCCESS in case of success, negative error code otherwise.
+ */
+int32_t ad463x_spi_reg_read(struct ad463x_dev *dev,
+			    uint16_t reg_addr,
+			    uint8_t *reg_data)
+{
+	int32_t ret;
+	uint8_t buf[3];
+
+	buf[0] = AD463X_REG_READ| ((reg_addr >> 8) & 0x7F);
+	buf[1] = (uint8_t)reg_addr;
+	buf[2] = AD463X_REG_READ_DUMMY;
+
+	ret = spi_write_and_read(dev->spi_desc, buf, 3);
+	*reg_data = buf[2];
+
+	return ret;
+}
+
+/**
+ * @brief Write device register.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param reg_data - The register data.
+ * @return SUCCESS in case of success, negative error code otherwise.
+ */
+int32_t ad463x_spi_reg_write(struct ad463x_dev *dev,
+			     uint16_t reg_addr,
+			     uint8_t reg_data)
+{
+	uint8_t buf[3];
+
+	buf[0] = ((reg_addr >> 8) & 0x7F);
+	buf[1] = (uint8_t)reg_addr;
+	buf[2] = reg_data;
+
+	return spi_write_and_read(dev->spi_desc, buf, 3);
+}
+
+/**
+ * @brief SPI write device register using a mask.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param mask - The mask.
+ * @param data - The register data.
+ * @return SUCCESS in case of success, negative error code otherwise.
+ */
+int32_t ad463x_spi_reg_write_masked(struct ad463x_dev *dev,
+				    uint16_t reg_addr,
+				    uint8_t mask,
+				    uint8_t data)
+{
+	uint8_t reg_data;
+	int32_t ret;
+
+	ret = ad463x_spi_reg_read(dev, reg_addr, &reg_data);
+	if (ret != SUCCESS)
+		return ret;
+
+	reg_data &= ~mask;
+	reg_data |= data;
+
+	return ad463x_spi_reg_write(dev, reg_addr, reg_data);
+}
+
+/**
+ * @brief Set power mode.
+ * @param dev - The device structure.
+ * @param mode - The power mode.
+ * @return SUCCESS in case of success, negative error code otherwise.
+ */
+int32_t ad463x_set_pwr_mode(struct ad463x_dev *dev, uint8_t mode)
+{
+	if ((mode != AD463X_NORMAL_MODE) && (mode != AD463X_LOW_POWER_MODE))
+		return FAILURE;
+
+	return ad463x_spi_reg_write(dev, AD463X_REG_DEVICE_CONFIG, mode);
+}
+
+/**
+ * @brief Set average frame length.
+ * @param dev - The device structure.
+ * @param mode - Average filter frame length mode.
+ * @return SUCCESS in case of success, negative error code otherwise.
+ */
+int32_t ad463x_set_avg_frame_len(struct ad463x_dev *dev, uint8_t mode)
+{
+	int32_t ret;
+
+	if (mode == AD463X_NORMAL_MODE) {
+		ret = ad463x_spi_reg_write_masked(dev,
+						  AD463X_REG_MODES,
+						  AD463X_OUT_DATA_MODE_MSK,
+						  AD463X_24_DIFF_8_COM);
+		if (ret != SUCCESS)
+			return ret;
+		ret = ad463x_spi_reg_write(dev,
+					   AD463X_REG_AVG,
+					   AD463X_AVG_FILTER_RESET);
+	} else {
+		if(mode < 1 || mode > 16)
+			return -EINVAL;
+		ret = ad463x_spi_reg_write_masked(dev,
+						  AD463X_REG_MODES,
+						  AD463X_OUT_DATA_MODE_MSK,
+						  AD463X_30_AVERAGED_DIFF);
+		if (ret != SUCCESS)
+			return ret;
+		ret = ad463x_spi_reg_write(dev,
+					   AD463X_REG_AVG,
+					   mode);
+	}
+
+	return ret;
+}
+
+/**
+ * @brief Set drive strength.
+ * @param dev - The device structure.
+ * @param mode - The register data.
+ * @return SUCCESS in case of success, negative error code otherwise.
+ */
+int32_t ad463x_set_drive_strength(struct ad463x_dev *dev, uint8_t mode)
+{
+	if((mode != AD463X_NORMAL_OUTPUT_STRENGTH)
+	    && (mode != AD463X_DOUBLE_OUTPUT_STRENGTH))
+		return -EINVAL;
+
+	return ad463x_spi_reg_write_masked(dev, AD463X_REG_IO,
+					   AD463X_DRIVER_STRENGTH_MASK, mode);
+}
+
+/**
+ * @brief Exit register configuration mode.
+ * @param dev - The device structure.
+ * @return SUCCESS in case of success, negative error code otherwise.
+ */
+int32_t ad463x_exit_reg_cfg_mode(struct ad463x_dev *dev)
+{
+	int32_t ret;
+
+	ret = ad463x_spi_reg_write(dev, AD463X_REG_EXIT_CFG_MODE,
+				   AD463X_EXIT_CFG_MODE);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = spi_engine_set_transfer_width(dev->spi_desc,
+					    dev->capture_data_width);
+	if (ret != SUCCESS)
+		return ret;
+
+	spi_engine_set_speed(dev->spi_desc, dev->spi_desc->max_speed_hz);
+
+	return ret;
+}
+
+/**
+ * @brief Set channel gain
+ * @param dev - The device structure.
+ * @param ch_idx - The channel index.
+ * @param gain - The gain value scaled by 10000.
+ * 			Example: to set gain 1.5, use 150000
+ * @return SUCCESS in case of success, negative error code otherwise.
+ */
+int32_t ad463x_set_ch_gain(struct ad463x_dev *dev, uint8_t ch_idx,
+			   uint64_t gain)
+{
+	int32_t ret;
+	uint32_t g;
+
+	if (gain < 0 || gain > AD463X_GAIN_MAX_VAL_SCALED)
+		return -EINVAL;
+
+	g = ((gain * 0xFFFF) / AD463X_GAIN_MAX_VAL_SCALED);
+
+	ret = ad463x_spi_reg_write(dev,
+				   AD463X_REG_CHAN_GAIN(ch_idx, AD463X_GAIN_LSB),
+				   g);
+	if (ret != SUCCESS)
+		return ret;
+
+	return ad463x_spi_reg_write(dev,
+				    AD463X_REG_CHAN_GAIN(ch_idx, AD463X_GAIN_MSB),
+				    g >> 8);
+}
+
+/**
+ * @brief Set channel offset
+ * @param dev - The device structure.
+ * @param ch_idx - The channel index.
+ * @param offset - The channel offset.
+ * @return SUCCESS in case of success, negative error code otherwise.
+ */
+int32_t ad463x_set_ch_offset(struct ad463x_dev *dev, uint8_t ch_idx,
+			     uint32_t offset)
+{
+	int32_t ret;
+
+	ret = ad463x_spi_reg_write(dev,
+				   AD463X_REG_CHAN_OFFSET(ch_idx, AD463X_OFFSET_0),
+				   offset);
+	if (ret < 0)
+		return ret;
+
+	ret = ad463x_spi_reg_write(dev,
+				   AD463X_REG_CHAN_OFFSET(ch_idx, AD463X_OFFSET_1),
+				   offset >> 8);
+	if (ret < 0)
+		return ret;
+
+	return ad463x_spi_reg_write(dev,
+				    AD463X_REG_CHAN_OFFSET(ch_idx, AD463X_OFFSET_2),
+				    offset >> 16);
+}
+
+
+/**
+ * @brief Initialize GPIO driver handlers for the GPIOs in the system.
+ *        ad463x_init() helper function.
+ * @param [out] dev - ad463x_dev device handler.
+ * @param [in] init_param - Pointer to the initialization structure.
+ * @return \ref SUCCESS in case of success, \ref FAILURE otherwise.
+ */
+static int32_t ad463x_init_gpio(struct ad463x_dev *dev,
+				struct ad463x_init_param *init_param)
+{
+	int32_t ret;
+
+	ret = gpio_get_optional(&dev->gpio_resetn, init_param->gpio_resetn);
+	if (ret != SUCCESS)
+		return ret;
+
+	/** Reset to configure pins */
+	if (dev->gpio_resetn) {
+		ret = gpio_direction_output(dev->gpio_resetn, GPIO_LOW);
+		if (ret != SUCCESS)
+			return ret;
+
+		mdelay(100);
+		ret = gpio_set_value(dev->gpio_resetn, GPIO_HIGH);
+		if (ret != SUCCESS)
+			return ret;
+
+		mdelay(100);
+	}
+
+	return SUCCESS;
+}
+
+/**
+ * @brief Read from device.
+ *        Enter register mode to read/write registers
+ * @param [in] dev - ad469x_dev device handler.
+ * @param [out] buf - data buffer.
+ * @param [in] samples - sample number.
+ * @return \ref SUCCESS in case of success, \ref FAILURE otherwise.
+ */
+int32_t ad463x_read_data(struct ad463x_dev *dev,
+			 uint32_t *buf,
+			 uint16_t samples)
+{
+	int32_t ret;
+	uint32_t commands_data[1] = {0};
+	struct spi_engine_offload_message msg;
+	uint32_t spi_eng_msg_cmds[3] = {
+		CS_LOW,
+		READ(dev->read_bytes_no),
+		CS_HIGH
+	};
+
+	ret = pwm_enable(dev->trigger_pwm_desc);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = spi_engine_offload_init(dev->spi_desc, dev->offload_init_param);
+	if (ret != SUCCESS)
+		return ret;
+
+	msg.commands = spi_eng_msg_cmds;
+	msg.no_commands = ARRAY_SIZE(spi_eng_msg_cmds);
+	msg.rx_addr = (uint32_t)buf;
+	msg.commands_data = commands_data;
+
+	ret = spi_engine_offload_transfer(dev->spi_desc, msg, samples);
+	if (ret != SUCCESS)
+		return ret;
+
+	if (dev->dcache_invalidate_range)
+		dev->dcache_invalidate_range(msg.rx_addr, samples * 2);
+
+	return ret;
+}
+
+/**
+ * @brief Initialize the device.
+ * @param [out] device - The device structure.
+ * @param [in] init_param - The structure that contains the device initial
+ * 		parameters.
+ * @return \ref SUCCESS in case of success, \ref FAILURE otherwise.
+ */
+int32_t ad463x_init(struct ad463x_dev **device,
+		    struct ad463x_init_param *init_param)
+{
+	struct ad463x_dev *dev;
+	int32_t ret;
+	uint8_t data = 0;
+	uint8_t sample_width;
+
+	if (!init_param || !device)
+		return FAILURE;
+
+	if (init_param->clock_mode == AD463X_SPI_COMPATIBLE_MODE &&
+	    init_param->data_rate == AD463X_DDR_MODE) {
+		pr_err("DDR_MODE not available when clock mode is SPI_COMPATIBLE\n");
+		return FAILURE;
+	}
+
+	dev = (struct ad463x_dev *)malloc(sizeof(*dev));
+	if (!dev)
+		return FAILURE;
+
+	/** Perform Hardware Reset */
+	ret = ad463x_init_gpio(dev, init_param);
+	if (ret != SUCCESS)
+		goto error_dev;
+
+	ret = axi_clkgen_init(&dev->clkgen, init_param->clkgen_init);
+	if (ret != SUCCESS) {
+		pr_err("error: %s: axi_clkgen_init() failed\n",
+		       init_param->clkgen_init->name);
+		goto error_gpio;
+	}
+
+	ret = axi_clkgen_set_rate(dev->clkgen, init_param->axi_clkgen_rate);
+	if (ret != SUCCESS) {
+		pr_err("error: %s: axi_clkgen_set_rate() failed\n",
+		       init_param->clkgen_init->name);
+		goto error_clkgen;
+	}
+
+	ret = spi_init(&dev->spi_desc, init_param->spi_init);
+	if (ret != SUCCESS)
+		goto error_clkgen;
+
+	dev->offload_init_param = init_param->offload_init_param;
+	dev->reg_access_speed = init_param->reg_access_speed;
+	dev->reg_data_width = init_param->reg_data_width;
+	dev->output_mode = init_param->output_mode;
+	dev->lane_mode = init_param->lane_mode;
+	dev->clock_mode = init_param->clock_mode;
+	dev->data_rate = init_param->data_rate;
+	dev->device_id = init_param->device_id;
+	dev->dcache_invalidate_range = init_param->dcache_invalidate_range;
+
+	if (dev->output_mode > AD463X_16_DIFF_8_COM)
+		sample_width = 32;
+	else
+		sample_width = 24;
+
+	switch (dev->lane_mode) {
+	case AD463X_ONE_LANE_PER_CH:
+		dev->capture_data_width = sample_width;
+		break;
+
+	case AD463X_TWO_LANES_PER_CH:
+		dev->capture_data_width = sample_width / 2;
+		break;
+
+	case AD463X_FOUR_LANES_PER_CH:
+		dev->capture_data_width = sample_width / 4;
+		break;
+
+	case AD463X_SHARED_TWO_CH:
+		dev->capture_data_width = sample_width * 2;
+		break;
+	default:
+		goto error_spi;
+		break;
+	}
+
+	if (dev->data_rate == AD463X_DDR_MODE)
+		dev->capture_data_width /= 2;
+
+	dev->read_bytes_no = dev->capture_data_width / 8;
+
+	ret = spi_engine_set_transfer_width(dev->spi_desc, dev->reg_data_width);
+	if (ret != SUCCESS)
+		goto error_spi;
+
+	spi_engine_set_speed(dev->spi_desc, dev->reg_access_speed);
+
+	ret = ad463x_spi_reg_read(dev, AD463X_CONFIG_TIMING, &data);
+	if (ret != SUCCESS)
+		goto error_spi;
+
+	ret = ad463x_spi_reg_write(dev, AD463X_REG_SCRATCH_PAD, AD463x_TEST_DATA);
+	if (ret != SUCCESS)
+		goto error_spi;
+
+	ret = ad463x_spi_reg_read(dev, AD463X_REG_SCRATCH_PAD, &data);
+	if (ret != SUCCESS)
+		goto error_spi;
+
+	if (data != AD463x_TEST_DATA) {
+		pr_err("Test Data read failed.\n");
+		goto error_spi;
+	}
+
+	ret = ad463x_spi_reg_write_masked(dev, AD463X_REG_MODES, AD463X_LANE_MODE_MSK,
+					  dev->lane_mode);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = ad463x_spi_reg_write_masked(dev, AD463X_REG_MODES, AD463X_CLK_MODE_MSK,
+					  dev->clock_mode);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = ad463x_spi_reg_write_masked(dev, AD463X_REG_MODES, AD463X_DDR_MODE_MSK,
+					  dev->data_rate);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = ad463x_spi_reg_write_masked(dev, AD463X_REG_MODES,
+					  AD463X_OUT_DATA_MODE_MSK, dev->output_mode);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = pwm_init(&dev->trigger_pwm_desc, init_param->trigger_pwm_init);
+	if (ret != SUCCESS)
+		goto error_spi;
+
+	*device = dev;
+
+	return ret;
+
+error_spi:
+	spi_remove(dev->spi_desc);
+error_clkgen:
+	axi_clkgen_remove(dev->clkgen);
+error_gpio:
+	gpio_remove(dev->gpio_resetn);
+error_dev:
+	free(dev);
+
+	return FAILURE;
+}
+
+/**
+ * @brief Free the memory allocated by ad463x_init().
+ * @param [in] dev - Pointer to the device handler.
+ * @return \ref SUCCESS in case of success, \ref FAILURE otherwise
+ */
+int32_t ad463x_remove(struct ad463x_dev *dev)
+{
+	int32_t ret;
+
+	if (!dev)
+		return FAILURE;
+
+	ret = pwm_remove(dev->trigger_pwm_desc);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = spi_remove(dev->spi_desc);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = gpio_remove(dev->gpio_resetn);
+	if (ret != SUCCESS)
+		return ret;
+
+	ret = axi_clkgen_remove(dev->clkgen);
+	if (ret != SUCCESS)
+		return ret;
+
+	free(dev);
+
+	return ret;
+}

drivers/adc/ad463x/ad463x.h
@@ -1,0 +1,305 @@
+/***************************************************************************//**
+ *   @file   ad463x.h
+ *   @brief  Header file of AD463x Driver.
+ *   @author Antoniu Miclaus (antoniu.miclaus@analog.com)
+********************************************************************************
+ * Copyright 2021(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef AD463X_H_
+#define AD463x_H_
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include <stdint.h>
+#include "spi_engine.h"
+#include "util.h"
+#include "clk_axi_clkgen.h"
+#include "pwm.h"
+#include "gpio.h"
+
+/******************************************************************************/
+/********************** Macros and Types Declarations *************************/
+/******************************************************************************/
+
+/* Register addresses */
+#define AD463X_REG_INTERFACE_CONFIG_A	0x00
+#define AD463X_REG_INTERFACE_CONFIG_B	0x01
+#define AD463X_REG_DEVICE_CONFIG	0x02
+#define AD463X_REG_CHIP_TYPE		0x03
+#define AD463X_REG_PRODUCT_ID_L		0x04
+#define AD463X_REG_PRODUCT_ID_H		0x05
+#define AD463X_REG_CHIP_GRADE		0x06
+#define AD463X_REG_SCRATCH_PAD		0x0A
+#define AD463X_REG_SPI_REVISION		0x0B
+#define AD463X_REG_VENDOR_L		0x0C
+#define AD463X_REG_VENDOR_H		0x0D
+#define AD463X_REG_STREAM_MODE		0x0E
+#define AD463X_REG_EXIT_CFG_MODE	0x14
+#define AD463X_REG_AVG			0x15
+#define AD463X_REG_OFFSET_BASE		0x16
+#define AD463X_REG_OFFSET_X0_0		0x16
+#define AD463X_REG_OFFSET_X0_1		0x17
+#define AD463X_REG_OFFSET_X0_2		0x18
+#define AD463X_REG_OFFSET_X1_0		0x19
+#define AD463X_REG_OFFSET_X1_1		0x1A
+#define AD463X_REG_OFFSET_X1_2		0x1B
+#define AD463X_REG_GAIN_BASE		0x1C
+#define AD463X_REG_GAIN_X0_LSB		0x1C
+#define AD463X_REG_GAIN_X0_MSB		0x1D
+#define AD463X_REG_GAIN_X1_LSB		0x1E
+#define AD463X_REG_GAIN_X1_MSB		0x1F
+#define AD463X_REG_MODES		0x20
+#define AD463X_REG_OSCILATOR		0x21
+#define AD463X_REG_IO			0x22
+#define AD463X_REG_PAT0			0x23
+#define AD463X_REG_PAT1			0x24
+#define AD463X_REG_PAT2			0x25
+#define AD463X_REG_PAT3			0x26
+#define AD463X_REG_DIG_DIAG		0x34
+#define AD463X_REG_DIG_ERR		0x35
+/* INTERFACE_CONFIG_A */
+#define AD463X_CFG_SW_RESET		(BIT(7) | BIT(0))
+#define AD463X_CFG_SDO_ENABLE		BIT(4)
+/* MODES */
+#define AD463X_SW_RESET_MSK		(BIT(7) | BIT(0))
+#define AD463X_LANE_MODE_MSK		(BIT(7) | BIT(6))
+#define AD463X_CLK_MODE_MSK		(BIT(5) | BIT(4))
+#define AD463X_DDR_MODE_MSK		BIT(3)
+#define AD463X_SDR_MODE			0x00
+#define AD463X_DDR_MODE			BIT(3)
+#define AD463X_OUT_DATA_MODE_MSK	(BIT(2) | BIT(1) | BIT(0))
+#define AD463X_24_DIFF			0x00
+#define AD463X_16_DIFF_8_COM		0x01
+#define AD463X_24_DIFF_8_COM		0x02
+#define AD463X_30_AVERAGED_DIFF		0x03
+#define AD463X_32_PATTERN		0x04
+/* EXIT_CFG_MD */
+#define AD463X_EXIT_CFG_MODE		BIT(0)
+/* CHANNEL */
+#define AD463X_CHANNEL_0		0x00
+#define AD463X_CHANNEL_1		0x01
+/* OFFSET */
+#define AD463X_OFFSET_0			0x00
+#define AD463X_OFFSET_1			0x01
+#define AD463X_OFFSET_2			0x02
+/* GAIN */
+#define AD463X_GAIN_LSB			0x00
+#define AD463X_GAIN_MSB			0x01
+/* LANE MODE */
+#define AD463X_ONE_LANE_PER_CH		0x00
+#define AD463X_TWO_LANES_PER_CH		BIT(6)
+#define AD463X_FOUR_LANES_PER_CH	BIT(7)
+#define AD463X_SHARED_TWO_CH		(BIT(6) | BIT(7))
+/* CLK MODE */
+#define AD463X_SPI_COMPATIBLE_MODE	0x00
+#define AD463X_ECHO_CLOCK_MODE		BIT(4)
+#define AD463X_CLOCK_MASTER_MODE	BIT(5)
+/* POWER MODE */
+#define AD463X_NORMAL_MODE 		0x00
+#define AD463X_LOW_POWER_MODE		(BIT(1) | BIT(0))
+/* AVG */
+#define AD463X_AVG_FILTER_RESET		BIT(7)
+#define AD463X_CONFIG_TIMING		0x2000
+#define AD463X_REG_READ_DUMMY		0x00
+#define AD463X_REG_WRITE        	0x00
+#define AD463X_REG_READ		    	BIT(7)
+#define AD463X_REG_CHAN_OFFSET(ch, pos)	(AD463X_REG_OFFSET_BASE + (3 * ch) + pos)
+#define AD463X_REG_CHAN_GAIN(ch, pos)	(AD463X_REG_GAIN_BASE + (2 * ch) + pos)
+/* IO */
+#define AD463X_DRIVER_STRENGTH_MASK	BIT(0)
+#define AD463X_NORMAL_OUTPUT_STRENGTH	0x00
+#define AD463X_DOUBLE_OUTPUT_STRENGTH	BIT(1)
+/* OUT_DATA_PAT */
+#define AD463X_OUT_DATA_PAT		0x5A5A0F0F
+
+#define AD463X_TRIGGER_PULSE_WIDTH_NS	0x0A
+
+#define AD463X_GAIN_MAX_VAL_SCALED	19997
+
+/**
+ * @enum ad463x_id
+ * @brief Device type
+ */
+enum ad463x_id {
+	/** AD4630-24 device */
+	ID_AD4630_24,
+	/** AD4630-20 device */
+	ID_AD4630_20,
+	/** AD4630-16 device */
+	ID_AD4630_16,
+	/** AD4631-24 device */
+	ID_AD4631_24,
+	/** AD4631-20 device */
+	ID_AD4631_20,
+	/** AD4631-16 device */
+	ID_AD4631_16,
+	/** AD4632-24 device */
+	ID_AD4632_24,
+	/** AD4632-20 device */
+	ID_AD4632_20,
+	/** AD4632-16 device */
+	ID_AD4632_16,
+};
+
+/**
+ * @struct ad463x_dev
+ * @brief Device initialization parameters.
+ */
+struct ad463x_init_param {
+	/** SPI */
+	struct spi_init_param *spi_init;
+	/** GPIO */
+	struct gpio_init_param *gpio_resetn;
+	/** PWM */
+	struct pwm_init_param *trigger_pwm_init;
+	/** SPI module offload init */
+	struct spi_engine_offload_init_param *offload_init_param;
+	/** Clock gen for hdl design init structure */
+	struct axi_clkgen_init *clkgen_init;
+	/** Clock generator rate */
+	uint32_t axi_clkgen_rate;
+	/** Register access speed */
+	uint32_t reg_access_speed;
+	/** Device id */
+	enum ad463x_id device_id;
+	/** Register data width */
+	uint8_t reg_data_width;
+	/** Lane Mode */
+	uint8_t lane_mode;
+	/** Clock Mode */
+	uint8_t clock_mode;
+	/** Data Rate Mode */
+	uint8_t data_rate;
+	/** Output Mode */
+	uint8_t output_mode;
+	/** Invalidate the Data cache for the given address range */
+	void (*dcache_invalidate_range)(uint32_t address, uint32_t bytes_count);
+};
+
+/**
+ * @struct ad463x_dev
+ * @brief Device structure.
+ */
+struct ad463x_dev {
+	/** SPI */
+	struct spi_desc *spi_desc;
+	/** GPIO */
+	struct gpio_desc *gpio_resetn;
+	/** PWM */
+	struct pwm_desc *trigger_pwm_desc;
+	/** SPI module offload init */
+	struct spi_engine_offload_init_param *offload_init_param;
+	/** Clock gen for hdl design structure */
+	struct axi_clkgen *clkgen;
+	/** Register access speed */
+	uint32_t reg_access_speed;
+	/** Device id */
+	enum ad463x_id device_id;
+	/** Register data width */
+	uint8_t reg_data_width;
+	/** Number of bytes to be read */
+	uint8_t read_bytes_no;
+	/** Capture data width */
+	uint8_t capture_data_width;
+	/** Lane Mode */
+	uint8_t lane_mode;
+	/** Clock Mode */
+	uint8_t clock_mode;
+	/** Data Rate Mode */
+	uint8_t data_rate;
+	/** Output Mode */
+	uint8_t output_mode;
+	/** Invalidate the Data cache for the given address range */
+	void (*dcache_invalidate_range)(uint32_t address, uint32_t bytes_count);
+};
+
+/******************************************************************************/
+/************************ Functions Declarations ******************************/
+/******************************************************************************/
+/** Read device register. */
+int32_t ad463x_spi_reg_read(struct ad463x_dev *dev,
+			    uint16_t reg_addr,
+			    uint8_t *reg_data);
+
+/** Write device register */
+int32_t ad463x_spi_reg_write(struct ad463x_dev *dev,
+			     uint16_t reg_addr,
+			     uint8_t reg_data);
+
+/** Read from device using a mask */
+int32_t ad463x_spi_reg_read_masked(struct ad463x_dev *dev,
+				   uint16_t reg_addr,
+				   uint8_t mask,
+				   uint8_t *data);
+
+/** Write to device using a mask */
+int32_t ad463x_spi_reg_write_masked(struct ad463x_dev *dev,
+				    uint16_t reg_addr,
+				    uint8_t mask,
+				    uint8_t data);
+
+/** Set power mode */
+int32_t ad463x_set_pwr_mode(struct ad463x_dev *dev, uint8_t mode);
+
+/** Set average frame length */
+int32_t ad463x_set_avg_frame_len(struct ad463x_dev *dev, uint8_t mode);
+
+/** Set drive strength */
+int32_t ad463x_set_drive_strength(struct ad463x_dev *dev, uint8_t mode);
+
+/** Exit register configuration mode */
+int32_t ad463x_exit_reg_cfg_mode(struct ad463x_dev *dev);
+
+/** Set channel gain */
+int32_t ad463x_set_ch_gain(struct ad463x_dev *dev, uint8_t ch_idx,
+			   uint64_t gain);
+
+/** Set channel offset */
+int32_t ad463x_set_ch_offset(struct ad463x_dev *dev, uint8_t ch_idx,
+			     uint32_t offset);
+
+/** Read data */
+int32_t ad463x_read_data(struct ad463x_dev *dev,
+			 uint32_t *buf,
+			 uint16_t samples);
+
+/** Device initialization */
+int32_t ad463x_init(struct ad463x_dev **device,
+		    struct ad463x_init_param *init_param);
+
+/** Free resources */
+int32_t ad463x_remove(struct ad463x_dev *dev);
+
+#endif // AD463X_H_

commit 0977893bb6024d67785af9587162ca387fa2e552

drivers: adc: ad463x: fix SPI error handling
Fix bug where register read writes to output before checking SPI
status, potentially returning garbage data on communication errors.

Fix invalid parameter error code in ad463x_set_pwr_mode().

Fixes: fe417e152 (adc: ad463x: initial commit)
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad463x/ad463x.c
@@ -82,9 +82,12 @@ int32_t ad463x_spi_reg_read(struct ad463x_dev *dev,
 	buf[2] = AD463X_REG_READ_DUMMY;
 
 	ret = no_os_spi_write_and_read(dev->spi_desc, buf, 3);
+	if (ret)
+		return ret;
+
 	*reg_data = buf[2];
 
-	return ret;
+	return 0;
 }
 
 /**
@@ -186,7 +189,7 @@ int32_t ad463x_spi_reg_write_masked(struct ad463x_dev *dev,
 int32_t ad463x_set_pwr_mode(struct ad463x_dev *dev, uint8_t mode)
 {
 	if ((mode != AD463X_NORMAL_MODE) && (mode != AD463X_LOW_POWER_MODE))
-		return -1;
+		return -EINVAL;
 
 	return ad463x_spi_reg_write(dev, AD463X_REG_DEVICE_CONFIG, mode);
 }