77853c576db6ec72a5710834a2c566a59612bb28
Alisa-Dariana Roman <alisa.roman@analog.com>
2026-01-09 13:57:13 +0200

    projects: eval-pqmon: use no_os_net for ethernet
    Define w5500_net_init_params with w5500_net_ops and pass to
    iio_app_init when PQM_CONN_ETH is enabled.
    
    Signed-off-by: Alisa-Dariana Roman <alisa.roman@analog.com>

--- projects/eval-pqmon/src/common/common_data.h
+++ projects/eval-pqmon/src/common/common_data.h
@@ -54,6 +54,7 @@
 #include "no_os_spi.h"
 #include "no_os_timer.h"
 #include "no_os_uart.h"
+#include "no_os_net.h"
 #include "parameters.h"
 #include "pqlib_example.h"
 #include "afe_calibration.h"
@@ -80,6 +81,7 @@ extern const struct no_os_spi_init_param w5500_spi_init_params;
 
 extern struct w5500_init_param w5500_ip;
 extern struct w5500_network_init_param w5500_network_ip;
+extern struct no_os_net_init_param w5500_net_init_params;
 #endif
 
 #if defined(PQM_CONN_USB)
