analogdevicesinc/no-OS

commit 16d0100d879c7043fb61dff7f36ec643216a8005

projects: eval-ade9153a: example project
Example project for ADE9153A.

Tested using the EV-ADE9153ASHIELDZ.
The example includes autocalibration and
measurements of the rms, power, energy,
power quality and temperature values.

Signed-off-by: Radu Etz <radu.etz@analog.com>

projects/eval-ade9153a/src/main.c
@@ -1,0 +1,235 @@
+/***************************************************************************//**
+ *   @file   main.c
+ *   @brief  Main function of ADE9153A example.
+ *   @author Radu Etz (radu.etz@analog.com)
+********************************************************************************
+ * Copyright 2024(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#include <stdio.h>
+#include "no_os_uart.h"
+#include "no_os_pwm.h"
+#include "no_os_delay.h"
+#include "no_os_gpio.h"
+#include "no_os_spi.h"
+#include "no_os_print_log.h"
+#include "no_os_units.h"
+#include "no_os_util.h"
+#include "no_os_error.h"
+#include "maxim_uart.h"
+#include "maxim_gpio.h"
+#include "maxim_uart_stdio.h"
+#include "maxim_pwm.h"
+#include "maxim_spi.h"
+#include "ade9153a.h"
+#include "common_data.h"
+
+struct no_os_irq_ctrl_desc *ade9153a_nvic_desc;
+struct no_os_irq_ctrl_desc *ade9153a_gpio_irq_desc;
+
+int main(void)
+{
+	uint8_t i = 0;
+	uint8_t ret;
+
+	// parameters initialization structure
+	struct ade9153a_init_param ade9153a_ip;
+	// device structure
+	struct ade9153a_dev *ade9153a_dev;
+
+	// uart descriptor
+	struct no_os_uart_desc *uart_desc;
+	// gpio descriptor
+	struct no_os_gpio_desc *gpio_desc;
+	// reset descriptor
+	struct no_os_gpio_desc *reset_desc;
+	// ss descriptor
+	struct no_os_gpio_desc *ss_desc;
+	// sck descriptor
+	struct no_os_gpio_desc *sck_desc;
+	// spi descriptor
+	struct no_os_spi_desc *spi_desc;
+
+	/* Initialize NVIC IRQ controller in order to be able to enable
+	GPIO IRQ interrupt */
+	struct no_os_irq_init_param ade9153a_nvic_ip = {
+		.platform_ops = &max_irq_ops,
+	};
+
+	/* Initialize GPIO IRQ controller */
+	ret = no_os_irq_ctrl_init(&ade9153a_nvic_desc,
+				  &ade9153a_nvic_ip);
+	if (ret)
+		goto error;
+
+	ret = no_os_irq_set_priority(ade9153a_nvic_desc,
+				     NVIC_GPIO_IRQ, 1);
+	if (ret)
+		goto remove_irq;
+
+	ret = no_os_irq_ctrl_init(&ade9153a_gpio_irq_desc,
+				  &ade9153a_gpio_irq_ip);
+	if (ret)
+		goto remove_irq;
+
+	ret = no_os_uart_init(&uart_desc, &uart_ip);
+	if (ret)
+		goto remove_irq;
+
+	ret = no_os_gpio_get_optional(&gpio_desc,
+				      &gpio_led1_ip);
+	if (ret)
+		goto remove_uart;
+
+	ret = no_os_gpio_get_optional(&ss_desc,
+				      &gpio_ss_ip);
+	if (ret)
+		goto remove_led;
+
+	ret = no_os_gpio_get_optional(&sck_desc,
+				      &gpio_sck_ip);
+	if (ret)
+		goto remove_ss;
+
+	ret = no_os_gpio_get_optional(&reset_desc,
+				      &gpio_reset_ip);
+	if (ret)
+		goto remove_sck;
+
+	if (gpio_desc)
+		ret = no_os_gpio_direction_output(gpio_desc,
+						  NO_OS_GPIO_LOW);
+	if (ret)
+		goto remove_reset;
+
+	/* Initialize SPI */
+	ade9153a_ip.spi_init = &ade9153a_spi_ip;
+	// ADE9153a dev DATA_RDY init params
+	ade9153a_ip.gpio_rdy = &ade9153a_gpio_rdy_ip;
+
+	// ADE9153a dev RESET init params
+	ade9153a_ip.gpio_reset = &gpio_reset_ip;
+
+	// ADE9153a dev SPI init params
+	ade9153a_ip.gpio_ss = &gpio_ss_ip;
+	ade9153a_ip.gpio_sck = &gpio_sck_ip;
+	ade9153a_ip.spi_en = SPI_EN;
+	// IRQ init params
+	ade9153a_ip.irq_ctrl = ade9153a_gpio_irq_desc;
+	// initialization for EV_ADE9153a_SHIELDZ
+	ade9153a_ip.ai_swap = ENABLE;
+	// PGA gain set to 16
+	ade9153a_ip.ai_pga_gain = ADE9153A_AI_GAIN_16;
+	// hpf corner frequency
+	ade9153a_ip.hpf_crn = ADE9153A_HPF_CORNER_0_625_HZ;
+	// frequency select
+	ade9153a_ip.freq = F_50_HZ;
+	// Vlevel value assuming 1/2 full scale
+	ade9153a_ip.vlevel = ADE9153A_VLEVEL;
+	// V_div rsmall value
+	ade9153a_ip.vlevel = ADE9153A_RSMALL;
+	// Energy accumulate set to 4000
+	ade9153a_ip.no_samples = ADE9153A_NO_SAMPLES;
+	// AIGAIN for IAP-IAN swap
+	ade9153a_ip.ai_gain = ADE9153A_AIGAIN;
+
+	no_os_uart_stdio(uart_desc);
+
+	pr_info("\n");
+	pr_info("\n");
+	pr_info("ADE9153A SPI example \n");
+
+	/* Init ade9153a struct */
+	ade9153a_dev = (struct ade9153a_dev *)no_os_calloc(1, sizeof(*ade9153a_dev));
+	if (!ade9153a_dev)
+		return -ENOMEM;
+
+	/* initialize the device with the values stored
+	in the initialization structure */
+	ret = ade9153a_init(&ade9153a_dev, ade9153a_ip);
+	if (ret)
+		goto free_dev;
+
+	// burst disable
+	ade9153a_dev->burst_en = 0;
+
+	no_os_mdelay(RESET_TIME);
+
+	ret = ade9153a_setup(ade9153a_dev, ade9153a_ip);
+	if (ret)
+		goto free_dev;
+
+	/* read the autocalibration values and update the
+	registers */
+	ret = autocalibration_read_vals(ade9153a_dev);
+	if (ret)
+		goto free_dev;
+
+	/* set the temperature sample time */
+	ret = ade9153a_temp_time(ade9153a_dev,
+				 ADE9153A_TEMP_TIME_SAMPLES_1024);
+	if (ret)
+		goto free_dev;
+
+	while(1) {
+		// time delay between readings
+		no_os_mdelay(READ_INTERVAL);
+		/* read and print the ade9153a measured values */
+		ret = read_measurements(ade9153a_dev);
+		if (ret)
+			goto free_dev;
+		ret = interface_toggle_led(gpio_desc);
+		if (ret)
+			goto free_dev;
+	}
+
+free_dev:
+	no_os_free(ade9153a_dev);
+remove_reset:
+	no_os_gpio_remove(reset_desc);
+remove_sck:
+	no_os_gpio_remove(sck_desc);
+remove_ss:
+	no_os_gpio_remove(ss_desc);
+remove_led:
+	no_os_gpio_remove(gpio_desc);
+remove_uart:
+	no_os_uart_remove(uart_desc);
+remove_irq:
+	no_os_irq_ctrl_remove(ade9153a_nvic_desc);
+
+error:
+	pr_err("ERROR\n");
+	return ret;
+
+}

projects/eval-ade9153a/Makefile
@@ -1,0 +1,5 @@
+include ../../tools/scripts/generic_variables.mk
+
+include src.mk
+
+include ../../tools/scripts/generic.mk

projects/eval-ade9153a/src.mk
@@ -1,0 +1,65 @@
+
+SRCS += $(PROJECT)/src/main.c
+
+INCS += $(INCLUDE)/no_os_alloc.h		\
+	$(INCLUDE)/no_os_delay.h     \
+	$(INCLUDE)/no_os_error.h		\
+	$(INCLUDE)/no_os_gpio.h      \
+	$(INCLUDE)/no_os_print_log.h \
+	$(INCLUDE)/no_os_spi.h       \
+	$(INCLUDE)/no_os_timer.h      \
+	$(INCLUDE)/no_os_mutex.h		\
+	$(INCLUDE)/no_os_util.h			\
+	$(INCLUDE)/no_os_lf256fifo.h		\
+	$(INCLUDE)/no_os_list.h			\
+	$(INCLUDE)/no_os_irq.h			\
+	$(INCLUDE)/no_os_units.h \
+	$(INCLUDE)/no_os_init.h \
+	$(INCLUDE)/no_os_alloc.h \
+	$(INCLUDE)/no_os_crc8.h \
+	$(INCLUDE)/no_os_crc16.h \
+	$(INCLUDE)/no_os_uart.h			\
+	$(INCLUDE)/no_os_pwm.h			\
+	$(INCLUDE)/no_os_dma.h \
+	$(PLATFORM_DRIVERS)/maxim_irq.h		\
+	$(PLATFORM_DRIVERS)/maxim_uart.h	\
+	$(PLATFORM_DRIVERS)/maxim_uart_stdio.h	\
+	$(PLATFORM_DRIVERS)/maxim_gpio.h      \
+    $(PLATFORM_DRIVERS)/maxim_spi.h       \
+	$(PLATFORM_DRIVERS)/maxim_gpio_irq.h  \
+	$(PLATFORM_DRIVERS)/../common/maxim_dma.h       \
+	$(PLATFORM_DRIVERS)/maxim_pwm.h
+
+SRCS +=	$(NO-OS)/util/no_os_alloc.c		\
+	$(DRIVERS)/api/no_os_gpio.c \
+	$(NO-OS)/util/no_os_mutex.c		\
+	$(NO-OS)/util/no_os_lf256fifo.c		\
+	$(NO-OS)/util/no_os_list.c		\
+	$(NO-OS)/util/no_os_util.c		\
+	$(NO-OS)/util/no_os_crc8.c \
+	$(NO-OS)/util/no_os_crc16.c \
+	$(DRIVERS)/api/no_os_irq.c		\
+	$(DRIVERS)/api/no_os_uart.c		\
+	$(DRIVERS)/api/no_os_pwm.c		\
+	$(DRIVERS)/api/no_os_spi.c  \
+	$(DRIVERS)/api/no_os_timer.c  \
+	$(DRIVERS)/api/no_os_dma.c \
+	$(PLATFORM_DRIVERS)/maxim_irq.c		\
+	$(PLATFORM_DRIVERS)/maxim_gpio_irq.c  \
+	$(PLATFORM_DRIVERS)/maxim_delay.c     \
+	$(PLATFORM_DRIVERS)/maxim_init.c      \
+	$(PLATFORM_DRIVERS)/maxim_uart_stdio.c	\
+	$(PLATFORM_DRIVERS)/maxim_gpio.c      \
+	$(PLATFORM_DRIVERS)/maxim_spi.c       \
+	$(PLATFORM_DRIVERS)/maxim_uart.c	\
+	$(PLATFORM_DRIVERS)/../common/maxim_dma.c       \
+	$(PLATFORM_DRIVERS)/maxim_pwm.c
+
+# Application entry point
+SRCS += $(PROJECT)/src/main.c
+INCS += $(PROJECT)/src/common/common_data.h
+SRCS += $(PROJECT)/src/common/common_data.c
+
+# ADE9153A driver files
+INCS += $(DRIVERS)/meter/ade9153a/ade9153a.h
+SRCS += $(DRIVERS)/meter/ade9153a/ade9153a.c

projects/eval-ade9153a/src/common/common_data.c
@@ -1,0 +1,409 @@
+/***************************************************************************//**
+ *   @file   common_data.c
+ *   @brief  Defines common data to be used by ADE9153A example project
+ *   @author REtz (radu.etz@analog.com)
+********************************************************************************
+ * Copyright (c) 2024 Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include "common_data.h"
+
+/******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+
+struct max_uart_init_param max_uart_extra_ip = {
+	.flow = UART_FLOW_DIS
+};
+
+struct max_gpio_init_param gpio_extra = {
+	.vssel = MXC_GPIO_VSSEL_VDDIOH,
+};
+
+struct max_spi_init_param ade9153a_spi_extra_ip  = {
+	.num_slaves = SPI_SLAVE_NUM,
+	.polarity = SPI_SS_POL_LOW,
+	.vssel = MXC_GPIO_VSSEL_VDDIOH,
+};
+
+struct max_gpio_init_param ade9153a_gpio_extra_ip = {
+	.vssel = MXC_GPIO_VSSEL_VDDIOH,
+};
+
+struct no_os_uart_init_param uart_ip = {
+	.device_id = UART_DEV_ID,
+	.baud_rate = UART_BAUD,
+	.size = NO_OS_UART_CS_8,
+	.parity = NO_OS_UART_PAR_NO,
+	.stop = NO_OS_UART_STOP_1_BIT,
+	.platform_ops = &max_uart_ops,
+	.extra = &max_uart_extra_ip,
+};
+
+// user led pin
+struct no_os_gpio_init_param gpio_led1_ip = {
+	.port = GPIO_LED_PORT,
+	.number = GPIO_LED_PIN,
+	.pull = NO_OS_PULL_UP,
+	.platform_ops = &max_gpio_ops,
+	.extra = &gpio_extra,
+};
+
+// ss pin
+struct no_os_gpio_init_param gpio_ss_ip = {
+	.port = GPIO_SS_PORT,
+	.number = GPIO_SS_PIN,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = &gpio_extra,
+};
+
+// sck pin
+struct no_os_gpio_init_param gpio_sck_ip = {
+	.port = GPIO_SCK_PORT,
+	.number = GPIO_SCK_PIN,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = &gpio_extra,
+};
+
+// reset pin
+struct no_os_gpio_init_param gpio_reset_ip = {
+	.port = GPIO_RESET_PORT,
+	.number = GPIO_RESET_PIN,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = &gpio_extra,
+};
+
+// rdy pin
+struct no_os_gpio_init_param ade9153a_gpio_rdy_ip = {
+	.port = GPIO_RDY_PORT,
+	.number = GPIO_RDY_PIN,
+	.pull = NO_OS_PULL_NONE,
+	.platform_ops = &max_gpio_ops,
+	.extra = &gpio_extra,
+};
+
+struct no_os_spi_init_param ade9153a_spi_ip = {
+	.device_id = SPI_DEVICE_ID,
+	.max_speed_hz = SPI_BAUDRATE,
+	.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
+	.mode = NO_OS_SPI_MODE_0,
+	.platform_ops = &max_spi_ops,
+	.chip_select = SPI_CS,
+	.extra = &ade9153a_spi_extra_ip,
+};
+
+struct no_os_irq_init_param ade9153a_gpio_irq_ip = {
+	.platform_ops = GPIO_IRQ_OPS,
+	.irq_ctrl_id = GPIO_CTRL_IRQ_ID,
+	.extra = GPIO_IRQ_EXTRA,
+};
+
+/******************************************************************************/
+/************************ Functions Definitions *******************************/
+/******************************************************************************/
+
+/**
+ * @brief Toggle Led
+ * @param gpio_led_desc - led descriptor
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int interface_toggle_led(struct no_os_gpio_desc *gpio_led_desc)
+{
+	uint8_t val;
+	int ret;
+
+	ret = no_os_gpio_get_value(gpio_led_desc, &val);
+	if (ret)
+		return ret;
+
+	if (val == NO_OS_GPIO_LOW)
+		val = NO_OS_GPIO_HIGH;
+	else
+		val = NO_OS_GPIO_LOW;
+
+	return no_os_gpio_set_value(gpio_led_desc, val);
+}
+
+/**
+ * @brief Read measurements
+* @param dev - The device structure.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int read_measurements(struct ade9153a_dev *dev)
+{
+	int ret;
+	struct ade9153a_temperature_value temp_val;
+	struct ade9153a_energy_values energy_vals;
+	struct ade9153a_power_values power_vals;
+	struct ade9153a_rms_values rms_vals;
+	struct ade9153a_half_rms_values hrms_vals;
+	struct ade9153a_pq_values pq_vals;
+	float temperature_val;
+	float active_energy, reactive_energy, apparent_energy;
+	float active_power, reactive_power, apparent_power;
+	float current_rms_val, voltage_rms_val;
+	float current_h_rms_val, voltage_h_rms_val;
+	float constant, power_factor_val, frequency_val, angle_ai_av_val;
+	// status of freq select
+	uint8_t status;
+	// skip temp if timeout
+	uint8_t skip_temp;
+
+
+	ret = ade9153a_temp_val(dev, &temp_val);
+	if (ret)
+		if (ret == -ENODATA) {
+			pr_err(" Timeout temp read \n");
+			skip_temp = 1;
+		} else {
+			return ret;
+		} else
+		skip_temp = 0;
+
+	if (!(skip_temp)) {
+		// compute the deg C value using the trim values
+		temperature_val = ((float)temp_val.offset_reg_val / (float)( 1 << 5 )) - (((
+					  float)temp_val.temperature_reg_val * (float)temp_val.gain_reg_val)
+				  / (float)( 1 << 17 ));
+		pr_info("Temperature: %.2f deg C \n", temperature_val);
+	}
+	// compute energy values
+	ret = ade9153a_energy_vals(dev, &energy_vals);
+	if (ret)
+		return ret;
+
+	active_energy = (float)energy_vals.active_energy_reg_val * ENERGY_CC / 1000;
+	// value in mWhr
+
+	reactive_energy = (float)energy_vals.fundamental_reactive_energy_reg_val *
+			  ENERGY_CC / 1000;
+	// value in mVARhr
+
+	apparent_energy = (float)(energy_vals.apparent_energy_reg_val) * ENERGY_CC /
+			  1000;
+	// value in mVAhr
+
+	pr_info("Active energy: %.2f Whr \n", active_energy / 1000);
+	pr_info("Fundamental reactive energy: %.2f mVARhr \n", reactive_energy / 1000);
+	pr_info("Apparent energy: %.2f mVAhr \n", apparent_energy / 1000);
+
+	// compute power values
+	ret = ade9153a_power_vals(dev, &power_vals);
+	if (ret)
+		return ret;
+
+	active_power = (float)power_vals.active_power_reg_val * POWER_CC / 1000;
+	// value in mW
+
+	reactive_power = (float)power_vals.fundamental_reactive_power_reg_val *
+			 POWER_CC / 1000;
+	// value in mVAR
+
+	apparent_power = (float)power_vals.apparent_power_reg_val * POWER_CC / 1000;
+	// value in mVA
+
+	pr_info("Active power: %.2f W \n", active_power / 1000);
+	pr_info("Fundamental reactive power: %.2f VAR \n", reactive_power / 1000);
+	pr_info("Apparent power: %.2f VA \n", apparent_power / 1000);
+
+	//compute rms values
+	ret = ade9153a_rms_vals(dev, &rms_vals);
+	if (ret)
+		return ret;
+
+	current_rms_val = (float)(rms_vals.current_rms_reg_val) * RMS_CURENT_CC / 1000;
+	// value in mA
+
+	voltage_rms_val = (float)rms_vals.voltage_rms_reg_val * RMS_VOLTAGE_CC / 1000;
+	// value in mV
+
+	pr_info("I rms: %.2f mA \n", current_rms_val);
+	pr_info("V rms: %.2f mV \n", voltage_rms_val);
+
+	//compute half rms values
+	ret = ade9153a_half_rms_vals(dev, &hrms_vals);
+	if (ret)
+		return ret;
+
+	current_h_rms_val = (float)hrms_vals.current_h_rms_reg_val * RMS_CURENT_CC /
+			    1000;
+	// value in mA
+
+	voltage_h_rms_val = (float)hrms_vals.voltage_h_rms_reg_val * RMS_VOLTAGE_CC /
+			    1000;
+	// value in mV
+
+	pr_info("I half rms: %.2f mA \n", current_h_rms_val);
+	pr_info("V half rms: %.2f mV \n", voltage_h_rms_val);
+
+	//compute power quality values
+	ret = ade9153a_power_quality_vals(dev, &pq_vals);
+	if (ret)
+		return ret;
+
+	power_factor_val = (float)pq_vals.power_factor_reg_val / (float)PF_CC;
+
+	frequency_val = (float)(FREQ_CC) / (float)(pq_vals.period_reg_val +1);
+	// frequency value Hz
+
+	// multiplier constant for selected frequency
+	ret = ade9153a_freq_s_status(dev, &status);
+	if (ret)
+		return ret;
+	if (!status)
+		constant = MULT_CC_50HZ;
+	else
+		constant = MULT_CC_60HZ;
+	angle_ai_av_val = (float)pq_vals.angle_ai_av_reg_val * constant;
+	// angle value in degrees
+
+	pr_info("Power factor: %.2f \n", power_factor_val);
+	pr_info("Frequency %.2f Hz \n", frequency_val);
+	pr_info("Angle: %.2f deg \n", angle_ai_av_val);
+
+	pr_info("\n");
+	pr_info("\n");
+	pr_info("\n");
+
+	return 0;
+}
+
+/**
+ * @brief Autocalibration read values
+* @param dev - The device structure.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int autocalibration_read_vals(struct ade9153a_dev *dev)
+{
+	int ret;
+	float i_gain, v_gain, aicc, avcc;
+	struct ade9153a_autocal_vals autocal_vals;
+
+	pr_info("Auto Calibration running \n");
+
+	ret = autocalibration_start(dev);
+	if (ret)
+		return ret;
+	ret = ade9153a_read_autocal_vals(dev, &autocal_vals);
+	if (ret)
+		return ret;
+
+	pr_info("AICC: %.2f \n", (float)autocal_vals.aicc_reg_val / ( 1 << 11 ));
+	pr_info("AVCC: %.2f \n", (float)autocal_vals.avcc_reg_val / ( 1 << 11 ));
+
+	aicc = (float)autocal_vals.aicc_reg_val / (float)( 1 << 11 );
+	i_gain = aicc / (RMS_CURENT_CC * 1000);
+	i_gain = (-i_gain - 1);
+	i_gain = i_gain * (float)( 1 << 27 );
+
+	avcc = (float)autocal_vals.avcc_reg_val / (float)( 1 << 11 );
+	v_gain = (float)((avcc / ((float)RMS_VOLTAGE_CC * (float)1000)) - 1) *
+		 (1 << 27);
+
+	pr_info("AI cert: %d \n", autocal_vals.aicert_reg_val);
+	pr_info("AV cert: %d \n", autocal_vals.avcert_reg_val);
+
+	ret = ade9153a_stop(dev);
+	if (ret)
+		return ret;
+
+	ret = ade9153a_write(dev,ADE9153A_REG_AIGAIN, (int32_t)i_gain);
+	if (ret)
+		return ret;
+	ret = ade9153a_write(dev,ADE9153A_REG_AVGAIN, (uint32_t)v_gain);
+	if (ret)
+		return ret;
+
+	ret = ade9153a_run(dev);
+	if (ret)
+		return ret;
+
+	pr_info("AI_GAIN: %d \n", (int32_t)i_gain);
+	pr_info("AV_GAIN: %d \n", (uint32_t)v_gain);
+	pr_info("Auto Calibration stop \n");
+
+	return 0;
+}
+
+/**
+ * @brief Autocalibration start
+ * @param dev - The device structure.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int autocalibration_start(struct ade9153a_dev *dev)
+{
+	int ret;
+
+	ret = ade9153a_start_autocal_ai(dev, NORMAL);
+	if (ret) {
+		pr_info("ERROR %d \n", ret);
+		return ret;
+	}
+
+	pr_info("IA autocalibration started \n");
+
+	// wait 2s so the current autocalibration is ready
+	no_os_mdelay(20000);
+
+	ret = ade9153a_write(dev, ADE9153A_REG_MS_ACAL_CFG, 0x00000000);
+	if (ret)
+		return ret;
+
+	no_os_mdelay(200);
+
+	ret = ade9153a_start_autocal_av(dev, NORMAL);
+	if (ret) {
+		pr_info("ERROR %d \n", ret);
+		return ret;
+	}
+
+	pr_info("VA autocalibration started \n");
+
+	// wait 5s so the voltage autocalibration is ready
+	no_os_mdelay(50000);
+
+	ret = ade9153a_write(dev, ADE9153A_REG_MS_ACAL_CFG, 0x00000000);
+	if (ret)
+		return ret;
+
+	no_os_mdelay(100);
+
+	return 0;
+}

commit 3de890d094063c61a01fea9189a247a461d35f1b

eval-ade9153a: Fix incorrect assignment of rsmall to vlevel
The rsmall value was incorrectly assigned to vlevel field instead
of the rsmall field in the initialization parameters.

Fixes: 16d0100d879c ("projects: eval-ade9153a: example project")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

projects/eval-ade9153a/src/main.c
@@ -151,7 +151,7 @@ int main(void)
 	// Vlevel value assuming 1/2 full scale
 	ade9153a_ip.vlevel = ADE9153A_VLEVEL;
 	// V_div rsmall value
-	ade9153a_ip.vlevel = ADE9153A_RSMALL;
+	ade9153a_ip.rsmall = ADE9153A_RSMALL;
 	// Energy accumulate set to 4000
 	ade9153a_ip.no_samples = ADE9153A_NO_SAMPLES;
 	// AIGAIN for IAP-IAN swap