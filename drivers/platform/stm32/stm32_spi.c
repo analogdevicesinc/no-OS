analogdevicesinc/no-OS

commit 1b0d4cedd2198cf8bd98833460279c056b24343e

platform: stm32: spi: fix dma callback
Currently there is no callback to undo the setup steps done dma xfer
is started. But also the callback passed as a parameter is not called when
dma xfer is done.

This is because dma driver uses the channel it allocates itself for the callback.
Use the channel allocated by the dma driver and assign a callback to it
that will free the memory and stop the dma/pwm that was started
on start.

Fixes: b21737ac ("drivers:platform:stm32: Added SPI DMA implementation")
Signed-off-by: Axel Haslam <ahaslam@baylibre.com>

drivers/platform/stm32/stm32_spi.c
@@ -204,29 +204,28 @@ int32_t stm32_spi_init(struct no_os_spi_desc **desc,
 		if (ret)
 			goto error;
 
-		if (sinit->rxdma_ch)
-			sdesc->rxdma_ch = sinit->rxdma_ch;
-
-		if (sinit->txdma_ch)
-			sdesc->txdma_ch = sinit->txdma_ch;
-
-		sdesc->dma_desc->sg_handler = sinit->dma_init->sg_handler;
+		if (sinit->rxdma_ch) {
+			sdesc->rxdma_ch = &sdesc->dma_desc->channels[0];
+			sdesc->rxdma_ch->id = sinit->rxdma_ch->hdma;
+			sdesc->rxdma_ch->extra = sinit->rxdma_ch;
+			sdesc->rxdma_ch->irq_num = sinit->irq_num;
+		}
+		if (sinit->txdma_ch) {
+			sdesc->txdma_ch = &sdesc->dma_desc->channels[1];
+			sdesc->txdma_ch->id = sinit->txdma_ch->hdma;
+			sdesc->txdma_ch->extra = sinit->txdma_ch;
+		}
 	}
 
 #ifdef HAL_TIM_MODULE_ENABLED
 	if (sinit->pwm_init) {
-		struct stm32_pwm_desc* spwm_desc;
-		/* Initialize CS PWM */
 		ret = no_os_pwm_init(&sdesc->pwm_desc, sinit->pwm_init);
 		if (ret)
 			goto error;
 
-		spwm_desc = sdesc->pwm_desc->extra;
 		ret = no_os_pwm_disable(sdesc->pwm_desc);
 		if (ret)
 			goto error_pwm;
-
-		spwm_desc->htimer.Instance->CNT = 0;
 	}
 #endif
 	sdesc->csip_extra.mode = GPIO_MODE_OUTPUT_PP;
@@ -477,16 +476,15 @@ int32_t stm32_spi_write_and_read(struct no_os_spi_desc *desc,
 int32_t stm32_config_dma_and_start(struct no_os_spi_desc* desc,
 				   struct no_os_spi_msg* msgs,
 				   uint32_t len,
-				   void (*callback)(void*),
+				   void (*callback)(
+						   struct no_os_dma_xfer_desc *old_xfer,
+						   struct no_os_dma_xfer_desc *next_xfer,
+						   void *ctx),
 				   void* ctx, bool is_async)
 {
 	struct stm32_spi_desc* sdesc = desc->extra;
 	struct no_os_dma_xfer_desc* rx_ch_xfer;
 	struct no_os_dma_xfer_desc* tx_ch_xfer;
-	struct no_os_dma_ch* tx_ch = sdesc->txdma_ch;
-	struct no_os_dma_ch* rx_ch = sdesc->rxdma_ch;
-	struct stm32_dma_channel* sdma_rx = rx_ch->extra;
-	struct stm32_dma_channel* sdma_tx = tx_ch->extra;
 	SPI_TypeDef* SPIx = sdesc->hspi.Instance;
 	int ret;
 	uint8_t i;
@@ -524,40 +522,41 @@ int32_t stm32_config_dma_and_start(struct no_os_spi_desc* desc,
 		rx_ch_xfer[i].xfer_type = DEV_TO_MEM;
 		rx_ch_xfer[i].length = msgs[i].bytes_number;
 		if (callback) {
-			sdesc->dma_desc->sg_handler = callback;
+			rx_ch_xfer[i].xfer_complete_cb = callback;
+			rx_ch_xfer[i].xfer_complete_ctx = ctx;
+
+			tx_ch_xfer[i].xfer_complete_cb = NULL;
+			tx_ch_xfer[i].xfer_complete_ctx = NULL;
 		}
 	}
 
-	rx_ch->id = sdma_rx->hdma;
-	rx_ch->sg_list = NULL;
+	sdesc->tx_ch_xfer = tx_ch_xfer;
+	sdesc->rx_ch_xfer = rx_ch_xfer;
 
-	ret = no_os_dma_config_xfer(sdesc->dma_desc, rx_ch_xfer, len, rx_ch);
+	ret = no_os_dma_config_xfer(sdesc->dma_desc, rx_ch_xfer, len, sdesc->rxdma_ch);
 	if (ret)
 		goto remove_dma;
 
-	tx_ch->id = sdma_tx->hdma;
-	tx_ch->sg_list = NULL;
-
-	ret = no_os_dma_config_xfer(sdesc->dma_desc, tx_ch_xfer, len, tx_ch);
+	ret = no_os_dma_config_xfer(sdesc->dma_desc, tx_ch_xfer, len, sdesc->txdma_ch);
 	if (ret)
 		goto remove_dma;
 
-	ret = no_os_dma_xfer_start(sdesc->dma_desc, tx_ch);
+	ret = no_os_dma_xfer_start(sdesc->dma_desc, sdesc->txdma_ch);
 	if (ret)
 		goto abort_transfer;
 
-	ret = no_os_dma_xfer_start(sdesc->dma_desc, rx_ch);
+	ret = no_os_dma_xfer_start(sdesc->dma_desc, sdesc->rxdma_ch);
 	if (ret)
 		goto abort_transfer;
 
-	if (tx_ch)
+	if (sdesc->txdma_ch)
 #if defined (STM32H5)
 		SET_BIT(sdesc->hspi.Instance->CFG1, SPI_CFG1_TXDMAEN);
 #else
 		SET_BIT(sdesc->hspi.Instance->CR2, SPI_CR2_TXDMAEN);
 #endif
 
-	if (rx_ch)
+	if (sdesc->rxdma_ch)
 #if defined (STM32H5)
 		SET_BIT(sdesc->hspi.Instance->CFG1, SPI_CFG1_RXDMAEN);
 #else
@@ -576,8 +575,8 @@ int32_t stm32_config_dma_and_start(struct no_os_spi_desc* desc,
 	return 0;
 
 abort_transfer:
-	no_os_dma_xfer_abort(sdesc->dma_desc, tx_ch);
-	no_os_dma_xfer_abort(sdesc->dma_desc, rx_ch);
+	no_os_dma_xfer_abort(sdesc->dma_desc, sdesc->txdma_ch);
+	no_os_dma_xfer_abort(sdesc->dma_desc, sdesc->rxdma_ch);
 remove_dma:
 	no_os_dma_remove(sdesc->dma_desc);
 free_tx_ch_xfer:
@@ -586,6 +585,51 @@ free_rx_ch_xfer:
 	no_os_free(rx_ch_xfer);
 
 	return ret;
+}
+
+void stm32_spi_dma_callback(struct no_os_dma_xfer_desc *old_xfer,
+			    struct no_os_dma_xfer_desc *next_xfer,
+			    void *ctx)
+{
+	struct no_os_spi_desc* desc = ctx;
+	struct stm32_spi_desc* sdesc = desc->extra;
+	SPI_TypeDef * SPIx = sdesc->hspi.Instance;
+
+	/* if more xfers pending dont do anything */
+	if (next_xfer)
+		return;
+
+	if (sdesc->pwm_desc)
+		no_os_pwm_disable(sdesc->pwm_desc);
+
+#if defined (STM32H5)
+	CLEAR_BIT(sdesc->hspi.Instance->CFG1, SPI_CFG1_TXDMAEN);
+#else
+	CLEAR_BIT(sdesc->hspi.Instance->CR2, SPI_CR2_TXDMAEN);
+#endif
+
+#if defined (STM32H5)
+	CLEAR_BIT(sdesc->hspi.Instance->CFG1, SPI_CFG1_RXDMAEN);
+#else
+	CLEAR_BIT(sdesc->hspi.Instance->CR2, SPI_CR2_RXDMAEN);
+#endif
+
+	no_os_dma_xfer_abort(sdesc->dma_desc, sdesc->txdma_ch);
+
+	no_os_dma_xfer_abort(sdesc->dma_desc, sdesc->rxdma_ch);
+
+	no_os_free(sdesc->tx_ch_xfer);
+	no_os_free(sdesc->rx_ch_xfer);
+
+	/* put CS pin back into gpio mode */
+	stm32_spi_altrnate_cs_enable(desc, false);
+
+	sdesc->stm32_spi_dma_done = true;
+
+	/* Dummy read to clear any pending read on SPI */
+	*(volatile uint8_t *)&SPIx->DR;
+	if (sdesc->stm32_spi_dma_user_cb)
+		sdesc->stm32_spi_dma_user_cb(sdesc->stm32_spi_dma_user_ctx);
 }
 
 /**
@@ -604,7 +648,12 @@ int32_t stm32_spi_dma_transfer_async(struct no_os_spi_desc* desc,
 				     void (*callback)(void*),
 				     void* ctx)
 {
-	return stm32_config_dma_and_start(desc, msgs, len, callback, ctx, true);
+	struct stm32_spi_desc* sdesc = desc->extra;
+
+	sdesc->stm32_spi_dma_user_cb = callback;
+	sdesc->stm32_spi_dma_user_ctx = ctx;
+	return stm32_config_dma_and_start(desc, msgs, len, stm32_spi_dma_callback, desc,
+					  true);
 }
 
 /**

drivers/platform/stm32/stm32_spi.h
@@ -64,9 +64,10 @@ struct stm32_spi_init_param {
 	/** DMA Initialization Parameters */
 	struct no_os_dma_init_param* dma_init;
 	/** RX DMA Channel Descriptor */
-	struct no_os_dma_ch* rxdma_ch;
+	struct stm32_dma_channel* rxdma_ch;
 	/** TX DMA Channel Descriptor */
-	struct no_os_dma_ch* txdma_ch;
+	struct stm32_dma_channel* txdma_ch;
+	uint32_t irq_num;
 #ifdef HAL_TIM_MODULE_ENABLED
 	/** CS PWM Initialization Parameters */
 	const struct no_os_pwm_init_param *pwm_init;
@@ -94,6 +95,11 @@ struct stm32_spi_desc {
 	struct no_os_dma_ch* txdma_ch;
 	struct stm32_gpio_init_param csip_extra;
 	struct no_os_gpio_init_param csip;
+	bool stm32_spi_dma_done;
+	void (*stm32_spi_dma_user_cb)(void *ctx);
+	void *stm32_spi_dma_user_ctx;
+	struct stm32_dma_channel* rx_ch_xfer;
+	struct stm32_dma_channel* tx_ch_xfer;
 #ifdef HAL_TIM_MODULE_ENABLED
 	/** CS PWM descriptor */
 	struct no_os_pwm_desc* pwm_desc;

drivers/platform/stm32/stm32_pwm.c
@@ -456,6 +456,9 @@ int32_t stm32_pwm_enable(struct no_os_pwm_desc *desc)
 	sparam = desc->extra;
 	chn_num = NO_OS_CHN_TO_STM32_CHN(sparam->timer_chn);
 
+	/* set counter to 0 to start from known state */
+	__HAL_TIM_SET_COUNTER(&sparam->htimer, 0);
+
 	if (desc->irq_id) {
 		if (sparam->complementary_channel) {
 			ret = HAL_TIMEx_PWMN_Start_IT(&sparam->htimer, chn_num);

commit a90cf4a6d4e4e1c1355a24d8193862a448f688a7

drivers:platform:stm32:spi: Fix warnings and data type
1. Fixed the data type of tx and rx dma xfer elements.
2. Fixed warnings from auto typecasting

Fixes: 1b0d4ce ("platform: stm32: spi: fix dma callback")
Signed-off-by: Swaroop PrakashKumar <Swaroop.PrakashKumar@analog.com>

drivers/platform/stm32/stm32_spi.c
@@ -164,7 +164,7 @@ int32_t stm32_spi_init(struct no_os_spi_desc **desc,
 		       const struct no_os_spi_init_param *param)
 {
 	int32_t ret;
-	struct no_os_spi_desc	*spi_desc;
+	struct no_os_spi_desc *spi_desc;
 
 	if (!desc || !param)
 		return -EINVAL;
@@ -206,13 +206,13 @@ int32_t stm32_spi_init(struct no_os_spi_desc **desc,
 
 		if (sinit->rxdma_ch) {
 			sdesc->rxdma_ch = &sdesc->dma_desc->channels[0];
-			sdesc->rxdma_ch->id = sinit->rxdma_ch->hdma;
+			sdesc->rxdma_ch->id = (uint32_t)sinit->rxdma_ch->hdma;
 			sdesc->rxdma_ch->extra = sinit->rxdma_ch;
 			sdesc->rxdma_ch->irq_num = sinit->irq_num;
 		}
 		if (sinit->txdma_ch) {
 			sdesc->txdma_ch = &sdesc->dma_desc->channels[1];
-			sdesc->txdma_ch->id = sinit->txdma_ch->hdma;
+			sdesc->txdma_ch->id = (uint32_t)sinit->txdma_ch->hdma;
 			sdesc->txdma_ch->extra = sinit->txdma_ch;
 		}
 	}
@@ -516,7 +516,7 @@ int32_t stm32_config_dma_and_start(struct no_os_spi_desc* desc,
 #ifndef SPI_SR_TXE
 		tx_ch_xfer[i].dst = &(SPIx->TXDR);
 #else
-		tx_ch_xfer[i].dst = &(SPIx->DR);
+		tx_ch_xfer[i].dst = (uint8_t *) & (SPIx->DR);
 #endif
 		tx_ch_xfer[i].xfer_type = MEM_TO_DEV;
 		tx_ch_xfer[i].periph = NO_OS_DMA_IRQ;
@@ -526,7 +526,7 @@ int32_t stm32_config_dma_and_start(struct no_os_spi_desc* desc,
 #ifndef SPI_SR_RXNE
 		rx_ch_xfer[i].src = &(SPIx->RXDR);
 #else
-		rx_ch_xfer[i].src = &(SPIx->DR);
+		rx_ch_xfer[i].src = (uint8_t *) & (SPIx->DR);
 #endif
 		rx_ch_xfer[i].periph = NO_OS_DMA_IRQ;
 		rx_ch_xfer[i].xfer_type = DEV_TO_MEM;

drivers/platform/stm32/stm32_spi.h
@@ -93,8 +93,8 @@ struct stm32_spi_desc {
 	bool stm32_spi_dma_done;
 	void (*stm32_spi_dma_user_cb)(void *ctx);
 	void *stm32_spi_dma_user_ctx;
-	struct stm32_dma_channel* rx_ch_xfer;
-	struct stm32_dma_channel* tx_ch_xfer;
+	struct no_os_dma_xfer_desc *rx_ch_xfer;
+	struct no_os_dma_xfer_desc *tx_ch_xfer;
 #ifdef HAL_TIM_MODULE_ENABLED
 	/** CS PWM descriptor */
 	struct no_os_pwm_desc* pwm_desc;