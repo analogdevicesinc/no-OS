analogdevicesinc/no-OS

commit 4a8dcd9ebd097b1acf439096d259ef9095288f20

Use no_os functions for dynamic memory
Use no_os_clloc, no_os_malloc, and no_os_free for dynamic memory
management.

Signed-off-by: George Mois <george.mois@analog.com>

drivers/adc/ad713x/ad713x.c
@@ -52,6 +52,7 @@
 #include "ad713x.h"
 #include "no_os_delay.h"
 #include "no_os_error.h"
+#include "no_os_alloc.h"
 
 /******************************************************************************/
 /***************************** Variable definition ****************************/
@@ -527,7 +528,7 @@ int32_t ad713x_init(struct ad713x_dev **device,
 	int32_t ret;
 	uint8_t data;
 
-	dev = (struct ad713x_dev *)calloc(1, sizeof(*dev));
+	dev = (struct ad713x_dev *)no_os_calloc(1, sizeof(*dev));
 	if (!dev)
 		return -1;
 
@@ -536,7 +537,7 @@ int32_t ad713x_init(struct ad713x_dev **device,
 		if (NO_OS_IS_ERR_VALUE(ret))
 			goto error_dev;
 	} else {
-		dev->spi_desc = calloc(1, sizeof *dev->spi_desc);
+		dev->spi_desc = no_os_calloc(1, sizeof *dev->spi_desc);
 		dev->spi_desc->chip_select = init_param->spi_init_prm.chip_select;
 		dev->spi_desc->extra = init_param->spi_common_dev->extra;
 		dev->spi_desc->max_speed_hz = init_param->spi_init_prm.max_speed_hz;
@@ -622,7 +623,7 @@ int32_t ad713x_remove(struct ad713x_dev *dev)
 	if(NO_OS_IS_ERR_VALUE(ret))
 		return -1;
 
-	free(dev);
+	no_os_free(dev);
 
 	return 0;
 }

commit 14002f93113340db7bc223a97a7eb9006ffde167

drivers: ad713x: fix NULL deref in init
Add NULL check after no_os_calloc() to prevent NULL pointer
dereference if memory allocation fails.

Fixes: 4a8dcd9eb ("Use no_os functions for dynamic memory")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad713x/ad713x.c
@@ -553,6 +553,8 @@ int32_t ad713x_init(struct ad713x_dev **device,
 			goto error_dev;
 	} else {
 		dev->spi_desc = no_os_calloc(1, sizeof * dev->spi_desc);
+		if (!dev->spi_desc)
+			goto error_dev;
 		dev->spi_desc->chip_select = init_param->spi_init_prm.chip_select;
 		dev->spi_desc->extra = init_param->spi_common_dev->extra;
 		dev->spi_desc->max_speed_hz = init_param->spi_init_prm.max_speed_hz;