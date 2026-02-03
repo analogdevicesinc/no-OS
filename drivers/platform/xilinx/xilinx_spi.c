analogdevicesinc/no-OS

commit 463a16fb8dcf18c4662fd11203d8a411758b08ab

xilinx: spi: add support for the generic layer
Update Xilinx SPI implementation using the spi generic layer.

Use `spi_platform_ops` structure to call Xilinx specific SPI functions.

Rename the xilinx spi source file to `xilinx_spi.c` and avoid naming
conflicts with the new spi generic layer.

Update Xilinx platform dependent projects to the new SPI structure.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/platform/xilinx/xilinx_spi.c
@@ -1,5 +1,5 @@
 /***************************************************************************//**
- *   @file   xilinx/spi.c
+ *   @file   xilinx/xilinx_spi.c
  *   @brief  Implementation of Xilinx SPI Generic Driver.
  *   @author Antoniu Miclaus (antoniu.miclaus@analog.com)
 ********************************************************************************
@@ -71,6 +71,16 @@
 /******************************************************************************/
 /************************ Functions Definitions *******************************/
 /******************************************************************************/
+
+
+/**
+ * @brief Xilinx platform specific SPI platform ops structure
+ */
+const struct spi_platform_ops xil_platform_ops = {
+	.spi_ops_init = &xil_spi_init,
+	.spi_ops_write_and_read = &xil_spi_write_and_read,
+	.spi_ops_remove = &xil_spi_remove
+};
 
 /**
  * @brief Initialize the hardware SPI peripherial
@@ -252,8 +262,8 @@ ps_error:
  * @param param - The structure that contains the SPI parameters.
  * @return SUCCESS in case of success, FAILURE otherwise.
  */
-int32_t spi_init(struct spi_desc **desc,
-		 const struct spi_init_param *param)
+int32_t xil_spi_init(struct spi_desc **desc,
+		     const struct spi_init_param *param)
 {
 	int32_t				ret;
 	enum xil_spi_type		*spi_type;
@@ -316,7 +326,7 @@ init_error:
  * @param desc - The SPI descriptor.
  * @return SUCCESS in case of success, FAILURE otherwise.
  */
-int32_t spi_remove(struct spi_desc *desc)
+int32_t xil_spi_remove(struct spi_desc *desc)
 {
 #ifdef XSPI_H
 	int32_t				ret;
@@ -384,10 +394,9 @@ error:
  * @param bytes_number - Number of bytes to write/read.
  * @return SUCCESS in case of success, FAILURE otherwise.
  */
-
-int32_t spi_write_and_read(struct spi_desc *desc,
-			   uint8_t *data,
-			   uint16_t bytes_number)
+int32_t xil_spi_write_and_read(struct spi_desc *desc,
+			       uint8_t *data,
+			       uint16_t bytes_number)
 {
 	int32_t			ret;
 	struct xil_spi_desc	*xdesc;

drivers/platform/xilinx/spi_extra.h
@@ -98,4 +98,24 @@ typedef struct xil_spi_desc {
 	void			*instance;
 } xil_spi_desc;
 
+/**
+ * @brief Xilinx platform specific SPI platform ops structure
+ */
+extern const struct spi_platform_ops xil_platform_ops;
+
+/******************************************************************************/
+/************************ Functions Declarations ******************************/
+/******************************************************************************/
+
+/* Initialize the SPI communication peripheral. */
+int32_t xil_spi_init(struct spi_desc **desc,
+		     const struct spi_init_param *param);
+
+/* Free the resources allocated by spi_init(). */
+int32_t xil_spi_remove(struct spi_desc *desc);
+
+/* Write and read data to/from SPI. */
+int32_t xil_spi_write_and_read(struct spi_desc *desc, uint8_t *data,
+			       uint16_t bytes_number);
+
 #endif // SPI_EXTRA_H_

projects/ad9081/src.mk
@@ -27,10 +27,11 @@ SRCS := $(PROJECT)/src/app.c						\
 	$(DRIVERS)/axi_core/jesd204/axi_jesd204_tx.c			\
 	$(DRIVERS)/axi_core/jesd204/jesd204_clk.c			\
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c		\
+	$(DRIVERS)/spi/spi.c						\
 	$(PLATFORM_DRIVERS)/axi_io.c					\
 	$(PLATFORM_DRIVERS)/delay.c					\
 	$(PLATFORM_DRIVERS)/gpio.c					\
-	$(PLATFORM_DRIVERS)/spi.c					\
+	$(PLATFORM_DRIVERS)/xilinx_spi.c				\
 	$(NO-OS)/util/clk.c						\
 	$(NO-OS)/util/util.c
 ifeq (y,$(strip $(QUAD_MXFE)))

projects/ad9081/src/app.c
@@ -85,6 +85,7 @@ int main(void)
 		.max_speed_hz = 1000000,
 		.mode = SPI_MODE_0,
 		.chip_select = PHY_CS,
+		.platform_ops = &xil_platform_ops,
 		.extra = &xil_spi_param
 	};
 	struct link_init_param jesd_tx_link = {

projects/ad9081/src/app_clock.c
@@ -93,6 +93,7 @@ int32_t app_clock_init(struct clk dev_refclk[MULTIDEVICE_INSTANCE_COUNT])
 #else
 		.chip_select = CLK_CS,
 #endif
+		.platform_ops = &xil_platform_ops,
 		.extra = &xil_spi_param
 	};
 

projects/ad9172/src.mk
@@ -10,7 +10,8 @@
 ################################################################################
 
 SRCS := $(PROJECT)/src/main.c
-SRCS += $(DRIVERS)/frequency/hmc7044/hmc7044.c				\
+SRCS += $(DRIVERS)/spi/spi.c						\
+	$(DRIVERS)/frequency/hmc7044/hmc7044.c				\
 	$(DRIVERS)/dac/ad917x/ad9172.c					\
 	$(DRIVERS)/dac/ad917x/ad917x_api/ad917x_api.c			\
 	$(DRIVERS)/dac/ad917x/ad917x_api/ad917x_jesd_api.c		\
@@ -25,7 +26,7 @@ SRCS += $(DRIVERS)/axi_core/axi_dac_core/axi_dac_core.c			\
 	$(DRIVERS)/axi_core/jesd204/xilinx_transceiver.c		\
 	$(NO-OS)/util/util.c
 SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c					\
-	$(PLATFORM_DRIVERS)/spi.c					\
+	$(PLATFORM_DRIVERS)/xilinx_spi.c				\
 	$(PLATFORM_DRIVERS)/gpio.c					\
 	$(PLATFORM_DRIVERS)/delay.c
 INCS := $(PROJECT)/src/parameters.h					\

projects/ad9172/src/README
@@ -6,9 +6,10 @@
 	cp ../../../include/spi.h ./
 	cp ../../../include/gpio.h ./
 	cp ../../../include/delay.h ./
+	cp ../../../drivers/spi/spi.c ./
 	cp ../../../drivers/platform/xilinx/axi_io.c ./
 	cp ../../../drivers/platform/xilinx/spi_extra.h ./
-	cp ../../../drivers/platform/xilinx/spi.c ./
+	cp ../../../drivers/platform/xilinx/xilinx_spi.c ./
 	cp ../../../drivers/platform/xilinx/gpio_extra.h ./
 	cp ../../../drivers/platform/xilinx/gpio.c ./
 	cp ../../../drivers/platform/xilinx/delay.c ./

projects/ad9172/src/main.c
@@ -77,6 +77,7 @@ int main(void)
 		.max_speed_hz = 10000000,
 		.mode = SPI_MODE_0,
 		.chip_select = SPI_HMC7044_CS,
+		.platform_ops = &xil_platform_ops,
 		.extra = &xil_spi_param
 	};
 
@@ -132,6 +133,7 @@ int main(void)
 		.max_speed_hz = 1000000,
 		.mode = SPI_MODE_0,
 		.chip_select = SPI_AD9172_CS,
+		.platform_ops = &xil_platform_ops,
 		.extra = &xil_spi_param
 	};
 

commit 0273db393c4b071509c138edcf1135b90846da8b

drivers: xilinx_spi: remove redundant free on NULL descriptor
The no_os_free(*desc) call in the error path serves no purpose since
*desc is NULL at that point. Remove this dead code.

Fixes: 463a16fb8 ("xilinx: spi: add support for the generic layer")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/platform/xilinx/xilinx_spi.c
@@ -249,10 +249,8 @@ int32_t xil_spi_init(struct no_os_spi_desc **desc,
 	}
 
 	*desc = no_os_malloc(sizeof(**desc));
-	if (! *desc) {
-		no_os_free(*desc);
+	if (!*desc)
 		return -1;
-	}
 
 	spi_type = param->extra;
 