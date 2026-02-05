analogdevicesinc/no-OS

commit 4fb4a24cc2ec6633363ee587ca697dbe229f8c39

drivers: adc: Add support for AD7091R-2/-4/-8
Add driver for AD7091R-2/-4/-8 devices which roughtly support the same
operations as AD7091R5, but are interfaced through SPI.

Signed-off-by: Marcelo Schmitt <marcelo.schmitt@analog.com>

drivers/adc/ad7091r8/iio_ad7091r8.c
@@ -1,0 +1,368 @@
+/***************************************************************************//**
+ *   @file   iio_ad7091r8.c
+ *   @brief  Implementation of IIO AD7091R8 Driver.
+ *   @author Marcelo Schmitt (marcelo.schmitt@analog.com)
+********************************************************************************
+ * Copyright 2024(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/******************************************************************************/
+/***************************** Include Files **********************************/
+/******************************************************************************/
+#include <stdlib.h>
+#include <stdio.h>
+#include "no_os_error.h"
+#include "no_os_util.h"
+#include "iio_ad7091r8.h"
+#include "ad7091r8.h"
+#include "no_os_units.h"
+#include "no_os_alloc.h"
+
+/******************************************************************************/
+/************************ Functions Declarations ******************************/
+/******************************************************************************/
+static int ad7091r8_iio_read_reg(struct ad7091r8_iio_dev *dev, uint32_t reg,
+				 uint32_t *readval);
+static int ad7091r8_iio_write_reg(struct ad7091r8_iio_dev *dev, uint32_t reg,
+				  uint32_t writeval);
+static int ad7091r8_iio_read_raw(void *dev, char *buf, uint32_t len,
+				 const struct iio_ch_info *channel, intptr_t priv);
+static int ad7091r8_iio_read_scale(void *dev, char *buf, uint32_t len,
+				   const struct iio_ch_info *channel, intptr_t priv);
+static int ad7091r8_buffer_preenable(void* dev, uint32_t mask);
+static int32_t ad7091r8_trigger_handler(struct iio_device_data *iio_dev_data);
+
+/******************************************************************************/
+/************************ Variable Declarations ******************************/
+/******************************************************************************/
+static struct iio_attribute ad7091r8_iio_attrs[] = {
+	{
+		.name = "raw",
+		.show = ad7091r8_iio_read_raw,
+	},
+	{
+		.name   = "scale",
+		.shared = IIO_SHARED_BY_TYPE,
+		.show   = ad7091r8_iio_read_scale,
+	},
+	END_ATTRIBUTES_ARRAY
+};
+
+static struct scan_type ad7091r8_iio_scan_type = {
+	.sign = 'u',
+	.realbits = 12,
+	.storagebits = 16,
+	.shift = 0,
+	.is_big_endian = false
+};
+
+#define AD7091R8_CHANNEL(index) {			\
+	.ch_type = IIO_VOLTAGE,				\
+	.indexed = 1,					\
+	.channel = index,				\
+	.scan_type = &ad7091r8_iio_scan_type,		\
+	.scan_index = index,				\
+	.attributes = ad7091r8_iio_attrs,		\
+	.ch_out = false					\
+}
+
+static struct iio_channel ad7091r2_channels[] = {
+	AD7091R8_CHANNEL(0),
+	AD7091R8_CHANNEL(1),
+};
+
+static struct iio_channel ad7091r4_channels[] = {
+	AD7091R8_CHANNEL(0),
+	AD7091R8_CHANNEL(1),
+	AD7091R8_CHANNEL(2),
+	AD7091R8_CHANNEL(3),
+};
+
+static struct iio_channel ad7091r8_channels[] = {
+	AD7091R8_CHANNEL(0),
+	AD7091R8_CHANNEL(1),
+	AD7091R8_CHANNEL(2),
+	AD7091R8_CHANNEL(3),
+	AD7091R8_CHANNEL(4),
+	AD7091R8_CHANNEL(5),
+	AD7091R8_CHANNEL(6),
+	AD7091R8_CHANNEL(7),
+};
+
+static struct iio_device ad7091r2_iio_device = ad7091r8_iio_device(
+			ad7091r2_channels);
+static struct iio_device ad7091r4_iio_device = ad7091r8_iio_device(
+			ad7091r4_channels);
+static struct iio_device ad7091r8_iio_device = ad7091r8_iio_device(
+			ad7091r8_channels);
+
+/******************************************************************************/
+/************************ Functions Definitions *******************************/
+/******************************************************************************/
+
+static int ad7091r8_iio_read_reg(struct ad7091r8_iio_dev *dev, uint32_t reg,
+				 uint32_t *readval)
+{
+	uint16_t read_data;
+	int ret;
+
+	return ad7091r8_spi_reg_read(dev->ad7091r8_dev, reg, &read_data);
+	if (ret)
+		return ret;
+
+	*readval = read_data;
+	return 0;
+}
+
+static int ad7091r8_iio_write_reg(struct ad7091r8_iio_dev *dev, uint32_t reg,
+				  uint32_t writeval)
+{
+	return ad7091r8_spi_reg_write(dev->ad7091r8_dev, reg, writeval);
+}
+
+/***************************************************************************//**
+ * @brief Handles the read request for raw attribute.
+ *
+ * @param dev     - The iio device structure.
+ * @param buf	  - Command buffer to be filled with requested data.
+ * @param len     - Length of the received command buffer in bytes.
+ * @param channel - Command channel info.
+ * @param priv    - Command attribute id.
+ *
+ * @return ret    - Result of the reading procedure.
+ *		    In case of success, the size of the read data is returned.
+*******************************************************************************/
+static int ad7091r8_iio_read_raw(void *dev, char *buf, uint32_t len,
+				 const struct iio_ch_info *channel, intptr_t priv)
+{
+	struct ad7091r8_iio_dev *iio_ad7091r8;
+	struct ad7091r8_dev *ad7091r8_dev;
+	int32_t read_val_32;
+	uint16_t read_val;
+	int ret;
+
+	if (!dev)
+		return -EINVAL;
+
+	iio_ad7091r8 = (struct ad7091r8_iio_dev *)dev;
+	ad7091r8_dev = iio_ad7091r8->ad7091r8_dev;
+	ret = ad7091r8_read_one(ad7091r8_dev, channel->ch_num, &read_val);
+	if (ret)
+		return ret;
+
+	if ((int16_t)no_os_field_get(AD7091R8_REG_RESULT_CH_ID_MASK, read_val)
+	    != channel->ch_num)
+		return -EIO;
+
+	read_val_32 = no_os_field_get(AD7091R8_REG_RESULT_DATA_MASK, read_val);
+
+	return iio_format_value(buf, len, IIO_VAL_INT, 1, &read_val_32);
+}
+
+/***************************************************************************//**
+ * @brief Handles the read request for scale attribute.
+ *
+ * @param dev     - The iio device structure.
+ * @param buf	  - Command buffer to be filled with requested data.
+ * @param len     - Length of the received command buffer in bytes.
+ * @param channel - Command channel info.
+ * @param priv    - Command attribute id.
+ *
+ * @return ret    - Result of the reading procedure.
+ * 		    In case of success, the size of the read data is returned.
+*******************************************************************************/
+static int ad7091r8_iio_read_scale(void *dev, char *buf, uint32_t len,
+				   const struct iio_ch_info *channel, intptr_t priv)
+{
+	struct ad7091r8_iio_dev *iio_ad7091r8;
+	struct ad7091r8_dev *ad7091r8_dev;
+	int32_t vals[2];
+
+	if (!dev)
+		return -EINVAL;
+
+	iio_ad7091r8 = (struct ad7091r8_iio_dev *)dev;
+	ad7091r8_dev = iio_ad7091r8->ad7091r8_dev;
+	vals[0] = ad7091r8_dev->vref_mv;
+	vals[1] = 12;
+	return iio_format_value(buf, len, IIO_VAL_FRACTIONAL_LOG2, 2, vals);
+}
+
+/***************************************************************************//**
+ * @brief Prepares the device for buffered capture mode.
+ *
+ * @param dev     - The iio device structure.
+ * @param mask    - Mask of enabled/disabled channels.
+ *
+ * @return ret    - Result of the pre enable setup.
+ * 		    Zero in case of success, errno otherwise.
+*******************************************************************************/
+static int ad7091r8_buffer_preenable(void* dev, uint32_t mask)
+{
+	struct ad7091r8_iio_dev *iio_ad7091r8;
+	struct ad7091r8_dev *ad7091r8;
+	uint16_t dummy;
+	int ret;
+
+	if (!dev)
+		return -ENODEV;
+
+	iio_ad7091r8 = dev;
+	ad7091r8 = iio_ad7091r8->ad7091r8_dev;
+
+	/* Dummy read cycle to pulse CONVST */
+	ret = ad7091r8_pulse_convst(ad7091r8);
+	if (ret)
+		return ret;
+
+	/* Set enabled channels in the channel register */
+	ret = ad7091r8_spi_reg_write(ad7091r8, AD7091R8_REG_CHANNEL, mask);
+	if (ret)
+		return ret;
+
+	/* Dummy read cycle to let the conversion sequence get updated */
+	return ad7091r8_sequenced_read(ad7091r8, &dummy);
+}
+
+/***************************************************************************//**
+ * @brief Prepares the device for buffered capture mode.
+ *
+ * @param iio_dev_data    - Object with pointers to ad7091r8_iio_dev and buffer.
+ *
+ * @return ret            - Result of the trigger handler routine.
+ * 		            Zero in case of success, errno otherwise.
+*******************************************************************************/
+static int32_t ad7091r8_trigger_handler(struct iio_device_data *iio_dev_data)
+{
+	struct ad7091r8_iio_dev *iio_ad7091r8;
+	struct ad7091r8_dev *ad7091r8;
+	uint16_t data_buff[8]; /* Data buffer to store one sample-set. */
+	uint16_t read_val;
+	int i, k = 0;
+	int ret;
+
+	if (!iio_dev_data)
+		return -EINVAL;
+
+	iio_ad7091r8 = (struct ad7091r8_iio_dev *)iio_dev_data->dev;
+
+	if (!iio_ad7091r8)
+		return -EINVAL;
+
+	ad7091r8 = iio_ad7091r8->ad7091r8_dev;
+
+	/* For each enabled channel do a sequenced read and push result to buff */
+	for (i = 0; i < AD7091R_NUM_CHANNELS(ad7091r8->device_id); i++) {
+		if (iio_dev_data->buffer->active_mask & NO_OS_BIT(i)) {
+			ret = ad7091r8_sequenced_read(ad7091r8, &read_val);
+			if (ret)
+				return ret;
+
+			data_buff[k++] = read_val;
+		}
+	}
+
+	return iio_buffer_push_scan(iio_dev_data->buffer, data_buff);
+}
+
+/***************************************************************************//**
+ * @brief Initializes the AD7091R8 IIO driver
+ *
+ * @param iio_dev    - The iio device structure.
+ * @param init_param - The structure that contains the device initial
+ * 		       		   parameters.
+ *
+ * @return ret       - Result of the initialization procedure.
+*******************************************************************************/
+int ad7091r8_iio_init(struct ad7091r8_iio_dev **iio_dev,
+		      struct ad7091r8_iio_dev_init_param *init_param)
+{
+	int ret;
+	struct ad7091r8_iio_dev *desc;
+
+	if (!init_param || !init_param->ad7091r8_dev_init)
+		return -EINVAL;
+
+	desc = (struct ad7091r8_iio_dev *)no_os_calloc(1, sizeof(*desc));
+	if (!desc)
+		return -ENOMEM;
+
+	switch (init_param->ad7091r8_dev_init->device_id) {
+	case AD7091R2:
+		desc->iio_dev = &ad7091r2_iio_device;
+		break;
+	case AD7091R4:
+		desc->iio_dev = &ad7091r4_iio_device;
+		break;
+	case AD7091R8:
+		desc->iio_dev = &ad7091r8_iio_device;
+		break;
+	default:
+		ret = -EINVAL;
+		goto error_ad7091r8_init;
+	}
+
+	// Initialize ad7091r8 driver
+	ret = ad7091r8_init(&desc->ad7091r8_dev, init_param->ad7091r8_dev_init);
+	if (ret)
+		goto error_ad7091r8_init;
+
+	*iio_dev = desc;
+
+	return 0;
+
+error_ad7091r8_init:
+	no_os_free(desc);
+	return ret;
+}
+
+/***************************************************************************//**
+ * @brief Free the resources allocated by ad7091r8_iio_init().
+ *
+ * @param desc - The IIO device structure.
+ *
+ * @return ret - Result of the remove procedure.
+*******************************************************************************/
+int ad7091r8_iio_remove(struct ad7091r8_iio_dev *desc)
+{
+	int ret;
+
+	ret = ad7091r8_remove(desc->ad7091r8_dev);
+	if (ret)
+		return ret;
+
+	no_os_free(desc);
+
+	return 0;
+}

drivers/adc/ad7091r8/ad7091r8.c
@@ -1,0 +1,545 @@
+/***************************************************************************//**
+ *   @file   ad7091r8.c
+ *   @brief  Implementation of AD7091R-8 Driver
+ *   @author Marcelo Schmitt (marcelo.schmitt@analog.com)
+********************************************************************************
+ * Copyright 2024(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include <stdlib.h>
+#include <errno.h>
+
+#include "ad7091r8.h"
+#include "no_os_alloc.h"
+#include "no_os_error.h"
+#include "no_os_util.h"
+#include "no_os_delay.h"
+
+/**
+ * Pull the CONVST line up then down to signal to the start of a read/write
+ * operation.
+ * @param dev - The device structure.
+ */
+int ad7091r8_pulse_convst(struct ad7091r8_dev *dev)
+{
+	int ret;
+
+	if (!dev)
+		return -EINVAL;
+
+	ret = no_os_gpio_set_value(dev->gpio_convst, NO_OS_GPIO_LOW);
+	if (ret)
+		return ret;
+
+	return no_os_gpio_set_value(dev->gpio_convst, NO_OS_GPIO_HIGH);
+}
+
+/**
+ * Write to device.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param reg_data - The register data.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_spi_reg_write(struct ad7091r8_dev *dev,
+			   uint8_t reg_addr,
+			   uint16_t reg_data)
+{
+	uint8_t buf[2];
+
+	if (!dev)
+		return -EINVAL;
+
+	/*
+	 * AD7091R-2/-4/-8 protocol (datasheet page 31) is to do a single SPI
+	 * transfer with reg address set in bits B15:B11 and value set in B9:B0.
+	 */
+	no_os_put_unaligned_be16(
+		no_os_field_prep(AD7091R8_REG_DATA_MSK, reg_data) |
+		no_os_field_prep(AD7091R8_RD_WR_FLAG_MSK, 1) |
+		no_os_field_prep(AD7091R8_REG_ADDR_MSK, reg_addr), buf);
+
+	return no_os_spi_write_and_read(dev->spi_desc, buf, 2);
+}
+
+/**
+ * Read from device.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param reg_data - The register data.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_spi_reg_read(struct ad7091r8_dev *dev,
+			  uint8_t reg_addr,
+			  uint16_t *reg_data)
+{
+	uint8_t buf[2];
+	int ret;
+
+	if (!dev || !reg_data)
+		return -EINVAL;
+
+	if (reg_addr == AD7091R8_REG_RESULT) {
+		ret = ad7091r8_pulse_convst(dev);
+		if (ret)
+			return ret;
+	}
+
+	no_os_put_unaligned_be16(
+		no_os_field_prep(AD7091R8_RD_WR_FLAG_MSK, 0) |
+		no_os_field_prep(AD7091R8_REG_ADDR_MSK, reg_addr), buf);
+
+	/* Reg data only comes out on the next transfer (datasheet figure 52) */
+	ret = no_os_spi_write_and_read(dev->spi_desc, buf, 2);
+	if (ret)
+		return ret;
+
+	if (reg_addr == AD7091R8_REG_RESULT) {
+		ret = ad7091r8_pulse_convst(dev);
+		if (ret)
+			return ret;
+	}
+
+	no_os_put_unaligned_be16(
+		no_os_field_prep(AD7091R8_RD_WR_FLAG_MSK, 0) |
+		no_os_field_prep(AD7091R8_REG_ADDR_MSK, reg_addr), buf);
+
+	ret = no_os_spi_write_and_read(dev->spi_desc, buf, 2);
+	if (ret)
+		return ret;
+
+	*reg_data = no_os_get_unaligned_be16(buf);
+	return 0;
+}
+
+/**
+ * SPI write to device using a mask.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param mask - The mask.
+ * @param data - The register data.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_spi_write_mask(struct ad7091r8_dev *dev,
+			    uint8_t reg_addr,
+			    uint16_t mask,
+			    uint16_t data)
+{
+	uint16_t reg_data;
+	int ret;
+
+	ret = ad7091r8_spi_reg_read(dev, reg_addr, &reg_data);
+	if (ret)
+		return ret;
+
+	reg_data &= ~mask;
+	reg_data |= data;
+
+	return ad7091r8_spi_reg_write(dev, reg_addr, reg_data);
+}
+
+/**
+ * @brief Set device sleep mode.
+ *
+ * @param dev - The device structure.
+ * @param mode - The device sleep mode to set.
+ *
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_set_sleep_mode(struct ad7091r8_dev *dev,
+			    enum ad7091r8_sleep_mode mode)
+{
+	return ad7091r8_spi_write_mask(dev, AD7091R8_REG_CONF,
+				       REG_CONF_SLEEP_MODE_MASK, mode);
+}
+
+/**
+ * Set output port value.
+ * @param dev - The device structure.
+ * @param port - Port number.
+ * @param value - Value.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_set_port(struct ad7091r8_dev *dev, enum ad7091r8_port port,
+		      bool value)
+{
+	uint16_t mask;
+	uint16_t val;
+
+	switch (port) {
+	case AD7091R8_GPO0:
+		mask = REG_CONF_GPO0_MASK;
+		val = no_os_field_get(REG_CONF_GPO0_MASK, value);
+		break;
+	case AD7091R8_GPO1:
+		mask = REG_CONF_GPO1_MASK;
+		val = no_os_field_get(REG_CONF_GPO1_MASK, value);
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return ad7091r8_spi_write_mask(dev, AD7091R8_REG_CONF, mask, val);
+}
+
+/**
+ * Set GPO0 mode.
+ * @param dev - The device structure.
+ * @param mode - GPO0 new mode.
+ * @param is_cmos - 0: GPO0 is open drain
+ * 		  - 1: GPO0 is CMOS.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_set_gpo0_mode(struct ad7091r8_dev *dev,
+			   enum ad7091r8_gpo0_mode mode, bool is_cmos)
+{
+	uint16_t value = mode;
+
+	if (is_cmos)
+		value |= NO_OS_BIT(6);
+
+	return ad7091r8_spi_write_mask(dev, AD7091R8_REG_CONF,
+				       REG_CONF_GPO0_MODE_MASK, value);
+}
+
+/**
+ * Set high limit, low limit, hysteresis.
+ * Device accepts 9 bits provided by the user and sets them as the MSBs.
+ * The 3 LSBs of the internal 12-bit registers are set either to 000 or 111.
+ * Round user input according to each case.
+ * @param dev - The device structure.
+ * @param limit - Limit.
+ * @param channel - Channel.
+ * @param value - Value.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_set_limit(struct ad7091r8_dev *dev,
+		       enum ad7091r8_limit limit,
+		       uint8_t channel,
+		       uint16_t value)
+{
+	uint16_t reg;
+
+	switch (limit) {
+	case AD7091R8_LOW_LIMIT:
+		reg = AD7091R8_REG_CH_LOW_LIMIT(channel);
+		break;
+	case AD7091R8_HIGH_LIMIT:
+		reg = AD7091R8_REG_CH_HIGH_LIMIT(channel);
+		break;
+	case AD7091R8_HYSTERESIS:
+		reg = AD7091R8_REG_CH_HYSTERESIS(channel);
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return ad7091r8_spi_reg_write(dev, reg,
+				      no_os_field_get(AD7091R8_CONV_MASK, value));
+}
+
+/**
+ * Get alert.
+ * @param dev - The device structure.
+ * @param channel - Channel.
+ * @param alert - Alert type.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_get_alert(struct ad7091r8_dev *dev, uint8_t channel,
+		       enum ad7091r8_alert_type *alert)
+{
+	int ret;
+	uint16_t data;
+
+	if (!dev || !alert)
+		return -EINVAL;
+
+	if (channel >= AD7091R_NUM_CHANNELS(dev->device_id))
+		return -EINVAL;
+
+	ret = ad7091r8_spi_reg_read(dev, AD7091R8_REG_ALERT, &data);
+	if (ret)
+		return ret;
+
+	*alert = REG_ALERT_MASK(data, channel);
+
+	return 0;
+}
+
+/**
+ * Get high limit, low limit, hysteresis.
+ * The 3 LSBs of the internal 12-bit registers are set either to 000 or 111.
+ * Adjust limit data to reflect actual limit in use by the device.
+ * @param dev - The device structure.
+ * @param limit - Limit.
+ * @param channel - Channel.
+ * @param value - Value.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_get_limit(struct ad7091r8_dev *dev,
+		       enum ad7091r8_limit limit,
+		       uint8_t channel,
+		       uint16_t *value)
+{
+	uint16_t reg;
+
+	switch (limit) {
+	case AD7091R8_LOW_LIMIT:
+		reg = AD7091R8_REG_CH_LOW_LIMIT(channel);
+		break;
+	case AD7091R8_HIGH_LIMIT:
+		reg = AD7091R8_REG_CH_HIGH_LIMIT(channel);
+		break;
+	case AD7091R8_HYSTERESIS:
+		reg = AD7091R8_REG_CH_HYSTERESIS(channel);
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return ad7091r8_spi_reg_read(dev, reg, value);
+}
+
+/**
+ * @brief Initiate a software reset or hardware reset through the RESET pin.
+ * @param dev - ad7091r8_dev device handler.
+ * @param is_software - true: Software reset
+ * 		      - false: hardware reset
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_reset(struct ad7091r8_dev *dev, bool is_software)
+{
+	int ret;
+
+	if (!dev)
+		return -EINVAL;
+
+	if (is_software) {
+		/* Bit is cleared automatically after reset */
+		return ad7091r8_spi_write_mask(dev, AD7091R8_REG_CONF,
+					       REG_CONF_RESET_MASK,
+					       NO_OS_BIT(9));
+	}
+	ret = no_os_gpio_set_value(dev->gpio_reset, NO_OS_GPIO_LOW);
+	if (ret)
+		return ret;
+
+	no_os_udelay(1);
+	return no_os_gpio_set_value(dev->gpio_reset, NO_OS_GPIO_HIGH);
+}
+
+/***************************************************************************//**
+ * Initializes the communication peripheral and the initial Values for
+ * AD7092R-8 Board.
+ * @param device     - The device structure.
+ * @param init_param - The structure that contains the device initial
+ * 		       parameters.
+ *
+ * @return ret - The result of the initialization procedure.
+ *               Example: -1 - SPI peripheral was not initialized or the
+ *                             device is not present.
+ *                         0 - SPI peripheral was initialized and the
+ *                             device is present.
+*******************************************************************************/
+int ad7091r8_init(struct ad7091r8_dev **device,
+		  struct ad7091r8_init_param *init_param)
+{
+	struct ad7091r8_dev *dev;
+	int ret;
+
+	if (!device)
+		return -EINVAL;
+
+	dev = (struct ad7091r8_dev *)no_os_calloc(1, sizeof(*dev));
+	if (!dev)
+		return -ENOMEM;
+
+	ret = no_os_spi_init(&dev->spi_desc, init_param->spi_init);
+	if (ret)
+		goto err_free_dev;
+
+	dev->device_id = init_param->device_id;
+
+	ret = no_os_gpio_get(&dev->gpio_convst, init_param->gpio_convst);
+	if (ret)
+		goto err_release_spi;
+
+	ret = no_os_gpio_direction_output(dev->gpio_convst, NO_OS_GPIO_HIGH);
+	if (ret)
+		goto err_release_convst;
+
+	ret = no_os_gpio_get_optional(&dev->gpio_reset, init_param->gpio_reset);
+	if (!ret && dev->gpio_reset) {
+		ret = no_os_gpio_direction_output(dev->gpio_reset,
+						  NO_OS_GPIO_HIGH);
+		if (ret)
+			goto err_release_reset;
+	}
+
+	ret = ad7091r8_reset(dev, !dev->gpio_reset);
+	if (ret)
+		goto err_release_reset;
+
+	/* Use external vref or enable internal vref */
+	dev->vref_mv = init_param->vref_mv;
+	if (!dev->vref_mv) {
+		dev->vref_mv = 2500;
+		ret = ad7091r8_set_sleep_mode(dev, AD7091R8_SLEEP_MODE_1);
+		if (ret)
+			goto err_release_reset;
+	}
+
+	/* Device powers-up in normal mode */
+	*device = dev;
+
+	return 0;
+
+err_release_reset:
+	no_os_gpio_remove(dev->gpio_reset);
+
+err_release_convst:
+	no_os_gpio_remove(dev->gpio_convst);
+
+err_release_spi:
+	no_os_spi_remove(dev->spi_desc);
+
+err_free_dev:
+	no_os_free(dev);
+	return ret;
+}
+
+/***************************************************************************//**
+ * @brief Free the resources allocated by ad7091r8_init().
+ *
+ * @param dev - The device structure.
+ *
+ * @return ret - The result of the remove procedure.
+*******************************************************************************/
+int ad7091r8_remove(struct ad7091r8_dev *dev)
+{
+	int ret;
+
+	ret = no_os_gpio_remove(dev->gpio_reset);
+	if (ret)
+		return ret;
+
+	ret = no_os_gpio_remove(dev->gpio_convst);
+	if (ret)
+		return ret;
+
+	ret = no_os_spi_remove(dev->spi_desc);
+	if (ret)
+		return ret;
+
+	no_os_free(dev);
+	return 0;
+}
+
+/**
+ * Set device channel.
+ * @param dev - The device structure.
+ * @param channel - Channel.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_set_channel(struct ad7091r8_dev *dev, uint8_t channel)
+{
+	int ret;
+
+	if (!dev)
+		return -EINVAL;
+
+	if (channel >= AD7091R_NUM_CHANNELS(dev->device_id))
+		return -EINVAL;
+
+	ret = ad7091r8_pulse_convst(dev);
+	if (ret)
+		return ret;
+
+	return ad7091r8_spi_reg_write(dev, AD7091R8_REG_CHANNEL,
+				      NO_OS_BIT(channel));
+}
+
+/**
+ * Read one sample.
+ * @param dev - The device structure.
+ * @param channel - Channel.
+ * @param read_val - Value.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_read_one(struct ad7091r8_dev *dev, uint8_t channel,
+		      uint16_t *read_val)
+{
+	int ret;
+
+	if (!dev || !read_val)
+		return -EINVAL;
+
+	if (channel >= AD7091R_NUM_CHANNELS(dev->device_id))
+		return -EINVAL;
+
+	ret = ad7091r8_set_channel(dev, channel);
+	if (ret)
+		return ret;
+
+	return ad7091r8_spi_reg_read(dev, AD7091R8_REG_RESULT, read_val);
+}
+
+/**
+ * Read the next channel set in the channel sequencer (channel register).
+ * @param dev - The device structure.
+ * @param read_val - Value.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int ad7091r8_sequenced_read(struct ad7091r8_dev *dev, uint16_t *read_val)
+{
+
+	uint8_t buf[2];
+	int ret;
+
+	if (!dev)
+		return -EINVAL;
+
+	ret = ad7091r8_pulse_convst(dev);
+	if (ret)
+		return ret;
+
+	no_os_put_unaligned_be16(0xf800, buf); /* NOP command */
+
+	ret = no_os_spi_write_and_read(dev->spi_desc, buf, 2);
+	if (ret)
+		return ret;
+
+	*read_val = no_os_get_unaligned_be16(buf);
+	return 0;
+}

commit 71174ec0bc120f4d5ade43e4126f9ec3085d10eb

drivers: ad7091r8: fix missing ret assignment
The function returned directly instead of assigning the result
to ret, causing readval to never be set and making subsequent
code unreachable.

Fixes: 4fb4a24cc ("drivers: adc: Add support for AD7091R-2/-4/-8")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad7091r8/iio_ad7091r8.c
@@ -118,7 +118,7 @@ static int ad7091r8_iio_read_reg(struct ad7091r8_iio_dev *dev, uint32_t reg,
 	uint16_t read_data;
 	int ret;
 
-	return ad7091r8_spi_reg_read(dev->ad7091r8_dev, reg, &read_data);
+	ret = ad7091r8_spi_reg_read(dev->ad7091r8_dev, reg, &read_data);
 	if (ret)
 		return ret;
 