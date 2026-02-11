analogdevicesinc/no-OS

commit f5f7bdc70e3186305c5f99443373b89d08196e27

drivers: ad738x: Enable  clkgen and trigger pwm
The driver is timing out and not returning any when using spi engine
offload. Clkgen and the trigger pwm needs to be enabled for offload to
work.

Add init parameters and enable clkgen and pwm  so that data is read
though spi when using spi engine offload.

Signed-off-by: Axel Haslam <ahaslam@baylibre.com>

drivers/adc/ad738x/ad738x.c
@@ -303,6 +303,10 @@ int32_t ad738x_read_data(struct ad738x_dev *dev,
 		CS_HIGH,
 	};
 
+	ret = no_os_pwm_enable(dev->pwm_desc);
+	if (ret != 0)
+		return ret;
+
 	ret = spi_engine_offload_init(dev->spi_desc, dev->offload_init_param);
 	if (ret != 0)
 		return ret;
@@ -318,6 +322,10 @@ int32_t ad738x_read_data(struct ad738x_dev *dev,
 
 	if (dev->dcache_invalidate_range)
 		dev->dcache_invalidate_range(msg.rx_addr, samples * 4);
+
+	ret = no_os_pwm_disable(dev->pwm_desc);
+	if (ret != 0)
+		return ret;
 #else
 	ret = no_os_spi_write_and_read(dev->spi_desc, buf, samples);
 	if (ret)
@@ -348,26 +356,49 @@ int32_t ad738x_init(struct ad738x_dev **device,
 #if !defined(USE_STANDARD_SPI)
 	dev->offload_init_param = init_param->offload_init_param;
 	dev->dcache_invalidate_range = init_param->dcache_invalidate_range;
+	ret = axi_clkgen_init(&dev->clkgen, init_param->clkgen_init);
+	if (ret)
+		goto err;
+
+	ret = axi_clkgen_set_rate(dev->clkgen, init_param->axi_clkgen_rate);
+	if (ret)
+		goto err;
+
+	ret = no_os_pwm_init(&dev->pwm_desc, init_param->pwm_init);
+	if (ret)
+		goto err;
+
 #endif
 	dev->conv_mode = init_param->conv_mode;
 	dev->ref_sel = init_param->ref_sel;
 	dev->ref_voltage_mv = init_param->ref_voltage_mv;
 
 	ret = no_os_spi_init(&dev->spi_desc, init_param->spi_param);
+	if (ret)
+		goto err;
 
-	ret |= ad738x_reset(dev, HARD_RESET);
+	ret = ad738x_reset(dev, HARD_RESET);
+	if (ret)
+		goto err;
+
 	no_os_mdelay(1000);
 	/* 1-wire or 2-wire mode */
-	ret |= ad738x_set_conversion_mode(dev, dev->conv_mode);
+	ret = ad738x_set_conversion_mode(dev, dev->conv_mode);
+	if (ret)
+		goto err;
+
 	/* Set internal or external reference */
-	ret |= ad738x_reference_sel(dev, dev->ref_sel);
+	ret = ad738x_reference_sel(dev, dev->ref_sel);
+	if (ret)
+		goto err;
 
 	*device = dev;
-
-	if (!ret)
-		printf("ad738x successfully initialized\n");
+	printf("ad738x successfully initialized\n");
 	no_os_mdelay(1000);
-
+	return 0;
+err:
+	printf("ad738x failed to initialize\n");
+	ad738x_remove(dev);
 	return ret;
 }
 /**
@@ -380,8 +411,18 @@ int32_t ad738x_remove(struct ad738x_dev *dev)
 	int32_t ret;
 
 	ret = no_os_spi_remove(dev->spi_desc);
+	if (ret)
+		goto out;
+#if !defined(USE_STANDARD_SPI)
+	ret = axi_clkgen_remove(dev->clkgen);
+	if (ret)
+		goto out;
 
+	ret = no_os_pwm_remove(dev->pwm_desc);
+	if (ret)
+		goto out;
+#endif
 	no_os_free(dev);
-
+out:
 	return ret;
 }

drivers/adc/ad738x/ad738x.h
@@ -45,6 +45,7 @@
 /***************************** Include Files **********************************/
 /******************************************************************************/
 #include "no_os_util.h"
+#include "clk_axi_clkgen.h"
 #if defined(USE_STANDARD_SPI)
 #include "no_os_spi.h"
 #endif
@@ -158,6 +159,8 @@ struct ad738x_dev {
 #if !defined(USE_STANDARD_SPI)
 	/** SPI module offload init */
 	struct spi_engine_offload_init_param *offload_init_param;
+	struct axi_clkgen *clkgen;
+	struct no_os_pwm_desc *pwm_desc;
 #endif
 	/* Device Settings */
 	enum ad738x_conv_mode 	conv_mode;
@@ -172,8 +175,11 @@ struct ad738x_init_param {
 	/* SPI */
 	struct no_os_spi_init_param		*spi_param;
 #if !defined(USE_STANDARD_SPI)
+	struct axi_clkgen_init *clkgen_init;
+	uint32_t axi_clkgen_rate;
 	/** SPI module offload init */
 	struct spi_engine_offload_init_param *offload_init_param;
+	struct no_os_pwm_init_param *pwm_init;
 #endif
 	/* Device Settings */
 	enum ad738x_conv_mode	conv_mode;

commit 9f2c281dae3999c213ac8bba70b747c68e892e72

adc: ad738x: fix uninitialized fields and dev leak
Use no_os_calloc to zero-init descriptor pointers, preventing garbage
dereferences in ad738x_remove on init failure. Also ensure dev is
always freed even when sub-remove calls fail.

Fixes: f5f7bdc70 ("drivers: ad738x: Enable  clkgen and trigger pwm")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad738x/ad738x.c
@@ -395,7 +395,7 @@ int32_t ad738x_init(struct ad738x_dev **device,
 	struct ad738x_dev *dev;
 	int32_t ret;
 
-	dev = (struct ad738x_dev *)no_os_malloc(sizeof(*dev));
+	dev = (struct ad738x_dev *)no_os_calloc(1, sizeof(*dev));
 	if (!dev)
 		return -1;
 
@@ -457,23 +457,15 @@ err:
  */
 int32_t ad738x_remove(struct ad738x_dev *dev)
 {
-	int32_t ret;
-
-	ret = no_os_spi_remove(dev->spi_desc);
-	if (ret)
-		goto out;
+	no_os_spi_remove(dev->spi_desc);
 
 #ifdef XILINX_PLATFORM
-	ret = axi_clkgen_remove(dev->clkgen);
-	if (ret)
-		goto out;
+	axi_clkgen_remove(dev->clkgen);
 #endif
 
-	ret = no_os_pwm_remove(dev->pwm_desc);
-	if (ret)
-		goto out;
+	no_os_pwm_remove(dev->pwm_desc);
 
 	no_os_free(dev);
-out:
-	return ret;
+
+	return 0;
 }