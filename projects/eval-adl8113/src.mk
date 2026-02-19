6ba82872ab9678c390c12f8d4c9ae0181585124e
RaulGeo289 <georgianraul301@gmail.com>
2026-02-19 11:49:48 +0200

    projects: eval-adl8113: Add basic example
    Signed-off-by: RaulGeo289 <georgianraul301@gmail.com>
    Added basic example for eval adl8113

---
+++ projects/eval-adl8113/src.mk
@@ -0,0 +1,14 @@
+NO_OS_INC_DIRS += \
+	$(INCLUDE) \
+	$(DRIVERS)/amplifiers/adl8113
+
+SRCS += $(DRIVERS)/api/no_os_gpio.c \
+	$(DRIVERS)/api/no_os_irq.c  \
+	$(DRIVERS)/api/no_os_dma.c  \
+	$(DRIVERS)/api/no_os_uart.c \
+	$(NO-OS)/util/no_os_alloc.c \
+	$(NO-OS)/util/no_os_mutex.c \
+	$(NO-OS)/util/no_os_list.c  \
+	$(NO-OS)/util/no_os_lf256fifo.c
+
+SRCS += $(DRIVERS)/amplifiers/adl8113/adl8113.c
