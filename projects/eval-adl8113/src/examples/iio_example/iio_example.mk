feea2ad2aa1c5f5a048cbc3317cbe9a24388b5ff
voc94 <popavasile02@yahoo.com>
2026-03-02 12:27:21 +0200

    projects: eval-adl8113: Add IIO example
      Added IIO example with LED feedback on mode change.
      Supports libiio serial backend for remote control.
    
      Signed-off-by: Alexandru Vasile Popa <Alexandruvasile.Popa@analog.com>

---
+++ projects/eval-adl8113/src/examples/iio_example/iio_example.mk
@@ -0,0 +1,5 @@
+IIOD = y
+INCS += $(DRIVERS)/amplifiers/adl8113/iio_adl8113.h
+SRCS += $(DRIVERS)/amplifiers/adl8113/iio_adl8113.c
+SRCS += $(PROJECT)/src/examples/iio_example/iio_example.c
+SRCS += $(MAXIM_LIBRARIES)/PeriphDrivers/Source/SYS/mxc_delay.c
