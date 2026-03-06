6b7f591854b31eb47fbb8bc5f5a225eb606386ff
Stefan Popa <stefan.popa@analog.com>
2026-03-06 15:07:31 +0200

    drivers: rf-transceiver: palma: Adapt API for no-OS
    Minimal changes to the vendor API for bare-metal no-OS compatibility:
    
    - platforms/adi_platform.c: Disable global function pointer definitions
      with #if 0 guard; they are defined by the no-OS HAL layer in
      projects/adrv903x/src/common/hal/no_os_platform.c to avoid
      multiple-definition linker errors.
    
    - platforms/adi_platform_aux.c: Comment out __gcov_flush() call which
      is only available when compiled with --coverage; not used in
      bare-metal builds.
    
    Signed-off-by: Stefan Popa <stefan.popa@analog.com>

--- drivers/rf-transceiver/palma/platforms/adi_platform_aux.c
+++ drivers/rf-transceiver/palma/platforms/adi_platform_aux.c
@@ -29,7 +29,7 @@ ADI_API int32_t adi_platform_gcov_flush()
 #ifdef __GNUC__
     (void) ADI_LIBRARY_PRINTF("Flushing gcov files ... OK\n"); /*not resolved*/
     ADI_LIBRARY_FFLUSH(stdout);
-    __gcov_flush();
+//    __gcov_flush();
 #endif
     return (0);
 }
