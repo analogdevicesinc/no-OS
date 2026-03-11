analogdevicesinc/no-OS

commit 347b8d8c9361f4f6144fcc878fa8e877323a71f7

projects: ltm3360b: Add example project for LTM3360B
Add basic example project for LTM3360B

Add make based CI build support

Signed-off-by: Janani Sunil <janani.sunil@analog.com>

projects/ltm3360b/src.mk
@@ -1,0 +1,36 @@
+SRCS += $(DRIVERS)/api/no_os_gpio.c \
+	$(DRIVERS)/api/no_os_i2c.c \
+	$(DRIVERS)/api/no_os_irq.c \
+	$(DRIVERS)/api/no_os_uart.c \
+	$(NO-OS)/util/no_os_list.c \
+	$(NO-OS)/util/no_os_util.c \
+	$(NO-OS)/util/no_os_alloc.c \
+	$(NO-OS)/util/no_os_delay.c \
+	$(NO-OS)/util/no_os_lf256fifo.c \
+	$(NO-OS)/util/no_os_mutex.c \
+	$(NO-OS)/util/no_os_crc8.c
+
+INCS += $(INCLUDE)/no_os_delay.h \
+	$(INCLUDE)/no_os_error.h \
+	$(INCLUDE)/no_os_gpio.h \
+	$(INCLUDE)/no_os_i2c.h \
+	$(INCLUDE)/no_os_irq.h \
+	$(INCLUDE)/no_os_print_log.h \
+	$(INCLUDE)/no_os_list.h \
+	$(INCLUDE)/no_os_uart.h \
+	$(INCLUDE)/no_os_util.h \
+	$(INCLUDE)/no_os_alloc.h \
+	$(INCLUDE)/no_os_lf256fifo.h \
+	$(INCLUDE)/no_os_mutex.h \
+	$(INCLUDE)/no_os_crc8.h
+
+SRCS += $(DRIVERS)/power/ltm3360b/ltm3360b.c
+INCS += $(DRIVERS)/power/ltm3360b/ltm3360b.h
+
+SRCS += $(PROJECT)/src/common/common_data.c \
+	$(PROJECT)/src/platform/$(PLATFORM)/parameters.c \
+	$(PROJECT)/src/platform/$(PLATFORM)/main.c \
+	$(PROJECT)/src/examples/basic/basic_example.c
+
+INCS += $(PROJECT)/src/common/common_data.h \
+	$(PROJECT)/src/platform/$(PLATFORM)/parameters.h

projects/ltm3360b/Makefile
@@ -1,0 +1,9 @@
+EXAMPLE ?= basic
+
+include ../../tools/scripts/generic_variables.mk
+
+include ../../tools/scripts/examples.mk
+
+include src.mk
+
+include ../../tools/scripts/generic.mk

projects/ltm3360b/sdp-k1.ioc
@@ -1,0 +1,173 @@
+#MicroXplorer Configuration settings - do not modify
+CAD.formats=
+CAD.pinconfig=
+CAD.provider=
+File.Version=6
+GPIO.groupedBy=Group By Peripherals
+KeepUserPlacement=false
+Mcu.CPN=STM32F469NIH6
+Mcu.Family=STM32F4
+Mcu.IP0=I2C1
+Mcu.IP1=NVIC
+Mcu.IP2=RCC
+Mcu.IP3=SYS
+Mcu.IP4=UART5
+Mcu.IPNb=5
+Mcu.Name=STM32F469NIHx
+Mcu.Package=TFBGA216
+Mcu.Pin0=PB8
+Mcu.Pin1=PB4
+Mcu.Pin10=VP_SYS_VS_Systick
+Mcu.Pin2=PB3
+Mcu.Pin3=PC12
+Mcu.Pin4=PA15
+Mcu.Pin5=PB7
+Mcu.Pin6=PD2
+Mcu.Pin7=PH0/OSC_IN
+Mcu.Pin8=PH1/OSC_OUT
+Mcu.Pin9=PA7
+Mcu.PinsNb=11
+Mcu.ThirdPartyNb=0
+Mcu.UserConstants=
+Mcu.UserName=STM32F469NIHx
+MxCube.Version=6.5.0
+MxDb.Version=DB.6.0.50
+NVIC.BusFault_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
+NVIC.DebugMonitor_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
+NVIC.ForceEnableDMAVector=false
+NVIC.HardFault_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
+NVIC.I2C1_ER_IRQn=true\:0\:0\:false\:false\:true\:true\:true\:true
+NVIC.I2C1_EV_IRQn=true\:0\:0\:false\:false\:true\:true\:true\:true
+NVIC.MemoryManagement_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
+NVIC.NonMaskableInt_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
+NVIC.PendSV_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
+NVIC.PriorityGroup=NVIC_PRIORITYGROUP_4
+NVIC.SVCall_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
+NVIC.SysTick_IRQn=true\:0\:0\:false\:false\:true\:false\:true\:false
+NVIC.UART5_IRQn=true\:0\:0\:false\:false\:true\:true\:true\:false
+NVIC.UsageFault_IRQn=true\:0\:0\:false\:false\:true\:false\:false\:false
+PA15.GPIOParameters=GPIO_PuPd
+PA15.GPIO_PuPd=GPIO_PULLDOWN
+PA15.Locked=true
+PA15.Signal=GPIO_Output
+PA7.GPIOParameters=GPIO_PuPd
+PA7.GPIO_PuPd=GPIO_PULLUP
+PA7.Locked=true
+PA7.Signal=SPI1_MOSI
+PB3.GPIOParameters=GPIO_PuPd
+PB3.GPIO_PuPd=GPIO_PULLUP
+PB3.Locked=true
+PB3.Signal=SPI1_SCK
+PB4.GPIOParameters=GPIO_PuPd
+PB4.GPIO_PuPd=GPIO_PULLUP
+PB4.Locked=true
+PB4.Signal=SPI1_MISO
+PB7.GPIOParameters=GPIO_Pu
+PB7.GPIO_Pu=GPIO_NOPULL
+PB7.Locked=true
+PB7.Mode=I2C
+PB7.Signal=I2C1_SDA
+PB8.GPIOParameters=GPIO_Pu
+PB8.GPIO_Pu=GPIO_NOPULL
+PB8.Mode=I2C
+PB8.Signal=I2C1_SCL
+PC12.Mode=Asynchronous
+PC12.Signal=UART5_TX
+PCC.Checker=false
+PCC.Line=STM32F469/479
+PCC.MCU=STM32F469NIHx
+PCC.PartNumber=STM32F469NIHx
+PCC.Series=STM32F4
+PCC.Temperature=25
+PCC.Vdd=3.3
+PD2.Mode=Asynchronous
+PD2.Signal=UART5_RX
+PH0/OSC_IN.Mode=HSE-External-Oscillator
+PH0/OSC_IN.Signal=RCC_OSC_IN
+PH1/OSC_OUT.Mode=HSE-External-Oscillator
+PH1/OSC_OUT.Signal=RCC_OSC_OUT
+PinOutPanel.CurrentBGAView=Top
+PinOutPanel.RotationAngle=0
+ProjectManager.AskForMigrate=true
+ProjectManager.BackupPrevious=false
+ProjectManager.CompilerLinker=GCC
+ProjectManager.CompilerOptimize=6
+ProjectManager.ComputerToolchain=false
+ProjectManager.CoupleFile=false
+ProjectManager.CustomerFirmwarePackage=
+ProjectManager.DefaultFWLocation=true
+ProjectManager.DeletePrevious=true
+ProjectManager.DeviceId=STM32F469NIHx
+ProjectManager.FirmwarePackage=STM32Cube FW_F4 V1.28.3
+ProjectManager.FreePins=false
+ProjectManager.FreePinsContext=
+ProjectManager.HalAssertFull=false
+ProjectManager.HeapSize=0x200
+ProjectManager.KeepUserCode=false
+ProjectManager.LastFirmware=true
+ProjectManager.LibraryCopy=1
+ProjectManager.MainLocation=Core/Src
+ProjectManager.NoMain=false
+ProjectManager.PreviousToolchain=STM32CubeIDE
+ProjectManager.ProjectBuild=false
+ProjectManager.ProjectFileName=sdp-k1.ioc
+ProjectManager.ProjectName=sdp-k1
+ProjectManager.ProjectStructure=
+ProjectManager.RegisterCallBack=I2C,UART
+ProjectManager.StackSize=0x400
+ProjectManager.TargetToolchain=STM32CubeIDE
+ProjectManager.ToolChainLocation=
+ProjectManager.UAScriptAfterPath=
+ProjectManager.UAScriptBeforePath=
+ProjectManager.UnderRoot=true
+ProjectManager.functionlistsort=1-MX_GPIO_Init-GPIO-false-HAL-false,2-SystemClock_Config-RCC-false-HAL-false,3-MX_UART5_Init-UART5-false-HAL-false,4-MX_I2C1_Init-I2C1-false-HAL-false
+RCC.AHBFreq_Value=180000000
+RCC.APB1CLKDivider=RCC_HCLK_DIV4
+RCC.APB1Freq_Value=45000000
+RCC.APB1TimFreq_Value=90000000
+RCC.APB2CLKDivider=RCC_HCLK_DIV2
+RCC.APB2Freq_Value=90000000
+RCC.APB2TimFreq_Value=180000000
+RCC.CortexFreq_Value=180000000
+RCC.DSIFreq_Value=20000000
+RCC.DSITXEscFreq_Value=5000000
+RCC.EthernetFreq_Value=180000000
+RCC.FCLKCortexFreq_Value=180000000
+RCC.FamilyName=M
+RCC.HCLKFreq_Value=180000000
+RCC.HSE_VALUE=8000000
+RCC.I2SFreq_Value=192000000
+RCC.IPParameters=AHBFreq_Value,APB1CLKDivider,APB1Freq_Value,APB1TimFreq_Value,APB2CLKDivider,APB2Freq_Value,APB2TimFreq_Value,CortexFreq_Value,DSIFreq_Value,DSITXEscFreq_Value,EthernetFreq_Value,FCLKCortexFreq_Value,FamilyName,HCLKFreq_Value,HSE_VALUE,I2SFreq_Value,LCDTFTFreq_Value,MCO2PinFreq_Value,PLLCLKFreq_Value,PLLDSIFreq_Value,PLLDSIVCOFreq_Value,PLLI2SQCLKFreq_Value,PLLI2SRCLKFreq_Value,PLLM,PLLN,PLLQCLKFreq_Value,PLLRCLKFreq_Value,PLLRFreq_Value,PLLSAIPCLKFreq_Value,PLLSAIQCLKFreq_Value,PLLSAIRCLKFreq_Value,RTCFreq_Value,RTCHSEDivFreq_Value,SAIAFreq_Value,SAIBFreq_Value,SDIOFreq_Value,SYSCLKFreq_VALUE,SYSCLKSource,USBFreq_Value,VCOI2SOutputFreq_Value,VCOInputFreq_Value,VCOOutputFreq_Value,VCOSAIOutputFreq_Value
+RCC.LCDTFTFreq_Value=96000000
+RCC.MCO2PinFreq_Value=180000000
+RCC.PLLCLKFreq_Value=180000000
+RCC.PLLDSIFreq_Value=160000000
+RCC.PLLDSIVCOFreq_Value=320000000
+RCC.PLLI2SQCLKFreq_Value=96000000
+RCC.PLLI2SRCLKFreq_Value=192000000
+RCC.PLLM=8
+RCC.PLLN=180
+RCC.PLLQCLKFreq_Value=90000000
+RCC.PLLRCLKFreq_Value=180000000
+RCC.PLLRFreq_Value=180000000
+RCC.PLLSAIPCLKFreq_Value=192000000
+RCC.PLLSAIQCLKFreq_Value=96000000
+RCC.PLLSAIRCLKFreq_Value=192000000
+RCC.RTCFreq_Value=32000
+RCC.RTCHSEDivFreq_Value=4000000
+RCC.SAIAFreq_Value=96000000
+RCC.SAIBFreq_Value=96000000
+RCC.SDIOFreq_Value=90000000
+RCC.SYSCLKFreq_VALUE=180000000
+RCC.SYSCLKSource=RCC_SYSCLKSOURCE_PLLCLK
+RCC.USBFreq_Value=90000000
+RCC.VCOI2SOutputFreq_Value=384000000
+RCC.VCOInputFreq_Value=2000000
+RCC.VCOOutputFreq_Value=360000000
+RCC.VCOSAIOutputFreq_Value=384000000
+UART5.BaudRate=230400
+UART5.IPParameters=VirtualMode,BaudRate
+UART5.VirtualMode=Asynchronous
+VP_SYS_VS_Systick.Mode=SysTick
+VP_SYS_VS_Systick.Signal=SYS_VS_Systick
+board=custom

projects/ltm3360b/src/common/common_data.c
@@ -1,0 +1,63 @@
+/***************************************************************************//**
+ *   @file   common_data.c
+ *   @brief  Defines common data to be used by LTM3360B example.
+ *   @author Janani Sunil (janani.sunil@analog.com)
+********************************************************************************
+ * Copyright 2026(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include "common_data.h"
+#include "no_os_uart.h"
+#include "no_os_i2c.h"
+#include "ltm3360b.h"
+
+struct no_os_uart_init_param ltm3360b_uart_ip = {
+	.device_id = UART_DEVICE_ID,
+	.irq_id = UART_IRQ_ID,
+	.asynchronous_rx = true,
+	.baud_rate = UART_BAUDRATE,
+	.size = NO_OS_UART_CS_8,
+	.parity = NO_OS_UART_PAR_NO,
+	.stop = NO_OS_UART_STOP_1_BIT,
+	.extra = UART_EXTRA,
+	.platform_ops = UART_OPS,
+};
+
+struct no_os_i2c_init_param ltm3360b_i2c_ip = {
+	.device_id = I2C_DEVICE_ID,
+	.max_speed_hz = I2C_BAUDRATE,
+	.slave_address = LTM3360B_I2C_ADDR,
+	.platform_ops = I2C_OPS,
+};
+
+struct ltm3360b_init_param ltm3360b_ip = {
+	.i2c_init = &ltm3360b_i2c_ip,
+	.auto_enable = true,
+	.imon_threshold = LTM3360B_IMON_TH_79_PERCENT,
+	.vout_mv = 1000
+};

projects/ltm3360b/src/common/common_data.h
@@ -1,0 +1,45 @@
+/***************************************************************************//**
+ *   @file   ltm3360b/src/common/common_data.h
+ *   @brief  Defines common data to be used by LTM3360B examples.
+ *   @author Janani Sunil (janani.sunil@analog.com)
+********************************************************************************
+ * Copyright 2026(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef __COMMON_DATA_H__
+#define __COMMON_DATA_H__
+
+#include "parameters.h"
+#include "no_os_uart.h"
+#include "no_os_i2c.h"
+#include "ltm3360b.h"
+
+extern struct no_os_uart_init_param ltm3360b_uart_ip;
+extern struct no_os_i2c_init_param ltm3360b_i2c_ip;
+extern struct ltm3360b_init_param ltm3360b_ip;
+
+#endif /* __COMMON_DATA_H__ */

projects/ltm3360b/src/examples/basic/basic_example.c
@@ -1,0 +1,103 @@
+/***************************************************************************//**
+ *   @file   basic_example.c
+ *   @brief  Basic example for LTM3360B project
+ *   @author Janani Sunil (janani.sunil@analog.com)
+********************************************************************************
+ * Copyright 2026(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include "no_os_print_log.h"
+#include "no_os_uart.h"
+#include "no_os_error.h"
+#include "common_data.h"
+#include "ltm3360b.h"
+
+/***************************************************************************//**
+ * @brief Basic example main execution.
+ * @return 0 in case of success, negative error code otherwise
+*******************************************************************************/
+int example_main()
+{
+	struct ltm3360b_dev *ltm3360b_dev;
+	struct no_os_uart_desc *uart_desc;
+	uint8_t adc_readback;
+	int ret;
+
+	ret = no_os_uart_init(&uart_desc, &ltm3360b_uart_ip);
+	if (ret) {
+		return ret;
+	}
+
+	pr_info("***** LTM3360B Basic Example *****\n");
+
+	ret = ltm3360b_init(&ltm3360b_dev, &ltm3360b_ip);
+	if (ret) {
+		pr_err("Failed to initialize LTM3360B: %d\n", ret);
+		goto error_uart;
+	}
+
+	ret = ltm3360b_adc_enable(ltm3360b_dev, true);
+	if (ret) {
+		pr_err("Failed to enable ADC: %d\n", ret);
+		goto error_device;
+	}
+
+	ret = ltm3360b_adc_read_raw(ltm3360b_dev, LTM3360B_ADC_VOUT_P, &adc_readback);
+	if (ret) {
+		pr_err("Failed to read VOUT_P ADC: %d\n", ret);
+		goto error_device;
+	}
+	pr_info("VOUT_P ADC Raw Reading: %d\n", adc_readback);
+
+	ret = ltm3360b_adc_read_raw(ltm3360b_dev, LTM3360B_ADC_VOUT_N, &adc_readback);
+	if (ret) {
+		pr_err("Failed to read VOUT_N ADC: %d\n", ret);
+		goto error_device;
+	}
+	pr_info("VOUT_N ADC Raw Reading: %d\n", adc_readback);
+
+	ret = ltm3360b_adc_read_raw(ltm3360b_dev, LTM3360B_ADC_VIN, &adc_readback);
+	if (ret) {
+		pr_err("Failed to read VIN ADC: %d\n", ret);
+		goto error_device;
+	}
+	pr_info("VIN ADC Raw Reading: %d\n", adc_readback);
+
+	ltm3360b_remove(ltm3360b_dev);
+	no_os_uart_remove(uart_desc);
+
+	return 0;
+
+error_device:
+	ltm3360b_remove(ltm3360b_dev);
+
+error_uart:
+	pr_err("LTM3360B example failed with error: %d\n", ret);
+	no_os_uart_remove(uart_desc);
+	return ret;
+}

projects/ltm3360b/src/platform/stm32/main.c
@@ -1,0 +1,44 @@
+/***************************************************************************//**
+ *   @file   main.c
+ *   @brief  Main file for STM32 platform of LTM3360B project.
+ *   @author Janani Sunil (janani.sunil@analog.com)
+********************************************************************************
+ * Copyright 2026(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#include "common_data.h"
+#include "no_os_error.h"
+#include "no_os_print_log.h"
+
+extern int example_main();
+extern int stm32_init();
+
+int main()
+{
+	stm32_init();
+	return example_main();
+}

projects/ltm3360b/src/platform/stm32/parameters.c
@@ -1,0 +1,43 @@
+/***************************************************************************//**
+ *   @file   parameters.c
+ *   @brief  Definition of STM32 platform data used by LTM3360B project.
+ *   @author Janani Sunil (janani.sunil@analog.com)
+********************************************************************************
+ * Copyright 2026(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include "parameters.h"
+#include "stm32_uart.h"
+#include "stm32_i2c.h"
+
+/**
+ * @brief STM32 UART extra initialization parameters for console communication
+ */
+struct stm32_uart_init_param ltm3360b_uart_extra_ip = {
+	.huart = &huart5,
+};

commit 5aed7a7214c6505f17d9d84a0290e1fb45c820fe

projects: ltm3360b: Bug fix in src.mk
Remove util/no_os_delay.c in the src.mk

Fixes: 347b8d8: "projects: ltm3360b: Add example project for LTM3360B"

Signed-off-by: Janani Sunil <janani.sunil@analog.com>

projects/ltm3360b/src.mk
@@ -5,7 +5,6 @@ SRCS += $(DRIVERS)/api/no_os_gpio.c \
 	$(NO-OS)/util/no_os_list.c \
 	$(NO-OS)/util/no_os_util.c \
 	$(NO-OS)/util/no_os_alloc.c \
-	$(NO-OS)/util/no_os_delay.c \
 	$(NO-OS)/util/no_os_lf256fifo.c \
 	$(NO-OS)/util/no_os_mutex.c \
 	$(NO-OS)/util/no_os_crc8.c