analogdevicesinc/no-OS

commit f3f0d00670c7ed3d4a9d0db4db7b0dadb6057303

adc-dac: ad74416h: add support for ad74414h
Add support for ad74414h part within the ad74416h driver.
The main difference between the two parts is that the ADC result is
stored into a single register. Also some configurations are not
supported on ad74414h.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc-dac/ad74416h/ad74416h.c
@@ -271,16 +271,22 @@ int ad74416h_get_raw_adc_result(struct ad74416h_desc *desc, uint32_t ch,
 	uint32_t val_msb, val_lsb;
 	int ret;
 
-	ret = ad74416h_reg_read(desc, AD74416H_ADC_RESULT_UPR(ch), &val_msb);
-	if (ret)
-		return ret;
+	if (desc->id == ID_AD74416H) {
+		ret = ad74416h_reg_read(desc, AD74416H_ADC_RESULT_UPR(ch),
+					&val_msb);
+		if (ret)
+			return ret;
+	}
 
 	ret = ad74416h_reg_read(desc, AD74416H_ADC_RESULT(ch), &val_lsb);
 	if (ret)
 		return ret;
 
-	*val = (no_os_field_get(AD74416H_CONV_RES_UPR_MSK, val_msb) << 16) |
-	       no_os_field_get(AD74416H_CONV_RESULT_MSK, val_lsb);
+	if (desc->id == ID_AD74416H)
+		*val = (no_os_field_get(AD74416H_CONV_RES_UPR_MSK, val_msb) << 16) |
+		       no_os_field_get(AD74416H_CONV_RESULT_MSK, val_lsb);
+	else
+		*val = no_os_field_get(AD74416H_CONV_RESULT_MSK, val_lsb);
 
 	return 0;
 }
@@ -352,6 +358,18 @@ int ad74416h_get_adc_range(struct ad74416h_desc *desc, uint32_t ch,
 int ad74416h_set_adc_range(struct ad74416h_desc *desc, uint32_t ch,
 			   enum ad74416h_adc_range val)
 {
+	if (desc->id == ID_AD74414H) {
+		switch(val) {
+		case AD74416H_RNG_NEG12_12_V:
+		case AD74416H_RNG_0_0P625V:
+		case AD74416H_RNG_NEG104_104MV:
+		case AD74416H_RNG_NEG2P5_2P5V:
+			return -EINVAL;
+		default:
+			break;
+		}
+	}
+
 	return ad74416h_reg_update(desc, AD74416H_ADC_CONFIG(ch),
 				   AD74416H_ADC_CONV_RANGE_MSK, val);
 }
@@ -422,6 +440,16 @@ int ad74416h_get_adc_conv_mux(struct ad74416h_desc *desc, uint32_t ch,
 int ad74416h_set_adc_conv_mux(struct ad74416h_desc *desc, uint32_t ch,
 			      enum ad74416h_adc_conv_mux val)
 {
+	if (desc->id == ID_AD74414H) {
+		switch(val) {
+		case AD74416H_MUX_VSENSEN_TO_AGND:
+		case AD74416H_MUX_LF_TO_VSENSEN:
+			return -EINVAL;
+		default:
+			break;
+		}
+	}
+
 	return ad74416h_reg_update(desc, AD74416H_ADC_CONFIG(ch),
 				   AD74416H_CONV_MUX_MSK, val);
 }
@@ -531,6 +559,17 @@ int ad74416h_set_channel_function(struct ad74416h_desc *desc,
 	int ret;
 	uint16_t dac_code;
 
+	if (desc->id == ID_AD74414H) {
+		switch(ch_func) {
+		case AD74416H_VOLTAGE_OUT:
+		case AD74416H_VOLTAGE_IN:
+		case AD74416H_RESISTANCE:
+			return -EINVAL;
+		default:
+			break;
+		}
+	}
+
 	ret = ad74416h_reg_update(desc, AD74416H_CH_FUNC_SETUP(ch),
 				  AD74416H_CH_FUNC_SETUP_MSK, AD74416H_HIGH_Z);
 	if (ret)
@@ -628,6 +667,16 @@ int ad74416h_set_channel_dac_code(struct ad74416h_desc *desc, uint32_t ch,
 int ad74416h_set_diag(struct ad74416h_desc *desc, uint32_t ch,
 		      enum ad74416h_diag_mode diag_code)
 {
+	if (desc->id == ID_AD74414H) {
+		switch(diag_code) {
+		case AD74416H_DIAG_LVIN:
+		case AD74416H_VSENSEN_C:
+			return -EINVAL;
+		default:
+			break;
+		}
+	}
+
 	return ad74416h_reg_update(desc, AD74416H_DIAG_ASSIGN,
 				   AD74416H_DIAG_ASSIGN_MSK(ch), diag_code);
 }
@@ -923,6 +972,7 @@ int ad74416h_init(struct ad74416h_desc **desc,
 	if (ret)
 		goto err;
 
+	descriptor->id = init_param->id;
 	descriptor->dev_addr = init_param->dev_addr;
 
 	no_os_crc8_populate_msb(_crc_table, AD74416H_CRC_POLYNOMIAL);

drivers/adc-dac/ad74416h/ad74416h.h
@@ -432,6 +432,14 @@
 /******************************************************************************/
 
 /**
+ * @brief Supported device ids.
+*/
+enum ad74416h_dev_id {
+	ID_AD74416H,
+	ID_AD74414H,
+};
+
+/**
  * @brief Operation modes of the device.
  */
 enum ad74416h_op_mode {
@@ -603,6 +611,7 @@ union ad74416h_live_status {
 };
 
 struct ad74416h_init_param {
+	enum ad74416h_dev_id id;
 	uint8_t dev_addr;
 	struct no_os_spi_init_param spi_ip;
 	struct no_os_gpio_init_param *reset_gpio_param;
@@ -623,6 +632,7 @@ struct ad74416h_channel_config {
  * @brief AD74416h device descriptor.
  */
 struct ad74416h_desc {
+	enum ad74416h_dev_id id;
 	uint8_t dev_addr;
 	struct no_os_spi_desc *spi_desc;
 	uint8_t comm_buff[AD74416H_FRAME_SIZE];

commit 43c692dc806e953fed9acb803cee447b5b3a860d

drivers: adc-dac: ad74416h: remove incorrect AD74414H conv mux gate
Remove the incorrect AD74414H gate that blocked CONV_MUX values
VSENSEN_TO_AGND (010) and LF_TO_VSENSEN (011). Per the AD74414H
datasheet, all five CONV_MUX options are supported on both devices.

Fixes: f3f0d0067 ("adc-dac: ad74416h: add support for ad74414h")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc-dac/ad74416h/ad74416h.c
@@ -447,16 +447,6 @@ int ad74416h_get_adc_conv_mux(struct ad74416h_desc *desc, uint32_t ch,
 int ad74416h_set_adc_conv_mux(struct ad74416h_desc *desc, uint32_t ch,
 			      enum ad74416h_adc_conv_mux val)
 {
-	if (desc->id == ID_AD74414H) {
-		switch (val) {
-		case AD74416H_MUX_VSENSEN_TO_AGND:
-		case AD74416H_MUX_LF_TO_VSENSEN:
-			return -EINVAL;
-		default:
-			break;
-		}
-	}
-
 	return ad74416h_reg_update(desc, AD74416H_ADC_CONFIG(ch),
 				   AD74416H_CONV_MUX_MSK, val);
 }