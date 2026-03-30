analogdevicesinc/no-OS

commit 7625d894c66ddc6a5f5e66d33e60a9dd23f96ee8

adc-dac: ad74416h: update regmap for silicon v2
Miscellaneous updates on the register map of ad74416h silicon version 2.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc-dac/ad74416h/ad74416h.h
@@ -144,6 +144,8 @@
 #define AD74416H_ALARM_DEG_PERIOD_MSK		NO_OS_BIT(12)
 #define AD74VOUT_4W_EN_MSK			NO_OS_BIT(11)
 #define AD74416H_WAIT_LDAC_CMD_MSK		NO_OS_BIT(10)
+#define AD74416H_VIOUT_DRV_EN_DLY		NO_OS_BIT(9)
+#define AD74416H_HART_COMPL_SETTLED		NO_OS_BIT(8)
 #define AD74416H_VOUT_RANGE_MSK			NO_OS_BIT(7)
 #define AD74416H_SLEW_EN_MSK			NO_OS_GENMASK(6, 5)
 #define AD74416H_SLEW_LIN_STEP_MSK		NO_OS_GENMASK(4, 3)
@@ -289,6 +291,7 @@
 #define AD74416H_CAL_MEM_ERR_MSK		NO_OS_BIT(0)
 
 /* AD74416H CHANNEL_ALERT_STATUS Register */
+#define AD74416H_VIOUT_SHUTDOWN_MSK		NO_OS_BIT(6)
 #define AD74416H_ANALOG_IO_OC_MSK		NO_OS_BIT(5)
 #define AD74416H_ANALOG_IO_SC_MSK		NO_OS_BIT(4)
 #define AD74416H_DO_TIMEOUT_MSK			NO_OS_BIT(3)
@@ -354,8 +357,8 @@
 #define AD74416H_TDR_MSK			NO_OS_GENMASK(7, 0)
 
 /* AD74416H HART_FCRn Register */
-#define AD74416H_TFTRIG_MSK			NO_OS_GENMASK(11, 8)
-#define AD74416H_RFTRIG_MSK			NO_OS_GENMASK(6, 3)
+#define AD74416H_TFTRIG_MSK			NO_OS_GENMASK(12, 8)
+#define AD74416H_RFTRIG_MSK			NO_OS_GENMASK(7, 3)
 #define AD74416H_TFCLR_MSK			NO_OS_BIT(2)
 #define AD74416H_RFCLR_MSK			NO_OS_BIT(1)
 #define AD74416H_FIFOEN_MSK			NO_OS_BIT(0)
@@ -364,10 +367,10 @@
 #define AD74416H_RTS_MSK			NO_OS_BIT(0)
 
 /* AD74416H HART_RFCn Register */
-#define AD74416H_RFC_MSK			NO_OS_GENMASK(4, 0)
+#define AD74416H_RFC_MSK			NO_OS_GENMASK(5, 0)
 
 /* AD74416H HART_TFCn Register */
-#define AD74416H_TFC_MSK			NO_OS_GENMASK(4, 0)
+#define AD74416H_TFC_MSK			NO_OS_GENMASK(5, 0)
 
 /* AD74416H HART_CONFIGn Register */
 #define AD74416H_CD_EXTD_QUAL_MSK		NO_OS_BIT(13)
@@ -434,9 +437,9 @@ enum ad74416h_op_mode {
 	AD74416H_RESISTANCE = 0x7,
 	AD74416H_DIGITAL_INPUT = 0x8,
 	AD74416H_DIGITAL_INPUT_LOOP = 0x9,
-	AD74416H_CURRENT_OUT_HART = 0xA,
-	AD74416H_CURRENT_IN_EXT_HART = 0xB,
-	AD74416H_CURRENT_IN_LOOP_HART = 0xC,
+	AD74416H_CURRENT_OUT_HART = 0x10,
+	AD74416H_CURRENT_IN_EXT_HART = 0x11,
+	AD74416H_CURRENT_IN_LOOP_HART = 0x12,
 };
 
 /**

commit 23b9d35891364a537f36944253e062e62eb4dba3

drivers: adc-dac: ad74416h: fix HART mode channel function values
The CH_FUNC_SETUP register field is bits [3:0] (4 bits wide), so the
maximum valid value is 0xF. The HART mode encodings were incorrectly
set to 0x10/0x11/0x12 which overflow this 4-bit field. The correct
values per the datasheet are 0xA/0xB/0xC.

Fixes: 7625d894c ("adc-dac: ad74416h: update regmap for silicon v2")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc-dac/ad74416h/ad74416h.h
@@ -437,9 +437,9 @@ enum ad74416h_op_mode {
 	AD74416H_RESISTANCE = 0x7,
 	AD74416H_DIGITAL_INPUT = 0x8,
 	AD74416H_DIGITAL_INPUT_LOOP = 0x9,
-	AD74416H_CURRENT_OUT_HART = 0x10,
-	AD74416H_CURRENT_IN_EXT_HART = 0x11,
-	AD74416H_CURRENT_IN_LOOP_HART = 0x12,
+	AD74416H_CURRENT_OUT_HART = 0xA,
+	AD74416H_CURRENT_IN_EXT_HART = 0xB,
+	AD74416H_CURRENT_IN_LOOP_HART = 0xC,
 };
 
 /**