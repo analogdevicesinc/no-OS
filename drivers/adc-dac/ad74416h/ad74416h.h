analogdevicesinc/no-OS

commit 554b344fb63db7e322adf939290fbe99c6908bdb

ad74416: update header masks and enums
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc-dac/ad74416h/ad74416h.h
@@ -133,7 +133,7 @@
 #define AD74416H_CH_FUNC_SETUP_MSK		NO_OS_GENMASK(3, 0)
 
 /* AD74416H_ADC_CONFIG */
-#define AD74416H_ADC_CONV_RATE_MSK		NO_OS_GENMASK(10, 8)
+#define AD74416H_ADC_CONV_RATE_MSK		NO_OS_GENMASK(11, 8)
 #define AD74416H_ADC_CONV_RANGE_MSK		NO_OS_GENMASK(6, 4)
 #define AD74416H_CONV_MUX_MSK			NO_OS_GENMASK(2, 0)
 
@@ -195,8 +195,8 @@
 #define AD74416H_DAC_ACTIVE_MSK			NO_OS_GENMASK(15, 0)
 
 /** ADC_CONV_CTRL Register */
-#define AD74416H_ADC_RDY_CTRL_MSK		NO_OS_BIT(12)
-#define AD74416H_CONV_RATE_DIAG_MSK		NO_OS_GENMASK(11, 10)
+#define AD74416H_ADC_RDY_CTRL_MSK		NO_OS_BIT(13)
+#define AD74416H_CONV_RATE_DIAG_MSK		NO_OS_GENMASK(12, 10)
 #define AD74416H_CONV_SEQ_MSK			NO_OS_GENMASK(9, 8)
 #define AD74416H_DIAG_EN_MSK(x)			(NO_OS_BIT(x) << 4)
 #define AD74416H_CH_EN_MSK(x)			NO_OS_BIT(x)
@@ -250,7 +250,6 @@
 #define AD74416H_TEMP_ALERT_MSK			NO_OS_BIT(4)
 #define AD74416H_SPI_ERR_MSK			NO_OS_BIT(3)
 #define AD74416H_SUPPLY_ERR_MSK			NO_OS_BIT(2)
-#define AD74416H_WDT_RESET_OCCURRED_MSK		NO_OS_BIT(1)
 #define AD74416H_RESET_OCCURRED_MSK		NO_OS_BIT(0)
 
 /** LIVE_STATUS Register */
@@ -490,13 +489,27 @@ enum ad74416h_adc_range {
  * @brief Conversion configuration values.
  */
 enum ad74416h_adc_rate {
-	AD74416H_10SPS_50_60HZ_HART_REJECTION,
-	AD74416H_20SPS_50_60HZ_REJECTION,
-	AD74416H_1K2SPS,
-	AD74416H_4K8SPS,
-	AD74416H_9K6SPS,
-	AD74416H_19K2SPS,
-	AD74416H_200SPS_HART_REJECTION,
+	AD74416H_10SPS_50_60HZ_HART_REJECTION = 0,
+	AD74416H_20SPS_50_60HZ_REJECTION = 1,
+	AD74416H_20SPS_50_60HZ_HART_REJECTION = 2,
+	AD74416H_200SPS_HART_REJECTION = 4,
+	AD74416H_200SPS_SIGN_HART_REJECTION = 6,
+	AD74416H_1K2SPS = 8,
+	AD74416H_1K2SPS_HART_REJECTION = 9,
+	AD74416H_4K8SPS = 12,
+	AD74416H_9K6SPS = 13,
+};
+
+/**
+ * @brief Diagnostics conversion configuration values.
+ */
+enum ad74416h_diag_rate {
+	AD74416H_DIAG_20SPS_50_60HZ_REJECTION,
+	AD74416H_DIAG_20SPS_50_60HZ_HART_REJECTION,
+	AD74416H_DIAG_1K2SPS_HART_REJECTION,
+	AD74416H_DIAG_4K8SPS,
+	AD74416H_DIAG_9K6SPS,
+	AD74416H_DIAG_19K2SPS,
 };
 
 /**

commit 7bb911a1e44ce9a99375963f87fd56e396a86407

drivers: adc-dac: ad74416h: fix 20SPS HART rejection ADC rate value
The 20SPS with 50/60Hz and HART rejection ADC conversion rate was set
to 0b0010 (2), which is an unspecified value for the CONV_RATE field
bits [11:8]. The correct value per the datasheet is 0b0011 (3).

Fixes: 554b344fb ("ad74416: update header masks and enums")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc-dac/ad74416h/ad74416h.h
@@ -473,7 +473,7 @@ enum ad74416h_adc_range {
 enum ad74416h_adc_rate {
 	AD74416H_10SPS_50_60HZ_HART_REJECTION = 0,
 	AD74416H_20SPS_50_60HZ_REJECTION = 1,
-	AD74416H_20SPS_50_60HZ_HART_REJECTION = 2,
+	AD74416H_20SPS_50_60HZ_HART_REJECTION = 3,
 	AD74416H_200SPS_HART_REJECTION = 4,
 	AD74416H_200SPS_SIGN_HART_REJECTION = 6,
 	AD74416H_1K2SPS = 8,