analogdevicesinc/no-OS

commit 0f181cb26f6c22404153d8ccde33b1b863e14343

drivers: ad405x: fix GPIO leak in error paths
Add proper cleanup of previously allocated GPIOs when
initialization fails partway through ad405x_init_gpios().

Also use consistent error checking with 'if (ret)' instead
of 'if (ret < 0)' throughout the function.

Fixes: de14f9a27 ("drivers:adc: Add ad405x no-OS drivers")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad405x/ad405x.c
@@ -1413,37 +1413,45 @@ static int ad405x_init_gpios(struct ad405x_dev *dev,
 	if (dev->gpio_gpio0) {
 		ret = no_os_gpio_direction_input(dev->gpio_gpio0);
 		if (ret)
-			return ret;
+			goto err_gpio0;
 	}
 
 	/* GPIO1 */
 	ret = no_os_gpio_get_optional(&dev->gpio_gpio1, init_param->gpio_gpio1);
-	if (ret < 0)
-		return ret;
+	if (ret)
+		goto err_gpio0;
 
 	if (dev->gpio_gpio1) {
 		ret = no_os_gpio_direction_input(dev->gpio_gpio1);
 		if (ret)
-			return ret;
+			goto err_gpio1;
 	}
 
 	/* Only SPI has CNV pin */
 	if (dev->comm_type != AD405X_SPI_COMM)
-		return ret;
+		return 0;
 
 	/* CNV */
 	ret = no_os_gpio_get_optional(&dev->extra.spi_extra.gpio_cnv,
 				      init_param->gpio_cnv);
-	if (ret < 0)
-		return ret;
+	if (ret)
+		goto err_gpio1;
 
 	if (dev->extra.spi_extra.gpio_cnv) {
 		ret = no_os_gpio_direction_output(dev->extra.spi_extra.gpio_cnv,
 						  NO_OS_GPIO_LOW);
 		if (ret)
-			return ret;
+			goto err_cnv;
 	}
 
+	return 0;
+
+err_cnv:
+	no_os_gpio_remove(dev->extra.spi_extra.gpio_cnv);
+err_gpio1:
+	no_os_gpio_remove(dev->gpio_gpio1);
+err_gpio0:
+	no_os_gpio_remove(dev->gpio_gpio0);
 	return ret;
 }
 

commit 74576e816f79af1fe5326ec056d7ff7092af7b95

drivers: adc: ad405x: fix GPIO leak in ad405x_init error paths
When ad405x_init_gpios() succeeds but the subsequent SPI/I3C init
fails, error_dev only calls no_os_free() without removing the GPIOs.
Similarly, error_com only removes the comm interface but not the
GPIOs. Add an error_gpio label between them to properly release
gpio_gpio0, gpio_gpio1 and gpio_cnv on failure.

Fixes: 0f181cb26f6c ("drivers: ad405x: fix GPIO leak in error paths")
Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

drivers/adc/ad405x/ad405x.c
@@ -117,7 +117,7 @@ int ad405x_init(struct ad405x_dev **device,
 				     init_param.comm_init.i3c_init);
 	}
 	if (ret)
-		goto error_dev;
+		goto error_gpio;
 
 	dev->dev_type = init_param.dev_type;
 
@@ -168,6 +168,11 @@ error_com:
 		no_os_spi_remove(dev->com_desc.spi_desc);
 	else
 		no_os_i3c_remove(dev->com_desc.i3c_desc);
+error_gpio:
+	no_os_gpio_remove(dev->gpio_gpio0);
+	no_os_gpio_remove(dev->gpio_gpio1);
+	if (dev->comm_type == AD405X_SPI_COMM)
+		no_os_gpio_remove(dev->extra.spi_extra.gpio_cnv);
 error_dev:
 	no_os_free(dev);
 	return ret;