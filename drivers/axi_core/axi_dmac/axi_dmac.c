analogdevicesinc/no-OS

commit bd9f1982b799243306f4989515c3db5dd5d6ce26

drivers: axi_dmac.c: Force DMA buffer alignment
Software must program the SRC_ADDRESS and DEST_ADDRESS registers
to be multiple of the corresponding MM data bus.

The following conditions must hold:

    SRC_ADDRESS MOD (DMA_DATA_WIDTH_SRC/8) == 0
    DEST_ADDRESS MOD (DMA_DATA_WIDTH_DEST/8) == 0

Signed-off-by: George Mois <george.mois@analog.com>

drivers/axi_core/axi_dmac/axi_dmac.c
@@ -453,11 +453,19 @@ int32_t axi_dmac_transfer_start(struct axi_dmac *dmac,
 			dmac->init_addr = dmac->next_dest_addr;
 			axi_dmac_write(dmac, AXI_DMAC_REG_DEST_ADDRESS, dmac->next_dest_addr);
 			axi_dmac_write(dmac, AXI_DMAC_REG_DEST_STRIDE, 0x0);
+			if (dmac->transfer.dest_addr % (dmac->width_dst / 8)) {
+				printf("Destination address should be aligned with destination data path width.\n\n");
+				return -1;
+			}
 			break;
 		case DMA_MEM_TO_DEV:
 			dmac->init_addr = dmac->next_src_addr;
 			axi_dmac_write(dmac, AXI_DMAC_REG_SRC_ADDRESS, dmac->next_src_addr);
 			axi_dmac_write(dmac, AXI_DMAC_REG_SRC_STRIDE, 0x0);
+			if (dmac->transfer.src_addr % (dmac->width_src / 8)) {
+				printf("Source address should be aligned with source data path width.\n");
+				return -1;
+			}
 			break;
 		case DMA_MEM_TO_MEM:
 			dmac->init_addr = dmac->next_src_addr;
@@ -465,6 +473,11 @@ int32_t axi_dmac_transfer_start(struct axi_dmac *dmac,
 			axi_dmac_write(dmac, AXI_DMAC_REG_DEST_STRIDE, 0x0);
 			axi_dmac_write(dmac, AXI_DMAC_REG_SRC_ADDRESS, dmac->next_src_addr);
 			axi_dmac_write(dmac, AXI_DMAC_REG_SRC_STRIDE, 0x0);
+			if ((dmac->transfer.dest_addr % (dmac->width_dst / 8))
+			    || (dmac->transfer.src_addr % (dmac->width_src / 8))) {
+				printf("Source and destination addresses should be aligned with data path widths.\n");
+				return -1;
+			}
 			break;
 		default:
 			return -1; /* Other directions are not supported yet. */

commit 7eac89afeeeefd389809aa16356ce7ada0c62673

drivers : axi_core : axi_dmac : Fix axi_dmac driver addressing issue
Fixed addressing issue in the if else statements in axi_dmac_transfer_start().
In the old version, in the conditions for verifying if the alignment is
right, in axi_dmac_transfer_start(), there was a divide by 8, which should transform the width in
bytes. But the width is already in bytes, at that point, because the transformation in bytes was
already done in axi_dmac_detect_caps(), specifically
dmac->width_dst =(1 << dmac->width_dst) and here dmac->width_src = (1 << dmac->width_src).

Fixes: bd9f1982b799243306f4989515c3db5dd5d6ce26 : ("drivers: axi_dmac.c:Force DMA buffer alignment")

Signed-off-by: Raul Georgian <Raul.Georgian@analog.com>

drivers/axi_core/axi_dmac/axi_dmac.c
@@ -459,8 +459,8 @@ int32_t axi_dmac_transfer_start(struct axi_dmac *dmac,
 			axi_dmac_write(dmac, AXI_DMAC_REG_DEST_STRIDE, 0x0);
 			axi_dmac_write(dmac, AXI_DMAC_REG_SRC_ADDRESS, dmac->next_src_addr);
 			axi_dmac_write(dmac, AXI_DMAC_REG_SRC_STRIDE, 0x0);
-			if ((dmac->transfer.dest_addr % (dmac->width_dst / 8))
-			    || (dmac->transfer.src_addr % (dmac->width_src / 8))) {
+			if ((dmac->transfer.dest_addr % (dmac->width_dst))
+			    || (dmac->transfer.src_addr % (dmac->width_src))) {
 				printf("Source and destination addresses should be aligned with data path widths.\n");
 				return -1;
 			}