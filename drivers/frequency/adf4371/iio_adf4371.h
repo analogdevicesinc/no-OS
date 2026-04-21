30830b6acb55ff6d66befa303a3e4cc524aaefb3
Jude-Osems <156699659+Jude-Osems@users.noreply.github.com>
2026-04-21 17:17:46 +0100

    drivers: frequency: adf4371: Add IIO support for ADF4371
    create IIO wrapper for each driver functions.

---
+++ drivers/frequency/adf4371/iio_adf4371.h
@@ -0,0 +1,70 @@
+/***************************************************************************//**
+ *   @file   iio_adf4371.h
+ *   @brief  Implementation of IIO ADF4371 Driver.
+ *   @author Jude Osemene (jude.osemene@analog.com)
+********************************************************************************
+ * Copyright 2024(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef IIO_ADF4371_H
+#define IIO_ADF4371_H
+
+#include "iio.h"
+
+struct adf4371_iio_dev {
+	struct adf4371_dev *adf4371_dev;
+	struct iio_device *iio_dev;
+};
+
+struct adf4371_iio_dev_init_param {
+	struct adf4371_init_param *adf4371_dev_init;
+};
+
+enum adf4371_iio_ch_attr_id {
+	ADF4371_IIO_CH_ATTR_CHANNEL_POWERDOWN,
+	ADF4371_IIO_CH_ATTR_RFOUT8_FREQ,
+	ADF4371_IIO_CH_ATTR_RFOUT8AUX_FREQ,
+	ADF4371_IIO_CH_ATTR_RFOUT16_FREQ,
+	ADF4371_IIO_CH_ATTR_RFOUT32_FREQ,
+};
+
+enum adf4371_iio_dev_attr_id {
+	ADF4371_IIO_DEV_ATTR_REF_FREQ,
+	ADF4371_IIO_DEV_ATTR_BLEED_CURRENT,
+	ADF4371_IIO_DEV_ATTR_CHARGE_PUMP_CURRENT,
+	ADF4371_IIO_DEV_ATTR_CHARGE_PUMP_AVAILABLE,
+	ADF4371_IIO_DEV_ATTR_REF_DIVIDER2_ENABLE,
+	ADF4371_IIO_DEV_ATTR_REF_DOUBLER_EN,
+	ADF4371_IIO_DEV_ATTR_R_WORD,
+};
+
+int adf4371_iio_init(struct adf4371_iio_dev **iio_dev,
+		     struct adf4371_iio_dev_init_param *init_param);
+
+int adf4371_iio_remove(struct adf4371_iio_dev *desc);
+
+#endif
