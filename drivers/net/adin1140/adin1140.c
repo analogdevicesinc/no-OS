585bf21f587a7798fc5bbd0fc4eee8c01ad5b81c
Ciprian Regus <ciprian.regus@analog.com>
2026-05-04 11:39:25 +0300

    add a driver for adin1140

---
+++ drivers/net/adin1140/adin1140.c
@@ -0,0 +1,623 @@
+/***************************************************************************//**
+ *   @file   adin1140.c
+ *   @brief  Source file of the ADIN1140 driver.
+ *   @author Ciprian Regus (ciprian.regus@analog.com)
+********************************************************************************
+ * Copyright 2025(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include <errno.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include "adin1140.h"
+#include "no_os_alloc.h"
+#include "no_os_util.h"
+
+/**
+ * @brief Write a register's value.
+ * @param desc - the device descriptor.
+ * @param addr - MMS-encoded register address.
+ * @param data - register value to write.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_reg_write(struct adin1140_desc *desc, uint32_t addr, uint32_t data)
+{
+	return oa_tc6_reg_write(desc->oa_desc, addr, data);
+}
+
+/**
+ * @brief Read a register's value.
+ * @param desc - the device descriptor.
+ * @param addr - MMS-encoded register address.
+ * @param data - pointer to store the register value.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_reg_read(struct adin1140_desc *desc, uint32_t addr, uint32_t *data)
+{
+	return oa_tc6_reg_read(desc->oa_desc, addr, data);
+}
+
+/**
+ * @brief Update a register's value based on a mask.
+ * @param desc - the device descriptor.
+ * @param addr - MMS-encoded register address.
+ * @param mask - the bits that may be modified.
+ * @param data - new value for the masked bits.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_reg_update(struct adin1140_desc *desc, uint32_t addr,
+			uint32_t mask, uint32_t data)
+{
+	uint32_t val;
+	int ret;
+
+	ret = adin1140_reg_read(desc, addr, &val);
+	if (ret)
+		return ret;
+
+	val &= ~mask;
+	val |= mask & data;
+
+	return adin1140_reg_write(desc, addr, val);
+}
+
+/**
+ * @brief Map a C45 MMD device number to the ADIN1140 MMS index.
+ * @param devnum - MMD device number.
+ * @return MMS index, or negative error code if unsupported.
+ */
+static int adin1140_get_phy_c45_mms(uint32_t devnum)
+{
+	switch (devnum) {
+	case ADIN1140_MMD_PCS:
+		return ADIN1140_PHY_C45_PCS_MMS;
+	case ADIN1140_MMD_PMA_PMD:
+		return ADIN1140_PHY_C45_PMA_PMD_MMS;
+	case ADIN1140_MMD_VEND2:
+		return ADIN1140_PHY_C45_VS_PLCA_MMS;
+	default:
+		return -EINVAL;
+	}
+}
+
+/**
+ * @brief Read a PHY register using clause 45 via MMS.
+ * @param desc - the device descriptor.
+ * @param devnum - MMD device number (1=PMA/PMD, 3=PCS, 31=VEND2).
+ * @param reg - register address within the MMD.
+ * @param data - pointer to store the register value.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_mdio_read_c45(struct adin1140_desc *desc, uint32_t devnum,
+			   uint16_t reg, uint16_t *data)
+{
+	uint32_t val;
+	int mms;
+	int ret;
+
+	mms = adin1140_get_phy_c45_mms(devnum);
+	if (mms < 0)
+		return mms;
+
+	ret = oa_tc6_reg_read(desc->oa_desc, OA_MMS_REG(mms, reg), &val);
+	if (ret)
+		return ret;
+
+	*data = (uint16_t)(val & 0xFFFF);
+
+	return 0;
+}
+
+/**
+ * @brief Write a PHY register using clause 45 via MMS.
+ * @param desc - the device descriptor.
+ * @param devnum - MMD device number (1=PMA/PMD, 3=PCS, 31=VEND2).
+ * @param reg - register address within the MMD.
+ * @param data - value to write.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_mdio_write_c45(struct adin1140_desc *desc, uint32_t devnum,
+			    uint16_t reg, uint16_t data)
+{
+	int mms;
+
+	mms = adin1140_get_phy_c45_mms(devnum);
+	if (mms < 0)
+		return mms;
+
+	return oa_tc6_reg_write(desc->oa_desc, OA_MMS_REG(mms, reg),
+				(uint32_t)data);
+}
+
+/**
+ * @brief Write a MAC address filter with mask to a specific slot.
+ * @param desc - the device descriptor.
+ * @param addr - 6-byte MAC address.
+ * @param mask - 6-byte MAC address mask.
+ * @param slot - filter slot index (0 to ADIN1140_MAC_FILT_MAX_SLOT - 1).
+ * @return 0 in case of success, negative error code otherwise.
+ */
+static int adin1140_mac_filter_write(struct adin1140_desc *desc,
+				     const uint8_t *addr,
+				     const uint8_t *mask, uint8_t slot)
+{
+	uint32_t val;
+	int ret;
+
+	if (slot >= ADIN1140_MAC_FILT_MAX_SLOT)
+		return -EINVAL;
+
+	val = no_os_get_unaligned_be16((uint8_t *)&addr[0]);
+	val |= ADIN1140_MAC_ADDR_APPLY2PORT1 | ADIN1140_MAC_ADDR_TO_HOST;
+	ret = adin1140_reg_write(desc, ADIN1140_MAC_ADDR_FILT_UPR_REG(slot), val);
+	if (ret)
+		return ret;
+
+	val = no_os_get_unaligned_be32((uint8_t *)&addr[2]);
+	ret = adin1140_reg_write(desc, ADIN1140_MAC_ADDR_FILT_LWR_REG(slot), val);
+	if (ret)
+		return ret;
+
+	val = no_os_get_unaligned_be16((uint8_t *)&mask[0]);
+	ret = adin1140_reg_write(desc, ADIN1140_MAC_ADDR_MASK_UPR_REG(slot), val);
+	if (ret)
+		return ret;
+
+	val = no_os_get_unaligned_be32((uint8_t *)&mask[2]);
+
+	return adin1140_reg_write(desc, ADIN1140_MAC_ADDR_MASK_LWR_REG(slot), val);
+}
+
+/**
+ * @brief Clear a MAC address filter slot.
+ * @param desc - the device descriptor.
+ * @param slot - filter slot index.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+static int adin1140_mac_filter_clear(struct adin1140_desc *desc, uint8_t slot)
+{
+	uint8_t mask[ADIN1140_ETH_ALEN];
+	uint8_t addr[ADIN1140_ETH_ALEN];
+
+	memset(mask, 0xFF, ADIN1140_ETH_ALEN);
+	memset(addr, 0x00, ADIN1140_ETH_ALEN);
+
+	return adin1140_mac_filter_write(desc, addr, mask, slot);
+}
+
+/**
+ * @brief Set the unicast MAC address filter.
+ * @param desc - the device descriptor.
+ * @param mac_address - the MAC address to filter on.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_set_mac_addr(struct adin1140_desc *desc,
+			  uint8_t mac_address[ADIN1140_ETH_ALEN])
+{
+	uint8_t mask[ADIN1140_ETH_ALEN];
+
+	memcpy(desc->mac_address, mac_address, ADIN1140_ETH_ALEN);
+	memset(mask, 0xFF, ADIN1140_ETH_ALEN);
+
+	return adin1140_mac_filter_write(desc, mac_address, mask,
+					 ADIN1140_MAC_FILT_UNICAST_SLOT);
+}
+
+/**
+ * @brief Enable/disable the broadcast filter.
+ * @param desc - the device descriptor.
+ * @param enabled - true to enable, false to disable.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_broadcast_filter(struct adin1140_desc *desc, bool enabled)
+{
+	uint8_t bcast[ADIN1140_ETH_ALEN];
+	uint8_t mask[ADIN1140_ETH_ALEN];
+
+	if (!enabled)
+		return adin1140_mac_filter_clear(desc,
+						 ADIN1140_MAC_FILT_BROADCAST_SLOT);
+
+	memset(bcast, 0xFF, ADIN1140_ETH_ALEN);
+	memset(mask, 0xFF, ADIN1140_ETH_ALEN);
+
+	return adin1140_mac_filter_write(desc, bcast, mask,
+					 ADIN1140_MAC_FILT_BROADCAST_SLOT);
+}
+
+/**
+ * @brief Enable/disable the all-multicast filter.
+ *        Matches any frame with the multicast bit set in the destination MAC.
+ * @param desc - the device descriptor.
+ * @param enabled - true to enable, false to disable.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_multicast_filter(struct adin1140_desc *desc, bool enabled)
+{
+	uint8_t mcast_addr[ADIN1140_ETH_ALEN] = {0x01, 0x00, 0x00,
+						  0x00, 0x00, 0x00};
+
+	if (!enabled)
+		return adin1140_mac_filter_clear(desc,
+						 ADIN1140_MAC_FILT_MULTICAST_SLOT);
+
+	return adin1140_mac_filter_write(desc, mcast_addr, mcast_addr,
+					 ADIN1140_MAC_FILT_MULTICAST_SLOT);
+}
+
+/**
+ * @brief Set/clear promiscuous mode via CONFIG2 FWD_UNK2HOST.
+ * @param desc - the device descriptor.
+ * @param promisc - true to enable promiscuous mode, false to disable.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_set_promisc(struct adin1140_desc *desc, bool promisc)
+{
+	return adin1140_reg_update(desc, ADIN1140_CONFIG2_REG,
+				   ADIN1140_CONFIG2_FWD_UNK2HOST,
+				   promisc ? ADIN1140_CONFIG2_FWD_UNK2HOST : 0);
+}
+
+/**
+ * @brief Write a frame to the TX FIFO via OA TC6.
+ * @param desc - the device descriptor.
+ * @param eth_buff - the frame to be transmitted.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_write_fifo(struct adin1140_desc *desc,
+			struct adin1140_eth_buff *eth_buff)
+{
+	struct oa_tc6_frame_buffer *oa_frame;
+	uint32_t frame_offset = 0;
+	int ret;
+
+	ret = oa_tc6_get_tx_frame(desc->oa_desc, &oa_frame);
+	if (ret)
+		return ret;
+
+	memcpy(&oa_frame->data[frame_offset], &eth_buff->mac_dest[0],
+	       ADIN1140_ETH_HDR_LEN);
+	frame_offset += ADIN1140_ETH_HDR_LEN;
+
+	memcpy(&oa_frame->data[frame_offset], eth_buff->payload,
+	       eth_buff->len - ADIN1140_ETH_HDR_LEN);
+
+	if (eth_buff->len < 64)
+		oa_frame->len = 64;
+	else
+		oa_frame->len = eth_buff->len;
+
+	oa_frame->vs = 0;
+
+	oa_tc6_put_tx_frame(desc->oa_desc, oa_frame);
+
+	return oa_tc6_thread(desc->oa_desc);
+}
+
+/**
+ * @brief Read a frame from the RX FIFO via OA TC6.
+ * @param desc - the device descriptor.
+ * @param eth_buff - buffer to receive the frame.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_read_fifo(struct adin1140_desc *desc,
+		       struct adin1140_eth_buff *eth_buff)
+{
+	struct oa_tc6_frame_buffer *frame;
+	uint32_t field_offset = 0;
+	int ret;
+
+	oa_tc6_thread(desc->oa_desc);
+
+	ret = oa_tc6_get_rx_frame(desc->oa_desc, &frame);
+	if (ret)
+		return ret;
+
+	memcpy(&eth_buff->mac_dest[0], &frame->data[field_offset],
+	       ADIN1140_ETH_HDR_LEN);
+	field_offset += ADIN1140_ETH_HDR_LEN;
+
+	memcpy(eth_buff->payload, &frame->data[field_offset],
+	       frame->len - ADIN1140_ETH_HDR_LEN);
+	eth_buff->len = frame->len;
+
+	oa_tc6_put_rx_frame(desc->oa_desc, frame);
+
+	return 0;
+}
+
+/**
+ * @brief Set the PLCA configuration.
+ * @param desc - the device descriptor.
+ * @param cfg - PLCA configuration to apply.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_plca_set_cfg(struct adin1140_desc *desc,
+			  struct adin1140_plca_cfg *cfg)
+{
+	uint32_t val;
+	int ret;
+
+	/* CTRL1: node_id and node_cnt */
+	val = no_os_field_prep(ADIN1140_PLCA_CTRL1_NCNT_MASK, cfg->node_cnt) |
+	      no_os_field_prep(ADIN1140_PLCA_CTRL1_ID_MASK, cfg->node_id);
+	ret = adin1140_reg_write(desc, ADIN1140_PLCA_CTRL1_REG, val);
+	if (ret)
+		return ret;
+
+	/* TOTMR: transmit opportunity timer */
+	val = no_os_field_prep(ADIN1140_PLCA_TOTMR_MASK, cfg->to_tmr);
+	ret = adin1140_reg_write(desc, ADIN1140_PLCA_TOTMR_REG, val);
+	if (ret)
+		return ret;
+
+	/* BURST: max burst count and burst timer */
+	val = no_os_field_prep(ADIN1140_PLCA_BURST_MAXBC_MASK, cfg->burst_cnt) |
+	      no_os_field_prep(ADIN1140_PLCA_BURST_BTMR_MASK, cfg->burst_tmr);
+	ret = adin1140_reg_write(desc, ADIN1140_PLCA_BURST_REG, val);
+	if (ret)
+		return ret;
+
+	/* CTRL0: enable/reset */
+	val = cfg->enabled ? ADIN1140_PLCA_CTRL0_EN : 0;
+	ret = adin1140_reg_write(desc, ADIN1140_PLCA_CTRL0_REG, val);
+	if (ret)
+		return ret;
+
+	return 0;
+}
+
+/**
+ * @brief Get the current PLCA configuration.
+ * @param desc - the device descriptor.
+ * @param cfg - pointer to store the PLCA configuration.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_plca_get_cfg(struct adin1140_desc *desc,
+			  struct adin1140_plca_cfg *cfg)
+{
+	uint32_t val;
+	int ret;
+
+	ret = adin1140_reg_read(desc, ADIN1140_PLCA_CTRL0_REG, &val);
+	if (ret)
+		return ret;
+	cfg->enabled = !!(val & ADIN1140_PLCA_CTRL0_EN);
+
+	ret = adin1140_reg_read(desc, ADIN1140_PLCA_CTRL1_REG, &val);
+	if (ret)
+		return ret;
+	cfg->node_cnt = no_os_field_get(ADIN1140_PLCA_CTRL1_NCNT_MASK, val);
+	cfg->node_id = no_os_field_get(ADIN1140_PLCA_CTRL1_ID_MASK, val);
+
+	ret = adin1140_reg_read(desc, ADIN1140_PLCA_TOTMR_REG, &val);
+	if (ret)
+		return ret;
+	cfg->to_tmr = no_os_field_get(ADIN1140_PLCA_TOTMR_MASK, val);
+
+	ret = adin1140_reg_read(desc, ADIN1140_PLCA_BURST_REG, &val);
+	if (ret)
+		return ret;
+	cfg->burst_cnt = no_os_field_get(ADIN1140_PLCA_BURST_MAXBC_MASK, val);
+	cfg->burst_tmr = no_os_field_get(ADIN1140_PLCA_BURST_BTMR_MASK, val);
+
+	return 0;
+}
+
+/**
+ * @brief Get the PLCA status (whether PLCA is active).
+ * @param desc - the device descriptor.
+ * @param active - pointer to store the status (true if PLCA is active).
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_plca_get_status(struct adin1140_desc *desc, bool *active)
+{
+	uint32_t val;
+	int ret;
+
+	ret = adin1140_reg_read(desc, ADIN1140_PLCA_STATUS_REG, &val);
+	if (ret)
+		return ret;
+
+	*active = !!(val & ADIN1140_PLCA_STATUS_PST);
+
+	return 0;
+}
+
+/**
+ * @brief Get the link state.
+ * @param desc - the device descriptor.
+ * @param state - pointer to store the link state (1 = up, 0 = down).
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_link_state(struct adin1140_desc *desc, uint32_t *state)
+{
+	int ret;
+
+	ret = adin1140_reg_read(desc, ADIN1140_STATUS1_REG, state);
+	if (ret)
+		return ret;
+
+	*state = no_os_field_get(ADIN1140_STATUS1_LINK_STATE, *state);
+
+	return 0;
+}
+
+/**
+ * @brief Configure the MAC: set ZARFE, clear TXFCSVE, set CPS=64,
+ *        disable loopback, and install default filters.
+ * @param desc - the device descriptor.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+static int adin1140_setup_mac(struct adin1140_desc *desc)
+{
+	uint32_t val;
+	int ret;
+
+	ret = adin1140_reg_read(desc, ADIN1140_CONFIG0_REG, &val);
+	if (ret)
+		return ret;
+
+	val |= ADIN1140_CONFIG0_ZARFE;
+	val &= ~ADIN1140_CONFIG0_TXFCSVE;
+	val |= ADIN1140_CONFIG0_CPS_64;
+
+	ret = adin1140_reg_write(desc, ADIN1140_CONFIG0_REG, val);
+	if (ret)
+		return ret;
+
+	/* Disable MAC loopback */
+	ret = adin1140_reg_write(desc, ADIN1140_MAC_P1_LOOP_REG, 0x0);
+	if (ret)
+		return ret;
+
+	/* Install default filters: broadcast + unicast MAC */
+	ret = adin1140_broadcast_filter(desc, true);
+	if (ret)
+		return ret;
+
+	return adin1140_set_mac_addr(desc, desc->mac_address);
+}
+
+/**
+ * @brief Verify the PHY identity by reading the MACPHY ID register.
+ * @param desc - the device descriptor.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+static int adin1140_setup_phy(struct adin1140_desc *desc)
+{
+	uint32_t phy_id;
+	int ret;
+
+	ret = adin1140_reg_read(desc, ADIN1140_MACPHY_ID_REG, &phy_id);
+	if (ret)
+		return ret;
+
+	if (phy_id != ADIN1140_PHY_ID)
+		return -EINVAL;
+
+	return 0;
+}
+
+/**
+ * @brief Complete the reset sequence by checking STATUS0 and setting SYNC.
+ * @param desc - the device descriptor.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+static int adin1140_check_reset(struct adin1140_desc *desc)
+{
+	uint32_t reg_val;
+	int ret;
+
+	ret = adin1140_reg_read(desc, ADIN1140_STATUS0_REG, &reg_val);
+	if (ret)
+		return ret;
+
+	if (!no_os_field_get(ADIN1140_STATUS0_RESETC, reg_val))
+		return -EBUSY;
+
+	return adin1140_reg_update(desc, ADIN1140_CONFIG0_REG,
+				   ADIN1140_CONFIG0_SYNC,
+				   ADIN1140_CONFIG0_SYNC);
+}
+
+/**
+ * @brief Initialize the ADIN1140 device.
+ * @param desc - pointer to the device descriptor to be initialized.
+ * @param param - the device's initialization parameters.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_init(struct adin1140_desc **desc,
+		  struct adin1140_init_param *param)
+{
+	struct oa_tc6_init_param oa_param;
+	struct adin1140_desc *d;
+	int ret;
+
+	if (!param->mac_address)
+		return -EINVAL;
+
+	d = no_os_calloc(1, sizeof(*d));
+	if (!d)
+		return -ENOMEM;
+
+	ret = no_os_spi_init(&d->comm_desc, &param->comm_param);
+	if (ret)
+		goto free_desc;
+
+	memcpy(d->mac_address, param->mac_address, ADIN1140_ETH_ALEN);
+
+	oa_param.comm_desc = d->comm_desc;
+	oa_param.prote_spi = true;
+	ret = oa_tc6_init(&d->oa_desc, &oa_param);
+	if (ret)
+		goto free_spi;
+
+	ret = adin1140_setup_mac(d);
+	if (ret)
+		goto free_oa;
+
+	ret = adin1140_setup_phy(d);
+	if (ret)
+		goto free_oa;
+
+	ret = adin1140_check_reset(d);
+	if (ret)
+		goto free_oa;
+
+	*desc = d;
+
+	return 0;
+
+free_oa:
+	oa_tc6_remove(d->oa_desc);
+free_spi:
+	no_os_spi_remove(d->comm_desc);
+free_desc:
+	no_os_free(d);
+
+	return ret;
+}
+
+/**
+ * @brief Free the ADIN1140 device descriptor.
+ * @param desc - the device descriptor to be removed.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int adin1140_remove(struct adin1140_desc *desc)
+{
+	if (!desc)
+		return -EINVAL;
+
+	oa_tc6_remove(desc->oa_desc);
+	no_os_spi_remove(desc->comm_desc);
+	no_os_free(desc);
+
+	return 0;
+}
---
+++ drivers/net/adin1140/adin1140.h
@@ -0,0 +1,237 @@
+/******************************************************************************
+ *   @file   adin1140.h
+ *   @brief  Header file of the ADIN1140 driver.
+ *   @author Ciprian Regus (ciprian.regus@analog.com)
+********************************************************************************
+ * Copyright 2025(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#ifndef _ADIN1140_H
+#define _ADIN1140_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "no_os_spi.h"
+#include "no_os_util.h"
+#include "oa_tc6.h"
+
+#define ADIN1140_ETH_ALEN		6
+#define ADIN1140_ETH_HDR_LEN		14
+
+/* MACPHY identification */
+#define ADIN1140_PHY_ID			0x0283BE00
+
+/* MMS 0: Standard OA TC6 registers */
+#define ADIN1140_MACPHY_ID_REG		OA_MMS_REG(0x0, 0x0001)
+#define ADIN1140_CONFIG0_REG		OA_MMS_REG(0x0, 0x0004)
+#define ADIN1140_CONFIG2_REG		OA_MMS_REG(0x0, 0x0006)
+#define ADIN1140_STATUS0_REG		OA_MMS_REG(0x0, 0x0008)
+#define ADIN1140_STATUS1_REG		OA_MMS_REG(0x0, 0x0009)
+#define ADIN1140_IMASK1_REG		OA_MMS_REG(0x0, 0x000D)
+
+/* CONFIG0 bits */
+#define ADIN1140_CONFIG0_SYNC		NO_OS_BIT(15)
+#define ADIN1140_CONFIG0_TXFCSVE	NO_OS_BIT(14)
+#define ADIN1140_CONFIG0_ZARFE		NO_OS_BIT(12)
+#define ADIN1140_CONFIG0_CPS_64		NO_OS_GENMASK(2, 1)
+
+/* CONFIG2 bits */
+#define ADIN1140_CONFIG2_FWD_UNK2HOST	NO_OS_BIT(2)
+
+/* STATUS0 bits */
+#define ADIN1140_STATUS0_RESETC		NO_OS_BIT(6)
+
+/* STATUS1 bits */
+#define ADIN1140_STATUS1_LINK_STATE	NO_OS_BIT(0)
+
+/* MMS 1: MAC registers */
+#define ADIN1140_MAC_ADDR_FILT_UPR_REG(x)	OA_MMS_REG(0x1, 0x50 + 2 * (x))
+#define ADIN1140_MAC_ADDR_FILT_LWR_REG(x)	OA_MMS_REG(0x1, 0x51 + 2 * (x))
+#define ADIN1140_MAC_ADDR_MASK_UPR_REG(x)	OA_MMS_REG(0x1, 0x70 + 2 * (x))
+#define ADIN1140_MAC_ADDR_MASK_LWR_REG(x)	OA_MMS_REG(0x1, 0x71 + 2 * (x))
+#define ADIN1140_MAC_P1_LOOP_REG		OA_MMS_REG(0x1, 0xC4)
+
+/* MAC address filter flags */
+#define ADIN1140_MAC_ADDR_APPLY2PORT1	NO_OS_BIT(30)
+#define ADIN1140_MAC_ADDR_TO_HOST	NO_OS_BIT(16)
+
+/* Filter slot assignments */
+#define ADIN1140_MAC_FILT_MULTICAST_SLOT	0
+#define ADIN1140_MAC_FILT_BROADCAST_SLOT	1
+#define ADIN1140_MAC_FILT_UNICAST_SLOT		2
+#define ADIN1140_MAC_FILT_MAX_SLOT		16
+
+/* PHY C45 MMS mapping */
+#define ADIN1140_PHY_C45_PCS_MMS	2
+#define ADIN1140_PHY_C45_PMA_PMD_MMS	3
+#define ADIN1140_PHY_C45_VS_PLCA_MMS	4
+
+/* C45 MMD device numbers */
+#define ADIN1140_MMD_PMA_PMD		1
+#define ADIN1140_MMD_PCS		3
+#define ADIN1140_MMD_VEND2		31
+
+/* PLCA registers (MMS 4 = MMD 31) */
+#define ADIN1140_PLCA_IDVER_REG		OA_MMS_REG(0x4, 0xCA00)
+#define ADIN1140_PLCA_CTRL0_REG		OA_MMS_REG(0x4, 0xCA01)
+#define ADIN1140_PLCA_CTRL1_REG		OA_MMS_REG(0x4, 0xCA02)
+#define ADIN1140_PLCA_STATUS_REG	OA_MMS_REG(0x4, 0xCA03)
+#define ADIN1140_PLCA_TOTMR_REG		OA_MMS_REG(0x4, 0xCA04)
+#define ADIN1140_PLCA_BURST_REG		OA_MMS_REG(0x4, 0xCA05)
+
+/* PLCA CTRL0 bits */
+#define ADIN1140_PLCA_CTRL0_EN		NO_OS_BIT(15)
+#define ADIN1140_PLCA_CTRL0_RST	NO_OS_BIT(14)
+
+/* PLCA CTRL1 bits */
+#define ADIN1140_PLCA_CTRL1_NCNT_MASK	NO_OS_GENMASK(15, 8)
+#define ADIN1140_PLCA_CTRL1_ID_MASK	NO_OS_GENMASK(7, 0)
+
+/* PLCA STATUS bits */
+#define ADIN1140_PLCA_STATUS_PST	NO_OS_BIT(15)
+
+/* PLCA TOTMR bits */
+#define ADIN1140_PLCA_TOTMR_MASK	NO_OS_GENMASK(7, 0)
+
+/* PLCA BURST bits */
+#define ADIN1140_PLCA_BURST_MAXBC_MASK	NO_OS_GENMASK(15, 8)
+#define ADIN1140_PLCA_BURST_BTMR_MASK	NO_OS_GENMASK(7, 0)
+
+/* MMS 1: Statistics registers */
+#define ADIN1140_RX_FRAME_CNT_REG	OA_MMS_REG(0x1, 0xA1)
+#define ADIN1140_RX_BCAST_CNT_REG	OA_MMS_REG(0x1, 0xA2)
+#define ADIN1140_RX_MCAST_CNT_REG	OA_MMS_REG(0x1, 0xA3)
+#define ADIN1140_RX_UCAST_CNT_REG	OA_MMS_REG(0x1, 0xA4)
+#define ADIN1140_RX_CRC_ERR_CNT_REG	OA_MMS_REG(0x1, 0xA5)
+#define ADIN1140_RX_ALGN_ERR_CNT_REG	OA_MMS_REG(0x1, 0xA6)
+#define ADIN1140_TX_FRAME_CNT_REG	OA_MMS_REG(0x1, 0xB1)
+#define ADIN1140_TX_BCAST_CNT_REG	OA_MMS_REG(0x1, 0xB2)
+#define ADIN1140_TX_MCAST_CNT_REG	OA_MMS_REG(0x1, 0xB3)
+#define ADIN1140_TX_UCAST_CNT_REG	OA_MMS_REG(0x1, 0xB4)
+
+/**
+ * @brief ADIN1140 device descriptor.
+ */
+struct adin1140_desc {
+	struct no_os_spi_desc *comm_desc;
+	uint8_t mac_address[ADIN1140_ETH_ALEN];
+	struct oa_tc6_desc *oa_desc;
+};
+
+/**
+ * @brief Initialization parameter for the device descriptor.
+ */
+struct adin1140_init_param {
+	struct no_os_spi_init_param comm_param;
+	uint8_t mac_address[ADIN1140_ETH_ALEN];
+};
+
+/**
+ * @brief Buffer structure used for frame RX and TX transactions.
+ */
+struct adin1140_eth_buff {
+	uint32_t len;
+	uint8_t mac_dest[ADIN1140_ETH_ALEN];
+	uint8_t mac_source[ADIN1140_ETH_ALEN];
+	uint8_t ethertype[2];
+	uint8_t *payload;
+};
+
+/**
+ * @brief PLCA (Physical Layer Collision Avoidance) configuration.
+ */
+struct adin1140_plca_cfg {
+	bool enabled;
+	uint8_t node_id;
+	uint8_t node_cnt;
+	uint8_t to_tmr;
+	uint8_t burst_cnt;
+	uint8_t burst_tmr;
+};
+
+/* Write a register */
+int adin1140_reg_write(struct adin1140_desc *desc, uint32_t addr, uint32_t data);
+
+/* Read a register */
+int adin1140_reg_read(struct adin1140_desc *desc, uint32_t addr, uint32_t *data);
+
+/* Update a register's value based on a mask */
+int adin1140_reg_update(struct adin1140_desc *desc, uint32_t addr,
+			uint32_t mask, uint32_t data);
+
+/* Read a PHY register using clause 45 via MMS */
+int adin1140_mdio_read_c45(struct adin1140_desc *desc, uint32_t devnum,
+			   uint16_t reg, uint16_t *data);
+
+/* Write a PHY register using clause 45 via MMS */
+int adin1140_mdio_write_c45(struct adin1140_desc *desc, uint32_t devnum,
+			    uint16_t reg, uint16_t data);
+
+/* Set the unicast MAC address filter */
+int adin1140_set_mac_addr(struct adin1140_desc *desc,
+			  uint8_t mac_address[ADIN1140_ETH_ALEN]);
+
+/* Enable/disable the broadcast filter */
+int adin1140_broadcast_filter(struct adin1140_desc *desc, bool enabled);
+
+/* Enable/disable the multicast filter */
+int adin1140_multicast_filter(struct adin1140_desc *desc, bool enabled);
+
+/* Set/clear promiscuous mode */
+int adin1140_set_promisc(struct adin1140_desc *desc, bool promisc);
+
+/* Write a frame to the TX FIFO */
+int adin1140_write_fifo(struct adin1140_desc *desc,
+			struct adin1140_eth_buff *eth_buff);
+
+/* Read a frame from the RX FIFO */
+int adin1140_read_fifo(struct adin1140_desc *desc,
+		       struct adin1140_eth_buff *eth_buff);
+
+/* Set the PLCA configuration */
+int adin1140_plca_set_cfg(struct adin1140_desc *desc,
+			  struct adin1140_plca_cfg *cfg);
+
+/* Get the PLCA configuration */
+int adin1140_plca_get_cfg(struct adin1140_desc *desc,
+			  struct adin1140_plca_cfg *cfg);
+
+/* Get the PLCA status */
+int adin1140_plca_get_status(struct adin1140_desc *desc, bool *active);
+
+/* Get the link state */
+int adin1140_link_state(struct adin1140_desc *desc, uint32_t *state);
+
+/* Initialize the device */
+int adin1140_init(struct adin1140_desc **desc,
+		  struct adin1140_init_param *param);
+
+/* Free a device descriptor */
+int adin1140_remove(struct adin1140_desc *desc);
+
+#endif
