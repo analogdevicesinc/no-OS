b22f16bc2d031a13dd66f9f346f33f9fedf1d042
Antoniu Miclaus <antoniu.miclaus@analog.com>
2026-05-27 14:08:45 +0300

    projects: ad74414h: add MBED example project
    Add AD74414H project with MBED platform support and examples for all
    supported channel modes: basic, current output, current input (external
    and loop powered), digital input (logic and loop powered), and digital
    output. The AD74414H is a subset of the AD74416H, so the project reuses
    the existing ad74416h driver with ID_AD74414H, excluding unsupported
    modes (voltage input/output, resistance/RTD).
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

---
+++ projects/ad74414h/src/platform/mbed/platform_src.mk
@@ -0,0 +1,12 @@
+INCS += $(PLATFORM_DRIVERS)/mbed_uart.h      \
+		$(PLATFORM_DRIVERS)/mbed_gpio.h      \
+		$(PLATFORM_DRIVERS)/mbed_irq.h       \
+		$(PLATFORM_DRIVERS)/mbed_gpio_irq.h  \
+		$(PLATFORM_DRIVERS)/mbed_spi.h
+
+SRCS += $(PLATFORM_DRIVERS)/mbed_uart.cpp       \
+		$(PLATFORM_DRIVERS)/mbed_gpio.cpp       \
+		$(PLATFORM_DRIVERS)/mbed_irq.cpp        \
+		$(PLATFORM_DRIVERS)/mbed_gpio_irq.cpp   \
+		$(PLATFORM_DRIVERS)/mbed_spi.cpp        \
+		$(PLATFORM_DRIVERS)/mbed_delay.cpp
