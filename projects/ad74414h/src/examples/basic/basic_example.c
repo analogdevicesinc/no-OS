a32091c250e8705f5e863e6e678db2ba89cb75d7
Antoniu Miclaus <antoniu.miclaus@analog.com>
2026-05-27 14:08:45 +0300

    projects: ad74414h: add MBED example project
    Add AD74414H project with MBED platform support and examples for all
    supported channel modes: basic, current output, current input (external
    and loop powered), digital input (logic and loop powered), and digital
    output. The AD74414H is a subset of the AD74416H, so the project reuses
    the existing ad74416h driver with ID_AD74414H, excluding unsupported
    modes (voltage input/output, resistance/RTD).
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

---
+++ projects/ad74414h/src/examples/basic/basic_example.c
@@ -0,0 +1,70 @@
+/***************************************************************************//**
+ *   @file   basic_example.c
+ *   @brief  Basic example header for eval-ad74414h project
+ *   @author Antoniu Miclaus (antoniu.miclaus@analog.com)
+********************************************************************************
+ * Copyright 2026(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+#include "common_data.h"
+#include "ad74416h.h"
+#include "no_os_delay.h"
+#include "no_os_gpio.h"
+#include "no_os_print_log.h"
+
+/***************************************************************************//**
+ * @brief Basic example main execution.
+ *
+ * @return ret - Result of the example execution. If working correctly, will
+ *               execute continuously the while(1) loop and will not return.
+*******************************************************************************/
+int example_main()
+{
+	struct ad74416h_desc *ad74414h_desc;
+	int ret;
+
+	ret = ad74416h_init(&ad74414h_desc, &ad74414h_ip);
+	if (ret)
+		goto error;
+
+	pr_info("ad74414h successfully initialized!\r\n");
+
+	ret = ad74416h_gpio_set(ad74414h_desc, AD74416H_CH_C, NO_OS_GPIO_HIGH);
+	if (ret)
+		goto error_ad74414h;
+
+	pr_info("ad74414h GPO2 set to HIGH\r\n");
+
+	return 0;
+
+error_ad74414h:
+	ad74416h_remove(ad74414h_desc);
+error:
+	pr_info("Error!\r\n");
+	return ret;
+}
