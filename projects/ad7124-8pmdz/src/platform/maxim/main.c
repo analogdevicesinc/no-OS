81040bd42cc8c57639dd6e1edf1b47a0785c287c
Radu Sabau <radu.sabau@analog.com>
2026-06-23 14:55:00 +0300

    projects: ad7124-8pmdz: add Maxim platform support
    Add src/platform/maxim/ with main.c, parameters.c/.h and platform_src.mk
    to support the MAX32690 (AD-APARD32690-SL). Update builds.json with the
    corresponding target.
    
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

--- projects/ad7124-8pmdz/src/platform/maxim/main.c
+++ projects/ad7124-8pmdz/src/platform/maxim/main.c
@@ -1,9 +1,9 @@
 /***************************************************************************//**
- *   @file   common_data.c
- *   @brief  Defines common data to be used by ad7124-8pmdz examples.
- *   @author Drimbarean Andrei (andrei.drimbarean@analog.com)
+ *   @file   main.c
+ *   @brief  Main file for Maxim platform of ad7124-8pmdz project.
+ *   @author Radu Sabau (radu.sabau@analog.com)
 ********************************************************************************
- * Copyright 2020(c) Analog Devices, Inc.
+ * Copyright 2026(c) Analog Devices, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -31,33 +31,17 @@
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
 
+#include "parameters.h"
 #include "common_data.h"
 
-struct no_os_uart_init_param ad7124_uart_ip = {
-	.device_id = UART_DEVICE_ID,
-	.irq_id = UART_IRQ_ID,
-	.asynchronous_rx = true,
-	.baud_rate = UART_BAUDRATE,
-	.size = NO_OS_UART_CS_8,
-	.parity = NO_OS_UART_PAR_NO,
-	.stop = NO_OS_UART_STOP_1_BIT,
-	.extra = UART_EXTRA,
-	.platform_ops = UART_OPS,
-};
+extern int example_main();
 
-struct no_os_spi_init_param ad7124_spi_ip = {
-	.device_id = SPI_DEVICE_ID,
-	.max_speed_hz = SPI_BAUDRATE,
-	.bit_order = NO_OS_SPI_BIT_ORDER_MSB_FIRST,
-	.mode = NO_OS_SPI_MODE_3,
-	.platform_ops = SPI_OPS,
-	.chip_select = SPI_CS,
-	.extra = SPI_EXTRA,
-};
-
-struct ad7124_init_param ad7124_ip = {
-	.spi_init = &ad7124_spi_ip,
-	.regs = ad7124_init_regs_default,
-	.spi_rdy_poll_cnt = 1000,
-	.active_device = ID_AD7124_8,
-};
+/***************************************************************************//**
+ * @brief Main function execution for Maxim platform.
+ *
+ * @return ret - Result of the enabled examples execution.
+*******************************************************************************/
+int main()
+{
+	return example_main();
+}
