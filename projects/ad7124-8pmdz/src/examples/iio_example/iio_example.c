eb8ac4822e75fe6bdfa2aff504efdb9684137af2
Radu Sabau <radu.sabau@analog.com>
2026-06-23 11:34:00 +0300

    projects: ad7124-8pmdz: add ADuCM3029 CMake build support
    Add CMake/Kconfig build support for the ad7124-8pmdz project targeting
    the ADuCM3029 platform (EVAL-ADICUP3029 board):
    
    - CMakeLists.txt: declares the cmake target, wires common sources,
      iio_example and optional ESP8266 Wi-Fi networking sources
    - Kconfig: defines AD7124_8PMDZ_IIO_EXAMPLE and AD7124_8PMDZ_NETWORKING
      (selects WIFI_NETWORKING, enables SSID/PWD config symbols)
    - iio_example.conf / iio_wifi.conf: top-level variant defconfigs
    - boards/iio_example/eval-adicup3029.conf: sets ADUCM_PINMUX_PATH
    - boards/iio_wifi/eval-adicup3029.conf: sets ADUCM_PINMUX_PATH + TIMER
    - projects/Kconfig: register the project Kconfig
    - builds.json: removed (all aducm3029 builds now covered by cmake)
    
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

--- projects/ad7124-8pmdz/src/examples/iio_example/iio_example.c
+++ projects/ad7124-8pmdz/src/examples/iio_example/iio_example.c
@@ -1,9 +1,9 @@
 /***************************************************************************//**
- *   @file   app_config.h
- *   @brief  Config file of AD7124-8 project.
- *   @author DBogdan (dragos.bogdan@analog.com)
+ *   @file   iio_example.c
+ *   @brief  Implementation of IIO example for ad7124-8pmdz project.
+ *   @author Drimbarean Andrei (andrei.drimbarean@analog.com)
 ********************************************************************************
- * Copyright 2015(c) Analog Devices, Inc.
+ * Copyright 2020(c) Analog Devices, Inc.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -19,7 +19,7 @@
  *    contributors may be used to endorse or promote products derived from this
  *    software without specific prior written permission.
  *
- * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. "AS IS" AND ANY EXPRESS OR
  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
  * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
@@ -30,11 +30,51 @@
  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
-#ifndef CONFIG_H_
-#define CONFIG_H_
 
-//#define XILINX_PLATFORM
-//#define ALTERA_PLATFORM
-//#define ADUCM_PLATFORM
+#include "iio_ad7124.h"
+#include "common_data.h"
+#include "iio_app.h"
+#include "no_os_util.h"
 
+#ifndef DATA_BUFFER_SIZE
+#define DATA_BUFFER_SIZE	400
 #endif
+
+static uint8_t iio_data_buffer[DATA_BUFFER_SIZE * sizeof(uint32_t)];
+
+/***************************************************************************//**
+ * @brief IIO example main execution.
+ *
+ * @return ret - Result of the example execution. If working correctly, will
+ *               execute continuously the iio_app_run and will not return.
+*******************************************************************************/
+int example_main()
+{
+	int ret;
+	struct ad7124_dev *ad7124_device;
+	struct iio_app_desc *app;
+	struct iio_data_buffer adc_buff = {
+		.buff = (void *)iio_data_buffer,
+		.size = DATA_BUFFER_SIZE * sizeof(uint32_t),
+	};
+	struct iio_app_init_param app_init_param = { 0 };
+
+	ret = ad7124_setup(&ad7124_device, &ad7124_ip);
+	if (ret)
+		return ret;
+
+	struct iio_app_device iio_devices[] = {
+		IIO_APP_DEVICE("ad7124-8", ad7124_device, &iio_ad7124_device,
+			       &adc_buff, NULL, NULL)
+	};
+
+	app_init_param.devices = iio_devices;
+	app_init_param.nb_devices = NO_OS_ARRAY_SIZE(iio_devices);
+	app_init_param.uart_init_params = ad7124_uart_ip;
+
+	ret = iio_app_init(&app, app_init_param);
+	if (ret)
+		return ret;
+
+	return iio_app_run(app);
+}
