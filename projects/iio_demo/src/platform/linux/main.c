74e8d747765c0c34bdb51a9d97d0f66631966e32
Ciprian Regus <ciprian.regus@analog.com>
2026-06-26 14:36:01 +0300

    cmake: add iio_demo project cmake support
    Add CMake support for the iio_demo project: its CMakeLists, Kconfig, board
    configuration files and per-variant defconfigs, plus the example/board
    selection wiring. Align the project sources (common data, examples and
    per-platform mains) with the Kconfig CONFIG_ example-selection macros.
    
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

--- projects/iio_demo/src/platform/linux/main.c
+++ projects/iio_demo/src/platform/linux/main.c
@@ -35,11 +35,11 @@
 #include "common_data.h"
 #include "no_os_error.h"
 
-#ifdef IIO_EXAMPLE
+#ifdef CONFIG_IIO_DEMO_IIO_EXAMPLE
 #include "iio_example.h"
 #endif
 
-#ifdef IIO_SW_TRIGGER_EXAMPLE
+#ifdef CONFIG_IIO_DEMO_IIO_SW_TRIGGER_EXAMPLE
 #include "iio_sw_trigger_example.h"
 #endif
 
@@ -52,21 +52,21 @@ int main()
 {
 	int ret = -EINVAL;
 
-#ifdef IIO_EXAMPLE
+#ifdef CONFIG_IIO_DEMO_IIO_EXAMPLE
 	ret = iio_example_main();
 #endif
 
-#ifdef IIO_SW_TRIGGER_EXAMPLE
+#ifdef CONFIG_IIO_DEMO_IIO_SW_TRIGGER_EXAMPLE
 	ret = iio_sw_trigger_example_main();
 #endif
 
-#ifdef IIO_TIMER_TRIGGER_EXAMPLE
+#ifdef CONFIG_IIO_DEMO_IIO_TIMER_TRIGGER_EXAMPLE
 #error Timer trigger example is not supported on linux platform.
 #endif
 
-#if (IIO_EXAMPLE + IIO_SW_TRIGGER_EXAMPLE == 0)
+#if (CONFIG_IIO_DEMO_IIO_EXAMPLE + CONFIG_IIO_DEMO_IIO_SW_TRIGGER_EXAMPLE == 0)
 #error At least one example has to be selected using y value in Makefile.
-#elif (IIO_EXAMPLE + IIO_SW_TRIGGER_EXAMPLE > 1)
+#elif (CONFIG_IIO_DEMO_IIO_EXAMPLE + CONFIG_IIO_DEMO_IIO_SW_TRIGGER_EXAMPLE > 1)
 #error Selected example projects cannot be enabled at the same time. \
 Please enable only one example and rebuild the project.
 #endif
