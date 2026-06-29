499bb509256969136a006b53eead83231a70e96e
RaulGeo289 <georgianraul301@gmail.com>
2026-06-29 17:11:20 +0300

    drivers: accel: adxl37x: Add driver support for ADXL371/372
    - modified adxl371_interrupt_config() to handle ADXL371 according to the
      datasheet.Its registers for INT1 and INT2 have bits 3:1
    reserved, unlike ADXL372, which can map INT1 or INT2 to flag
    fifo_rdy,fifo_ovr or fifo_full.
    
    - added chip_info structure to keep the differences between devices in
      one place, according to the design used in AD5686.The user will
    configure the settings for bandwidth, wake-up rate and output data rate
    from here, these being the major differences between chips.These
    settings will be forwarded from the init structure to the dev structure
    in adxl37x_init(), based on the field from the init structure named type,
    a instance of enum adxl_type.This was done in adxl38x.I used the chip_info
     structure, because the user doesen't need to interact with any of the functions
     in the driver, only choose the settings he wants in the chip_structure instance,at
     the beggining of the driver.Also AD5686 instantiated chip_structure
    object in the driver at the beginning.
    
    Signed-off-by: Raul Georgian <Raul.Georgian@analog.com>
    Signed-off-by: Brandon Hurst <brandon.hurst@analog.com>

---
+++ drivers/accel/adxl37x/adxl37x_i2c.c
@@ -0,0 +1,95 @@
+/*******************************************************************************
+ *   @file   adxl37x_i2c.c
+ *   @brief  Implementation of adxl37x I2C Driver.
+ *   @author BHurst (brandon.hurst@analog.com)
+ ********************************************************************************
+ * Copyright 2026(c) Analog Devices, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*******************************************************************************/
+
+#include "adxl37x.h"
+#include "no_os_error.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Read from device.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param reg_data - The register data.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t adxl37x_i2c_reg_read(struct adxl37x_dev *dev, uint8_t reg_addr,
+			     uint8_t *reg_data)
+{
+	uint8_t data;
+	int32_t ret;
+
+	data = reg_addr;
+
+	ret = no_os_i2c_write(dev->i2c_desc, &data, 1, 0);
+	if (ret < 0)
+		return ret;
+
+	ret = no_os_i2c_read(dev->i2c_desc, &data, 1, 0);
+	if (ret < 0)
+		return ret;
+
+	*reg_data = data;
+
+	return ret;
+}
+
+/**
+ * Write to device.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param reg_data - The register data.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t adxl37x_i2c_reg_write(struct adxl37x_dev *dev, uint8_t reg_addr,
+			      uint8_t reg_data)
+{
+	uint8_t buf[2];
+
+	buf[0] = reg_addr;
+	buf[1] = reg_data & 0xFF;
+
+	return no_os_i2c_write(dev->i2c_desc, buf, NO_OS_ARRAY_SIZE(buf), 0);
+}
+
+/**
+ * Multibyte read from device. A register read begins with the address
+ * and autoincrements for each aditional byte in the transfer.
+ * @param dev - The device structure.
+ * @param reg_addr - The register address.
+ * @param reg_data - The register data.
+ * @param count - Number of bytes to read.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int32_t adxl37x_i2c_reg_read_multiple(struct adxl37x_dev *dev, uint8_t reg_addr,
+				      uint8_t *reg_data, uint16_t count)
+{
+	uint8_t buf[512];
+	int32_t ret;
+
+	if (count > 512)
+		return -EINVAL;
+
+	buf[0] = reg_addr;
+	memset(&buf[1], 0x00, count - 1);
+
+	ret = no_os_i2c_write(dev->i2c_desc, buf, 1, 0);
+	if (ret < 0)
+		return ret;
+
+	ret = no_os_i2c_read(dev->i2c_desc, buf, count, 0);
+	if (ret < 0)
+		return ret;
+
+	memcpy(reg_data, buf, count);
+
+	return ret;
+}
