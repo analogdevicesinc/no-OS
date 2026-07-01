analogdevicesinc/no-OS

commit 04887b1c931b67adba2417e185be3954157a4a52

projects:adf5902_sdz: add IIO support
Allow the user to read/write via UART interface using IIO oscilloscope
app.

Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

projects/adf5902_sdz/src.mk
@@ -17,8 +17,21 @@ SRCS +=	$(PLATFORM_DRIVERS)/axi_io.c				\
 	$(PLATFORM_DRIVERS)/xilinx_spi.c				\
 	$(PLATFORM_DRIVERS)/xilinx_gpio.c				\
 	$(PLATFORM_DRIVERS)/delay.c
+ifeq (y,$(strip $(TINYIIOD)))
+LIBRARIES += iio
+SRCS += $(NO-OS)/util/xml.c						\
+	$(NO-OS)/util/fifo.c						\
+	$(NO-OS)/util/list.c						\
+	$(PLATFORM_DRIVERS)/uart.c					\
+	$(PLATFORM_DRIVERS)/irq.c					\
+	$(NO-OS)/iio/iio_app/iio_app.c				\
+	$(DRIVERS)/frequency/adf5902/iio_adf5902.c
+endif
 INCS +=	$(PROJECT)/src/app_config.h					\
 	$(PROJECT)/src/parameters.h
+ifeq (y,$(strip $(TINYIIOD)))
+INCS +=	$(DRIVERS)/frequency/adf5902/iio_adf5902.h
+endif
 INCS += $(DRIVERS)/frequency/adf5902/adf5902.h
 INCS +=	$(PLATFORM_DRIVERS)/spi_extra.h				\
 	$(PLATFORM_DRIVERS)/gpio_extra.h
@@ -29,3 +42,13 @@ INCS +=	$(INCLUDE)/axi_io.h							\
 	$(INCLUDE)/delay.h								\
 	$(INCLUDE)/util.h								\
 	$(INCLUDE)/print_log.h
+ifeq (y,$(strip $(TINYIIOD)))
+INCS += $(INCLUDE)/xml.h						\
+	$(INCLUDE)/fifo.h						\
+	$(INCLUDE)/irq.h						\
+	$(INCLUDE)/uart.h						\
+	$(INCLUDE)/list.h						\
+	$(PLATFORM_DRIVERS)/irq_extra.h					\
+	$(PLATFORM_DRIVERS)/uart_extra.h				\
+	$(NO-OS)/iio/iio_app/iio_app.h
+endif

projects/adf5902_sdz/src/adf5902_sdz.c
@@ -48,6 +48,12 @@
 #include "error.h"
 #include "adf5902.h"
 #include "parameters.h"
+#include "util.h"
+
+#ifdef IIO_SUPPORT
+#include "iio_app.h"
+#include "iio_adf5902.h"
+#endif
 
 #define LOG_LEVEL 6
 #include "print_log.h"
@@ -214,6 +220,14 @@ int main(void)
 	}
 
 	pr_info("ADF5902 Temperature value: %.2f degC \n", temperature);
+
+#ifdef IIO_SUPPORT
+	struct iio_app_device devices[] = {
+		IIO_APP_DEVICE("adf5902_dev", dev, &adf5902_iio_descriptor,
+			       NULL, NULL),
+	};
+	return iio_app_run(devices, ARRAY_SIZE(devices));
+#endif
 
 	ret = adf5902_remove(dev);
 

projects/adf5902_sdz/src/parameters.h
@@ -53,6 +53,13 @@
 
 #define GPIO_DEVICE_ID				XPAR_PS7_GPIO_0_DEVICE_ID
 
+#define UART_DEVICE_ID				XPAR_XUARTPS_0_DEVICE_ID
+#define UART_IRQ_ID				    XPAR_XUARTPS_1_INTR
+
+#define UART_BAUDRATE	            115200
+
+#define INTC_DEVICE_ID				XPAR_SCUGIC_SINGLE_DEVICE_ID
+
 #define GPIO_OFFSET					32 + 54
 #define GPIO_CE						GPIO_OFFSET + 1
 #define GPIO_TX_DATA				GPIO_OFFSET + 2

commit c868d5c0e975e60aa55016cd1b4efa140b5edf71

projects: adf5902_sdz: fix IIO interrupt source paths
The IIO build referenced a non-existent $(PLATFORM_DRIVERS)/irq.c, which
broke the build. Replace it with the correct no_os_irq.c API source and
the platform-specific $(PLATFORM)_irq.c.

Fixes: 04887b1c931b ("projects:adf5902_sdz: add IIO support")
Signed-off-by: Stefan Popa <stefan.popa@analog.com>

projects/adf5902_sdz/src.mk
@@ -26,7 +26,8 @@ SRCS += $(NO-OS)/util/no_os_fifo.c \
 	$(NO-OS)/util/no_os_list.c \
 	$(PLATFORM_DRIVERS)/$(PLATFORM)_uart.c \
 	$(NO-OS)/util/no_os_lf256fifo.c \
-	$(PLATFORM_DRIVERS)/irq.c \
+	$(DRIVERS)/api/no_os_irq.c \
+	$(PLATFORM_DRIVERS)/$(PLATFORM)_irq.c \
 	$(NO-OS)/iio/iio_app/iio_app.c \
 	$(DRIVERS)/api/no_os_uart.c \
 	$(DRIVERS)/frequency/adf5902/iio_adf5902.c