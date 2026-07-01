7547966d50b9128db3d76c386e0ae53a10c64004
Stefan Popa <stefan.popa@analog.com>
2026-07-01 11:50:11 +0300

    projects: display_demo: add missing build sources and headers
    The ssd_1306 driver uses the I2C API (no_os_i2c_*) and no_os_udelay, and
    the delay backend pulls in no_os_div_u64, but src.mk never compiled the
    corresponding sources nor exposed their headers. This made the project
    fail to build. Add no_os_i2c.c, xilinx_delay.c and no_os_util.c to SRCS
    and no_os_i2c.h / no_os_util.h to INCS.
    
    Fixes: 5f50a6da59ca ("display_demo : adding display driver using ssd_1306 controller")
    Signed-off-by: Stefan Popa <stefan.popa@analog.com>

--- projects/display_demo/src.mk
+++ projects/display_demo/src.mk
@@ -4,19 +4,24 @@ SRCS += $(PROJECT)/src/app/main.c \
         $(DRIVERS)/display/display.c \
 	$(DRIVERS)/api/no_os_gpio.c \
         $(DRIVERS)/api/no_os_spi.c \
+        $(DRIVERS)/api/no_os_i2c.c \
         $(DRIVERS)/platform/xilinx/xilinx_spi.c \
         $(DRIVERS)/platform/xilinx/xilinx_gpio.c \
+        $(DRIVERS)/platform/xilinx/xilinx_delay.c \
 	$(NO-OS)/util/no_os_font_8x8.c \
         $(NO-OS)/util/no_os_alloc.c \
+        $(NO-OS)/util/no_os_util.c \
 	$(NO-OS)/util/no_os_mutex.c
 
 INCS += $(INCLUDE)/no_os_gpio.h \
 	$(INCLUDE)/no_os_spi.h \
+	$(INCLUDE)/no_os_i2c.h \
         $(DRIVERS)/display/ssd_1306/ssd_1306.h \
         $(DRIVERS)/display/display.h \
         $(PROJECT)/src/app/parameters.h \
         $(INCLUDE)/no_os_error.h \
         $(INCLUDE)/no_os_delay.h \
+        $(INCLUDE)/no_os_util.h \
         $(INCLUDE)/no_os_alloc.h \
         $(INCLUDE)/no_os_mutex.h \
         $(DRIVERS)/platform/xilinx/$(PLATFORM)_gpio.h \
