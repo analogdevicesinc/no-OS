ef8c854c758652ceb0ac93143b7c15eb18d1a4e4
Ramon Miguel Imbao <ramonmiguel.imbao@analog.com>
2026-07-01 08:51:23 +0800

    capi: platform: maxim: max32657: Add CAPI GPIO driver
    Add the implementation for CAPI GPIO driver
    
    Signed-off-by: Ramon Miguel Imbao <ramonmiguel.imbao@analog.com>

---
+++ capi/platform/maxim/max32657/maxim_capi_gpio_priv.h
@@ -0,0 +1,50 @@
+/***************************************************************************//**
+ *   @file   maxim_capi_gpio_priv.h
+ *   @brief  Header file for the GPIO private handle
+ *   @author Ramon Miguel Imbao (ramonmiguel.imbao@analog.com)
+********************************************************************************
+ * Copyright 2026(c) Analog Devices, Inc.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+*******************************************************************************/
+
+#ifndef MAXIM_CAPI_GPIO_PRIV_H_
+#define MAXIM_CAPI_GPIO_PRIV_H_
+
+#include "maxim_capi_gpio.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif /* __cplusplus */
+
+/**
+ * @struct max_capi_gpio_port_priv
+ * @brief Private structure for the GPIO port
+ */
+struct max_capi_gpio_port_priv {
+	/** Identifier */
+	uint32_t id;
+	/** The GPIO port */
+	mxc_gpio_regs_t *port;
+	/** Number of pins on the GPIO port */
+	uint8_t num_pins;
+	/** Bitmask for the GPIO port pins */
+	uint32_t pin_mask;
+	/** Bitmask for the GPIO port pin directions */
+	uint32_t direction_mask;
+	/** Platform-specific extra configuration */
+	struct max_capi_gpio_extra_config extra;
+};
+
+#define CAPI_GPIO_HANDLE_MAXIM_INIT()				\
+	(&(struct capi_gpio_port_handle) {			\
+		.ops = NULL,					\
+		.init_allocated = false,			\
+		.priv = &(struct max_capi_gpio_port_priv){0}	\
+	})
+
+#if defined(__cplusplus)
+}
+#endif /* __cplusplus */
+
+#endif /* MAXIM_CAPI_GPIO_PRIV_H_ */
