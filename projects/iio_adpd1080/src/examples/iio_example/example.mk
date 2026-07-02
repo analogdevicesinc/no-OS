58c1fb3d3f15f25b3c7bca43d2a68c74e4e09de9
Ramona Gradinariu <ramona.gradinariu@analog.com>
2026-07-02 16:54:54 +0300

    projects: iio_adpd1080: restruct to EXAMPLE build
    Move ADPD188 IIO app (incl. 32kHz calibration) to the EXAMPLE-based flow
    (common/examples/platform) mirroring eval-adxrs290-pmdz. Behavior
    unchanged.
    
    Signed-off-by: Ramona Gradinariu <ramona.gradinariu@analog.com>

---
+++ projects/iio_adpd1080/src/examples/iio_example/example.mk
@@ -0,0 +1,3 @@
+IIOD=y
+
+SRC_DIRS += $(NO-OS)/drivers/photo-electronic/adpd188
---
+++ projects/iio_adpd1080/src/platform/aducm3029/parameters.h
@@ -0,0 +1,78 @@
+/***************************************************************************//**
+ *   @file   parameters.h
+ *   @brief  Definitions specific to ADuCM3029 platform used by iio_adpd1080
+ *           project.
+ *   @author DBogdan (dragos.bogdan@analog.com)
+ *   @author Andrei Drimbarean (andrei.drimbarean@analog.com)
+********************************************************************************
+ * Copyright 2020(c) Analog Devices, Inc.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef __PARAMETERS_H__
+#define __PARAMETERS_H__
+
+#include "stdio.h"
+#include "platform_init.h"
+#include "aducm3029_gpio.h"
+#include "aducm3029_gpio_irq.h"
+#include "aducm3029_i2c.h"
+#include "aducm3029_irq.h"
+#include "aducm3029_timer.h"
+#include "aducm3029_uart.h"
+
+#define UART_DEVICE_ID	0
+#define UART_IRQ_ID		ADUCM_UART_INT_ID
+#define UART_BAUDRATE	115200
+#define UART_EXTRA      NULL
+#define UART_OPS        &aducm_uart_ops
+
+#define I2C_SLAVE_ADDR  0x64
+#define I2C_BAUDRATE    400000
+#define I2C_OPS         &aducm_i2c_ops
+#define I2C_EXTRA       NULL
+
+#define GPIO_OPS        &aducm_gpio_ops
+#define GPIO_EXTRA      NULL
+
+#define GPIO_SYNC_NUM   0x0D
+
+#define GPIO_IRQ_ID     ADUCM_GPIO_B_GROUP_SOFT_CTRL
+#define GPIO_IRQ_OPS    &aducm_gpio_irq_ops
+#define GPIO_IRQ_EXTRA  NULL
+
+#define TIMER_DEVICE_ID 0
+#define TIMER_FREQ_HZ   1
+#define TIMER_TICKS     0
+#define TIMER_OPS       &aducm_timer_ops
+#define TIMER_EXTRA     NULL
+
+#ifdef NO_OS_NETWORKING
+#define WIFI_SSID	"RouterSSID"
+#define WIFI_PWD	"******"
+#endif
+
+#endif /* __PARAMETERS_H__ */
