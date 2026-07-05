ecffb797e59f5d5480225fbdffdd3a0badb825a7
Ciprian Regus <ciprian.regus@analog.com>
2026-07-05 20:15:09 +0300

    drivers: net: oa_tc6: Add mutex locks for data and control transactions
    Both data and control transactions take some user data (register
    address/values or Ethernet frame data) and fill an intermediary buffer,
    that will be used during the SPI transfers as source/destination. These
    buffers are part of the oa_tc6_desc struct, thus will be accessed from
    every context that calls ctrl/data transfer functions. Add mutex locks
    to protect the buffer access.
    
    Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

--- drivers/net/oa_tc6/oa_tc6.c
+++ drivers/net/oa_tc6/oa_tc6.c
@@ -38,6 +38,7 @@
 
 #include "no_os_alloc.h"
 #include "no_os_delay.h"
+#include "no_os_mutex.h"
 #include "oa_tc6.h"
 
 int oa_rx_chunk_to_frame(struct oa_tc6_desc *desc, uint8_t *chunks,
@@ -103,14 +104,28 @@ static void oa_tc6_prepare_tx_ctrl(struct oa_tc6_desc *desc, uint32_t addr,
 	desc->ctrl_tx_credit++;
 }
 
-/**
- * @brief Read a register value.
- * @param desc - the OA TC6 descriptor.
- * @param addr - Register address.
- * @param val - Register value.
- * @return 0 in case of success, negative error code otherwise
- */
-int oa_tc6_reg_read(struct oa_tc6_desc *desc, uint32_t addr, uint32_t *val)
+static int oa_tc6_do_ctrl_transfer(struct oa_tc6_desc *desc)
+{
+	struct no_os_spi_msg xfer = {0};
+
+	if (desc->ctrl_rx_credit || desc->ctrl_tx_credit) {
+		xfer.tx_buff = desc->ctrl_chunks;
+		xfer.rx_buff = desc->ctrl_chunks;
+		xfer.cs_change = 1;
+
+		if (desc->prote_spi)
+			xfer.bytes_number = 2 * (OA_HEADER_LEN + OA_REG_LEN);
+		else
+			xfer.bytes_number = 2 * OA_HEADER_LEN + OA_REG_LEN;
+
+		return no_os_spi_transfer(desc->comm_desc, &xfer, 1);
+	}
+
+	return 0;
+}
+
+static int __oa_tc6_reg_read(struct oa_tc6_desc *desc, uint32_t addr,
+			     uint32_t *val)
 {
 	uint32_t comp_val;
 	int ret;
@@ -119,7 +134,7 @@ int oa_tc6_reg_read(struct oa_tc6_desc *desc, uint32_t addr, uint32_t *val)
 		return -ENODEV;
 
 	oa_tc6_prepare_rx_ctrl(desc, addr);
-	ret = oa_tc6_thread(desc);
+	ret = oa_tc6_do_ctrl_transfer(desc);
 	if (ret)
 		return ret;
 
@@ -142,7 +157,19 @@ int oa_tc6_reg_read(struct oa_tc6_desc *desc, uint32_t addr, uint32_t *val)
  * @param val - Register value.
  * @return 0 in case of success, negative error code otherwise
  */
-int oa_tc6_reg_write(struct oa_tc6_desc *desc, uint32_t addr, uint32_t val)
+int oa_tc6_reg_read(struct oa_tc6_desc *desc, uint32_t addr, uint32_t *val)
+{
+	int ret;
+
+	no_os_mutex_lock(desc->lock);
+	ret = __oa_tc6_reg_read(desc, addr, val);
+	no_os_mutex_unlock(desc->lock);
+
+	return ret;
+}
+
+static int __oa_tc6_reg_write(struct oa_tc6_desc *desc, uint32_t addr,
+			      uint32_t val)
 {
 	int ret;
 
@@ -150,13 +177,47 @@ int oa_tc6_reg_write(struct oa_tc6_desc *desc, uint32_t addr, uint32_t val)
 		return -ENODEV;
 
 	oa_tc6_prepare_tx_ctrl(desc, addr, val);
-	ret = oa_tc6_thread(desc);
+	ret = oa_tc6_do_ctrl_transfer(desc);
 	if (ret)
 		return ret;
 
 	desc->ctrl_tx_credit = 0;
 
 	return 0;
+}
+
+/**
+ * @brief Write a register value.
+ * @param desc - the OA TC6 descriptor.
+ * @param addr - Register address.
+ * @param val - Register value.
+ * @return 0 in case of success, negative error code otherwise
+ */
+int oa_tc6_reg_write(struct oa_tc6_desc *desc, uint32_t addr, uint32_t val)
+{
+	int ret;
+
+	no_os_mutex_lock(desc->lock);
+	ret = __oa_tc6_reg_write(desc, addr, val);
+	no_os_mutex_unlock(desc->lock);
+
+	return ret;
+}
+
+static int __oa_tc6_reg_update(struct oa_tc6_desc *desc, uint32_t addr,
+			       uint32_t val, uint32_t mask)
+{
+	uint32_t reg_val;
+	int ret;
+
+	ret = __oa_tc6_reg_read(desc, addr, &reg_val);
+	if (ret)
+		return ret;
+
+	reg_val &= ~mask;
+	reg_val |= val & mask;
+
+	return __oa_tc6_reg_write(desc, addr, reg_val);
 }
 
 /**
@@ -170,17 +231,13 @@ int oa_tc6_reg_write(struct oa_tc6_desc *desc, uint32_t addr, uint32_t val)
 int oa_tc6_reg_update(struct oa_tc6_desc *desc, uint32_t addr,
 		      uint32_t val, uint32_t mask)
 {
-	uint32_t reg_val;
 	int ret;
 
-	ret = oa_tc6_reg_read(desc, addr, &reg_val);
-	if (ret)
-		return ret;
+	no_os_mutex_lock(desc->lock);
+	ret = __oa_tc6_reg_update(desc, addr, val, mask);
+	no_os_mutex_unlock(desc->lock);
 
-	reg_val &= ~mask;
-	reg_val |= val & mask;
-
-	return oa_tc6_reg_write(desc, addr, reg_val);
+	return ret;
 }
 
 /**
@@ -616,7 +673,8 @@ static int oa_tc6_update_stats(struct oa_tc6_desc *desc)
 	uint32_t reg_val;
 	int ret;
 
-	ret = oa_tc6_reg_read(desc, OA_TC6_BUFST_REG, &reg_val);
+	/* Called from oa_tc6_thread() with desc->lock held; use the unlocked variant. */
+	ret = __oa_tc6_reg_read(desc, OA_TC6_BUFST_REG, &reg_val);
 	if (ret)
 		return ret;
 
@@ -664,22 +722,11 @@ int oa_tc6_thread(struct oa_tc6_desc *desc)
 	struct oa_tc6_frame_buffer *frame_buffer;
 	struct no_os_spi_msg xfer = {0};
 
-	if (desc->ctrl_rx_credit || desc->ctrl_tx_credit) {
-		xfer.tx_buff = desc->ctrl_chunks;
-		xfer.rx_buff = desc->ctrl_chunks;
-		xfer.cs_change = 1;
-
-		if (desc->prote_spi)
-			xfer.bytes_number = 2 * (OA_HEADER_LEN + OA_REG_LEN);
-		else
-			xfer.bytes_number = 2 * OA_HEADER_LEN + OA_REG_LEN;
-
-		return no_os_spi_transfer(desc->comm_desc, &xfer, 1);
-	}
+	no_os_mutex_lock(desc->lock);
 
 	ret = oa_tc6_update_stats(desc);
 	if (ret)
-		return ret;
+		goto unlock;
 
 	if (desc->data_tx_credit) {
 		ret = oa_tc6_get_first_tx_frame(desc, &frame_buffer);
@@ -700,13 +747,13 @@ int oa_tc6_thread(struct oa_tc6_desc *desc)
 		if (ret) {
 			memset(desc->data_chunks, 0, bytes_total);
 
-			return ret;
+			goto unlock;
 		}
 
 		ret = oa_tc6_rx_chunk_to_frame(desc, desc->data_chunks,
 					       bytes_total / (OA_CHUNK_SIZE + OA_HEADER_LEN));
 		if (ret)
-			return ret;
+			goto unlock;
 
 		ret = oa_tc6_get_first_tx_frame(desc, &frame_buffer);
 		if (!ret)
@@ -720,7 +767,10 @@ int oa_tc6_thread(struct oa_tc6_desc *desc)
 			break;
 	}
 
-	return 0;
+unlock:
+	no_os_mutex_unlock(desc->lock);
+
+	return ret;
 }
 
 /**
@@ -773,25 +823,30 @@ int oa_tc6_init(struct oa_tc6_desc **desc, struct oa_tc6_init_param *param)
 	descriptor->comm_desc = param->comm_desc;
 	descriptor->prote_spi = param->prote_spi;
 
+	no_os_mutex_init(&descriptor->lock);
+
 	ret = oa_tc6_sw_reset(descriptor);
 	if (ret)
 		goto free_lock;
 
 #if CONFIG_OA_ZERO_SWO_ONLY
 	/* For now, we'll only support receiving frames with SWO = 0 */
-	int ret = oa_tc6_reg_update(descriptor, OA_TC6_CONFIG0_REG,
-				    OA_TC6_CONFIG0_ZARFE_MASK,
-				    OA_TC6_CONFIG0_ZARFE_MASK);
-	if (ret) {
-		no_os_free(descriptor);
-
-		return ret;
-	}
+	ret = oa_tc6_reg_update(descriptor, OA_TC6_CONFIG0_REG,
+				OA_TC6_CONFIG0_ZARFE_MASK,
+				OA_TC6_CONFIG0_ZARFE_MASK);
+	if (ret)
+		goto free_lock;
 #endif
 
 	*desc = descriptor;
 
 	return 0;
+
+free_lock:
+	no_os_mutex_remove(descriptor->lock);
+	no_os_free(descriptor);
+
+	return ret;
 }
 
 /**
@@ -804,6 +859,7 @@ int oa_tc6_remove(struct oa_tc6_desc *desc)
 	if (!desc)
 		return -ENODEV;
 
+	no_os_mutex_remove(desc->lock);
 	no_os_free(desc);
 
 	return 0;
