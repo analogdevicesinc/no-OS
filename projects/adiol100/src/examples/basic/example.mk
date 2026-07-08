8bafe538ead2953f21194125b4d95edda5327ab7
Liviu Stan <liviu.stan@analog.com>
2026-07-08 16:05:06 +0300

    cmake port

--- projects/adiol100/src/examples/basic/example.mk
+++
@@ -1,4 +0,0 @@
-CFLAGS += -DBASIC_EXAMPLE=1
-
-SRCS += $(PROJECT)/src/examples/$(EXAMPLE)/basic_example.c
-INCS += $(PROJECT)/src/examples/$(EXAMPLE)/basic_example.h
--- projects/adiol100/src/platform/platform_includes.h
+++
@@ -1,6 +0,0 @@
-#ifndef __PLATFORM_INCLUDES_H__
-#define __PLATFORM_INCLUDES_H__
-#ifdef MAXIM_PLATFORM
-#include "maxim/parameters.h"
-#endif
-#endif
