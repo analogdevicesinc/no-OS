6e929f786880074b1f029485a843e7681af4cf3a
Cristina Suteu <cristina.suteu@analog.com>
2026-09-02 16:18:14 +0300

    projects/eval-ade7978: use capi_time
    replace no_os_delay with capi_time
    
    Signed-off-by: Cristina Suteu <cristina.suteu@analog.com>

--- projects/eval-ade7978/src/main.c
+++ projects/eval-ade7978/src/main.c
@@ -33,7 +33,7 @@
 #include <stdio.h>
 #include "no_os_uart.h"
 #include "no_os_pwm.h"
-#include "no_os_delay.h"
+#include "capi_time.h"
 #include "no_os_gpio.h"
 #include "no_os_spi.h"
 #include "no_os_print_log.h"
