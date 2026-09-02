333fad7ef2e8da3d6f8c5e656902dee9b28fb723
Cristina Suteu <cristina.suteu@analog.com>
2026-09-02 16:30:42 +0300

    projects/ltc3208: use capi_time
    replace no_os_delay with capi_time
    
    Signed-off-by: Cristina Suteu <cristina.suteu@analog.com>

--- projects/ltc3208/src/examples/basic_example/basic_example.c
+++ projects/ltc3208/src/examples/basic_example/basic_example.c
@@ -33,7 +33,7 @@
 
 #include "common_data.h"
 #include "ltc3208.h"
-#include "no_os_delay.h"
+#include "capi_time.h"
 #include "no_os_print_log.h"
 
 /*****************************************************************************
@@ -50,13 +50,13 @@ int test_rgb(struct ltc3208_dev *dev)
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_4_bit_dac(dev, GREEN_RED_REG, 0, 8);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_4_bit_dac(dev, GREEN_RED_REG, 0, 0);
 	if (ret)
@@ -66,7 +66,7 @@ int test_rgb(struct ltc3208_dev *dev)
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_4_bit_dac(dev, GREEN_RED_REG, 8, 8);
 	if (ret)
@@ -89,37 +89,37 @@ int test_aux(struct ltc3208_dev *dev)
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_all_aux_led_dac(dev, MAIN, MAIN, MAIN, MAIN);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_all_aux_led_dac(dev, SUB, SUB, SUB, SUB);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_all_aux_led_dac(dev, CAM, CAM, CAM, CAM);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_aux_led_dac(dev, MAIN, 2);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_aux_led_dac(dev, SUB, 1);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_aux_led_dac(dev, AUX, 3);
 	if (ret)
@@ -142,19 +142,19 @@ int test_cam(struct ltc3208_dev *dev)
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_cam_high(dev, true);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_cam_high(dev, false);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 
 	return 0;
@@ -178,13 +178,13 @@ int test_enrgbs(struct ltc3208_dev *dev)
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = no_os_gpio_set_value(dev->gpio_enrgbs_desc, NO_OS_GPIO_HIGH);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_sub_enable(dev, true);
 	if (ret)
@@ -194,13 +194,13 @@ int test_enrgbs(struct ltc3208_dev *dev)
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = no_os_gpio_set_value(dev->gpio_enrgbs_desc, NO_OS_GPIO_HIGH);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_sub_enable(dev, false);
 	if (ret)
@@ -210,13 +210,13 @@ int test_enrgbs(struct ltc3208_dev *dev)
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = no_os_gpio_set_value(dev->gpio_enrgbs_desc, NO_OS_GPIO_HIGH);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	return 0;
 }
@@ -247,25 +247,25 @@ int test_dropout(struct ltc3208_dev *dev)
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_4_bit_dac(dev, GREEN_RED_REG, 8, 13);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_4_bit_dac(dev, GREEN_RED_REG, 8, 9);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_4_bit_dac(dev, AUX_BLUE_REG, 8, 13);
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	ret = ltc3208_set_all_aux_led_dac(dev, AUX, AUX, AUX, AUX);
 	if (ret)
@@ -275,7 +275,7 @@ int test_dropout(struct ltc3208_dev *dev)
 	if (ret)
 		return ret;
 
-	no_os_mdelay(1000);
+	capi_wait_ms(1000);
 
 	return 0;
 }
@@ -300,49 +300,49 @@ int example_main()
 		if (ret)
 			goto free_dev;
 
-		no_os_mdelay(1000);
+		capi_wait_ms(1000);
 
 		ret = ltc3208_set_8_bit_dac(dev, MAIN_REG, 40);
 		if (ret)
 			goto free_dev;
 
-		no_os_mdelay(500);
+		capi_wait_ms(500);
 
 		ret = ltc3208_set_8_bit_dac(dev, SUB_REG, 20);
 		if (ret)
 			goto free_dev;
 
-		no_os_mdelay(500);
+		capi_wait_ms(500);
 
 		ret = test_cam(dev);
 		if (ret)
 			goto free_dev;
 
-		no_os_mdelay(1000);
+		capi_wait_ms(1000);
 
 		ret = test_rgb(dev);
 		if (ret)
 			goto free_dev;
 
-		no_os_mdelay(1000);
+		capi_wait_ms(1000);
 
 		ret = test_aux(dev);
 		if (ret)
 			goto free_dev;
 
-		no_os_mdelay(1000);
+		capi_wait_ms(1000);
 
 		ret = test_enrgbs(dev);
 		if (ret)
 			goto free_dev;
 
-		no_os_mdelay(1000);
+		capi_wait_ms(1000);
 
 		ret = test_dropout(dev);
 		if (ret)
 			goto free_dev;
 
-		no_os_mdelay(1000);
+		capi_wait_ms(1000);
 	}
 
 free_dev:
