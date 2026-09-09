480de27207a276ff52b5ebf3b3d579e1c186f5d6
Radu Sabau <radu.sabau@analog.com>
2026-09-09 15:30:08 +0300

    drivers: dac: ad9739a: rename ad_serdes_clk to ad9739a_operation_mode
    Header declared ad9739a_operation_mode; source defined ad_serdes_clk,
    whose own doc comment already described it as the baseband/mix-mode
    selector matching the header's intended name.
    
    Fixes: b03e549759 ("ad9739a: Update driver")
    Signed-off-by: Radu Sabau <radu.sabau@analog.com>

--- drivers/dac/ad9739a/ad9739a.c
+++ drivers/dac/ad9739a/ad9739a.c
@@ -153,8 +153,8 @@ int32_t ad9739a_power_down(struct ad9739a_dev *dev,
  *
  * @return Returns negative error code or 0 in case of success.
 *******************************************************************************/
-int32_t ad_serdes_clk(struct ad9739a_dev *dev,
-		      uint8_t mode)
+int32_t ad9739a_operation_mode(struct ad9739a_dev *dev,
+			       uint8_t mode)
 {
 	int32_t ret;
 	uint8_t reg_data;
