analogdevicesinc/no-OS

commit 4a8dcd9ebd097b1acf439096d259ef9095288f20

Use no_os functions for dynamic memory
Use no_os_clloc, no_os_malloc, and no_os_free for dynamic memory
management.

Signed-off-by: George Mois <george.mois@analog.com>

drivers/adc/ad9208/ad9208.c
@@ -47,6 +47,7 @@
 #include "ad9208.h"
 #include <inttypes.h>
 #include "api_def.h"
+#include "no_os_alloc.h"
 
 /**
  * Spi write and read compatible with ad9208 API
@@ -60,7 +61,7 @@ static int ad9208_spi_xfer(void *user_data, uint8_t *wbuf,
 			   uint8_t *rbuf, int len)
 {
 	struct no_os_spi_desc *spi = user_data;
-	uint8_t * buffer = (uint8_t *) malloc(len);
+	uint8_t * buffer = (uint8_t *) no_os_malloc(len);
 	int32_t ret;
 
 	memcpy(buffer, wbuf, 3);
@@ -71,7 +72,7 @@ static int ad9208_spi_xfer(void *user_data, uint8_t *wbuf,
 	else
 		memcpy(rbuf, buffer, len);
 
-	free(buffer);
+	no_os_free(buffer);
 
 	return ret;
 }
@@ -381,11 +382,11 @@ int32_t ad9208_initialize(ad9208_dev **device, ad9208_init_param *init_param)
 	ad9208_dev *dev;
 	int32_t i, ret;
 
-	dev = (ad9208_dev *)calloc(1, sizeof(*dev));
+	dev = (ad9208_dev *)no_os_calloc(1, sizeof(*dev));
 	if (!dev)
 		return -ENOMEM;
 
-	dev->st = (struct ad9208_state *)calloc(1, sizeof(*dev->st));
+	dev->st = (struct ad9208_state *)no_os_calloc(1, sizeof(*dev->st));
 	if (!dev->st) {
 		ret = -ENOMEM;
 		goto error;
@@ -408,19 +409,19 @@ int32_t ad9208_initialize(ad9208_dev **device, ad9208_init_param *init_param)
 	if (ret < 0)
 		goto error;
 
-	struct ad9208_state *st = (struct ad9208_state *)calloc(1, sizeof(*st));
+	struct ad9208_state *st = (struct ad9208_state *)no_os_calloc(1, sizeof(*st));
 	if (!st) {
 		ret = -ENOMEM;
 		goto error;
 	}
 
-	st->jesd_param = (jesd_param_t *)calloc(1, sizeof(*st->jesd_param));
+	st->jesd_param = (jesd_param_t *)no_os_calloc(1, sizeof(*st->jesd_param));
 	if (!st->jesd_param) {
 		ret = -ENOMEM;
 		goto error;
 	}
 
-	st->adc_h = (ad9208_handle_t *)calloc(1, sizeof(*st->adc_h));
+	st->adc_h = (ad9208_handle_t *)no_os_calloc(1, sizeof(*st->adc_h));
 	if (!st->adc_h) {
 		ret = -ENOMEM;
 		goto error;
@@ -482,19 +483,19 @@ int32_t ad9208_initialize(ad9208_dev **device, ad9208_init_param *init_param)
 
 error:
 	if (st->adc_h)
-		free(st->adc_h);
+		no_os_free(st->adc_h);
 	if (st->jesd_param)
-		free(st->jesd_param);
+		no_os_free(st->jesd_param);
 	if (st)
-		free(st);
+		no_os_free(st);
 	if (dev->gpio_powerdown)
 		no_os_gpio_remove(dev->gpio_powerdown);
 	if (dev->spi_desc)
 		no_os_spi_remove(dev->spi_desc);
 	if (dev->st)
-		free(dev->st);
+		no_os_free(dev->st);
 	if (dev)
-		free(dev);
+		no_os_free(dev);
 
 	return ret;
 }
@@ -507,11 +508,11 @@ int32_t ad9208_remove(ad9208_dev *device)
 	ret |= no_os_spi_remove(device->spi_desc);
 
 	if (device->st->adc_h)
-		free(device->st->adc_h);
+		no_os_free(device->st->adc_h);
 	if (device->st)
-		free(device->st);
+		no_os_free(device->st);
 	if (device)
-		free(device);
+		no_os_free(device);
 
 	return ret;
 }

commit d5fe265feaa9c5909c511455e06fbdcfa9b6ac2d

adc: ad9208: fix NULL dereference of device->st in remove
ad9208_remove() dereferenced device->st->adc_h before checking
device->st for NULL, crashing instead of skipping cleanup when st
was never allocated.

Fixes: 4a8dcd9ebd ("Use no_os functions for dynamic memory")
Signed-off-by: Radu Sabau <radu.sabau@analog.com>

drivers/adc/ad9208/ad9208.c
@@ -500,10 +500,11 @@ int32_t ad9208_remove(ad9208_dev *device)
 	ret = no_os_gpio_remove(device->gpio_powerdown);
 	ret |= no_os_spi_remove(device->spi_desc);
 
-	if (device->st->adc_h)
-		no_os_free(device->st->adc_h);
-	if (device->st)
+	if (device->st) {
+		if (device->st->adc_h)
+			no_os_free(device->st->adc_h);
 		no_os_free(device->st);
+	}
 	if (device)
 		no_os_free(device);
 