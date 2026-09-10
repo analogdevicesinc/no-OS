analogdevicesinc/no-OS

commit 02dbfb231809a846a486d978b8565374960c1211

drivers/platform/chibios: chibios_i2c
no_os i2c implementation in ChibiOS

Signed-off-by: rbudai <robert.budai@analog.com>

drivers/platform/chibios/chibios_i2c.c
@@ -1,0 +1,207 @@
+/***************************************************************************//**
+ *   @file   chibios/chibios_i2c.c
+ *   @brief  Implementation of chibios i2c driver.
+ *   @author Robert Budai (robert.budai@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include "no_os_util.h"
+#include "no_os_alloc.h"
+#include "no_os_i2c.h"
+#include "chibios_i2c.h"
+
+#if (HAL_USE_I2C==TRUE)
+
+/**
+ * @brief Initialize the I2C communication peripheral.
+ * @param desc - The I2C descriptor.
+ * @param param - The structure that contains the I2C parameters.
+ * @return 0 in case of success, -1 otherwise.
+ */
+int chibios_i2c_init(struct no_os_i2c_desc **desc,
+		     const struct no_os_i2c_init_param *param)
+{
+	int ret;
+	struct no_os_i2c_desc *descriptor;
+	struct chibios_i2c_desc *xdesc;
+
+	if (!desc || !param)
+		return -EINVAL;
+
+	descriptor = (struct no_os_i2c_desc *)no_os_calloc(1,
+			sizeof(struct no_os_i2c_desc));
+	if (!descriptor)
+		return -ENOMEM;
+
+	xdesc = (struct chibios_i2c_desc *)no_os_calloc(1,
+			sizeof(struct chibios_i2c_desc));
+	if (!xdesc) {
+		ret = -ENOMEM;
+		goto error_1;
+	}
+
+	descriptor->extra = xdesc;
+	descriptor->device_id = param->device_id;
+	descriptor->max_speed_hz = param->max_speed_hz;
+	descriptor->slave_address = param->slave_address;
+
+	xdesc->hi2c = ((struct chibios_i2c_init_param *)(param->extra))->hi2c;
+	xdesc->i2ccfg = ((struct chibios_i2c_init_param *)(param->extra))->i2ccfg;
+	xdesc->i2caddr = param->slave_address;
+
+	ret = i2cStart(xdesc->hi2c, xdesc->i2ccfg);
+	if (ret != I2C_NO_ERROR) {
+		ret = -EIO;
+		goto error_2;
+	}
+
+	*desc = descriptor;
+
+	return 0;
+error_2:
+	no_os_free(xdesc);
+error_1:
+	no_os_free(descriptor);
+	return ret;
+}
+
+/**
+ * @brief Free the resources allocated by no_os_i2c_init().
+ * @param desc - The I2C descriptor.
+ * @return 0 in case of success, -1 otherwise.
+ */
+int chibios_i2c_remove(struct no_os_i2c_desc *desc)
+{
+	struct chibios_i2c_desc *sdesc;
+
+	if (!desc || !desc->extra)
+		return -EINVAL;
+
+	sdesc = desc->extra;
+	i2cStop(sdesc->hi2c);
+	no_os_free(desc->extra);
+	no_os_free(desc);
+
+	return 0;
+}
+
+/**
+ * @brief I2C write transaction as master.
+ * @param desc - The I2C descriptor.
+ * @param data - The buffer with the data to transmit.
+ * @param bytes_number - Number of bytes in the buffer.
+ * @param stop_bit - Specifis whether to end the transaction with a stop bit.
+ * @return 0 in case of success, -1 otherwise.
+ */
+int chibios_i2c_write(struct no_os_i2c_desc *desc,
+		      uint8_t *data,
+		      uint8_t bytes_number,
+		      uint8_t stop_bit)
+{
+	int32_t ret;
+	struct chibios_i2c_desc *xdesc;
+
+	if (!desc || !desc->extra || !data)
+		return -EINVAL;
+
+	xdesc = desc->extra;
+
+	if (!stop_bit) {
+		chI2CBuffer = (uint8_t *)no_os_malloc(bytes_number*sizeof(uint8_t));
+		memcpy(chI2CBuffer, data, bytes_number);
+		buffSize = bytes_number;
+		ret = I2C_NO_ERROR;
+	} else {
+		ret = i2cMasterTransmitTimeout(xdesc->hi2c,  desc->slave_address, data,
+					       bytes_number, NULL, 0, TIME_INFINITE);
+	}
+
+	if (ret != I2C_NO_ERROR)
+		return -EIO;
+
+	return 0;
+}
+
+/**
+ * @brief I2C read transaction as master.
+ * @param desc - The I2C descriptor.
+ * @param data - The buffer where received data is to be stored.
+ * @param bytes_number - Number of bytes to receive.
+ * @param stop_bit - Specifis whether to end the transaction with a stop bit.
+ * @return 0 in case of success, -1 otherwise.
+ */
+
+int chibios_i2c_read(struct no_os_i2c_desc *desc,
+		     uint8_t *data,
+		     uint8_t bytes_number,
+		     uint8_t stop_bit)
+{
+	int32_t ret;
+	struct chibios_i2c_desc *xdesc;
+
+	if (!desc || !desc->extra || !data)
+		return -EINVAL;
+
+	xdesc = desc->extra;
+
+	if (!stop_bit) {
+//		no current implementation, (from no-os drivers only adxl372 uses consequtive reads without stop bit)
+		ret = I2C_NO_ERROR;
+	} else {
+		ret = i2cMasterTransmitTimeout(xdesc->hi2c,  desc->slave_address, chI2CBuffer,
+					       buffSize, data, bytes_number, TIME_INFINITE);
+		no_os_free(chI2CBuffer);
+		buffSize=0;
+	}
+
+	if (ret != I2C_NO_ERROR)
+		return -EIO;
+
+	return 0;
+}
+
+/**
+ * @brief chibios platform specific I2C platform ops structure
+ */
+const struct no_os_i2c_platform_ops chibios_i2c_ops = {
+	.i2c_ops_init = &chibios_i2c_init,
+	.i2c_ops_write = &chibios_i2c_write,
+	.i2c_ops_read = &chibios_i2c_read,
+	.i2c_ops_remove = &chibios_i2c_remove
+};
+
+#endif // HAL_USE_I2C

drivers/platform/chibios/chibios_i2c.h
@@ -1,0 +1,87 @@
+/***************************************************************************//**
+ *   @file   chibios/chibios_i2c.h
+ *   @brief  Header file for the chibios i2c driver.
+ *   @author Robert Budai (robert.budai@analog.com)
+********************************************************************************
+ * Copyright 2023(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef CHIBIOS_I2C_H_
+#define CHIBIOS_I2C_H_
+
+#include <stdint.h>
+#include "no_os_i2c.h"
+#include "hal.h"
+
+#if (HAL_USE_I2C==TRUE)
+
+/**
+ * @struct chibios_i2c_desc
+ * @brief chibios platform specific I2C descriptor iwth config and slave address parameters
+ */
+struct chibios_i2c_desc {
+	/** I2C instance */
+	I2CDriver *hi2c;
+	/** I2C init specific parameter*/
+	I2CConfig *i2ccfg;
+	/**I2C address*/
+	uint16_t i2caddr;
+};
+
+/**
+ * @struct chibios_i2c_init_param
+ * @brief ChibiOS i2c param struct
+ *
+ */
+struct chibios_i2c_init_param {
+	/** I2C instance*/
+	I2CDriver *hi2c;
+	/** I2C Config*/
+	I2CConfig *i2ccfg;
+};
+
+/**
+ * @brief chibios specific I2C platform ops structure
+ */
+extern const struct no_os_i2c_platform_ops chibios_i2c_ops;
+
+/**
+ * @brief global variable for temporary buffers for transmit and receive
+ *
+ */
+static uint8_t* chI2CBuffer;
+static uint16_t buffSize;
+
+#endif // HAL_USE_I2C==TRUE
+
+#endif // CHIBIOS_I2C_H_

commit 0b8a7fb09e848cd26894bc56f4f30efe7489a60d

drivers: platform: chibios: fix shared no-stop-bit I2C staging buffer
chI2CBuffer/buffSize were file-scope statics shared across every
chibios_i2c_desc instance, instead of per-descriptor state. A
no-stop-bit write on one I2C descriptor could clobber the staged
buffer/size of another descriptor's in-flight write-then-read
sequence. Moved both into struct chibios_i2c_desc.

Fixes: 02dbfb2318 ("drivers/platform/chibios: chibios_i2c")
Signed-off-by: Radu Sabau <radu.sabau@analog.com>

drivers/platform/chibios/chibios_i2c.c
@@ -135,9 +135,9 @@ int chibios_i2c_write(struct no_os_i2c_desc *desc,
 	xdesc = desc->extra;
 
 	if (!stop_bit) {
-		chI2CBuffer = (uint8_t *)no_os_malloc(bytes_number * sizeof(uint8_t));
-		memcpy(chI2CBuffer, data, bytes_number);
-		buffSize = bytes_number;
+		xdesc->buffer = (uint8_t *)no_os_malloc(bytes_number * sizeof(uint8_t));
+		memcpy(xdesc->buffer, data, bytes_number);
+		xdesc->buffer_size = bytes_number;
 		ret = I2C_NO_ERROR;
 	} else {
 		ret = i2cMasterTransmitTimeout(xdesc->hi2c,  desc->slave_address, data,
@@ -176,10 +176,10 @@ int chibios_i2c_read(struct no_os_i2c_desc *desc,
 //		no current implementation, (from no-os drivers only adxl372 uses consequtive reads without stop bit)
 		ret = I2C_NO_ERROR;
 	} else {
-		ret = i2cMasterTransmitTimeout(xdesc->hi2c,  desc->slave_address, chI2CBuffer,
-					       buffSize, data, bytes_number, TIME_INFINITE);
-		no_os_free(chI2CBuffer);
-		buffSize = 0;
+		ret = i2cMasterTransmitTimeout(xdesc->hi2c,  desc->slave_address, xdesc->buffer,
+					       xdesc->buffer_size, data, bytes_number, TIME_INFINITE);
+		no_os_free(xdesc->buffer);
+		xdesc->buffer_size = 0;
 	}
 
 	if (ret != I2C_NO_ERROR)

drivers/platform/chibios/chibios_i2c.h
@@ -50,6 +50,11 @@ struct chibios_i2c_desc {
 	I2CConfig *i2ccfg;
 	/**I2C address*/
 	uint16_t i2caddr;
+	/** Temporary buffer staged by a no-stop-bit write, consumed by the
+	 *  following no-stop-bit read */
+	uint8_t *buffer;
+	/** Number of bytes staged in buffer */
+	uint16_t buffer_size;
 };
 
 /**
@@ -68,13 +73,6 @@ struct chibios_i2c_init_param {
  * @brief chibios specific I2C platform ops structure
  */
 extern const struct no_os_i2c_platform_ops chibios_i2c_ops;
-
-/**
- * @brief global variable for temporary buffers for transmit and receive
- *
- */
-static uint8_t* chI2CBuffer;
-static uint16_t buffSize;
 
 #endif // HAL_USE_I2C==TRUE
 