26d76397535023a9df8fd786e00cedea797002f7
Ramon Miguel Imbao <ramonmiguel.imbao@analog.com>
2026-09-14 14:06:06 +0800

    drivers: platform: maxim: Update CPU flags for MAX32657
    Set COMMON_CPU_FLAGS depending on whether the target is a MAX32657 (Cortex-M33) or not
    
    Signed-off-by: Ramon Miguel Imbao <ramonmiguel.imbao@analog.com>

--- drivers/platform/maxim/toolchain.cmake
+++ drivers/platform/maxim/toolchain.cmake
@@ -77,8 +77,14 @@ set(CMAKE_EXECUTABLE_SUFFIX_ASM ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_C ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_CXX ".elf")
 
+# CPU flags depending on target. Nearly all Maxim parts are Cortex-M4. MAX32657 is Cortex-M33.
+if(TARGET_NUM STREQUAL "32657")
+    set(COMMON_CPU_FLAGS "-mthumb -mcpu=cortex-m33 -mfloat-abi=softfp -mfpu=fpv5-sp-d16 -mno-unaligned-access")
+else()
+    set(COMMON_CPU_FLAGS "-mthumb -mcpu=cortex-m4 -mfloat-abi=softfp -mfpu=fpv4-sp-d16")
+endif()
+
 # Common flags for all build types
-set(COMMON_CPU_FLAGS "-mthumb -mcpu=cortex-m4 -mfloat-abi=softfp -mfpu=fpv4-sp-d16")
 set(CMAKE_C_FLAGS "${COMMON_CPU_FLAGS} -ffunction-sections -fdata-sections -MD" CACHE STRING "C compiler flags" FORCE)
 set(CMAKE_CXX_FLAGS "${COMMON_CPU_FLAGS} -ffunction-sections -fdata-sections -MD" CACHE STRING "C++ compiler flags" FORCE)
 set(CMAKE_ASM_FLAGS "${COMMON_CPU_FLAGS} -x assembler-with-cpp" CACHE STRING "ASM compiler flags" FORCE)
