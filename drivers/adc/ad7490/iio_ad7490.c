analogdevicesinc/no-OS

commit 2ed123983d8410cc061e627ef605daafaa82dfe8

drivers: adc: ad7490 : Add IIO support for AD7490
Add initial header and source files for AD7490 no-OS IIO driver.

Signed-off-by: Radu Sabau <radu.sabau@analog.com>

drivers/adc/ad7490/iio_ad7490.c
@@ -1,0 +1,423 @@
+/**************************************************************************//**
+*   @file   iio_ad7490.c
+*   @brief  Source file of iio_ad7490
+*   @author Radu Sabau (radu.sabau@analog.com)
+*******************************************************************************
+* Copyright 2025(c) Analog Devices, Inc.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+*
+* 3. Neither the name of Analog Devices, Inc. nor the names of its
+*    contributors may be used to endorse or promote products derived from this
+*    software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+* EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+* OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+#include "iio_ad7490.h"
+#include "no_os_alloc.h"
+#include "no_os_error.h"
+
+#define AD7490_V_FS_2V5		2500
+#define AD7490_V_FS_5V		5000
+
+#define AD7490_CHANNEL(_idx) {			\
+	.ch_type = IIO_VOLTAGE,			\
+	.channel = _idx,			\
+	.indexed = true,			\
+	.attributes = ad7490_iio_attrs,		\
+	.address = _idx,			\
+	.scan_type = &ad7490_iio_scan_type,	\
+	.scan_index = _idx			\
+}
+
+static struct scan_type ad7490_iio_scan_type = {
+	.sign = 'u',
+	.realbits = 12,
+	.storagebits = 16,
+	.shift = 0,
+	.is_big_endian = false,
+};
+
+static const char *const ad7490_range_avail[2] = {
+	[AD7490_RANGE_2XREFIN] = "2X_REF_IN",
+	[AD7490_RANGE_REFIN] = "REF_IN",
+};
+
+static const char *const ad7490_polarity_avail[2] = {
+	[AD7490_CODING_TWOS] = "BIPOLAR",
+	[AD7490_CODING_BINARY] = "UNIPOLAR",
+};
+
+static int ad7490_read_samples(struct ad7490_desc *desc, int16_t *data,
+			       uint32_t samples)
+{
+	uint32_t i, sample_size;
+	int16_t channels_val[16];
+	int ret;
+
+	sample_size = desc->nb_channels_seq * sizeof(int16_t);
+
+	for (i = 0; i < samples; i++) {
+		ret = ad7490_read_seq(desc, data);
+		if (ret)
+			return ret;
+		data += sample_size;
+	}
+
+	return 0;
+}
+
+static int ad7490_iio_read_raw(void *device, char *buf, uint32_t len,
+			       const struct iio_ch_info *channel,
+			       intptr_t priv)
+{
+	struct ad7490_iio_desc *iio_desc = device;
+	int16_t val;
+	int ret;
+
+	ret = ad7490_read_ch(iio_desc->ad7490_desc, channel->address, &val);
+	if (ret)
+		return ret;
+
+	return iio_format_value(buf, len, IIO_VAL_INT, 1, &val);
+}
+
+static int ad7490_iio_read_scale(void *device, char *buf, uint32_t len,
+				 const struct iio_ch_info *channel,
+				 intptr_t priv)
+{
+	struct ad7490_iio_desc *iio_desc = device;
+	int32_t vals[2];
+	int16_t val;
+	int ret;
+
+	ret = ad7490_read_ch(iio_desc->ad7490_desc, channel->address, &val);
+	if (ret)
+		return ret;
+
+	if (no_os_field_get(AD7490_RANGE_MASK,
+			    iio_desc->ad7490_desc->cfg) == AD7490_RANGE_2XREFIN)
+		vals[0] = AD7490_V_FS_5V;
+	else
+		vals[0] = AD7490_V_FS_2V5;
+
+	vals[1] = ad7490_iio_scan_type.realbits;
+	ret = ad7490_read_ch(iio_desc->ad7490_desc, channel->address, &val);
+	if (ret)
+		return ret;
+
+	return iio_format_value(buf, len, IIO_VAL_FRACTIONAL_LOG2, 2, vals);
+}
+
+static int ad7490_iio_read_polarity(void *device, char *buf, uint32_t len,
+				    const struct iio_ch_info *channel,
+				    intptr_t priv)
+{
+	struct ad7490_iio_desc *desc = device;
+
+	return sprintf(buf, "%s ",
+		       ad7490_polarity_avail[no_os_field_get(AD7490_CODING_MASK,
+					     desc->ad7490_desc->cfg)]);
+}
+
+static int ad7490_iio_write_polarity(void *device, char *buf, uint32_t len,
+				     const struct iio_ch_info *channel,
+				     intptr_t priv)
+{
+	struct ad7490_iio_desc *desc = device;
+	enum ad7490_range range;
+	enum ad7490_dout_state dout_state;
+	uint32_t i;
+	int ret;
+
+	for (i = 0; i < NO_OS_ARRAY_SIZE(ad7490_polarity_avail); i++)
+		if (!strcmp(buf, ad7490_polarity_avail[i]))
+			break;
+
+	if (i == NO_OS_ARRAY_SIZE(ad7490_polarity_avail))
+		return -EINVAL;
+
+	range = no_os_field_get(AD7490_RANGE_MASK, desc->ad7490_desc->cfg);
+	dout_state = no_os_field_get(AD7490_WEAKTRI_MASK, desc->ad7490_desc->cfg);
+	ret = ad7490_config(desc->ad7490_desc, dout_state, range, i);
+	if (ret)
+		return ret;
+
+	desc->iio_dev->channels->scan_type->sign = (i == AD7490_CODING_BINARY) ? 'u' :
+			's';
+
+	return 0;
+}
+
+static int ad7490_iio_read_polarity_avail(void *device, char *buf, uint32_t len,
+		const struct iio_ch_info *channel,
+		intptr_t priv)
+{
+	uint32_t avail_size = NO_OS_ARRAY_SIZE(ad7490_polarity_avail);
+	uint32_t length = 0;
+	uint32_t i;
+
+	for (i = 0; i < avail_size; i++)
+		length += sprintf(buf + length, "%s ", ad7490_polarity_avail[i]);
+
+	return length;
+}
+
+static int ad7490_iio_read_range(void *device, char *buf, uint32_t len,
+				 const struct iio_ch_info *channel,
+				 intptr_t priv)
+{
+	struct ad7490_iio_desc *desc = device;
+
+	return sprintf(buf, "%s ", ad7490_range_avail[no_os_field_get(AD7490_RANGE_MASK,
+			desc->ad7490_desc->cfg)]);
+}
+
+static int ad7490_iio_write_range(void *device, char *buf, uint32_t len,
+				  const struct iio_ch_info *channel,
+				  intptr_t priv)
+{
+	struct ad7490_iio_desc *desc = device;
+	enum ad7490_coding coding;
+	enum ad7490_dout_state dout_state;
+	uint32_t i;
+
+	for (i = 0; i < NO_OS_ARRAY_SIZE(ad7490_range_avail); i++)
+		if (!strcmp(buf, ad7490_range_avail[i]))
+			break;
+
+	if (i == NO_OS_ARRAY_SIZE(ad7490_range_avail))
+		return -EINVAL;
+
+	coding = no_os_field_get(AD7490_CODING_MASK, desc->ad7490_desc->cfg);
+	dout_state = no_os_field_get(AD7490_WEAKTRI_MASK, desc->ad7490_desc->cfg);
+	return ad7490_config(desc->ad7490_desc, dout_state, i, coding);
+}
+
+static int ad7490_iio_read_range_avail(void *device, char *buf, uint32_t len,
+				       const struct iio_ch_info *channel,
+				       intptr_t priv)
+{
+	uint32_t avail_size = NO_OS_ARRAY_SIZE(ad7490_range_avail);
+	uint32_t length = 0;
+	uint32_t i;
+
+	for (i = 0; i < avail_size; i++)
+		length += sprintf(buf + length, "%s ", ad7490_range_avail[i]);
+
+	return length;
+}
+
+static int ad7490_iio_read_sampling_freq(void *device, char *buf, uint32_t len,
+		const struct iio_ch_info *channel,
+		intptr_t priv)
+{
+	int32_t val;
+
+	val = 1000000;
+
+	return iio_format_value(buf, len, IIO_VAL_INT, 1, &val);
+}
+
+static int ad7490_iio_pre_enable(void *device, uint32_t mask)
+{
+	struct ad7490_iio_desc *desc = device;
+
+	desc->active_channels = mask;
+	desc->no_of_active_channels = no_os_hweight16(mask);
+
+	return ad7490_start_seq(desc->ad7490_desc, AD7490_SHADOW_SEQ, mask, 0);
+}
+
+static int ad7490_iio_post_disable(void *device, uint32_t mask)
+{
+	struct ad7490_iio_desc *desc = device;
+
+	return ad7490_stop_seq(desc->ad7490_desc);
+}
+
+static int iio_ad7490_submit_buffer(struct iio_device_data *iio_dev_data)
+{
+	struct ad7490_iio_desc *iio_desc = iio_dev_data->dev;
+	struct ad7490_desc *desc = iio_desc->ad7490_desc;
+	struct iio_buffer *buffer = iio_dev_data->buffer;
+	uint32_t total_samples;
+	int16_t *push_data, channels_val[16] = {0};
+	uint32_t i = 0, j;
+	int ret;
+	void *buff;
+
+	ret = iio_buffer_get_block(buffer, &buff);
+	if (ret)
+		return ret;
+
+	total_samples = iio_desc->no_of_active_channels * buffer->samples;
+
+	push_data = buff;
+	while (i < total_samples) {
+		ret = ad7490_read_seq(desc, channels_val);
+		if (ret)
+			return ret;
+
+		for (j = 0; j < iio_desc->no_of_active_channels; j++) {
+			push_data[i] = channels_val[j];
+			i++;
+		}
+	}
+
+	return iio_buffer_block_done(buffer);
+}
+
+static int ad7490_iio_read_samples(void *device, int *buff, uint32_t samples)
+{
+	struct ad7490_iio_desc *desc = device;
+	int16_t channels_val[16];
+	uint32_t i = 0, j;
+	int ret;
+
+	while (i < samples * desc->no_of_active_channels) {
+		ret = ad7490_read_seq(desc->ad7490_desc, channels_val);
+		if (ret)
+			return ret;
+
+		for (j = 0; j < desc->no_of_active_channels; j++) {
+			buff[i] = channels_val[j];
+			i++;
+		}
+	}
+
+	return samples;
+}
+
+static struct iio_attribute ad7490_iio_attrs[] = {
+	{
+		.name = "raw",
+		.show = ad7490_iio_read_raw,
+	},
+	{
+		.name = "scale",
+		.show = ad7490_iio_read_scale,
+	},
+	END_ATTRIBUTES_ARRAY
+};
+
+static struct iio_attribute ad7490_global_attrs[] = {
+	{
+		.name = "polarity",
+		.show = ad7490_iio_read_polarity,
+		.store = ad7490_iio_write_polarity,
+		.shared = IIO_SHARED_BY_ALL,
+	},
+	{
+		.name = "polarity_available",
+		.show = ad7490_iio_read_polarity_avail,
+		.shared = IIO_SHARED_BY_ALL,
+	},
+	{
+		.name = "range",
+		.show = ad7490_iio_read_range,
+		.store = ad7490_iio_write_range,
+		.shared = IIO_SHARED_BY_ALL,
+	},
+	{
+		.name = "range_available",
+		.show = ad7490_iio_read_range_avail,
+		.shared = IIO_SHARED_BY_ALL,
+	},
+	{
+		.name = "sampling_frequency",
+		.show = ad7490_iio_read_sampling_freq,
+		.shared = IIO_SHARED_BY_ALL,
+	},
+	END_ATTRIBUTES_ARRAY
+};
+
+static struct iio_channel ad7490_channels[] = {
+	AD7490_CHANNEL(0),
+	AD7490_CHANNEL(1),
+	AD7490_CHANNEL(2),
+	AD7490_CHANNEL(3),
+	AD7490_CHANNEL(4),
+	AD7490_CHANNEL(5),
+	AD7490_CHANNEL(6),
+	AD7490_CHANNEL(7),
+	AD7490_CHANNEL(8),
+	AD7490_CHANNEL(9),
+	AD7490_CHANNEL(10),
+	AD7490_CHANNEL(11),
+	AD7490_CHANNEL(12),
+	AD7490_CHANNEL(13),
+	AD7490_CHANNEL(14),
+	AD7490_CHANNEL(15),
+};
+
+static struct iio_device ad7490_iio_dev = {
+	.num_ch = NO_OS_ARRAY_SIZE(ad7490_channels),
+	.channels = ad7490_channels,
+	.attributes = ad7490_global_attrs,
+	.read_dev = (int32_t (*)())ad7490_iio_read_samples,
+	.pre_enable = (int32_t (*)())ad7490_iio_pre_enable,
+	.post_disable = (int32_t (*)())ad7490_iio_post_disable,
+	.submit = (int32_t (*)())iio_ad7490_submit_buffer,
+};
+
+int ad7490_iio_init(struct ad7490_iio_desc **iio_desc,
+		    struct ad7490_iio_init_param *init_param)
+{
+	struct ad7490_iio_desc *iio_descriptor;
+	int ret;
+
+	iio_descriptor = (struct ad7490_iio_desc *)no_os_calloc(1,
+			 sizeof(*iio_descriptor));
+	if (!iio_descriptor)
+		return -ENOMEM;
+
+	iio_descriptor->iio_dev = &ad7490_iio_dev;
+	ret = ad7490_init(&iio_descriptor->ad7490_desc, init_param->ad7490_init);
+	if (ret)
+		goto free_desc;
+
+	ret = ad7490_config(iio_descriptor->ad7490_desc, init_param->dout_state,
+			    init_param->range, init_param->coding);
+	if (ret)
+		goto free_ad7490;
+
+	iio_descriptor->iio_dev->channels->scan_type->sign = (init_param->coding ==
+			AD7490_CODING_BINARY) ? 'u' : 's';
+
+	*iio_desc = iio_descriptor;
+
+	return 0;
+
+free_ad7490:
+	ad7490_remove(iio_descriptor->ad7490_desc);
+free_desc:
+	no_os_free(iio_descriptor);
+
+	return 0;
+}
+
+int ad7490_iio_remove(struct ad7490_iio_desc *iio_desc)
+{
+	ad7490_remove(iio_desc->ad7490_desc);
+	no_os_free(iio_desc);
+
+	return 0;
+}

drivers/adc/ad7490/iio_ad7490.h
@@ -1,0 +1,62 @@
+/**************************************************************************//**
+*   @file   iio_ad7490.h
+*   @brief  Header file of iio_ad7490
+*   @author Radu Sabau (radu.sabau@analog.com)
+*******************************************************************************
+* Copyright 2025(c) Analog Devices, Inc.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* 1. Redistributions of source code must retain the above copyright notice,
+*    this list of conditions and the following disclaimer.
+*
+* 2. Redistributions in binary form must reproduce the above copyright notice,
+*    this list of conditions and the following disclaimer in the documentation
+*    and/or other materials provided with the distribution.
+*
+* 3. Neither the name of Analog Devices, Inc. nor the names of its
+*    contributors may be used to endorse or promote products derived from this
+*    software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. “AS IS” AND ANY EXPRESS OR
+* IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+* EVENT SHALL ANALOG DEVICES, INC. BE LIABLE FOR ANY DIRECT, INDIRECT,
+* INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+* OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+* LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+* NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+* EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+******************************************************************************/
+
+#ifndef IIO_AD7490_H
+#define IIO_AD7490_H
+
+#include "iio.h"
+#include "iio_types.h"
+#include "ad7490.h"
+
+struct ad7490_iio_desc {
+	struct ad7490_desc *ad7490_desc;
+	struct iio_device *iio_dev;
+
+	uint32_t active_channels;
+	uint8_t no_of_active_channels;
+};
+
+struct ad7490_iio_init_param {
+	struct ad7490_init_param *ad7490_init;
+
+	enum ad7490_dout_state dout_state;
+	enum ad7490_range range;
+	enum ad7490_coding coding;
+};
+
+int ad7490_iio_init(struct ad7490_iio_desc **iio_desc,
+		    struct ad7490_iio_init_param *init_param);
+
+int ad7490_iio_remove(struct ad7490_iio_desc *iio_desc);
+
+#endif /** IIO_AD7490_H */

commit 0d94391f310f4bebc167f07f39059606ae699a78

adc: fix bugs across several ADC drivers
ad7490: error labels never checked/propagated ret, so init
returned 0 (success) even after a real failure.

ltc2358: SPI init failure was discarded and 0 (success) returned
instead of the actual error.

ad9467: ad9467_setup() ignored no_os_spi_init()'s return value;
ad9467_analog_input_coupling() checked coupling_mode == 7 instead
of == 1 for DC coupling; ad9467_transfer() polled the device-update
bit with no bound and could hang forever.

max11205: gpio_rdy was optional but dereferenced unconditionally in
the IRQ handler, remove(), and most of init; made it mandatory.

ad799x: no_os_i2c_init()'s return value was ignored and *device was
always set, so callers could get a non-NULL device with an
uninitialized i2c_desc, and dev was never freed on failure.

ad9208: ad9208_remove() dereferenced device->st->adc_h before
checking device->st for NULL.

ad9250: header declared ad9250_reset_PN29 while the source defines
ad9250_reset_pn9; renamed the declaration to match.

ad9434: ad9434_data_delay() was defined and documented but missing
from the header, making it unusable by external callers.

Fixes: 2ed123983d ("drivers: adc: ad7490 : Add IIO support for AD7490")
Fixes: 43e777daea ("drivers:adc: add ltc2358 support")
Fixes: d216339326 ("Added the AD9467 driver.")
Fixes: 2f9d3ec8d9 ("drivers:adc:max11205: Add driver for max11205 device")
Fixes: 45bacc11c6 ("PMODS: Added the PmodAD2 demonstration software.")
Fixes: 4a8dcd9ebd ("Use no_os functions for dynamic memory")
Fixes: 59e08071fb ("ad9250: Rename files and update comments")
Fixes: 1b3a6279de ("drivers\ad9434: Initial commit.")
Signed-off-by: Radu Sabau <radu.sabau@analog.com>

drivers/adc/ad7490/iio_ad7490.c
@@ -392,8 +392,7 @@ free_ad7490:
 	ad7490_remove(iio_descriptor->ad7490_desc);
 free_desc:
 	no_os_free(iio_descriptor);
-
-	return 0;
+	return ret;
 }
 
 int ad7490_iio_remove(struct ad7490_iio_desc *iio_desc)

drivers/adc/ad799x/ad799x.c
@@ -58,6 +58,10 @@ int8_t ad799x_init(struct ad799x_dev **device,
 
 	/* Initialize I2C peripheral. */
 	status = no_os_i2c_init(&dev->i2c_desc, &init_param.i2c_init);
+	if (status) {
+		no_os_free(dev);
+		return status;
+	}
 
 	/* Determine the number of bits available for a conversion. */
 	switch (init_param.part_number) {

drivers/adc/ad9208/ad9208.c
@@ -500,10 +500,11 @@ int32_t ad9208_remove(ad9208_dev *device)
 	ret = no_os_gpio_remove(device->gpio_powerdown);
 	ret |= no_os_spi_remove(device->spi_desc);
 
-	if (device->st->adc_h)
-		no_os_free(device->st->adc_h);
-	if (device->st)
+	if (device->st) {
+		if (device->st->adc_h)
+			no_os_free(device->st->adc_h);
 		no_os_free(device->st);
+	}
 	if (device)
 		no_os_free(device);
 

drivers/adc/ad9250/ad9250.h
@@ -553,8 +553,8 @@ int32_t ad9250_output_invert(struct ad9250_dev *dev,
 int32_t ad9250_output_format(struct ad9250_dev *dev,
 			     int32_t format);
 /*! Sets (1) or clears (0) the reset short PN sequence bit(PN9). */
-int32_t ad9250_reset_PN29(struct ad9250_dev *dev,
-			  int32_t rst);
+int32_t ad9250_reset_pn9(struct ad9250_dev *dev,
+			 int32_t rst);
 /*! Sets (1) or clears (0) the reset long PN sequence bit(PN23). */
 int32_t ad9250_reset_pn23(struct ad9250_dev *dev,
 			  int32_t rst);

drivers/adc/ad9434/ad9434.h
@@ -97,6 +97,8 @@ int32_t ad9434_spi_write(struct ad9434_dev *dev,
 			 uint8_t reg_data);
 int32_t ad9434_testmode_set(struct ad9434_dev *dev,
 			    uint8_t mode);
+int32_t ad9434_data_delay(struct ad9434_dev *dev,
+			  int16_t delay);
 int32_t ad9434_setup(struct ad9434_dev **device,
 		     struct ad9434_init_param init_param);
 int32_t ad9434_outputmode_set(struct ad9434_dev *dev,

drivers/adc/ad9467/ad9467.c
@@ -57,6 +57,8 @@ int32_t ad9467_setup(struct ad9467_dev **device,
 
 	/* SPI */
 	ret = no_os_spi_init(&dev->spi_desc, &init_param.spi_init);
+	if (ret < 0)
+		return ret;
 
 	/* Disable test mode. */
 	ret = ad9467_write(dev, AD9467_REG_TEST_IO, 0x00);
@@ -805,7 +807,7 @@ int32_t ad9467_analog_input_coupling(struct ad9467_dev *dev,
 	int32_t ret = 0;
 	uint8_t read;
 
-	if ((coupling_mode == 0) || (coupling_mode == 7)) {
+	if ((coupling_mode == 0) || (coupling_mode == 1)) {
 		ret = ad9467_set_bits_to_reg(dev,
 					     AD9467_REG_ANALOG_INPUT,
 					     coupling_mode *
@@ -940,6 +942,7 @@ int32_t ad9467_buffer_current_2(struct ad9467_dev *dev,
 *******************************************************************************/
 int32_t ad9467_transfer(struct ad9467_dev *dev)
 {
+	int32_t timeout = 0xFFFF;
 	int32_t ret = 0;
 	int8_t sw_bit = 0;
 	uint8_t read;
@@ -956,7 +959,12 @@ int32_t ad9467_transfer(struct ad9467_dev *dev)
 			return ret;
 		}
 		sw_bit = read & AD9467_REG_DEVICE_UPDATE;
-	} while (sw_bit == 1);
+		timeout--;
+	} while ((sw_bit == 1) && (timeout != 0));
+
+	if (sw_bit == 1) {
+		return -ETIMEDOUT;
+	}
 
 	return 0;
 }

drivers/adc/ltc2358/ltc2358.c
@@ -123,11 +123,11 @@ int32_t ltc2358_init(struct ltc2358_dev **device,
 
 	*device = dev;
 
-	return ret;
+	return 0;
 error:
 	no_os_free(dev);
 
-	return 0;
+	return ret;
 }
 
 /**

drivers/adc/max11205/max11205.c
@@ -84,6 +84,9 @@ int max11205_init(struct max11205_dev **device,
 	if (!init_param.irq_ctrl)
 		return -EINVAL;
 
+	if (!init_param.gpio_rdy)
+		return -EINVAL;
+
 	if (init_param.vref_mv > MAX11205_VREF_MAX_MV)
 		return -EINVAL;
 
@@ -104,15 +107,13 @@ int max11205_init(struct max11205_dev **device,
 	if (ret)
 		goto error_dev;
 
-	ret = no_os_gpio_get_optional(&dev->gpio_rdy, init_param.gpio_rdy);
+	ret = no_os_gpio_get(&dev->gpio_rdy, init_param.gpio_rdy);
 	if (ret)
 		goto error_spi;
 
-	if (dev->gpio_rdy) {
-		ret = no_os_gpio_direction_input(dev->gpio_rdy);
-		if (ret)
-			goto error_gpio;
-	}
+	ret = no_os_gpio_direction_input(dev->gpio_rdy);
+	if (ret)
+		goto error_gpio;
 
 	ret = no_os_irq_register_callback(init_param.irq_ctrl,
 					  dev->gpio_rdy->number, &irq_cb);